cdd14ba56a3898e80e73e72a0605f9d63b8ca774
Chi-Vien Ly <chivly@cisco.com>
2013-03-27 11:39:20 -0700

    This commit fixes the followings - Change routing.dijkstra_implementation to implement ITopologyManagerAware instead of IListenTopoUpdates - Correct the bundle bringup sequence using the init(), start() called by dependency manager - Add routing.dijkstra_implementation restartability through OSGI stop/start
    Change-Id: Ia624ab4ea51f4d35c5ffe31d14b1aed58ca123c3
    Signed-off-by: Chi-Vien Ly <chivly@cisco.com>

--- opendaylight/routing/dijkstra_implementation/src/main/java/org/opendaylight/controller/routing/dijkstra_implementation/internal/Activator.java
+++ opendaylight/routing/dijkstra_implementation/src/main/java/org/opendaylight/controller/routing/dijkstra_implementation/internal/Activator.java
@@ -18,9 +18,10 @@ import org.slf4j.LoggerFactory;
 import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
 import org.opendaylight.controller.sal.routing.IListenRoutingUpdates;
 import org.opendaylight.controller.sal.routing.IRouting;
-import org.opendaylight.controller.sal.topology.IListenTopoUpdates;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
 import org.opendaylight.controller.sal.reader.IReadService;
+import org.opendaylight.controller.topologymanager.ITopologyManager;
+import org.opendaylight.controller.topologymanager.ITopologyManagerAware;
 
 public class Activator extends ComponentActivatorAbstractBase {
     protected static final Logger logger = LoggerFactory
@@ -33,7 +34,6 @@ public class Activator extends ComponentActivatorAbstractBase {
      *
      */
     public void init() {
-        logger.debug("routing.dijkstra_implementation INIT called!");
     }
 
     /**
@@ -76,8 +76,8 @@ public class Activator extends ComponentActivatorAbstractBase {
         if (imp.equals(DijkstraImplementation.class)) {
             // export the service
             Dictionary<String, String> props = new Hashtable<String, String>();
-            props.put("salListenerName", "routing.Dijkstra");
-            c.setInterface(new String[] { IListenTopoUpdates.class.getName(),
+            props.put("topoListenerName", "routing.Dijkstra");
+            c.setInterface(new String[] { ITopologyManagerAware.class.getName(),
                     IRouting.class.getName() }, props);
 
             // Now lets add a service dependency to make sure the
@@ -90,6 +90,10 @@ public class Activator extends ComponentActivatorAbstractBase {
             c.add(createContainerServiceDependency(containerName).setService(
                     ISwitchManager.class).setCallbacks("setSwitchManager",
                     "unsetSwitchManager").setRequired(true));
+
+            c.add(createContainerServiceDependency(containerName).setService(
+                    ITopologyManager.class).setCallbacks("setTopologyManager",
+                    "unsetTopologyManager").setRequired(true));
 
             c.add(createContainerServiceDependency(containerName).setService(
                     IReadService.class).setCallbacks("setReadService",
