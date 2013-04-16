70a0cb0c28771588bab2ab669b1f82e57d5a8214
Andrew Kim <andrekim@cisco.com>
2013-04-16 15:35:12 -0700

    Refactor frontend JS
    Refactored one.js into lib.js and open.js. lib.js contains all the common library functions
    one.js and other page.js can use. open.js is the core engine that runs the web UI.
    
    Frontend is now extensible by automatically including  lib.js and overriding default JS engine.
    
    Change-Id: Ia6678a81a23266c6d0e1c1d56c6cec547f792487
    Signed-off-by: Andrew Kim <andrekim@cisco.com>

--- opendaylight/web/root/src/main/resources/WEB-INF/jsp/main.jsp
+++ opendaylight/web/root/src/main/resources/WEB-INF/jsp/main.jsp
@@ -5,162 +5,181 @@
  - terms of the Eclipse Public License v1.0 which accompanies this distribution, 
  - and is available at http://www.eclipse.org/legal/epl-v10.html
 --%>
-<%@ page import="java.net.URL" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import="java.net.URL"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- Bootstrap CSS - 1 -->
-	<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<!-- Bootstrap CSS - 1 -->
+<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-	<!-- Core CSS - 2 -->
-	<link rel="stylesheet/less" type="text/css" href="/css/one.less">
+<!-- Core CSS - 2 -->
+<link rel="stylesheet/less" type="text/css" href="/css/one.less">
 
-<c:set var="version" value="Version 0.1" scope="application"/>
-<c:set var="name" value="OpenDaylight" scope="application"/>
+<c:set var="version" value="Version 0.1" scope="application" />
+<c:set var="name" value="OpenDaylight" scope="application" />
 
 <%	
     String filePath = "/WEB-INF/jsp/custommain.jsp"; 
     URL fileURL = pageContext.getServletContext().getResource(filePath);
     if(fileURL!=null) {
 %>
-	  <jsp:include page="<%=filePath%>" flush="true"/>
+<jsp:include page="<%=filePath%>" flush="true" />
 <% } %>
 
-    <title>${name}</title>
-	<!-- jQuery - 1 -->
-	<script src="/js/jquery-1.9.1.min.js"></script>
-	
-	<!-- Bootstrap JS - 2 -->
-	<script src="/js/bootstrap.min.js"></script>
+<title>${name}</title>
+<!-- jQuery - 1 -->
+<script src="/js/jquery-1.9.1.min.js"></script>
 
-	<!-- LESS - 3 -->
-	<script type="text/javascript">
+<!-- Bootstrap JS - 2 -->
+<script src="/js/bootstrap.min.js"></script>
+
+<!-- LESS - 3 -->
+<script type="text/javascript">
 		less = {
 			env: "production"
 		};
 	</script>
-	<script src="/js/less-1.3.3.min.js"></script>
-	
-	<!-- Topology - 4 -->
-	<script src="/js/jit.js"></script>
+<script src="/js/less-1.3.3.min.js"></script>
+
+<!-- Topology - 4 -->
+<script src="/js/jit.js"></script>
 </head>
 <body>
 
-<!-- #menu -->
-<div id="menu" class="navbar navbar-fixed-top">
-	<div class="navbar-inner row-fluid">
-		<div class="span10">
-			<a class="brand" href="/" title="${version}">${name}</a> 
-			<ul class="nav nav-tabs">
-			</ul>
-		</div>
-		<div class="span2">
-			<div id="toolbar" class="btn-group">
-				<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-					<div class="icon-user"></div> ${username}
-					<span class="caret"></span>
-				</a>
-				<ul class="dropdown-menu">
-					<li><a href="#admin" id="admin" data-role="${role}"><div class="icon-users"></div> Users</a></li>
-					<li><a href="#save" id="save"><div class="icon-save"></div> Save</a></li>
-					<li><a href="#logout" id="logout"><div class="icon-logout"></div> Logout</a></li>
+	<!-- #menu -->
+	<div id="menu" class="navbar navbar-fixed-top">
+		<div class="navbar-inner row-fluid">
+			<div class="span10">
+				<a class="brand" href="/" title="${version}">${name}</a>
+				<ul class="nav nav-tabs">
 				</ul>
 			</div>
-		</div>
-	</div>
-</div><!-- END #menu -->
-
-<!-- #footer -->
-<div id="footer" class="navbar navbar-fixed-bottom">
-	<div class="navbar-inner row-fluid">
-		<div class="alert hide" id="alert">
-			<button type="button" class="close">&times;</button>
-			<p></p>
-		</div>
-	</div>
-</div><!-- END #footer -->
-
-<!-- #main -->
-<div id="main">
-	
-<!-- #left -->
-<div id="left">
-
-	<!-- #left-top -->
-	<div id="left-top">
-
-		<div class="dash">
-			<ul class="nav nav-tabs">
-			</ul>
-			<div class="dashlet row-fluid">
+			<div class="span2">
+				<div id="toolbar" class="btn-group">
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+						<div class="icon-user"></div> ${username} <span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#admin" id="admin" data-role="${role}"><div
+									class="icon-users"></div> Users</a></li>
+						<li><a href="#save" id="save"><div class="icon-save"></div>
+								Save</a></li>
+						<li><a href="#logout" id="logout"><div
+									class="icon-logout"></div> Logout</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
-
-	</div><!-- END #left-top -->
-
-	<!-- #left-bottom -->
-	<div id="left-bottom">
-
-		<div class="dash">
-			<ul class="nav nav-tabs">
-			</ul>
-			<div class="dashlet row-fluid">
-			</div>
-		</div>
-
-	</div><!-- END #left-bottom -->
-
-</div><!-- END #left -->
-
-<!-- #right -->
-<div id="right">
-
-	<!-- #right-top -->
-	<div id="right-top">
-
-		<div class="dash">
-			<div id="topology"></div>
-		</div>
-
-	</div><!-- END #right-top -->
-
-	<!-- #right-bottom -->
-	<div id="right-bottom">
-
-		<div class="dash">
-			<ul class="nav nav-tabs">
-			</ul>
-			<div class="dashlet row-fluid">
-			</div>
-		</div>
-
-	</div><!-- END #right-bottom -->
-
-</div><!-- END #right -->
-
-</div><!-- END #main -->
-
-<!-- modal -->
-<div id="modal" class="modal hide fade">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h3></h3>
 	</div>
-	<div class="modal-body"></div>
-	<div class="modal-footer"></div>
-</div>
-<!-- END modal -->
+	<!-- END #menu -->
 
-<!-- Core JS - 6 -->
-<script src="/js/one.js"></script>
+	<!-- #footer -->
+	<div id="footer" class="navbar navbar-fixed-bottom">
+		<div class="navbar-inner row-fluid">
+			<div class="alert hide" id="alert">
+				<button type="button" class="close">&times;</button>
+				<p></p>
+			</div>
+		</div>
+	</div>
+	<!-- END #footer -->
 
-<!-- Topology JS - 7 -->
-<script src="/js/one-topology.js"></script>
+	<!-- #main -->
+	<div id="main">
+
+		<!-- #left -->
+		<div id="left">
+
+			<!-- #left-top -->
+			<div id="left-top">
+
+				<div class="dash">
+					<ul class="nav nav-tabs">
+					</ul>
+					<div class="dashlet row-fluid"></div>
+				</div>
+
+			</div>
+			<!-- END #left-top -->
+
+			<!-- #left-bottom -->
+			<div id="left-bottom">
+
+				<div class="dash">
+					<ul class="nav nav-tabs">
+					</ul>
+					<div class="dashlet row-fluid"></div>
+				</div>
+
+			</div>
+			<!-- END #left-bottom -->
+
+		</div>
+		<!-- END #left -->
+
+		<!-- #right -->
+		<div id="right">
+
+			<!-- #right-top -->
+			<div id="right-top">
+
+				<div class="dash">
+					<div id="topology"></div>
+				</div>
+
+			</div>
+			<!-- END #right-top -->
+
+			<!-- #right-bottom -->
+			<div id="right-bottom">
+
+				<div class="dash">
+					<ul class="nav nav-tabs">
+					</ul>
+					<div class="dashlet row-fluid"></div>
+				</div>
+
+			</div>
+			<!-- END #right-bottom -->
+
+		</div>
+		<!-- END #right -->
+
+	</div>
+	<!-- END #main -->
+
+	<!-- modal -->
+	<div id="modal" class="modal hide fade">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal"
+				aria-hidden="true">&times;</button>
+			<h3></h3>
+		</div>
+		<div class="modal-body"></div>
+		<div class="modal-footer"></div>
+	</div>
+	<!-- END modal -->
+
+	<!-- Lib JS - 6 -->
+	<script src="/js/lib.js"></script>
+	<%	
+    String jsPath = "/WEB-INF/jsp/customjs.jsp"; 
+    URL jsURL = pageContext.getServletContext().getResource(jsPath);
+    if(jsURL!=null) {
+%>
+	<jsp:include page="<%=jsPath%>" flush="true" />
+	<% } else { %>
+	<!-- Core JS - 7 -->
+	<script src="/js/open.js"></script>
+
+	<!-- Topology JS - 8 -->
+	<script src="/js/open-topology.js"></script>
+	<% } %>
 
 </body>
 
---
+++ opendaylight/web/root/src/main/resources/js/open.js
@@ -0,0 +1,465 @@
+one.main = {};
+
+one.main.constants = {
+	address : {
+		menu : "/web.json",
+		prefix : "/controller/web",
+		save : "/save"
+	}
+}
+
+one.main.menu = {
+	load : function() {
+		one.main.menu.ajax(function(data) {
+			// reparse the ajax data
+			var result = one.main.menu.data.menu(data);
+			// transform into list to append to menu
+			var $div = one.main.menu.menu(result);
+			// append to menu
+			$("#menu .nav").append($div.children());
+			// binding all menu items
+			var $menu = $("#menu .nav a");
+			$menu.click(function() {
+				var href = $(this).attr('href').substring(1);
+				one.main.page.load(href);
+				var $li = $(this).parent();
+				// reset all other active
+				$menu.each(function(index, value) {
+					$(value).parent().removeClass('active');
+				});
+				$li.addClass('active');
+			});
+			// reset or go to first menu item by default
+			var currentLocation = location.hash;
+			if (data[currentLocation.substring(1)] == undefined) {
+				$($menu[0]).click();
+			} else {
+				$menu.each(function(index, value) {
+					var menuLocation = $(value).attr('href');
+					if (currentLocation == menuLocation) {
+						$($menu[index]).click();
+						return;
+					}
+				});
+			}
+		});
+	},
+	menu : function(result) {
+		var $div = $(document.createElement('div'));
+		$(result).each(function(index, value) {
+			if (value != undefined) {
+				var $li = $(document.createElement('li'));
+				var $a = $(document.createElement('a'));
+				$a.text(value['name']);
+				$a.attr('href', '#' + value['id']);
+				$li.append($a);
+				$div.append($li);
+			}
+		});
+		return $div;
+	},
+	ajax : function(successCallback) {
+		$.getJSON(one.main.constants.address.menu, function(data) {
+			successCallback(data);
+		});
+	},
+	data : {
+		menu : function(data) {
+			var result = [];
+			$.each(data, function(key, value) {
+				var order = value['order'];
+				if (order >= 0) {
+					var name = value['name'];
+					var entry = {
+						'name' : name,
+						'id' : key
+					};
+					result[order] = entry;
+				}
+			});
+			return result;
+		}
+	}
+}
+
+one.main.page = {
+	load : function(page) {
+		if (one.f !== undefined && one.f.cleanUp !== undefined) {
+			one.f.cleanUp();
+		}
+		// clear page related
+		delete one.f;
+		$('.dashlet', '#main').empty();
+		$('.nav', '#main').empty();
+		// fetch page's js
+		$.getScript(one.main.constants.address.prefix + "/" + page
+				+ "/js/page.js");
+
+		$.ajaxSetup({
+			data : {
+				'x-page-url' : page
+			}
+		});
+	},
+	dashlet : function($nav, dashlet) {
+		var $li = $(document.createElement('li'));
+		var $a = $(document.createElement('a'));
+		$a.text(dashlet.name);
+		$a.attr('id', dashlet.id);
+		$a.attr('href', '#');
+		$li.append($a);
+		$nav.append($li);
+	}
+}
+
+one.main.admin = {
+	id : {
+		modal : {
+			main : "one_main_admin_id_modal_main",
+			close : "one_main_admin_id_modal_close",
+			user : "one_main_admin_id_modal_user",
+			add : {
+				user : "one_main_admin_id_modal_add_user",
+				close : "one_main_admin_id_modal_add_close",
+				form : {
+					name : "one_main_admin_id_modal_add_form_name",
+					role : "one_main_admin_id_modal_add_form_role",
+					password : "one_main_admin_id_modal_add_form_password"
+				}
+			},
+			remove : {
+				user : "one_main_admin_id_modal_remove_user",
+				close : "one_main_admin_id_modal_remove_close"
+			}
+		},
+		add : {
+			user : "one_main_admin_id_add_user"
+		}
+	},
+	address : {
+		root : "/admin",
+		users : "/users"
+	},
+	modal : {
+		initialize : function(callback) {
+			var h3 = "Welcome " + $('#admin').text();
+			var footer = one.main.admin.modal.footer();
+			var $modal = one.lib.modal.spawn(one.main.admin.id.modal.main, h3,
+					'', footer);
+
+			// close binding
+			$('#' + one.main.admin.id.modal.close, $modal).click(function() {
+				$modal.modal('hide');
+			});
+
+			// body inject
+			one.main.admin.ajax.users(function($body) {
+				one.lib.modal.inject.body($modal, $body);
+			});
+
+			// modal show callback
+			callback($modal);
+		},
+		footer : function() {
+			var footer = [];
+
+			var closeButton = one.lib.dashlet.button.single("Close",
+					one.main.admin.id.modal.close, "", "");
+			var $closeButton = one.lib.dashlet.button.button(closeButton);
+			footer.push($closeButton);
+
+			return footer;
+		}
+	},
+	ajax : {
+		users : function(callback) {
+			$.getJSON(one.main.admin.address.root
+					+ one.main.admin.address.users, function(data) {
+				var body = one.main.admin.data.users(data);
+				var $body = one.main.admin.body.users(body);
+				callback($body);
+			});
+		}
+	},
+	data : {
+		users : function(data) {
+			var body = [];
+			$(data).each(function(index, value) {
+				var tr = {};
+				var entry = [];
+				entry.push(value['user']);
+				entry.push(value['role']);
+				tr['entry'] = entry;
+				tr['id'] = value['user'];
+				body.push(tr);
+			});
+			return body;
+		}
+	},
+	body : {
+		users : function(body) {
+			var $div = $(document.createElement('div'));
+			var $h5 = $(document.createElement('h5'));
+			$h5.append("Manage Users");
+			var attributes = [ "table-striped", "table-bordered",
+					"table-hover", "table-cursor" ];
+			var $table = one.lib.dashlet.table.table(attributes);
+			var headers = [ "User", "Role" ];
+			var $thead = one.lib.dashlet.table.header(headers);
+			var $tbody = one.lib.dashlet.table.body(body);
+			$table.append($thead).append($tbody);
+
+			// bind table
+			if (one.role < 2) {
+				$table.find('tr').click(function() {
+					var id = $(this).data('id');
+					one.main.admin.remove.modal.initialize(id);
+				});
+			}
+
+			// append to div
+			$div.append($h5).append($table);
+
+			if (one.role < 2) {
+				var addUserButton = one.lib.dashlet.button.single("Add User",
+						one.main.admin.id.add.user, "btn-primary", "btn-mini");
+				var $addUserButton = one.lib.dashlet.button
+						.button(addUserButton);
+				$div.append($addUserButton);
+
+				// add user binding
+				$addUserButton.click(function() {
+					one.main.admin.add.modal.initialize();
+				});
+			}
+
+			return $div;
+		}
+	},
+	remove : {
+		modal : {
+			initialize : function(id) {
+				var h3 = "Remove User";
+				var footer = one.main.admin.remove.footer();
+				var $body = one.main.admin.remove.body();
+				var $modal = one.lib.modal.spawn(one.main.admin.id.modal.user,
+						h3, $body, footer);
+
+				// close binding
+				$('#' + one.main.admin.id.modal.remove.close, $modal).click(
+						function() {
+							$modal.modal('hide');
+						});
+
+				// remove binding
+				$('#' + one.main.admin.id.modal.remove.user, $modal).click(
+						function() {
+							one.main.admin.remove.modal.ajax(id, function(
+									result) {
+								if (result == 'Success') {
+									$modal.modal('hide');
+									// body inject
+									var $admin = $('#'
+											+ one.main.admin.id.modal.main);
+									one.main.admin.ajax.users(function($body) {
+										one.lib.modal.inject
+												.body($admin, $body);
+									});
+								} else
+									alert("Failed to remove user: " + result);
+							});
+						});
+
+				$modal.modal();
+			},
+			ajax : function(id, callback) {
+				$.post(one.main.admin.address.root
+						+ one.main.admin.address.users + '/' + id, function(
+						data) {
+					callback(data);
+				});
+			},
+		},
+
+		footer : function() {
+			var footer = [];
+
+			var removeButton = one.lib.dashlet.button.single("Remove User",
+					one.main.admin.id.modal.remove.user, "btn-danger", "");
+			var $removeButton = one.lib.dashlet.button.button(removeButton);
+			footer.push($removeButton);
+
+			var closeButton = one.lib.dashlet.button.single("Close",
+					one.main.admin.id.modal.remove.close, "", "");
+			var $closeButton = one.lib.dashlet.button.button(closeButton);
+			footer.push($closeButton);
+
+			return footer;
+		},
+		body : function() {
+			var $p = $(document.createElement('p'));
+			$p.append("Remove user?");
+			return $p;
+		},
+	},
+	add : {
+		modal : {
+			initialize : function() {
+				var h3 = "Add User";
+				var footer = one.main.admin.add.footer();
+				var $body = one.main.admin.add.body();
+				var $modal = one.lib.modal.spawn(one.main.admin.id.modal.user,
+						h3, $body, footer);
+
+				// close binding
+				$('#' + one.main.admin.id.modal.add.close, $modal).click(
+						function() {
+							$modal.modal('hide');
+						});
+
+				// add binding
+				$('#' + one.main.admin.id.modal.add.user, $modal).click(
+						function() {
+							one.main.admin.add.modal.add($modal, function(
+									result) {
+								if (result == 'Success') {
+									$modal.modal('hide');
+									// body inject
+									var $admin = $('#'
+											+ one.main.admin.id.modal.main);
+									one.main.admin.ajax.users(function($body) {
+										one.lib.modal.inject
+												.body($admin, $body);
+									});
+								} else
+									alert("Failed to add user: " + result);
+							});
+						});
+
+				$modal.modal();
+			},
+			add : function($modal, callback) {
+				var user = {};
+				user['user'] = $modal.find(
+						'#' + one.main.admin.id.modal.add.form.name).val();
+				user['password'] = $modal.find(
+						'#' + one.main.admin.id.modal.add.form.password).val();
+				user['role'] = $modal.find(
+						'#' + one.main.admin.id.modal.add.form.role).find(
+						'option:selected').attr('value');
+
+				var resource = {};
+				resource['json'] = JSON.stringify(user);
+				resource['action'] = 'add'
+
+				one.main.admin.add.modal.ajax(resource, callback);
+			},
+			ajax : function(data, callback) {
+				$.post(one.main.admin.address.root
+						+ one.main.admin.address.users, data, function(data) {
+					callback(data);
+				});
+			}
+		},
+		body : function() {
+			var $form = $(document.createElement('form'));
+			var $fieldset = $(document.createElement('fieldset'));
+			// user
+			var $label = one.lib.form.label('Username');
+			var $input = one.lib.form.input('Username');
+			$input.attr('id', one.main.admin.id.modal.add.form.name);
+			$fieldset.append($label).append($input);
+			// password
+			var $label = one.lib.form.label('Password');
+			var $input = one.lib.form.input('Password');
+			$input.attr('id', one.main.admin.id.modal.add.form.password);
+			$input.attr('type', 'password');
+			$fieldset.append($label).append($input);
+			// roles
+			var $label = one.lib.form.label('Roles');
+			var options = {
+				"Network-Admin" : "Network Administrator",
+				"Network-Operator" : "Network Operator"
+			};
+			var $select = one.lib.form.select.create(options);
+			$select.attr('id', one.main.admin.id.modal.add.form.role);
+			$fieldset.append($label).append($select);
+			$form.append($fieldset);
+			return $form;
+		},
+		footer : function() {
+			var footer = [];
+
+			var addButton = one.lib.dashlet.button.single("Add User",
+					one.main.admin.id.modal.add.user, "btn-primary", "");
+			var $addButton = one.lib.dashlet.button.button(addButton);
+			footer.push($addButton);
+
+			var closeButton = one.lib.dashlet.button.single("Close",
+					one.main.admin.id.modal.add.close, "", "");
+			var $closeButton = one.lib.dashlet.button.button(closeButton);
+			footer.push($closeButton);
+
+			return footer;
+		}
+	}
+}
+
+one.main.dashlet = {
+	left : {
+		top : $("#left-top .dashlet"),
+		bottom : $("#left-bottom .dashlet")
+	},
+	right : {
+		bottom : $("#right-bottom .dashlet")
+	}
+}
+
+/** BOOTSTRAP */
+$(".modal").on('hidden', function() {
+	$(this).remove();
+});
+
+$("#alert .close").click(function() {
+	$("#alert").hide();
+});
+
+/** INIT */
+
+// parse role
+one.role = $('#admin').data('role');
+
+// user admin
+$("#admin").click(function() {
+	one.main.admin.modal.initialize(function($modal) {
+		$modal.modal();
+	});
+});
+
+// save
+$("#save").click(function() {
+	$.post(one.main.constants.address.save, function(data) {
+		if (data == "Success") {
+			one.lib.alert("Configuration Saved");
+		} else {
+			one.lib.alert("Unable to save configuration: " + data);
+		}
+	});
+});
+
+// logout
+$("#logout").click(function() {
+	location.href = "/logout";
+});
+
+$.ajaxSetup({
+	complete : function(xhr, textStatus) {
+		var mime = xhr.getResponseHeader('Content-Type');
+		if (mime.substring(0, 9) == 'text/html') {
+			location.href = '/';
+		}
+	}
+});
+
+/** MAIN PAGE LOAD */
+one.main.menu.load();
