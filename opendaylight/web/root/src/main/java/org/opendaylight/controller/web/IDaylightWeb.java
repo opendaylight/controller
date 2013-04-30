e687bbb842391bf94f7c23a3c58833cf53a2e438
Pramila Singh <pramisin@cisco.com>
2013-04-30 13:16:06 -0700

    Javadoc for controller.web
    Signed-off-by: Pramila Singh <pramisin@cisco.com>

--- opendaylight/web/root/src/main/java/org/opendaylight/controller/web/IDaylightWeb.java
+++ opendaylight/web/root/src/main/java/org/opendaylight/controller/web/IDaylightWeb.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
  *
@@ -12,15 +11,40 @@ package org.opendaylight.controller.web;
 import org.opendaylight.controller.sal.authorization.UserLevel;
 
 /**
- *
- *
+ * 
+ * @author andrekim
+ * 
  */
 public interface IDaylightWeb {
+    /**
+     * Returns the name of the bundle. In the GUI, this name will be displayed
+     * on the tab.
+     * 
+     * @return Name assigned to the bundle.
+     */
     public String getWebName();
 
+    /**
+     * Returns the Id assigned to the web bundle.
+     * 
+     * @return Id assigned to the web bundle.
+     */
     public String getWebId();
 
+    /**
+     * Returns the position where the bundle tab will be placed in the GUI.
+     * 
+     * @return Position number for the bundle tab.
+     */
     public short getWebOrder();
-    
+
+    /**
+     * This method checks if the user is authorized to access the bundle.
+     * 
+     * @param userLevel
+     *            user role level in the controller space.
+     * 
+     * @return true, if user is authorized to access the bundle, else false.
+     */
     public boolean isAuthorized(UserLevel userLevel);
 }
