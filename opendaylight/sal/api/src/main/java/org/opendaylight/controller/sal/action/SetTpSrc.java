fcab1998a98eef2d040118b71562a9e155977ae0
Ed Warnicke <eaw@cisco.com>
2013-05-15 11:17:48 -0500

    Six  more Equals/HashCode/StringBuilder replacements
    I'm intentionally breaking these up into small groups so they can
    be reasonably reviewed, rather than one giant group that can't.
    Contributes to fixing bug 20.
    
    Fourth in set.
    
    Change-Id: Ic24cd900b1b88a1496c5f0a14f8f24650c00b297
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/action/SetTpSrc.java
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/action/SetTpSrc.java
@@ -14,9 +14,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 /**
  * Set source transport port action
  *
@@ -47,13 +44,25 @@ public class SetTpSrc extends Action {
     }
 
     @Override
-    public boolean equals(Object other) {
-        return EqualsBuilder.reflectionEquals(this, other);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SetTpSrc other = (SetTpSrc) obj;
+        if (port != other.port)
+            return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + port;
+        return result;
     }
 
     @Override
--- opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/action/SetVlanPcp.java
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/action/SetVlanPcp.java
@@ -14,9 +14,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 /**
  * Set vlan PCP action
  */
@@ -46,13 +43,25 @@ public class SetVlanPcp extends Action {
     }
 
     @Override
-    public boolean equals(Object other) {
-        return EqualsBuilder.reflectionEquals(this, other);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SetVlanPcp other = (SetVlanPcp) obj;
+        if (pcp != other.pcp)
+            return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + pcp;
+        return result;
     }
 
     @Override
