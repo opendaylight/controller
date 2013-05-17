5bb507f89fcefee470c47d50bbd5d146a038ad64
Ed Warnicke <eaw@cisco.com>
2013-05-17 08:33:10 -0700

    Equals/HashCode/StringBuilder removal from Supported/PeerBandwidth
    Supported/PeerBandwidth does not add any new non-static members, so it should be
    safe to simply fall back to the parent classes equals/hashcode.
    
    Change-Id: I9951a7539b8371365266ac9fe49e286573748f06
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/core/PeerBandwidth.java
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/core/PeerBandwidth.java
@@ -11,9 +11,6 @@ package org.opendaylight.controller.sal.core;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 /**
  * @file   PeerBandWidth.java
  *
@@ -45,16 +42,7 @@ public class PeerBandwidth extends Bandwidth {
 		return new PeerBandwidth(this.bandwidthValue);  
     }
 	
-	@Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
-    }  
-    
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
