45917842fb93bd7b1553f13628abc5fdb3b805d6
Aditya Prakash Vaja <aditya.vaja@bigswitch.com>
2013-05-31 12:28:15 -0700

    Add 'TableStatistics' to SAL and Northbound Statistics API.
    It is similar to the FlowStats and PortStats (NodeConnectorStats) that are present now.
    
    Change-Id: Ibecb95c9c4a4f911e5fd3847bbf1f2e77eb16be1
    Signed-off-by: Aditya Prakash Vaja <aditya.vaja@bigswitch.com>

---
+++ opendaylight/protocol_plugins/openflow/src/main/java/org/opendaylight/controller/protocol_plugin/openflow/internal/TableStatisticsConverter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013 Big Switch Networks, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.protocol_plugin.openflow.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.reader.NodeTableStatistics;
+import org.opendaylight.controller.sal.utils.NodeCreator;
+import org.openflow.protocol.statistics.OFStatistics;
+import org.openflow.protocol.statistics.OFTableStatistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Aditya Prakash Vaja <aditya.vaja@bigswitch.com>
+ * Converts an openflow list of table statistics in a SAL list of
+ * NodeTableStatistics objects
+ */
+public class TableStatisticsConverter {
+    private static final Logger log = LoggerFactory
+            .getLogger(TableStatisticsConverter.class);
+
+    private final long switchId;
+    private List<OFStatistics> ofStatsList;
+    private List<NodeTableStatistics> ntStatsList;
+
+    public TableStatisticsConverter(long switchId, List<OFStatistics> statsList) {
+        this.switchId = switchId;
+        if (statsList == null || statsList.isEmpty()) {
+            this.ofStatsList = new ArrayList<OFStatistics>(1); // dummy list
+        } else {
+            this.ofStatsList = new ArrayList<OFStatistics>(statsList);
+        }
+        this.ntStatsList = null;
+    }
+
+    public List<NodeTableStatistics> getNodeTableStatsList() {
+        if (this.ofStatsList != null && this.ntStatsList == null) {
+            this.ntStatsList = new ArrayList<NodeTableStatistics>();
+            OFTableStatistics ofTableStat;
+            Node node = NodeCreator.createOFNode(switchId);
+            for (OFStatistics ofStat : this.ofStatsList) {
+                ofTableStat = (OFTableStatistics) ofStat;
+                NodeTableStatistics ntStat = new NodeTableStatistics();
+                ntStat.setNodeTable(TableConverter.toNodeTable(
+                        ofTableStat.getTableId(), node));
+                ntStat.setActiveCount(ofTableStat.getActiveCount());
+                ntStat.setLookupCount(ofTableStat.getLookupCount());
+                ntStat.setMatchedCount(ofTableStat.getMatchedCount());
+                this.ntStatsList.add(ntStat);
+            }
+        }
+        log.trace("OFStatistics: {} NodeTableStatistics: {}", ofStatsList,
+                ntStatsList);
+        return this.ntStatsList;
+    }
+}
---
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/reader/NodeTableStatistics.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2013 Big Switch Networks, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.reader;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.opendaylight.controller.sal.core.NodeTable;
+
+/**
+ * @author Aditya Prakash Vaja <aditya.vaja@bigswitch.com>
+ * Represents the Table statistics for the node
+ * 
+ */
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class NodeTableStatistics {
+    @XmlElement
+    private NodeTable nodeTable;
+    @XmlElement
+    private String name;
+    @XmlElement
+    private int activeCount;
+    @XmlElement
+    private long lookupCount;
+    @XmlElement
+    private long matchedCount;
+
+
+    //To Satisfy JAXB
+    public NodeTableStatistics() {
+
+    }
+
+    /**
+     * @return the node table
+     */
+    public NodeTable getNodeTable() {
+        return nodeTable;
+    }
+
+    /**
+     * @param table of the node
+     */
+    public void setNodeTable(NodeTable table) {
+        this.nodeTable = table;
+    }
+
+    /**
+     * @return name of the table
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name - set the table name to name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the activeCount
+     */
+    public int getActiveCount() {
+        return activeCount;
+    }
+
+    /**
+     * @param activeCount the activeCount to set
+     */
+    public void setActiveCount(int activeCount) {
+        this.activeCount = activeCount;
+    }
+
+    /**
+     * @return the lookupCount
+     */
+    public long getLookupCount() {
+        return lookupCount;
+    }
+
+    /**
+     * @param lookupCount the lookupCount to set
+     */
+    public void setLookupCount(long lookupCount) {
+        this.lookupCount = lookupCount;
+    }
+
+    /**
+     * @return the matchedCount
+     */
+    public long getMatchedCount() {
+        return matchedCount;
+    }
+
+    /**
+     * @param matchedCount the matchedCount to set
+     */
+    public void setMatchedCount(long matchedCount) {
+        this.matchedCount = matchedCount;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeTableStats[tableId = " + nodeTable
+                + ", activeCount = " + activeCount
+                + ", lookupCount = " + lookupCount
+                + ", matchedCount = " + matchedCount + "]";
+    }
+}
