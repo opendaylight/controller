0e53d658f7121b95f9f7536926a7486add969c22
Rob Adams <rob.adams@bigswitch.com>
2013-06-05 14:58:02 -0700

    Initial port of device manager to cisco controller.  Currently capable of tracking devices but still a long list of TODOs: - No VLAN support - Ethernet frame parser can't parse VLAN tags and   would be a bit of work to add it since current code can't handle   anything but fixed-format header field which doesn't cover 802.1Q - No shared framework for asynchronous tasks.  For now device manager   just allocates its own scheduled thread pool but a shared thread   pool would be much better. - No clustering support in this code.  There is code that uses an   eventually-consistent data store to support this but it's commented   out since this is not available in daylight.  It would be possible   to port this to the consistent store but this would lose valuable   semantics and a component in the core should handle eventual   consistency. - No web API support.  Would need to write a new implementation of the   API; shouldn't be too hard presumably - Debug counters not ported - No mechanism for documenting log messages.  This doesn't exist in   ODL. - DHCP parsing not supported; disable pulling hostname from DHCP. - Node/Port IDs are derived from Object and have no useful semantics.   Device manager needs them to be comparable in order to get   attachment points correct and consistent in all cases.  At the   moment it just blindly casts them - Topology module doesn't implement the needed semantics.  First need   to port net-virt-platform topology code, but there are impedance   mismatches in the APIs to resolve first - Need to merge host tracker and device manager APIs to a new unified   API and port all existing code to the new API, or adopt the device   manager API. - Need to replace ARP handler to just handle ARPs and not be   responsible for trying to discover devices - Unit tests need to be ported as well; currently commented out
    Signed-off-by: Rob Adams <rob.adams@bigswitch.com>

---
+++ opendaylight/hosttracker/implementation/src/test/java/org/opendaylight/controller/hosttracker/test/MockFlexEntityClassifier.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2013 Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.opendaylight.controller.hosttracker.test;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.opendaylight.controller.hosttracker.Entity;
+import org.opendaylight.controller.hosttracker.IDeviceService;
+import org.opendaylight.controller.hosttracker.IDeviceService.DeviceField;
+import org.opendaylight.controller.hosttracker.IEntityClass;
+import org.opendaylight.controller.hosttracker.internal.DefaultEntityClassifier;
+
+import static org.opendaylight.controller.hosttracker.IDeviceService.DeviceField.*;
+
+
+/**
+ * Extension to simple entity classifier to help in unit tests to provide table
+ * based multiple entity classification mock for reclassification tests
+ *
+ */
+public class MockFlexEntityClassifier extends DefaultEntityClassifier {
+	Map <Long, IEntityClass> switchEntities;
+	Map <Short, IEntityClass> vlanEntities;
+	
+    public static class TestEntityClass implements IEntityClass {
+    	String name;
+    	public TestEntityClass(String name) {
+    		this.name = name;
+    	}
+        @Override
+        public EnumSet<DeviceField> getKeyFields() {
+            return EnumSet.of(MAC);
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
+    public static IEntityClass defaultClass = new TestEntityClass("default");
+    public MockFlexEntityClassifier() {
+    	switchEntities = new HashMap<Long, IEntityClass> ();
+    	vlanEntities = new HashMap<Short, IEntityClass> ();
+    }
+    public IEntityClass createTestEntityClass(String name) {
+    	return new TestEntityClass(name);
+    }
+    
+    public void addSwitchEntity(Long dpid, IEntityClass entityClass) {
+    	switchEntities.put(dpid, entityClass);
+    }
+    public void removeSwitchEntity(Long dpid) {
+    	switchEntities.remove(dpid);
+    }
+    public void addVlanEntities(Short vlan, IEntityClass entityClass) {
+    	vlanEntities.put(vlan, entityClass);
+    }
+    public void removeVlanEntities(Short vlan) {
+    	vlanEntities.remove(vlan);
+    }
+    @Override
+    public IEntityClass classifyEntity(Entity entity) {
+        if (switchEntities.containsKey((Long)entity.getPort().getNode().getID()))
+        	return switchEntities.get((Long)entity.getPort().getNode().getID());
+        if (vlanEntities.containsKey(entity.getVlan()))
+        	return vlanEntities.get(entity.getVlan());
+        return defaultClass;
+    }
+    @Override
+    public EnumSet<IDeviceService.DeviceField> getKeyFields() {
+        return EnumSet.of(MAC, VLAN, SWITCHPORT);
+    }
+}
