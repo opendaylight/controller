77bcc51ea5194675d3ab73abc2ed9298e682d065
patrichu <patrichu@cisco.com>
2013-06-08 23:36:31 -0700

    SwitchNorthbound tests
    Added tests for all available SwitchManager REST APIs.
    Made changes to stub InventoryService to allow for updating nodeProps and nodeConnectorProps.
    Made changes to SwitchNorthbound.java to check for both nodeType and nodeId when getting a Node.
    Made changes in all northbound tests to throw JSONException instead of simple assertion failure.
    
    Change-Id: I816dcbaea2ed10833e3270395fe894e5c152f5d8
    Signed-off-by: patrichu <patrichu@cisco.com>

--- opendaylight/protocol_plugins/stub/src/main/java/org/opendaylight/controller/protocol_plugins/stub/internal/InventoryService.java
+++ opendaylight/protocol_plugins/stub/src/main/java/org/opendaylight/controller/protocol_plugins/stub/internal/InventoryService.java
@@ -42,9 +42,19 @@ public class InventoryService implements IPluginInInventoryService {
     private static final Logger logger = LoggerFactory
             .getLogger(InventoryService.class);
 
-    private ConcurrentMap<Node, Map<String, Property>> nodeProps; // properties are maintained in global container only
-    private ConcurrentMap<NodeConnector, Map<String, Property>> nodeConnectorProps; // properties are maintained in global container only
-
+    private ConcurrentMap<Node, Map<String, Property>> nodeProps; // properties
+                                                                  // are
+                                                                  // maintained
+                                                                  // in global
+                                                                  // container
+                                                                  // only
+    private ConcurrentMap<NodeConnector, Map<String, Property>> nodeConnectorProps; // properties
+                                                                                    // are
+                                                                                    // maintained
+                                                                                    // in
+                                                                                    // global
+                                                                                    // container
+                                                                                    // only
 
     /**
      * Function called by the dependency manager when all the required
@@ -55,7 +65,95 @@ public class InventoryService implements IPluginInInventoryService {
         nodeProps = new ConcurrentHashMap<Node, Map<String, Property>>();
         nodeConnectorProps = new ConcurrentHashMap<NodeConnector, Map<String, Property>>();
         Node.NodeIDType.registerIDType("STUB", Integer.class);
-        NodeConnector.NodeConnectorIDType.registerIDType("STUB", Integer.class, "STUB");
+        NodeConnector.NodeConnectorIDType.registerIDType("STUB", Integer.class,
+                "STUB");
+
+        setupNodeProps();
+        setupNodeConnectorProps();
+    }
+
+    private void setupNodeConnectorProps() {
+        Map<String, Property> ncPropMap = new HashMap<String, Property>();
+        Capabilities cap = new Capabilities(
+                CapabilitiesType.FLOW_STATS_CAPABILITY.getValue());
+        ncPropMap.put(Capabilities.CapabilitiesPropName, cap);
+        Bandwidth bw = new Bandwidth(Bandwidth.BW1Gbps);
+        ncPropMap.put(Bandwidth.BandwidthPropName, bw);
+        State st = new State(State.EDGE_UP);
+        ncPropMap.put(State.StatePropName, st);
+
+        // setup property map for all node connectors
+        NodeConnector nc;
+        Node node;
+        try {
+            node = new Node("STUB", new Integer(0xCAFE));
+            nc = new NodeConnector("STUB", 0xCAFE, node);
+        } catch (ConstructionException e) {
+            nc = null;
+            node = null;
+        }
+        nodeConnectorProps.put(nc, ncPropMap);
+
+        try {
+            node = new Node("STUB", 3366);
+            nc = new NodeConnector("STUB", 12, node);
+        } catch (ConstructionException e) {
+            nc = null;
+            node = null;
+        }
+        nodeConnectorProps.put(nc, ncPropMap);
+
+        try {
+            node = new Node("STUB", 4477);
+            nc = new NodeConnector("STUB", 34, node);
+        } catch (ConstructionException e) {
+            nc = null;
+            node = null;
+        }
+        nodeConnectorProps.put(nc, ncPropMap);
+
+    }
+
+    private void setupNodeProps() {
+        Map<String, Property> propMap = new HashMap<String, Property>();
+
+        Tables t = new Tables((byte) 1);
+        propMap.put(Tables.TablesPropName, t);
+        Capabilities c = new Capabilities((int) 3);
+        propMap.put(Capabilities.CapabilitiesPropName, c);
+        Actions a = new Actions((int) 2);
+        propMap.put(Actions.ActionsPropName, a);
+        Buffers b = new Buffers((int) 1);
+        propMap.put(Buffers.BuffersPropName, b);
+        Long connectedSinceTime = 100000L;
+        TimeStamp timeStamp = new TimeStamp(connectedSinceTime,
+                "connectedSince");
+        propMap.put(TimeStamp.TimeStampPropName, timeStamp);
+
+        // setup property map for all nodes
+        Node node;
+        try {
+            node = new Node("STUB", new Integer(0xCAFE));
+        } catch (ConstructionException e) {
+            node = null;
+        }
+
+        nodeProps.put(node, propMap);
+
+        try {
+            node = new Node("STUB", 3366);
+        } catch (ConstructionException e) {
+            node = null;
+        }
+        nodeProps.put(node, propMap);
+
+        try {
+            node = new Node("STUB", 4477);
+        } catch (ConstructionException e) {
+            node = null;
+        }
+        nodeProps.put(node, propMap);
+
     }
 
     /**
@@ -89,47 +187,6 @@ public class InventoryService implements IPluginInInventoryService {
      */
     @Override
     public ConcurrentMap<Node, Map<String, Property>> getNodeProps() {
-
-        //  setup nodeProps
-        Map<String, Property> propMap = new HashMap<String, Property>();
-
-        Tables t = new Tables((byte)1);
-        propMap.put(Tables.TablesPropName, t);
-        Capabilities c = new Capabilities((int)3);
-        propMap.put(Capabilities.CapabilitiesPropName, c);
-        Actions a = new Actions((int)2);
-        propMap.put(Actions.ActionsPropName, a);
-        Buffers b = new Buffers((int)1);
-        propMap.put(Buffers.BuffersPropName, b);
-        Long connectedSinceTime = 100000L;
-        TimeStamp timeStamp = new TimeStamp(connectedSinceTime,
-                "connectedSince");
-        propMap.put(TimeStamp.TimeStampPropName, timeStamp);
-
-        // setup property map for all nodes
-        Node node;
-        try{
-            node = new Node("STUB", new Integer(0xCAFE));
-        }catch(ConstructionException e){
-            node = null;
-        }
-
-        nodeProps.put(node, propMap);
-
-        try{
-            node = new Node("STUB", 3366);
-        }catch(ConstructionException e){
-            node = null;
-        }
-        nodeProps.put(node, propMap);
-
-        try{
-            node = new Node("STUB", 4477);
-        }catch(ConstructionException e){
-            node = null;
-        }
-        nodeProps.put(node, propMap);
-
         return nodeProps;
     }
 
@@ -139,49 +196,7 @@ public class InventoryService implements IPluginInInventoryService {
     @Override
     public ConcurrentMap<NodeConnector, Map<String, Property>> getNodeConnectorProps(
             Boolean refresh) {
-
-        //  setup nodeConnectorProps
-        Map<String, Property> ncPropMap = new HashMap<String, Property>();
-        Capabilities cap = new Capabilities
-                (CapabilitiesType.FLOW_STATS_CAPABILITY.getValue());
-        ncPropMap.put(Capabilities.CapabilitiesPropName, cap);
-        Bandwidth bw = new Bandwidth (Bandwidth.BW1Gbps);
-        ncPropMap.put(Bandwidth.BandwidthPropName, bw);
-        State st = new State (State.EDGE_UP);
-        ncPropMap.put(State.StatePropName, st);
-
-        // setup property map for all node connectors
-        NodeConnector nc;
-        Node node;
-        try{
-            node = new Node("STUB", new Integer(0xCAFE));
-            nc = new NodeConnector("STUB", 0xCAFE, node);
-        }catch(ConstructionException e){
-            nc = null;
-            node = null;
-        }
-        nodeConnectorProps.put(nc, ncPropMap);
-
-        try{
-            node = new Node("STUB", 3366);
-            nc = new NodeConnector("STUB", 12, node);
-         } catch(ConstructionException e){
-            nc = null;
-            node = null;
-         }
-         nodeConnectorProps.put(nc, ncPropMap);
-
-         try{
-            node = new Node("STUB", 4477);
-            nc = new NodeConnector("STUB", 34, node);
-         }catch(ConstructionException e){
-            nc = null;
-            node = null;
-         }
-         nodeConnectorProps.put(nc, ncPropMap);
-
-         return nodeConnectorProps;
+        return nodeConnectorProps;
     }
-
 
 }
