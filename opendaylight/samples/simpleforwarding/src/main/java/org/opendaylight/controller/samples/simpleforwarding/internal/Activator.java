770bd445c5de20e24f176533404da193761a7af9
Colin Dixon <ckd@us.ibm.com>
2013-06-10 11:00:18 -0500

    Adding a simple broadcast hander
    This adds:
    1.) An interface IBroadcastHandler for enabbling/disabling and controlling
        the mode of operation for a module that provides broadcast handling and
        a function to broadcast a packet.
    2.) An implementation of that interface, SimpleBroadcastHandler, that both
        handles broadcast traffic received from the DataPacketService and
        provides a way to braodcast a synthetically-generated packet.
    
    There are four modes only two of which have currently been extensively
    tested: DISABLED and BROADCAST_TO_HOSTS.
    
    Change-Id: I6ab6042ad7ec08b819a8840e8518022c7805f4e2
    Signed-off-by: Colin Dixon <ckd@us.ibm.com>

--- opendaylight/samples/simpleforwarding/src/main/java/org/opendaylight/controller/samples/simpleforwarding/internal/Activator.java
+++ opendaylight/samples/simpleforwarding/src/main/java/org/opendaylight/controller/samples/simpleforwarding/internal/Activator.java
@@ -9,6 +9,9 @@
 
 package org.opendaylight.controller.samples.simpleforwarding.internal;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.apache.felix.dm.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +21,12 @@ import org.opendaylight.controller.forwardingrulesmanager.IForwardingRulesManage
 import org.opendaylight.controller.hosttracker.IfIptoHost;
 import org.opendaylight.controller.hosttracker.IfNewHostNotify;
 import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
+import org.opendaylight.controller.sal.packet.IDataPacketService;
+import org.opendaylight.controller.sal.packet.IListenDataPacket;
 import org.opendaylight.controller.sal.routing.IListenRoutingUpdates;
 import org.opendaylight.controller.sal.routing.IRouting;
+import org.opendaylight.controller.samples.simpleforwarding.IBroadcastHandler;
+import org.opendaylight.controller.samples.simpleforwarding.IBroadcastPortSelector;
 import org.opendaylight.controller.switchmanager.IInventoryListener;
 import org.opendaylight.controller.switchmanager.ISwitchManager;
 import org.opendaylight.controller.topologymanager.ITopologyManager;
@@ -39,7 +46,8 @@ public class Activator extends ComponentActivatorAbstractBase {
      * Object
      */
     public Object[] getImplementations() {
-        Object[] res = { SimpleForwardingImpl.class };
+        Object[] res = { SimpleForwardingImpl.class,
+                         SimpleBroadcastHandlerImpl.class };
         return res;
     }
 
@@ -88,6 +96,29 @@ public class Activator extends ComponentActivatorAbstractBase {
             c.add(createContainerServiceDependency(containerName).setService(
                     IRouting.class).setCallbacks("setRouting", "unsetRouting")
                     .setRequired(false));
+        }else if (imp.equals(SimpleBroadcastHandlerImpl.class)) {
+            Dictionary<String, String> props = new Hashtable<String, String>();
+            props.put("salListenerName", "simplebroadcasthandler");
+
+            // export the service
+            c.setInterface(new String[] { IBroadcastHandler.class.getName(),
+                    IListenDataPacket.class.getName() }, props);
+
+            c.add(createContainerServiceDependency(containerName).setService(
+                    IDataPacketService.class).setCallbacks("setDataPacketService",
+                   "unsetDataPacketService").setRequired(false));
+
+            c.add(createContainerServiceDependency(containerName).setService(
+                   ITopologyManager.class).setCallbacks("setTopologyManager",
+                   "unsetTopologyManager").setRequired(true));
+
+            c.add(createContainerServiceDependency(containerName).setService(
+                   IBroadcastPortSelector.class).setCallbacks("setBroadcastPortSelector",
+                   "unsetBroadcastPortSelector").setRequired(false));
+
+            c.add(createContainerServiceDependency(containerName).setService(
+                   ISwitchManager.class).setCallbacks("setSwitchManager",
+                   "unsetSwitchManager").setRequired(false));
         }
     }
 }
