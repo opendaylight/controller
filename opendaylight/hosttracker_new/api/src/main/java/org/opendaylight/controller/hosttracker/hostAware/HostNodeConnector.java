44abc0ad2bf7a7738e2c4b644a2808fef32d1130
Kalvin Hom <kahom@cisco.com>
2013-06-12 09:56:03 -0700

    HostTracker Bundle Separation
    Moved the new HostTracker into
    hostracker_new bundles.
    
    Added exclusion to bin.xml so it doesn't
    get assembled into distribution.
    
    Added original hosttracker back.
    
    Goal is to allow collaboration and
    incremental check-ins for this
    module without breaking anything else.
    
    Original information from Rob Adams:
        Author: Rob Adams <rob.adams@bigswitch.com>
        Date:   Wed Jun 5 14:58:02 2013 -0700
    
        Initial port of device manager to cisco controller.  Currently capable
        of tracking devices but still a long list of TODOs:
        - No VLAN support - Ethernet frame parser can't parse VLAN tags and
          would be a bit of work to add it since current code can't handle
          anything but fixed-format header field which doesn't cover 802.1Q
        - No shared framework for asynchronous tasks.  For now device manager
          just allocates its own scheduled thread pool but a shared thread
          pool would be much better.
        - No clustering support in this code.  There is code that uses an
          eventually-consistent data store to support this but it's commented
          out since this is not available in daylight.  It would be possible
          to port this to the consistent store but this would lose valuable
          semantics and a component in the core should handle eventual
          consistency.
        - No web API support.  Would need to write a new implementation of the
          API; shouldn't be too hard presumably
        - Debug counters not ported
        - No mechanism for documenting log messages.  This doesn't exist in
          ODL.
        - DHCP parsing not supported; disable pulling hostname from DHCP.
        - Node/Port IDs are derived from Object and have no useful semantics.
          Device manager needs them to be comparable in order to get
          attachment points correct and consistent in all cases.  At the
          moment it just blindly casts them
        - Topology module doesn't implement the needed semantics.  First need
          to port net-virt-platform topology code, but there are impedance
          mismatches in the APIs to resolve first
        - Need to merge host tracker and device manager APIs to a new unified
          API and port all existing code to the new API, or adopt the device
          manager API.
        - Need to replace ARP handler to just handle ARPs and not be
          responsible for trying to discover devices
        - Unit tests need to be ported as well; currently commented out
    
    Change-Id: If65e6582599a9c6fafaabfeb4b93b1d931965ab1
    Signed-off-by: Kalvin Hom <kahom@cisco.com>

---
+++ opendaylight/hosttracker_new/api/src/main/java/org/opendaylight/controller/hosttracker/hostAware/HostNodeConnector.java
@@ -0,0 +1,214 @@
+
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.hosttracker.hostAware;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.util.Arrays;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.opendaylight.controller.sal.core.ConstructionException;
+import org.opendaylight.controller.sal.core.Host;
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.packet.address.EthernetAddress;
+
+@XmlRootElement(name="host")
+@XmlAccessorType(XmlAccessType.NONE)
+public class HostNodeConnector extends Host {
+    private static final long serialVersionUID = 1L;
+    @XmlElement
+    private NodeConnector nodeConnector;
+    @XmlElement
+    private short vlan;
+    @XmlElement
+    private boolean staticHost;
+    private transient short arpSendCountDown;
+
+    /**
+     * Private constructor used for JAXB mapping
+     */
+    @SuppressWarnings("unused")
+    private HostNodeConnector() {
+    }
+
+    public HostNodeConnector(InetAddress ip) throws ConstructionException {
+        this(ip, null);
+    }
+
+    public HostNodeConnector(InetAddress ip, NodeConnector nc)
+            throws ConstructionException {
+        this(new EthernetAddress(new byte[] { (byte) 0x00, (byte) 0x00,
+                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00 }), ip, nc,
+                (short) 0);
+    }
+
+    public HostNodeConnector(byte[] mac, InetAddress ip, NodeConnector nc,
+            short vlan) throws ConstructionException {
+        this(new EthernetAddress(mac.clone()), ip, nc, vlan);
+    }
+
+    public HostNodeConnector(EthernetAddress eaddr, InetAddress naddr,
+            NodeConnector nc, short vlan) throws ConstructionException {
+        super(eaddr, naddr);
+        this.nodeConnector = nc;
+        this.vlan = vlan;
+    }
+
+    /**
+     * @return the NodeConnector
+     */
+    public NodeConnector getnodeConnector() {
+        return this.nodeConnector;
+    }
+
+    /**
+     * @return the Node
+     */
+    public Node getnodeconnectorNode() {
+        return this.nodeConnector.getNode();
+    }
+
+    /**
+     * @return the NodeId
+     */
+    public Long getnodeconnectornodeId() {
+        return (Long) this.nodeConnector.getNode().getID();
+    }
+
+    /**
+     * @return the port
+     */
+    public Short getnodeconnectorportId() {
+        return (Short) this.nodeConnector.getID();
+    }
+
+    /**
+     * @return the DataLayerAddress
+     */
+    public byte[] getDataLayerAddressBytes() {
+        byte[] macaddr = null;
+        if (getDataLayerAddress() instanceof EthernetAddress) {
+            EthernetAddress e = (EthernetAddress) getDataLayerAddress();
+            macaddr = e.getValue();
+        }
+        return macaddr;
+    }
+
+    /**
+     * @return the vlan
+     */
+    public short getVlan() {
+        return this.vlan;
+    }
+
+    public boolean isStaticHost() {
+        return this.staticHost;
+    }
+
+    public HostNodeConnector setStaticHost(boolean statically_learned) {
+        this.staticHost = statically_learned;
+        return this;
+    }
+
+    public HostNodeConnector initArpSendCountDown() {
+        this.arpSendCountDown = 24;
+        return this;
+    }
+
+    public short getArpSendCountDown() {
+        return (this.arpSendCountDown);
+    }
+
+    public HostNodeConnector setArpSendCountDown(short cntdown) {
+        this.arpSendCountDown = cntdown;
+        return this;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result
+                + ((nodeConnector == null) ? 0 : nodeConnector.hashCode());
+        result = prime * result + (staticHost ? 1231 : 1237);
+        result = prime * result + vlan;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        HostNodeConnector other = (HostNodeConnector) obj;
+        if (nodeConnector == null) {
+            if (other.nodeConnector != null)
+                return false;
+        } else if (!nodeConnector.equals(other.nodeConnector))
+            return false;
+        if (staticHost != other.staticHost)
+            return false;
+        if (vlan != other.vlan)
+            return false;
+        return true;
+    }
+
+    public boolean equalsByIP(InetAddress networkAddress) {
+        return (this.getNetworkAddress().equals(networkAddress));
+    }
+
+    public boolean isRewriteEnabled() {
+        byte[] emptyArray = new byte[] { (byte) 0x00, (byte) 0x00, (byte) 0x00,
+                (byte) 0x00, (byte) 0x00, (byte) 0x00 };
+        byte[] macaddr = null;
+        if (getDataLayerAddress() instanceof EthernetAddress) {
+            EthernetAddress e = (EthernetAddress) getDataLayerAddress();
+            macaddr = e.getValue();
+        }
+        if (macaddr == null)
+            return false;
+        return !Arrays.equals(emptyArray, macaddr);
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "HostNodeConnector[" + ReflectionToStringBuilder.toString(this)
+                + "]";
+    }
+
+    public boolean isV4Host() {
+        return (getNetworkAddress() instanceof Inet4Address);
+    }
+
+    public boolean isV6Host() {
+        return (getNetworkAddress() instanceof Inet6Address);
+    }
+
+    public String toJson() {
+        return "{\"host\":\"" + super.toString() + "\", " + "\"vlan\":\""
+                + String.valueOf(vlan) + "\",\"NodeConnector\":\""
+                + nodeConnector.toString() + "\"," + "\"static\":\""
+                + String.valueOf(isStaticHost()) + "\"}";
+    }
+
+}
---
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/utils/IListener.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011 Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *    Originally created by David Erickson, Stanford University 
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the
+ *    License. You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License. 
+ */
+
+package org.opendaylight.controller.sal.utils;
+
+public interface IListener<T> {
+    public enum Command {
+        CONTINUE, STOP
+    }
+    
+    /**
+     * The name assigned to this listener
+     * @return
+     */
+    public String getName();
+
+    /**
+     * Check if the module called name is a callback ordering prerequisite
+     * for this module.  In other words, if this function returns true for 
+     * the given name, then this listener will be called after that
+     * message listener.
+     * @param type the object type to which this applies
+     * @param name the name of the module
+     * @return whether name is a prerequisite.
+     */
+    public boolean isCallbackOrderingPrereq(T type, String name);
+
+    /**
+     * Check if the module called name is a callback ordering post-requisite
+     * for this module.  In other words, if this function returns true for 
+     * the given name, then this listener will be called before that
+     * message listener.
+     * @param type the object type to which this applies
+     * @param name the name of the module
+     * @return whether name is a post-requisite.
+     */
+    public boolean isCallbackOrderingPostreq(T type, String name);
+}
