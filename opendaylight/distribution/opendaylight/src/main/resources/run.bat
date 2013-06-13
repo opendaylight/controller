1b0c620a44839ee52f1259902bd2928a122c7240
Giovanni Meo <gmeo@cisco.com>
2013-06-13 12:00:36 +0200

    Enable checkstyle for other non-source files
    Just to make sure there is uniformity of threatment, also other files
    are being exposed to checkstyle, limited to space and tabs rules.
    Along with the rules, bulk changing the code to avoid breakages in the
    build.
    
    Change-Id: I11c6a10d00fc7fc6a14d41369e2a39fd6ad01d6b
    Signed-off-by: Giovanni Meo <gmeo@cisco.com>

--- opendaylight/distribution/opendaylight/src/main/resources/run.bat
+++ opendaylight/distribution/opendaylight/src/main/resources/run.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 SETLOCAL ENABLEDELAYEDEXPANSION
 IF EXIST "%JAVA_HOME%" (
-   	 	set basedir=%~dp0
+        set basedir=%~dp0
 REM       Now set the classpath:
                 set cp="!basedir!lib\org.eclipse.osgi-3.8.1.v20120830-144521.jar;!basedir!lib\org.eclipse.virgo.kernel.equinox.extensions-3.6.0.RELEASE.jar;!basedir!lib\org.eclipse.equinox.launcher-1.3.0.v20120522-1813.jar"
 
