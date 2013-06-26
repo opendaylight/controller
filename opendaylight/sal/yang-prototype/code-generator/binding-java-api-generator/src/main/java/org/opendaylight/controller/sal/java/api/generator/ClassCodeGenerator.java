ffbeb1ac6861305dbc14f2f630c620141637e566
Jozef Gloncak <jgloncak@cisco.com>
2013-06-26 11:45:12 +0200

    Added following mapping: from: - subelement >>PATTERN<< of YANG build-in type string to: - constant list string >>PATTERN_CONSTANTS<< which contains list of regular expressions specified in >>PATTENR<< - member class attribute >>patterns<< which is list of Pattern objects.
    Change-Id: I47ec9f4a5cd3e7eef85b78189a3788dd2f9e007f
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- opendaylight/sal/yang-prototype/code-generator/binding-java-api-generator/src/main/java/org/opendaylight/controller/sal/java/api/generator/ClassCodeGenerator.java
+++ opendaylight/sal/yang-prototype/code-generator/binding-java-api-generator/src/main/java/org/opendaylight/controller/sal/java/api/generator/ClassCodeGenerator.java
@@ -16,7 +16,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.opendaylight.controller.binding.generator.util.TypeConstants;
 import org.opendaylight.controller.sal.binding.model.api.CodeGenerator;
+import org.opendaylight.controller.sal.binding.model.api.Constant;
 import org.opendaylight.controller.sal.binding.model.api.Enumeration;
 import org.opendaylight.controller.sal.binding.model.api.GeneratedProperty;
 import org.opendaylight.controller.sal.binding.model.api.GeneratedTransferIdentityObject;
@@ -32,12 +34,13 @@ public final class ClassCodeGenerator implements CodeGenerator {
         final Writer writer = new StringWriter();
 
         if (type instanceof GeneratedTransferObject) {
-            GeneratedTransferObject genTO = (GeneratedTransferObject) type;            
+            GeneratedTransferObject genTO = (GeneratedTransferObject) type;
             imports = GeneratorUtil.createImports(genTO);
-            
+
             final String currentPkg = genTO.getPackageName();
             final List<GeneratedProperty> fields = genTO.getProperties();
             final List<Enumeration> enums = genTO.getEnumerations();
+            final List<Constant> consts = genTO.getConstantDefinitions();
 
             writer.write(GeneratorUtil.createPackageDeclaration(currentPkg));
             writer.write(NL);
@@ -48,54 +51,61 @@ public final class ClassCodeGenerator implements CodeGenerator {
             }
             writer.write(NL);
 
-            writer.write(GeneratorUtil.createClassDeclaration(genTO, "",
-                    imports, genTO.isAbstract()));
+            writer.write(GeneratorUtil.createClassDeclaration(genTO, "", imports, genTO.isAbstract()));
             writer.write(NL);
             writer.write(NL);
-            
-            if (enums != null) {           
-               	EnumGenerator enumGenerator = new EnumGenerator();
-            	for ( Enumeration e : enums ) {            		
-            		writer.write(enumGenerator.generateInnerEnumeration(e, TAB).toString());
-            		writer.write(NL);
-            	}
+
+            if (consts != null) {
+                for (Constant con : consts) {
+                    writer.write(GeneratorUtil.createConstant(con, TAB, imports, currentPkg));
+                    writer.write(NL);
+                }
             }
 
-            if (fields != null) {
-                for (GeneratedProperty field : fields) {
-                    writer.write(GeneratorUtil.createField(field, TAB, imports,
-                            currentPkg) + NL);
+            if (enums != null) {
+                EnumGenerator enumGenerator = new EnumGenerator();
+                for (Enumeration e : enums) {
+                    writer.write(enumGenerator.generateInnerEnumeration(e, TAB).toString());
+                    writer.write(NL);
+                }
+            }
+
+            boolean memberPatternListCodeRequired = false;
+            memberPatternListCodeRequired = (GeneratorUtil.isConstantInTO(TypeConstants.PATTERN_CONSTANT_NAME, genTO));
+            if (fields != null || memberPatternListCodeRequired) {
+                if (fields != null) {
+                    for (GeneratedProperty field : fields) {
+                        writer.write(GeneratorUtil.createField(field, TAB, imports, currentPkg) + NL);
+                    }
+                }
+                if (memberPatternListCodeRequired) {
+                    writer.write(TAB + PRIVATE + GAP + "List<Pattern>" + GAP + MEMBER_PATTERN_LIST + GAP + ASSIGN + GAP
+                            + "new ArrayList<Pattern>()" + GAP + SC + NL);
+
                 }
                 writer.write(NL);
-                writer.write(GeneratorUtil.createConstructor(genTO, TAB,
-                        imports, genTO.isAbstract()) + NL);
+                writer.write(GeneratorUtil.createConstructor(genTO, TAB, imports, genTO.isAbstract()) + NL);
                 writer.write(NL);
-                for (GeneratedProperty field : fields) {
-                    writer.write(GeneratorUtil.createGetter(field, TAB,
-                            imports, currentPkg) + NL);
-                    if (!field.isReadOnly()) {
-                        writer.write(GeneratorUtil.createSetter(field, TAB,
-                                imports, currentPkg) + NL);
+                if (fields != null) {
+                    for (GeneratedProperty field : fields) {
+                        writer.write(GeneratorUtil.createGetter(field, TAB, imports, currentPkg) + NL);
+                        if (!field.isReadOnly()) {
+                            writer.write(GeneratorUtil.createSetter(field, TAB, imports, currentPkg) + NL);
+                        }
                     }
                 }
                 writer.write(NL);
 
                 if (!genTO.getHashCodeIdentifiers().isEmpty()) {
-                    writer.write(GeneratorUtil.createHashCode(
-                            genTO.getHashCodeIdentifiers(), TAB)
-                            + NL);
+                    writer.write(GeneratorUtil.createHashCode(genTO.getHashCodeIdentifiers(), TAB) + NL);
                 }
 
                 if (!genTO.getEqualsIdentifiers().isEmpty()) {
-                    writer.write(GeneratorUtil.createEquals(genTO,
-                            genTO.getEqualsIdentifiers(), TAB)
-                            + NL);
+                    writer.write(GeneratorUtil.createEquals(genTO, genTO.getEqualsIdentifiers(), TAB) + NL);
                 }
 
                 if (!genTO.getToStringIdentifiers().isEmpty()) {
-                    writer.write(GeneratorUtil.createToString(genTO,
-                            genTO.getToStringIdentifiers(), TAB)
-                            + NL);
+                    writer.write(GeneratorUtil.createToString(genTO, genTO.getToStringIdentifiers(), TAB) + NL);
 
                 }
 
