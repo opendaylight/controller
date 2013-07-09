ad1f5405413abf77c25a67e36a081636fc014709
Tony Tkacik <ttkacik@cisco.com>
2013-07-09 17:25:36 +0200

    Updated PoC APIs of Model-driven SAL
      - Bumped version of YANG tools
      - Added Concepts Lang package with reusable tranformator functionality
    
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/sal/yang-prototype/sal/sal-binding-spi/src/main/java/org/opendaylight/controller/sal/binding/spi/DataDomToJavaTransformer.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.binding.spi;
+
+import org.opendaylight.controller.concepts.lang.Transformer;
+import org.opendaylight.controller.yang.binding.DataObject;
+import org.opendaylight.controller.yang.common.QName;
+import org.opendaylight.controller.yang.data.api.CompositeNode;
+
+public interface DataDomToJavaTransformer<P extends DataObject> extends Transformer<CompositeNode, P> {
+
+    /**
+     * Returns a QName of valid input composite node.
+     * 
+     * @return
+     */
+    QName getQName();
+}
