a7567544fbe006f0cf87055c6633c04b5ce2c50e
Martin Vitez <mvitez@cisco.com>
2013-07-19 11:15:10 +0200

    Projects moved under correct parent.
    Moved code-generator/maven-yang -> yang/.
    Moved code-generator/maven-yang-plugin -> yang/.
    Moved code-generator/maven-yang-plugin-it -> yang/.
    Moved code-generator/yang-model-parser-api -> yang/.
    Moved code-generator/yang-model-parser-impl -> yang/.
    
    Configured site plugin for project. Fixed some javadocs.
    Fixed bug in ParserUtils causing NullPointerException.
    Fixed bug in resolving uses statement.
    Improved tests.
    
    Signed-off-by: Martin Vitez <mvitez@cisco.com>

---
+++ opendaylight/sal/yang-prototype/yang/maven-yang/src/main/java/org/opendaylight/controller/yang2sources/spi/CodeGenerator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.yang2sources.spi;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
+import org.opendaylight.controller.yang.model.api.Module;
+import org.opendaylight.controller.yang.model.api.SchemaContext;
+
+/**
+ * Classes implementing this interface can be submitted to maven-yang-plugin's
+ * generate-sources goal.
+ */
+public interface CodeGenerator {
+
+    /**
+     * Generate sources from provided {@link SchemaContext}
+     *
+     * @param context
+     *            parsed from yang files
+     * @param outputBaseDir
+     *            expected output directory for generated sources configured by
+     *            user
+     * @param currentModules
+     *            yang modules parsed from yangFilesRootDir
+     * @return collection of files that were generated from schema context
+     * @throws IOException
+     */
+    Collection<File> generateSources(SchemaContext context, File outputBaseDir, Set<Module> currentModules)
+            throws IOException;
+
+    /**
+     * Utilize maven logging if necessary
+     *
+     * @param log
+     */
+    void setLog(Log log);
+
+    /**
+     * Provided map contains all configuration that was set in pom for code
+     * generator in additionalConfiguration tag
+     *
+     * @param additionalConfiguration
+     */
+    void setAdditionalConfig(Map<String, String> additionalConfiguration);
+
+    /**
+     * Provided folder is marked as resources and its content will be packaged
+     * in resulting jar. Feel free to add necessary resources
+     *
+     * @param resourceBaseDir
+     */
+    void setResourceBaseDir(File resourceBaseDir);
+
+    /**
+     * Provided maven project object. Any additional information about current
+     * maven project can be accessed from it.
+     *
+     * @param project
+     */
+    void setMavenProject(MavenProject project);
+}
