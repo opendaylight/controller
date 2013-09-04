59105462f989fef69c2acc0f0c845d425542c700
Tony Tkacik <ttkacik@cisco.com>
2013-09-03 17:42:26 -0700

    Updated toaster sample to match new Binding specification of YANG Tools.
    Change-Id: I95a7373868993d766b467a1e53252caeb24fdc15
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/sal/yang-prototype/sal/samples/toaster-consumer/src/main/java/org/opendaylight/controller/sample/toaster/provider/api/ToastConsumer.java
+++ opendaylight/sal/yang-prototype/sal/samples/toaster-consumer/src/main/java/org/opendaylight/controller/sample/toaster/provider/api/ToastConsumer.java
@@ -7,7 +7,7 @@
  */
 package org.opendaylight.controller.sample.toaster.provider.api;
 
-import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.toaster.rev20091120.ToastType;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.toaster.rev091120.ToastType;
 
 public interface ToastConsumer {
 	
