c151c6549dcebf3e105499fb1952753bfe835410
Tony Tkacik <ttkacik@cisco.com>
2013-09-25 18:52:19 +0200

    Added sal-connector-* skeletons (REST connector, MB, Netconf).
    Change-Id: Id20c9cd8798948f79c655b4d7f8ecad0d1dd392c
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-connector-api/src/main/java/org/opendaylight/controller/sal/connector/api/Connector.java
@@ -0,0 +1,9 @@
+package org.opendaylight.controller.sal.connector.api;
+
+import java.util.Set;
+
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+
+public interface Connector {
+
+}
---
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/api/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 
+ *
+ */
+package org.opendaylight.controller.sal.rest.api;
