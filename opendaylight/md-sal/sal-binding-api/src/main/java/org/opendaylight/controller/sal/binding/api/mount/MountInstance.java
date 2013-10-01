1dc39689a7f82596588a1da70a2d294cee52b624
Tony Tkacik <ttkacik@cisco.com>
2013-10-01 19:34:46 +0200

    Added Binding-Aware APIs for Mount YANG extension
    Change-Id: If9e68625b38962e0f5432d605cb2e0300b217472
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/mount/MountInstance.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.binding.api.mount;
+
+import org.opendaylight.controller.sal.binding.api.NotificationService;
+import org.opendaylight.controller.sal.binding.api.data.DataBrokerService;
+import org.opendaylight.yangtools.yang.binding.RpcService;
+
+public interface MountInstance extends NotificationService, DataBrokerService {
+
+    <T extends RpcService> T getRpcService(Class<T> rpcService);
+    
+}
--- opendaylight/md-sal/sal-connector-api/src/main/java/org/opendaylight/controller/sal/connector/api/Connector.java
+++ opendaylight/md-sal/sal-connector-api/src/main/java/org/opendaylight/controller/sal/connector/api/Connector.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.controller.sal.connector.api;
 
 import java.util.Set;
@@ -5,7 +12,6 @@ import java.util.Set;
 import org.opendaylight.controller.sal.core.api.RpcImplementation;
 import org.opendaylight.controller.sal.core.api.notify.NotificationListener;
 import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
 
 public interface Connector extends RpcImplementation, NotificationListener {
 
