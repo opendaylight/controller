a1525cb86064874dc58dd5e3131e6bf9e51e8ba7
Moiz Raja <moraja@cisco.com>
2013-10-02 10:43:54 -0700

    Simple Sanity Test to validate that all the bundles in a distribution are active and resolved
    The idea behind the Sanity Test is to ultimately fail a build when the test fails. The test would
    launch run.sh but with a slightly modified config.ini where the sanitytest bundle would be injected
    as a framework bundle.
    
    The test waits for 30 seconds for the container to finish loading all the bundles. It then gets all the
    bundles from the BundleContext and checks if they are ACTIVE or RESOLVED. If they are neither it prints
    out the name of the bundle and it's state and returns an error code.
    
    Change-Id: Ia6905318f1cf63e852791ba107193094a83fddf1
    Signed-off-by: Moiz Raja <moraja@cisco.com>

---
+++ opendaylight/distribution/sanitytest/src/main/java/org/opendaylight/controller/sanitytest/internal/Activator.java
@@ -0,0 +1,54 @@
+package org.opendaylight.controller.sanitytest.internal;
+
+import org.osgi.framework.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Activator implements BundleActivator {
+    //30 Second
+    private static final int DELAY = 30000;
+
+
+    private String stateToString(int state) {
+        switch (state) {
+        case Bundle.ACTIVE:
+            return "ACTIVE";
+        case Bundle.INSTALLED:
+            return "INSTALLED";
+        case Bundle.RESOLVED:
+            return "RESOLVED";
+        case Bundle.UNINSTALLED:
+            return "UNINSTALLED";
+        default:
+            return "Not CONVERTED";
+        }
+    }
+
+    public void start(final BundleContext bundleContext) throws Exception {
+        Timer monitorTimer = new Timer("monitor timer", true);
+
+        monitorTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                boolean failed = false;
+                for(Bundle bundle : bundleContext.getBundles()){
+                    if(bundle.getState() != Bundle.ACTIVE && bundle.getState() != Bundle.RESOLVED) {
+                        System.out.println("Failed to activate/resolve bundle = " + bundle.getSymbolicName() + " state = " + stateToString(bundle.getState()));
+                        failed = true;
+                    }
+                }
+
+                if(failed){
+                    System.exit(-1);
+                } else {
+                    System.exit(0);
+                }
+            }
+        }, DELAY);
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
