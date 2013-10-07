d684dd484ca6d7a31bd4fc66fd6d4778938586ba
Giovanni Meo <gmeo@cisco.com>
2013-10-07 15:18:21 +0000

    Revert "Integrate the Distribution Sanity Test with the build" It failed merge the patch is not behaving well. This reverts commit d78e985bc4ed590407c8c7800ba796d5ea55aa6c.
    Change-Id: Id2d3a6215f7373165217ad92dfda7a4e9b07d7d8
    Signed-off-by: Giovanni Meo <gmeo@cisco.com>

--- opendaylight/distribution/opendaylight/run.sh
+++
@@ -1,8 +0,0 @@
-cp ../sanitytest/target/sanitytest-0.4.1-SNAPSHOT.jar ./target/distribution.opendaylight-osgipackage/opendaylight/plugins
-pushd ./target/distribution.opendaylight-osgipackage/opendaylight/
-./run.sh
-success=`echo $?`
-popd
-rm ./target/distribution.opendaylight-osgipackage/opendaylight/plugins/sanitytest-0.4.1-SNAPSHOT.jar
-exit $success
-
