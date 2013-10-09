c74f363d71886dc302da87ec3be1904a6227a204
Alessandro Boch <aboch@cisco.com>
2013-10-09 12:04:09 -0700

    Make use of NetUtils.getBroadcastMacAddr()
    ISSUE: I suggested to make NetUtils.broadcastMacAddr public, but that was not
           a good idea as we already have the getter and because that is an array
           therefore vulnerable to modifications.
    
    Change-Id: I098ca828d79ffe1d478c761d8b749ee100dfba5b
    Signed-off-by: Alessandro Boch <aboch@cisco.com>

--- opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/utils/NetUtils.java
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/utils/NetUtils.java
@@ -43,7 +43,7 @@ public abstract class NetUtils {
     /**
      * Constant holding the broadcast MAC address
      */
-    public static final byte[] BroadcastMACAddr = {-1, -1, -1, -1, -1, -1};
+    private static final byte[] BroadcastMACAddr = {-1, -1, -1, -1, -1, -1};
 
     /**
      * Converts a 4 bytes array into an integer number
