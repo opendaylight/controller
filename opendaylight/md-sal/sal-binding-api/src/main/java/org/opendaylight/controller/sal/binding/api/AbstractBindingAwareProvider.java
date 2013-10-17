997fe28af0196c9fe4fc70ec9c0494d9be1d5a25
Tony Tkacik <ttkacik@cisco.com>
2013-10-17 10:26:47 +0200

    Updated BindingAware Activators to prevent premature initialization
    Fixes a bug when BindingAwareActivators loaded in wrong order and
    tried to retrieve MD-SAL broker before broker bundle was started.
    
    Change-Id: I87f807974b8581a14b387928a52c2ee2fd83208c
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/AbstractBindingAwareProvider.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/AbstractBindingAwareProvider.java
@@ -17,31 +17,10 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
-public abstract class AbstractBindingAwareProvider implements BindingAwareProvider, BundleActivator {
-
-    /**
-     * Initializes the bundle.
-     * 
-     * Implementation of initialization get's the Binding Aware Broker from
-     * service registry and register itself as a {@link BindingAwareProvider}
-     * 
-     * Callback order is:
-     * <ol>
-     * <li>{@link #startImpl(BundleContext)}
-     * <li>{@link #onSessionInitiated(ProviderContext)}
-     * <li>Registration of global {@link RpcService} implementations returned by
-     * {@link #getImplementations()}
-     * <li>Registration of {@link ProviderFunctionality} implementations
-     * returned by {@link #getFunctionality()}
-     * </ol>
-     * 
-     */
+public abstract class AbstractBindingAwareProvider extends AbstractBrokerAwareActivator implements BindingAwareProvider {
+    
     @Override
-    public final void start(BundleContext context) throws Exception {
-        startImpl(context);
-        ServiceReference<BindingAwareBroker> brokerRef = context.getServiceReference(BindingAwareBroker.class);
-        BindingAwareBroker broker = context.getService(brokerRef);
-
+    protected final void onBrokerAvailable(BindingAwareBroker broker, BundleContext context) {
         ProviderContext ctx = broker.registerProvider(this, context);
         registerRpcImplementations(ctx);
         registerFunctionality(ctx);
@@ -67,62 +46,6 @@ public abstract class AbstractBindingAwareProvider implements BindingAwareProvid
             // ctx.addRpcImplementation(type, implementation);
         }
 
-    }
-
-    /**
-     * Called when this bundle is started (before
-     * {@link #onSessionInitiated(ProviderContext)} so the Framework can perform
-     * the bundle-specific activities necessary to start this bundle. This
-     * method can be used to register services or to allocate any resources that
-     * this bundle needs.
-     * 
-     * <p>
-     * This method must complete and return to its caller in a timely manner.
-     * 
-     * @param context
-     *            The execution context of the bundle being started.
-     * @throws Exception
-     *             If this method throws an exception, this bundle is marked as
-     *             stopped and the Framework will remove this bundle's
-     *             listeners, unregister all services registered by this bundle,
-     *             and release all services used by this bundle.
-     */
-    protected void startImpl(BundleContext context) {
-        // NOOP
-    }
-
-    /**
-     * Called when this bundle is stopped so the Framework can perform the
-     * bundle-specific activities necessary to stop the bundle. In general, this
-     * method should undo the work that the {@code BundleActivator.start} method
-     * started. There should be no active threads that were started by this
-     * bundle when this bundle returns. A stopped bundle must not call any
-     * Framework objects.
-     * 
-     * <p>
-     * This method must complete and return to its caller in a timely manner.
-     * 
-     * @param context The execution context of the bundle being stopped.
-     * @throws Exception If this method throws an exception, the bundle is still
-     *         marked as stopped, and the Framework will remove the bundle's
-     *         listeners, unregister all services registered by the bundle, and
-     *         release all services used by the bundle.
-     */
-    protected void stopImpl(BundleContext context) {
-        // NOOP
-    }
-
-    /**
-     * Bundle stop callback
-     * 
-     * 
-     * Custom implementation of bundle stop could be carried by overriding
-     * {@link #stopImpl(BundleContext)} method.
-     * 
-     */
-    @Override
-    public final void stop(BundleContext context) throws Exception {
-        stopImpl(context);
     }
 
     @Override
