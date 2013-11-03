5a4ff5581941289ca4d67e27cc38126f25411c8d
Alissa Bonas <abonas@redhat.com>
2013-11-03 17:01:16 +0200

    Add checkstyle rule EmptyStatement and fix the code
    Change-Id: I49d87dd950eae2c3f75d34403e555c7db9cc04f9
    Signed-off-by: Alissa Bonas <abonas@redhat.com>

--- opendaylight/networkconfiguration/neutron/src/main/java/org/opendaylight/controller/networkconfig/neutron/NeutronSubnet.java
+++ opendaylight/networkconfiguration/neutron/src/main/java/org/opendaylight/controller/networkconfig/neutron/NeutronSubnet.java
@@ -265,7 +265,7 @@ public class NeutronSubnet {
                 allocationPools = source.splitPool(gatewayIP);
             }
         } catch (Exception e) {
-            ;
+
         }
     }
 
@@ -346,12 +346,13 @@ public class NeutronSubnet {
              * split the pool and add both pieces to the new list
              */
             if (!(pool.getPoolEnd().equalsIgnoreCase(ipAddress) &&
-                    pool.getPoolStart().equalsIgnoreCase(ipAddress))) {
-                if (pool.contains(ipAddress)) {
-                    List<NeutronSubnet_IPAllocationPool> pools = pool.splitPool(ipAddress);
-                    newList.addAll(pools);
-                } else
-                    newList.add(pool);
+            		pool.getPoolStart().equalsIgnoreCase(ipAddress))) {
+            	if (pool.contains(ipAddress)) {
+            		List<NeutronSubnet_IPAllocationPool> pools = pool.splitPool(ipAddress);
+            		newList.addAll(pools);
+            	} else  {
+            		newList.add(pool);
+            	}
             }
         }
         allocationPools = newList;
