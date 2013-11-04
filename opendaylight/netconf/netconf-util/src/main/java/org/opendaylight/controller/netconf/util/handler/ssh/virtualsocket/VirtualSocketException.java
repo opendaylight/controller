6acb70151540486772b1f7c8e95b128acfa66bf0
Robert Gallas <rgallas@cisco.com>
2013-11-04 15:42:24 +0100

    Added Netty Ssh handler
    Creation of Ganymed wrapper bundle
    Created SSH netty handler
    Updated pom.xml in relevant projects
    
    Change-Id: I2615f074aa463e49e18a30ef4824b7d09dc87876
    Signed-off-by: Robert Gallas <rgallas@cisco.com>

---
+++ opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/handler/ssh/virtualsocket/VirtualSocketException.java
@@ -0,0 +1,7 @@
+package org.opendaylight.controller.netconf.util.handler.ssh.virtualsocket;
+
+/**
+ * Exception class which provides notification about exceptional situations at the virtual socket layer.
+ */
+public class VirtualSocketException extends RuntimeException {
+}
