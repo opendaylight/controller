c79012eccfde22515037e04b10b594e1adee5315
Milos Fabian <milfabia@cisco.com>
2013-11-05 14:42:30 +0100

    Threadpool APIs and thread-related configuration service interfaces.
    EventBusServiceInterface - Service representing an event bus. The service acts as message router between event producers and event consumers.
    ScheduledThredPoolServiceInterface - An extension of the simple pool of threads able to schedule work to be executed at some point in time.
    ThreadFactoryServiceInterface - Service representing a ThreadFactory instance. It is directly useful in Java world,
    where various library pieces need to create threads and you may want to inject a customized thread implementation.
    ThreadPoolServiceInterface - Representing a simple pool of threads able to execute work.
    
    Change-Id: I4bb9c3fb118f317816ee24c7edaa292f8780ae32
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ opendaylight/config/threadpool-config-api/src/main/java/org/opendaylight/controller/config/threadpool/ThreadPool.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.config.threadpool;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Interface representing thread pool.
+ */
+public interface ThreadPool {
+
+    public ExecutorService getExecutor();
+
+    public int getMaxThreadCount();
+}
