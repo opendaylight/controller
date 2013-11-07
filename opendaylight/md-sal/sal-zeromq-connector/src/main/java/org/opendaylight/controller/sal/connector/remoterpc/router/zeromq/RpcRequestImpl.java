11c95da1ad1cb9ec7b38ec5b885aad30c74a6e37
Abhishek Kumar <abhishk2@cisco.com>
2013-11-06 16:18:07 -0800

    Adding copyright notice
    Change-Id: Idcb951db8f2020fe73e4094a8ef3c38169ddb117
    Signed-off-by: Abhishek Kumar <abhishk2@cisco.com>

--- opendaylight/md-sal/sal-zeromq-connector/src/main/java/org/opendaylight/controller/sal/connector/remoterpc/router/zeromq/RpcRequestImpl.java
+++ opendaylight/md-sal/sal-zeromq-connector/src/main/java/org/opendaylight/controller/sal/connector/remoterpc/router/zeromq/RpcRequestImpl.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.controller.sal.connector.remoterpc.router.zeromq;
 
 import org.opendaylight.controller.sal.connector.api.RpcRouter;
@@ -6,13 +13,6 @@ import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 
 import java.io.Serializable;
 
-/**
- * Created with IntelliJ IDEA.
- * User: abhishk2
- * Date: 10/25/13
- * Time: 12:32 PM
- * To change this template use File | Settings | File Templates.
- */
 public class RpcRequestImpl implements RpcRouter.RpcRequest<QName, QName, InstanceIdentifier, Object>,Serializable {
 
   private RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier;
