04590cdc4ce5666f94e3e5be64f0bf46bba00688
Tony Tkacik <ttkacik@cisco.com>
2013-11-15 15:43:43 +0100

    Fix for Bug 144 - added more defensive locking mechanism
      - Added defensive locking mechanism to class generation code
      - Provider's and Consumer's crossing boundatories between
        brokers are blocked util their YANG schema is ready and processed.
      - Added set of APIs to make generated codec contracts and interactions
        more explicit.
    
    Change-Id: I299924e911f63aba1fdeca3debeb8cc80f66f5ac
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/sal/binding/dom/serializer/impl/BindingClassListener.java
@@ -0,0 +1,7 @@
+package org.opendaylight.controller.sal.binding.dom.serializer.impl;
+
+public interface BindingClassListener {
+
+    void onBindingClassCaptured(Class<?> cls);
+    
+}
---
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/sal/binding/dom/serializer/impl/GeneratorListener.java
@@ -0,0 +1,16 @@
+package org.opendaylight.controller.sal.binding.dom.serializer.impl;
+
+import java.util.Map;
+
+import org.opendaylight.yangtools.yang.binding.BindingCodec;
+import org.opendaylight.yangtools.yang.common.QName;
+
+public interface GeneratorListener {
+
+    
+    
+    void onCodecCreated(Class<?> codec);
+    void onValueCodecCreated(Class<?> valueClass,Class<?> valueCodec);
+    void onChoiceCodecCreated(Class<?> choiceClass,Class<? extends BindingCodec<Map<QName, Object>,Object>> choiceCodec);
+    void onCaseCodecCreated(Class<?> choiceClass,Class<? extends BindingCodec<Map<QName, Object>,Object>> choiceCodec);
+}
