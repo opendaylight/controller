248c43ef5a13e9919d577b46d63a8fa8b46feb1e
Tony Tkacik <ttkacik@cisco.com>
2013-11-27 17:48:30 +0100

    Wired ClusteredDataStore with Configuration Subsytem
    MD-SAL clustered Data Store is available as separete
    configurable modules.
    
    Change-Id: I15a98f19dd2eb851e8dfb73a9c4df461e6860db5
    Signed-off-by: Moiz Raja <moraja@cisco.com>
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/clustered-data-store/implementation/src/test/java/org/opendaylight/controller/datastore/internal/ClusteredDataStoreManagerTest.java
+++ opendaylight/md-sal/clustered-data-store/implementation/src/test/java/org/opendaylight/controller/datastore/internal/ClusteredDataStoreManagerTest.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
  *
@@ -9,6 +8,8 @@
 
 package org.opendaylight.controller.datastore.internal;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.felix.dm.Component;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,7 +18,8 @@ import org.opendaylight.controller.clustering.services.CacheExistException;
 import org.opendaylight.controller.clustering.services.IClusterGlobalServices;
 import org.opendaylight.controller.md.sal.common.api.data.DataCommitHandler.DataCommitTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.DataModification;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -29,103 +31,78 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 public class ClusteredDataStoreManagerTest {
-	
-	private  static ClusteredDataStoreManager clusteredDSMgr = null;
-	private IClusterGlobalServices icClusterGlbServices =  mock(IClusterGlobalServices.class);
-	 
-	@BeforeClass
-	public static void construct(){
-		clusteredDSMgr = new ClusteredDataStoreManager();
+
+    private static ClusteredDataStoreManager clusteredDSMgr = null;
+    private IClusterGlobalServices icClusterGlbServices = mock(IClusterGlobalServices.class);
+
+    @BeforeClass
+    public static void construct() {
+        clusteredDSMgr = new ClusteredDataStoreManager();
         assertNotNull(clusteredDSMgr);
-	}
-	 
-	@Test
-	public void construct_OnSetClusterGlobalServices_AssertNoException(){
-		icClusterGlbServices =  mock(IClusterGlobalServices.class);
-		 
-		clusteredDSMgr.setClusterGlobalServices(icClusterGlbServices);
-	 }
-	 
-	 @Test
-	 public void construct_OnUnSetClusterGlobalServices_AssertNoException(){
-		 IClusterGlobalServices icClusterGlbServices =  mock(IClusterGlobalServices.class);
-		 
-		 clusteredDSMgr.unsetClusterGlobalServices(icClusterGlbServices);
-	 }
-	 
-	 @Test
-	 public void construct_init_AssertNoException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreImpl clusteredDSImpl =  mock(ClusteredDataStoreImpl.class);
-		 
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-	 }
-	 
-	 @Test(expected = IllegalStateException.class)
-	 public void construct_init_AssertCacheExistException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doThrow(CacheExistException.class).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-	 }
-	 
-	 @Test(expected = IllegalStateException.class)
-	 public void construct_init_AssertCacheConfigException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doThrow(CacheConfigException.class).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-	 }
-	 
-	 @Test
-	 public void construct_readOperationalData_AssertNoException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreImpl clusteredDSImpl =  mock(ClusteredDataStoreImpl.class);
-		 
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-		 
-		 Object o = mock(Object.class);
-		 
-		 when(clusteredDSImpl.readOperationalData(any(InstanceIdentifier.class))).thenReturn(o);
-		 assertEquals(o,clusteredDSManager.readOperationalData(any(InstanceIdentifier.class)));
-	 }
-	 
-	 
-	 @Test
-	 public void construct_readConfigurationData_AssertNoException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreImpl clusteredDSImpl =  mock(ClusteredDataStoreImpl.class);
-		 
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-		 Object o = mock(Object.class);
-		 
-		 when(clusteredDSImpl.readConfigurationData(any(InstanceIdentifier.class))).thenReturn(o);
-		 assertEquals(o,clusteredDSManager.readConfigurationData(any(InstanceIdentifier.class)));
-	 }
-	 
-	 @Test
-	 public void construct_requestCommit_AssertNoException() throws CacheExistException,CacheConfigException{
-		 ClusteredDataStoreImpl clusteredDSImpl =  mock(ClusteredDataStoreImpl.class);
-		 
-		 ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
-		 doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore(any(Component.class));
-		 Component c = mock(Component.class);
-		 
-		 clusteredDSManager.init(c);
-		 DataCommitTransaction dataCommitTransaction = mock(DataCommitTransaction.class);
-		 
-		 when(clusteredDSImpl.requestCommit(any(DataModification.class))).thenReturn(dataCommitTransaction);
-		 assertEquals(dataCommitTransaction,clusteredDSManager.requestCommit(any(DataModification.class)));
-	 }
+    }
+
+    @Test
+    public void construct_OnSetClusterGlobalServices_AssertNoException() {
+        doReturn(new ConcurrentHashMap<InstanceIdentifier, CompositeNode>()).when(icClusterGlbServices).getCache(ClusteredDataStoreImpl.CONFIGURATION_DATA_CACHE);
+        doReturn(new ConcurrentHashMap<InstanceIdentifier, CompositeNode>()).when(icClusterGlbServices).getCache(ClusteredDataStoreImpl.OPERATIONAL_DATA_CACHE);
+        clusteredDSMgr.setClusterGlobalServices(icClusterGlbServices);
+    }
+
+    @Test
+    public void construct_init_AssertNoException() throws CacheExistException, CacheConfigException {
+        ClusteredDataStoreImpl clusteredDSImpl = mock(ClusteredDataStoreImpl.class);
+
+        ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
+        doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore();
+        clusteredDSManager.start();
+    }
+
+
+    @Test
+    public void construct_readOperationalData_AssertNoException() throws CacheExistException, CacheConfigException {
+        ClusteredDataStoreImpl clusteredDSImpl = mock(ClusteredDataStoreImpl.class);
+
+        ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
+        doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore();
+        Component c = mock(Component.class);
+        
+        clusteredDSManager.start();
+        clusteredDSManager.setClusterGlobalServices(icClusterGlbServices);
+        CompositeNode o = mock(CompositeNode.class);
+
+        when(clusteredDSImpl.readOperationalData(any(InstanceIdentifier.class))).thenReturn(o);
+        assertEquals(o, clusteredDSManager.readOperationalData(any(InstanceIdentifier.class)));
+    }
+
+    @Test
+    public void construct_readConfigurationData_AssertNoException() throws CacheExistException, CacheConfigException {
+        ClusteredDataStoreImpl clusteredDSImpl = mock(ClusteredDataStoreImpl.class);
+
+        ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
+        doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore();
+        Component c = mock(Component.class);
+
+        clusteredDSManager.start();
+        clusteredDSManager.setClusterGlobalServices(icClusterGlbServices);
+        
+        CompositeNode o = mock(CompositeNode.class);
+
+        when(clusteredDSImpl.readConfigurationData(any(InstanceIdentifier.class))).thenReturn(o);
+        assertEquals(o, clusteredDSManager.readConfigurationData(any(InstanceIdentifier.class)));
+    }
+
+    @Test
+    public void construct_requestCommit_AssertNoException() throws CacheExistException, CacheConfigException {
+        ClusteredDataStoreImpl clusteredDSImpl = mock(ClusteredDataStoreImpl.class);
+
+        ClusteredDataStoreManager clusteredDSManager = spy(new ClusteredDataStoreManager());
+        doReturn(clusteredDSImpl).when(clusteredDSManager).createClusteredDataStore();
+        IClusterGlobalServices globalServices = mock(IClusterGlobalServices.class);
+        clusteredDSManager.setClusterGlobalServices(globalServices);
+        clusteredDSManager.start();
+        DataCommitTransaction dataCommitTransaction = mock(DataCommitTransaction.class);
+
+        when(clusteredDSImpl.requestCommit(any(DataModification.class))).thenReturn(dataCommitTransaction);
+        assertEquals(dataCommitTransaction, clusteredDSManager.requestCommit(any(DataModification.class)));
+    }
 }
