94e7e860358b95953f1ad7e92543431b94a5a254
Artur Downar <adownar@advaoptical.com>
2013-11-27 17:53:47 +0100

    Fix bug 171.
    Change-Id: I096f8389068948faad734ae1fef4b9b40b0f3cb9
    Signed-off-by: Artur Downar <adownar@advaoptical.com>

--- opendaylight/protocol_plugins/openflow/src/main/java/org/opendaylight/controller/protocol_plugin/openflow/core/internal/SwitchHandler.java
+++ opendaylight/protocol_plugins/openflow/src/main/java/org/opendaylight/controller/protocol_plugin/openflow/core/internal/SwitchHandler.java
@@ -38,6 +38,7 @@ import org.opendaylight.controller.protocol_plugin.openflow.core.ISwitch;
 import org.openflow.protocol.OFBarrierReply;
 import org.openflow.protocol.OFBarrierRequest;
 import org.openflow.protocol.OFEchoReply;
+import org.openflow.protocol.OFEchoRequest;
 import org.openflow.protocol.OFError;
 import org.openflow.protocol.OFFeaturesReply;
 import org.openflow.protocol.OFFlowMod;
@@ -371,6 +372,15 @@ public class SwitchHandler implements ISwitch {
                 break;
             case ECHO_REQUEST:
                 OFEchoReply echoReply = (OFEchoReply) factory.getMessage(OFType.ECHO_REPLY);
+                              
+                if (msg instanceof OFEchoRequest) { //it must be true 
+                	byte []payload = ((OFEchoRequest)msg).getPayload();
+                        
+                        // the response must have the same payload as the request
+                        echoReply.setPayload(payload);
+                	echoReply.setLength( (short) (echoReply.getLength() + payload.length ));             	
+                }
+                
                 // respond immediately
                 asyncSendNow(echoReply, msg.getXid());
 
