80fdf67d62daf81984812d95fc761f73ea294487
Tomas Olvecky <tolvecky@cisco.com>
2013-11-27 18:04:40 +0100

    Add config-persister-directory-adapter. This implementation reads initial configuration from a directory. It can combine multiple files into one snapshot to be sent to netconf. It does not support writing when configuration changes.
    Change-Id: I6d92ce6a7221c424f64b2fe07b61fee30e9590f0
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

---
+++ opendaylight/config/config-persister-directory-adapter/src/test/java/org/opendaylight/controller/config/persist/storage/file/DirectoryStorageAdapterTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.config.persist.storage.file;
+
+import com.google.common.base.Optional;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.controller.config.persist.api.ConfigSnapshotHolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class DirectoryStorageAdapterTest {
+    DirectoryPersister tested;
+    SortedSet<String> expectedCapabilities;
+    String expectedSnapshot;
+
+    @Before
+    public void setUp() throws Exception {
+        expectedCapabilities = new TreeSet<>(IOUtils.readLines(getClass().getResourceAsStream("/expectedCapabilities.txt")));
+        expectedSnapshot = IOUtils.toString(getClass().getResourceAsStream("/expectedSnapshot.xml"));
+    }
+
+    @Test
+    public void testEmptyDirectory() throws Exception {
+        File folder = new File("target/emptyFolder");
+        folder.mkdir();
+        tested = new DirectoryPersister((folder));
+        assertEquals(Optional.<ConfigSnapshotHolder>absent(), tested.loadLastConfig());
+
+        try {
+            tested.persistConfig(new ConfigSnapshotHolder() {
+                @Override
+                public String getConfigSnapshot() {
+                    throw new RuntimeException();
+                }
+
+                @Override
+                public SortedSet<String> getCapabilities() {
+                    throw new RuntimeException();
+                }
+            });
+            fail();
+        } catch (UnsupportedOperationException e) {
+
+        }
+    }
+
+    private File getFolder(String folderName) {
+        File result = new File(("src/test/resources/" +
+                folderName).replace("/", File.separator));
+        assertTrue(result + " is not a directory", result.isDirectory());
+        return result;
+    }
+
+    @Test
+    public void testOneFile() throws Exception {
+        File folder = getFolder("oneFile");
+        tested = new DirectoryPersister((folder));
+        assertExpected();
+    }
+
+    private void assertExpected() throws IOException {
+        Optional<ConfigSnapshotHolder> maybeResult = tested.loadLastConfig();
+        assertTrue(maybeResult.isPresent());
+        ConfigSnapshotHolder result = maybeResult.get();
+        assertEquals(expectedCapabilities, result.getCapabilities());
+        assertEquals(expectedSnapshot, result.getConfigSnapshot());
+    }
+
+    @Test
+    public void testTwoFiles() throws Exception {
+        File folder = getFolder("twoFiles");
+        tested = new DirectoryPersister((folder));
+        assertExpected();
+    }
+
+}
