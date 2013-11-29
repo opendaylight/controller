0d7444fae6e9873e7af11a762aeb59d833d18573
Tomas Olvecky <tolvecky@cisco.com>
2013-11-29 12:17:06 +0100

    Fix race conditions between config-manager and persister.
    Config-manager commits a blank transaction each time new ModuleFactory is discovered. At the same time
    persister might try to push configuration. Add retries to both routines to overcome optimistic lock failure.
    
    Change-Id: Ia45505d285b2eb6cc7fa949289a3fbeb4c62f9e7
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

---
+++ opendaylight/netconf/config-persister-impl/src/test/java/org/opendaylight/controller/netconf/persist/impl/ConfigPersisterNotificationHandlerTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.netconf.persist.impl;
+
+import org.junit.Test;
+import org.opendaylight.controller.config.api.ConflictingVersionException;
+import org.opendaylight.controller.netconf.api.NetconfMessage;
+import org.opendaylight.controller.netconf.util.xml.XmlElement;
+import org.opendaylight.controller.netconf.util.xml.XmlUtil;
+import org.w3c.dom.Document;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class ConfigPersisterNotificationHandlerTest {
+
+    @Test
+    public void testConflictingVersionDetection() throws Exception {
+        Document document = XmlUtil.readXmlToDocument(getClass().getResourceAsStream("/conflictingVersionResponse.xml"));
+        try{
+            ConfigPersisterNotificationHandler.checkIsOk(XmlElement.fromDomDocument(document).getOnlyChildElement(), new NetconfMessage(document));
+            fail();
+        }catch(ConflictingVersionException e){
+            assertThat(e.getMessage(), containsString("Optimistic lock failed. Expected parent version 21, was 18"));
+        }
+    }
+
+}
