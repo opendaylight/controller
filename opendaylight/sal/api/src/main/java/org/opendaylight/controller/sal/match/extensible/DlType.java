e634963da73a12a34ddbb653859c80525d241b06
Alessandro Boch <aboch@cisco.com>
2013-12-10 09:42:10 -0800

    Initial commit for new Match classes
    - Existing match classes rely on a static enum of match types.
      This change is to allow extensibility for new matching classes.
    - This commit introduces new classes without replacing old ones
      and without migrating any code to use the new ones yet.
    
    Change-Id: I76531c10ae480123044ec1023bce8750ce7906ba
    Signed-off-by: Alessandro Boch <aboch@cisco.com>

---
+++ opendaylight/sal/api/src/main/java/org/opendaylight/controller/sal/match/extensible/DlType.java
@@ -0,0 +1,92 @@
+package org.opendaylight.controller.sal.match.extensible;
+
+import org.opendaylight.controller.sal.utils.EtherTypes;
+import org.opendaylight.controller.sal.utils.NetUtils;
+
+public class DlType extends MatchField<Short> {
+    private static final long serialVersionUID = 1L;
+    public static final String TYPE = "DL_TYPE";
+    private short ethertype;
+
+    /**
+     * Creates a Match field for the data layer type
+     *
+     * @param address
+     *            the data layer type
+     */
+    public DlType(short ethertype) {
+        super(TYPE);
+        this.ethertype = ethertype;
+    }
+
+    @Override
+    public Short getValue() {
+        return ethertype;
+    }
+
+    @Override
+    protected String getValueString() {
+        return String.format("0X%s", Integer.toHexString(NetUtils.getUnsignedShort(ethertype)));
+    }
+
+    @Override
+    public Short getMask() {
+        return null;
+    }
+
+    @Override
+    protected String getMaskString() {
+        return null;
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public boolean hasReverse() {
+        return false;
+    }
+
+    @Override
+    public DlType getReverse() {
+        return this.clone();
+    }
+
+    @Override
+    public DlType clone() {
+        return new DlType(ethertype);
+    }
+
+    @Override
+    public boolean isV6() {
+        return this.ethertype == EtherTypes.IPv6.shortValue();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ethertype;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof DlType)) {
+            return false;
+        }
+        DlType other = (DlType) obj;
+        if (ethertype != other.ethertype) {
+            return false;
+        }
+        return true;
+    }
+}
