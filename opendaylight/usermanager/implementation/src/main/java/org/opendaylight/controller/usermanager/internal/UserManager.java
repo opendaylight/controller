1326bf82d764b7c2c5dd4aa3e8a9f92e24f502ea
Martin Bobak <mbobak@cisco.com>
2013-12-12 15:47:01 +0100

    Netconf SSH user authentication using UserManager
    Change-Id: I48112f86cf8f839d5cbfdc62f0b7a6866118ed25
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- opendaylight/usermanager/implementation/src/main/java/org/opendaylight/controller/usermanager/internal/UserManager.java
+++ opendaylight/usermanager/implementation/src/main/java/org/opendaylight/controller/usermanager/internal/UserManager.java
@@ -1095,31 +1095,31 @@ public class UserManager implements IUserManager, IObjectReader,
     }
 
     // Following are setters for use in unit testing
-    void setLocalUserConfigList(ConcurrentMap<String, UserConfig> ucl) {
+    private void setLocalUserConfigList(ConcurrentMap<String, UserConfig> ucl) {
         if (ucl != null) {
             this.localUserConfigList = ucl;
         }
     }
 
-    void setRemoteServerConfigList(ConcurrentMap<String, ServerConfig> scl) {
+    private void setRemoteServerConfigList(ConcurrentMap<String, ServerConfig> scl) {
         if (scl != null) {
             this.remoteServerConfigList = scl;
         }
     }
 
-    void setAuthorizationConfList(ConcurrentMap<String, AuthorizationConfig> acl) {
+    private void setAuthorizationConfList(ConcurrentMap<String, AuthorizationConfig> acl) {
         if (acl != null) {
             this.authorizationConfList = acl;
         }
     }
 
-    void setActiveUsers(ConcurrentMap<String, AuthenticatedUser> au) {
+    private void setActiveUsers(ConcurrentMap<String, AuthenticatedUser> au) {
         if (au != null) {
             this.activeUsers = au;
         }
     }
 
-    void setAuthProviders(ConcurrentMap<String, IAAAProvider> ap) {
+    private void setAuthProviders(ConcurrentMap<String, IAAAProvider> ap) {
         if (ap != null) {
             this.authProviders = ap;
         }
