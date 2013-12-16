714ed7a96435c1963443ef4b35219f1b9712554e
Ladislav Borak <lborak@cisco.com>
2013-12-16 15:52:18 +0100

    Added tests for rpc status codes
    Change-Id: I4c14a1a04c8cae91b13d6103afdc88f79e0eeece
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestConfImplRPCStatusTest.java
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestConfImplRPCStatusTest.java
@@ -37,96 +37,76 @@ import com.google.common.base.Charsets;
 
 public class RestConfImplRPCStatusTest extends JerseyTest {
 
-	private static ControllerContext controllerContext;
-	private static BrokerFacade brokerFacade;
-	private static RestconfImpl restconfImpl;
-	private static final MediaType MEDIA_TYPE = new MediaType("application",
-			"vnd.yang.data+xml");
-	private static final MediaType MEDIA_TYPE_DRAFT02 = new MediaType(
-			"application", "yang.data+xml");
+    private static ControllerContext controllerContext;
+    private static BrokerFacade brokerFacade;
+    private static RestconfImpl restconfImpl;
+    private static final MediaType MEDIA_TYPE = new MediaType("application", "vnd.yang.data+xml");
+    private static final MediaType MEDIA_TYPE_DRAFT02 = new MediaType("application", "yang.data+xml");
 
-	@BeforeClass
-	public static void init() throws FileNotFoundException {
-		Set<Module> allModules = TestUtils.loadModules(RestconfImplTest.class
-				.getResource("/invoke-rpc-status").getPath());
-		SchemaContext schemaContext = TestUtils.loadSchemaContext(allModules);
-		controllerContext = ControllerContext.getInstance();
-		controllerContext.setSchemas(schemaContext);
-		brokerFacade = mock(BrokerFacade.class);
-		restconfImpl = RestconfImpl.getInstance();
-		restconfImpl.setBroker(brokerFacade);
-		restconfImpl.setControllerContext(controllerContext);
-	}
+    @BeforeClass
+    public static void init() throws FileNotFoundException {
+        Set<Module> allModules = TestUtils.loadModules(RestconfImplTest.class.getResource("/invoke-rpc-status")
+                .getPath());
+        SchemaContext schemaContext = TestUtils.loadSchemaContext(allModules);
+        controllerContext = ControllerContext.getInstance();
+        controllerContext.setSchemas(schemaContext);
+        brokerFacade = mock(BrokerFacade.class);
+        restconfImpl = RestconfImpl.getInstance();
+        restconfImpl.setBroker(brokerFacade);
+        restconfImpl.setControllerContext(controllerContext);
+    }
 
-	@Test
-	public void testRPCStatusCodes() throws UnsupportedEncodingException,
-			FileNotFoundException {
+    @Test
+    public void testRPCStatusCodes() throws UnsupportedEncodingException, FileNotFoundException {
 
-		String uri = createUri("/operations/", "invoke-rpc-status:testInput");
+        String uri = createUri("/operations/", "invoke-rpc-status:testInput");
 
-		InputStream xmlStream = RestconfImplTest.class
-				.getResourceAsStream("/invoke-rpc-status/xml/data-rpc-input.xml");
+        InputStream xmlStream = RestconfImplTest.class.getResourceAsStream("/invoke-rpc-status/xml/data-rpc-input.xml");
 
-		InputStream xmlStreamOutput = RestconfImplTest.class
-				.getResourceAsStream("/invoke-rpc-status/xml/data-rpc-output.xml");
-		CompositeNode outputCompositeNode = TestUtils
-				.loadCompositeNode(xmlStreamOutput);
+        InputStream xmlStreamOutput = RestconfImplTest.class
+                .getResourceAsStream("/invoke-rpc-status/xml/data-rpc-output.xml");
+        CompositeNode outputCompositeNode = TestUtils.loadCompositeNode(xmlStreamOutput);
 
-		String xml = TestUtils.getDocumentInPrintableForm(TestUtils
-				.loadDocumentFrom(xmlStream));
-		Entity<String> entity = Entity.entity(xml, MEDIA_TYPE_DRAFT02);
+        String xml = TestUtils.getDocumentInPrintableForm(TestUtils.loadDocumentFrom(xmlStream));
+        Entity<String> entity = Entity.entity(xml, MEDIA_TYPE_DRAFT02);
 
-		when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class)))
-				.thenReturn(
-						new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true)
-								.result(null).build());
-		Response response = target(uri).request(MEDIA_TYPE).post(entity);
-		assertEquals(204, response.getStatus());
+        when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class))).thenReturn(
+                new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true).result(null).build());
+        Response response = target(uri).request(MEDIA_TYPE).post(entity);
+        assertEquals(204, response.getStatus());
 
-		when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class)))
-				.thenReturn(
-						new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true)
-								.result(outputCompositeNode).build());
-		response = target(uri).request(MEDIA_TYPE).post(entity);
-		assertEquals(200, response.getStatus());
+        when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class))).thenReturn(
+                new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true).result(outputCompositeNode).build());
+        response = target(uri).request(MEDIA_TYPE).post(entity);
+        assertEquals(200, response.getStatus());
 
-		when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class)))
-				.thenReturn(
-						new DummyRpcResult.Builder<CompositeNode>().isSuccessful(false)
-								.result(null).build());
-		response = target(uri).request(MEDIA_TYPE).post(entity);
-		assertEquals(500, response.getStatus());
+        when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class))).thenReturn(
+                new DummyRpcResult.Builder<CompositeNode>().isSuccessful(false).result(null).build());
+        response = target(uri).request(MEDIA_TYPE).post(entity);
+        assertEquals(500, response.getStatus());
 
-		uri = createUri("/operations/", "invoke-rpc-status:testInputs");
+        uri = createUri("/operations/", "invoke-rpc-status:testInputs");
 
-		when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class)))
-				.thenReturn(
-						new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true)
-								.result(null).build());
-		response = target(uri).request(MEDIA_TYPE).post(entity);
-		assertEquals(404, response.getStatus());
-	}
+        when(brokerFacade.invokeRpc(any(QName.class), any(CompositeNode.class))).thenReturn(
+                new DummyRpcResult.Builder<CompositeNode>().isSuccessful(true).result(null).build());
+        response = target(uri).request(MEDIA_TYPE).post(entity);
+        assertEquals(404, response.getStatus()); 
+    }
 
-	@Override
-	protected Application configure() {
-		enable(TestProperties.LOG_TRAFFIC);
-		enable(TestProperties.DUMP_ENTITY);
-		enable(TestProperties.RECORD_LOG_LEVEL);
-		set(TestProperties.RECORD_LOG_LEVEL, Level.ALL.intValue());
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+        enable(TestProperties.RECORD_LOG_LEVEL);
+        set(TestProperties.RECORD_LOG_LEVEL, Level.ALL.intValue());
 
-		ResourceConfig resourceConfig = new ResourceConfig();
-		resourceConfig = resourceConfig.registerInstances(restconfImpl,
-				StructuredDataToXmlProvider.INSTANCE,
-				XmlToCompositeNodeProvider.INSTANCE);
-		return resourceConfig;
-	}
+        ResourceConfig resourceConfig = new ResourceConfig();
+        resourceConfig = resourceConfig.registerInstances(restconfImpl, StructuredDataToXmlProvider.INSTANCE,
+                XmlToCompositeNodeProvider.INSTANCE);
+        return resourceConfig;
+    }
 
-	private String createUri(String prefix, String encodedPart)
-			throws UnsupportedEncodingException {
-		return URI.create(
-				prefix
-						+ URLEncoder.encode(encodedPart,
-								Charsets.US_ASCII.name()).toString())
-				.toASCIIString();
-	}
+    private String createUri(String prefix, String encodedPart) throws UnsupportedEncodingException {
+        return URI.create(prefix + URLEncoder.encode(encodedPart, Charsets.US_ASCII.name()).toString()).toASCIIString();
+    }
 }
