5309045eb35792668445a0ece3ed4f152d09d5e5
Tomas Olvecky <tolvecky@cisco.com>
2013-12-17 18:44:46 +0100

    Fix date initialization in NetconfOperationServiceImplTest.
    Use SimpleDateFormat instead of Date(0) as the later fixes the date to UTC rendering local date to
    1969-12-31 for timezones > 0
    
    Change-Id: I455610603e66b9748df8e5b035761ca22617c4eb
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

--- opendaylight/netconf/config-netconf-connector/src/test/java/org/opendaylight/controller/netconf/confignetconfconnector/osgi/NetconfOperationServiceImplTest.java
+++ opendaylight/netconf/config-netconf-connector/src/test/java/org/opendaylight/controller/netconf/confignetconfconnector/osgi/NetconfOperationServiceImplTest.java
@@ -19,16 +19,27 @@ import org.opendaylight.controller.config.yangjmxgenerator.ModuleMXBeanEntry;
 import org.opendaylight.yangtools.yang.common.QName;
 
 import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 public class NetconfOperationServiceImplTest {
 
-    private Date date = new Date(0);
+    private static final Date date1970_01_01;
+
+    static {
+        try {
+            date1970_01_01 = new SimpleDateFormat("yyyy-MM-dd").parse("1970-01-01");
+        } catch (ParseException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @Test
     public void testCheckConsistencyBetweenYangStoreAndConfig_ok() throws Exception {
@@ -51,22 +62,22 @@ public class NetconfOperationServiceImplTest {
                 mockYangStoreSnapshot());
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test
     public void testCheckConsistencyBetweenYangStoreAndConfig_yangStoreMore() throws Exception {
         try {
             NetconfOperationServiceImpl.checkConsistencyBetweenYangStoreAndConfig(mockJmxClient("qname1"),
                     mockYangStoreSnapshot("qname2", "qname1"));
+            fail("An exception of type " + IllegalArgumentException.class + " was expected");
         } catch (IllegalStateException e) {
             String message = e.getMessage();
             Assert.assertThat(
                     message,
                     JUnitMatchers
-                            .containsString(" missing from config subsystem but present in yangstore: [(namespace?revision=1970-01-01)qname2]"));
+                            .containsString("missing from config subsystem but present in yangstore: [(namespace?revision=1970-01-01)qname2]"));
             Assert.assertThat(
                     message,
                     JUnitMatchers
                             .containsString("All modules present in config: [(namespace?revision=1970-01-01)qname1]"));
-            throw e;
         }
     }
 
@@ -97,7 +108,7 @@ public class NetconfOperationServiceImplTest {
     }
 
     private QName getQName(String qname) {
-        return new QName(URI.create("namespace"), date, qname);
+        return new QName(URI.create("namespace"), date1970_01_01, qname);
     }
 
     private LookupRegistry mockJmxClient(String... visibleQNames) {
