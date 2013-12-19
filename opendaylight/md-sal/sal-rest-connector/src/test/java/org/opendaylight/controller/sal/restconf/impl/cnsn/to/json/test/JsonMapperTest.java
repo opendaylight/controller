0da2963ce3bcd3bb6477b37d08604980fe1b44c4
Ladislav Borak <lborak@cisco.com>
2013-12-19 17:07:27 +0100

    Added tests for JasonMapper and XmlMapper
    Change-Id: I7e57771289da20d07ce4f7f43a38dbca87c2c1df
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

---
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/cnsn/to/json/test/JsonMapperTest.java
@@ -0,0 +1,49 @@
+package org.opendaylight.controller.sal.restconf.impl.cnsn.to.json.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Set;
+
+import javax.ws.rs.WebApplicationException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.controller.sal.rest.impl.StructuredDataToJsonProvider;
+import org.opendaylight.controller.sal.restconf.impl.test.TestUtils;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+
+public class JsonMapperTest {
+
+    private static CompositeNode compositeNode;
+    private static Set<Module> modules;
+
+    @BeforeClass
+    public static void init() throws FileNotFoundException {
+        compositeNode = TestUtils.loadCompositeNode("/cnsn-to-json/json_mapper_test/xml/data.xml");
+        modules = TestUtils.resolveModulesFrom("/cnsn-to-json/json_mapper_test");
+    }
+
+    @Test
+    public void writeTest() throws IOException {
+
+        String jsonOutput = "";
+
+        Module module = TestUtils.resolveModule(null, modules);
+        DataSchemaNode dataSchemaNode = TestUtils.resolveDataSchemaNode("user", module);
+        TestUtils.normalizeCompositeNode(compositeNode, modules, "modul:user/name");
+
+        try {
+            jsonOutput = TestUtils.writeCompNodeWithSchemaContextToOutput(compositeNode, modules, dataSchemaNode,
+                    StructuredDataToJsonProvider.INSTANCE);
+        } catch (WebApplicationException | IOException e) {
+            assertTrue(false);
+        }
+
+        assertNotNull(jsonOutput);
+    }
+}
