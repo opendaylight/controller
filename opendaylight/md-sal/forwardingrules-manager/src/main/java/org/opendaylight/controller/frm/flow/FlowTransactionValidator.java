26b5fc5bd27e6275a51a3b6de37f617b8372d4e3
Govind <p.govinda.rajulu@ericsson.com>
2013-12-27 15:23:23 +0530

    https://git.opendaylight.org/gerrit/3894
    Signed-off-by: Govind <p.govinda.rajulu@ericsson.com>
    Change-Id: Ia96bfa5f687e75f8faa4fb3ed94afc6aed27f835

--- opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/flow/FlowTransactionValidator.java
+++ opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/flow/FlowTransactionValidator.java
@@ -1,9 +1,125 @@
 package org.opendaylight.controller.frm.flow;
 
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.opendaylight.controller.forwardingrulesmanager.consumer.impl.FRMUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FlowTransactionValidator {
+    protected static final Logger logger = LoggerFactory.getLogger(FlowTransactionValidator.class);
 
     public static void validate(FlowTransaction transaction) throws IllegalStateException {
         // NOOP
+        Set<Entry<InstanceIdentifier<?>, DataObject>> createdEntries = transaction.getModification()
+                .getCreatedConfigurationData().entrySet();
+
+        Set<Entry<InstanceIdentifier<?>, DataObject>> updatedEntries = new HashSet<Entry<InstanceIdentifier<?>, DataObject>>();
+        updatedEntries.addAll(transaction.getModification().getUpdatedConfigurationData().entrySet());
+        updatedEntries.removeAll(createdEntries);
+
+        Set<InstanceIdentifier<?>> removeEntriesInstanceIdentifiers = transaction.getModification()
+                .getRemovedConfigurationData();
+        transaction.getModification().getOriginalConfigurationData();
+
+        for (Entry<InstanceIdentifier<?>, DataObject> entry : createdEntries) {
+            if (entry.getValue() instanceof Flow) {
+                Flow flow = (Flow) entry.getValue();
+                InstanceIdentifier nodeInstanceId = entry.getKey().firstIdentifierOf(Node.class);
+                if (!validateNode(nodeInstanceId)) {
+                    logger.error("Node is null");
+                    return;
+                }
+                logger.debug("Coming add cc in FlowDatacommitHandler");
+                boolean status = validate(flow);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+
+        for (Entry<InstanceIdentifier<?>, DataObject> entry : updatedEntries) {
+            if (entry.getValue() instanceof Flow) {
+                logger.debug("Coming update cc in FlowDatacommitHandler");
+                Flow updatedFlow = (Flow) entry.getValue();
+                Flow originalFlow = (Flow) transaction.getModification().getOriginalConfigurationData()
+                        .get(entry.getKey());
+                InstanceIdentifier nodeInstanceId = entry.getKey().firstIdentifierOf(Node.class);
+                if (!validateNode(nodeInstanceId)) {
+                    logger.error("Node is null");
+                    return;
+                }
+                logger.debug("Coming update cc in FlowDatacommitHandler");
+                boolean status = validate(updatedFlow);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+
+        for (InstanceIdentifier<?> instanceId : removeEntriesInstanceIdentifiers) {
+            DataObject removeValue = transaction.getModification().getOriginalConfigurationData().get(instanceId);
+            if (removeValue instanceof Flow) {
+                logger.debug("Coming remove cc in FlowDatacommitHandler");
+                Flow flow = (Flow) removeValue;
+                InstanceIdentifier nodeInstanceId = instanceId.firstIdentifierOf(Node.class);
+                if (!validateNode(nodeInstanceId)) {
+                    logger.error("Node is null");
+                    return;
+                }
+                logger.debug("Coming remove cc in FlowDatacommitHandler");
+                boolean status = validate(flow);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public static boolean validateNode(InstanceIdentifier nodeInstanceId) {
+        return (nodeInstanceId != null);
+    }
+
+    public static boolean validate(Flow flow) {
+
+        String msg = ""; // Specific part of warn/error log
+
+        boolean result = true;
+        // flow Name validation
+        if (!FRMUtil.isNameValid(flow.getFlowName())) {
+            msg = "Invalid Flow name";
+            result = false;
+        }
+
+        // TODO: Validate we are seeking to program a flow against a valid
+        // Node
+
+        if (result == true && flow.getPriority() != null) {
+            if (flow.getPriority() < 0 || flow.getPriority() > 65535) {
+                msg = String.format("priority %s is not in the range 0 - 65535", flow.getPriority());
+                result = false;
+            }
+        }
+
+        if (!FRMUtil.validateMatch(flow)) {
+            logger.error("Not a valid Match");
+            result = false;
+        }
+        if (!FRMUtil.validateInstructions(flow)) {
+            logger.error("Not a valid Instruction");
+            result = false;
+        }
+        if (result == false) {
+            logger.warn("Invalid Configuration for flow {}. The failure is {}", flow, msg);
+            logger.error("Invalid Configuration ({})", msg);
+        }
+        return result;
     }
 
 }
--- opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/group/GroupTransactionValidator.java
+++ opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/group/GroupTransactionValidator.java
@@ -1,9 +1,108 @@
 package org.opendaylight.controller.frm.group;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.opendaylight.controller.forwardingrulesmanager.consumer.impl.FRMUtil;
+import org.opendaylight.controller.md.sal.common.api.data.DataModification;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.Group;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.GroupTypes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.group.Buckets;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.group.buckets.Bucket;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GroupTransactionValidator {
+    protected static final Logger logger = LoggerFactory.getLogger(GroupTransactionValidator.class);
 
     public static void validate(GroupTransaction transaction) throws IllegalStateException {
         // NOOP
+        DataModification<InstanceIdentifier<?>, DataObject> modification = transaction.getModification();
+        // get created entries
+        Set<Entry<InstanceIdentifier<? extends DataObject>, DataObject>> createdEntries = modification
+                .getCreatedConfigurationData().entrySet();
+
+        // get updated entries
+        Set<Entry<InstanceIdentifier<? extends DataObject>, DataObject>> updatedEntries = new HashSet<Entry<InstanceIdentifier<? extends DataObject>, DataObject>>();
+
+        updatedEntries.addAll(modification.getUpdatedConfigurationData().entrySet());
+        updatedEntries.removeAll(createdEntries);
+
+        // get removed entries
+        Set<InstanceIdentifier<? extends DataObject>> removeEntriesInstanceIdentifiers = modification
+                .getRemovedConfigurationData();
+
+        for (Entry<InstanceIdentifier<? extends DataObject>, DataObject> entry : createdEntries) {
+            if (entry.getValue() instanceof Group) {
+                Group group = (Group) entry.getValue();
+                boolean status = validateGroup(group);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+
+        for (Entry<InstanceIdentifier<?>, DataObject> entry : updatedEntries) {
+            if (entry.getValue() instanceof Group) {
+                Group originalGroup = (Group) modification.getOriginalConfigurationData().get(entry.getKey());
+                Group updatedGroup = (Group) entry.getValue();
+                boolean status = validateGroup(updatedGroup);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+
+        for (InstanceIdentifier<?> instanceId : removeEntriesInstanceIdentifiers) {
+            DataObject removeValue = modification.getOriginalConfigurationData().get(instanceId);
+            if (removeValue instanceof Group) {
+                Group removeGroup = (Group) removeValue;
+                boolean status = validateGroup(removeGroup);
+                if (!status) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public static boolean validateGroup(Group group) {
+        String groupName;
+        Iterator<Bucket> bucketIterator;
+        Buckets groupBuckets;
+
+        if (null != group) {
+            groupName = group.getGroupName();
+            if (!FRMUtil.isNameValid(groupName)) {
+                logger.error("Group Name is invalid %s" + groupName);
+                return false;
+            }
+
+            if (!(group.getGroupType().getIntValue() >= GroupTypes.GroupAll.getIntValue() && group.getGroupType()
+                    .getIntValue() <= GroupTypes.GroupFf.getIntValue())) {
+                logger.error("Invalid Group type %d" + group.getGroupType().getIntValue());
+                return false;
+            }
+
+            groupBuckets = group.getBuckets();
+
+            if (null != groupBuckets && null != groupBuckets.getBucket()) {
+                bucketIterator = groupBuckets.getBucket().iterator();
+
+                while (bucketIterator.hasNext()) {
+                    if (!(FRMUtil.validateActions(bucketIterator.next().getAction()))) {
+                        logger.error("Error in action bucket");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return false;
+
     }
 
 }
