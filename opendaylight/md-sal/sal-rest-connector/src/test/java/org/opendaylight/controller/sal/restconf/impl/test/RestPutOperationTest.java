3403f87bf4c635340bb9c887dfd643707b1d6c18
Martin Sunal <msunal@cisco.com>
2014-01-14 15:07:49 +0100

    Changed mount point URI decoding in restconf
    - string "yang-ext:mount" in URI means mount point.
      After this string must be string in format moduleName:dataNode
      example:
      /rectconf/config/moduleName1:cont1/cont2/yang-ext:mount/moduleName2:cont3/cont4
         mount point path - moduleName1:cont1/cont2/
         path behind mount point - moduleName2:cont3/cont4
    
    - refactoring of POST, PUT, GET, DELETE tests for status codes
    
    Change-Id: I79bfaec1fab6ffd89295fb8461c167c0c85638ae
    Signed-off-by: Martin Sunal <msunal@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestPutOperationTest.java
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestPutOperationTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.opendaylight.controller.sal.restconf.impl.test.RestOperationUtils.JSON;
 import static org.opendaylight.controller.sal.restconf.impl.test.RestOperationUtils.XML;
 import static org.opendaylight.controller.sal.restconf.impl.test.RestOperationUtils.createUri;
-import static org.opendaylight.controller.sal.restconf.impl.test.RestOperationUtils.entity;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,7 +13,6 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.concurrent.Future;
-import java.util.logging.Level;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
@@ -24,13 +21,10 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.TestProperties;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
 import org.opendaylight.controller.sal.core.api.mount.MountService;
-import org.opendaylight.controller.sal.rest.api.Draft01;
 import org.opendaylight.controller.sal.rest.api.Draft02;
 import org.opendaylight.controller.sal.rest.impl.JsonToCompositeNodeProvider;
 import org.opendaylight.controller.sal.rest.impl.StructuredDataToJsonProvider;
@@ -47,7 +41,6 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 public class RestPutOperationTest extends JerseyTest {
 
     private static String xmlData;
-    private static String jsonData;
 
     private static BrokerFacade brokerFacade;
     private static RestconfImpl restconfImpl;
@@ -67,25 +60,18 @@ public class RestPutOperationTest extends JerseyTest {
         loadData();
     }
 
-    @Before
-    public void logs() throws IOException {
-        /* enable/disable Jersey logs to console */
-        /*
-         * List<LogRecord> loggedRecords = getLoggedRecords(); for (LogRecord l
-         * : loggedRecords) { System.out.println(l.getMessage()); }
-         */
+    private static void loadData() throws IOException {
+        InputStream xmlStream = RestconfImplTest.class.getResourceAsStream("/parts/ietf-interfaces_interfaces.xml");
+        xmlData = TestUtils.getDocumentInPrintableForm(TestUtils.loadDocumentFrom(xmlStream));
     }
 
     @Override
     protected Application configure() {
         /* enable/disable Jersey logs to console */
-        /*
-         * enable(TestProperties.LOG_TRAFFIC);
-         */
-        enable(TestProperties.DUMP_ENTITY);
-        enable(TestProperties.RECORD_LOG_LEVEL);
-        set(TestProperties.RECORD_LOG_LEVEL, Level.ALL.intValue());
-
+//        enable(TestProperties.LOG_TRAFFIC);
+//        enable(TestProperties.DUMP_ENTITY);
+//        enable(TestProperties.RECORD_LOG_LEVEL);
+//        set(TestProperties.RECORD_LOG_LEVEL, Level.ALL.intValue());
         ResourceConfig resourceConfig = new ResourceConfig();
         resourceConfig = resourceConfig.registerInstances(restconfImpl, StructuredDataToXmlProvider.INSTANCE,
                 StructuredDataToJsonProvider.INSTANCE, XmlToCompositeNodeProvider.INSTANCE,
@@ -94,73 +80,30 @@ public class RestPutOperationTest extends JerseyTest {
     }
 
     /**
-     * Test method
-     * {@link RestconfImpl#updateConfigurationData(String, CompositeNode)} of
-     * RestconfImpl for "/config/...identifier..." URL. Return status code is
-     * 200.
-     * 
+     * Tests of status codes for "/config/{identifier}".
      */
     @Test
-    public void putConfigDataViaUrlTest200() throws UnsupportedEncodingException {
+    public void putConfigStatusCodes() throws UnsupportedEncodingException {
+        String uri = createUri("/config/", "ietf-interfaces:interfaces/interface/eth0");
         mockCommitConfigurationDataPutMethod(TransactionStatus.COMMITED);
-        putDataViaUrlTest("/config/", Draft02.MediaTypes.DATA + JSON, jsonData, 200);
-        putDataViaUrlTest("/config/", Draft02.MediaTypes.DATA + XML, xmlData, 200);
-        putDataViaUrlTest("/config/", MediaType.APPLICATION_JSON, jsonData, 200);
-        putDataViaUrlTest("/config/", MediaType.APPLICATION_XML, xmlData, 200);
-        putDataViaUrlTest("/config/", MediaType.TEXT_XML, xmlData, 200);
-
-    }
-
-    /**
-     * Test method
-     * {@link RestconfImpl#updateConfigurationData(String, CompositeNode)} of
-     * RestconfImpl for "/config/...identifier..." URL. Return status code is
-     * 500.
-     * 
-     */
-    @Test
-    public void putConfigDataViaUrlTest500() throws UnsupportedEncodingException {
+        assertEquals(200, put(uri, MediaType.APPLICATION_XML, xmlData));
+        
         mockCommitConfigurationDataPutMethod(TransactionStatus.FAILED);
-        putDataViaUrlTest("/config/", Draft02.MediaTypes.DATA + JSON, jsonData, 500);
-        putDataViaUrlTest("/config/", Draft02.MediaTypes.DATA + XML, xmlData, 500);
-        putDataViaUrlTest("/config/", MediaType.APPLICATION_JSON, jsonData, 500);
-        putDataViaUrlTest("/config/", MediaType.APPLICATION_XML, xmlData, 500);
-        putDataViaUrlTest("/config/", MediaType.TEXT_XML, xmlData, 500);
-
+        assertEquals(500, put(uri, MediaType.APPLICATION_XML, xmlData));
     }
 
+
     /**
-     * Test method
-     * {@link RestconfImpl#updateConfigurationData(String, CompositeNode)} of
-     * RestconfImpl for "/datastore/...identifier..." URL. Return status code is
-     * 200.
-     * 
+     * Tests of status codes for "/datastore/{identifier}".
      */
     @Test
-    public void putDatastoreDataViaUrlTest200() throws UnsupportedEncodingException {
+    public void putDatastoreStatusCodes() throws UnsupportedEncodingException {
+        String uri = createUri("/datastore/", "ietf-interfaces:interfaces/interface/eth0");
         mockCommitConfigurationDataPutMethod(TransactionStatus.COMMITED);
-        putDataViaUrlTest("/datastore/", Draft01.MediaTypes.DATA + JSON, jsonData, 200);
-        putDataViaUrlTest("/datastore/", Draft01.MediaTypes.DATA + XML, xmlData, 200);
-        putDataViaUrlTest("/datastore/", MediaType.APPLICATION_JSON, jsonData, 200);
-        putDataViaUrlTest("/datastore/", MediaType.APPLICATION_XML, xmlData, 200);
-        putDataViaUrlTest("/datastore/", MediaType.TEXT_XML, xmlData, 200);
-    }
-
-    /**
-     * Test method
-     * {@link RestconfImpl#updateConfigurationData(String, CompositeNode)} of
-     * RestconfImpl for "/datastore/...identifier..." URL. Return status code is
-     * 500.
-     * 
-     */
-    @Test
-    public void putDatastoreDataViaUrlTest500() throws UnsupportedEncodingException {
+        assertEquals(200, put(uri, MediaType.APPLICATION_XML, xmlData));
+        
         mockCommitConfigurationDataPutMethod(TransactionStatus.FAILED);
-        putDataViaUrlTest("/datastore/", Draft01.MediaTypes.DATA + JSON, jsonData, 500);
-        putDataViaUrlTest("/datastore/", Draft01.MediaTypes.DATA + XML, xmlData, 500);
-        putDataViaUrlTest("/datastore/", MediaType.APPLICATION_JSON, jsonData, 500);
-        putDataViaUrlTest("/datastore/", MediaType.APPLICATION_XML, xmlData, 500);
-        putDataViaUrlTest("/datastore/", MediaType.TEXT_XML, xmlData, 500);
+        assertEquals(500, put(uri, MediaType.APPLICATION_XML, xmlData));
     }
 
     @Test
@@ -184,19 +127,8 @@ public class RestPutOperationTest extends JerseyTest {
         assertEquals(200, response.getStatus());
     }
 
-    private void putDataViaUrlTest(String uriPrefix, String mediaType, String data, int responseStatus)
-            throws UnsupportedEncodingException {
-        String uri = createUri(uriPrefix, "ietf-interfaces:interfaces/interface/eth0");
-        Response response = target(uri).request(mediaType).put(entity(data, mediaType));
-        assertEquals(responseStatus, response.getStatus());
-    }
-
-    private static void loadData() throws IOException {
-        InputStream xmlStream = RestconfImplTest.class.getResourceAsStream("/parts/ietf-interfaces_interfaces.xml");
-        xmlData = TestUtils.getDocumentInPrintableForm(TestUtils.loadDocumentFrom(xmlStream));
-
-        String jsonPath = RestconfImplTest.class.getResource("/parts/ietf-interfaces_interfaces.json").getPath();
-        jsonData = TestUtils.loadTextFile(jsonPath);
+    private int put(String uri, String mediaType, String data) throws UnsupportedEncodingException {
+        return target(uri).request(mediaType).put(Entity.entity(data, mediaType)).getStatus();
     }
 
     private void mockCommitConfigurationDataPutMethod(TransactionStatus statusName) {
