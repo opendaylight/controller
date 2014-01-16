645c76b611cd9b75428d4f3472893db964e28fd0
Robert Varga <rovarga@cisco.com>
2014-01-16 11:58:29 +0100

    Move protocol framework from BGPCEP project
    This moves the current parts of BGPCEP protocol framework into the
    controller project, eliminating the circular dependency forced by
    netconf being resident in this project.
    
    Change-Id: I7c8457ebaec5c261217587d60a95a7b35e473d1e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/commons/protocol-framework/src/main/java/org/opendaylight/protocol/framework/SessionNegotiatorFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.framework;
+
+import io.netty.channel.Channel;
+import io.netty.util.concurrent.Promise;
+
+/**
+ * A factory class creating SessionNegotiators.
+ *
+ * @param <S> session type
+ */
+public interface SessionNegotiatorFactory<M, S extends ProtocolSession<?>, L extends SessionListener<?, ?, ?>> {
+    /**
+     * Create a new negotiator attached to a channel, which will notify
+     * a promise once the negotiation completes.
+     *
+     * @param channel Underlying channel
+     * @param promise Promise to be notified
+     * @return new negotiator instance
+     */
+    SessionNegotiator<S> getSessionNegotiator(SessionListenerFactory<L> factory, Channel channel, Promise<S> promise);
+}
