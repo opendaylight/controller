d8a5f91b72027281165bd43687efa3feb56a0f6e
Martin Bobak <mbobak@cisco.com>
2014-01-22 16:24:50 +0100

    introducing general purpose websocket client
    Change-Id: I0207c39b50e88548959b994cdfdedef5580b6dda
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ opendaylight/websocket/websocket-client/src/main/java/org/opendaylight/controller/websocket/client/callback/IClientMessageCallback.java
@@ -0,0 +1,9 @@
+package org.opendaylight.controller.websocket.client.callback;
+
+/**
+ * Created by mbobak on 1/22/14.
+ */
+public interface IClientMessageCallback {
+
+    public void onMessageReceived(Object message);
+}
---
+++ opendaylight/websocket/websocket-server/src/main/java/org/opendaylight/controller/websocket/server/WebSocketServer.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.opendaylight.controller.websocket.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A HTTP server which serves Web Socket requests at:
+ *
+ * http://localhost:8080/websocket
+ *
+ * Open your browser at http://localhost:8080/, then the demo page will be loaded and a Web Socket connection will be
+ * made automatically.
+ *
+ * This server illustrates support for the different web socket specification versions and will work with:
+ *
+ * <ul>
+ * <li>Safari 5+ (draft-ietf-hybi-thewebsocketprotocol-00)
+ * <li>Chrome 6-13 (draft-ietf-hybi-thewebsocketprotocol-00)
+ * <li>Chrome 14+ (draft-ietf-hybi-thewebsocketprotocol-10)
+ * <li>Chrome 16+ (RFC 6455 aka draft-ietf-hybi-thewebsocketprotocol-17)
+ * <li>Firefox 7+ (draft-ietf-hybi-thewebsocketprotocol-10)
+ * <li>Firefox 11+ (RFC 6455 aka draft-ietf-hybi-thewebsocketprotocol-17)
+ * </ul>
+ */
+public class WebSocketServer implements Runnable {
+
+    private final int port;
+    private final ServerBootstrap bootstrap = new ServerBootstrap();
+    private final EventLoopGroup bossGroup = new NioEventLoopGroup();
+    private final EventLoopGroup workerGroup = new NioEventLoopGroup();
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketServer.class.toString());
+
+    public WebSocketServer(int port) {
+        this.port = port;
+    }
+
+    public void run(){
+        try {
+            startServer();
+        } catch (Exception e) {
+            logger.trace("Exception occured while starting webSocket server {}",e);
+        }
+    }
+    public void startServer() throws Exception {
+        try {
+            bootstrap.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .childHandler(new WebSocketServerInitializer());
+
+            Channel ch = bootstrap.bind(port).sync().channel();
+            logger.info("Web socket server started at port " + port + '.');
+            logger.info("Open your browser and navigate to http://localhost:" + port + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+            System.out.println("Websocket server stopped.");
+        }
+    }
+
+
+}
