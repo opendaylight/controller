7d7f86f108c85b5325f8bea5d3dbf60efca776ce
Sapan Shah <sapshah@cisco.com>
2014-01-29 01:01:32 -0800

    TS GUI does not show ToS field in flow stats
    Change-Id: I4b0bfc1988ea4fcc3eae02a73c3aebae1622f382
    Signed-off-by: Sapan Shah <sapshah@cisco.com>

--- opendaylight/web/troubleshoot/src/main/resources/js/page.js
+++ opendaylight/web/troubleshoot/src/main/resources/js/page.js
@@ -374,6 +374,11 @@ one.f.troubleshooting.existingNodes = {
                             sortable: true
                         },
                         {
+                            property: 'nwTOS',
+                            label: 'ToS Bits',
+                            sortable: true
+                        },
+                        {
                             property: 'nwProto',
                             label: 'NW Proto',
                             sortable: true
@@ -437,6 +442,7 @@ one.f.troubleshooting.existingNodes = {
                     entry.push(value["dlVlan"]);
                     entry.push(value["nwSrc"]);
                     entry.push(value["nwDst"]);
+                    entry.push(value["nwTOS"]);
                     entry.push(value["nwProto"]);
                     entry.push(value["tpSrc"]);
                     entry.push(value["tpDst"]);
