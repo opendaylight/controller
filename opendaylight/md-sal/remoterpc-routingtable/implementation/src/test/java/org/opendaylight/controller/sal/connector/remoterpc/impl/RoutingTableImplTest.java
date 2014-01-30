3e40e385327f62e7df8cbb6d3428bfe99e3d14f9
Abhishek Kumar <abhishk2@cisco.com>
2014-01-30 13:29:43 -0800

    Added Unit Tests 1. Added unit tests for routing table 2. Removed RouteIdentifier <-> JSON conversion. Its not used anymore 3. Rebased with master
    Change-Id: I7cbdad970ddeb3db89b3226dc1f4153a2306f998
    Signed-off-by: Abhishek Kumar <abhishk2@cisco.com>

--- opendaylight/md-sal/remoterpc-routingtable/implementation/src/test/java/org/opendaylight/controller/sal/connector/remoterpc/impl/RoutingTableImplTest.java
+++ opendaylight/md-sal/remoterpc-routingtable/implementation/src/test/java/org/opendaylight/controller/sal/connector/remoterpc/impl/RoutingTableImplTest.java
@@ -16,204 +16,315 @@ import org.junit.Test;
 import org.opendaylight.controller.clustering.services.IClusterGlobalServices;
 import org.opendaylight.controller.clustering.services.IClusterServices;
 import org.opendaylight.controller.sal.connector.api.RpcRouter;
-import org.opendaylight.controller.sal.connector.remoterpc.api.RouteChangeListener;
+import org.opendaylight.controller.sal.connector.remoterpc.api.RoutingTable;
+import org.opendaylight.controller.sal.connector.remoterpc.api.RoutingTableException;
+import org.opendaylight.controller.sal.connector.remoterpc.api.SystemException;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 
 import java.net.URI;
 import java.util.EnumSet;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-/**
- * @author: syedbahm
- */
 public class RoutingTableImplTest {
 
   private final URI namespace = URI.create("http://cisco.com/example");
   private final QName QNAME = new QName(namespace, "global");
 
-  private IClusterGlobalServices ics;
-  private RoutingTableImpl rti;
+  private IClusterGlobalServices clusterService;
+  private RoutingTableImpl<RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier>, String> routingTable;
   ConcurrentMap mockGlobalRpcCache;
   ConcurrentMap mockRpcCache;
 
   @Before
   public void setUp() throws Exception{
-    ics = mock(IClusterGlobalServices.class);
-    rti = new RoutingTableImpl();
-    mockGlobalRpcCache = mock(ConcurrentMap.class);
-    mockRpcCache = mock(ConcurrentMap.class);
+    clusterService = mock(IClusterGlobalServices.class);
+    routingTable = new RoutingTableImpl<RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier>, String>();
+    mockGlobalRpcCache = new ConcurrentHashMap<>();
+    mockRpcCache = new ConcurrentHashMap<>();
     createRoutingTableCache();
   }
 
   @After
   public void tearDown(){
-    reset(ics);
-    reset(mockGlobalRpcCache);
-    reset(mockRpcCache);
+    reset(clusterService);
+    mockGlobalRpcCache = null;
+    mockRpcCache = null;
   }
 
   @Test
-  public void testAddGlobalRoute() throws Exception {
-    //ConcurrentMap concurrentMap = createRoutingTableCache();
+  public void addGlobalRoute_ValidArguments_ShouldAdd() throws Exception {
 
     Assert.assertNotNull(mockGlobalRpcCache);
-    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = mock(RpcRouter.RouteIdentifier.class);
-    InstanceIdentifier identifier = mock(InstanceIdentifier.class);
-    when(routeIdentifier.getType()).thenReturn(QNAME);
-    when(routeIdentifier.getRoute()).thenReturn(identifier);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
 
-    rti.addGlobalRoute(routeIdentifier, "172.27.12.1:5000");
+    final String expectedRoute = "172.27.12.1:5000";
+    routingTable.addGlobalRoute(routeIdentifier, expectedRoute);
 
-    Set<String> globalService = new HashSet<String>();
-    globalService.add("172.27.12.1:5000");
-
-    when(mockGlobalRpcCache.get(routeIdentifier)).thenReturn(globalService);
-    ConcurrentMap latestCache = rti.getGlobalRpcCache();
-
+    ConcurrentMap latestCache = routingTable.getGlobalRpcCache();
     Assert.assertEquals(mockGlobalRpcCache, latestCache);
+    Assert.assertEquals(expectedRoute, latestCache.get(routeIdentifier));
+  }
 
-    Set<String> servicesGlobal = (Set<String>) latestCache.get(routeIdentifier);
-    Assert.assertEquals(servicesGlobal.size(), 1);
+  @Test (expected = RoutingTable.DuplicateRouteException.class)
+  public void addGlobalRoute_DuplicateRoute_ShouldThrow() throws Exception{
 
-    Assert.assertEquals(servicesGlobal.iterator().next(), "172.27.12.1:5000");
+    Assert.assertNotNull(mockGlobalRpcCache);
+
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
+    routingTable.addGlobalRoute(routeIdentifier, new String());
+    routingTable.addGlobalRoute(routeIdentifier, new String());
+  }
+
+  @Test
+  public void getGlobalRoute_ExistingRouteId_ShouldReturnRoute() throws Exception {
+
+    Assert.assertNotNull(mockGlobalRpcCache);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
+    String expectedRoute = "172.27.12.1:5000";
+
+    routingTable.addGlobalRoute(routeIdentifier, expectedRoute);
+
+    String actualRoute = (String) routingTable.getGlobalRoute(routeIdentifier);
+    Assert.assertEquals(expectedRoute, actualRoute);
+  }
+
+  @Test
+  public void getGlobalRoute_NonExistentRouteId_ShouldReturnNull() throws Exception {
+
+    Assert.assertNotNull(mockGlobalRpcCache);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
+
+    String actualRoute = (String) routingTable.getGlobalRoute(routeIdentifier);
+    Assert.assertNull(actualRoute);
+  }
+
+  @Test
+  public void removeGlobalRoute_ExistingRouteId_ShouldRemove() throws Exception {
+
+    Assert.assertNotNull(mockGlobalRpcCache);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
+
+    ConcurrentMap cache = routingTable.getGlobalRpcCache();
+    Assert.assertTrue(cache.size() == 0);
+    routingTable.addGlobalRoute(routeIdentifier, "172.27.12.1:5000");
+    Assert.assertTrue(cache.size() == 1);
+
+    routingTable.removeGlobalRoute(routeIdentifier);
+    Assert.assertTrue(cache.size() == 0);
 
   }
 
   @Test
-  public void testGetGlobalRoute() throws Exception {
-    //ConcurrentMap concurrentMap = createRoutingTableCache();
+  public void removeGlobalRoute_NonExistentRouteId_ShouldDoNothing() throws Exception {
 
     Assert.assertNotNull(mockGlobalRpcCache);
-    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = mock(RpcRouter.RouteIdentifier.class);
-    InstanceIdentifier identifier = mock(InstanceIdentifier.class);
-    when(routeIdentifier.getContext()).thenReturn(QNAME);
-    when(routeIdentifier.getRoute()).thenReturn(identifier);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = getRouteIdentifier();
 
-    rti.addGlobalRoute(routeIdentifier, "172.27.12.1:5000");
+    ConcurrentMap cache = routingTable.getGlobalRpcCache();
+    Assert.assertTrue(cache.size() == 0);
 
-    String globalService = "172.27.12.1:5000";
+    routingTable.removeGlobalRoute(routeIdentifier);
+    Assert.assertTrue(cache.size() == 0);
 
-    when(mockGlobalRpcCache.get(routeIdentifier)).thenReturn(globalService);
-    ConcurrentMap latestCache = rti.getGlobalRpcCache();
-
-    Assert.assertEquals(mockGlobalRpcCache, latestCache);
-
-    String servicesGlobal = (String) rti.getGlobalRoute(routeIdentifier);
-    Assert.assertEquals(servicesGlobal, "172.27.12.1:5000");
   }
 
-  //    @Test
-//    public void testRegisterRouteChangeListener() throws Exception {
-//        Assert.assertEquals(rti.getRegisteredRouteChangeListeners().size(),0);
-//        rti.registerRouteChangeListener(new RouteChangeListenerImpl());
-//
-//        Assert.assertEquals(rti.getRegisteredRouteChangeListeners().size(),0); //old should not work
-//        //what about the new approach - using whiteboard pattern
-//        rti.setRouteChangeListener(new RouteChangeListenerImpl());
-//
-//        Assert.assertEquals(rti.getRegisteredRouteChangeListeners().size(),1); //should not work
-//
-//
-//    }
   @Test
-  public void testRemoveGlobalRoute() throws Exception {
+  public void addRoute_ForNewRouteId_ShouldAddRoute() throws Exception {
+    Assert.assertTrue(mockRpcCache.size() == 0);
 
-    //ConcurrentMap concurrentMap = createRoutingTableCache();
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeId = getRouteIdentifier();
 
-    Assert.assertNotNull(mockGlobalRpcCache);
-    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = mock(RpcRouter.RouteIdentifier.class);
-    InstanceIdentifier identifier = mock(InstanceIdentifier.class);
-    when(routeIdentifier.getContext()).thenReturn(QNAME);
-    when(routeIdentifier.getRoute()).thenReturn(identifier);
+    routingTable.addRoute(routeId, new String());
+    Assert.assertTrue(mockRpcCache.size() == 1);
 
-    rti.addGlobalRoute(routeIdentifier, "172.27.12.1:5000");
+    Set<String> routes = routingTable.getRoutes(routeId);
+    Assert.assertEquals(1, routes.size());
+  }
 
-    String globalService = "172.27.12.1:5000";
+  @Test
+  public void addRoute_ForExistingRouteId_ShouldAppendRoute() throws Exception {
 
-    when(mockGlobalRpcCache.get(routeIdentifier)).thenReturn(globalService);
-    ConcurrentMap latestCache = rti.getGlobalRpcCache();
+    Assert.assertTrue(mockRpcCache.size() == 0);
 
-    Assert.assertEquals(mockGlobalRpcCache, latestCache);
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeId = getRouteIdentifier();
 
-    String servicesGlobal = (String)rti.getGlobalRoute(routeIdentifier);
+    String route_1 = "10.0.0.1:5955";
+    String route_2 = "10.0.0.2:5955";
 
-    Assert.assertEquals(servicesGlobal, "172.27.12.1:5000");
+    routingTable.addRoute(routeId, route_1);
+    routingTable.addRoute(routeId, route_2);
 
-    rti.removeGlobalRoute(routeIdentifier);
+    Assert.assertTrue(mockRpcCache.size() == 1);
 
-    Assert.assertNotNull(rti.getGlobalRoute(routeIdentifier));
+    Set<String> routes = routingTable.getRoutes(routeId);
+    Assert.assertEquals(2, routes.size());
+    Assert.assertTrue(routes.contains(route_1));
+    Assert.assertTrue(routes.contains(route_2));
+  }
 
+  @Test
+  public void addRoute_UsingMultipleThreads_ShouldNotOverwrite(){
+    ExecutorService threadPool = Executors.newCachedThreadPool();
+
+    int numOfRoutesToAdd = 100;
+    String routePrefix_1   = "10.0.0.1:555";
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    threadPool.submit(addRoutes(numOfRoutesToAdd, routePrefix_1, routeId));
+    String routePrefix_2   = "10.0.0.1:556";
+    threadPool.submit(addRoutes(numOfRoutesToAdd, routePrefix_2, routeId));
+
+    // wait for all tasks to complete; timeout in 10 sec
+    threadPool.shutdown();
+    try {
+      threadPool.awaitTermination(10, TimeUnit.SECONDS); //
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    Assert.assertEquals(2*numOfRoutesToAdd, routingTable.getRoutes(routeId).size());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void addRoute_NullRouteId_shouldThrowNpe() throws Exception {
+
+    routingTable.addRoute(null, new String());
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void addRoute_NullRoute_shouldThrowNpe() throws Exception{
+
+    routingTable.addRoute(getRouteIdentifier(), null);
+  }
+
+  @Test (expected = UnsupportedOperationException.class)
+  public void getRoutes_Call_ShouldReturnImmutableCopy() throws Exception{
+    Assert.assertNotNull(routingTable);
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    routingTable.addRoute(routeId, new String());
+
+    Set<String> routes = routingTable.getRoutes(routeId); //returns Immutable Set
+
+    routes.add(new String()); //can not be modified; should throw
+  }
+
+  @Test
+  public void getRoutes_With2RoutesFor1RouteId_ShouldReturnASetWithSize2() throws Exception{
+    Assert.assertNotNull(routingTable);
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    routingTable.addRoute(routeId, "10.0.0.1:5555");
+    routingTable.addRoute(routeId, "10.0.0.2:5555");
+
+    Set<String> routes = routingTable.getRoutes(routeId); //returns Immutable Set
+
+    Assert.assertEquals(2, routes.size());
+  }
+
+  @Test
+  public void getLastAddedRoute_WhenMultipleRoutesExists_ShouldReturnLatestRoute()
+    throws Exception {
+
+    Assert.assertNotNull(routingTable);
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    String route_1 = "10.0.0.1:5555";
+    String route_2 = "10.0.0.2:5555";
+    routingTable.addRoute(routeId, route_1);
+    routingTable.addRoute(routeId, route_2);
+
+    Assert.assertEquals(route_2, routingTable.getLastAddedRoute(routeId));
+  }
+
+  @Test
+  public void removeRoute_WhenMultipleRoutesExist_RemovesGivenRoute() throws Exception{
+    Assert.assertNotNull(routingTable);
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    String route_1 = "10.0.0.1:5555";
+    String route_2 = "10.0.0.2:5555";
+
+    routingTable.addRoute(routeId, route_1);
+    routingTable.addRoute(routeId, route_2);
+
+    Assert.assertEquals(2, routingTable.getRoutes(routeId).size());
+
+    routingTable.removeRoute(routeId, route_1);
+    Assert.assertEquals(1, routingTable.getRoutes(routeId).size());
 
   }
 
+  @Test
+  public void removeRoute_WhenOnlyOneRouteExists_RemovesRouteId() throws Exception{
+    Assert.assertNotNull(routingTable);
+    RpcRouter.RouteIdentifier routeId = getRouteIdentifier();
+    String route_1 = "10.0.0.1:5555";
+
+    routingTable.addRoute(routeId, route_1);
+    Assert.assertEquals(1, routingTable.getRoutes(routeId).size());
+
+    routingTable.removeRoute(routeId, route_1);
+    ConcurrentMap cache = routingTable.getRpcCache();
+    Assert.assertFalse(cache.containsKey(routeId));
+
+  }
+
+  /*
+   * Private helper methods
+   */
   private void createRoutingTableCache() throws Exception {
 
     //here init
     Component c = mock(Component.class);
 
-    when(ics.existCache(
+    when(clusterService.existCache(
         RoutingTableImpl.GLOBALRPC_CACHE)).thenReturn(false);
 
-    when(ics.createCache(RoutingTableImpl.GLOBALRPC_CACHE,
+    when(clusterService.createCache(RoutingTableImpl.GLOBALRPC_CACHE,
         EnumSet.of(IClusterServices.cacheMode.TRANSACTIONAL))).
         thenReturn(mockGlobalRpcCache);
 
-    when(ics.existCache(
+    when(clusterService.existCache(
         RoutingTableImpl.RPC_CACHE)).thenReturn(false);
 
-    when(ics.createCache(RoutingTableImpl.RPC_CACHE,
+    when(clusterService.createCache(RoutingTableImpl.RPC_CACHE,
         EnumSet.of(IClusterServices.cacheMode.TRANSACTIONAL))).
         thenReturn(mockRpcCache);
 
-    rti.setClusterGlobalServices(this.ics);
-    rti.init(c);
+    doNothing().when(clusterService).tbegin();
+    doNothing().when(clusterService).tcommit();
 
-    Assert.assertEquals(mockGlobalRpcCache, rti.getGlobalRpcCache());
-    Assert.assertEquals(mockRpcCache, rti.getRpcCache());
+    routingTable.setClusterGlobalServices(this.clusterService);
+    routingTable.init(c);
+
+    Assert.assertEquals(mockGlobalRpcCache, routingTable.getGlobalRpcCache());
+    Assert.assertEquals(mockRpcCache, routingTable.getRpcCache());
   }
 
+  private RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> getRouteIdentifier(){
+    RpcRouter.RouteIdentifier<QName, QName, InstanceIdentifier> routeIdentifier = mock(RpcRouter.RouteIdentifier.class);
+    InstanceIdentifier identifier = mock(InstanceIdentifier.class);
+    when(routeIdentifier.getType()).thenReturn(QNAME);
+    when(routeIdentifier.getRoute()).thenReturn(identifier);
 
-  @Test
-  public void testCreateRoutingTableCacheReturnExistingCache() throws Exception {
-    //ConcurrentMap concurrentMap = createRoutingTableCache();
-
-    //OK here we should try creating again the cache but this time it should return the existing one
-    when(ics.existCache(
-        RoutingTableImpl.GLOBALRPC_CACHE)).thenReturn(true);
-
-    when(ics.getCache(
-        RoutingTableImpl.GLOBALRPC_CACHE)).thenReturn(mockGlobalRpcCache);
-
-
-    //here init
-    Component c = mock(Component.class);
-
-    rti.init(c);
-
-    Assert.assertEquals(mockGlobalRpcCache, rti.getGlobalRpcCache());
-
-
+    return routeIdentifier;
   }
 
-  private class RouteChangeListenerImpl<I, R> implements RouteChangeListener<I, R> {
-
-    @Override
-    public void onRouteUpdated(I key, R new_value) {
-      //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void onRouteDeleted(I key) {
-      //To change body of implemented methods use File | Settings | File Templates.
-    }
+  private Runnable addRoutes(final int numRoutes, final String routePrefix, final RpcRouter.RouteIdentifier routeId){
+    return new Runnable() {
+      @Override
+      public void run() {
+        for (int i=0;i<numRoutes;i++){
+          String route = routePrefix + i;
+          try {
+            routingTable.addRoute(routeId, route);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    };
   }
-
 }
