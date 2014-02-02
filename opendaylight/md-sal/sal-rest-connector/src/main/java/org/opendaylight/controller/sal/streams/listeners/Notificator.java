944af0435d5827cc4b61f69e925b63d966935a42
Martin Sunal <msunal@cisco.com>
2014-02-03 00:26:58 +0100

    Added resource /streams/stream/<streamName>
    - added netty web socket server - port 8181
    
    Subscriber can be registered for notifications in this flow:
    1. Subscriber invokes RPC create-data-change-event-subscription with input element regarding to module sal-remote revision "2014-01-14"
    2. Subscriber gets sream name as a response from server
    3. Subscriber calls GET operation on /streams/stream/<streamName>
    4. Subscriber gets response with status code 200 OK and header field Location contains URI of web socket server
    5. Subscriber creates web socket http connection to obtained URI in step 4.
    6. Subscriber can be unregistered from receiving notifications by sending CloseWebSocketFrame with reasonText containing stream name
    
    Change-Id: I92925f5a50b2ceadf2cc8f0eaee2c99dd6a63d8d
    Signed-off-by: Martin Sunal <msunal@cisco.com>

---
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/streams/listeners/Notificator.java
@@ -0,0 +1,91 @@
+package org.opendaylight.controller.sal.streams.listeners;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+
+public class Notificator {
+
+    private static Map<String, ListenerAdapter> listenersByStreamName = new ConcurrentHashMap<>();
+    private static Map<InstanceIdentifier, ListenerAdapter> listenersByInstanceIdentifier = new ConcurrentHashMap<>();
+    private static final Lock lock = new ReentrantLock();
+
+    private Notificator() {
+    }
+
+    public static ListenerAdapter getListenerFor(String streamName) {
+        return listenersByStreamName.get(streamName);
+    }
+
+    public static ListenerAdapter getListenerFor(InstanceIdentifier path) {
+        return listenersByInstanceIdentifier.get(path);
+    }
+
+    public static boolean existListenerFor(InstanceIdentifier path) {
+        return listenersByInstanceIdentifier.containsKey(path);
+    }
+
+    public static ListenerAdapter createListener(InstanceIdentifier path, String streamName) {
+        ListenerAdapter listener = new ListenerAdapter(path, streamName);
+        lock.lock();
+        listenersByInstanceIdentifier.put(path, listener);
+        listenersByStreamName.put(streamName, listener);
+        lock.unlock();
+        return listener;
+    }
+
+    public static void removeListener(InstanceIdentifier path) {
+        ListenerAdapter listener = listenersByInstanceIdentifier.get(path);
+        deleteListener(listener);
+    }
+
+    public static String createStreamNameFromUri(String uri) {
+        if (uri == null) {
+            return null;
+        }
+        String result = uri;
+        if (result.startsWith("/")) {
+            result = result.substring(1);
+        }
+        if (result.endsWith("/")) {
+            result = result.substring(0, result.length());
+        }
+        return result;
+    }
+    
+    public static void removeAllListeners() {
+        for (ListenerAdapter listener : listenersByInstanceIdentifier.values()) {
+            try {
+                listener.close();
+            } catch (Exception e) {
+            }
+        }
+        lock.lock();
+        listenersByStreamName = new ConcurrentHashMap<>();
+        listenersByInstanceIdentifier = new ConcurrentHashMap<>();
+        lock.unlock();
+    }
+    
+    public static void removeListenerIfNoSubscriberExists(ListenerAdapter listener) {
+        if (!listener.hasSubscribers()) {
+            deleteListener(listener);
+        }
+    }
+    
+    private static void deleteListener(ListenerAdapter listener) {
+        if (listener != null) {
+            try {
+                listener.close();
+            } catch (Exception e) {
+            }
+            lock.lock();
+            listenersByInstanceIdentifier.remove(listener.getPath());
+            listenersByStreamName.remove(listener).getStreamName();
+            lock.unlock();
+        }
+    }
+
+}
