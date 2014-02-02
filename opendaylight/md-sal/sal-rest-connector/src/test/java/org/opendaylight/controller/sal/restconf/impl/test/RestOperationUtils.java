301d60623f2cffd3b6cded1f18f2589e82912945
Martin Sunal <msunal@cisco.com>
2014-02-03 00:26:58 +0100

    Added resource /streams/stream/<streamName>
    - added netty web socket server - port 8181
    
    Subscriber can be registered for notifications in this flow:
    1. Subscriber invokes RPC create-data-change-event-subscription from module sal-remote revision "2014-01-14"
    2. Subscriber gets sream name as a response from server
    3. Subscriber calls GET operation on /streams/stream/<streamName>
    4. Subscriber gets response with status code 200 OK and header field Location contains URI of web socket server
    5. Subscriber creates web socket http connection to obtained URI in step 4.
    6. Subscriber can be unregistered from receiving notifications by sending CloseWebSocketFrame
    
    Change-Id: I92925f5a50b2ceadf2cc8f0eaee2c99dd6a63d8d
    Signed-off-by: Martin Sunal <msunal@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestOperationUtils.java
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestOperationUtils.java
@@ -15,13 +15,13 @@ import com.google.common.base.Charsets;
 
 public class RestOperationUtils {
 
-    static final String JSON = "+json";
-    static final String XML = "+xml";
+    public static final String JSON = "+json";
+    public static final String XML = "+xml";
 
     private RestOperationUtils() {
     }
 
-    static String createUri(String prefix, String encodedPart) throws UnsupportedEncodingException {
+    public static String createUri(String prefix, String encodedPart) throws UnsupportedEncodingException {
         return URI.create(prefix + URLEncoder.encode(encodedPart, Charsets.US_ASCII.name()).toString()).toASCIIString();
     }
 }
