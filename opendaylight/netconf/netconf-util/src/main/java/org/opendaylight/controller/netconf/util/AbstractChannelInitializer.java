beef9d3b4cadc6dc8890cf5ed1aa609716e1307b
Robert Varga <rovarga@cisco.com>
2014-02-07 22:08:39 +0100

    Get rid of using ProtocolHandlerFactory, part 2
    Now that we do not rely on passing it around, we can safely keep
    NetconfHandlerFactory wired directly to the
    ProtocolMessageEncoder/Decoder.
    
    Change-Id: I47a9b76f8d9bbb98304e06960e5a85d2730994f7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/AbstractChannelInitializer.java
+++ opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/AbstractChannelInitializer.java
@@ -10,17 +10,17 @@ package org.opendaylight.controller.netconf.util;
 
 import io.netty.channel.socket.SocketChannel;
 import io.netty.util.concurrent.Promise;
+
 import org.opendaylight.controller.netconf.api.NetconfSession;
 import org.opendaylight.controller.netconf.util.handler.FramingMechanismHandlerFactory;
 import org.opendaylight.controller.netconf.util.handler.NetconfHandlerFactory;
 import org.opendaylight.controller.netconf.util.handler.NetconfMessageAggregator;
 import org.opendaylight.controller.netconf.util.messages.FramingMechanism;
-import org.opendaylight.controller.netconf.util.messages.NetconfMessageFactory;
 
 public abstract class AbstractChannelInitializer {
 
     public void initialize(SocketChannel ch, Promise<? extends NetconfSession> promise){
-        NetconfHandlerFactory handlerFactory = new NetconfHandlerFactory(new NetconfMessageFactory());
+        NetconfHandlerFactory handlerFactory = new NetconfHandlerFactory();
         ch.pipeline().addLast("aggregator", new NetconfMessageAggregator(FramingMechanism.EOM));
         ch.pipeline().addLast(handlerFactory.getDecoders());
         initializeAfterDecoder(ch, promise);
