ad0e6133a6acedb1269506824de2379d7a8d037d
Robert Varga <rovarga@cisco.com>
2014-02-08 04:06:48 +0100

    Add EXI codecs without wiring them
    Change-Id: Ic708ef26d92e138f8269a49a2759f01d88085bf1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/handler/NetconfEXICodec.java
@@ -0,0 +1,63 @@
+package org.opendaylight.controller.netconf.util.handler;
+
+import org.openexi.proc.HeaderOptionsOutputType;
+import org.openexi.proc.common.AlignmentType;
+import org.openexi.proc.common.EXIOptions;
+import org.openexi.proc.common.EXIOptionsException;
+import org.openexi.proc.common.GrammarOptions;
+import org.openexi.proc.grammars.GrammarCache;
+import org.openexi.sax.EXIReader;
+import org.openexi.sax.Transmogrifier;
+
+import com.google.common.base.Preconditions;
+
+final class NetconfEXICodec {
+	/**
+	 * NETCONF is XML environment, so the use of EXI cookie is not really needed. Adding it
+	 * decreases efficiency of encoding by adding human-readable 4 bytes "EXI$" to the head
+	 * of the stream. This is really useful, so let's output it now.
+	 */
+	private static final boolean OUTPUT_EXI_COOKIE = true;
+	private final AlignmentType alignmentType;
+	private final EXIOptions exiOptions;
+
+	public NetconfEXICodec(final AlignmentType alignmentType, final EXIOptions exiOptions) {
+		this.alignmentType = Preconditions.checkNotNull(alignmentType);
+		this.exiOptions = Preconditions.checkNotNull(exiOptions);
+	}
+
+	private GrammarCache getGrammarCache() {
+		short go = GrammarOptions.DEFAULT_OPTIONS;
+		if (exiOptions.getPreserveComments()) {
+			go = GrammarOptions.addCM(go);
+		}
+		if (exiOptions.getPreserveDTD()) {
+			go = GrammarOptions.addDTD(go);
+		}
+		if (exiOptions.getPreserveNS()) {
+			go = GrammarOptions.addNS(go);
+		}
+		if (exiOptions.getPreservePIs()) {
+			go = GrammarOptions.addPI(go);
+		}
+
+		return new GrammarCache(null, go);
+	}
+
+	EXIReader getReader() throws EXIOptionsException {
+		final EXIReader r = new EXIReader();
+		r.setPreserveLexicalValues(exiOptions.getPreserveLexicalValues());
+		r.setGrammarCache(getGrammarCache());
+		return r;
+	}
+
+	Transmogrifier getTransmogrifier() throws EXIOptionsException {
+		final Transmogrifier transmogrifier = new Transmogrifier();
+		transmogrifier.setAlignmentType(alignmentType);
+		transmogrifier.setBlockSize(exiOptions.getBlockSize());
+		transmogrifier.setGrammarCache(getGrammarCache());
+		transmogrifier.setOutputCookie(OUTPUT_EXI_COOKIE);
+		transmogrifier.setOutputOptions(HeaderOptionsOutputType.all);
+		return transmogrifier;
+	}
+}
---
+++ opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/handler/NetconfEXIToMessageDecoder.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.netconf.util.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.io.InputStream;
+import java.util.List;
+
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.opendaylight.controller.netconf.api.NetconfMessage;
+import org.openexi.sax.EXIReader;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import com.google.common.base.Preconditions;
+
+public final class NetconfEXIToMessageDecoder extends ByteToMessageDecoder {
+	private static final SAXTransformerFactory saxTransformerFactory = (SAXTransformerFactory)SAXTransformerFactory.newInstance();
+
+	// FIXME: is this needed?
+	//	private static final SAXParserFactory saxParserFactory;
+	//	static {
+	//		saxParserFactory = SAXParserFactory.newInstance();
+	//		saxParserFactory.setNamespaceAware(true);
+	//	}
+
+	private final NetconfEXICodec codec;
+
+	public NetconfEXIToMessageDecoder(final NetconfEXICodec codec) {
+		this.codec = Preconditions.checkNotNull(codec);
+	}
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+		/*
+		 * Note that we could loop here and process all the messages, but we can't do that.
+		 * The reason is <stop-exi> operation, which has the contract of immediately stopping
+		 * the use of EXI, which means the next message needs to be decoded not by us, but rather
+		 * by the XML decoder.
+		 */
+		final DOMResult result = new DOMResult();
+		final EXIReader r = codec.getReader();
+
+		final TransformerHandler transformerHandler = saxTransformerFactory.newTransformerHandler();
+		transformerHandler.setResult(result);
+		r.setContentHandler(transformerHandler);
+
+		try (final InputStream is = new ByteBufInputStream(in)) {
+			r.parse(new InputSource(is));
+		}
+		out.add(new NetconfMessage((Document) result.getNode()));
+	}
+}
