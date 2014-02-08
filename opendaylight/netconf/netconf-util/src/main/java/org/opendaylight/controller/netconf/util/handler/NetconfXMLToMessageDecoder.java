ed3a457eb149f7d6993760dc63a088ea281368f2
Robert Varga <rovarga@cisco.com>
2014-02-08 03:45:52 +0100

    Rewrite POSSIBLE_ENDS and POSSIBLE_STARTS
    These two are really simple byte arrays, initialized them as such
    withouth going through strings/UTF8.
    
    Change-Id: Id80151d9ff57df4b7a5595f76f30a379d89af3dd
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/handler/NetconfXMLToMessageDecoder.java
+++ opendaylight/netconf/netconf-util/src/main/java/org/opendaylight/controller/netconf/util/handler/NetconfXMLToMessageDecoder.java
@@ -31,113 +31,119 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
 
 public final class NetconfXMLToMessageDecoder extends ByteToMessageDecoder {
-    private static final Logger LOG = LoggerFactory.getLogger(NetconfXMLToMessageDecoder.class);
+	private static final Logger LOG = LoggerFactory.getLogger(NetconfXMLToMessageDecoder.class);
 
-    // FIXME: this is funky way of creating arrays
-    private static final List<byte[]> POSSIBLE_ENDS = ImmutableList.of(
-            "]\n".getBytes(Charsets.UTF_8), "]\r\n".getBytes(Charsets.UTF_8));
-    private static final List<byte[]> POSSIBLE_STARTS = ImmutableList.of(
-            "[".getBytes(Charsets.UTF_8), "\r\n[".getBytes(Charsets.UTF_8), "\n[".getBytes(Charsets.UTF_8));
+	private static final List<byte[]> POSSIBLE_ENDS = ImmutableList.of(
+			new byte[] { ']', '\n' },
+			new byte[] { ']', '\r', '\n' });
+	private static final List<byte[]> POSSIBLE_STARTS = ImmutableList.of(
+			new byte[] { '[' },
+			new byte[] { '\r', '\n', '[' },
+			new byte[] { '\n', '[' });
 
-    @Override
-    @VisibleForTesting
-    public void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        if (in.readableBytes() == 0) {
-            LOG.debug("No more content in incoming buffer.");
-            return;
-        }
+	@Override
+	@VisibleForTesting
+	public void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+		if (in.readableBytes() == 0) {
+			LOG.debug("No more content in incoming buffer.");
+			return;
+		}
 
-        in.markReaderIndex();
-        try {
-            LOG.trace("Received to decode: {}", ByteBufUtil.hexDump(in));
-            byte[] bytes = new byte[in.readableBytes()];
-            in.readBytes(bytes);
+		in.markReaderIndex();
+		try {
+			LOG.trace("Received to decode: {}", ByteBufUtil.hexDump(in));
+			byte[] bytes = new byte[in.readableBytes()];
+			in.readBytes(bytes);
 
-            logMessage(bytes);
+			logMessage(bytes);
 
-            String additionalHeader = null;
+			String additionalHeader = null;
 
-            if (startsWithAdditionalHeader(bytes)) {
-                // Auth information containing username, ip address... extracted for monitoring
-                int endOfAuthHeader = getAdditionalHeaderEndIndex(bytes);
-                if (endOfAuthHeader > -1) {
-                    byte[] additionalHeaderBytes = Arrays.copyOfRange(bytes, 0, endOfAuthHeader + 2);
-                    additionalHeader = additionalHeaderToString(additionalHeaderBytes);
-                    bytes = Arrays.copyOfRange(bytes, endOfAuthHeader + 2, bytes.length);
-                }
-            }
-            NetconfMessage message;
-            try {
-                Document doc = XmlUtil.readXmlToDocument(new ByteArrayInputStream(bytes));
-                message = new NetconfMessage(doc, additionalHeader);
-            } catch (final SAXException | IOException | IllegalStateException e) {
-                throw new NetconfDeserializerException("Could not parse message from " + new String(bytes), e);
-            }
+			// FIXME: this has to be moved into the negotiator and explained as to what the heck
+			// is going on. This is definitely not specified in NETCONF and has no place here. It
+			// requires reading all data and incurs inefficiency by being unable to pipe the ByteBuf
+			// directly into the XML decoder.
+			if (startsWithAdditionalHeader(bytes)) {
+				// Auth information containing username, ip address... extracted for monitoring
+				int endOfAuthHeader = getAdditionalHeaderEndIndex(bytes);
+				if (endOfAuthHeader > -1) {
+					byte[] additionalHeaderBytes = Arrays.copyOfRange(bytes, 0, endOfAuthHeader + 2);
+					additionalHeader = additionalHeaderToString(additionalHeaderBytes);
+					bytes = Arrays.copyOfRange(bytes, endOfAuthHeader + 2, bytes.length);
+				}
+			}
+			NetconfMessage message;
+			try {
+				Document doc = XmlUtil.readXmlToDocument(new ByteArrayInputStream(bytes));
+				message = new NetconfMessage(doc, additionalHeader);
+			} catch (final SAXException | IOException | IllegalStateException e) {
+				throw new NetconfDeserializerException("Could not parse message from " + new String(bytes), e);
+			}
 
-            out.add(message);
-        } finally {
-            in.discardReadBytes();
-        }
-    }
+			out.add(message);
+		} finally {
+			in.discardReadBytes();
+		}
+	}
 
-    private int getAdditionalHeaderEndIndex(byte[] bytes) {
-        for (byte[] possibleEnd : POSSIBLE_ENDS) {
-            int idx = findByteSequence(bytes, possibleEnd);
+	private int getAdditionalHeaderEndIndex(byte[] bytes) {
+		for (byte[] possibleEnd : POSSIBLE_ENDS) {
+			int idx = findByteSequence(bytes, possibleEnd);
 
-            if (idx != -1) {
-                return idx;
-            }
-        }
+			if (idx != -1) {
+				return idx;
+			}
+		}
 
-        return -1;
-    }
+		return -1;
+	}
 
-    private static int findByteSequence(final byte[] bytes, final byte[] sequence) {
-        if (bytes.length < sequence.length) {
-            throw new IllegalArgumentException("Sequence to be found is longer than the given byte array.");
-        }
-        if (bytes.length == sequence.length) {
-            if (Arrays.equals(bytes, sequence)) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
-        int j = 0;
-        for (int i = 0; i < bytes.length; i++) {
-            if (bytes[i] == sequence[j]) {
-                j++;
-                if (j == sequence.length) {
-                    return i - j + 1;
-                }
-            } else {
-                j = 0;
-            }
-        }
-        return -1;
-    }
+	private static int findByteSequence(final byte[] bytes, final byte[] sequence) {
+		if (bytes.length < sequence.length) {
+			throw new IllegalArgumentException("Sequence to be found is longer than the given byte array.");
+		}
+		if (bytes.length == sequence.length) {
+			if (Arrays.equals(bytes, sequence)) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+		int j = 0;
+		for (int i = 0; i < bytes.length; i++) {
+			if (bytes[i] == sequence[j]) {
+				j++;
+				if (j == sequence.length) {
+					return i - j + 1;
+				}
+			} else {
+				j = 0;
+			}
+		}
+		return -1;
+	}
 
-    private boolean startsWithAdditionalHeader(byte[] bytes) {
-        for (byte[] possibleStart : POSSIBLE_STARTS) {
-            int i = 0;
-            for (byte b : possibleStart) {
-                if(bytes[i] != b)
-                    break;
+	private boolean startsWithAdditionalHeader(byte[] bytes) {
+		for (byte[] possibleStart : POSSIBLE_STARTS) {
+			int i = 0;
+			for (byte b : possibleStart) {
+				if(bytes[i] != b)
+					break;
 
-                return true;
-            }
-        }
+				return true;
+			}
+		}
 
-        return false;
-    };
+		return false;
+	};
 
-    private void logMessage(byte[] bytes) {
-        String s = Charsets.UTF_8.decode(ByteBuffer.wrap(bytes)).toString();
-        LOG.debug("Parsing message \n{}", s);
-    }
+	private void logMessage(byte[] bytes) {
+		String s = Charsets.UTF_8.decode(ByteBuffer.wrap(bytes)).toString();
+		LOG.debug("Parsing message \n{}", s);
+	}
 
-    private String additionalHeaderToString(byte[] bytes) {
-        return Charsets.UTF_8.decode(ByteBuffer.wrap(bytes)).toString();
-    }
+	private String additionalHeaderToString(byte[] bytes) {
+		return Charsets.UTF_8.decode(ByteBuffer.wrap(bytes)).toString();
+	}
 
 }
