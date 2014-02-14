84c506752f2041f6133bd4514d87321fb9a311fe
Robert Varga <rovarga@cisco.com>
2014-02-14 20:24:05 +0100

    Move statistics request functions into trackers
    This moves the gory details of specific requests into their respective
    trackers. It also makes the request pipeline asynchronous.
    
    Change-Id: I56ad175bc75b1af18263ae294cb3ffc118557abd
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/statistics-manager/src/main/java/org/opendaylight/controller/md/statistics/manager/GroupDescStatsTracker.java
+++ opendaylight/md-sal/statistics-manager/src/main/java/org/opendaylight/controller/md/statistics/manager/GroupDescStatsTracker.java
@@ -8,21 +8,28 @@
 package org.opendaylight.controller.md.statistics.manager;
 
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
-import org.opendaylight.controller.sal.binding.api.data.DataProviderService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.transaction.rev131103.TransactionId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.GetGroupDescriptionInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.NodeGroupDescStats;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.NodeGroupDescStatsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.OpendaylightGroupStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.group.desc.GroupDescBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.group.desc.stats.reply.GroupDescStats;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.Group;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.GroupBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.GroupKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.ListenableFuture;
+
 final class GroupDescStatsTracker extends AbstractStatsTracker<GroupDescStats, GroupDescStats> {
-    public GroupDescStatsTracker(final InstanceIdentifier<Node> targetNodeIdentifier, final DataProviderService dps, final long lifetimeNanos) {
-        super(targetNodeIdentifier, dps, lifetimeNanos);
+    private final OpendaylightGroupStatisticsService groupStatsService;
+
+    public GroupDescStatsTracker(OpendaylightGroupStatisticsService groupStatsService, final FlowCapableContext context, final long lifetimeNanos) {
+        super(context, lifetimeNanos);
+        this.groupStatsService = Preconditions.checkNotNull(groupStatsService);
     }
 
     @Override
@@ -49,5 +56,12 @@ final class GroupDescStatsTracker extends AbstractStatsTracker<GroupDescStats, G
         InstanceIdentifier<NodeGroupDescStats> groupRef = getNodeIdentifierBuilder().augmentation(FlowCapableNode.class)
                 .child(Group.class, new GroupKey(item.getGroupId())).augmentation(NodeGroupDescStats.class).build();
         trans.removeOperationalData(groupRef);
+    }
+
+    public ListenableFuture<TransactionId> request() {
+        final GetGroupDescriptionInputBuilder input = new GetGroupDescriptionInputBuilder();
+        input.setNode(getNodeRef());
+
+        return requestHelper(groupStatsService.getGroupDescription(input.build()));
     }
 }
