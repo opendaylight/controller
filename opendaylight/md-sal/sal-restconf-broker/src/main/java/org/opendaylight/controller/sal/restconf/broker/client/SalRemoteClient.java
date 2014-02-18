276d910d8e50783917bbfc19fb57b58b6aaebf31
Milos Fabian <milfabia@cisco.com>
2014-02-18 10:44:24 +0100

    SalRemoteClientDeployer - instantiate SalRemoteClient for given URL.
    SalRemoteClient:
    -Creates schema context.
    -Initialize mapping service
    -Creates restconf client
    -Starts sal remote service broker
    -Provides consumer registaration
    
    Change-Id: Idc0d8be4c088bac8a4df24b98e482184a6e54665
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ opendaylight/md-sal/sal-restconf-broker/src/main/java/org/opendaylight/controller/sal/restconf/broker/client/SalRemoteClient.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.restconf.broker.client;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ConsumerContext;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+
+public interface SalRemoteClient extends AutoCloseable {
+
+    ConsumerContext registerConsumer();
+
+    /**
+     * Temporary work-around method
+     * 
+     * @param object
+     */
+    <T extends Augmentation<?>> void getCodecForAugmentation(Class<T> object);
+
+}
