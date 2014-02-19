8a0b9196258ea036d25f3006ac1b7ffd960cfcc7
Robert Varga <rovarga@cisco.com>
2014-02-19 11:44:06 +0100

    Prune long-deprecated APIs
    These APIs are from prototype days and have never properly worked. Prune
    them to reduce the baggage we carry around.
    
    Change-Id: I9ac89552edf76f0740c660c100ec0426627b413a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/data/DataBrokerService.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/data/DataBrokerService.java
@@ -7,24 +7,19 @@
  */
 package org.opendaylight.controller.sal.binding.api.data;
 
-import java.util.concurrent.Future;
-
 import org.opendaylight.controller.md.sal.common.api.data.DataChangePublisher;
 import org.opendaylight.controller.md.sal.common.api.data.DataModificationTransactionFactory;
 import org.opendaylight.controller.md.sal.common.api.data.DataReader;
 import org.opendaylight.controller.sal.binding.api.BindingAwareService;
-import org.opendaylight.controller.sal.common.DataStoreIdentifier;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
-import org.opendaylight.yangtools.yang.binding.DataRoot;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 
 /**
  * DataBrokerService provides unified access to the data stores available in the
  * system.
- * 
- * 
+ *
+ *
  * @see DataProviderService
  */
 public interface DataBrokerService extends //
@@ -32,163 +27,37 @@ public interface DataBrokerService extends //
         DataModificationTransactionFactory<InstanceIdentifier<? extends DataObject>, DataObject>, //
         DataReader<InstanceIdentifier<? extends DataObject>, DataObject>, //
         DataChangePublisher<InstanceIdentifier<? extends DataObject>, DataObject, DataChangeListener> {
-
-    /**
-     * Returns a data from specified Data Store.
-     * 
-     * Returns all the data visible to the consumer from specified Data Store.
-     * 
-     * @param <T>
-     *            Interface generated from YANG module representing root of data
-     * @param store
-     *            Identifier of the store, from which will be data retrieved
-     * @return data visible to the consumer
-     */
-    @Deprecated
-    <T extends DataRoot> T getData(DataStoreIdentifier store, Class<T> rootType);
-
-    /**
-     * Returns a filtered subset of data from specified Data Store.
-     * 
-     * <p>
-     * The filter is modeled as an hierarchy of Java TOs starting with
-     * implementation of {@link DataRoot} representing data root. The semantics
-     * of the filter tree is the same as filter semantics defined in the NETCONF
-     * protocol for rpc operations <code>get</code> and <code>get-config</code>
-     * in Section 6 of RFC6241.
-     * 
-     * 
-     * @see http://tools.ietf.org/html/rfc6241#section-6
-     * @param <T>
-     *            Interface generated from YANG module representing root of data
-     * @param store
-     *            Identifier of the store, from which will be data retrieved
-     * @param filter
-     *            Data tree filter similar to the NETCONF filter
-     * @return
-     */
-    @Deprecated
-    <T extends DataRoot> T getData(DataStoreIdentifier store, T filter);
-
-    /**
-     * Returns a candidate data which are not yet commited.
-     * 
-     * 
-     * @param <T>
-     *            Interface generated from YANG module representing root of data
-     * @param store
-     *            Identifier of the store, from which will be data retrieved
-     * @return
-     */
-    @Deprecated
-    <T extends DataRoot> T getCandidateData(DataStoreIdentifier store, Class<T> rootType);
-
-    /**
-     * Returns a filtered subset of candidate data from specified Data Store.
-     * 
-     * <p>
-     * The filter is modeled as an hierarchy of {@link Node} starting with
-     * {@link CompositeNode} representing data root. The semantics of the filter
-     * tree is the same as filter semantics defined in the NETCONF protocol for
-     * rpc operations <code>get</code> and <code>get-config</code> in Section 6
-     * of RFC6241.
-     * 
-     * 
-     * @see http://tools.ietf.org/html/rfc6241#section-6
-     * @param <T>
-     *            Interface generated from YANG module representing root of data
-     * @param store
-     *            Identifier of the store, from which will be data retrieved
-     * @param filter
-     *            A filter data root
-     * @return
-     */
-    @Deprecated
-    <T extends DataRoot> T getCandidateData(DataStoreIdentifier store, T filter);
-
-    /**
-     * 
-     * @param <T>
-     *            Interface generated from YANG module representing root of data
-     * @param store
-     *            Identifier of the store, in which will be the candidate data
-     *            modified
-     * @param changeSet
-     *            Modification of data tree.
-     * @return Result object containing the modified data tree if the operation
-     *         was successful, otherwise list of the encountered errors.
-     */
-    @Deprecated
-    RpcResult<DataRoot> editCandidateData(DataStoreIdentifier store, DataRoot changeSet);
-
-    /**
-     * Initiates a two-phase commit of candidate data.
-     * 
-     * <p>
-     * The {@link Consumer} could initiate a commit of candidate data
-     * 
-     * <p>
-     * The successful commit changes the state of the system and may affect
-     * several components.
-     * 
-     * <p>
-     * The effects of successful commit of data are described in the
-     * specifications and YANG models describing the {@link Provider} components
-     * of controller. It is assumed that {@link Consumer} has an understanding
-     * of this changes.
-     * 
-     * 
-     * @see DataCommitHandler for further information how two-phase commit is
-     *      processed.
-     * @param store
-     *            Identifier of the store, where commit should occur.
-     * @return Result of the commit, containing success information or list of
-     *         encountered errors, if commit was not successful.
-     */
-    @Deprecated
-    Future<RpcResult<Void>> commit(DataStoreIdentifier store);
-
-    @Deprecated
-    DataObject getData(InstanceIdentifier<? extends DataObject> data);
-
-    @Deprecated
-    DataObject getConfigurationData(InstanceIdentifier<?> data);
     /**
      * Creates a data modification transaction.
-     * 
+     *
      * @return new blank data modification transaction.
      */
-    DataModificationTransaction beginTransaction();
-
-    @Deprecated
-    public void registerChangeListener(InstanceIdentifier<? extends DataObject> path, DataChangeListener changeListener);
-
-    @Deprecated
-    public void unregisterChangeListener(InstanceIdentifier<? extends DataObject> path, DataChangeListener changeListener);
+    @Override
+	DataModificationTransaction beginTransaction();
 
     /**
-     * Reads data subtree from configurational store. 
-     * (Store which is populated by consumer, which is usually used to 
+     * Reads data subtree from configurational store.
+     * (Store which is populated by consumer, which is usually used to
      * inject state into providers. E.g. Flow configuration)-
-     * 
+     *
      */
     @Override
     public DataObject readConfigurationData(InstanceIdentifier<? extends DataObject> path);
-    
+
     /**
-     * Reads data subtree from operational store. 
-     * (Store which is populated by providers, which is usually used to 
+     * Reads data subtree from operational store.
+     * (Store which is populated by providers, which is usually used to
      * capture state of providers. E.g. Topology)
-     * 
+     *
      */
     @Override
     public DataObject readOperationalData(InstanceIdentifier<? extends DataObject> path);
-    
+
     /**
-     * Register a data change listener for particular subtree. 
-     * 
+     * Register a data change listener for particular subtree.
+     *
      * Callback is invoked each time data in subtree changes.
-     * 
+     *
      */
     @Override
     public ListenerRegistration<DataChangeListener> registerDataChangeListener(
--- opendaylight/md-sal/sal-restconf-broker/src/main/java/org/opendaylight/controller/sal/restconf/binding/impl/DataBrokerServiceImpl.java
+++ opendaylight/md-sal/sal-restconf-broker/src/main/java/org/opendaylight/controller/sal/restconf/binding/impl/DataBrokerServiceImpl.java
@@ -8,19 +8,16 @@
 package org.opendaylight.controller.sal.restconf.binding.impl;
 
 import java.net.URL;
-import java.util.concurrent.Future;
+
 import org.opendaylight.controller.sal.binding.api.data.DataBrokerService;
 import org.opendaylight.controller.sal.binding.api.data.DataChangeListener;
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
-import org.opendaylight.controller.sal.common.DataStoreIdentifier;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.restconf.client.api.RestconfClientContext;
 import org.opendaylight.yangtools.restconf.client.api.RestconfClientContextFactory;
 import org.opendaylight.yangtools.restconf.client.api.UnsupportedProtocolException;
 import org.opendaylight.yangtools.yang.binding.DataObject;
-import org.opendaylight.yangtools.yang.binding.DataRoot;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.data.impl.codec.BindingIndependentMappingService;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextHolder;
 import org.slf4j.Logger;
@@ -35,60 +32,11 @@ public class DataBrokerServiceImpl implements DataBrokerService {
     public DataBrokerServiceImpl(URL baseUrl, BindingIndependentMappingService mappingService, SchemaContextHolder schemaContextHolder) throws UnsupportedProtocolException {
         this.restconfClientContext = restconfClientContextFactory.getRestconfClientContext(baseUrl, mappingService, schemaContextHolder);
     }
-    @Override
-    public <T extends DataRoot> T getData(DataStoreIdentifier store, Class<T> rootType) {
-        return null;
-    }
-
-    @Override
-    public <T extends DataRoot> T getData(DataStoreIdentifier store, T filter) {
-        return null;
-    }
-
-    @Override
-    public <T extends DataRoot> T getCandidateData(DataStoreIdentifier store, Class<T> rootType) {
-        return null;
-    }
-
-    @Override
-    public <T extends DataRoot> T getCandidateData(DataStoreIdentifier store, T filter) {
-        return null;
-    }
-
-    @Override
-    public RpcResult<DataRoot> editCandidateData(DataStoreIdentifier store, DataRoot changeSet) {
-        return null;
-    }
-
-    @Override
-    public Future<RpcResult<Void>> commit(DataStoreIdentifier store) {
-        return null;
-    }
-
-    @Override
-    public DataObject getData(InstanceIdentifier<? extends DataObject> data) {
-        return null;
-    }
-
-    @Override
-    public DataObject getConfigurationData(InstanceIdentifier<?> data) {
-        return null;
-    }
 
     @Override
     public DataModificationTransaction beginTransaction() {
         //TODO implementation using sal-remote
         return null;
-    }
-
-    @Override
-    public void registerChangeListener(InstanceIdentifier<? extends DataObject> path, DataChangeListener changeListener) {
-
-    }
-
-    @Override
-    public void unregisterChangeListener(InstanceIdentifier<? extends DataObject> path, DataChangeListener changeListener) {
-
     }
 
     @Override
@@ -109,9 +57,4 @@ public class DataBrokerServiceImpl implements DataBrokerService {
         //TODO implementation using restconf-client
         return null;
     }
-
-
-
-
-
 }
