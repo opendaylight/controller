b94ff28ecfcba7a16c38a170d9abd23c6ff8595f
Robert Varga <rovarga@cisco.com>
2014-02-19 03:55:48 +0100

    Fix comments and remove use of deprecated class
    This primarily removes SchemaServiceListener, as that is deprecated and
    implied by SchemaContextListener, which is replacing it.
    
    Change-Id: I977e818ff466ab27f6cd213d1e0e255e2f1b59a4
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/sal/core/api/RpcImplementation.java
+++ opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/sal/core/api/RpcImplementation.java
@@ -16,11 +16,11 @@ import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 
 /**
- * {@link Provider}'s implementation of rpc.
- * 
- * In order to expose the rpc to other components, the provider MUST register
- * concrete implementation of this interface
- * 
+ * {@link Provider}'s implementation of an RPC.
+ *
+ * In order to expose an RPC to other components, the provider MUST register
+ * a concrete implementation of this interface.
+ *
  * The registration could be done by :
  * <ul>
  * <li>returning an instance of implementation in the return value of
@@ -29,9 +29,9 @@ import org.opendaylight.yangtools.yang.data.api.CompositeNode;
  * arguments to the
  * {@link ProviderSession#addRpcImplementation(QName, RpcImplementation)}
  * </ul>
- * 
+ *
  * The simplified process of the invocation of rpc is following:
- * 
+ *
  * <ol>
  * <li> {@link Consumer} invokes
  * {@link ConsumerSession#rpc(QName, CompositeNode)}
@@ -42,31 +42,31 @@ import org.opendaylight.yangtools.yang.data.api.CompositeNode;
  * {@link RpcResult}
  * <li> {@link Broker} returns the {@link RpcResult} to {@link Consumer}
  * </ol>
- * 
- * 
+ *
+ *
  */
 public interface RpcImplementation extends Provider.ProviderFunctionality {
 
     /**
      * A set of rpc types supported by implementation.
-     * 
+     *
      * The set of rpc {@link QName}s which are supported by this implementation.
      * This set is used, when {@link Provider} is registered to the SAL, to
      * register and expose the implementation of the returned rpcs.
-     * 
+     *
      * @return Set of QNames identifying supported RPCs
      */
     Set<QName> getSupportedRpcs();
 
     /**
      * Invokes a implementation of specified rpc.
-     * 
-     * 
+     *
+     *
      * @param rpc
      *            Rpc to be invoked
      * @param input
      *            Input data for rpc.
-     * 
+     *
      * @throws IllegalArgumentException
      *             <ul>
      *             <li>If rpc is null.
