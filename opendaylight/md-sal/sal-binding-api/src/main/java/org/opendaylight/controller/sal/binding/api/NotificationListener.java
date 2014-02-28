a25503c13d9dc46fc7064a51ed139dccb87361f4
Robert Varga <rovarga@cisco.com>
2014-02-28 13:41:49 +0100

    Add interface documentation
    Change-Id: I3573d1153a62bfe36a01504198a3d1182d1013c1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/NotificationListener.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/sal/binding/api/NotificationListener.java
@@ -11,7 +11,21 @@ import java.util.EventListener;
 
 import org.opendaylight.yangtools.yang.binding.Notification;
 
+/**
+ * Interface implemented by objects interested in some sort of Notification. This
+ * class acts as a base interface for specific listeners which usually are a type
+ * capture of this interface.
+ *
+ * @param <T> Notification type
+ */
 public interface NotificationListener<T extends Notification> extends EventListener {
-
+	/**
+	 * Invoked to deliver the notification. Note that this method may be invoked
+	 * from a shared thread pool, so implementations SHOULD NOT perform CPU-intensive
+	 * operations and they definitely MUST NOT invoke any potentially blocking
+	 * operations.
+	 *
+	 * @param notification Notification being delivered.
+	 */
     void onNotification(T notification);
 }
