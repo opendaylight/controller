c437b9d4aeb4ed43875d45ed88593f7ecc5dfc19
Robert Varga <rovarga@cisco.com>
2014-03-12 15:54:52 +0100

    Bug 500: Type capture for DOM and Binding APIs
    This commit introduces the type captures of new interfaces introduced in
    the preceding commit.
    
    Change-Id: I8da8ecd0a0404e89ae20d0d85894fc9da59e9903
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/md/sal/dom/api/DOMDataReadTransaction.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.api;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncReadTransaction;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public interface DOMDataReadTransaction extends AsyncReadTransaction<InstanceIdentifier, NormalizedNode<?, ?>> {
+
+}
