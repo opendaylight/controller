a10bb0fd710d409613c2fbfcec43361aa75b86f1
Robert Varga <rovarga@cisco.com>
2014-03-18 17:00:55 +0100

    Bug 499: Initial implementation of supporting tree structures
      - StoreTreeNode - interface defining common interface to tree
        structures used by data store.
    
      - TreeNodeUtils - utility methods which uses StoreTreeNode
          as tree definitions and provides functionality such
          as retrieval of deep nodes.
    
      - StoreMetadataNode - immutable tree node (and tree), representing
            versioning metadata and data.
    
      - NodeModification - mutable tree node (and tree),
           representing modification (diff) which should be applied to
           data tree. Tree is lazily populated by invoking client
           operations to be maded.
    
    This patchset requires https://git.opendaylight.org/gerrit/#/c/5646/
    to be merged.
    
    Change-Id: I2d2437ea64ba54565ba60826fe0e7c14f071598e
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/tree/StoreMetadataNode.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.store.impl.tree;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.Map;
+
+import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.primitives.UnsignedLong;
+
+public class StoreMetadataNode implements Immutable, Identifiable<PathArgument>, StoreTreeNode<StoreMetadataNode> {
+
+    private final UnsignedLong nodeVersion;
+    private final UnsignedLong subtreeVersion;
+    private final NormalizedNode<?, ?> data;
+
+    private final Map<PathArgument, StoreMetadataNode> children;
+
+    protected StoreMetadataNode(final NormalizedNode<?, ?> data, final UnsignedLong nodeVersion, final UnsignedLong subtreeVersion,
+            final Map<PathArgument, StoreMetadataNode> children) {
+        this.nodeVersion = nodeVersion;
+        this.subtreeVersion = subtreeVersion;
+        this.data = data;
+        this.children = ImmutableMap.copyOf(children);
+
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public UnsignedLong getNodeVersion() {
+        return this.nodeVersion;
+    }
+
+    @Override
+    public PathArgument getIdentifier() {
+        return data.getIdentifier();
+    }
+
+    public UnsignedLong getSubtreeVersion() {
+        return subtreeVersion;
+    }
+
+    public NormalizedNode<?, ?> getData() {
+        return this.data;
+    }
+
+    public Iterable<StoreMetadataNode> getChildren() {
+        return children.values();
+    }
+
+    @Override
+    public Optional<StoreMetadataNode> getChild(final PathArgument key) {
+        return Optional.fromNullable(children.get(key));
+    }
+
+    @Override
+    public String toString() {
+        return "StoreMetadataNode [identifier=" + getIdentifier() + ", nodeVersion=" + nodeVersion + "]";
+    }
+
+    public static Optional<UnsignedLong> getVersion(final Optional<StoreMetadataNode> currentMetadata) {
+        if (currentMetadata.isPresent()) {
+            return Optional.of(currentMetadata.get().getNodeVersion());
+        }
+        return Optional.absent();
+    }
+
+    public static Optional<StoreMetadataNode> getChild(final Optional<StoreMetadataNode> parent, final PathArgument child) {
+        if (parent.isPresent()) {
+            return parent.get().getChild(child);
+        }
+        return Optional.absent();
+    }
+
+    public static final StoreMetadataNode createRecursivelly(final NormalizedNode<?, ?> node, final UnsignedLong version) {
+        Builder builder = builder() //
+                .setNodeVersion(version) //
+                .setSubtreeVersion(version) //
+                .setData(node);
+        if(node instanceof NormalizedNodeContainer<?, ?, ?>) {
+
+            @SuppressWarnings("unchecked")
+            NormalizedNodeContainer<?, ?, NormalizedNode<?, ?>> nodeContainer = (NormalizedNodeContainer<?, ?, NormalizedNode<?, ?>>) node;
+            for(NormalizedNode<?, ?> subNode : nodeContainer.getValue()) {
+                builder.add(createRecursivelly(subNode, version));
+            }
+        }
+        return builder.build();
+    }
+
+    public static class Builder {
+
+        private Builder() {
+
+        }
+
+        UnsignedLong nodeVersion = UnsignedLong.valueOf(0);
+        UnsignedLong subtreeVersion = UnsignedLong.valueOf(0);
+        NormalizedNode<?, ?> data;
+
+        final ImmutableMap.Builder<PathArgument, StoreMetadataNode> children = ImmutableMap.builder();
+
+        public UnsignedLong getVersion() {
+            return nodeVersion;
+
+        }
+
+        public Builder setNodeVersion(final UnsignedLong version) {
+            this.nodeVersion = version;
+            return this;
+        }
+
+        public Builder setSubtreeVersion(final UnsignedLong version) {
+            this.subtreeVersion = version;
+            return this;
+        }
+
+        public Builder setData(final NormalizedNode<?,?> data) {
+            this.data = data;
+            return this;
+        }
+
+        public Builder add(final StoreMetadataNode node) {
+            children.put(node.getIdentifier(), node);
+            return this;
+        }
+
+        public StoreMetadataNode build() {
+            checkState(data != null,"Data node should not be null.");
+            checkState(subtreeVersion.compareTo(nodeVersion) >= 0, "Subtree version must be equals or greater than node version.");
+            return new StoreMetadataNode(data, nodeVersion, subtreeVersion, children.build());
+        }
+    }
+
+}
