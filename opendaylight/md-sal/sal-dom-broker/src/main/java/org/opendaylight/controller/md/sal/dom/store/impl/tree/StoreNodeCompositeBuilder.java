f7022ce2dbccd1ea63ff033da25542ef0172a5f8
Tony Tkacik <ttkacik@cisco.com>
2014-03-18 17:00:55 +0100

    Bug 499: Initial implementation of supporting tree structures
      - StoreTreeNode - interface defining common interface to tree
        structures used by data store.
    
      - TreeNodeUtils - utility methods which uses StoreTreeNode
          as tree definitions and provides functionality such
          as retrieval of deep nodes.
    
      - StoreMetadataNode - immutable tree node (and tree), representing
            versioning metadata and data.
    
      - NodeModification - mutable tree node (and tree),
           representing modification (diff) which should be applied to
           data tree. Tree is lazily populated by invoking client
           operations to be maded.
    
    This patchset requires https://git.opendaylight.org/gerrit/#/c/5646/
    to be merged.
    
    Change-Id: I2d2437ea64ba54565ba60826fe0e7c14f071598e
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/tree/StoreNodeCompositeBuilder.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.store.impl.tree;
+
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeContainerBuilder;
+
+/**
+ *
+ * Helper builder
+ *
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class StoreNodeCompositeBuilder {
+
+    private final StoreMetadataNode.Builder metadata;
+
+    private final NormalizedNodeContainerBuilder data;
+
+
+    private StoreNodeCompositeBuilder(final NormalizedNodeContainerBuilder nodeBuilder) {
+        this.metadata = StoreMetadataNode.builder();
+        this.data = nodeBuilder;
+    }
+
+    @SuppressWarnings("unchecked")
+    public StoreNodeCompositeBuilder add(final StoreMetadataNode node) {
+        metadata.add(node);
+        data.withChild(node.getData());
+        return this;
+    }
+
+
+    public StoreMetadataNode build() {
+        return metadata.setData(data.build()).build();
+    }
+
+
+    public static StoreNodeCompositeBuilder from(final NormalizedNodeContainerBuilder nodeBuilder) {
+        return new StoreNodeCompositeBuilder(nodeBuilder);
+    }
+
+    public static StoreNodeCompositeBuilder from(final StoreMetadataNode previous, final NormalizedNodeContainerBuilder nodeBuilder) {
+
+        return new StoreNodeCompositeBuilder(nodeBuilder);
+    }
+
+    public StoreNodeCompositeBuilder setIdentifier(final PathArgument identifier) {
+        data.withNodeIdentifier(identifier);
+        return this;
+    }
+
+}
