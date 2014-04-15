6a1658fc128f5f8dfbcc908fefa802cdde9e94a6
Vaclav Demcak <vdemcak@cisco.com>
2014-04-15 18:26:20 +0200

    Bug 740: Forwardingrules-Manager support for DataChangeListeners
    migration from xtend to java files FRM
    add DataChangeListener to FRM (Flow, Meter, Group)
    remove FRM handlers (Flow, Meter, Group)
    fix delete changes registration (in ImmutableDataChangeEvent -> old DataStore)
    
    Change-Id: I56003833519d9689a489ed5f77b3f7fc4ac2e204
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/group/GroupTransactionValidator.java
+++ opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/group/GroupTransactionValidator.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -9,8 +9,7 @@ package org.opendaylight.controller.frm.group;
 
 public class GroupTransactionValidator {
 
-    public static void validate(GroupTransaction transaction) throws IllegalStateException {
+    public static void validate(GroupChangeListener transaction) throws IllegalStateException {
         // NOOP
     }
-
 }
