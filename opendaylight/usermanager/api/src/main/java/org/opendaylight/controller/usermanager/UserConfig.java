c198e5355b6702b6d96f1174bc4d1477143397cc
Prasanth Pallamreddy <ppallamr@cisco.com>
2014-04-16 11:48:08 -0700

    Allow abilitiy to boot controller with non-default admin credentials
      - Default admin password can be overridden using the reset
        mechanism. NETWORK_ADMIN_PASSWORD_RECOVERY can optionally
        contain the password to be reset to.
      - Provide optional configuration to disallow factory default
        password. (-Dusermanager.disable-default-admin-password=true)
      - Convert ATN & ATZ security sensitive messages to 'trace' level
        instead of 'info' level
    
    Change-Id: I7dcaf15e8d562f61a3a4636a8abc244b0d6365ae
    Signed-off-by: Prasanth Pallamreddy <ppallamr@cisco.com>

--- opendaylight/usermanager/api/src/main/java/org/opendaylight/controller/usermanager/UserConfig.java
+++ opendaylight/usermanager/api/src/main/java/org/opendaylight/controller/usermanager/UserConfig.java
@@ -223,7 +223,7 @@ public class UserConfig extends ConfigurationObject implements Serializable {
         return new Status(StatusCode.SUCCESS);
     }
 
-    private Status validateClearTextPassword(String password) {
+    public static Status validateClearTextPassword(String password) {
         if (password == null || password.isEmpty()) {
             return new Status(StatusCode.BADREQUEST, "Password cannot be empty");
         }
@@ -247,7 +247,7 @@ public class UserConfig extends ConfigurationObject implements Serializable {
 
         // To make any changes to a user configured profile, current password
         // must always be provided
-        if (!this.password.equals(hash(this.salt, currentPassword))) {
+        if (!isPasswordMatch(currentPassword)) {
             return new Status(StatusCode.BADREQUEST, "Current password is incorrect");
         }
 
@@ -271,9 +271,13 @@ public class UserConfig extends ConfigurationObject implements Serializable {
         return status;
     }
 
+    public boolean isPasswordMatch(String otherPass) {
+        return this.password.equals(hash(this.salt, otherPass));
+    }
+
     public AuthResponse authenticate(String clearTextPassword) {
         AuthResponse locResponse = new AuthResponse();
-        if (password.equals(hash(this.salt, clearTextPassword))) {
+        if (isPasswordMatch(clearTextPassword)) {
             locResponse.setStatus(AuthResultEnum.AUTH_ACCEPT_LOC);
             locResponse.addData(getRolesString());
         } else {
