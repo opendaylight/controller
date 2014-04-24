dabd22f43215969a3553857ddec2745eb8532185
Vaishali Mithbaokar <vmithbao@cisco.com>
2014-04-24 15:50:41 -0700

    bug 1129:POM Versioning changes for automated weekly releases
    Change-Id: I75ddee10faabc5b62fe81e192de15ecb719a3105
    Signed-off-by: Surekha Bejgam <sbejgam@cisco.com>
    Signed-off-by: Vaishali Mithbaokar <vmithbao@cisco.com>

--- opendaylight/md-sal/sal-remoterpc-connector/implementation/src/test/java/org/opendaylight/controller/sal/connector/remoterpc/utils/RemoteServerTestClient.java
+++ opendaylight/md-sal/sal-remoterpc-connector/implementation/src/test/java/org/opendaylight/controller/sal/connector/remoterpc/utils/RemoteServerTestClient.java
@@ -8,6 +8,7 @@ import org.opendaylight.controller.sal.connector.remoterpc.dto.Message;
 import org.opendaylight.controller.sal.connector.remoterpc.util.XmlUtils;
 import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 import org.zeromq.ZMQ;
+import org.opendaylight.controller.sal.connector.remoterpc.utils.MessagingUtil;
 
 public class RemoteServerTestClient {
 
@@ -37,53 +38,53 @@ public class RemoteServerTestClient {
 
     return ping;
   }
-  public Message createAddFlowMessage(String serverAddress ){
-
-    RpcRouter.RouteIdentifier<?, ?, ?> routeIdentifier = getAddFlowRpcIdentifier();
-
-    Message addFlow = new Message.MessageBuilder()
-        .type(Message.MessageType.REQUEST)
-        .sender("localhost:5444")
-        .recipient(serverAddress)
-        .route(routeIdentifier)
-        .payload(getAddFlowPayload(1,1))
-        .build();
-
-    return addFlow;
-  }
+//  public Message createAddFlowMessage(String serverAddress ){
+//
+//    RpcRouter.RouteIdentifier routeIdentifier = getAddFlowRpcIdentifier();
+//
+//    Message addFlow = new Message.MessageBuilder()
+//        .type(Message.MessageType.REQUEST)
+//        .sender("localhost:5444")
+//        .recipient(serverAddress)
+//        .route(routeIdentifier)
+//        .payload(getAddFlowPayload(1,1))
+//        .build();
+//
+//    return addFlow;
+//  }
 
   private RpcRouter.RouteIdentifier<?, ?, ?> getAddFlowRpcIdentifier(){
     throw new UnsupportedOperationException();
   }
 
-  private CompositeNode getAddFlowPayload(int flowId, int tableId){
-    final String xml =
-    "<flow xmlns=\"urn:opendaylight:flow:inventory\">"
-    + "<priority>5</priority>"
-    + "<flow-name>Foo</flow-name>"
-    + "<match>"
-    + "<ethernet-match>"
-    + "<ethernet-type>"
-    + "<type>2048</type>"
-    + "</ethernet-type>"
-    + "</ethernet-match>"
-    + "<ipv4-destination>10.0.10.2/24</ipv4-destination>"
-    + "</match>"
-    + "<id>" + flowId + "</id>"
-    + "<table_id>" + tableId + "</table_id>"
-    + "<instructions>"
-    + "<instruction>"
-    + "<order>0</order>"
-    + "<apply-actions>"
-    + "<action>"
-    + "<order>0</order>"
-    + "<dec-nw-ttl/>"
-    + "</action>"
-    + "</apply-actions>"
-    + "</instruction>"
-    + "</instructions>"
-    + "</flow>";
-
-    return XmlUtils.xmlToCompositeNode(xml);
-  }
+//  private CompositeNode getAddFlowPayload(int flowId, int tableId){
+//    final String xml =
+//    "<flow xmlns=\"urn:opendaylight:flow:inventory\">"
+//    + "<priority>5</priority>"
+//    + "<flow-name>Foo</flow-name>"
+//    + "<match>"
+//    + "<ethernet-match>"
+//    + "<ethernet-type>"
+//    + "<type>2048</type>"
+//    + "</ethernet-type>"
+//    + "</ethernet-match>"
+//    + "<ipv4-destination>10.0.10.2/24</ipv4-destination>"
+//    + "</match>"
+//    + "<id>" + flowId + "</id>"
+//    + "<table_id>" + tableId + "</table_id>"
+//    + "<instructions>"
+//    + "<instruction>"
+//    + "<order>0</order>"
+//    + "<apply-actions>"
+//    + "<action>"
+//    + "<order>0</order>"
+//    + "<dec-nw-ttl/>"
+//    + "</action>"
+//    + "</apply-actions>"
+//    + "</instruction>"
+//    + "</instructions>"
+//    + "</flow>";
+//
+//    return XmlUtils.xmlToCompositeNode(xml);
+//  }
 }
