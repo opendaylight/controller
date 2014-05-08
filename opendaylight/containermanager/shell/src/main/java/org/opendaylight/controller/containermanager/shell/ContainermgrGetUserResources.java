839cdfb56df36d689cd249b6fc51e85cd0bf81ac
jfokkan <jfokkan@inocybe.com>
2014-05-08 11:32:42 -0400

    added containermanager shell feature
    PatchSet5: Worked containermanager.shell into distro
               and pom files so that it both builds and
               is pulled into the old distribution/opendaylight.
               Also checked to make sure that it worked there.
    
    PatchSet6: Fixed adding of new shell bundle to old
    		   distro
    
    Change-Id: I7f4d86c9902a7f4a8f6bd4bae74ebe4a9634e3bc
    Signed-off-by: Jonathan Fok kan <jfokkan@inocybe.com>
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ opendaylight/containermanager/shell/src/main/java/org/opendaylight/controller/containermanager/shell/ContainermgrGetUserResources.java
@@ -0,0 +1,26 @@
+package org.opendaylight.controller.containermanager.shell;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.controller.containermanager.IContainerManagerShell;
+
+@Command(scope = "containermanager", name = "containermgrGetUserResources", description="Get user resources")
+public class ContainermgrGetUserResources extends OsgiCommandSupport{
+    private IContainerManagerShell containerManager;
+
+    @Argument(index=0, name="userName", description="user name", required=true, multiValued=false)
+    String userName = null;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        for(String p : containerManager.containermgrGetUserResources(userName)) {
+            System.out.println(p);
+        }
+        return null;
+    }
+
+    public void setContainerManager(IContainerManagerShell containerManager){
+        this.containerManager = containerManager;
+    }
+}
