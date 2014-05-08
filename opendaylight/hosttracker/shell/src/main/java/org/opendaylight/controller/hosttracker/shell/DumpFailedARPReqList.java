70e28c6099995e1db9d05c5be7d2bcda96147e9f
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.ca>
2014-05-08 17:32:28 -0400

    Added Karaf feature hosttracker shell
    Change-Id: I6cd9a2702dd7b5814d310878e105de019c13ed70
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ opendaylight/hosttracker/shell/src/main/java/org/opendaylight/controller/hosttracker/shell/DumpFailedARPReqList.java
@@ -0,0 +1,14 @@
+package org.opendaylight.controller.hosttracker.shell;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+@Command(scope = "hosttracker", name = "dumpFailedARPReqList", description="Display the dump failed ARPReqList")
+public class DumpFailedARPReqList extends OsgiCommandSupport{
+
+    @Override
+    protected Object doExecute() throws Exception {
+        System.out.println("Executing hostracker dumpFailedARPReqList");
+        return null;
+    }
+}
