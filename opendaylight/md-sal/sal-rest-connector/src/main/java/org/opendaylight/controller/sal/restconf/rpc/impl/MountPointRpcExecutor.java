5c2759f9897bae0c0d097fbab73ba5f1e26915ce
Devin Avery <devin.avery@brocade.com>
2014-05-08 09:00:29 -0400

    Bug 953 - Enable mounted RPC calls via RestConf
    Break out the executor of an RPC mount point into multiple files.
    
    Change-Id: Ib214b4c82ffc487ee2f7b912e65d5c189b82154a
    Signed-off-by: Devin Avery <devin.avery@brocade.com>

---
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/rpc/impl/MountPointRpcExecutor.java
@@ -0,0 +1,43 @@
+package org.opendaylight.controller.sal.restconf.rpc.impl;
+
+import java.util.concurrent.ExecutionException;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.opendaylight.controller.sal.core.api.mount.MountInstance;
+import org.opendaylight.controller.sal.restconf.impl.ResponseException;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * Provides an implementation which invokes rpc methods via a mounted yang data model.
+ * @author Devin Avery
+ *
+ */
+public class MountPointRpcExecutor extends AbstractRpcExecutor {
+    private final MountInstance mountPoint;
+
+    public MountPointRpcExecutor(RpcDefinition rpcDef, MountInstance mountPoint) {
+        super( rpcDef );
+        this.mountPoint = mountPoint;
+        if( mountPoint == null )
+        {
+            throw new NullPointerException( "MountInstance is null" );
+        }
+    }
+
+    @Override
+    public RpcResult<CompositeNode> invokeRpc( CompositeNode rpcRequest ) throws ResponseException {
+        ListenableFuture<RpcResult<CompositeNode>> rpcFuture =
+                mountPoint.rpc( getRpcDefinition().getQName(), rpcRequest);
+        try {
+            return rpcFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new ResponseException(Status.INTERNAL_SERVER_ERROR,
+                                        e.getCause().getMessage() );
+        }
+    }
+}
