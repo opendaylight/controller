76d8a66814be207ade5e4cba867a21bb5e06054a
Flavio Fernandes <ffernand@redhat.com>
2014-05-14 08:56:58 -0400

    [DO NOT MERGE]
    This is an extension to the existing wiki page on md-sal ping tutorial.
    It exposes the use of Future<> with the rpc api generated by yang-tools,
    to make the ping service non-blocking; using the uris:
    
       .../ping/async/start/{ipAddress}
       .../ping/async/get/{ipAddress}
       .../ping/async/clear/{ipAddress}
    
    If this becomes interesting enough, I will update the link https://wiki.opendaylight.org/view/Ping
    with the stepping stones.
    
    Example:
    
    for x in 127.0.0.1 128.0.0.1 192.168.1.1 ;
       do curl --user "admin":"admin" -X PUT  http://localhost:8080/controller/nb/v2/ping/async/start/${x} ; echo ; done
    
    while : ; do for x in 127.0.0.1 128.0.0.1 192.168.1.1 ;
       do curl --user "admin":"admin" -X PUT  http://localhost:8080/controller/nb/v2/ping/async/get/${x} ; echo ;
    done ; sleep 2 ; done
    
    for x in 127.0.0.1 128.0.0.1 192.168.1.1 ;
       do curl --user "admin":"admin" -X PUT  http://localhost:8080/controller/nb/v2/ping/async/clear/${x} ; echo ; done
    
    [DO NOT MERGE]
    
    Change-Id: Ibfaee34ace0435cac39006a7e9c6577df2bf71a8
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- opendaylight/ping/service/src/main/java/org/opendaylight/controller/ping/service/api/PingServiceAPI.java
+++ opendaylight/ping/service/src/main/java/org/opendaylight/controller/ping/service/api/PingServiceAPI.java
@@ -3,13 +3,70 @@ package org.opendaylight.controller.ping.service.api;
 
 public interface PingServiceAPI {
 
+    public enum PingResult {
+        InProgress(0),
+        GotResponse(1),
+        NoResponse(2),
+        Error(3);
+
+        int value;
+        static java.util.Map<java.lang.Integer, PingResult> valueMap;
+
+        static {
+            valueMap = new java.util.HashMap<>();
+            for (PingResult enumItem : PingResult.values())
+            {
+                valueMap.put(enumItem.value, enumItem);
+            }
+        }
+
+        private PingResult(int value) {
+            this.value = value;
+        }
+
+        /**
+         * @return integer value
+         */
+        public int getIntValue() {
+            return value;
+        }
+
+        /**
+         * @param valueArg
+         * @return corresponding EchoResult item
+         */
+        public static PingResult forValue(int valueArg) {
+            return valueMap.get(valueArg);
+        }
+    }
+
     /**
-     * pingDestination
+     * pingDestinationSync
+     *
+     * Will block caller until ping operation is finished.
      *
      * @param address An IPv4 address to be pinged
-     * @return True if address is reachable,
-     * false if address is unreachable or error occurs.
+     * @return PingResult enum. Will never return InProgress.
      */
-    boolean pingDestination(String address);
+    PingResult pingDestinationSync(String address);
+
+    /**
+     * pingDestinationAsync
+     *
+     * Will return last known state for given address.
+     *
+     * @param address An IPv4 address to be pinged
+     * @return PingResult enum.
+     */
+    PingResult pingDestinationAsync(String address);
+
+    /**
+     * pingAsyncClear
+     *
+     * Will remove async ping for given address.
+     *
+     * @param address An IPv4 address to be pinged
+     */
+    void pingAsyncClear(String address);
 }
 
--- opendaylight/ping/service/src/main/java/org/opendaylight/controller/ping/service/impl/PingServiceImpl.java
+++ opendaylight/ping/service/src/main/java/org/opendaylight/controller/ping/service/impl/PingServiceImpl.java
@@ -1,6 +1,9 @@
 package org.opendaylight.controller.ping.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import org.opendaylight.controller.ping.service.api.PingServiceAPI;
 import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareConsumer;
@@ -13,54 +16,127 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.ping.rev130911.SendEchoOutp
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PingServiceImpl extends AbstractBindingAwareConsumer implements
         BundleActivator, BindingAwareConsumer, PingServiceAPI {
+    private static Logger log = LoggerFactory.getLogger(PingServiceImpl.class);
 
-    private PingService ping;
+    private PingService pingService;
     private ConsumerContext session;
+    private Map<Ipv4Address, Future<RpcResult<SendEchoOutput>>> asyncPingEntryMap;
 
     @Override
     public void onSessionInitialized(ConsumerContext session) {
         this.session = session;
+        this.pingService = this.session.getRpcService(PingService.class);
     }
 
     @Override
     protected void startImpl(BundleContext context) {
+        asyncPingEntryMap = new HashMap<>();
         context.registerService(PingServiceAPI.class, this, null);
     }
 
     @Override
-    public boolean pingDestination(String address) {
-
-        if (ping == null) {
-            ping = this.session.getRpcService(PingService.class);
-            if (ping == null) {
-
-                /* No ping service found. */
-                return false;
-            }
-        }
-
-        Ipv4Address destination = new Ipv4Address(address);
-
-        SendEchoInputBuilder ib = new SendEchoInputBuilder();
-        ib.setDestination(destination);
-        try {
-            RpcResult<SendEchoOutput> result = ping.sendEcho(ib.build()).get();
-            switch (result.getResult().getEchoResult()) {
-            case Reachable:
-                return true;
-            case Unreachable:
-            case Error:
-            default:
-                return false;
-            }
-        } catch (InterruptedException ie) {
-        } catch (ExecutionException ee) {
-        }
-
-        return false;
+    protected void stopImpl(BundleContext context) {
+        asyncPingEntryMap.clear();
     }
 
+    @Override
+    public PingResult pingDestinationSync(String address) {
+        return _pingDestinationSync(address);
+    }
+
+    @Override
+    public PingResult pingDestinationAsync(String address) {
+        return _pingDestinationAsync(address);
+    }
+
+    @Override
+    public void pingAsyncClear(String address) {
+        _pingAsyncClear(address);
+    }
+
+    private PingResult _pingDestinationSync(String address) {
+        if (pingService == null) { return PingResult.Error; } // No pingService service found.
+
+        try {
+            Ipv4Address destination = new Ipv4Address(address);
+            SendEchoInputBuilder ib = new SendEchoInputBuilder();
+            ib.setDestination(destination);
+            return mapResult(pingService.sendEcho(ib.build()).get().getResult().getEchoResult());
+        } catch (InterruptedException ie) {
+            log.warn("InterruptedException received by pingDestinationSync: {} from: {}",
+                    address, ie.getMessage());
+        } catch (ExecutionException ee) {
+            log.warn("ExecutionException received by pingDestinationSync: {} from: {}",
+                    address, ee.getMessage());
+        }
+        return PingResult.Error;
+    }
+
+    private synchronized PingResult _pingDestinationAsync(String address) {
+        if (pingService == null) { return PingResult.Error; } // No pingService service found.
+
+        /** Look for destination in asyncPingEntryMap. If none is found, create a new entry
+         * and return "in progress". This will happen on the very first time async is requested.
+         *
+         * NOTE: In a real scenario, you would want to consider a cache which automatically drops
+         * entries, so implementation does not need to rely on users calling async clear to remove
+         * the entries from the map. An example for doing such would be Google's caches or a
+         * weakhash map; which we deliberately chose not to use here for sake of simplicity.
+         */
+        final Ipv4Address destination = new Ipv4Address(address);
+        final Future<RpcResult<SendEchoOutput>> rpcResultFuture = asyncPingEntryMap.get(destination);
+        if (rpcResultFuture == null) {
+            SendEchoInputBuilder ib = new SendEchoInputBuilder();
+            ib.setDestination(destination);
+            asyncPingEntryMap.put(destination, pingService.sendEcho(ib.build()));
+            log.info("Starting pingDestinationAsync: {}", address);
+            return PingResult.InProgress;
+        }
+
+        /** Pending result may not be ready to be consumed. In such case, use "in progress".
+         */
+        if (!rpcResultFuture.isDone()) {
+            log.info("pingDestinationAsync: {} get result is not ready (ie. inProgress)", address);
+            return PingResult.InProgress;
+        }
+
+        /** If we made it this far, we know that rpcResultFuture is ready for consumption.
+         */
+        try {
+            PingResult pingResult = mapResult(rpcResultFuture.get().getResult().getEchoResult());
+            log.info("pingDestinationAsync: {} get result is {}", address, pingResult);
+            return pingResult;
+        } catch (InterruptedException ie) {
+            log.warn("InterruptedException received by pingDestinationAsync: {} from: {}",
+                    address, ie.getMessage());
+        } catch (ExecutionException ee) {
+            log.warn("ExecutionException received by pingDestinationAsync: {} from: {}",
+                    address, ee.getMessage());
+        }
+        return PingResult.Error;
+    }
+
+    private synchronized void _pingAsyncClear(String address) {
+        asyncPingEntryMap.remove(new Ipv4Address(address));
+        log.info("Removing pingDestinationAsync: {}", address);
+    }
+
+    private static PingResult mapResult(SendEchoOutput.EchoResult echoResult) {
+        // Translate echoResult to pingResult
+        switch (echoResult) {
+            case Reachable:
+                return PingResult.GotResponse;
+            case Unreachable:
+                return PingResult.NoResponse;
+            case Error:
+            default:
+                break;
+        }
+        return PingResult.Error;
+    }
 }
