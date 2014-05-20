0dc58e503c111621d7d28c880b1a3fca61e47311
Robert Varga <rovarga@cisco.com>
2014-05-20 10:52:59 +0200

    BUG-509: Refactor listener walking
    This patch creates a proper abstraction for examining a
    DataTreeCandidate for changes. This has the nice property of finishing
    the hiding of all implementation details behind proper interfaces, such
    that our implementation is free to do proper lifecycle as it pleases.
    
    Also gets rid of the AutoCloseable from candidate. It can be
    reintroduced in the need appears.
    
    Change-Id: I13301548594d4ae34e929c7202661997ea5aca90
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/tree/DataTreeCandidateNode.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.store.impl.tree;
+
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+import com.google.common.base.Optional;
+
+public interface DataTreeCandidateNode {
+    PathArgument getIdentifier();
+    Iterable<DataTreeCandidateNode> getChildNodes();
+
+    ModificationType getModificationType();
+    Optional<NormalizedNode<?, ?>> getDataAfter();
+    Optional<NormalizedNode<?, ?>> getDataBefore();
+}
