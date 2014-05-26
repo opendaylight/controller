4814dedf25a9439efd9c2b0f44cce80573c2a545
Tony Tkacik <ttkacik@cisco.com>
2014-05-26 12:04:45 +0200

    Bug 1073: Added support to DOMBrokerImpl for Transaction Chaining
    Splitted functionality of DOMBrokerImpl to separate classes
    which allows easier code reuse with transaction chaining:
    
    New internal APIs:
    DOMDataCommitExecutor - Commit executor which invokes
       three-phase commit coordination
    DOMDataCommitErrorListener - Error listener for commit executor
    and one execution
    
    AbstractDOMForwardedTransactionFactory - Factory which creates
       composite transactions on top of DOMStore transactions
    DOMDataCommitCoordinatorImpl - Commit Executor implementation
    DOMBrokerTransactionChainImpl - Implementation of DOMTransactionChain.
    
    Change-Id: Ic2290d7fb3d4ea52a44bea02b493c1e537e929a6
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/broker/impl/DOMDataCommitImplementation.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.broker.impl;
+
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.controller.sal.core.spi.data.DOMStoreThreePhaseCommitCohort;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * 
+ * Implementation prototype of commit method for
+ * {@link DOMForwardedWriteTransaction}.
+ * 
+ */
+public interface DOMDataCommitImplementation {
+
+    /**
+     * User-supplied implementation of {@link DOMDataWriteTransaction#commit()}
+     * for transaction.
+     * 
+     * Callback invoked when {@link DOMDataWriteTransaction#commit()} is invoked
+     * on transaction created by this factory.
+     * 
+     * @param transaction
+     *            Transaction on which {@link DOMDataWriteTransaction#commit()}
+     *            was invoked.
+     * @param cohorts
+     *            Iteration of cohorts for subtransactions associated with
+     *            commited transaction.
+     * 
+     */
+    ListenableFuture<RpcResult<TransactionStatus>> commit(final DOMDataWriteTransaction transaction,
+            final Iterable<DOMStoreThreePhaseCommitCohort> cohorts);
+}
