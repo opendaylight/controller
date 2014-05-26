f7344109ea508d4c60230f7f452090bd2000e16a
Tony Tkacik <ttkacik@cisco.com>
2014-05-26 12:04:45 +0200

    Bug 1073: Added support to DOMBrokerImpl for Transaction Chaining
    Splitted functionality of DOMBrokerImpl to separate classes
    which allows easier code reuse with transaction chaining:
    
    New internal APIs:
    DOMDataCommitExecutor - Commit executor which invokes
       three-phase commit coordination
    DOMDataCommitErrorListener - Error listener for commit executor
    and one execution
    
    AbstractDOMForwardedTransactionFactory - Factory which creates
       composite transactions on top of DOMStore transactions
    DOMDataCommitCoordinatorImpl - Commit Executor implementation
    DOMBrokerTransactionChainImpl - Implementation of DOMTransactionChain.
    
    Added 2 JUnit tests for Transaction Chains:
      - Test positive scenario (chain of write, read, delete transactions)
      - Test IllegalStateException when previous transaction
        was not commited.
    
    Change-Id: Ic2290d7fb3d4ea52a44bea02b493c1e537e929a6
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/broker/impl/DOMDataCommitExecutor.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.broker.impl;
+
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.controller.sal.core.spi.data.DOMStoreThreePhaseCommitCohort;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * Executor of Three Phase Commit coordination for
+ * {@link DOMDataWriteTransaction} transactions.
+ * 
+ * Implementations are responsible for executing implementation of three-phase
+ * commit protocol on supplied {@link DOMStoreThreePhaseCommitCohort}s.
+ * 
+ * 
+ */
+interface DOMDataCommitExecutor {
+
+    /**
+     * Submits supplied transaction to be executed in context of provided
+     * cohorts.
+     * 
+     * Transaction is used only as a context, cohorts should be associated with
+     * this transaction.
+     * 
+     * @param tx
+     *            Transaction to be used as context for reporting
+     * @param cohort
+     *            DOM Store cohorts representing provided transaction, its
+     *            subtransactoins.
+     * @param listener
+     *            Error listener which should be notified if transaction failed.
+     * @return ListenableFuture which contains RpcResult with
+     *         {@link TransactionStatus#COMMITED} if commit coordination on
+     *         cohorts finished successfully.
+     * 
+     */
+    ListenableFuture<RpcResult<TransactionStatus>> submit(DOMDataWriteTransaction tx,
+            Iterable<DOMStoreThreePhaseCommitCohort> cohort, Optional<DOMDataCommitErrorListener> listener);
+
+}
