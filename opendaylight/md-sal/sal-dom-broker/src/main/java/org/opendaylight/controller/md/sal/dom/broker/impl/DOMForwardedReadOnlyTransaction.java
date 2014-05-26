3aa233436097f898e202e5cc84243d67ac652d05
Tony Tkacik <ttkacik@cisco.com>
2014-05-26 12:04:45 +0200

    Bug 1073: Added support to DOMBrokerImpl for Transaction Chaining
    Splitted functionality of DOMBrokerImpl to separate classes
    which allows easier code reuse with transaction chaining:
    
    New internal APIs:
    DOMDataCommitExecutor - Commit executor which invokes
       three-phase commit coordination
    DOMDataCommitErrorListener - Error listener for commit executor
    and one execution
    
    AbstractDOMForwardedTransactionFactory - Factory which creates
       composite transactions on top of DOMStore transactions
    DOMDataCommitCoordinatorImpl - Commit Executor implementation
    DOMBrokerTransactionChainImpl - Implementation of DOMTransactionChain.
    
    Change-Id: Ic2290d7fb3d4ea52a44bea02b493c1e537e929a6
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/broker/impl/DOMForwardedReadOnlyTransaction.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.broker.impl;
+
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadTransaction;
+import org.opendaylight.controller.sal.core.spi.data.DOMStoreReadTransaction;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * 
+ * Read Only Transaction, which is composed of several
+ * {@link DOMStoreReadTransaction} transactions. Subtransaction is selected by
+ * {@link LogicalDatastoreType} type parameter in
+ * {@link #read(LogicalDatastoreType, InstanceIdentifier)}.
+ */
+class DOMForwardedReadOnlyTransaction extends
+        AbstractDOMForwardedCompositeTransaction<LogicalDatastoreType, DOMStoreReadTransaction> implements
+        DOMDataReadTransaction {
+
+    protected DOMForwardedReadOnlyTransaction(final Object identifier,
+            final ImmutableMap<LogicalDatastoreType, DOMStoreReadTransaction> backingTxs) {
+        super(identifier, backingTxs);
+    }
+
+    @Override
+    public ListenableFuture<Optional<NormalizedNode<?, ?>>> read(final LogicalDatastoreType store,
+            final InstanceIdentifier path) {
+        return getSubtransaction(store).read(path);
+    }
+
+}
