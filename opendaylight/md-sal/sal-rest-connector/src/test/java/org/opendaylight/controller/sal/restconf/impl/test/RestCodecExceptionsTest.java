82eb3f94c133e091ca5dd2f26450da9f121c12ca
Tony Tkacik <ttkacik@cisco.com>
2014-05-30 16:39:36 +0200

    Bug 981: Converted test to use BitsType.create()
    Change-Id: I759c55c48de64e48c5757aba5b7665f2478aa0fb
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestCodecExceptionsTest.java
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/restconf/impl/test/RestCodecExceptionsTest.java
@@ -7,20 +7,28 @@
  */
 package org.opendaylight.controller.sal.restconf.impl.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
+
+import java.util.Collections;
 
 import org.junit.Test;
 import org.opendaylight.controller.sal.restconf.impl.RestCodec;
 import org.opendaylight.yangtools.concepts.Codec;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
 import org.opendaylight.yangtools.yang.model.api.type.IdentityrefTypeDefinition;
 import org.opendaylight.yangtools.yang.model.util.BitsType;
 
 public class RestCodecExceptionsTest {
 
+    private static final SchemaPath PATH = SchemaPath.create(true, QName.create("test", "2014-05-30", "test"));
+
     @Test
     public void serializeExceptionTest() {
-        Codec<Object, Object> codec = RestCodec.from(new BitsType(null), null);
+        Codec<Object, Object> codec = RestCodec.from(BitsType.create(PATH, Collections.<Bit>emptyList()), null);
         String serializedValue = (String) codec.serialize("incorrect value"); // set
                                                                               // expected
         assertEquals("incorrect value", serializedValue);
