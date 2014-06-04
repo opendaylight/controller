e937449dfd480d58da4d6cc2340bbaa6a4c68d7c
Mathieu Lemay <mlemay@inocybe.com>
2014-06-04 15:50:56 -0400

    Modified Jersey2 with proper OSGi extender pattern support
    This patch provides a modified Jersey2 OSGi publisher which looks at all services and finds JAX-RS annotated resources and publishes them to a Jersey instance published by HTTPService.
    
    Change-Id: Ib933323decb7c589bf649fe0a8241b82b857942f
    Signed-off-by: Jake Shamash <jshamash@inocybe.ca>
    Signed-off-by: Mathieu Lemay <mlemay@inocybe.com>

---
+++ opendaylight/northbound/clustering/src/test/java/org/opendaylight/controller/clustering/northbound/ClusteringNorthboundTest.java
@@ -0,0 +1,16 @@
+
+package org.opendaylight.controller.clustering.northbound;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+// TODO tests
+public class ClusteringNorthboundTest {
+
+    @Test
+    public void testClustering() {
+        Assert.assertEquals(1, 1);
+    }
+
+}
