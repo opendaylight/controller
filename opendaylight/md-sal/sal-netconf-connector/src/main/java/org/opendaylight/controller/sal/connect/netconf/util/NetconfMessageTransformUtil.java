15266d997dc5e4337e8c8abfbb0174fa459f3e9f
Maros Marsalek <mmarsale@cisco.com>
2014-06-05 14:03:07 +0200

    BUG-1041 Make rpc-request -> xml translation use schema context
    Sal-netconf-connector now uses schema context if available to transform between rpc-request represented as DOM nodes to Xml.
    
    This is crucial for e.g. identity-ref type leaves in edit-config.
    
    Change-Id: I5b8602e7bf21e52c27b94daa3ac55213375641b5
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/util/NetconfMessageTransformUtil.java
+++ opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/util/NetconfMessageTransformUtil.java
@@ -11,8 +11,9 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import java.util.Map;
+import java.util.Set;
+
 import javax.annotation.Nullable;
 
 import org.opendaylight.controller.netconf.api.NetconfDocumentedException;
@@ -26,8 +27,19 @@ import org.opendaylight.yangtools.yang.data.api.Node;
 import org.opendaylight.yangtools.yang.data.impl.CompositeNodeTOImpl;
 import org.opendaylight.yangtools.yang.data.impl.ImmutableCompositeNode;
 import org.opendaylight.yangtools.yang.data.impl.SimpleNodeTOImpl;
-import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlDocumentUtils;
 import org.opendaylight.yangtools.yang.data.impl.util.CompositeNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
+import org.opendaylight.yangtools.yang.model.api.ConstraintDefinition;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.UsesNode;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -35,6 +47,8 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class NetconfMessageTransformUtil {
 
@@ -154,6 +168,20 @@ public class NetconfMessageTransformUtil {
                         NETCONF_GET_QNAME.getLocalName()));
     }
 
+    public static boolean isDataEditOperation(final QName rpc) {
+        return NETCONF_URI.equals(rpc.getNamespace())
+                && rpc.getLocalName().equals(NETCONF_EDIT_CONFIG_QNAME.getLocalName());
+    }
+
+
+    public static DataNodeContainer createSchemaForEdit(final SchemaContext schemaContext) {
+        final QName config = QName.create(NETCONF_EDIT_CONFIG_QNAME, "config");
+        final QName editConfig = QName.create(NETCONF_EDIT_CONFIG_QNAME, "edit-config");
+        final NodeContainerProxy configProxy = new NodeContainerProxy(config, schemaContext.getChildNodes());
+        final NodeContainerProxy editConfigProxy = new NodeContainerProxy(editConfig, Sets.<DataSchemaNode>newHashSet(configProxy));
+        return new NodeContainerProxy(NETCONF_RPC_QNAME, Sets.<DataSchemaNode>newHashSet(editConfigProxy));
+    }
+
     public static CompositeNodeTOImpl wrap(final QName name, final Node<?> node) {
         if (node != null) {
             return new CompositeNodeTOImpl(name, null, Collections.<Node<?>> singletonList(node));
@@ -178,6 +206,119 @@ public class NetconfMessageTransformUtil {
             return it.add(node).toInstance();
         } else {
             return it.toInstance();
+        }
+    }
+
+    private static class NodeContainerProxy implements ContainerSchemaNode {
+
+        private final Map<QName, DataSchemaNode> childNodes;
+        private final QName qName;
+
+        public NodeContainerProxy(final QName qName, final Map<QName, DataSchemaNode> childNodes) {
+            this.childNodes = childNodes;
+            this.qName = qName;
+        }
+
+        public NodeContainerProxy(final QName qName, final Set<DataSchemaNode> childNodes) {
+            this(qName, asMap(childNodes));
+        }
+
+        private static Map<QName, DataSchemaNode> asMap(final Set<DataSchemaNode> childNodes) {
+            Map<QName, DataSchemaNode> mapped = Maps.newHashMap();
+            for (DataSchemaNode childNode : childNodes) {
+                mapped.put(childNode.getQName(), childNode);
+            }
+            return mapped;
+        }
+
+        @Override
+        public Set<TypeDefinition<?>> getTypeDefinitions() {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public Set<DataSchemaNode> getChildNodes() {
+            return Sets.newHashSet(childNodes.values());
+        }
+
+        @Override
+        public Set<GroupingDefinition> getGroupings() {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public DataSchemaNode getDataChildByName(final QName qName) {
+            return childNodes.get(qName);
+        }
+
+        @Override
+        public DataSchemaNode getDataChildByName(final String s) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Set<UsesNode> getUses() {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public boolean isPresenceContainer() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Set<AugmentationSchema> getAvailableAugmentations() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isAugmenting() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isAddedByUses() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isConfiguration() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ConstraintDefinition getConstraints() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public QName getQName() {
+            return qName;
+        }
+
+        @Override
+        public SchemaPath getPath() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getDescription() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getReference() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Status getStatus() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public List<UnknownSchemaNode> getUnknownSchemaNodes() {
+            return Collections.emptyList();
         }
     }
 
