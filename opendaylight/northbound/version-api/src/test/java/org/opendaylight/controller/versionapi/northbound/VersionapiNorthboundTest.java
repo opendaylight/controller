94dc9989add35aea9905b0b98ed5382c0245704c
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.ca>
2014-06-06 11:58:34 -0400

    Added feature versionapi northbound
    Change-Id: Icdab5f5583c2c7a1db8f5754cbbbba196af33426
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.ca>

---
+++ opendaylight/northbound/version-api/src/test/java/org/opendaylight/controller/versionapi/northbound/VersionapiNorthboundTest.java
@@ -0,0 +1,16 @@
+
+package org.opendaylight.controller.versionapi.northbound;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+// TODO tests
+public class VersionapiNorthboundTest {
+
+    @Test
+    public void testClustering() {
+        Assert.assertEquals(1, 1);
+    }
+
+}
