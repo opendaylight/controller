32b22207fa277c2803c06ad8c293dca07bc5efd0
Vaclav Demcak <vdemcak@cisco.com>
2014-06-10 10:33:53 +0200

    bug 537 - Node Reconciliation
    https://wiki.opendaylight.org/view/OpenDaylight_OpenFlow_Plugin:Backlog:Node_Status_Reconciliation
    
    * sal-node-reconciliation.yang - own notification for preventing unexpected call
    	* NodeReconcilAdd notif - new device has been registred and FRM add
    	  all pre-configured flows, meters and groups fromm config DataStore
    	* NodeReconcilDel notif - device disconnected and FRM has to clean
    	  internal MapHolder for a relation flow_cookie to flow_id
    * FRM Reconciliation Notify Listener
    * FRM MapHolder for a realtion flow_cookie to flow_id
    * FlowStatTracke change the add flow_id function for the flow statistics
    
    In additional commit has fixed Bug 282 - Pre-configuration of flows causes exception
    
    Change-Id: Iadc2b111780524b9a8fd86cfc248e5fa194e6638
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/FlowNodeReconcilListener.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.frm;
+
+import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.reconcil.rev140616.NodeReconcilAdd;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.reconcil.rev140616.NodeReconcilDel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.reconcil.rev140616.SalNodeReconciliationListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.meters.Meter;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.FlowTableRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowCookie;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.AddGroupInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.GroupRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.Group;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterRef;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * forwardingrules-manager
+ * org.opendaylight.controller.frm
+ *
+ *
+ * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
+ *
+ * Created: Jun 13, 2014
+ */
+public class FlowNodeReconcilListener implements SalNodeReconciliationListener {
+
+    private final FlowNodeReconcilProvider provider;
+
+    public FlowNodeReconcilListener(final FlowNodeReconcilProvider provider) {
+        this.provider = provider;
+    }
+
+    @Override
+    public void onNodeReconcilAdd( final NodeReconcilAdd reconcilNotif ) {
+        final NodeRef nodeRef = reconcilNotif.getNodeRef();
+        final InstanceIdentifier<Node> nodeIdent = (InstanceIdentifier<Node>) nodeRef.getValue();
+
+        DataModificationTransaction modifTran = this.provider.startChange();
+        DataObject nodeData = modifTran.readConfigurationData(nodeIdent);
+
+        if (nodeData != null && nodeData instanceof Node) {
+            final Node node = (Node) nodeData;
+            final FlowCapableNode flowCapNode = node.getAugmentation( FlowCapableNode.class );
+            final InstanceIdentifier<FlowCapableNode> flowNodeIdent = nodeIdent.builder()
+                    .augmentation( FlowCapableNode.class ).build();
+
+            /* Groups - have to be first */
+            for (Group group : flowCapNode.getGroup()) {
+                final AddGroupInputBuilder groupBuilder = new AddGroupInputBuilder( group );
+                groupBuilder.setNode( reconcilNotif.getNodeRef() );
+                groupBuilder.setGroupRef( new GroupRef( flowNodeIdent.builder().child( Group.class, group.getKey() ).build() ) );
+
+                this.provider.getSalGroupService().addGroup( groupBuilder.build() );
+            }
+
+            /* Meters */
+            for (Meter meter : flowCapNode.getMeter()) {
+                final AddMeterInputBuilder meterBuilder = new AddMeterInputBuilder( meter );
+                meterBuilder.setNode( nodeRef );
+                meterBuilder.setMeterRef( new MeterRef( flowNodeIdent.builder().child( Meter.class, meter.getKey() ).build() ) );
+
+                this.provider.getSalMeterService().addMeter( meterBuilder.build() );
+            }
+
+            /* Flows */
+            for (Table flowTable : flowCapNode.getTable()) {
+                final InstanceIdentifier<Table> tableInstanceId =
+                        flowNodeIdent.builder().child(Table.class, flowTable.getKey()).build();
+                for (Flow flow : flowTable.getFlow()) {
+                    FlowCookie flowCookie = new FlowCookie( FlowCookieManager.INSTANCE.getNewCookie( tableInstanceId ) );
+                    AddFlowInputBuilder flowBuilder = new AddFlowInputBuilder( flow );
+                    flowBuilder.setCookie( flowCookie );
+                    flowBuilder.setNode( nodeRef );
+                    flowBuilder.setFlowTable( new FlowTableRef( tableInstanceId ) );
+                    flowBuilder.setFlowRef( new FlowRef(tableInstanceId.builder().child( Flow.class, flow.getKey() ).build() ) );
+
+                    FlowCookieManager.flowCookieMapUpdate( this.provider.getDataService(),
+                            flowCookie, flow.getId(), tableInstanceId );
+                    this.provider.getSalFlowService().addFlow( flowBuilder.build() );
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onNodeReconcilDel( NodeReconcilDel reconcilNotif ) {
+        final NodeRef nodeRef = reconcilNotif.getNodeRef();
+        final InstanceIdentifier<Node> nodeIdent = (InstanceIdentifier<Node>) nodeRef.getValue();
+
+        DataModificationTransaction modifTran = this.provider.startChange();
+        DataObject nodeData = modifTran.readConfigurationData(nodeIdent);
+
+        if (nodeData != null && nodeData instanceof Node) {
+            final Node node = (Node) nodeData;
+            final FlowCapableNode flowCapNode = node.getAugmentation( FlowCapableNode.class );
+            final InstanceIdentifier<FlowCapableNode> flowNodeIdent =
+                    nodeIdent.builder().augmentation( FlowCapableNode.class ).build();
+            /* Cookie Key Map cleaning */
+            for (Table flowTable : flowCapNode.getTable()) {
+                final InstanceIdentifier<Table> tableInstanceId =
+                        flowNodeIdent.builder().child(Table.class, flowTable.getKey()).build();
+                FlowCookieManager.INSTANCE.clean( tableInstanceId );
+            }
+        }
+    }
+}
