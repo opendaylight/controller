cf7bdadede47bff0d65a17a6b9c7833190223091
Tomas Olvecky <tolvecky@cisco.com>
2014-06-13 11:54:01 +0200

    Bug 1077 - Draft of data store api and spi
    Change-Id: Ibaf3295ccbda2d19b3063f158bac1b70ab84cb1b
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

---
+++ opendaylight/md-sal/dstracer-api/src/main/java/org/opendaylight/controller/mdsal/dstracer/api/DataStorePersisterController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.mdsal.dstracer.api;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+
+public interface DataStorePersisterController {
+
+    AutoCloseable startTracing(LogicalDatastoreType store, InstanceIdentifier path, DataChangeScope triggeringScope);
+
+}
