ba0ab46887c62b2339823cc03c24ba3b17060383
Tony Tkacik <ttkacik@cisco.com>
2014-06-16 18:52:14 +0200

    API Clarity: Documented Async Data Broker APIs.
    Documented AsyncDataBroker APIs, which are
    base abstract APIs from which Binding and DOM
    data broker APIs are derived.
    
    Introduced code-examples illustrating
    transaction isollation and conflict detection.
    
    Change-Id: I3c881b1fceb0b68c0cc28a1caa7780b9b6c2af3f
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataChangeEvent.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataChangeEvent.java
@@ -13,63 +13,126 @@ import java.util.Set;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.concepts.Path;
 
-public interface AsyncDataChangeEvent<P extends Path<P>,D> extends Immutable {
+/**
+ *
+ * Data Change Event which contains capture of changes in data subtree
+ *
+ * <p>
+ * Represents a notification indicating that some data at or under a particular
+ * path has changed. The notification contains a capture of the changes in data
+ * subtree. This event is triggered by successful application of modifications
+ * from a transaction on the global data tree. Use the
+ * {@link AsyncDataBroker#registerDataChangeListener(LogicalDatastoreType, Path, AsyncDataChangeListener, org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope)}
+ * method to register a listener for data change events.
+ *
+ * <p>
+ * A listener will only receive notifications for changes to data under the path
+ * they register for. See
+ * {@link AsyncDataBroker#registerDataChangeListener(LogicalDatastoreType, Path, AsyncDataChangeListener, org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope)}
+ * to learn more about registration scopes.
+ *
+ * <p>
+ * The entire subtree under the path will be provided via instance methods of Data
+ * Change Event even if just a leaf node changes.
+ *
+ * <p>
+ * <b>Implementation Note:</b> This interface is not intended to be implemented
+ * by users of MD-SAL, but only to be consumed by them.
+ *
+ * @param <P>
+ *            Type of path (subtree identifier), which represents location in
+ *            tree
+ * @param <D>
+ *            Type of data (payload), which represents data payload
+ */
+public interface AsyncDataChangeEvent<P extends Path<P>, D> extends Immutable {
     /**
-     * Returns a immutable map of paths and newly created objects
+     * Returns a map of paths and newly created objects, which were introduced by
+     * this change into conceptual data tree, if no new objects were introduced
+     * this map will be empty.
+     *
+     * This map contains all data tree nodes (and paths to them), which were in
+     * scope of listener registration and was created. This data tree nodes
+     * contain their whole subtree with their current state.
      *
      * @return map of paths and newly created objects
      */
     Map<P, D> getCreatedData();
 
     /**
-     * Returns a immutable map of paths and respective updated objects after update.
+     * Returns a map of paths and updated objects, which were changed by this
+     * change into conceptual data tree, if no existing objects were updated
+     * this map will be empty.
      *
-     * Original state of the object is in
-     * {@link #getOriginalData()}
+     * This map contains data tree nodes (and paths to them) which are in the
+     * scope of the listener registration and have been updated. The data tree
+     * nodes contain the whole subtree with their current state.
+     *
+     * A Node is considered updated if it contents were replaced or one of its
+     * children was created, removed or updated.
+     *
+     * Original state of the updated data tree nodes is in
+     * {@link #getOriginalData()} stored with same path.
      *
      * @return map of paths and newly created objects
      */
     Map<P, D> getUpdatedData();
 
     /**
-     * Returns a immutable set of removed paths.
+     * Returns an immutable set of removed paths.
      *
-     * Original state of the object is in
-     * {@link #getOriginalData()}
+     * This set contains the paths to the data tree nodes which are in the scope
+     * of the listener registration and have been removed.
+     *
+     * Original state of the removed data tree nodes is in
+     * {@link #getOriginalData()} stored with same path.
      *
      * @return set of removed paths
      */
     Set<P> getRemovedPaths();
 
     /**
-     * Return a immutable map of paths and original state of updated and removed objects.
+     * Return an immutable map of paths and original state of updated and removed
+     * objects.
      *
-     * This map is populated if at changed path was previous object, and captures
-     * state of previous object.
+     * This map contains the original version of the data tree nodes (and paths
+     * to them), which are in the scope of the listener registration.
      *
      * @return map of paths and original state of updated and removed objects.
      */
     Map<P, ? extends D> getOriginalData();
 
     /**
-     * Returns a  immutable stable view of data state, which
-     * captures state of data store before the reported change.
+     * Returns an immutable stable view of data state, which captures state of
+     * data store before the reported change.
      *
+     * The view is rooted at the point where the listener, to which the event is
+     * being delivered, was registered.
      *
-     * The view is rooted at the point where the listener, to which the event is being delivered, was registered.
+     * If listener used a wildcarded path (if supported by path type) during
+     * registration for change listeners this method returns null, and original
+     * state can be accessed only via {@link #getOriginalData()}
      *
-     * @return Stable view of data before the change happened, rooted at the listener registration path.
+     * @return Stable view of data before the change happened, rooted at the
+     *         listener registration path.
      *
      */
     D getOriginalSubtree();
 
     /**
-     * Returns a immutable stable view of data, which captures state of data store
-     * after the reported change.
+     * Returns an immutable stable view of data, which captures state of data
+     * store after the reported change.
      *
-     * The view is rooted at the point where the listener, to which the event is being delivered, was registered.
+     * The view is rooted at the point where the listener, to which the event is
+     * being delivered, was registered.
      *
-     * @return Stable view of data after the change happened, rooted at the listener registration path.
+     * If listener used a wildcarded path (if supported by path type) during
+     * registration for change listeners this method returns null, and state
+     * can be accessed only via {@link #getCreatedData()},
+     * {@link #getUpdatedData()}, {@link #getRemovedPaths()}
+     *
+     * @return Stable view of data after the change happened, rooted at the
+     *         listener registration path.
      */
     D getUpdatedSubtree();
 }
--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataTransactionFactory.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataTransactionFactory.java
@@ -9,12 +9,111 @@ package org.opendaylight.controller.md.sal.common.api.data;
 
 import org.opendaylight.yangtools.concepts.Path;
 
+/**
+ * Transaction factory which allocates new transactions which operates on data
+ * tree.
+ *
+ * <p>
+ * <b>Note:</b> This interface is not intended to be used directly, but rather
+ * via subinterfaces which introduces additional semantics to allocated
+ * transactions.
+ * <ul>
+ * <li> {@link AsyncDataBroker}
+ * <li> {@link TransactionChain}
+ * </ul>
+ *
+ * <p>
+ * All operations on data tree are performed via one of the transactions:
+ * <ul>
+ * <li>Read-Only - allocated using {@link #newReadOnlyTransaction()}
+ * <li>Write-Only - allocated using {@link #newWriteOnlyTransaction()}
+ * <li>Read-Write - allocated using {@link #newReadWriteTransaction()}
+ * </ul>
+ *
+ * <p>
+ * These transactions provides stable isolated view of data tree, which is
+ * guaranteed to be not affected by other concurrent transactions, until
+ * transaction is committed.
+ *
+ * <p>
+ * For detailed explanation of isolated on transaction concurrency, see
+ * {@link AsyncReadTransaction}, {@link AsyncWriteTransaction},
+ * {@link AsyncReadWriteTransaction} and {@link AsyncWriteTransaction#commit()}
+ * for more details how transaction are isolated and how transaction-local
+ * changes are commited to global data tree.
+ *
+ * <p>
+ * It is strongly recommended to use the type of transaction, which which
+ * provides only the minimal capabilities you need. This allows for
+ * optimisations at the data broker / data store level. For example,
+ * implementations may optimise the transaction for reading if they know ahead
+ * of time that you only need to read data in such way, that they do not need to
+ * keep additional metadata, which may be required for write transactions.
+ *
+ * <b>Implementation Note:</b> This interface is not intended to be implemented
+ * by users of MD-SAL, but only to be consumed by them.
+ *
+ * @see AsyncDataBroker
+ * @see TransactionChain
+ *
+ * @param <P>
+ *            Type of path (subtree identifier), which represents location in
+ *            tree
+ * @param <D>
+ *            Type of data (payload), which represents data payload
+ */
 public interface AsyncDataTransactionFactory<P extends Path<P>, D> {
 
+    /**
+     * Allocates new read-only transaction which provides latest snapshot of
+     * immutable view of data tree, which could be consumed by clients.
+     *
+     * View of data tree is immutable snapshot of current data tree state when
+     * transaction was allocated.
+     *
+     * @return new read-only transaction
+     */
     AsyncReadTransaction<P, D> newReadOnlyTransaction();
 
+    /**
+     * Allocates new read-write transaction provides latest mutable view of data
+     * tree, which could be consumed by clients.
+     *
+     * <p>
+     * Preconditions for mutation of data tree are captured from snapshot of
+     * data tree state, when transaction was allocated, if data was
+     * changed during transaction in incompatible way, commit of this transaction
+     * will fail. See {@link AsyncWriteTransaction#commit()} for more
+     * details about conflicting and not-conflicting changes and
+     * failure scenarios.
+     *
+     * @return new read-write transaction
+     */
     AsyncReadWriteTransaction<P, D> newReadWriteTransaction();
 
-    AsyncWriteTransaction<P,D> newWriteOnlyTransaction();
+    /**
+     * Allocates new write-only transaction based on latest state of data
+     * tree, which could be consumed by clients.
+     *
+     * <p>
+     * Preconditions for mutation of data tree are captured from snapshot of
+     * data tree state, when transaction was allocated, if data was
+     * changed during transaction in incompatible way, commit of this transaction
+     * will fail. See {@link AsyncWriteTransaction#commit()} for more
+     * details about conflicting and not-conflicting changes and
+     * failure scenarios.
+     *
+     * <p>
+     * Since this transaction does not provide view of data, it SHOULD BE
+     * used only by callers, which are exclusive writers (exporters of data)
+     * to subtree they modify, in order to prevent optimistic
+     * lock failures as described in {@link AsyncWriteTransaction#commit()}.
+     * <p>
+     * Exclusivity of writers to particular subtree SHOULD BE enforced by
+     * external locking mechanism.
+     *
+     * @return new write-only transaction
+     */
+    AsyncWriteTransaction<P, D> newWriteOnlyTransaction();
 
 }
