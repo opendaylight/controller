098a4e5ffe770b14aaee1b3d94f9fa8faaa638c4
Moiz Raja <moraja@cisco.com>
2014-06-16 13:14:58 -0700

    Add copyright messages for java files
    Change-Id: I8ac584f45481d9453480d8ccc5316a0f0baca199
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/FindPrimary.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/FindPrimary.java
@@ -1,8 +1,14 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.controller.cluster.datastore.messages;
 
 import com.google.common.base.Preconditions;
-import com.google.protobuf.GeneratedMessage;
-import com.google.protobuf.Message;
 
 /**
  * The FindPrimary message is used to locate the primary of any given shard
