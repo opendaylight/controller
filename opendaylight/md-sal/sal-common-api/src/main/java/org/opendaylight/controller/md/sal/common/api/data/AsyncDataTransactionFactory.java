d03aadc640a7365e9bf78b32cb4b1dcdaecc8898
Tony Tkacik <ttkacik@cisco.com>
2014-06-16 18:52:14 +0200

    API Clarity: Documented Async Data Broker APIs.
    Documented AsyncDataBroker APIs, which are
    base abstract APIs from which Binding and DOM
    data broker APIs are derived.
    
    Introduced code-examples illustrating
    transaction isollation and conflict detection.
    
    Change-Id: I3c881b1fceb0b68c0cc28a1caa7780b9b6c2af3f
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataTransactionFactory.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncDataTransactionFactory.java
@@ -9,12 +9,77 @@ package org.opendaylight.controller.md.sal.common.api.data;
 
 import org.opendaylight.yangtools.concepts.Path;
 
+/**
+ * Transaction factory which allocates new transactions which operates on
+ * data tree.
+ *
+ * <b>Note:</b> This interface is not intended to be used
+ * directly, but rather via subinterfaces which introduces
+ * additional semantics to allocated transanctions.
+ * <ul>
+ * <li> {@link AsyncDataBroker}
+ * <li> {@link TransactionChain}
+ * </ul>
+ *
+ * All operations on data tree are performed via one of the transactions:
+ * <ul>
+ * <li>Read-Only - allocated using {@link #newReadOnlyTransaction()}
+ * <li>Write-Only - allocated using {@link #newWriteOnlyTransaction()}
+ * <li>Read-Write - allocated using {@link #newReadWriteTransaction()}
+ * </ul>
+ *
+ * It is strongly recommended to use the type of transaction, which which
+ * provides only the minimal capabilities you need. This allows for
+ * optimizations at the data broker / data store level. For example,
+ * implementations may optimize the transaction for reading if they know ahead
+ * of time that you only need to read data in such way, that they do not need to
+ * keep additional metadata, which may be required for write transactions.
+ *
+ * <b>Implementation Note:</b> This interface is not intended to be implemented
+ * by users of MD-SAL, but only to be consumed by them.
+ *
+ * @see AsyncDataBroker
+ * @see TransactionChain
+ *
+ * @param <P>
+ *            Type of path (subtree identifier), which represents location in
+ *            tree
+ * @param <D>
+ *            Type of data (payload), which represents data payload
+ */
 public interface AsyncDataTransactionFactory<P extends Path<P>, D> {
 
+    /**
+     * Allocates new Read-only transaction which provides latest snapshot of
+     * immutable view of data tree, which could be consumed by clients.
+     *
+     * View of data tree is immutable snapshot of current data tree state when
+     * transaction was allocated.
+     *
+     * @return new read-only transaction
+     */
     AsyncReadTransaction<P, D> newReadOnlyTransaction();
 
+    /**
+     * Allocates new write-only transaction provides latest mutable view of data tree,
+     * which could be consumed by clients.
+     *
+     * Preconditions for mutation of data tree are captured from snapshot of
+     * data tree state, when transaction was allocated.
+     *
+     * @return new read-write transaction
+     */
     AsyncReadWriteTransaction<P, D> newReadWriteTransaction();
 
-    AsyncWriteTransaction<P,D> newWriteOnlyTransaction();
+    /**
+     * Allocates new write-only transaction provides latest mutable view of data tree,
+     * which could be consumed by clients.
+     *
+     * Preconditions for mutation of data tree are captured from snapshot of
+     * data tree state, when transaction was allocated.
+     *
+     * @return new write-only transaction
+     */
+    AsyncWriteTransaction<P, D> newWriteOnlyTransaction();
 
 }
