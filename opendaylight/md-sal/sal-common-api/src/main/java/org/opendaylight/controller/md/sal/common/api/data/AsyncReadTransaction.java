f93fc3d5dd9e65d12fc0348373ba981623c6541a
Tony Tkacik <ttkacik@cisco.com>
2014-06-16 18:52:14 +0200

    API Clarity: Documented Async Data Broker APIs.
    Documented AsyncDataBroker APIs, which are
    base abstract APIs from which Binding and DOM
    data broker APIs are derived.
    
    Introduced code-examples illustrating
    transaction isollation and conflict detection.
    
    Change-Id: I3c881b1fceb0b68c0cc28a1caa7780b9b6c2af3f
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncReadTransaction.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncReadTransaction.java
@@ -12,12 +12,61 @@ import org.opendaylight.yangtools.concepts.Path;
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.ListenableFuture;
 
+/**
+ *
+ * Read transaction which provides read-only view of data tree, which could
+ * be consumed by clients.
+ *
+ * View of data tree is stable point-in-time snapshot of current
+ * data tree state captured when transaction was created and
+ * it's state and underlying data tree
+ * is not affected by other concurrently running transactions.
+ *
+ * <b>Implementation Note:</b> This interface is not intended to be implemented
+ * by users of MD-SAL, but only to be consumed by them.
+ *
+ * <h2>Transaction isolation example</h2>
+ * Lest assume initial state of data tree for <code>PATH</code> is <code>A</code>.
+ *
+ * <pre>
+ * <code>
+ * txRead = broker.newReadOnlyTransaction(); // read Transaction is snapshot of data
+ * txWrite = broker.newReadWriteTransactoin(); // concurrent write transaction
+ * txRead.read(OPERATIONAL,PATH).get(); // will return Optional containing A
+ *
+ * txWrite = broker.put(OPERATIONAL,PATH,B); // writes b to PATH
+ *
+ * txRead.read(OPERATIONAL,PATH).get(); // still returns Optional containing A
+ *
+ * txWrite.commit().get(); // data tree is updated, PATH contains B
+ * txRead.read(OPERATIONAL,PATH).get(); // still returns Optional containing A
+ *
+ * txAfterCommit = broker.newReadOnlyTransaction(); // read Transaction is snapshot of new state
+ * txAfterCommit.read(OPERATIONAL,PATH).get(); // returns Optional containing B;
+ * </code>
+ * </pre>
+ *
+ * <b>Note:</b> example contains blocking calls on future only to illustrate
+ * that action happened after other asynchronous action. Use of blocking
+ * call {@link ListenableFuture#get()} is discouraged for most uses
+ * and you should use {@link ListenableFuture#addListener(Runnable, java.util.concurrent.Executor)}
+ * or helper functions from {@link com.google.common.uti.concurrent.Futures} to
+ * register more specific listeners.
+ *
+ * @param <P>
+ *            Type of path (subtree identifier), which represents location in
+ *            tree
+ * @param <D>
+ *            Type of data (payload), which represents data payload
+ */
 public interface AsyncReadTransaction<P extends Path<P>, D> extends AsyncTransaction<P, D> {
 
     /**
      *
-     * Reads data from provided logical data store located at provided path
+     * Reads data from provided logical data store located at provided path.
      *
+     * If the target is subtree, whole subtree is read (accessible from
+     * returned data object).
      *
      * @param store
      *            Logical data store from which read should occur.
@@ -26,10 +75,10 @@ public interface AsyncReadTransaction<P extends Path<P>, D> extends AsyncTransac
      *            read
      * @return Listenable Future which contains read result
      *         <ul>
-     *         <li>If data at supplied path exists the {@link Future#get()}
-     *         returns Optional object containing data
+     *         <li>If data at supplied path exists the {@link ListeblaFuture#get()}
+     *         returns Optional object containing data once read is done.
      *         <li>If data at supplied path does not exists the
-     *         {@link Future#get()} returns {@link Optional#absent()}.
+     *         {@link ListenbleFuture#get()} returns {@link Optional#absent()}.
      *         </ul>
      */
     ListenableFuture<Optional<D>> read(LogicalDatastoreType store, P path);
--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncWriteTransaction.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/data/AsyncWriteTransaction.java
@@ -13,31 +13,84 @@ import org.opendaylight.yangtools.yang.common.RpcResult;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-public interface AsyncWriteTransaction<P extends Path<P>, D>  extends AsyncTransaction<P, D> {
+/**
+ * Write transaction provides mutation capabilities of data tree
+ *
+ * <p>
+ * Initial state of write transaction is stable snapshot of current data tree
+ * state captured when transaction was created and it's state and underlying
+ * data tree are not affected by other concurrently running transactions.
+ * <p>
+ * Write transaction is isolated from other concurrent write transactions,
+ * all writes are local to the transaction and represents only proposal
+ * of state change for data tree and it is not visible to any
+ * other concurrently running transactions.
+ * <p>
+ * Application (publishing) of changes proposed in this transaction
+ * is done by commiting transaction via {@link #commit()}
+ * message, which seals transaction (prevents any further writes
+ * using this transaction) and submits it to be processed and applied
+ * to global conceptual data tree.
+ * <p>
+ * Transaction commit may fail due to concurrent transaction modified
+ * data in incompatible way and was commited earlier. See {@link #commit()}
+ * for more concrete examples.
+ *
+ * <b>Implementation Note:</b> This interface is not intended to be implemented
+ * by users of MD-SAL, but only to be consumed by them.
+ *
+ * @param <P>
+ *            Type of path (subtree identifier), which represents location in
+ *            tree
+ * @param <D>
+ *            Type of data (payload), which represents data payload
+ */
+public interface AsyncWriteTransaction<P extends Path<P>, D> extends AsyncTransaction<P, D> {
     /**
      * Cancels transaction.
      *
-     * Transaction could be only cancelled if it's status
-     * is {@link TransactionStatus#NEW} or {@link TransactionStatus#SUBMITED}
+     * Transaction could be only cancelled if it's status is
+     * {@link TransactionStatus#NEW} or {@link TransactionStatus#SUBMITED}
      *
-     * Invoking cancel() on {@link TransactionStatus#FAILED} or {@link TransactionStatus#CANCELED}
-     * will have no effect.
+     * Invoking cancel() on {@link TransactionStatus#FAILED} or
+     * {@link TransactionStatus#CANCELED} will have no effect.
      *
-     * @throws IllegalStateException If transaction status is {@link TransactionStatus#COMMITED}
+     * @throws IllegalStateException
+     *             If transaction status is {@link TransactionStatus#COMMITED}
      *
      */
     public void cancel();
 
     /**
-     * Store a piece of data at specified path. This acts as a add / replace operation,
-     * which is to say that whole subtree will be replaced by specified path.
+     * Store a piece of data at specified path. This acts as an add / replace
+     * operation, which is to say that whole subtree will be replaced by
+     * specified path. Performing the following put operations:
      *
-     * If you need add or merge of current object with specified use {@link #merge(LogicalDatastoreType, Path, Object)}
+     * <pre>
+     * 1) container { list [ a ] }
+     * 2) container { list [ b ] }
+     * </pre>
      *
-     * @param store Logical data store which should be modified
-     * @param path Data object path
-     * @param data Data object to be written to specified path
-     * @throws IllegalStateException if the transaction is no longer {@link TransactionStatus#NEW}
+     * will result in the following data being present:
+     *
+     * <pre>
+     * container { list [ b ] }
+     * </pre>
+     *
+     *
+     * If you need to  make sure parent object exists,
+     * but you do not want modify its preexisting state (if
+     * node exists consider using
+     * {@link #merge(LogicalDatastoreType, Path, Object)}
+     *
+     * @param store
+     *            Logical data store which should be modified
+     * @param path
+     *            Data object path
+     * @param data
+     *            Data object to be written to specified path
+     * @throws IllegalStateException
+     *             if the transaction is no longer {@link TransactionStatus#NEW}
      */
     public void put(LogicalDatastoreType store, P path, D data);
 
@@ -45,24 +98,33 @@ public interface AsyncWriteTransaction<P extends Path<P>, D>  extends AsyncTrans
      * Store a piece of data at specified path. This acts as a merge operation,
      * which is to say that any pre-existing data which is not explicitly
      * overwritten will be preserved. This means that if you store a container,
-     * its child lists will be merged. Performing the following put operations:
+     * its child lists will be merged. Performing the following merge operations:
      *
+     * <pre>
      * 1) container { list [ a ] }
      * 2) container { list [ b ] }
+     * </pre>
      *
      * will result in the following data being present:
      *
+     * <pre>
      * container { list [ a, b ] }
+     * </pre>
      *
-     * This also means that storing the container will preserve any augmentations
-     * which have been attached to it.
+     * This also means that storing the container will preserve any
+     * augmentations which have been attached to it.
      *
-     * If you require an explicit replace operation, use {@link #put(LogicalDatastoreType, Path, Object)} instead.
+     * If you require an explicit replace operation, use
+     * {@link #put(LogicalDatastoreType, Path, Object)} instead.
      *
-     * @param store Logical data store which should be modified
-     * @param path Data object path
-     * @param data Data object to be written to specified path
-     * @throws IllegalStateException if the transaction is no longer {@link TransactionStatus#NEW}
+     * @param store
+     *            Logical data store which should be modified
+     * @param path
+     *            Data object path
+     * @param data
+     *            Data object to be written to specified path
+     * @throws IllegalStateException
+     *             if the transaction is no longer {@link TransactionStatus#NEW}
      */
     public void merge(LogicalDatastoreType store, P path, D data);
 
@@ -70,9 +132,12 @@ public interface AsyncWriteTransaction<P extends Path<P>, D>  extends AsyncTrans
      * Remove a piece of data from specified path. This operation does not fail
      * if the specified path does not exist.
      *
-     * @param store Logical data store which should be modified
-     * @param path Data object path
-     * @throws IllegalStateException if the transaction is no longer {@link TransactionStatus#NEW}
+     * @param store
+     *            Logical data store which should be modified
+     * @param path
+     *            Data object path
+     * @throws IllegalStateException
+     *             if the transaction is no longer {@link TransactionStatus#NEW}
      */
     public void delete(LogicalDatastoreType store, P path);
 
@@ -80,41 +145,93 @@ public interface AsyncWriteTransaction<P extends Path<P>, D>  extends AsyncTrans
      *
      * Closes transaction and resources allocated to the transaction.
      *
-     * This call does not change Transaction status. Client SHOULD
-     * explicitly {@link #commit()} or {@link #cancel()} transaction.
+     * This call does not change Transaction status. Client SHOULD explicitly
+     * {@link #commit()} or {@link #cancel()} transaction.
      *
-     * @throws IllegalStateException if the transaction has not been
-     *         updated by invoking {@link #commit()} or {@link #cancel()}.
+     * @throws IllegalStateException
+     *             if the transaction has not been updated by invoking
+     *             {@link #commit()} or {@link #cancel()}.
      */
     @Override
     public void close();
 
     /**
-     * Initiates a commit of modification. This call logically seals the
-     * transaction, preventing any the client from interacting with the
-     * data stores. The transaction is marked as {@link TransactionStatus#SUBMITED}
-     * and enqueued into the data store backed for processing.
+     * Submits transaction to be applied on logical
+     * <p>
+     * This call logically seals the transaction, which prevents any the client
+     * from from changing data tree using this transaction. Any subsequent calls
+     * to {@link #put(LogicalDatastoreType, Path, Object)}.
+     * {@link #merge(LogicalDatastoreType, Path, Object)} or
+     * {@link #delete(LogicalDatastoreType, Path)} will fail with
+     * {@link IllegalStateException}.
+     *
+     * The transaction is marked as {@link TransactionStatus#SUBMITED} and
+     * enqueued into the data store backed for processing.
      *
      * <p>
      * The successful commit changes the state of the system and may affect
-     * several components.
+     * several components The effects of successful commit of data depends on
+     * other data change listeners in the system and how they react to change.
+     * Please see YANG models for more details about effects which you may
+     * expect by modifying specific subtrees.
      *
+     * <h2>Failure scenarios</h2>
      * <p>
-     * The effects of successful commit of data are described in the
-     * specifications and YANG models describing the Provider components of
-     * controller. It is assumed that Consumer has an understanding of this
-     * changes.
+     * Transaction may fail because of multiple reasons, such as
+     * <ul>
+     * <li>Other transaction finished earlier and modified same node in
+     * non-compatible way. Returned future will fail with
+     * {@link OptimisticLockFailedException}. User may try to create new
+     * transaction and submit same modification in order to update data tree.</li>
+     * <li>Data change introduced by this transaction did not pass validation by
+     * commit handlers or data was incorrectly structured. Returned future will
+     * fail with {@link DataValidationFailedException}. User should not retry to
+     * create new transaction with same data, since it probably will fail again.
+     * </li>
+     * </ul>
      *
-     * @see DataCommitHandler for further information how two-phase commit is
-     *      processed.
-     * @param store Identifier of the store, where commit should occur.
+     *
+     * <h3>Examples of failure scenarios</h3>
+     *
+     * <h4>Conflict of two transactions</h4> This example illustrates two
+     * concurrent transactions, which introduces are derived from same initial
+     * state of data tree and proposes conflicting modifications.
+     *
+     * <pre>
+     * <code>
+     * txA = broker.newWriteTransaction(); // allocates new transaction, data tree is empty
+     * txB = broker.newWriteTransaction(); // allocates new transaction, data tree is empty
+     *
+     * txA.put(CONFIGURATION, PATH, A); // we writes to PATH value A
+     * txB.put(CONFIGURATION, PATH, B) // we writes to PATH value B
+     *
+     * ListenableFuture futureA = txA.commit(); // transaction A is sealed and commited
+     * ListenebleFuture futureB = txB.commit(); // transaction B is sealed and commited
+     * </code>
+     * </pre>
+     *
+     * Commit of transaction A will be processed asynchronously and data tree
+     * will be updated to contain value <code>A</code> for <code>PATH</code>
+     * Future will successfully complete once state is applied to data tree.
+     *
+     * Commit of Transaction B will fail, because previous transaction also
+     * modified path in concurrent way, so state introduced by transaction B
+     * will not be applied. Future will fail with
+     * {@link OptimisticLockFailedException} exception, which indicates
+     * to client that concurrent transaction prevented submitted
+     * transaction to be applied.
+     *
      * @return Result of the Commit, containing success information or list of
      *         encountered errors, if commit was not successful. The Future
      *         blocks until {@link TransactionStatus#COMMITED} is reached.
-     *         Future will fail with {@link TransactionCommitFailedException}
-     *         if Commit of this transaction failed.
+     *         Future will fail with {@link TransactionCommitFailedException} if
+     *         Commit of this transaction failed.
+     *         TODO: Usability: Consider change from ListenableFuture to
+     *  {@link com.google.common.util.concurrent.CheckedFuture}
+     * which will throw {@link TransactionCommitFailedException}.
      *
-     * @throws IllegalStateException if the transaction is not {@link TransactionStatus#NEW}
+     * @throws IllegalStateException
+     *             if the transaction is not {@link TransactionStatus#NEW}
      */
     public ListenableFuture<RpcResult<TransactionStatus>> commit();
 
