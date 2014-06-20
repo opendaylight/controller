0ba4da986712ca6a1bdce2ceb6b6ad1875416da6
Moiz Raja <moraja@cisco.com>
2014-06-20 16:42:19 -0700

    Ensure that modifications are tracked by ShardTransaction
    As modifications are made on a ShardTransaction they are applied to the transaction object that ShardTransaction wraps
    However the modifications also need to be tracked. This tracking would be used ultimately by the Shard when persisting data.
    
    Change-Id: I96061aa66a1cf3a9f225012bb22667504cfb3469
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ShardTransaction.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ShardTransaction.java
@@ -28,6 +28,12 @@ import org.opendaylight.controller.cluster.datastore.messages.ReadyTransaction;
 import org.opendaylight.controller.cluster.datastore.messages.ReadyTransactionReply;
 import org.opendaylight.controller.cluster.datastore.messages.WriteData;
 import org.opendaylight.controller.cluster.datastore.messages.WriteDataReply;
+import org.opendaylight.controller.cluster.datastore.modification.CompositeModification;
+import org.opendaylight.controller.cluster.datastore.modification.DeleteModification;
+import org.opendaylight.controller.cluster.datastore.modification.ImmutableCompositeModification;
+import org.opendaylight.controller.cluster.datastore.modification.MergeModification;
+import org.opendaylight.controller.cluster.datastore.modification.MutableCompositeModification;
+import org.opendaylight.controller.cluster.datastore.modification.WriteModification;
 import org.opendaylight.controller.sal.core.spi.data.DOMStoreReadWriteTransaction;
 import org.opendaylight.controller.sal.core.spi.data.DOMStoreThreePhaseCommitCohort;
 import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
@@ -37,26 +43,31 @@ import java.util.concurrent.ExecutionException;
 
 /**
  * The ShardTransaction Actor represents a remote transaction
- *
+ *<p>
  * The ShardTransaction Actor delegates all actions to DOMDataReadWriteTransaction
- *
+ *</p>
+ *<p>
  * Even though the DOMStore and the DOMStoreTransactionChain implement multiple types of transactions
  * the ShardTransaction Actor only works with read-write transactions. This is just to keep the logic simple. At this
  * time there are no known advantages for creating a read-only or write-only transaction which may change over time
  * at which point we can optimize things in the distributed store as well.
- *
- * Handles Messages
- * ----------------
- * {@link org.opendaylight.controller.cluster.datastore.messages.ReadData}
- * {@link org.opendaylight.controller.cluster.datastore.messages.WriteData}
- * {@link org.opendaylight.controller.cluster.datastore.messages.MergeData}
- * {@link org.opendaylight.controller.cluster.datastore.messages.DeleteData}
- * {@link org.opendaylight.controller.cluster.datastore.messages.ReadyTransaction}
- * {@link org.opendaylight.controller.cluster.datastore.messages.CloseTransaction}
+ *</p>
+ *<p>
+ * Handles Messages <br/>
+ * ---------------- <br/>
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.ReadData}
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.WriteData}
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.MergeData}
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.DeleteData}
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.ReadyTransaction}
+ * <li> {@link org.opendaylight.controller.cluster.datastore.messages.CloseTransaction}
+ * </p>
  */
 public class ShardTransaction extends UntypedActor {
 
   private final DOMStoreReadWriteTransaction transaction;
+
+  private final MutableCompositeModification modification = new MutableCompositeModification();
 
   private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
@@ -89,6 +100,9 @@ public class ShardTransaction extends UntypedActor {
       readyTransaction((ReadyTransaction) message);
     } else if(message instanceof CloseTransaction){
       closeTransaction((CloseTransaction) message);
+    } else if(message instanceof GetCompositedModification){
+      // This is here for testing only
+      getSender().tell(new GetCompositeModificationReply(new ImmutableCompositeModification(modification)), getSelf());
     }
   }
 
@@ -118,16 +132,19 @@ public class ShardTransaction extends UntypedActor {
 
 
   private void writeData(WriteData message){
+    modification.addModification(new WriteModification(message.getPath(), message.getData()));
     transaction.write(message.getPath(), message.getData());
     getSender().tell(new WriteDataReply(), getSelf());
   }
 
   private void mergeData(MergeData message){
+    modification.addModification(new MergeModification(message.getPath(), message.getData()));
     transaction.merge(message.getPath(), message.getData());
     getSender().tell(new MergeDataReply(), getSelf());
   }
 
   private void deleteData(DeleteData message){
+    modification.addModification(new DeleteModification(message.getPath()));
     transaction.delete(message.getPath());
     getSender().tell(new DeleteDataReply(), getSelf());
   }
@@ -142,5 +159,26 @@ public class ShardTransaction extends UntypedActor {
   private void closeTransaction(CloseTransaction message){
     transaction.close();
     getSender().tell(new CloseTransactionReply(), getSelf());
+  }
+
+
+  // These classes are in here for test purposes only
+
+  static class GetCompositedModification {
+
+  }
+
+  static class GetCompositeModificationReply {
+    private final CompositeModification modification;
+
+
+    GetCompositeModificationReply(CompositeModification modification) {
+      this.modification = modification;
+    }
+
+
+    public CompositeModification getModification() {
+      return modification;
+    }
   }
 }
