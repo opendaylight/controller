b39e407a918be3926532b16947da2c47e086783e
Moiz Raja <moraja@cisco.com>
2014-06-24 14:41:58 -0700

    Implement TransactionProxy#read
    - Move utility code into ActorContext
    - Create a reusable MockActorContext for tests
    - Update DistributedDataStore to use the common code
    - Update TransactionChainProxy to construct the appropriate TransactionProxy's
    
    Change-Id: I30386d2370f2683020e05d8a3842c0c63708df6f
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DistributedDataStore.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DistributedDataStore.java
@@ -9,14 +9,11 @@
 package org.opendaylight.controller.cluster.datastore;
 
 import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
-import akka.util.Timeout;
-import org.opendaylight.controller.cluster.datastore.messages.FindPrimary;
-import org.opendaylight.controller.cluster.datastore.messages.PrimaryFound;
 import org.opendaylight.controller.cluster.datastore.messages.RegisterChangeListener;
 import org.opendaylight.controller.cluster.datastore.messages.RegisterChangeListenerReply;
 import org.opendaylight.controller.cluster.datastore.messages.UpdateSchemaContext;
+import org.opendaylight.controller.cluster.datastore.utils.ActorContext;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeListener;
 import org.opendaylight.controller.sal.core.spi.data.DOMStore;
@@ -31,14 +28,6 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.concurrent.Await;
-import scala.concurrent.Future;
-import scala.concurrent.duration.Duration;
-import scala.concurrent.duration.FiniteDuration;
-
-import java.util.concurrent.TimeUnit;
-
-import static akka.pattern.Patterns.ask;
 
 /**
  *
@@ -48,93 +37,59 @@ public class DistributedDataStore implements DOMStore, SchemaContextListener {
     private static final Logger
         LOG = LoggerFactory.getLogger(DistributedDataStore.class);
 
-    final FiniteDuration ASK_DURATION = Duration.create(5, TimeUnit.SECONDS);
-    final Duration AWAIT_DURATION = Duration.create(5, TimeUnit.SECONDS);
 
-    private final ActorRef shardManager;
-    private final ActorSystem actorSystem;
     private final String type;
-
+    private final ActorContext actorContext;
 
     public DistributedDataStore(ActorSystem actorSystem, String type) {
-        this.actorSystem = actorSystem;
-        this.type = type;
-        shardManager = actorSystem.actorOf(ShardManager.props(type));
+        this(new ActorContext(actorSystem, actorSystem.actorOf(ShardManager.props(type))), type);
     }
+
+    public DistributedDataStore(ActorContext actorContext, String type) {
+        this.type = type;
+        this.actorContext = actorContext;
+    }
+
 
     @Override
     public <L extends AsyncDataChangeListener<InstanceIdentifier, NormalizedNode<?, ?>>> ListenerRegistration<L> registerChangeListener(
         InstanceIdentifier path, L listener,
         AsyncDataBroker.DataChangeScope scope) {
 
-        ActorSelection primary = findPrimary();
+        ActorRef dataChangeListenerActor = actorContext.getActorSystem().actorOf(DataChangeListener.props());
 
-        ActorRef dataChangeListenerActor = actorSystem.actorOf(DataChangeListener.props());
-
-        Object result =
-            getResult(primary, new RegisterChangeListener(path, dataChangeListenerActor.path(),
-                AsyncDataBroker.DataChangeScope.BASE), ASK_DURATION);
+        Object result = actorContext.executeShardOperation(Shard.DEFAULT_NAME,
+            new RegisterChangeListener(path, dataChangeListenerActor.path(),
+                AsyncDataBroker.DataChangeScope.BASE),
+            ActorContext.ASK_DURATION);
 
         RegisterChangeListenerReply reply = (RegisterChangeListenerReply) result;
         return new ListenerRegistrationProxy(reply.getListenerRegistrationPath());
     }
 
-    private ActorSelection findPrimary() {
-        Object result = getResult(shardManager, new FindPrimary(Shard.DEFAULT_NAME), ASK_DURATION);
-
-        if(result instanceof PrimaryFound){
-            PrimaryFound found = (PrimaryFound) result;
-            LOG.error("Primary found {}", found.getPrimaryPath());
-
-            return actorSystem.actorSelection(found.getPrimaryPath());
-        }
-        throw new RuntimeException("primary was not found");
-    }
-
-    private Object getResult(ActorRef actor, Object message, FiniteDuration duration){
-        Future<Object> future =
-            ask(actor, message, new Timeout(duration));
-
-        try {
-            return Await.result(future, AWAIT_DURATION);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Object getResult(ActorSelection actor, Object message, FiniteDuration duration){
-        Future<Object> future =
-            ask(actor, message, new Timeout(duration));
-
-        try {
-            return Await.result(future, AWAIT_DURATION);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
     @Override
     public DOMStoreTransactionChain createTransactionChain() {
-        return new TransactionChainProxy();
+        return new TransactionChainProxy(actorContext);
     }
 
     @Override
     public DOMStoreReadTransaction newReadOnlyTransaction() {
-        return new TransactionProxy();
+        return new TransactionProxy(actorContext, TransactionProxy.TransactionType.READ_ONLY);
     }
 
     @Override
     public DOMStoreWriteTransaction newWriteOnlyTransaction() {
-        return new TransactionProxy();
+        return new TransactionProxy(actorContext, TransactionProxy.TransactionType.WRITE_ONLY);
     }
 
     @Override
     public DOMStoreReadWriteTransaction newReadWriteTransaction() {
-        return new TransactionProxy();
+        return new TransactionProxy(actorContext, TransactionProxy.TransactionType.READ_WRITE);
     }
 
     @Override public void onGlobalContextUpdated(SchemaContext schemaContext) {
-        shardManager.tell(new UpdateSchemaContext(schemaContext), null);
+        actorContext.getShardManager().tell(new UpdateSchemaContext(schemaContext), null);
     }
 }
