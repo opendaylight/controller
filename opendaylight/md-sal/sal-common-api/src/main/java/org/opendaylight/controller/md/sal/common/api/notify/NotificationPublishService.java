e3bbf8a75734668c3e71608605ee6a66d48a8553
Tony Tkacik <ttkacik@cisco.com>
2014-06-25 10:26:15 +0200

    Bug 1235: Documented Notification services of MD-SAL
    Change-Id: I56c7b41b56eef8c92854e986a5f9f7af21ae1d09
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/notify/NotificationPublishService.java
+++ opendaylight/md-sal/sal-common-api/src/main/java/org/opendaylight/controller/md/sal/common/api/notify/NotificationPublishService.java
@@ -9,9 +9,40 @@ package org.opendaylight.controller.md.sal.common.api.notify;
 
 import java.util.concurrent.ExecutorService;
 
+/**
+ * Interface for publishing YANG-modeled notifications.
+ * <p>
+ * Users of this interface can publish any YANG-modeled notification which will be delivered
+ * to all subscribed listeners.
+ * <p>
+ * Publication of notifications is done by invoking
+ * {@link #publish(Object)} or
+ * {@link #publish(Object, ExecutorService)}.
+ * <p>
+ * The metadata required to deliver a notification to the correct listeners is extracted
+ * from the published notification.
+ *
+ * @param <N> the type of notifications
+ */
 public interface NotificationPublishService<N> {
 
+    /**
+     * Publishes a notification and notifies subscribed listeners. All listener notifications are
+     * done via a default executor.
+     * <p>
+     * <b>Note:</b> This call will block when the default executor is saturated
+     * and the notification queue for this executor is full.
+     *
+     * @param notification the notification to publish.
+     */
     void publish(N notification);
 
+    /**
+     * Publishes a notification and notifies subscribed listeners. All listener notifications are done
+     * via the provided executor.
+     *
+     * @param notification the notification to publish.
+     * @param executor the executor that will be used to deliver notifications to subscribed listeners.
+     */
     void publish(N notification,ExecutorService executor);
 }
