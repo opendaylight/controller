8a4a384176bc5dba87d12ce465cdf7fdaeac06d8
Basheeruddin Ahmed <syedbahm@cisco.com>
2014-06-24 19:05:54 -0700

    Distributed Datastore integration with config subsystem Updated with the usage of sal-dom-spi exposed Config Datastore and Operation Datastore services
    Change-Id: Ic8ebaf5a90239122f8112712101e3c3ecb09f2fc
    Signed-off-by: Basheeruddin Ahmed <syedbahm@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/distributed_datastore_provider/DistributedOperationalDataStoreProviderModule.java
@@ -0,0 +1,38 @@
+package org.opendaylight.controller.config.yang.config.distributed_datastore_provider;
+
+import akka.actor.ActorSystem;
+import com.typesafe.config.ConfigFactory;
+import org.opendaylight.controller.cluster.datastore.DistributedDataStore;
+
+public class DistributedOperationalDataStoreProviderModule extends org.opendaylight.controller.config.yang.config.distributed_datastore_provider.AbstractDistributedOperationalDataStoreProviderModule {
+    public DistributedOperationalDataStoreProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public DistributedOperationalDataStoreProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.distributed_datastore_provider.DistributedOperationalDataStoreProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+  @Override
+  public java.lang.AutoCloseable createInstance() {
+    final ActorSystem actorSystem = ActorSystem.create("opendaylight-cluster", ConfigFactory
+        .load().getConfig("ODLCluster"));
+    final DistributedDataStore operationalStore = new DistributedDataStore(actorSystem, "operational");
+
+    final class AutoCloseableDistributedDataStore implements AutoCloseable {
+
+      @Override
+      public void close() throws Exception {
+        actorSystem.shutdown();
+      }
+    }
+
+    return new AutoCloseableDistributedDataStore();
+  }
+
+}
