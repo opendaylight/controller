eaed8ebc2304559fb62a4604f3fbd30270a9de19
Moiz Raja <moraja@cisco.com>
2014-06-24 17:58:34 -0700

    Complete implementation of DataChangeListenerRegistration and related classes
    Change-Id: If1bb663145fd6dbbb2123d014c1ea91f65f8e028
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DataChangeListenerRegistration.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DataChangeListenerRegistration.java
@@ -11,39 +11,40 @@ package org.opendaylight.controller.cluster.datastore;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.japi.Creator;
-import org.opendaylight.controller.cluster.datastore.messages.CloseListenerRegistration;
-import org.opendaylight.controller.cluster.datastore.messages.CloseListenerRegistrationReply;
+import org.opendaylight.controller.cluster.datastore.messages.CloseDataChangeListenerRegistration;
+import org.opendaylight.controller.cluster.datastore.messages.CloseDataChangeListenerRegistrationReply;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeListener;
 import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
-public class ListenerRegistration extends UntypedActor{
+public class DataChangeListenerRegistration extends UntypedActor{
 
   private final org.opendaylight.yangtools.concepts.ListenerRegistration<AsyncDataChangeListener<InstanceIdentifier, NormalizedNode<?, ?>>> registration;
 
-  public ListenerRegistration(org.opendaylight.yangtools.concepts.ListenerRegistration<AsyncDataChangeListener<InstanceIdentifier, NormalizedNode<?, ?>>> registration) {
+  public DataChangeListenerRegistration(
+      org.opendaylight.yangtools.concepts.ListenerRegistration<AsyncDataChangeListener<InstanceIdentifier, NormalizedNode<?, ?>>> registration) {
     this.registration = registration;
   }
 
   @Override
   public void onReceive(Object message) throws Exception {
-    if(message instanceof CloseListenerRegistration){
-      closeListenerRegistration((CloseListenerRegistration) message);
+    if(message instanceof CloseDataChangeListenerRegistration){
+      closeListenerRegistration((CloseDataChangeListenerRegistration) message);
     }
   }
 
   public static Props props(final org.opendaylight.yangtools.concepts.ListenerRegistration<AsyncDataChangeListener<InstanceIdentifier, NormalizedNode<?, ?>>> registration){
-    return Props.create(new Creator<ListenerRegistration>(){
+    return Props.create(new Creator<DataChangeListenerRegistration>(){
 
       @Override
-      public ListenerRegistration create() throws Exception {
-        return new ListenerRegistration(registration);
+      public DataChangeListenerRegistration create() throws Exception {
+        return new DataChangeListenerRegistration(registration);
       }
     });
   }
 
-  private void closeListenerRegistration(CloseListenerRegistration message){
+  private void closeListenerRegistration(CloseDataChangeListenerRegistration message){
     registration.close();
-    getSender().tell(new CloseListenerRegistrationReply(), getSelf());
+    getSender().tell(new CloseDataChangeListenerRegistrationReply(), getSelf());
   }
 }
--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/CloseDataChangeListenerRegistration.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/CloseDataChangeListenerRegistration.java
@@ -8,5 +8,5 @@
 
 package org.opendaylight.controller.cluster.datastore.messages;
 
-public class CloseListenerRegistration {
+public class CloseDataChangeListenerRegistration {
 }
