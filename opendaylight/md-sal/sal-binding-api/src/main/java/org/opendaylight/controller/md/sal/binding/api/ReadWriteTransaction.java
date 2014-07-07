c0d0779ec154283525e42f655890e18f729c2f02
Tony Tkacik <ttkacik@cisco.com>
2014-07-07 14:59:08 +0200

    Added #putAndEnsureParents to ReadWriteTransaction.
    Change-Id: Id4a88b015e05b4717e9c393ddf821c7f93e7e541
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ReadWriteTransaction.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ReadWriteTransaction.java
@@ -8,6 +8,7 @@
 package org.opendaylight.controller.md.sal.binding.api;
 
 import org.opendaylight.controller.md.sal.common.api.data.AsyncReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
@@ -17,5 +18,80 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
  * For more information on usage and examples, please see the documentation in {@link AsyncReadWriteTransaction}.
  */
 public interface ReadWriteTransaction extends ReadTransaction, WriteTransaction, AsyncReadWriteTransaction<InstanceIdentifier<?>, DataObject> {
+
+    @Override
+    public void put(LogicalDatastoreType store, InstanceIdentifier<?> path,
+            DataObject data);
+
+    @Override
+    public void merge(LogicalDatastoreType store, InstanceIdentifier<?> path,
+            DataObject data);
+
+    /**
+     * Puts data and creates default parents if parent nodes are missing.
+     *
+     * <p>
+     * This method behaves as {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     * but also ensures parents if possible: allows for deep writes,
+     * and creates missing parents if required. Parents are created
+     * using {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     * method with only minimum necessary fields set (usually key only)
+     * which are derived from provided keys in {@link InstanceIdentifier}
+     * supplied in path.
+     * <p>
+     * This is equivalent of:
+     * <pre>
+     * Iterator&lt;PathArgument&gt; pathArg = path.getPathArguments().iterator();
+     * InstanceIdentifier current = InstanceIdentifier.builder.build();
+     * while(pathArg.hasNext()) {
+     *     current = current.child(pathArg.next);
+     *     if(false == tx.read(store,current).get().isPresent()) {
+     *          tx.merge(current,defaultValueFor(current));
+     *     }
+     * }
+     * </pre>
+     * But has potential to be more efficient, since MD-SAL implementations tend
+     * to optimise it.
+     *
+     *
+     * @param store
+     *            Logical data store which should be modified
+     * @param path
+     *            Data object path
+     * @param data
+     *            Data object to be written to specified path
+     * @throws IllegalStateException
+     */
+    public void putAndEnsureParents(LogicalDatastoreType store, InstanceIdentifier<?> path,
+            DataObject data);
+
+    /**
+     *
+     * <p>
+     * This method behaves as {@link #put(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     * but also ensures parents if possible: allows for deep writes,
+     * and creates missing parents if required. Parents are created
+     * using {@link #merge(LogicalDatastoreType, InstanceIdentifier, DataObject)}
+     * method with only minimum necessary fields set (usually key only)
+     * which are derived from provided keys in {@link InstanceIdentifier}
+     * supplied in path.
+     * <p>
+     * This is equivalent of:
+     * <pre>
+     * Iterator&lt;PathArgument&gt; pathArg = path.getPathArguments().iterator();
+     * InstanceIdentifier current = InstanceIdentifier.builder.build();
+     * while(pathArg.hasNext()) {
+     *     current = current.child(pathArg.next);
+     *     if(false == tx.read(store,current).get().isPresent()) {
+     *          tx.merge(current,defaultValueFor(current));
+     *     }
+     * }
+     * </pre>
+     * But has potential to be more efficient, since MD-SAL implementations tend
+     * to optimise it.
+     *
+     */
+    public void mergeAndEnsureParents(LogicalDatastoreType store, InstanceIdentifier<?> path,
+            DataObject data);
 
 }
--- opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingDataReadWriteTransactionImpl.java
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingDataReadWriteTransactionImpl.java
@@ -8,20 +8,39 @@
 package org.opendaylight.controller.md.sal.binding.impl;
 
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataReadWriteTransaction;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.ListenableFuture;
 
 class BindingDataReadWriteTransactionImpl extends
-        BindingDataWriteTransactionImpl<DOMDataReadWriteTransaction> implements ReadWriteTransaction {
+        AbstractReadWriteTransaction implements ReadWriteTransaction {
 
     protected BindingDataReadWriteTransactionImpl(final DOMDataReadWriteTransaction delegate,
             final BindingToNormalizedNodeCodec codec) {
         super(delegate, codec);
+    }
+
+    @Override
+    public void delete(final LogicalDatastoreType store, final InstanceIdentifier<?> path) {
+        doDelete(store, path);
+    }
+
+    @Override
+    public void merge(final LogicalDatastoreType store, final InstanceIdentifier<?> path,
+            final DataObject data) {
+        doMerge(store, path, data);
+    }
+
+    @Override
+    public void put(final LogicalDatastoreType store, final InstanceIdentifier<?> path,
+            final DataObject data) {
+        doPut(store, path, data);
     }
 
     @Override
@@ -35,5 +54,27 @@ class BindingDataReadWriteTransactionImpl extends
     public <T extends DataObject> ListenableFuture<Optional<T>> readChecked(
             final LogicalDatastoreType store, final InstanceIdentifier<T> path) {
         return (ListenableFuture) read(store, path);
+    }
+
+    @Override
+    public void putAndEnsureParents(final LogicalDatastoreType store,
+            final InstanceIdentifier<?> path, final DataObject data) {
+        doPutWithEnsureParents(store, path, data);
+    }
+
+    @Override
+    public void mergeAndEnsureParents(final LogicalDatastoreType store,
+            final InstanceIdentifier<?> path, final DataObject data) {
+        doMergeWithEnsureParents(store, path, data);
+    }
+
+    @Override
+    public boolean cancel() {
+        return doCancel();
+        }
+
+    @Override
+    public ListenableFuture<RpcResult<TransactionStatus>> commit() {
+        return doCommit();
     }
 }
