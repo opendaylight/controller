18c3a8b165bf4d69374b8c38fe24e45f048efa0e
Ed Warnicke <eaw@cisco.com>
2014-07-24 14:12:18 -0500

    DO NOT MERGE - start on wiring sal-rest-connector
    OK... this works entirely *except* for getting
    the opendaylight/distribution/pom.xml
    to copy the file into the configuration/initial
    directory.
    
    Please see integration/distributions/base/pom.xml
    for an example of how to do that (note: I'm tryign to
    here, but have yet to debug why its not working).
    
    Change-Id: If826bb7352101833acd288b81cbcf7cec2f28b43
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/api/RestConnector.java
@@ -0,0 +1,9 @@
+package org.opendaylight.controller.sal.rest.api;
+
+/*
+ * This is a simple dummy interface to allow us to create instances of RestconfProvider
+ * via the config subsystem.
+ */
+public interface RestConnector {
+
+}
