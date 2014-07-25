a0ef2c9bd9439367106421628c3649db1f65fc0f
Jozef Gloncak <jgloncak@cisco.com>
2014-07-25 09:15:27 +0200

    Pick up eventBus instance creation from ListenerAdapter
    instance of eventBus is now created outside ListenerAdapter and also
    outside Notificator class and is input parameter of
    Notificator.createListener().
    
    Change-Id: Ice3b279127fe243951a74c254b42074232ded690
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/streams/listeners/Notificator.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/streams/listeners/Notificator.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.controller.sal.streams.listeners;
 
+import com.google.common.eventbus.EventBus;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -75,8 +76,9 @@ public class Notificator {
      *            The name of the stream.
      * @return New {@link ListenerAdapter} listener from {@link YangInstanceIdentifier} path and stream name.
      */
-    public static ListenerAdapter createListener(YangInstanceIdentifier path, String streamName) {
-        ListenerAdapter listener = new ListenerAdapter(path, streamName);
+    public static ListenerAdapter createListener(YangInstanceIdentifier path, String streamName,
+            final EventBus eventBus) {
+        ListenerAdapter listener = new ListenerAdapter(path, streamName, eventBus);
         try {
             lock.lock();
             listenersByInstanceIdentifier.put(path, listener);
