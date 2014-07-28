d8559c028ee6ab8a81fc485063818ce4de09f1c7
Jamie Goodyear <jgoodyea@cisco.com>
2014-07-28 03:56:20 -0500

    DO NOT MERGE Updated featuretracker
    Started seperating the config-pusher-api
    which may be a bad move, and may want to delete.
    
    Interium push containing service trackers and initial feature filtering logic
    
    Rebased onto a place that has a working feature (odl-config-netty)
    that pulls in a config file (configuration/initial/00-netty.xml)
    
    Initial logic implemented, tested with odl config netty feature.
    
    Added naive file type check on config files to be processed
    
    Update MockBundleContext to handle configpusher service registration
    
    Added thread safe queue
    
    PatchSet13:
    	Added config files to:
    		odl-mdsal-broker
    		odl-netconf-client
    		odl-md-sal-toaster
    		odl-md-sal-restconf
    		odl-config-netconf-connector
    	Removed config files from opendaylight-karaf's configuration/initial
    
    	Changed custom.properties so we don't look for that directory anymore
    
    	Set up config file from features to go into configuration/karaf,
    	and made it configuratable with the config.configfile.directory
    	in opendaylight/commons/opendaylight/pom.xml (the parent pom).
    
    PatchSet14:
    	Fixed mdsal feature so it pulls in its dependencies
    	Fixed various issues in feature files.
    	Current state:
    		feature:install odl-config-startup
    		bundle:install -s mvn:org.opendaylight.controller/featuretracker
    		feature:install odl-config-netty
    		feature:install odl-mdsal-broker
    		feature:install odl-mdsal-restconf
    		feature:install odl-mdsal-all
    		feature:install odl-mdsal-toaster
    		feature:install odl-mdsal-netconf-connector
    
    		Will work if run in that order, except for odl-mdsal-netconf-connector
    		which is still broken.  It should *not* be necessary to run a stack
    		of features in order, you should just be able to run the feature
    		you need... but we aren't quite there yet.
    
    		TODO:
    			1) Make ConfigFeatureListener a
    			   ServiceTrackerCustomizer<ConfigPusher,ConfigPusher>
    			   so we can add it to the odl-config-startup feature.
    			2) Add mvn:org.opendaylight.controller/featuretracker
    			   to odl-config-startup feature (its defined in
    			   controller/features/config/src/main/resources/features.xml)
    			3) Get a queue in ConfigFeatureListener so it can simply
    				push the configs and have separate thread take them
    				and send them to ConfigPusher
    			4) Figure out how not to reload the config every time we
    			   see a new feature (ie, non-repetition)
    
    PatchSet16:
    	A small fix for unnecessary wrapping.
    
    Change-Id: I71f9a43785489bfb4ce52cf9f98227ac31103428
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: jgoodyear <jgoodyea@cisco.com>

---
+++ features/config-test/src/test/java/org/opendaylight/controller/config/ConfigFeatureSuite.java
@@ -0,0 +1,11 @@
+package org.opendaylight.controller.config;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ConfigFeatureTest.class })
+public class ConfigFeatureSuite {
+
+}
