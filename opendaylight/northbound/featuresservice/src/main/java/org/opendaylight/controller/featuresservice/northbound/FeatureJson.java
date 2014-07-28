fe65bc33a3290bba3fff66621c33f0a9fb199310
Jonathan Fok kan <jfokkan@inocybe.com>
2014-07-28 10:32:04 -0400

    Added northbound api for features service(rebased)
    Change-Id: I6f321717c173ab4d8ca62a623d16326c2dd02bf1
    Signed-off-by: Jonathan Fok Kan <jfokkan@inocybe.com>

---
+++ opendaylight/northbound/featuresservice/src/main/java/org/opendaylight/controller/featuresservice/northbound/FeatureJson.java
@@ -0,0 +1,81 @@
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+package org.opendaylight.controller.featuresservice.northbound;
+
+public class FeatureJson {
+    String id;
+    String name;
+    String description;
+    String details;
+    String version;
+    Boolean hasVersion;
+    String resolver;
+    Boolean isInstall;
+
+    public FeatureJson(String id, String name, String description, String details,
+            String version, Boolean hasVersion, String resolver,
+            Boolean isInstall) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.details = details;
+        this.version = version;
+        this.hasVersion = hasVersion;
+        this.resolver = resolver;
+        this.isInstall = isInstall;
+    }
+    public Boolean getIsInstall() {
+        return isInstall;
+    }
+    public void setIsInstall(Boolean isInstall) {
+        this.isInstall = isInstall;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    public Boolean getHasVersion() {
+        return hasVersion;
+    }
+    public void setHasVersion(Boolean hasVersion) {
+        this.hasVersion = hasVersion;
+    }
+    public String getResolver() {
+        return resolver;
+    }
+    public void setResolver(String resolver) {
+        this.resolver = resolver;
+    }
+}
