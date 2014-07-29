089947a89b4051e868c89d1c8150cc7310554173
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2014-07-29 14:35:32 -0400

    Generated a skeleton for DLUX bundles (do not merge)
    Change-Id: Id70ed9ee2ec1184b874f7cec5ce9d9da306dd725
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ opendaylight/dlux/core/src/main/java/org/opendaylight/controller/dlux/core/Activator.java
@@ -0,0 +1,23 @@
+package org.opendaylight.controller.dlux.core;
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.http.HttpService;
+
+public class Activator implements BundleActivator {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+    }
+}
