57015d13f53dc10a88628824e43fbf0728b4d7ce
Jonathan Fok kan <jfokkan@inocybe.com>
2014-07-29 12:42:08 -0400

    Added a version REST api for Karaf
    Provides a rest api for getting information about the controller
    The endpoints are:
    :8181/services/getversion
    
    Change-Id: Icdab5f5583c2c7a1db8f5754cbbbba196af33426
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
    Signed-off-by: Jonathan Fok Kan <jfokkan@inocybe.com>

---
+++ opendaylight/northbound/version-api/src/main/java/org/opendaylight/controller/versionapi/Activator.java
@@ -0,0 +1,42 @@
+package org.opendaylight.controller.versionapi;
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+import java.util.Hashtable;
+import org.opendaylight.controller.versionapi.northbound.ConfigUpdater;
+import org.opendaylight.controller.versionapi.northbound.VersionapiNorthbound;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ManagedService;
+
+public class Activator implements BundleActivator {
+   private ServiceRegistration registration;
+   // version.release.cfg in karaf /etc
+   private static final String CONFIG_PID = "version.release";
+
+   @Override
+   public void start(BundleContext context) throws Exception {
+      Hashtable<String, Object> properties = new Hashtable<String, Object>();
+      properties.put(Constants.SERVICE_PID, CONFIG_PID);
+
+      ConfigUpdater cu = new ConfigUpdater();
+      registration = context.registerService(
+      ManagedService.class.getName(), cu, properties);
+
+      VersionapiNorthbound cnb = new VersionapiNorthbound();
+      cnb.setConfigUpdater(cu);
+      registration = context.registerService(
+      VersionapiNorthbound.class.getName(), cnb, null);
+   }
+
+   @Override
+   public void stop(BundleContext context) throws Exception {
+      registration.unregister();
+   }
+}
---
+++ opendaylight/northbound/version-api/src/main/java/org/opendaylight/controller/versionapi/northbound/Version.java
@@ -0,0 +1,55 @@
+package org.opendaylight.controller.versionapi.northbound;
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+public class Version {
+    private String name;
+    private String version;
+    private String timestamp;
+    private String stream;
+
+    public Version(){}
+
+    public Version(String name, String version, String timestamp) {
+        super();
+        this.name = name;
+        this.version = version;
+        this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getStream() {
+        return stream;
+    }
+
+    public void setStream(String stream) {
+        this.stream = stream;
+    }
+}
