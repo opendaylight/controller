2a4a337127d4e4664a244bef2a840232d119ddc0
Jonathan Fok kan <jfokkan@inocybe.com>
2014-07-29 12:27:52 -0400

    Added northbound api for features service
    Provides a rest api for feature:install, feature:uninstall and feature:list
    
    The endpoints are:
    :8181/services/features/list
    :8181/services/features/installed
    :8181/services/features/install
    :8181/services/features/uninstall
    
    Change-Id: I6f321717c173ab4d8ca62a623d16326c2dd02bf1
    Signed-off-by: Jonathan Fok Kan <jfokkan@inocybe.com>

---
+++ opendaylight/northbound/featuresservice/src/main/java/org/opendaylight/controller/featuresservice/northbound/FeatureName.java
@@ -0,0 +1,31 @@
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+package org.opendaylight.controller.featuresservice.northbound;
+
+public class FeatureName {
+    String name;
+
+    public FeatureName() {
+    }
+
+    public FeatureName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
