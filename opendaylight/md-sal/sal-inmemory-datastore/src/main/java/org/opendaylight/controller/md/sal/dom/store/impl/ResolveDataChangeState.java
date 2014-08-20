6507fe93329e5f80e94e51097e63f6902f4fb41c
Robert Varga <rovarga@cisco.com>
2014-08-20 13:05:51 +0200

    BUG-1493: activate recursion elision
    Previous patch prepared the infrastructure for elision, but it did not
    actually activate it. This patch flips the switch on.
    
    Change-Id: Ic02af3ccc7f5ee5f8ae4d0617562b8fdffc771c6
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-inmemory-datastore/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/ResolveDataChangeState.java
+++ opendaylight/md-sal/sal-inmemory-datastore/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/ResolveDataChangeState.java
@@ -135,8 +135,7 @@ final class ResolveDataChangeState {
             return true;
         }
 
-        // FIXME: do we need anything else? If not, flip this to 'false'
-        return true;
+        return false;
     }
 
     /**
