88352b657e6495919bc23fec794e207cb8d6b366
Moiz Raja <moraja@cisco.com>
2014-08-22 11:27:52 -0700

    Utilize CompositeNodeCompatibility in NormalizedNodeToNodeCodec
    This switches the serialization/deserialization mechanism from the
    old way to this new way
    
    Change-Id: I6a217a7f2540179351ed6d8aa50d5ceca884226d
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/NormalizedNodeToNodeCodec.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/NormalizedNodeToNodeCodec.java
@@ -10,11 +10,15 @@
 
 package org.opendaylight.controller.cluster.datastore.node;
 
-import org.opendaylight.controller.protobuff.messages.common.NormalizedNodeMessages;
+import com.google.common.base.Optional;
 import org.opendaylight.controller.cluster.datastore.node.utils.PathUtils;
-import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizationException;
+import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizer;
+import org.opendaylight.controller.protobuff.messages.common.NormalizedNodeMessages;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,45 +33,71 @@ public class NormalizedNodeToNodeCodec {
     }
 
     public NormalizedNodeMessages.Container encode(YangInstanceIdentifier id, NormalizedNode node){
+
         String parentPath = "";
 
         if(id != null){
             parentPath = PathUtils.getParentPath(id.toString());
         }
 
+        NormalizedNodeMessages.Container.Builder containerBuilder =
+            NormalizedNodeMessages.Container.newBuilder();
 
-        NormalizedNodeToProtocolBufferNode encoder = new NormalizedNodeToProtocolBufferNode();
-        encoder.encode(parentPath, node);
+        NormalizedNodeMessages.Container container;
 
-        return encoder.getContainer();
+        if (node != null) {
+
+            NormalizedNodeMessages.Node compositeNodeCompatible =
+                CompositeNodeCompatibility.toNode(node);
+
+            container =
+                containerBuilder.setParentPath(parentPath).setNormalizedNode(
+                    compositeNodeCompatible).build();
+        } else {
+            //this can happen when an attempt was made to read from datastore and normalized node was null.
+            container = containerBuilder.setParentPath(parentPath).build();
+        }
+
+        return container;
 
 
     }
 
     public NormalizedNode<?,?> decode(YangInstanceIdentifier id, NormalizedNodeMessages.Node node){
-            NodeToNormalizedNodeBuilder currentOp = NodeToNormalizedNodeBuilder.from(ctx);
 
-            for(YangInstanceIdentifier.PathArgument pathArgument : id.getPathArguments()){
-                currentOp = currentOp.getChild(pathArgument);
+
+        DataNormalizer normalizer = new DataNormalizer(ctx);
+
+        try {
+
+            // Note : To get to the schemaNode it appears that you need a new
+            // YangInstanceIdentifier instead of the legacy one. This may be
+            // a bug in DataNormalizationOperation
+
+            Optional<DataSchemaNode> optionalSchemaNode =
+                normalizer.getOperation(id).getDataSchemaNode();
+
+            DataSchemaNode schemaNode = null;
+
+            if(optionalSchemaNode.isPresent()) {
+                schemaNode = optionalSchemaNode.get();
             }
 
-            QName nodeType = null;
+            if(node.getChildCount() == 0){
+                return CompositeNodeCompatibility.toSimpleNormalizedNode(node, schemaNode);
+            }
 
-            if(id.getPath().size() < 1){
-                nodeType = null;
-            } else {
-                final YangInstanceIdentifier.PathArgument pathArgument = id.getPath().get(id.getPath().size() - 1);
-                if(pathArgument instanceof YangInstanceIdentifier.AugmentationIdentifier){
-                    nodeType = null;
-                } else {
-                    nodeType = pathArgument.getNodeType();
-                }
-            }
-            if((node != null)&& (!node.getType().isEmpty())){
-               return currentOp.normalize(nodeType, node);
-            } else{
-              return null;
-            }
+            CompositeNode compositeNode = CompositeNodeCompatibility
+                .toComposite(node, schemaNode);
+
+            YangInstanceIdentifier idLegacy = normalizer.toLegacy(id);
+
+            return normalizer.toNormalized(idLegacy, compositeNode).getValue();
+        } catch (DataNormalizationException e) {
+            return null;
+        }
+
+
     }
 
 
