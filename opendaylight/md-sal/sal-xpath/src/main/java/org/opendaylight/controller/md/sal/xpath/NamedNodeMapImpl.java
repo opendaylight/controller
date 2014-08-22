93bcf77626c3d9af7f4b053b57b26d7454357f6a
Neel <neel250294@gmail.com>
2014-08-22 21:38:20 +0530

    [DO NOT MERGE] Initial code prototype for XPath query capabilities on MD-SAL
    Change-Id: I485810dc49504e9cad9cd50c34f552c22579cf11
    Signed-off-by: Neel <neel250294@gmail.com>

---
+++ opendaylight/md-sal/sal-xpath/src/main/java/org/opendaylight/controller/md/sal/xpath/NamedNodeMapImpl.java
@@ -0,0 +1,67 @@
+/*
+ * Author : Neel Bommisetty
+ * Email : neel250294@gmail.com
+
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.xpath;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class NamedNodeMapImpl implements NamedNodeMap {
+
+    @Override
+    public Node getNamedItem(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node setNamedItem(Node arg) throws DOMException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node removeNamedItem(String name) throws DOMException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node item(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Node getNamedItemNS(String namespaceURI, String localName)
+            throws DOMException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node setNamedItemNS(Node arg) throws DOMException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node removeNamedItemNS(String namespaceURI, String localName)
+            throws DOMException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ opendaylight/md-sal/sal-xpath/src/main/java/org/opendaylight/controller/md/sal/xpath/NodeBuilderText.java
@@ -0,0 +1,98 @@
+/*
+ * Author : Neel Bommisetty
+ * Email : neel250294@gmail.com
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.xpath;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class NodeBuilderText extends ThrowExceptionText {
+    Node parent;
+    Node nextSibling;
+    String nodeValue;
+
+    public NodeBuilderText(String Nodevalue, Element parentNode,
+            Node nextsibling) {
+
+        nodeValue = Nodevalue;
+        parent = parentNode;
+        nextSibling = nextsibling;
+    }
+
+    @Override
+    public short getNodeType() {
+        return Node.TEXT_NODE;
+
+    }
+
+    @Override
+    public boolean hasAttributes() {
+        return false;
+    }
+
+    @Override
+    public String getNodeValue() {
+        return nodeValue;
+    }
+
+    @Override
+    public String getNamespaceURI() {
+        return null;
+
+    }
+
+    @Override
+    public boolean hasChildNodes() {
+        return false;
+    }
+
+    @Override
+    public NamedNodeMap getAttributes() {
+        return null;
+    }
+
+    @Override
+    public String getNodeName() {
+
+        return "#text";
+    }
+
+    @Override
+    public Node getParentNode() {
+        return parent;
+    }
+
+    @Override
+    public String getLocalName() {
+        return "#text";
+    }
+
+    @Override
+    public Node getNextSibling() {
+        return nextSibling;
+    }
+
+    @Override
+    public Node getFirstChild() {
+
+        return null;
+    }
+
+    @Override
+    public Node getLastChild() {
+
+        return null;
+    }
+
+    @Override
+    public String getTextContent() {
+        return nodeValue;
+
+    }
+}
