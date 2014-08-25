afed46e16505c95eb0c6fc4d236aeeb0c25ab968
tpantelis <tpanteli@brocade.com>
2014-08-24 20:29:58 -0400

    Bug 1637: Change Rpc actor calls to async
    Changed RemoteRpcImplementation and RpcBroker to use async calls and
    modified/created unit tests.
    
    Added an RpcErrorsException to transfer RpcError info on failure. This
    is now used in lieu of ErrorResponse to communicate failures.
    
    Change-Id: Ib0bbda1867caff9b6c584b9b8a9b336f8acf53d7
    Signed-off-by: tpantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
@@ -1,9 +1,13 @@
 package org.opendaylight.controller.remote.rpc;
 
+import static akka.pattern.Patterns.ask;
 import akka.actor.ActorRef;
-import com.google.common.util.concurrent.Futures;
+import akka.dispatch.OnComplete;
+import akka.util.Timeout;
+
 import com.google.common.util.concurrent.ListenableFuture;
-import org.opendaylight.controller.remote.rpc.messages.ErrorResponse;
+import com.google.common.util.concurrent.SettableFuture;
+
 import org.opendaylight.controller.remote.rpc.messages.InvokeRpc;
 import org.opendaylight.controller.remote.rpc.messages.RpcResponse;
 import org.opendaylight.controller.remote.rpc.utils.ActorUtil;
@@ -13,73 +17,82 @@ import org.opendaylight.controller.sal.core.api.RpcImplementation;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
 import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import scala.concurrent.ExecutionContext;
+
 import java.util.Collections;
 import java.util.Set;
 
-public class RemoteRpcImplementation implements RpcImplementation,
-    RoutedRpcDefaultImplementation {
-  private static final Logger LOG = LoggerFactory.getLogger(RemoteRpcImplementation.class);
-  private ActorRef rpcBroker;
-  private SchemaContext schemaContext;
+public class RemoteRpcImplementation implements RpcImplementation, RoutedRpcDefaultImplementation {
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteRpcImplementation.class);
+    private final ActorRef rpcBroker;
+    private final SchemaContext schemaContext;
 
-  public RemoteRpcImplementation(ActorRef rpcBroker, SchemaContext schemaContext) {
-    this.rpcBroker = rpcBroker;
-    this.schemaContext = schemaContext;
-  }
-
-  @Override
-  public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc, YangInstanceIdentifier identifier, CompositeNode input) {
-    InvokeRpc rpcMsg = new InvokeRpc(rpc, identifier, input);
-
-    return executeMsg(rpcMsg);
-  }
-
-  @Override
-  public Set<QName> getSupportedRpcs() {
-    // TODO : check if we need to get this from routing registry
-    return Collections.emptySet();
-  }
-
-  @Override
-  public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc, CompositeNode input) {
-    InvokeRpc rpcMsg = new InvokeRpc(rpc, null, input);
-    return executeMsg(rpcMsg);
-  }
-
-  private ListenableFuture<RpcResult<CompositeNode>> executeMsg(Object rpcMsg) {
-    ListenableFuture<RpcResult<CompositeNode>> listenableFuture = null;
-
-    try {
-      Object response = ActorUtil.executeOperation(rpcBroker, rpcMsg, ActorUtil.ASK_DURATION, ActorUtil.AWAIT_DURATION);
-      if(response instanceof RpcResponse) {
-
-        RpcResponse rpcResponse = (RpcResponse) response;
-        CompositeNode result = XmlUtils.xmlToCompositeNode(rpcResponse.getResultCompositeNode());
-        listenableFuture = Futures.immediateFuture(RpcResultBuilder.success(result).build());
-
-      } else if(response instanceof ErrorResponse) {
-
-        ErrorResponse errorResponse = (ErrorResponse) response;
-        Exception e = errorResponse.getException();
-        final RpcResultBuilder<CompositeNode> failed = RpcResultBuilder.failed();
-        failed.withError(null, null, e.getMessage(), null, null, e.getCause());
-        listenableFuture = Futures.immediateFuture(failed.build());
-
-      }
-    } catch (Exception e) {
-      LOG.error("Error occurred while invoking RPC actor {}", e);
-
-      final RpcResultBuilder<CompositeNode> failed = RpcResultBuilder.failed();
-      failed.withError(null, null, e.getMessage(), null, null, e.getCause());
-      listenableFuture = Futures.immediateFuture(failed.build());
+    public RemoteRpcImplementation(ActorRef rpcBroker, SchemaContext schemaContext) {
+        this.rpcBroker = rpcBroker;
+        this.schemaContext = schemaContext;
     }
 
-    return listenableFuture;
-  }
+    @Override
+    public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc,
+            YangInstanceIdentifier identifier, CompositeNode input) {
+        InvokeRpc rpcMsg = new InvokeRpc(rpc, identifier, input);
+
+        return executeMsg(rpcMsg);
+    }
+
+    @Override
+    public Set<QName> getSupportedRpcs() {
+        // TODO : check if we need to get this from routing registry
+        return Collections.emptySet();
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc, CompositeNode input) {
+        InvokeRpc rpcMsg = new InvokeRpc(rpc, null, input);
+        return executeMsg(rpcMsg);
+    }
+
+    private ListenableFuture<RpcResult<CompositeNode>> executeMsg(InvokeRpc rpcMsg) {
+
+        final SettableFuture<RpcResult<CompositeNode>> listenableFuture = SettableFuture.create();
+
+        scala.concurrent.Future<Object> future = ask(rpcBroker, rpcMsg,
+                new Timeout(ActorUtil.ASK_DURATION));
+
+        OnComplete<Object> onComplete = new OnComplete<Object>() {
+            @Override
+            public void onComplete(Throwable failure, Object reply) throws Throwable {
+                if(failure != null) {
+                    LOG.error("InvokeRpc failed", failure);
+
+                    RpcResult<CompositeNode> rpcResult;
+                    if(failure instanceof RpcErrorsException) {
+                        rpcResult = RpcResultBuilder.<CompositeNode>failed().withRpcErrors(
+                                ((RpcErrorsException)failure).getRpcErrors()).build();
+                    } else {
+                        rpcResult = RpcResultBuilder.<CompositeNode>failed().withError(
+                                ErrorType.RPC, failure.getMessage(), failure).build();
+                    }
+
+                    listenableFuture.set(rpcResult);
+                    return;
+                }
+
+                RpcResponse rpcReply = (RpcResponse)reply;
+                CompositeNode result = XmlUtils.xmlToCompositeNode(rpcReply.getResultCompositeNode());
+                listenableFuture.set(RpcResultBuilder.success(result).build());
+            }
+        };
+
+        future.onComplete(onComplete, ExecutionContext.Implicits$.MODULE$.global());
+
+        return listenableFuture;
+    }
 }
---
+++ opendaylight/md-sal/sal-remoterpc-connector/src/test/java/org/opendaylight/controller/remote/rpc/AbstractRpcTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.remote.rpc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.mockito.Mockito;
+import org.opendaylight.controller.sal.core.api.Broker;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
+import org.opendaylight.yangtools.yang.data.api.Node;
+import org.opendaylight.yangtools.yang.data.impl.ImmutableCompositeNode;
+import org.opendaylight.yangtools.yang.data.impl.util.CompositeNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.JavaTestKit;
+
+import com.google.common.collect.ImmutableList;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * Base class for RPC tests.
+ *
+ * @author Thomas Pantelis
+ */
+public class AbstractRpcTest {
+    static final String TEST_REV = "2014-08-28";
+    static final String TEST_NS = "urn:test";
+    static final URI TEST_URI = URI.create(TEST_NS);
+    static final QName TEST_RPC = QName.create(TEST_NS, TEST_REV, "test-rpc");
+    static final QName TEST_RPC_INPUT = QName.create(TEST_NS, TEST_REV, "input");
+    static final QName TEST_RPC_INPUT_DATA = QName.create(TEST_NS, TEST_REV, "input-data");
+    static final QName TEST_RPC_OUTPUT = QName.create(TEST_NS, TEST_REV, "output");
+    static final QName TEST_RPC_OUTPUT_DATA = new QName(TEST_URI, "output-data");
+
+    static ActorSystem node1;
+    static ActorSystem node2;
+
+    protected ActorRef rpcBroker1;
+    protected JavaTestKit probeReg1;
+    protected ActorRef rpcBroker2;
+    protected JavaTestKit probeReg2;
+    protected Broker.ProviderSession brokerSession;
+    protected SchemaContext schemaContext;
+
+    @BeforeClass
+    public static void setup() throws InterruptedException {
+        node1 = ActorSystem.create("opendaylight-rpc", ConfigFactory.load().getConfig("memberA"));
+        node2 = ActorSystem.create("opendaylight-rpc", ConfigFactory.load().getConfig("memberB"));
+    }
+
+    @AfterClass
+    public static void teardown() {
+        JavaTestKit.shutdownActorSystem(node1);
+        JavaTestKit.shutdownActorSystem(node2);
+        node1 = null;
+        node2 = null;
+    }
+
+    @Before
+    public void setUp() {
+        schemaContext = new YangParserImpl().parseFiles(Arrays.asList(
+                new File(RpcBrokerTest.class.getResource("/test-rpc.yang").getPath())));
+
+        brokerSession = Mockito.mock(Broker.ProviderSession.class);
+        probeReg1 = new JavaTestKit(node1);
+        rpcBroker1 = node1.actorOf(RpcBroker.props(brokerSession, probeReg1.getRef(), schemaContext));
+        probeReg2 = new JavaTestKit(node2);
+        rpcBroker2 = node2.actorOf(RpcBroker.props(brokerSession, probeReg2.getRef(), schemaContext));
+
+    }
+
+    static void assertRpcErrorEquals(RpcError rpcError, ErrorSeverity severity,
+            ErrorType errorType, String tag, String message, String applicationTag, String info,
+            String causeMsg) {
+        assertEquals("getSeverity", severity, rpcError.getSeverity());
+        assertEquals("getErrorType", errorType, rpcError.getErrorType());
+        assertEquals("getTag", tag, rpcError.getTag());
+        assertTrue("getMessage contains " + message, rpcError.getMessage().contains(message));
+        assertEquals("getApplicationTag", applicationTag, rpcError.getApplicationTag());
+        assertEquals("getInfo", info, rpcError.getInfo());
+
+        if(causeMsg == null) {
+            assertNull("Unexpected cause " + rpcError.getCause(), rpcError.getCause());
+        } else {
+            assertEquals("Cause message", causeMsg, rpcError.getCause().getMessage());
+        }
+    }
+
+    static void assertCompositeNodeEquals(CompositeNode exp, CompositeNode actual) {
+        assertEquals("NodeType getNamespace", exp.getNodeType().getNamespace(),
+                actual.getNodeType().getNamespace());
+        assertEquals("NodeType getLocalName", exp.getNodeType().getLocalName(),
+                actual.getNodeType().getLocalName());
+        for(Node<?> child: exp.getValue()) {
+            List<Node<?>> c = actual.get(child.getNodeType());
+            assertNotNull("Missing expected child " + child.getNodeType(), c);
+            if(child instanceof CompositeNode) {
+                assertCompositeNodeEquals((CompositeNode) child, (CompositeNode)c.get(0));
+            } else {
+                assertEquals("Value for Node " + child.getNodeType(), child.getValue(),
+                        c.get(0).getValue());
+            }
+        }
+    }
+
+    static CompositeNode makeRPCInput(String data) {
+        CompositeNodeBuilder<ImmutableCompositeNode> builder = ImmutableCompositeNode.builder()
+                .setQName(TEST_RPC_INPUT).addLeaf(TEST_RPC_INPUT_DATA, data);
+        return ImmutableCompositeNode.create(
+                TEST_RPC, ImmutableList.<Node<?>>of(builder.toInstance()));
+    }
+
+    static CompositeNode makeRPCOutput(String data) {
+        CompositeNodeBuilder<ImmutableCompositeNode> builder = ImmutableCompositeNode.builder()
+                .setQName(TEST_RPC_OUTPUT).addLeaf(TEST_RPC_OUTPUT_DATA, data);
+        return ImmutableCompositeNode.create(
+                TEST_RPC, ImmutableList.<Node<?>>of(builder.toInstance()));
+    }
+
+    static void assertFailedRpcResult(RpcResult<CompositeNode> rpcResult, ErrorSeverity severity,
+            ErrorType errorType, String tag, String message, String applicationTag, String info,
+            String causeMsg) {
+
+        assertNotNull("RpcResult was null", rpcResult);
+        assertEquals("isSuccessful", false, rpcResult.isSuccessful());
+        Collection<RpcError> rpcErrors = rpcResult.getErrors();
+        assertEquals("RpcErrors count", 1, rpcErrors.size());
+        assertRpcErrorEquals(rpcErrors.iterator().next(), severity, errorType, tag, message,
+                applicationTag, info, causeMsg);
+    }
+
+    static void assertSuccessfulRpcResult(RpcResult<CompositeNode> rpcResult,
+            CompositeNode expOutput) {
+
+        assertNotNull("RpcResult was null", rpcResult);
+        assertEquals("isSuccessful", true, rpcResult.isSuccessful());
+        assertCompositeNodeEquals(expOutput, rpcResult.getResult());
+    }
+
+    static class TestException extends Exception {
+        private static final long serialVersionUID = 1L;
+
+        static final String MESSAGE = "mock error";
+
+        TestException() {
+            super(MESSAGE);
+        }
+    }
+}
