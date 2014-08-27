bb0a35749d5e61c186d89d93467cd58eff7fb1cb
Ed Warnicke <eaw@cisco.com>
2014-08-27 16:09:18 -0500

    Initial clustering feature
    Besides the clustering feature changes a couple of other changes
    had to be made to the distributed data store and remote rpc modules
    to make the solution work with karaf
    
    1. The akka.conf in sal-clustering-config has now been changed so that
    it works in a single node cluster out of the box. Specifically the hostnames
    have been set to 127.0.0.1 and the member role has been set to member-1
    
    2. The distributed-data-store and the remote-rpcconnector now load akka
    configuration from the configuration/initial folder. The positive thing about
    this is that even in a multi-node cluster one does not need to pass the location
    of akka.conf on the command line. The negative thing about this is that you
    cannot change the location from which the akka configuration can be picked up.
    Though this should be ok because we specifically pick modules.conf and
    module-shards.conf from the configuration/initial folder as well.
    
    NOTE : Just with these changes the build will break because the sanity test looks
    for sal-remoterpc-connector and sal-distributed-datastore in the regular controller
    distribution and does not find it. This is therefore still a WIP
    
    Change-Id: I1701ae8e9508c82779483963fb73b6af91095d7d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: Moiz Raja <moraja@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ActorSystemFactory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ActorSystemFactory.java
@@ -10,24 +10,55 @@ package org.opendaylight.controller.cluster.datastore;
 
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import com.google.common.base.Function;
+import akka.osgi.BundleDelegatingClassLoader;
+import com.google.common.base.Preconditions;
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.osgi.framework.BundleContext;
 
-import javax.annotation.Nullable;
+import java.io.File;
 
 public class ActorSystemFactory {
-    private static final ActorSystem actorSystem = (new Function<Void, ActorSystem>(){
 
-        @Nullable @Override public ActorSystem apply(@Nullable Void aVoid) {
-                ActorSystem system =
-                    ActorSystem.create("opendaylight-cluster-data", ConfigFactory
-                        .load().getConfig("odl-cluster-data"));
-                system.actorOf(Props.create(TerminationMonitor.class), "termination-monitor");
-                return system;
-        }
-    }).apply(null);
+    public static final String AKKA_CONF_PATH = "./configuration/initial/akka.conf";
+    public static final String ACTOR_SYSTEM_NAME = "opendaylight-cluster-data";
+    public static final String CONFIGURATION_NAME = "odl-cluster-data";
+
+    private static volatile ActorSystem actorSystem = null;
 
     public static final ActorSystem getInstance(){
         return actorSystem;
+    }
+
+    /**
+     * This method should be called only once during initialization
+     *
+     * @param bundleContext
+     */
+    public static final ActorSystem createInstance(final BundleContext bundleContext) {
+        if(actorSystem == null) {
+            // Create an OSGi bundle classloader for actor system
+            BundleDelegatingClassLoader classLoader = new BundleDelegatingClassLoader(bundleContext.getBundle(),
+                Thread.currentThread().getContextClassLoader());
+            synchronized (ActorSystemFactory.class) {
+                // Double check
+
+                if (actorSystem == null) {
+                    ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME,
+                        ConfigFactory.load(readAkkaConfiguration()).getConfig(CONFIGURATION_NAME), classLoader);
+                    system.actorOf(Props.create(TerminationMonitor.class), "termination-monitor");
+                    actorSystem = system;
+                }
+            }
+        }
+
+        return actorSystem;
+    }
+
+
+    private static final Config readAkkaConfiguration(){
+        File defaultConfigFile = new File(AKKA_CONF_PATH);
+        Preconditions.checkState(defaultConfigFile.exists(), "akka.conf is missing");
+        return ConfigFactory.parseFile(defaultConfigFile);
     }
 }
