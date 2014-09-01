6f2e40092c832fe6b2f7c21ac279a2d9c677f6a8
Tony Tkacik <ttkacik@cisco.com>
2014-09-01 21:56:45 +0200

    Bug 1441: Added proper handling of reading JSON list items.
    JSON draft used together with restconf, requires special
    handling of reading one list item, when resulting
    JSON is not as in reading its parent
    
        {list:[{id:"foo"}]}
    
    but {list:{id:"foo"}}.
    
    This bug was introduced by migration to new stream based
    JSON writers.
    
    Change-Id: I38ef0b40cea1a22e313d22f64db3806c293d67d6
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/NormalizedNodeJsonBodyWriter.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/NormalizedNodeJsonBodyWriter.java
@@ -8,6 +8,7 @@
 package org.opendaylight.controller.sal.rest.impl;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -25,9 +26,16 @@ import org.opendaylight.controller.sal.rest.api.RestconfService;
 import org.opendaylight.controller.sal.restconf.impl.InstanceIdentifierContext;
 import org.opendaylight.controller.sal.restconf.impl.NormalizedNodeContext;
 import org.opendaylight.controller.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 
 @Provider
 @Produces({ Draft02.MediaTypes.API + RestconfService.JSON, Draft02.MediaTypes.DATA + RestconfService.JSON,
@@ -48,16 +56,45 @@ public class NormalizedNodeJsonBodyWriter implements MessageBodyWriter<Normalize
     public void writeTo(final NormalizedNodeContext t, final Class<?> type, final Type genericType, final Annotation[] annotations,
             final MediaType mediaType, final MultivaluedMap<String, Object> httpHeaders, final OutputStream entityStream)
                     throws IOException, WebApplicationException {
-        if (t.getData() == null) {
+        NormalizedNode<?, ?> data = t.getData();
+        InstanceIdentifierContext context = t.getInstanceIdentifierContext();
+        SchemaPath path = context.getSchemaNode().getPath();
+        OutputStreamWriter outputWriter = new OutputStreamWriter(entityStream, Charsets.UTF_8);
+        if (data == null) {
             throw new RestconfDocumentedException(Response.Status.NOT_FOUND);
         }
 
-        InstanceIdentifierContext pathContext = t.getInstanceIdentifierContext();
-        OutputStreamWriter ouWriter = new OutputStreamWriter(entityStream, Charsets.UTF_8);
-        NormalizedNodeStreamWriter jsonWriter = JSONNormalizedNodeStreamWriter.create(pathContext.getSchemaContext(),ouWriter);
-        NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(jsonWriter);
+        outputWriter.write('{');
+        if (SchemaPath.ROOT.equals(path)) {
+            // FIXME: Add proper handling of reading root.
+        } else if (data instanceof MapEntryNode) {
+            writeMapEntryStart(outputWriter,data.getNodeType(),context.getSchemaContext());
+        } else if (data instanceof ContainerNode) {
+            path = path.getParent();
+        } else {
+            throw new IllegalArgumentException("Unsupported data type: " + data.getClass());
+        }
 
-        nnWriter.write(t.getData());
+        NormalizedNodeStreamWriter jsonWriter = JSONNormalizedNodeStreamWriter.create(context.getSchemaContext(),path,outputWriter);
+        NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(jsonWriter);
+        nnWriter.write(data);
         nnWriter.flush();
+
+        outputWriter.write('}');
+        outputWriter.flush();
     }
+
+    private void writeMapEntryStart(OutputStreamWriter writer, QName qname, SchemaContext schemaContext) throws IOException {
+        Module module = schemaContext.findModuleByNamespaceAndRevision(qname.getNamespace(), qname.getRevision());
+        Preconditions.checkArgument(module != null,"YANG Module not available for %s",qname);
+
+
+        writer.write('"');
+        writer.write(module.getName());
+        writer.write(':');
+        writer.write(qname.getLocalName());
+        writer.write('"');
+        writer.write(':');
+    }
+
 }
