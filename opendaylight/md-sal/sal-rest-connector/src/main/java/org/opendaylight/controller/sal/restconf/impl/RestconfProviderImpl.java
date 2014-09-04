548c726286fb15732c10d69900f454cda27ccd23
Tony Tkacik <ttkacik@cisco.com>
2014-09-04 13:48:08 +0200

    Bug 222: Added basic Restconf operational statistics
    Added basic restconf statistics which counts
    incoming requests and exported it via Config subsystem.
    
    Export via config subsystem provides this
    statistics via JMX and Netconf. In distribution, where
    sal-netconf-connector is present and 99-sal-netconf-connector.xml is enabled,
    Restconf statistics are also available via
    
    restconf/operational/opendaylight-inventory:nodes/node/controller-config/yang-ext:mount/config:modules/module/rest-connector-default-impl
    
    Change-Id: Ibd0fb6875104d1a90eeaf62271c9a2f87d2f85f0
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfProviderImpl.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfProviderImpl.java
@@ -5,27 +5,34 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.sal.rest.impl;
+package org.opendaylight.controller.sal.restconf.impl;
 
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Collections;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Config;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Get;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Operational;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Post;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Put;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.RestConnectorRuntimeMXBean;
+import org.opendaylight.controller.config.yang.md.sal.rest.connector.Rpcs;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
 import org.opendaylight.controller.sal.core.api.Broker.ProviderSession;
 import org.opendaylight.controller.sal.core.api.Provider;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.controller.sal.rest.api.RestConnector;
-import org.opendaylight.controller.sal.restconf.impl.BrokerFacade;
-import org.opendaylight.controller.sal.restconf.impl.ControllerContext;
 import org.opendaylight.controller.sal.streams.websockets.WebSocketServer;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 
-public class RestconfProviderImpl implements Provider, AutoCloseable, RestConnector {
+public class RestconfProviderImpl implements Provider, AutoCloseable, RestConnector, RestConnectorRuntimeMXBean {
 
     public final static String NOT_INITALIZED_MSG = "Restconf is not initialized yet. Please try again later";
 
+    private final StatisticsRestconfServiceWrapper stats = StatisticsRestconfServiceWrapper.getInstance();
     private ListenerRegistration<SchemaContextListener> listenerRegistration;
     private PortNumber port;
     public void setWebsocketPort(PortNumber port) {
@@ -62,5 +69,38 @@ public class RestconfProviderImpl implements Provider, AutoCloseable, RestConnec
             listenerRegistration.close();
         }
         webSocketServerThread.interrupt();
+    }
+
+    @Override
+    public Config getConfig() {
+        Config config = new Config();
+        Get get = new Get();
+        get.setReceivedRequests(stats.getConfigGet());
+        config.setGet(get);
+        Post post = new Post();
+        post.setReceivedRequests(stats.getConfigPost());
+        config.setPost(post);
+        Put put = new Put();
+        put.setReceivedRequests(stats.getConfigPut());
+        config.setPut(put);
+        return config;
+    }
+
+    @Override
+    public Operational getOperational() {
+        BigInteger opGet = stats.getOperationalGet();
+        Operational operational = new Operational();
+        Get get = new Get();
+        get.setReceivedRequests(opGet);
+        operational.setGet(get);
+        return operational;
+    }
+
+    @Override
+    public Rpcs getRpcs() {
+        BigInteger rpcInvoke = stats.getRpc();
+        Rpcs rpcs = new Rpcs();
+        rpcs.setReceivedRequests(rpcInvoke);
+        return rpcs ;
     }
 }
