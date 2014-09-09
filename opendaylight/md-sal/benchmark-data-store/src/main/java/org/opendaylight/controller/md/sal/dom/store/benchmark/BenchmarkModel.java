ec0392e23fa88f242aaf45f6963203492444cb6c
Lukas Sedlak <lsedlak@cisco.com>
2014-09-09 16:35:44 +0200

    Created Benchmark for InMemoryDataStore Write Op
    Created Benchmark suite for testing InMemoryDataStore write operations. The benchmark
    is designed to measure efficiency of InMemoryDataStore write operations for scenarios:
     -100K Writes executed as One commit (100K items in one commit) of One outer list item with one inner list item
     -100K Writes executed as Each commit per Write (100K commits) of One outer list item with one inner list item
     -50K Writes executed as One commit (50K items in one commit) of One outer list item with two inner list items
     -50K Writes executed as Each commit per Write (50K commits) of One outer list item with two inner list items
     -10K Writes executed as One commit (10K items in one commit) of One outer list item with ten inner list items
     -10K Writes executed as Each commit per Write (10K commits) of One outer list item with ten inner list items
    
    The md-sal parent pom is extended with benchmarks profile.
    
    Change-Id: I04f6e4545445d5879d8de9785d7ba1a53ec007af
    Signed-off-by: Lukas Sedlak <lsedlak@cisco.com>

---
+++ opendaylight/md-sal/benchmark-data-store/src/main/java/org/opendaylight/controller/md/sal/dom/store/benchmark/BenchmarkModel.java
@@ -0,0 +1,46 @@
+package org.opendaylight.controller.md.sal.dom.store.benchmark;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Set;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+
+/**
+ * @author Lukas Sedlak <lsedlak@cisco.com>
+ */
+public class BenchmarkModel {
+
+    public static final QName TEST_QNAME = QName
+        .create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom:store:test", "2014-03-13",
+            "test");
+    public static final QName OUTER_LIST_QNAME = QName.create(TEST_QNAME, "outer-list");
+    public static final QName INNER_LIST_QNAME = QName.create(TEST_QNAME, "inner-list");
+    public static final QName OUTER_CHOICE_QNAME = QName.create(TEST_QNAME, "outer-choice");
+    public static final QName ID_QNAME = QName.create(TEST_QNAME, "id");
+    public static final QName NAME_QNAME = QName.create(TEST_QNAME, "name");
+    public static final QName VALUE_QNAME = QName.create(TEST_QNAME, "value");
+    private static final String DATASTORE_TEST_YANG = "/odl-datastore-test.yang";
+
+    public static final YangInstanceIdentifier TEST_PATH = YangInstanceIdentifier.of(TEST_QNAME);
+    public static final YangInstanceIdentifier OUTER_LIST_PATH = YangInstanceIdentifier.builder(TEST_PATH).node(OUTER_LIST_QNAME).build();
+
+    public static final InputStream getDatastoreBenchmarkInputStream() {
+        return getInputStream(DATASTORE_TEST_YANG);
+    }
+
+    private static InputStream getInputStream(final String resourceName) {
+        return BenchmarkModel.class.getResourceAsStream(resourceName);
+    }
+
+    public static SchemaContext createTestContext() {
+        YangParserImpl parser = new YangParserImpl();
+        Set<Module> modules = parser.parseYangModelsFromStreams(Collections.singletonList(
+            getDatastoreBenchmarkInputStream()));
+        return parser.resolveSchemaContext(modules);
+    }
+}
