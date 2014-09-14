4df0eecec9de056fb373c0f98b2fb28a8c6c6fa3
Vaclav Demcak <vdemcak@cisco.com>
2014-09-14 12:37:38 +0200

    fix 1484 - StatisticManager performance improvement refactoring : * migration to new MD-SAL API (DataBroker DataChangeListener) * splitting project api/impl + (javaDoc for api) * clean pom.xml (xtend dependency and private-package, add sal-binding-broker-impl for test) * chaining req/resp StatisticNotification per FlowNode per statistic for prevent DS and RPC queue overload * add multipart msg joiner (StatManager wait for all relevant multipart msg for notification before write relevant 	Oper/DS action) - possibility to remove all not used subtrees (target -> reflect actual device contain asap for 	all alien node's data). * add FlowNode registratration - registration needs all SwitchFuture information for make a correct Stat target list (flows, 	groups, meters, queues, tables, nrOfTables, ports) - fix for sending not supported notifications to device * add logic for unique identification FlowId - DeviceFlow (create unique tweens FlowId and flowHash from responded device 	flow fields like match, priority, flowCookie, flowCookieMask) * change FlowCookieMap to FlowHashMap -> List<FlowHash, FlowId> * add MockTestSuite for a StatManager workflow interaction testing
    PASS tests https://jenkins.opendaylight.org/integration/job/integration-master-csit-base-of13
    	BUT Commit needs to have sleep time in integration tests
    
    So commit depends on https://git.opendaylight.org/gerrit/11150
    
    Change-Id: I265b57378fc19573d6f18036825cb57f4131ca80
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

---
+++ opendaylight/md-sal/statistics-manager/src/test/java/test/mock/NodeRegistrationTest.java
@@ -0,0 +1,49 @@
+package test.mock;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
+import org.opendaylight.controller.md.statistics.manager.StatisticsManagerActivator;
+import org.opendaylight.controller.md.statistics.manager.StatisticsManagerProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import test.mock.util.StatisticsManagerTest;
+
+public class NodeRegistrationTest extends StatisticsManagerTest {
+
+    @Test
+    public void nodeRegistrationTest() throws ExecutionException, InterruptedException {
+        final StatisticsManagerActivator activator = new StatisticsManagerActivator();
+        final StatisticsManagerProvider statisticsManagerProvider = new StatisticsManagerProvider(activator);
+        activator.onSessionInitiated(providerContext);
+
+        addFlowCapableNode(s1Key);
+        Thread.sleep(1000);
+        final InstanceIdentifier<Node> nodeII = InstanceIdentifier.create(Nodes.class).child(Node.class, s1Key);
+
+        assertTrue(statisticsManagerProvider.getStatisticsManager().getStatCollector().isProvidedFlowNodeActive(nodeII));
+    }
+
+    @Test
+    public void nodeUnregistrationTest() throws ExecutionException, InterruptedException {
+        final StatisticsManagerActivator activator = new StatisticsManagerActivator();
+        final StatisticsManagerProvider statisticsManagerProvider = new StatisticsManagerProvider(activator);
+        activator.onSessionInitiated(providerContext);
+
+        addFlowCapableNode(s1Key);
+        Thread.sleep(1000);
+        final InstanceIdentifier<Node> nodeII = InstanceIdentifier.create(Nodes.class).child(Node.class, s1Key);
+
+        assertTrue(statisticsManagerProvider.getStatisticsManager().getStatCollector().isProvidedFlowNodeActive(nodeII));
+
+        removeNode(s1Key);
+        Thread.sleep(1000);
+        assertFalse(statisticsManagerProvider.getStatisticsManager().getStatCollector().isProvidedFlowNodeActive(nodeII));
+    }
+}
+
