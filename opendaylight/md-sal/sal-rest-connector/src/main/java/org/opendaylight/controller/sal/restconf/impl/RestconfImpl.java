ac336958b4b19237784d277f1cbc2c196eb2f995
Jozef Gloncak <jgloncak@cisco.com>
2014-09-17 10:11:55 +0200

    BUG 1269 - fill HTTP POST response location attribute
    HTTP POST response atribute LOCATION is now filled with URI to top level
    data which were inserted via POST HTTP operation.
    
    !!!!!
    RestCodec call was added. Not all codecs have String as output of serialize method
    (InstanceIdentifier, Identityref). Currently serialize output is
    transformed to string via toString method.
    !!!!!
    
    Change-Id: I7e6abbf100b2458907a1afd3b0dd96a6c54d5652
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfImpl.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfImpl.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
@@ -489,7 +490,7 @@ public class RestconfImpl implements RestconfService {
         final YangInstanceIdentifier pathIdentifier = ((YangInstanceIdentifier) pathValue);
         String streamName = null;
         if (!Iterables.isEmpty(pathIdentifier.getPathArguments())) {
-            String fullRestconfIdentifier = this.controllerContext.toFullRestconfIdentifier(pathIdentifier);
+            String fullRestconfIdentifier = this.controllerContext.toFullRestconfIdentifier(pathIdentifier, null);
 
             LogicalDatastoreType datastore = parseEnumTypeParameter(value, LogicalDatastoreType.class,
                     DATASTORE_PARAM_NAME);
@@ -847,7 +848,7 @@ public class RestconfImpl implements RestconfService {
     }
 
     @Override
-    public Response createConfigurationData(final String identifier, final Node<?> payload) {
+    public Response createConfigurationData(final String identifier, final Node<?> payload, final UriInfo uriInfo) {
         if (payload == null) {
             throw new RestconfDocumentedException("Input is required.", ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
         }
@@ -913,11 +914,17 @@ public class RestconfImpl implements RestconfService {
             throw new RestconfDocumentedException("Error creating data", e);
         }
 
-        return Response.status(Status.NO_CONTENT).build();
+
+        ResponseBuilder responseBuilder = Response.status(Status.NO_CONTENT);
+        URI location = resolveLocation(uriInfo, "config", mountPoint, normalizedII);
+        if (location != null) {
+            responseBuilder.location(location);
+        }
+        return responseBuilder.build();
     }
 
     @Override
-    public Response createConfigurationData(final Node<?> payload) {
+    public Response createConfigurationData(final Node<?> payload, final UriInfo uriInfo) {
         if (payload == null) {
             throw new RestconfDocumentedException("Input is required.", ErrorType.PROTOCOL, ErrorTag.MALFORMED_MESSAGE);
         }
@@ -961,7 +968,24 @@ public class RestconfImpl implements RestconfService {
             throw new RestconfDocumentedException("Error creating data", e);
         }
 
-        return Response.status(Status.NO_CONTENT).build();
+        ResponseBuilder responseBuilder = Response.status(Status.NO_CONTENT);
+        URI location = resolveLocation(uriInfo, "", mountPoint, normalizedII);
+        if (location != null) {
+            responseBuilder.location(location);
+        }
+        return responseBuilder.build();
+    }
+
+    private URI resolveLocation(final UriInfo uriInfo, final String uriBehindBase, final DOMMountPoint mountPoint, final YangInstanceIdentifier normalizedII) {
+        UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+        uriBuilder.path("config");
+        try {
+            uriBuilder.path(controllerContext.toFullRestconfIdentifier(normalizedII, mountPoint));
+        } catch (Exception e) {
+            LOG.debug("Location for instance identifier"+normalizedII+"wasn't created", e);
+            return null;
+        }
+        return uriBuilder.build();
     }
 
     @Override
