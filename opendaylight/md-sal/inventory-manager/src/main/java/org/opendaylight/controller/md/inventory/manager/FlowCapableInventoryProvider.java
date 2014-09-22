8b9cce858e7a0f7d976a8fa4d8f6469c35bf7b56
Vaclav Demcak <vdemcak@cisco.com>
2014-09-22 23:06:27 +0200

    bug 2034 - InventoryManager create all Tables
    changes:
    
    FlowCapableNode PUT/MERGE fix - Handshake produces NodeUpdated notification twotime.
    	1) NodeUpdate contains SwitchFeatures - SwitchFeatures could get only from Handshake and it really means "new kid on the block"
    		so we'd like to PUT the FlowCapableNode
    	2) PostHandshake produces second NodeUpdate notification with device description but without SwitchFeatures, so we'd like to
    		MERGE this FlowCapableNode
    
    Tables missing fix - any FlowCapableNode can't exist without tables (OF_1.0 tableO, OF_1.3 0-254 tables) so we'd like to have
    	all tables in same time as FlowCapableNode is created in Operational/DS. It could prevent OptimisticLockException for adding data
    	to some table node for first time call.
    
    onTransactionChainListenerFailed impl. fix - code followed javadoc instructions "Invoked if when a transaction in the chain fails. All
    	other transactions are automatically cancelled by the time this notification is invoked. Implementations should invoke
    	chain.close() to close the chain." + start new one to continue.
    
    Thread vs Executor - any fail in Thread could stop execution, so we'd like to have Executor for a thread resurection functionality.
    
    fix code for @SuppressWarnings("unchecked")
    
    PASS tests https://jenkins.opendaylight.org/integration/job/integration-master-csit-base-of13/
    PASS tests https://jenkins.opendaylight.org/integration/job/integration-master-csit-karaf-compatible-min/
    PASS tests https://jenkins.opendaylight.org/integration/job/integration-master-csit-karaf-l2switch-only/
    
    Change-Id: I015f028ce1971560d2506d82495db894facb4d29
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/inventory-manager/src/main/java/org/opendaylight/controller/md/inventory/manager/FlowCapableInventoryProvider.java
+++ opendaylight/md-sal/inventory-manager/src/main/java/org/opendaylight/controller/md/inventory/manager/FlowCapableInventoryProvider.java
@@ -7,12 +7,12 @@
  */
 package org.opendaylight.controller.md.inventory.manager;
 
-import com.google.common.base.Preconditions;
-import com.google.common.util.concurrent.CheckedFuture;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadFactory;
+
 import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
@@ -25,6 +25,12 @@ import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 class FlowCapableInventoryProvider implements AutoCloseable, Runnable, TransactionChainListener {
     private static final Logger LOG = LoggerFactory.getLogger(FlowCapableInventoryProvider.class);
     private static final int QUEUE_DEPTH = 500;
@@ -32,26 +38,29 @@ class FlowCapableInventoryProvider implements AutoCloseable, Runnable, Transacti
 
     private final BlockingQueue<InventoryOperation> queue = new LinkedBlockingDeque<>(QUEUE_DEPTH);
     private final NotificationProviderService notificationService;
+    private final ExecutorService invDataStoreOperationServ;
 
     private final DataBroker dataBroker;
     private BindingTransactionChain txChain;
     private ListenerRegistration<?> listenerRegistration;
-    private Thread thread;
 
     FlowCapableInventoryProvider(final DataBroker dataBroker, final NotificationProviderService notificationService) {
         this.dataBroker = Preconditions.checkNotNull(dataBroker);
         this.notificationService = Preconditions.checkNotNull(notificationService);
+        final ThreadFactory invThFact = new ThreadFactoryBuilder().setNameFormat("odl-OF-inventory-thread-%d").build();
+        invDataStoreOperationServ = Executors.newSingleThreadExecutor(invThFact);
+    }
+
+    public String getName() {
+        return "FlowCapableInventoryProvider";
     }
 
     void start() {
         final NodeChangeCommiter changeCommiter = new NodeChangeCommiter(FlowCapableInventoryProvider.this);
-        this.listenerRegistration = this.notificationService.registerNotificationListener(changeCommiter);
+        listenerRegistration = notificationService.registerNotificationListener(changeCommiter);
 
-        this.txChain = dataBroker.createTransactionChain(this);
-        thread = new Thread(this);
-        thread.setDaemon(true);
-        thread.setName("FlowCapableInventoryProvider");
-        thread.start();
+        txChain = dataBroker.createTransactionChain(this);
+        invDataStoreOperationServ.execute(this);
 
         LOG.info("Flow Capable Inventory Provider started.");
     }
@@ -59,7 +68,7 @@ class FlowCapableInventoryProvider implements AutoCloseable, Runnable, Transacti
     void enqueue(final InventoryOperation op) {
         try {
             queue.put(op);
-        } catch (InterruptedException e) {
+        } catch (final InterruptedException e) {
             LOG.warn("Failed to enqueue operation {}", op, e);
         }
     }
@@ -67,20 +76,16 @@ class FlowCapableInventoryProvider implements AutoCloseable, Runnable, Transacti
     @Override
     public void close() throws InterruptedException {
         LOG.info("Flow Capable Inventory Provider stopped.");
-        if (this.listenerRegistration != null) {
+        if (listenerRegistration != null) {
             try {
-                this.listenerRegistration.close();
-            } catch (Exception e) {
+                listenerRegistration.close();
+            } catch (final Exception e) {
                 LOG.error("Failed to stop inventory provider", e);
             }
             listenerRegistration = null;
         }
+        invDataStoreOperationServ.shutdown();
 
-        if (thread != null) {
-            thread.interrupt();
-            thread.join();
-            thread = null;
-        }
         if (txChain != null) {
             txChain.close();
             txChain = null;
@@ -121,14 +126,17 @@ class FlowCapableInventoryProvider implements AutoCloseable, Runnable, Transacti
 
                     @Override
                     public void onFailure(final Throwable throwable) {
-                        LOG.error("Transaction {} failed.", tx.getIdentifier(), throwable);
+                        LOG.warn("Transaction {} failed.", tx.getIdentifier(), throwable);
                     }
                 });
             }
-        } catch (InterruptedException e) {
+        } catch (final Exception e) {
             LOG.info("Processing interrupted, terminating", e);
         }
+        cleanOperationQueue();
+    }
 
+    private void cleanOperationQueue() {
         // Drain all events, making sure any blocked threads are unblocked
         while (!queue.isEmpty()) {
             queue.poll();
@@ -138,8 +146,10 @@ class FlowCapableInventoryProvider implements AutoCloseable, Runnable, Transacti
     @Override
     public void onTransactionChainFailed(final TransactionChain<?, ?> chain, final AsyncTransaction<?, ?> transaction,
                                          final Throwable cause) {
-        LOG.error("Failed to export Flow Capable Inventory, Transaction {} failed.", transaction.getIdentifier(), cause);
-
+        LOG.warn("Failed to export Flow Capable Inventory, Transaction {} failed.", transaction.getIdentifier(), cause);
+        txChain.close();
+        txChain = dataBroker.createTransactionChain(FlowCapableInventoryProvider.this);
+        cleanOperationQueue();
     }
 
     @Override
