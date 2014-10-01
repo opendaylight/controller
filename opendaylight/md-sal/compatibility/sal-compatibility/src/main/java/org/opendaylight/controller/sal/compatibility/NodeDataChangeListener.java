0f79000f2ebe13dcb30eaefb86b954491e398849
Ed Warnicke <eaw@cisco.com>
2014-10-01 09:40:29 -0700

    Bug 2107: Use DataCommitListeners for inventory events
    Change-Id: I648bba5e7585cc9d1a70f3dcaf6f8fdbcfeb1787
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ opendaylight/md-sal/compatibility/sal-compatibility/src/main/java/org/opendaylight/controller/sal/compatibility/NodeDataChangeListener.java
@@ -0,0 +1,52 @@
+package org.opendaylight.controller.sal.compatibility;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeUpdated;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeUpdatedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRemovedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeUpdatedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NodeDataChangeListener extends AbstractDataChangeListener<Node> {
+    private static final Logger LOG = LoggerFactory.getLogger(NodeDataChangeListener.class);
+
+
+    public NodeDataChangeListener (final InventoryAndReadAdapter adapter, final DataBroker db) {
+        super(adapter,db,Node.class);
+    }
+
+    protected void add(InstanceIdentifier<Node> createKeyIdent, Node node) {
+        FlowCapableNode fcn = node.getAugmentation(FlowCapableNode.class);
+        if(fcn != null) {
+            FlowCapableNodeUpdatedBuilder fcbnu = new FlowCapableNodeUpdatedBuilder(fcn);
+            NodeUpdatedBuilder builder = new NodeUpdatedBuilder();
+            builder.setId(node.getId());
+            builder.setNodeRef(new NodeRef(createKeyIdent));
+            builder.setNodeConnector(node.getNodeConnector());
+            builder.addAugmentation(FlowCapableNodeUpdated.class, fcbnu.build());
+            adapter.onNodeUpdatedInternal(builder.build());
+        }
+    }
+
+    protected void update(InstanceIdentifier<Node> updateKeyIdent, Node original,
+            Node update) {
+        this.add(updateKeyIdent, update);
+    }
+
+    protected void remove(InstanceIdentifier<Node> ident, Node removeValue) {
+        NodeRemovedBuilder builder = new NodeRemovedBuilder();
+        builder.setNodeRef(new NodeRef(ident));
+        adapter.onNodeRemovedInternal(builder.build());
+    }
+
+    protected InstanceIdentifier<Node> getWildCardPath() {
+        return InstanceIdentifier.create(Nodes.class).child(Node.class);
+    }
+
+}
