09ab0255af4e9e97884e599b609614c1da02396d
Jan Hajnar <jhajnar@cisco.com>
2014-10-08 16:39:55 +0200

    Bug 1944 - Controller does not delete existing flows from the network at startup (karaf edition)
    * changed node connection listener to listen to nodeUpdated notification
    instead of datastore notification
    * added method to delete all flows on node when node is connected
    (method is run called before reconciliation)
    * added configuration option to enable or disable flow removal via
    config subsystem
    * updated tests
    
    Change-Id: I72970e5f0bbef744afb34383b4d614561167781c
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/impl/FlowNodeReconciliationImpl.java
+++ opendaylight/md-sal/forwardingrules-manager/src/main/java/org/opendaylight/controller/frm/impl/FlowNodeReconciliationImpl.java
@@ -8,49 +8,49 @@
 
 package org.opendaylight.controller.frm.impl;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import org.opendaylight.controller.frm.FlowNodeReconciliation;
 import org.opendaylight.controller.frm.ForwardingRulesManager;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.meters.Meter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.meters.MeterKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.Group;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.groups.GroupKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRemoved;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorUpdated;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRemoved;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Preconditions;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * forwardingrules-manager
  * org.opendaylight.controller.frm
- *
+ * <p/>
  * FlowNode Reconciliation Listener
  * Reconciliation for a new FlowNode
  *
  * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
- *
- * Created: Jun 13, 2014
+ *         <p/>
+ *         Created: Jun 13, 2014
  */
 public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
 
@@ -58,53 +58,22 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
 
     private final ForwardingRulesManager provider;
 
-    private ListenerRegistration<DataChangeListener> listenerRegistration;
+    private ListenerRegistration notifListenerRegistration;
 
-    public FlowNodeReconciliationImpl (final ForwardingRulesManager manager, final DataBroker db) {
+    public FlowNodeReconciliationImpl(final ForwardingRulesManager manager, NotificationProviderService notif) {
         this.provider = Preconditions.checkNotNull(manager, "ForwardingRulesManager can not be null!");
-        Preconditions.checkNotNull(db, "DataBroker can not be null!");
-        /* Build Path */
-        InstanceIdentifier<FlowCapableNode> flowNodeWildCardIdentifier = InstanceIdentifier.create(Nodes.class)
-                .child(Node.class).augmentation(FlowCapableNode.class);
-        this.listenerRegistration = db.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
-                flowNodeWildCardIdentifier, FlowNodeReconciliationImpl.this, DataChangeScope.BASE);
+        this.notifListenerRegistration = notif.registerNotificationListener(this);
     }
 
     @Override
     public void close() {
-        if (listenerRegistration != null) {
+        if (notifListenerRegistration != null) {
             try {
-                listenerRegistration.close();
+                notifListenerRegistration.close();
             } catch (Exception e) {
                 LOG.error("Error by stop FRM FlowNodeReconilListener.", e);
             }
-            listenerRegistration = null;
-        }
-    }
-
-    @Override
-    public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changeEvent) {
-        Preconditions.checkNotNull(changeEvent,"Async ChangeEvent can not be null!");
-        /* All DataObjects for create */
-        final Set<InstanceIdentifier<?>>  createdData = changeEvent.getCreatedData() != null
-                ? changeEvent.getCreatedData().keySet() : Collections.<InstanceIdentifier<?>> emptySet();
-        /* All DataObjects for remove */
-        final Set<InstanceIdentifier<?>> removeData = changeEvent.getRemovedPaths() != null
-                ? changeEvent.getRemovedPaths() : Collections.<InstanceIdentifier<?>> emptySet();
-
-        for (InstanceIdentifier<?> entryKey : removeData) {
-            final InstanceIdentifier<FlowCapableNode> nodeIdent = entryKey
-                    .firstIdentifierOf(FlowCapableNode.class);
-            if ( ! nodeIdent.isWildcarded()) {
-                flowNodeDisconnected(nodeIdent);
-            }
-        }
-        for (InstanceIdentifier<?> entryKey : createdData) {
-            final InstanceIdentifier<FlowCapableNode> nodeIdent = entryKey
-                    .firstIdentifierOf(FlowCapableNode.class);
-            if ( ! nodeIdent.isWildcarded()) {
-                flowNodeConnected(nodeIdent);
-            }
+            notifListenerRegistration = null;
         }
     }
 
@@ -115,10 +84,22 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
 
     @Override
     public void flowNodeConnected(InstanceIdentifier<FlowCapableNode> connectedNode) {
-        if ( ! provider.isNodeActive(connectedNode)) {
+        if (!provider.isNodeActive(connectedNode)) {
             provider.registrateNewNode(connectedNode);
+            if (provider.getConfiguration().isCleanAlienFlowsOnReconcil()) {
+                cleanUpAlienFlows(connectedNode);
+            }
             reconciliation(connectedNode);
         }
+    }
+
+    private void cleanUpAlienFlows(final InstanceIdentifier<FlowCapableNode> nodeIdent) {
+        RemoveFlowInputBuilder rfiBuilder = new RemoveFlowInputBuilder();
+        rfiBuilder.setBarrier(false);
+        rfiBuilder.setNode(new NodeRef(nodeIdent));
+        // setting tableId to 255 will delete flows from all tables
+        rfiBuilder.setTableId((short) 255);
+        provider.getSalFlowService().removeFlow(rfiBuilder.build());
     }
 
     private void reconciliation(final InstanceIdentifier<FlowCapableNode> nodeIdent) {
@@ -128,15 +109,14 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
 
         try {
             flowNode = trans.read(LogicalDatastoreType.CONFIGURATION, nodeIdent).get();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOG.error("Fail with read Config/DS for Node {} !", nodeIdent, e);
         }
 
         if (flowNode.isPresent()) {
             /* Groups - have to be first */
             List<Group> groups = flowNode.get().getGroup() != null
-                    ? flowNode.get().getGroup() : Collections.<Group> emptyList();
+                    ? flowNode.get().getGroup() : Collections.<Group>emptyList();
             for (Group group : groups) {
                 final KeyedInstanceIdentifier<Group, GroupKey> groupIdent =
                         nodeIdent.child(Group.class, group.getKey());
@@ -144,7 +124,7 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
             }
             /* Meters */
             List<Meter> meters = flowNode.get().getMeter() != null
-                    ? flowNode.get().getMeter() : Collections.<Meter> emptyList();
+                    ? flowNode.get().getMeter() : Collections.<Meter>emptyList();
             for (Meter meter : meters) {
                 final KeyedInstanceIdentifier<Meter, MeterKey> meterIdent =
                         nodeIdent.child(Meter.class, meter.getKey());
@@ -152,11 +132,11 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
             }
             /* Flows */
             List<Table> tables = flowNode.get().getTable() != null
-                    ? flowNode.get().getTable() : Collections.<Table> emptyList();
+                    ? flowNode.get().getTable() : Collections.<Table>emptyList();
             for (Table table : tables) {
                 final KeyedInstanceIdentifier<Table, TableKey> tableIdent =
                         nodeIdent.child(Table.class, table.getKey());
-                List<Flow> flows = table.getFlow() != null ? table.getFlow() : Collections.<Flow> emptyList();
+                List<Flow> flows = table.getFlow() != null ? table.getFlow() : Collections.<Flow>emptyList();
                 for (Flow flow : flows) {
                     final KeyedInstanceIdentifier<Flow, FlowKey> flowIdent =
                             tableIdent.child(Flow.class, flow.getKey());
@@ -166,6 +146,42 @@ public class FlowNodeReconciliationImpl implements FlowNodeReconciliation {
         }
         /* clean transaction */
         trans.close();
+    }
+
+    @Override
+    public void onNodeConnectorRemoved(NodeConnectorRemoved notification) {
+        // NO-OP
+    }
+
+    @Override
+    public void onNodeConnectorUpdated(NodeConnectorUpdated notification) {
+        // NO-OP
+    }
+
+    @Override
+    public void onNodeRemoved(NodeRemoved notification) {
+        if (notification.getNodeRef() != null && notification.getNodeRef().getValue() != null) {
+            InstanceIdentifier<Node> nodeIdent = notification.getNodeRef().getValue().firstIdentifierOf(Node.class);
+            if (nodeIdent != null && !nodeIdent.isWildcarded()) {
+                InstanceIdentifier<FlowCapableNode> flowNodeIdent= nodeIdent.augmentation(FlowCapableNode.class);
+                flowNodeDisconnected(flowNodeIdent);
+            }
+        }
+    }
+
+    @Override
+    public void onNodeUpdated(NodeUpdated notification) {
+        final FlowCapableNodeUpdated newFlowNode =
+                notification.getAugmentation(FlowCapableNodeUpdated.class);
+        if (newFlowNode != null && newFlowNode.getSwitchFeatures() != null) {
+            if (notification.getNodeRef() != null && notification.getNodeRef().getValue() != null) {
+                InstanceIdentifier<Node> nodeIdent = notification.getNodeRef().getValue().firstIdentifierOf(Node.class);
+                if (nodeIdent != null && !nodeIdent.isWildcarded()) {
+                    InstanceIdentifier<FlowCapableNode> flowNodeIdent= nodeIdent.augmentation(FlowCapableNode.class);
+                    flowNodeConnected(flowNodeIdent);
+                }
+            }
+        }
     }
 }
 
