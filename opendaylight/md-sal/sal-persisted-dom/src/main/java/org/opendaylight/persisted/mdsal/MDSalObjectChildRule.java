fb4daaa787e5f41f33c309d491bceadb3bf63398
Sharon Aicler <saichler@cisco.com>
2014-10-17 08:19:13 -0700

    MDSAL Persisted DataStore (work in progress...DO NOT MERGE)
    Change-Id: I5323f34489985042bd483858390591c1f9ed0149
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

---
+++ opendaylight/md-sal/sal-persisted-dom/src/main/java/org/opendaylight/persisted/mdsal/MDSalObjectChildRule.java
@@ -0,0 +1,19 @@
+package org.opendaylight.persisted.mdsal;
+
+import org.opendaylight.persisted.codec.AttributeDescriptor;
+import org.opendaylight.persisted.codec.TypeDescriptor;
+import org.opendaylight.persisted.codec.observers.IChildAttributeObserver;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+public class MDSalObjectChildRule implements IChildAttributeObserver{
+
+    @Override
+    public boolean isChildAttribute(AttributeDescriptor ad) {
+        return DataObject.class.isAssignableFrom(ad.getReturnType());
+    }
+
+    @Override
+    public boolean isChildAttribute(TypeDescriptor td) {
+        return DataObject.class.isAssignableFrom(td.getTypeClass());
+    }
+}
