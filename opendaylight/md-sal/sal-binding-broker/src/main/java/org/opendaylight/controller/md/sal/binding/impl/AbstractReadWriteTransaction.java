bf361f1929827dcf6fe922f20a172d9d9bca04d6
Tony Tkacik <ttkacik@cisco.com>
2014-11-06 10:18:23 +0100

    Bug 2328: Change ensureParent algorithm.
    EnsureParents algorithm has two different implementations
    with small buggy differences in behaviour - one was used in
    Legacy API always used merge operation for each and every
    parent node and also user written node.
    
    New Data Broker API implementation used algorithm which
    used merge operation only for nodes, which did not existed
    in transaction, but this triggered createParents=true
    to fail, if parent node was deleted in concurrent transaction
    which finished earlier.
    
    Unified this implementations into one in abstract class:
    merge is used always for each parent node, except
    writen node which is already supplied by client of API.
    
    New algorithm does not need to do read from transaction
    which makes this transaction faster during creation time,
    but with smaller performance penalty at commit time.
    
    Change-Id: I0fb9f8d4e77648739673566729984e1ecb1e5146
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/AbstractReadWriteTransaction.java
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/AbstractReadWriteTransaction.java
@@ -7,19 +7,9 @@
  */
 package org.opendaylight.controller.md.sal.binding.impl;
 
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
-import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizationException;
-import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizationOperation;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataReadWriteTransaction;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class AbstractReadWriteTransaction extends AbstractWriteTransaction<DOMDataReadWriteTransaction> {
 
@@ -28,38 +18,5 @@ public class AbstractReadWriteTransaction extends AbstractWriteTransaction<DOMDa
     public AbstractReadWriteTransaction(final DOMDataReadWriteTransaction delegate, final BindingToNormalizedNodeCodec codec) {
         super(delegate, codec);
     }
-
-    @Override
-    protected final void ensureParentsByMerge(final LogicalDatastoreType store,
-            final org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier normalizedPath,
-            final InstanceIdentifier<?> path) {
-        List<PathArgument> currentArguments = new ArrayList<>();
-        DataNormalizationOperation<?> currentOp = getCodec().getDataNormalizer().getRootOperation();
-        Iterator<PathArgument> iterator = normalizedPath.getPathArguments().iterator();
-        while (iterator.hasNext()) {
-            PathArgument currentArg = iterator.next();
-            try {
-                currentOp = currentOp.getChild(currentArg);
-            } catch (DataNormalizationException e) {
-                throw new IllegalArgumentException(String.format("Invalid child encountered in path %s", path), e);
-            }
-            currentArguments.add(currentArg);
-            org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier currentPath = org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.create(
-                    currentArguments);
-
-            final Boolean exists;
-            try {
-                exists = getDelegate().exists(store, currentPath).checkedGet();
-            } catch (ReadFailedException e) {
-                LOG.error("Failed to read pre-existing data from store {} path {}", store, currentPath, e);
-                throw new IllegalStateException("Failed to read pre-existing data", e);
-            }
-
-            if (!exists && iterator.hasNext()) {
-                getDelegate().merge(store, currentPath, currentOp.createDefault(currentArg));
-            }
-        }
-    }
-
 
 }
