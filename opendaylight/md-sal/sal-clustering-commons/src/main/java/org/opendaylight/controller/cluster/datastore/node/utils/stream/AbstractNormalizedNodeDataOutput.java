8976f4ccffeba131a6f0fe1f44c7a088ef1132c8
Robert Varga <rovarga@cisco.com>
2014-11-10 16:16:57 +0100

    Bug 2062 - StreamWriter APIs loses information about leaf-set ordering
    * modified clustering NormalizedNodeStreamWriter implementation to use
    OrderedLeafSet
    
    Change-Id: I663f6b6d894b8366b7a54a3c56be05f20fef43c2
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/AbstractNormalizedNodeDataOutput.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/AbstractNormalizedNodeDataOutput.java
@@ -163,6 +163,14 @@ abstract class AbstractNormalizedNodeDataOutput implements NormalizedNodeDataOut
     }
 
     @Override
+    public void startOrderedLeafSet(final YangInstanceIdentifier.NodeIdentifier name, final int childSizeHint) throws IOException, IllegalArgumentException {
+        Preconditions.checkNotNull(name, "Node identifier should not be null");
+        LOG.debug("Starting a new ordered leaf set");
+
+        startNode(name.getNodeType(), NodeTypes.ORDERED_LEAF_SET);
+    }
+
+    @Override
     public void leafSetEntryNode(final Object value) throws IOException, IllegalArgumentException {
         LOG.debug("Writing a new leaf set entry node");
 
