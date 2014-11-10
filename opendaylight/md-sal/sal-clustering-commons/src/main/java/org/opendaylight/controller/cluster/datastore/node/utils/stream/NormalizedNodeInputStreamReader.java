f8253ef7d4235ccfed49478683eb123e519855de
Jan Hajnar <jhajnar@cisco.com>
2014-11-10 16:16:57 +0100

    Bug 2062 - StreamWriter APIs loses information about leaf-set ordering
    * modified clustering NormalizedNodeStreamWriter implementation to use
    OrderedLeafSet
    
    Change-Id: I663f6b6d894b8366b7a54a3c56be05f20fef43c2
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputStreamReader.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputStreamReader.java
@@ -207,6 +207,13 @@ public class NormalizedNodeInputStreamReader implements NormalizedNodeStreamRead
                 return addLeafSetChildren(identifier.getNodeType(),
                         Builders.leafSetBuilder().withNodeIdentifier(identifier)).build();
 
+            case NodeTypes.ORDERED_LEAF_SET:
+                LOG.debug("Read leaf set node");
+                ListNodeBuilder<Object, LeafSetEntryNode<Object>> orderedLeafSetBuilder =
+                        Builders.orderedLeafSetBuilder().withNodeIdentifier(identifier);
+                orderedLeafSetBuilder = addLeafSetChildren(identifier.getNodeType(), orderedLeafSetBuilder);
+                return orderedLeafSetBuilder.build();
+
             default :
                 return null;
         }
