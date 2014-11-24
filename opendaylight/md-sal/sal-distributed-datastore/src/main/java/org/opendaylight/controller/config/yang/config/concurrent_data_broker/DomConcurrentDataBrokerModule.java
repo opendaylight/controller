edb647be8736a4bb9284a14b47a76524fe887b90
Robert Varga <rovarga@cisco.com>
2014-11-24 17:15:11 +0100

    Move ConcurrentDOMDataBroker to clustered-datastore
    The concurrent data broker is only useful for the distributed datastore,
    so move it along with the configuration binding there. Fixes the
    potential for users to misconfigure their datastore.
    
    Change-Id: I145c42ac64427db43889872abe06dd84bf751484
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/concurrent_data_broker/DomConcurrentDataBrokerModule.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/concurrent_data_broker/DomConcurrentDataBrokerModule.java
@@ -5,52 +5,37 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.config.yang.md.sal.dom.impl;
+package org.opendaylight.controller.config.yang.config.concurrent_data_broker;
 
 import com.google.common.collect.Lists;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
+import org.opendaylight.controller.cluster.datastore.ConcurrentDOMDataBroker;
+import org.opendaylight.controller.config.api.DependencyResolver;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitDeadlockException;
 import org.opendaylight.controller.md.sal.common.util.jmx.AbstractMXBean;
 import org.opendaylight.controller.md.sal.common.util.jmx.ThreadExecutorStatsMXBeanImpl;
-import org.opendaylight.controller.md.sal.dom.broker.impl.SerializedDOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.broker.impl.jmx.CommitStatsMXBeanImpl;
 import org.opendaylight.controller.md.sal.dom.store.impl.InMemoryDOMDataStoreFactory;
 import org.opendaylight.controller.sal.core.spi.data.DOMStore;
 import org.opendaylight.yangtools.util.DurationStatisticsTracker;
-import org.opendaylight.yangtools.util.concurrent.DeadlockDetectingListeningExecutorService;
 import org.opendaylight.yangtools.util.concurrent.SpecialExecutors;
 
-/**
-*
-*/
-public final class DomInmemoryDataBrokerModule extends
-        org.opendaylight.controller.config.yang.md.sal.dom.impl.AbstractDomInmemoryDataBrokerModule {
-
+public class DomConcurrentDataBrokerModule extends AbstractDomConcurrentDataBrokerModule {
     private static final String JMX_BEAN_TYPE = "DOMDataBroker";
 
-    public DomInmemoryDataBrokerModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier,
-            final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+    public DomConcurrentDataBrokerModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public DomInmemoryDataBrokerModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier,
-            final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
-            final DomInmemoryDataBrokerModule oldModule, final java.lang.AutoCloseable oldInstance) {
-
+    public DomConcurrentDataBrokerModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final DependencyResolver dependencyResolver, final DomConcurrentDataBrokerModule oldModule, final AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
     @Override
-    protected void customValidation() {
-        // Add custom validation for module attributes here.
-    }
-
-    @Override
-    public java.lang.AutoCloseable createInstance() {
+    public AutoCloseable createInstance() {
         //Initializing Operational DOM DataStore defaulting to InMemoryDOMDataStore if one is not configured
         DOMStore operStore =  getOperationalDataStoreDependency();
         if(operStore == null){
@@ -81,31 +66,10 @@ public final class DomInmemoryDataBrokerModule extends
                 "CommitFutures");
 
         final List<AbstractMXBean> mBeans = Lists.newArrayList();
+
         final DurationStatisticsTracker commitStatsTracker;
-
-        /*
-         * We use a single-threaded executor for commits with a bounded queue capacity. If the
-         * queue capacity is reached, subsequent commit tasks will be rejected and the commits will
-         * fail. This is done to relieve back pressure. This should be an extreme scenario - either
-         * there's deadlock(s) somewhere and the controller is unstable or some rogue component is
-         * continuously hammering commits too fast or the controller is just over-capacity for the
-         * system it's running on.
-         */
-        ExecutorService commitExecutor = SpecialExecutors.newBoundedSingleThreadExecutor(
-            getMaxDataBrokerCommitQueueSize(), "WriteTxCommit");
-
-        SerializedDOMDataBroker sdb = new SerializedDOMDataBroker(datastores,
-            new DeadlockDetectingListeningExecutorService(commitExecutor,
-                TransactionCommitDeadlockException.DEADLOCK_EXCEPTION_SUPPLIER,
-                listenableFutureExecutor));
-        commitStatsTracker = sdb.getCommitStatsTracker();
-
-        final AbstractMXBean commitExecutorStatsMXBean =
-                ThreadExecutorStatsMXBeanImpl.create(commitExecutor, "CommitExecutorStats",
-                    JMX_BEAN_TYPE, null);
-        if(commitExecutorStatsMXBean != null) {
-            mBeans.add(commitExecutorStatsMXBean);
-        }
+        final ConcurrentDOMDataBroker cdb = new ConcurrentDOMDataBroker(datastores, listenableFutureExecutor);
+        commitStatsTracker = cdb.getCommitStatsTracker();
 
         if(commitStatsTracker != null) {
             final CommitStatsMXBeanImpl commitStatsMXBean = new CommitStatsMXBeanImpl(
@@ -121,7 +85,7 @@ public final class DomInmemoryDataBrokerModule extends
             mBeans.add(commitFutureStatsMXBean);
         }
 
-        sdb.setCloseable(new AutoCloseable() {
+        cdb.setCloseable(new AutoCloseable() {
             @Override
             public void close() {
                 for(AbstractMXBean mBean: mBeans) {
@@ -130,6 +94,6 @@ public final class DomInmemoryDataBrokerModule extends
             }
         });
 
-        return sdb;
+        return cdb;
     }
 }
