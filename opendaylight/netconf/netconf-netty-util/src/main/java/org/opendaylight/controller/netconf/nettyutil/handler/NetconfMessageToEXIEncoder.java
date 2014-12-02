38eb4430ad380a5640e0012d3566d8d7f255a75d
Robert Varga <rovarga@cisco.com>
2014-12-02 20:39:10 +0100

    BUG-2459: buffer writes towards Netty
    OpenEXI performs quite a few single-byte writes, which are forwarded as
    ByteBuf accesses. Instantiating a buffer should help with lowering the
    overhead imposed by netty when accessing the buffer.
    
    Change-Id: I0110eb0db0183bde9707a7abcdcd563ca8a52cf1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/controller/netconf/nettyutil/handler/NetconfMessageToEXIEncoder.java
+++ opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/controller/netconf/nettyutil/handler/NetconfMessageToEXIEncoder.java
@@ -12,6 +12,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import javax.xml.transform.Transformer;
@@ -48,7 +49,13 @@ public final class NetconfMessageToEXIEncoder extends MessageToByteEncoder<Netco
         LOG.trace("Sent to encode : {}", msg);
 
         try (final OutputStream os = new ByteBufOutputStream(out)) {
-            transmogrifier.setOutputStream(os);
+            /*
+             * Writing directly into ByteButOutputStream is costly due to sanity checks
+             * done by Netty. Instantiating a frontend buffer improves efficiency here,
+             * because OpenEXI performs many of single-byte writes.
+             */
+            transmogrifier.setOutputStream(new BufferedOutputStream(os));
+
             final ContentHandler handler = transmogrifier.getSAXTransmogrifier();
             final Transformer transformer = ThreadLocalTransformers.getDefaultTransformer();
             transformer.transform(new DOMSource(msg.getDocument()), new SAXResult(handler));
