eee2d42a911e9da2e62bba8b303f7dca5765785d
Thanh Ha <thanh.ha@linuxfoundation.org>
2014-12-09 17:49:34 -0500

    Fix checkstyle if-statements must use braces logback-config
    Change-Id: Ie857c55d72e6ab582a9abde8bf7e649e23f8323a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- opendaylight/config/logback-config/src/main/java/org/opendaylight/controller/config/yang/logback/config/LogbackStatusListener.java
+++ opendaylight/config/logback-config/src/main/java/org/opendaylight/controller/config/yang/logback/config/LogbackStatusListener.java
@@ -106,8 +106,9 @@ public class LogbackStatusListener implements StatusListener, LogbackRuntimeMXBe
 
     @Override
     public void close() throws IOException {
-        if (reg != null)
+        if (reg != null) {
             reg.close();
+        }
         unregisterFromLogback();
     }
 
