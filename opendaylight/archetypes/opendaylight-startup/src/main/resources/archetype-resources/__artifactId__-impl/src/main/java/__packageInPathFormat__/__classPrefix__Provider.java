a6506ca21f257d4cbfe9312937392cf0349e3e67
Ed Warnicke <eaw@cisco.com>
2015-01-12 17:26:54 -0600

    Working opendaylight-startup archetype
    Change-Id: I48b68884ccae84379fe9896ba9d660eea5cd37d6
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ opendaylight/archetypes/opendaylight-startup/src/main/resources/archetype-resources/__artifactId__-impl/src/main/java/__packageInPathFormat__/__classPrefix__Provider.java
@@ -0,0 +1,32 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * ${copyright} and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package ${package};
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ${classPrefix}Provider implements BindingAwareProvider, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(${classPrefix}Provider.class);
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("${classPrefix}Provider Session Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("${classPrefix}Provider Closed");
+    }
+
+}
