2291c5b778844fc83d5c707aae1274219164ba2c
Tony Tkacik <ttkacik@cisco.com>
2015-01-21 14:10:12 +0100

    Bug 1435: Introduced Binding Data Commit Cohort & Validation APIs
    Introduced Binding Data Broker level APIs for 3PC commit cohorts
    which may validate data and/or participate in data processing
    during their commit.
    
    Change-Id: If3c7c00409e230c47619a2ca59e39e2b4495e596
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/DataCommitCohortRegistration.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package org.opendaylight.controller.md.sal.binding.api;
+
+import org.opendaylight.yangtools.concepts.ObjectRegistration;
+
+public interface DataCommitCohortRegistration<T extends DataCommitCohort> extends ObjectRegistration<T> {
+
+    @Override
+    public void close();
+
+}
