e5e4f25a3a8acef27f5f48196c2a3aa774a0b8e5
Jan Hajnar <jhajnar@cisco.com>
2015-01-21 14:59:34 +0100

    Bug 2592 - XSQL: "Address already in use" reported by MD-SAL when restarting Karaf
    * added method to close open server sockets on bundle unload.
    * made XSQLAdapter non-singleton instance.
    
    Change-Id: I0617450e3071494edbdfb36bda44633d0c9e78b3
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-dom-xsql/src/main/java/org/opendaylight/yang/gen/v1/http/netconfcentral/org/ns/xsql/rev140626/XSQLModule.java
+++ opendaylight/md-sal/sal-dom-xsql/src/main/java/org/opendaylight/yang/gen/v1/http/netconfcentral/org/ns/xsql/rev140626/XSQLModule.java
@@ -2,8 +2,12 @@ package org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.xsql.rev140626;
 
 import org.opendaylight.controller.md.sal.dom.xsql.XSQLAdapter;
 import org.opendaylight.xsql.XSQLProvider;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 
 public class XSQLModule extends org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.xsql.rev140626.AbstractXSQLModule {
+    public static XSQLAdapter xsqlAdapter;
+
     public XSQLModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
@@ -19,12 +23,22 @@ public class XSQLModule extends org.opendaylight.yang.gen.v1.http.netconfcentral
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        XSQLAdapter xsqlAdapter = XSQLAdapter.getInstance();
-        getSchemaServiceDependency().registerSchemaContextListener(xsqlAdapter);
+        xsqlAdapter = new XSQLAdapter();
+        final ListenerRegistration<SchemaContextListener> schemaListenerReg = getSchemaServiceDependency()
+                .registerSchemaContextListener(xsqlAdapter);
         xsqlAdapter.setDataBroker(getAsyncDataBrokerDependency());
-        XSQLProvider p = new XSQLProvider();
-        p.buildXSQL(getDataBrokerDependency());
-        return p;
+        XSQLProvider provider = new XSQLProvider();
+        provider.buildXSQL(getDataBrokerDependency());
+        return new AutoCloseable() {
+            @Override
+            public void close() throws Exception {
+                schemaListenerReg.close();
+                xsqlAdapter.close();
+            }
+        };
     }
 
+    public static XSQLAdapter getXSQLAdapter() {
+        return xsqlAdapter;
+    }
 }
