7973aa62756ca14dad0580a4b5e785445f4cf93f
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-01-28 17:50:13 -0500

    Added RESTConf for karaf feature / bundle management (DO NOT MERGE)
    Change-Id: I2b542a42e80f2ccc261080e5e978ac490bb0b501
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ opendaylight/odl-features-service/bundles-service-impl/src/main/java/org/opendaylight/controller/config/yang/config/bundles_service/impl/BundlesServiceImplModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.controller.config.yang.config.bundles_service.impl;
+public class BundlesServiceImplModule extends org.opendaylight.controller.config.yang.config.bundles_service.impl.AbstractBundlesServiceImplModule {
+    public BundlesServiceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public BundlesServiceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.bundles_service.impl.BundlesServiceImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
