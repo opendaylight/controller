5231fa8e384a62f62f64f5e98f655d2c056c8f34
Robert Varga <rovarga@cisco.com>
2015-02-02 14:46:16 +0100

    BUG-2573: add NormalizedNode-based RPC API
    This API introduces a NormalizedNode-based API for invoking RPCs,
    supplanting the CompositeNodes-based one.
    
    Change-Id: I256f83e80fc4a7d169bbfb60c5738ca489d16aeb
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/md/sal/dom/api/DOMRpcAvailabilityListenerRegistration.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.api;
+
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+
+/**
+ * Registration handle of a {@link DOMRpcAvailabilityListener}. Invoke {@link #close()} to stop
+ * the listener from being notified.
+ */
+public interface DOMRpcAvailabilityListenerRegistration extends ListenerRegistration<DOMRpcAvailabilityListener> {
+
+}
---
+++ opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/md/sal/dom/api/DOMRpcIdentifier.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.api;
+
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+
+// FIXME: document this concept
+public abstract class DOMRpcIdentifier {
+    private static final class Global extends DOMRpcIdentifier {
+        private Global(final @Nonnull SchemaPath type) {
+            super(type);
+        }
+
+        @Override
+        public YangInstanceIdentifier getContextReference() {
+            return null;
+        }
+    }
+
+    private static final class Local extends DOMRpcIdentifier {
+        private final YangInstanceIdentifier contextReference;
+
+        private Local(final @Nonnull SchemaPath type, final @Nonnull YangInstanceIdentifier contextReference) {
+            super(type);
+            this.contextReference = Preconditions.checkNotNull(contextReference);
+        }
+
+        @Override
+        public YangInstanceIdentifier getContextReference() {
+            return contextReference;
+        }
+    }
+
+    private final SchemaPath type;
+
+    private DOMRpcIdentifier(final SchemaPath type) {
+        this.type = Preconditions.checkNotNull(type);
+    }
+
+    // FIXME: javadoc
+    public static DOMRpcIdentifier create(final @Nonnull SchemaPath type) {
+        return new Global(type);
+    }
+
+    // FIXME: javadoc
+    public static DOMRpcIdentifier create(final @Nonnull SchemaPath type, final @Nullable YangInstanceIdentifier contextReference) {
+        if (contextReference == null) {
+            return new Global(type);
+        } else {
+            return new Local(type, contextReference);
+        }
+    }
+
+    // FIXME: javadoc
+    public final @Nonnull SchemaPath getType() {
+        return type;
+    }
+
+    // FIXME: javadoc
+    public abstract @Nullable YangInstanceIdentifier getContextReference();
+
+    // FIXME: hashcode/equals
+}
