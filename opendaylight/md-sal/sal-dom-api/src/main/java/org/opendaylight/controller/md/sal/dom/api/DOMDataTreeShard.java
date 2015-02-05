8c08f58b7bfa72f1027d90e1e80e1d1e90458c7f
Robert Varga <rovarga@cisco.com>
2015-02-05 18:30:28 +0100

    DO NOT MERGE: Preliminary DOMDataTree APIs
    This patch introduces the end-user visible API model for interacting
    with the conceptual data tree. Unlike our previous APIs, these revolve
    around applications explicitly binding their inputs (former data change
    listeners) and outputs (former explicit writes). This allows efficient
    inter-datastore interactions to be maintained by understanding what
    sort of output applications make and what sort of indexes are required
    to feed them the data they require. Beryllium evolution will see the
    DOMDataTreeListener being subscribed to an XQuery expression, thus
    providing a dense definition of what data the applciation actually
    extracts out of the subtree.
    
    An SPI-type interface will be added, which will allow individual data
    store implementations to plug into this mechanism driver, effectively
    becoming shards of the overall tree.
    
    Change-Id: I11446ce7f8dbd7cf61540f72e463460b483ff15f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-api/src/main/java/org/opendaylight/controller/md/sal/dom/api/DOMDataTreeShard.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.dom.api;
+
+import java.util.EventListener;
+import javax.annotation.Nonnull;
+
+/**
+ * A single shard of the conceptual data tree. This interface defines the basic notifications
+ * a shard can receive. Each shard implementation is expected to also implement some of the
+ * datastore-level APIs. Which interfaces are required depends on the {@link DOMDataTreeShardingService}
+ * implementation.
+ */
+public interface DOMDataTreeShard extends EventListener {
+    void onChildAttached(@Nonnull DOMDataTreeIdentifier prefix, @Nonnull DOMDataTreeShard child);
+    void onChildDetached(@Nonnull DOMDataTreeIdentifier prefix, @Nonnull DOMDataTreeShard child);
+}
