2aaaf063b034288d0d6eb8f276a449dcb53b279a
Moiz Raja <moraja@cisco.com>
2015-02-12 12:22:11 -0800

    Try to eliminate random failures in RaftActorTest
    This patch attempts to add the following enhancements,
    1. All test actors get created using a TestActorFactory which when closed
       automatically kills the actors created by it
    2. Provide a method to auto generate a friendly actor name/id for cases
       where we need to use such names
    
    Change-Id: Ia1fd81f08baf00dfc614dfa8466be2a62f18952d
    Signed-off-by: Moiz Raja <moraja@cisco.com>

---
+++ opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/TestActorFactory.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.raft;
+
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.testkit.TestActorRef;
+import java.util.LinkedList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TestActorFactory provides methods to create both normal and test actors and to kill them when the factory is closed
+ * The ideal usage for TestActorFactory is with try with resources, <br/>
+ * For example <br/>
+ * <pre>
+ *     try (TestActorFactory factory = new TestActorFactory(getSystem())){
+ *         factory.createActor(props);
+ *         factory.createTestActor(props);
+ *         factory.generateActorId("leader-");
+ *     }
+ * </pre>
+ */
+public class TestActorFactory implements AutoCloseable {
+    private final ActorSystem system;
+    List<ActorRef> createdActors = new LinkedList<>();
+    Logger LOG = LoggerFactory.getLogger(getClass());
+    private static int actorCount = 1;
+
+    public TestActorFactory(ActorSystem system){
+        this.system = system;
+    }
+
+    /**
+     * Create a normal actor with an auto-generated name
+     *
+     * @param props
+     * @return
+     */
+    public ActorRef createActor(Props props){
+        ActorRef actorRef = system.actorOf(props);
+        createdActors.add(actorRef);
+        return actorRef;
+    }
+
+    /**
+     * Create a normal actor with the passed in name
+     * @param props
+     * @param actorId name of actor
+     * @return
+     */
+    public ActorRef createActor(Props props, String actorId){
+        ActorRef actorRef = system.actorOf(props, actorId);
+        createdActors.add(actorRef);
+        return actorRef;
+    }
+
+    /**
+     * Create a test actor with the passed in name
+     * @param props
+     * @param actorId
+     * @param <T>
+     * @return
+     */
+    public <T extends Actor> TestActorRef<T> createTestActor(Props props, String actorId){
+        TestActorRef<T> actorRef = TestActorRef.create(system, props, actorId);
+        createdActors.add(actorRef);
+        return actorRef;
+    }
+
+    /**
+     * Create a test actor with an auto-generated name
+     * @param props
+     * @param <T>
+     * @return
+     */
+    public <T extends Actor> TestActorRef<T> createTestActor(Props props){
+        TestActorRef<T> actorRef = TestActorRef.create(system, props);
+        createdActors.add(actorRef);
+        return actorRef;
+    }
+
+    /**
+     * Generate a friendly but unique actor id/name
+     * @param prefix
+     * @return
+     */
+    public String generateActorId(String prefix){
+        return prefix + actorCount++;
+    }
+
+    @Override
+    public void close() throws Exception {
+        for(ActorRef actor : createdActors){
+            LOG.info("Killing actor {}", actor);
+            actor.tell(PoisonPill.getInstance(), null);
+        }
+    }
+}
