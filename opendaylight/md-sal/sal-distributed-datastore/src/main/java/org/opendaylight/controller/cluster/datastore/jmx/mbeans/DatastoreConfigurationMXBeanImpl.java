778ad1926981ab366a06894a1ca2d27613541690
tpantelis <tpanteli@brocade.com>
2015-02-18 07:37:53 -0500

    Use Config Admin to overlay datastore properties
    Added functionality to configure DatastoreContext settings via a
    properties file, org.opendaylight.controller.cluster.datastore.cfg, in the karaf
    etc dir. The properties are overlayed onto the DatastoreContext that is
    initialized via the config yang XML file. This allows for separation of
    the wiring in the config yang XML from the user-facing config settings
    to facilitate preserving the user-facing config settings on upgrade
    while applying wiring changes.
    
    The DatastoreContextIntrospector class is responsible for applying the
    property key/values obtained from the Config Admin Configuration to the
    current DatastoreContext.
    
    The DatastoreContextConfigAdminOverlay class interfaces with the
    ConfigurationAdmin OSGi service to obtain the configuration and calls
    into the DatastoreContextIntrospector.
    
    Modified DatastoreContext.Builder to support loading from an existing
    DatastoreContext instance and applying changes to it to yield a new
    instance on build.
    
    Added a DatastoreConfigurationMXBean that reports the DatastoreContext
    settings via JXM. This is useful to see what settings are currently
    applied.
    
    Most of the other changes were to wire the new classes from the Module
    class to the DistributedDatastore.
    
    Added new unit tests to cover the new code.
    
    Change-Id: I9427907dac576bd416bad677a56fa8259af59a09
    Signed-off-by: tpantelis <tpanteli@brocade.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/jmx/mbeans/DatastoreConfigurationMXBeanImpl.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.jmx.mbeans;
+
+import org.opendaylight.controller.cluster.datastore.DatastoreContext;
+import org.opendaylight.controller.md.sal.common.util.jmx.AbstractMXBean;
+
+/**
+ * Implementation of DatastoreConfigurationMXBean.
+ *
+ * @author Thomas Pantelis
+ */
+public class DatastoreConfigurationMXBeanImpl extends AbstractMXBean implements DatastoreConfigurationMXBean {
+    public static final String JMX_CATEGORY_CONFIGURATION = "Configuration";
+
+    private DatastoreContext context;
+
+    public DatastoreConfigurationMXBeanImpl(String mxBeanType) {
+        super("Datastore", mxBeanType, JMX_CATEGORY_CONFIGURATION);
+    }
+
+    public void setContext(DatastoreContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public long getShardTransactionIdleTimeoutInSeconds() {
+        return context.getShardTransactionIdleTimeout().toSeconds();
+    }
+
+    @Override
+    public long getOperationTimeoutInSeconds() {
+        return context.getOperationTimeoutInSeconds();
+    }
+
+    @Override
+    public long getShardHeartbeatIntervalInMillis() {
+        return context.getShardRaftConfig().getHeartBeatInterval().toMillis();
+    }
+
+    @Override
+    public int getShardJournalRecoveryLogBatchSize() {
+        return context.getShardRaftConfig().getJournalRecoveryLogBatchSize();
+    }
+
+    @Override
+    public long getShardIsolatedLeaderCheckIntervalInMillis() {
+        return context.getShardRaftConfig().getIsolatedCheckInterval().toMillis();
+    }
+
+    @Override
+    public long getShardElectionTimeoutFactor() {
+        return context.getShardRaftConfig().getElectionTimeoutFactor();
+    }
+
+    @Override
+    public int getShardSnapshotDataThresholdPercentage() {
+        return context.getShardRaftConfig().getSnapshotDataThresholdPercentage();
+    }
+
+    @Override
+    public long getShardSnapshotBatchCount() {
+        return context.getShardRaftConfig().getSnapshotBatchCount();
+    }
+
+    @Override
+    public long getShardTransactionCommitTimeoutInSeconds() {
+        return context.getShardTransactionCommitTimeoutInSeconds();
+    }
+
+    @Override
+    public int getShardTransactionCommitQueueCapacity() {
+        return context.getShardTransactionCommitQueueCapacity();
+    }
+
+    @Override
+    public long getShardInitializationTimeoutInSeconds() {
+        return context.getShardInitializationTimeout().duration().toSeconds();
+    }
+
+    @Override
+    public long getShardLeaderElectionTimeoutInSeconds() {
+        return context.getShardLeaderElectionTimeout().duration().toSeconds();
+    }
+
+    @Override
+    public boolean isPersistent() {
+        return context.isPersistent();
+    }
+
+    @Override
+    public long getTransactionCreationInitialRateLimit() {
+        return context.getTransactionCreationInitialRateLimit();
+    }
+
+    @Override
+    public int getMaxShardDataChangeExecutorPoolSize() {
+        return context.getDataStoreProperties().getMaxDataChangeExecutorPoolSize();
+    }
+
+    @Override
+    public int getMaxShardDataChangeExecutorQueueSize() {
+        return context.getDataStoreProperties().getMaxDataChangeExecutorQueueSize();
+    }
+
+    @Override
+    public int getMaxShardDataChangeListenerQueueSize() {
+        return context.getDataStoreProperties().getMaxDataChangeListenerQueueSize();
+    }
+
+    @Override
+    public int getMaxShardDataStoreExecutorQueueSize() {
+        return context.getDataStoreProperties().getMaxDataStoreExecutorQueueSize();
+    }
+}
--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/distributed_datastore_provider/DistributedConfigDataStoreProviderModule.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/distributed_datastore_provider/DistributedConfigDataStoreProviderModule.java
@@ -1,13 +1,8 @@
 package org.opendaylight.controller.config.yang.config.distributed_datastore_provider;
 
-import java.util.concurrent.TimeUnit;
-
 import org.opendaylight.controller.cluster.datastore.DatastoreContext;
 import org.opendaylight.controller.cluster.datastore.DistributedDataStoreFactory;
-import org.opendaylight.controller.md.sal.dom.store.impl.InMemoryDOMDataStoreConfigProperties;
 import org.osgi.framework.BundleContext;
-
-import scala.concurrent.duration.Duration;
 
 public class DistributedConfigDataStoreProviderModule extends
     org.opendaylight.controller.config.yang.config.distributed_datastore_provider.AbstractDistributedConfigDataStoreProviderModule {
@@ -42,23 +37,19 @@ public class DistributedConfigDataStoreProviderModule extends
 
         DatastoreContext datastoreContext = DatastoreContext.newBuilder()
                 .dataStoreType("config")
-                .dataStoreProperties(InMemoryDOMDataStoreConfigProperties.create(
-                        props.getMaxShardDataChangeExecutorPoolSize().getValue().intValue(),
-                        props.getMaxShardDataChangeExecutorQueueSize().getValue().intValue(),
-                        props.getMaxShardDataChangeListenerQueueSize().getValue().intValue(),
-                        props.getMaxShardDataStoreExecutorQueueSize().getValue().intValue()))
-                .shardTransactionIdleTimeout(Duration.create(
-                        props.getShardTransactionIdleTimeoutInMinutes().getValue(), TimeUnit.MINUTES))
+                .maxShardDataChangeExecutorPoolSize(props.getMaxShardDataChangeExecutorPoolSize().getValue().intValue())
+                .maxShardDataChangeExecutorQueueSize(props.getMaxShardDataChangeExecutorQueueSize().getValue().intValue())
+                .maxShardDataChangeListenerQueueSize(props.getMaxShardDataChangeListenerQueueSize().getValue().intValue())
+                .maxShardDataStoreExecutorQueueSize(props.getMaxShardDataStoreExecutorQueueSize().getValue().intValue())
+                .shardTransactionIdleTimeoutInMinutes(props.getShardTransactionIdleTimeoutInMinutes().getValue())
                 .operationTimeoutInSeconds(props.getOperationTimeoutInSeconds().getValue())
                 .shardJournalRecoveryLogBatchSize(props.getShardJournalRecoveryLogBatchSize().
                         getValue().intValue())
                 .shardSnapshotBatchCount(props.getShardSnapshotBatchCount().getValue().intValue())
                 .shardSnapshotDataThresholdPercentage(props.getShardSnapshotDataThresholdPercentage().getValue().intValue())
-                .shardHeartbeatIntervalInMillis(props.getShardHearbeatIntervalInMillis().getValue())
-                .shardInitializationTimeout(props.getShardInitializationTimeoutInSeconds().getValue(),
-                        TimeUnit.SECONDS)
-                .shardLeaderElectionTimeout(props.getShardLeaderElectionTimeoutInSeconds().getValue(),
-                        TimeUnit.SECONDS)
+                .shardHeartbeatIntervalInMillis(props.getShardHeartbeatIntervalInMillis().getValue())
+                .shardInitializationTimeoutInSeconds(props.getShardInitializationTimeoutInSeconds().getValue())
+                .shardLeaderElectionTimeoutInSeconds(props.getShardLeaderElectionTimeoutInSeconds().getValue())
                 .shardTransactionCommitTimeoutInSeconds(
                         props.getShardTransactionCommitTimeoutInSeconds().getValue().intValue())
                 .shardTransactionCommitQueueCapacity(
@@ -67,7 +58,7 @@ public class DistributedConfigDataStoreProviderModule extends
                 .shardIsolatedLeaderCheckIntervalInMillis(
                     props.getShardIsolatedLeaderCheckIntervalInMillis().getValue())
                 .shardElectionTimeoutFactor(props.getShardElectionTimeoutFactor().getValue())
-                .transactionCreationInitialRateLimit(props.getTxCreationInitialRateLimit().getValue())
+                .transactionCreationInitialRateLimit(props.getTransactionCreationInitialRateLimit().getValue())
                 .build();
 
         return DistributedDataStoreFactory.createInstance(getConfigSchemaServiceDependency(),
