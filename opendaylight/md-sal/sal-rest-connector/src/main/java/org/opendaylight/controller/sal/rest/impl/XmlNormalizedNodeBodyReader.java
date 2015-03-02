518fe70bf6e613f11b7836e80c76c6d45e207398
Vaclav Demcak <vdemcak@cisco.com>
2015-03-02 15:18:03 +0100

    BUG 2412 - restconf @POST createConfigurationData method migration
    * migration to new faster Infrastructure API and Codecs for method
    @POST createConfigurationData(String,NormalizedNodeContext,UriInfo)
    on @Path {/config/identifier}
    
    New faster Infrastructure API works with NormizedNodeContext and
    we are replacing createConfigurationData method from RestconfService
    to use NormalizedNodeContext.
    
    * add additional functionality to find a correct child DataSchemaNode
    for @POST input payload in XmlNormalizedNodeBodyReader
    
    * add fix or comment tests - problem with RestconfDocumentedExceptionMapper
          - it has to be fixed in future commit in this chain
    
    Change-Id: I8348002aaa817bc4816a0d132f5811fa84eda389
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/XmlNormalizedNodeBodyReader.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/XmlNormalizedNodeBodyReader.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import javax.ws.rs.Consumes;
@@ -34,6 +35,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlUtils;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 import org.slf4j.Logger;
@@ -51,14 +53,14 @@ public class XmlNormalizedNodeBodyReader extends AbstractIdentifierAwareJaxRsPro
     private static final DocumentBuilderFactory BUILDERFACTORY;
 
     static {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
             factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
             factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
             factory.setXIncludeAware(false);
             factory.setExpandEntityReferences(false);
-        } catch (ParserConfigurationException e) {
+        } catch (final ParserConfigurationException e) {
             throw new ExceptionInInitializerError(e);
         }
         factory.setNamespaceAware(true);
@@ -80,19 +82,19 @@ public class XmlNormalizedNodeBodyReader extends AbstractIdentifierAwareJaxRsPro
             final MultivaluedMap<String, String> httpHeaders, final InputStream entityStream) throws IOException,
             WebApplicationException {
         try {
-            Optional<InstanceIdentifierContext> path = getIdentifierWithSchema();
+            final Optional<InstanceIdentifierContext> path = getIdentifierWithSchema();
 
             final DocumentBuilder dBuilder;
             try {
                 dBuilder = BUILDERFACTORY.newDocumentBuilder();
-            } catch (ParserConfigurationException e) {
+            } catch (final ParserConfigurationException e) {
                 throw new RuntimeException("Failed to parse XML document", e);
             }
-            Document doc = dBuilder.parse(entityStream);
+            final Document doc = dBuilder.parse(entityStream);
 
-            NormalizedNode<?, ?> result = parse(path.get(),doc);
+            final NormalizedNode<?, ?> result = parse(path.get(),doc);
             return new NormalizedNodeContext(path.get(),result);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             LOG.debug("Error parsing json input", e);
 
             throw new RestconfDocumentedException("Error parsing input: " + e.getMessage(), ErrorType.PROTOCOL,
@@ -100,13 +102,29 @@ public class XmlNormalizedNodeBodyReader extends AbstractIdentifierAwareJaxRsPro
         }
     }
 
-    private static NormalizedNode<?,?> parse(InstanceIdentifierContext pathContext,Document doc) {
-        List<Element> elements = Collections.singletonList(doc.getDocumentElement());
+    private static NormalizedNode<?,?> parse(final InstanceIdentifierContext pathContext,final Document doc) {
+
+        final List<Element> elements = Collections.singletonList(doc.getDocumentElement());
         DataSchemaNode schemaNode = pathContext.getSchemaNode();
+
+        final String docRootElm = doc.getDocumentElement().getLocalName();
+        final String schemaNodeName = pathContext.getSchemaNode().getQName().getLocalName();
+
+        // TODO : do we want to really follow netconf-restconf specification ?
+        if (!schemaNodeName.equalsIgnoreCase(docRootElm)) {
+            final Collection<DataSchemaNode> children = ((DataNodeContainer) schemaNode).getChildNodes();
+            for (final DataSchemaNode child : children) {
+                if (child.getQName().getLocalName().equalsIgnoreCase(docRootElm)) {
+                    schemaNode = child;
+                    break;
+                }
+            }
+        }
+
         if(schemaNode instanceof ContainerSchemaNode) {
             return DOM_PARSER_FACTORY.getContainerNodeParser().parse(Collections.singletonList(doc.getDocumentElement()), (ContainerSchemaNode) schemaNode);
         } else if(schemaNode instanceof ListSchemaNode) {
-            ListSchemaNode casted = (ListSchemaNode) schemaNode;
+            final ListSchemaNode casted = (ListSchemaNode) schemaNode;
             return DOM_PARSER_FACTORY.getMapEntryNodeParser().parse(elements, casted);
         }
         return null;
