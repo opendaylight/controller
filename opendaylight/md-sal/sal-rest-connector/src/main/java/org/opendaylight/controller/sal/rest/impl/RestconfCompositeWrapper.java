d0247556cbbe79066e0f37139f931007711d9c72
Jan Hajnar <jhajnar@cisco.com>
2015-03-02 22:28:46 +0100

    BUG 2412 - restconf @POST invokeRpc for notification subscription
    * modified notification subscription to work on new api
    * added conversion to normalized instance identifier in notification
    subscription path
    * removed mixin nodes from data change event path
    * modified returned paths to skip augmentation identifiers in change
    event path
    * modified returned location header entry on get stream to use "ws"
    protocol
    
    Change-Id: If7f5e92eedbebe0daeb149a38d429a1dcb47c1c9
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/RestconfCompositeWrapper.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/RestconfCompositeWrapper.java
@@ -7,7 +7,6 @@ import org.opendaylight.controller.md.sal.rest.schema.SchemaExportContext;
 import org.opendaylight.controller.md.sal.rest.schema.SchemaRetrievalService;
 import org.opendaylight.controller.sal.rest.api.RestconfService;
 import org.opendaylight.controller.sal.restconf.impl.NormalizedNodeContext;
-import org.opendaylight.controller.sal.restconf.impl.StructuredData;
 
 public class RestconfCompositeWrapper implements RestconfService, SchemaRetrievalService {
 
