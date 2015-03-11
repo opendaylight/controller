03116ef756b565f99075f3a0b7ba90ff4b5dc352
Vaclav Demcak <vdemcak@cisco.com>
2015-03-11 14:13:22 +0100

    Fix bug 2821 - restconf RPC parsing returns http status 500
    * json input read @Provider has to change RpcDefinition SchemaNode
    to Input ContainerSchemaNode for a correct parsing RPC input data from stream
    
    * no-payload Rpc - Rpc Call could be without payload, but jersey always does
    to parse empty stream to NormalizedNode so we add check for stream.available
    before start parsing input NormalizedNode from stream and return NNContext
    with null data. All checks for data exist have to be implemented for every
    method with NormalizedNodeContext.
    
    * fix RpcOutput NormalzidNodeContext in RestconfImpl - we expect to have
    RpcDefinition as reultNodeSchema and data are payloaded inside as Output obj.
    
    * fix NormalizedNodeJsonBodyWriter and NormalizedNodeXmlBodyWriter for RpcDefinition
    - RpcDefinition has no child elements so RpcDefinition.output has to be a root
    DataSchemaNode for json parser
    
    * add JsonBodyReader/Writer test suite
    
    Change-Id: I31273bee937f57fa1551a33fcc4d6c81b984afb6
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/NormalizedNodeXmlBodyWriter.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/rest/impl/NormalizedNodeXmlBodyWriter.java
@@ -8,7 +8,6 @@
 package org.opendaylight.controller.sal.rest.impl;
 
 import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
@@ -37,9 +36,9 @@ import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStre
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XMLStreamNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 
 @Provider
 @Produces({ Draft02.MediaTypes.API + RestconfService.XML, Draft02.MediaTypes.DATA + RestconfService.XML,
@@ -70,7 +69,7 @@ public class NormalizedNodeXmlBodyWriter implements MessageBodyWriter<Normalized
             final Annotation[] annotations, final MediaType mediaType,
             final MultivaluedMap<String, Object> httpHeaders, final OutputStream entityStream) throws IOException,
             WebApplicationException {
-        final InstanceIdentifierContext pathContext = t.getInstanceIdentifierContext();
+        final InstanceIdentifierContext<?> pathContext = t.getInstanceIdentifierContext();
         if (t.getData() == null) {
             return;
         }
@@ -86,29 +85,21 @@ public class NormalizedNodeXmlBodyWriter implements MessageBodyWriter<Normalized
         NormalizedNode<?, ?> data = t.getData();
         SchemaPath schemaPath = pathContext.getSchemaNode().getPath();
 
-        // The utility method requires the path to be size of 2
-        boolean isRpc = false;
-        if(Iterables.size(schemaPath.getPathFromRoot()) > 1) {
-            isRpc = SchemaContextUtil.getRpcDataSchema(t.getInstanceIdentifierContext().getSchemaContext(), schemaPath) != null;
-        }
-
         boolean isDataRoot = false;
         if (SchemaPath.ROOT.equals(schemaPath)) {
             isDataRoot = true;
-        // The rpc definitions required the schema path to point to the output container, not the parent (rpc itself)
+        }  else if (pathContext.getSchemaNode() instanceof RpcDefinition) {
+            isDataRoot = true;
+            schemaPath = ((RpcDefinition) pathContext.getSchemaNode()).getOutput().getPath();
         } else {
-            if(!isRpc) {
-                schemaPath = schemaPath.getParent();
-            }
+            schemaPath = schemaPath.getParent();
         }
 
         final NormalizedNodeStreamWriter jsonWriter = XMLStreamNormalizedNodeStreamWriter.create(xmlWriter,
                 pathContext.getSchemaContext(), schemaPath);
         final NormalizedNodeWriter nnWriter = NormalizedNodeWriter.forStreamWriter(jsonWriter);
         if (isDataRoot) {
-            writeRootElement(xmlWriter, nnWriter, (ContainerNode) data, SchemaContext.NAME);
-        } else if(isRpc) {
-            writeRootElement(xmlWriter, nnWriter, (ContainerNode) data, schemaPath.getLastComponent());
+            writeRootElement(xmlWriter, nnWriter, (ContainerNode) data);
         } else {
             if (data instanceof MapEntryNode) {
                 // Restconf allows returning one list item. We need to wrap it
@@ -120,9 +111,10 @@ public class NormalizedNodeXmlBodyWriter implements MessageBodyWriter<Normalized
         }
     }
 
-    private void writeRootElement(final XMLStreamWriter xmlWriter, final NormalizedNodeWriter nnWriter, final ContainerNode data, final QName name)
+    private void writeRootElement(final XMLStreamWriter xmlWriter, final NormalizedNodeWriter nnWriter, final ContainerNode data)
             throws IOException {
         try {
+            final QName name = SchemaContext.NAME;
             xmlWriter.writeStartElement(name.getNamespace().toString(), name.getLocalName());
             for (final DataContainerChild<? extends PathArgument, ?> child : data.getValue()) {
                 nnWriter.write(child);
