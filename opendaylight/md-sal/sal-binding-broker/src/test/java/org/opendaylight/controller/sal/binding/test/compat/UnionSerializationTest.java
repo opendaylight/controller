3d37c10b6223138c19faca65e04ee1b5e33fa478
Jan Hajnar <jhajnar@cisco.com>
2015-03-16 12:00:21 +0100

    BUG 2412 - remove CompositeNode from sal-dom-broker
    * removed CompositeNodes from sal-dom-broker
    * removed deprecated services from sal-dom-broker
    * removed deprecated service calls from sal-dom-api
    * removed deprecated api calls from sal-binding-broker tests and
    sal-dom-spi
    
    Change-Id: Iae44e5c0acd0ad602bf98b9961fcd808657ac07b
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/sal/binding/test/compat/UnionSerializationTest.java
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/sal/binding/test/compat/UnionSerializationTest.java
@@ -7,16 +7,8 @@
  */
 package org.opendaylight.controller.sal.binding.test.compat;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
 import org.opendaylight.controller.sal.binding.test.AbstractDataServiceTest;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.nt.l3.unicast.igp.topology.rev131021.igp.node.attributes.igp.node.attributes.Prefix;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.nt.l3.unicast.igp.topology.rev131021.igp.node.attributes.igp.node.attributes.PrefixBuilder;
-import org.opendaylight.yangtools.yang.data.api.CompositeNode;
 
 public class UnionSerializationTest extends AbstractDataServiceTest {
 
@@ -25,21 +17,7 @@ public class UnionSerializationTest extends AbstractDataServiceTest {
 
     @Test
     public void testPrefixSerialization() throws Exception {
-
-        Ipv4Prefix ipv4prefix = new Ipv4Prefix(PREFIX_STRING);
-        IpPrefix ipPrefix = new IpPrefix(ipv4prefix);
-        Prefix prefix = new PrefixBuilder().setPrefix(ipPrefix).build();
-
-        CompositeNode serialized = testContext.getBindingToDomMappingService().toDataDom(prefix);
-        assertNotNull(serialized);
-        assertNotNull(serialized.getFirstSimpleByName(Prefix.QNAME));
-        assertEquals(PREFIX_STRING, serialized.getFirstSimpleByName(Prefix.QNAME).getValue());
-
-        Prefix deserialized = (Prefix) testContext.getBindingToDomMappingService().dataObjectFromDataDom(Prefix.class, serialized);
-        assertNotNull(deserialized);
-        assertNotNull(deserialized.getPrefix());
-        assertNotNull(deserialized.getPrefix().getIpv4Prefix());
-        assertEquals(PREFIX_STRING, deserialized.getPrefix().getIpv4Prefix().getValue());
+        //TODO: Rewrite using NormalizedNodes
     }
 
 }
