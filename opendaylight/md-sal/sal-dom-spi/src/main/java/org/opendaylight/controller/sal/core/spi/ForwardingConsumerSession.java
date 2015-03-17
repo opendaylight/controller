59bcd1c9dcf3c2f15cbd4085e746c72ec9d88840
Tony Tkacik <ttkacik@cisco.com>
2015-03-17 15:58:37 +0100

    Bug 868: Introduced DOM ForwardingSession utility classes.
    Introduced DOM ForwardingSession abstract classes,
    in order to migrate AAA from CompositeNode interfaces,
    since they were using it only to forward calls to backing
    implementation.
    
    Change-Id: If5ed1a5dadc02c0d7433fd9652a4af9b586a9be7
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-spi/src/main/java/org/opendaylight/controller/sal/core/spi/ForwardingConsumerSession.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.core.spi;
+
+import java.util.concurrent.Future;
+import org.opendaylight.controller.sal.core.api.Broker.ConsumerSession;
+import org.opendaylight.controller.sal.core.api.BrokerService;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.data.api.CompositeNode;
+
+public abstract class ForwardingConsumerSession implements ConsumerSession {
+
+    protected abstract ConsumerSession delegate();
+
+    @Override
+    public void close() {
+        delegate().close();
+    }
+
+    @Override
+    public <T extends BrokerService> T getService(Class<T> arg0) {
+        return delegate().getService(arg0);
+    }
+
+    @Override
+    public boolean isClosed() {
+        return delegate().isClosed();
+    }
+
+    @Override
+    @Deprecated
+    public Future<RpcResult<CompositeNode>> rpc(QName arg0, CompositeNode arg1) {
+        return delegate().rpc(arg0, arg1);
+    }
+
+
+}
