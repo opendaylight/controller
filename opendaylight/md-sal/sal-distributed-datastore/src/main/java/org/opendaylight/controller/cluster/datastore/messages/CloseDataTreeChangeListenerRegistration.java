66d39ecc3effd52c96c7a772a46612008e34fbc9
Robert Varga <rovarga@cisco.com>
2015-03-17 11:18:53 +0100

    BUG-2673: make CDS implement DOMDataTreeChangeListener
    This patch adds the base support for registering
    DOMDataTreeChangeListeners. These are delivered only on the local node,
    as efficient serialization requires interaction with the cluster
    topology and shard leadership handoff. That functionality will be
    delivered in a follow-up patch.
    
    It also introduces a bit common infrastructure to be used by
    DataChangeListener code, as it performs a very similar function.
    
    Change-Id: Ifb91d08857684fb160fd923bc25c294d2fca4bc3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/CloseDataTreeChangeListenerRegistration.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.messages;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public final class CloseDataTreeChangeListenerRegistration implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final CloseDataTreeChangeListenerRegistration INSTANCE = new CloseDataTreeChangeListenerRegistration();
+
+    private CloseDataTreeChangeListenerRegistration() {
+    }
+
+    public static CloseDataTreeChangeListenerRegistration getInstance() {
+        return INSTANCE;
+    }
+
+    private Object readResolve() throws ObjectStreamException {
+        return INSTANCE;
+    }
+}
