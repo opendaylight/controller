8a81af762d691b7ba404571d4fe301eaa9d4944e
Igor Bartak <ibartak@cisco.com>
2015-03-17 11:28:46 +0100

    BUG 2799: Migration of Message Bus from deprecated Helium MD-SAL APIs to Lithium API (added unit tests)
    Change-Id: I2206b4b532e4feead26c166b793966b077f0f26f
    Signed-off-by: Igor Bartak <ibartak@cisco.com>

--- opendaylight/md-sal/messagebus-impl/src/main/java/org/opendaylight/controller/messagebus/app/impl/EventSourceTopic.java
+++ opendaylight/md-sal/messagebus-impl/src/main/java/org/opendaylight/controller/messagebus/app/impl/EventSourceTopic.java
@@ -9,36 +9,36 @@
 package org.opendaylight.controller.messagebus.app.impl;
 
 import com.google.common.base.Preconditions;
+import java.util.Map;
 import java.util.regex.Pattern;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
-import org.opendaylight.controller.mdsal.MdSAL;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.messagebus.eventaggregator.rev141202.NotificationPattern;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.messagebus.eventaggregator.rev141202.TopicId;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.messagebus.eventsource.rev141202.EventSourceService;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.messagebus.eventsource.rev141202.JoinTopicInput;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.messagebus.eventsource.rev141202.JoinTopicInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.LoggerFactory;
 
-public class Topic implements DataChangeListener {
-    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(Topic.class);
+public class EventSourceTopic implements DataChangeListener {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(EventSourceTopic.class);
     private final NotificationPattern notificationPattern;
+    private final EventSourceService sourceService;
     private final Pattern nodeIdPattern;
     private final TopicId topicId;
-    private final MdSAL mdSal;
 
-    public Topic(final NotificationPattern notificationPattern, final String nodeIdPattern, final MdSAL mdSal) {
+    public EventSourceTopic(final NotificationPattern notificationPattern, final String nodeIdPattern, final EventSourceService eventSource) {
         this.notificationPattern = Preconditions.checkNotNull(notificationPattern);
+        this.sourceService = eventSource;
 
         // FIXME: regex should be the language of nodeIdPattern
         final String regex = Util.wildcardToRegex(nodeIdPattern);
         this.nodeIdPattern = Pattern.compile(regex);
-        this.mdSal = Preconditions.checkNotNull(mdSal);
+
 
         // FIXME: We need to perform some salting in order to make
         //        the topic IDs less predictable.
@@ -51,26 +51,27 @@ public class Topic implements DataChangeListener {
 
     @Override
     public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> event) {
-        // TODO: affected must return topologyNode !!!
-        final Node node = Util.getAffectedNode(event);
-        if (nodeIdPattern.matcher(node.getId().getValue()).matches()) {
-            notifyNode(node.getId());
-        } else {
-            LOG.debug("Skipping node {}", node.getId());
+                for (final Map.Entry<InstanceIdentifier<?>, DataObject> changeEntry : event.getUpdatedData().entrySet()) {
+            if (changeEntry.getValue() instanceof Node) {
+                final Node node = (Node) changeEntry.getValue();
+                if (nodeIdPattern.matcher(node.getId().getValue()).matches()) {
+                    notifyNode(changeEntry.getKey());
+                }
+            }
         }
     }
 
-    public void notifyNode(final NodeId nodeId) {
-        JoinTopicInput jti = getJoinTopicInputArgument(nodeId);
-        EventSourceService ess = mdSal.getRpcService(EventSourceService.class);
-        Preconditions.checkState(ess != null, "EventSourceService is not registered");
-
-        ess.joinTopic(jti);
+    public void notifyNode(final InstanceIdentifier<?> nodeId) {
+        try {
+            sourceService.joinTopic(getJoinTopicInputArgument(nodeId));
+        } catch (final Exception e) {
+            LOG.error("Could not invoke join topic for node %s", nodeId);
+        }
     }
 
-    private JoinTopicInput getJoinTopicInputArgument(final NodeId nodeId) {
-        NodeRef nodeRef = MdSAL.createNodeRef(nodeId);
-        JoinTopicInput jti =
+    private JoinTopicInput getJoinTopicInputArgument(final InstanceIdentifier<?> path) {
+        final NodeRef nodeRef = new NodeRef(path);
+        final JoinTopicInput jti =
                 new JoinTopicInputBuilder()
                         .setNode(nodeRef.getValue())
                         .setTopicId(topicId)
@@ -78,4 +79,6 @@ public class Topic implements DataChangeListener {
                         .build();
         return jti;
     }
+
+
 }
