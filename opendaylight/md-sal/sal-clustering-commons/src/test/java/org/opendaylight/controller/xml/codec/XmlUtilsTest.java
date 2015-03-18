afadbe2076d635ace649c9f84a8f5a2426ec061e
Jan Hajnar <jhajnar@cisco.com>
2015-03-18 15:25:15 +0100

    BUG 2412 - remove CompositeNode from sal-clustering-common
    * cleaning all help methods from Util classes which are working
    with deprecated CompositeNode class
    
    Change-Id: I31397a1379ef541d642acec68542860ac689481e
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/xml/codec/XmlUtilsTest.java
+++ opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/xml/codec/XmlUtilsTest.java
@@ -8,30 +8,18 @@
 
 package org.opendaylight.controller.xml.codec;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.io.ByteSource;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.CompositeNode;
-import org.opendaylight.yangtools.yang.data.api.ModifyAction;
-import org.opendaylight.yangtools.yang.data.api.Node;
-import org.opendaylight.yangtools.yang.data.api.SimpleNode;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.impl.ImmutableCompositeNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
 
+// FIXME : CompositeNode is not avaliable anymore so fix the test to use NormalizedNodeContainer ASAP
 public class XmlUtilsTest {
 
   private static final DocumentBuilderFactory BUILDERFACTORY;
@@ -65,65 +53,5 @@ public class XmlUtilsTest {
     final Module rpcTestModule = schema.getModules().iterator().next();
     testRpc = rpcTestModule.getRpcs().iterator().next();
   }
-
-  @Test
-  public void testNullInputXmlToComposite() {
-    CompositeNode node = XmlUtils.inputXmlToCompositeNode(testRpc.getQName(), null, schema);
-    Assert.assertNull(node);
-  }
-
-  @Test
-  public void testNullRpcXmlToComposite() {
-    CompositeNode node = XmlUtils.inputXmlToCompositeNode(null, XML_CONTENT, schema);
-    Assert.assertNull(node);
-  }
-
-  @Test
-  public void testInputXmlToCompositeNode() {
-    CompositeNode node = XmlUtils.inputXmlToCompositeNode(testRpc.getQName(), XML_CONTENT, schema);
-    ImmutableList<SimpleNode<?>> input = (ImmutableList<SimpleNode<?>>)node.getValue().get(0).getValue();
-    SimpleNode<?> firstNode = input.get(0);
-
-    Assert.assertEquals("id", firstNode.getNodeType().getLocalName());
-    Assert.assertEquals("flowid", firstNode.getValue());
-
-    SimpleNode<?> secondNode = input.get(1);
-    Assert.assertEquals("flow", secondNode.getNodeType().getLocalName());
-
-    YangInstanceIdentifier instance = (YangInstanceIdentifier) secondNode.getValue();
-    Iterable<YangInstanceIdentifier.PathArgument> iterable = instance.getPathArguments();
-    Iterator<YangInstanceIdentifier.PathArgument> it = iterable.iterator();
-    YangInstanceIdentifier.NodeIdentifier firstPath = (YangInstanceIdentifier.NodeIdentifier) it.next();
-    Assert.assertEquals("node", firstPath.getNodeType().getLocalName());
-    YangInstanceIdentifier.NodeIdentifierWithPredicates secondPath = (YangInstanceIdentifier.NodeIdentifierWithPredicates)it.next();
-    Short value = (Short)secondPath.getKeyValues().values().iterator().next();
-    Short expected = 3;
-    Assert.assertEquals(expected, value);
-  }
-
-  @Test
-  public void testInputCompositeNodeToXML() {
-    CompositeNode input = XmlUtils.inputXmlToCompositeNode(testRpc.getQName(), XML_CONTENT, schema);
-    List<Node<?>> childNodes = new ArrayList<>();
-    childNodes.add(input);
-    QName rpcQName = schema.getOperations().iterator().next().getQName();
-    CompositeNode node = new ImmutableCompositeNode(rpcQName, input.getValue(), ModifyAction.REPLACE);
-    String xml = XmlUtils.inputCompositeNodeToXml(node, schema);
-    Assert.assertNotNull(xml);
-    Assert.assertTrue(xml.contains("3@java.lang.Short"));
-  }
-
-  @Test
-  public void testNullCompositeNodeToXml(){
-    String xml = XmlUtils.inputCompositeNodeToXml(null, schema);
-    Assert.assertTrue(xml.isEmpty());
-  }
-
-  @Test
-  public void testNullSchemaCompositeNodeToXml(){
-    String xml = XmlUtils.inputCompositeNodeToXml(new ImmutableCompositeNode(QName.create("ns", "2013-12-09", "child1"), new ArrayList<Node<?>>(), ModifyAction.REPLACE), null);
-    Assert.assertTrue(xml.isEmpty());
-  }
-
 
 }
