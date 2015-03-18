86024f5f5c072e38b51e8367226ad166e3f42d0f
Jan Hajnar <jhajnar@cisco.com>
2015-03-18 17:37:56 +0100

    BUG 2412 - remove CompositeNode from sal-remoterpc-connector
    * akka remote RPC distribution needs move to new DOM API
    * remove all tests which are use CompositeNode. Test has to be
    rewriten with NormalizedNodes ASAP.
    
    Change-Id: I8863f5c9657992d23be67acb2f542b0fda81db8c
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
@@ -1,95 +1,87 @@
 package org.opendaylight.controller.remote.rpc;
 
+import static akka.pattern.Patterns.ask;
 import akka.actor.ActorRef;
 import akka.dispatch.OnComplete;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.JdkFutureAdapters;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+import org.opendaylight.controller.cluster.datastore.node.utils.serialization.NormalizedNodeSerializer;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcException;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementation;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementationNotAvailableException;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
+import org.opendaylight.controller.md.sal.dom.spi.DefaultDOMRpcResult;
 import org.opendaylight.controller.remote.rpc.messages.InvokeRpc;
 import org.opendaylight.controller.remote.rpc.messages.RpcResponse;
-import org.opendaylight.controller.sal.core.api.RoutedRpcDefaultImplementation;
-import org.opendaylight.controller.sal.core.api.RpcImplementation;
-import org.opendaylight.controller.xml.codec.XmlUtils;
-import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
-import org.opendaylight.yangtools.yang.data.api.CompositeNode;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.concurrent.ExecutionContext;
 
-import java.util.Collections;
-import java.util.Set;
-
-import static akka.pattern.Patterns.ask;
-
-public class RemoteRpcImplementation implements RpcImplementation, RoutedRpcDefaultImplementation {
+public class RemoteRpcImplementation implements DOMRpcImplementation {
     private static final Logger LOG = LoggerFactory.getLogger(RemoteRpcImplementation.class);
     private final ActorRef rpcBroker;
-    private final SchemaContext schemaContext;
     private final RemoteRpcProviderConfig config;
 
-    public RemoteRpcImplementation(ActorRef rpcBroker, SchemaContext schemaContext, RemoteRpcProviderConfig config) {
+    public RemoteRpcImplementation(final ActorRef rpcBroker, final RemoteRpcProviderConfig config) {
         this.rpcBroker = rpcBroker;
-        this.schemaContext = schemaContext;
         this.config = config;
     }
 
     @Override
-    public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc,
-            YangInstanceIdentifier identifier, CompositeNode input) {
-        InvokeRpc rpcMsg = new InvokeRpc(rpc, identifier, input);
+    public CheckedFuture<DOMRpcResult, DOMRpcException> invokeRpc(final DOMRpcIdentifier rpc, final NormalizedNode<?, ?> input) {
+        final InvokeRpc rpcMsg = new InvokeRpc(rpc.getType().getLastComponent(), rpc.getContextReference(), input);
 
-        return executeMsg(rpcMsg);
-    }
+        final SettableFuture<DOMRpcResult> settableFuture = SettableFuture.create();
 
-    @Override
-    public Set<QName> getSupportedRpcs() {
-        // TODO : check if we need to get this from routing registry
-        return Collections.emptySet();
-    }
+        final ListenableFuture<DOMRpcResult> listenableFuture =
+                JdkFutureAdapters.listenInPoolThread(settableFuture);
 
-    @Override
-    public ListenableFuture<RpcResult<CompositeNode>> invokeRpc(QName rpc, CompositeNode input) {
-        InvokeRpc rpcMsg = new InvokeRpc(rpc, null, input);
-        return executeMsg(rpcMsg);
-    }
+        final scala.concurrent.Future<Object> future = ask(rpcBroker, rpcMsg, config.getAskDuration());
 
-    private ListenableFuture<RpcResult<CompositeNode>> executeMsg(InvokeRpc rpcMsg) {
-
-        final SettableFuture<RpcResult<CompositeNode>> listenableFuture = SettableFuture.create();
-
-        scala.concurrent.Future<Object> future = ask(rpcBroker, rpcMsg, config.getAskDuration());
-
-        OnComplete<Object> onComplete = new OnComplete<Object>() {
+        final OnComplete<Object> onComplete = new OnComplete<Object>() {
             @Override
-            public void onComplete(Throwable failure, Object reply) throws Throwable {
+            public void onComplete(final Throwable failure, final Object reply) throws Throwable {
                 if(failure != null) {
                     LOG.error("InvokeRpc failed", failure);
 
-                    RpcResult<CompositeNode> rpcResult;
-                    if(failure instanceof RpcErrorsException) {
-                        rpcResult = RpcResultBuilder.<CompositeNode>failed().withRpcErrors(
-                                ((RpcErrorsException)failure).getRpcErrors()).build();
-                    } else {
-                        rpcResult = RpcResultBuilder.<CompositeNode>failed().withError(
-                                ErrorType.RPC, failure.getMessage(), failure).build();
+                    final String message = String.format("Execution of RPC %s failed",  rpcMsg.getRpc());
+                    Collection<RpcError> errors = ((RpcErrorsException)failure).getRpcErrors();
+                    if(errors == null || errors.size() == 0) {
+                        errors = Arrays.asList(RpcResultBuilder.newError(ErrorType.RPC, null, message));
                     }
+                    final DOMRpcResult rpcResult = new DefaultDOMRpcResult(errors);
 
-                    listenableFuture.set(rpcResult);
+                    settableFuture.set(rpcResult);
                     return;
                 }
 
-                RpcResponse rpcReply = (RpcResponse)reply;
-                CompositeNode result = XmlUtils.xmlToCompositeNode(rpcReply.getResultCompositeNode());
-                listenableFuture.set(RpcResultBuilder.success(result).build());
+                final RpcResponse rpcReply = (RpcResponse)reply;
+                final NormalizedNode<?, ?> result =
+                        NormalizedNodeSerializer.deSerialize(rpcReply.getResultNormalizedNode());
+                settableFuture.set(new DefaultDOMRpcResult(result));
             }
         };
 
-        future.onComplete(onComplete, ExecutionContext.Implicits$.MODULE$.global());
 
-        return listenableFuture;
+        future.onComplete(onComplete, ExecutionContext.Implicits$.MODULE$.global());
+        // FIXME find non blocking way for implementation
+        try {
+            return Futures.immediateCheckedFuture(listenableFuture.get());
+        }
+        catch (InterruptedException | ExecutionException e) {
+            LOG.debug("Unexpected remote RPC exception.", e);
+            return Futures.immediateFailedCheckedFuture((DOMRpcException) new DOMRpcImplementationNotAvailableException(e, "Unexpected remote RPC exception"));
+        }
     }
 }
--- opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RpcManager.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RpcManager.java
@@ -14,13 +14,17 @@ import akka.actor.OneForOneStrategy;
 import akka.actor.Props;
 import akka.actor.SupervisorStrategy;
 import akka.japi.Function;
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import org.opendaylight.controller.cluster.common.actor.AbstractUntypedActor;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
 import org.opendaylight.controller.remote.rpc.messages.UpdateSchemaContext;
 import org.opendaylight.controller.remote.rpc.registry.RpcRegistry;
-import org.opendaylight.controller.sal.core.api.Broker;
-import org.opendaylight.controller.sal.core.api.RpcProvisionRegistry;
-import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,98 +38,104 @@ import scala.concurrent.duration.Duration;
 
 public class RpcManager extends AbstractUntypedActor {
 
-  private static final Logger LOG = LoggerFactory.getLogger(RpcManager.class);
+    private static final Logger LOG = LoggerFactory.getLogger(RpcManager.class);
 
-  private SchemaContext schemaContext;
-  private ActorRef rpcBroker;
-  private ActorRef rpcRegistry;
-  private final Broker.ProviderSession brokerSession;
-  private final RemoteRpcProviderConfig config;
-  private RpcListener rpcListener;
-  private RoutedRpcListener routeChangeListener;
-  private RemoteRpcImplementation rpcImplementation;
-  private final RpcProvisionRegistry rpcProvisionRegistry;
+    private SchemaContext schemaContext;
+    private ActorRef rpcBroker;
+    private ActorRef rpcRegistry;
+    private final RemoteRpcProviderConfig config;
+    private RpcListener rpcListener;
+    private RoutedRpcListener routeChangeListener;
+    private RemoteRpcImplementation rpcImplementation;
+    private final DOMRpcProviderService rpcProvisionRegistry;
+    private final DOMRpcService rpcServices;
 
-  private RpcManager(SchemaContext schemaContext,
-                     Broker.ProviderSession brokerSession,
-                     RpcProvisionRegistry rpcProvisionRegistry) {
-    this.schemaContext = schemaContext;
-    this.brokerSession = brokerSession;
-    this.rpcProvisionRegistry = rpcProvisionRegistry;
-    this.config = new RemoteRpcProviderConfig(getContext().system().settings().config());
+    private RpcManager(final SchemaContext schemaContext,
+                       final DOMRpcProviderService rpcProvisionRegistry,
+                       final DOMRpcService rpcSevices) {
+        this.schemaContext = schemaContext;
+        this.rpcProvisionRegistry = rpcProvisionRegistry;
+        rpcServices = rpcSevices;
+        config = new RemoteRpcProviderConfig(getContext().system().settings().config());
 
-    createRpcActors();
-    startListeners();
-  }
-
-
-    public static Props props(final SchemaContext schemaContext, final Broker.ProviderSession brokerSession,
-            final RpcProvisionRegistry rpcProvisionRegistry) {
-        return Props.create(RpcManager.class, schemaContext, brokerSession, rpcProvisionRegistry);
+        createRpcActors();
+        startListeners();
     }
 
-  private void createRpcActors() {
-    LOG.debug("Create rpc registry and broker actors");
 
-    rpcRegistry =
-            getContext().actorOf(Props.create(RpcRegistry.class).
-                withMailbox(config.getMailBoxName()), config.getRpcRegistryName());
+      public static Props props(final SchemaContext schemaContext,
+              final DOMRpcProviderService rpcProvisionRegistry, final DOMRpcService rpcServices) {
+          Preconditions.checkNotNull(schemaContext, "SchemaContext can not be null!");
+          Preconditions.checkNotNull(rpcProvisionRegistry, "RpcProviderService can not be null!");
+          Preconditions.checkNotNull(rpcServices, "RpcService can not be null!");
+          return Props.create(RpcManager.class, schemaContext, rpcProvisionRegistry, rpcServices);
+      }
 
-    rpcBroker =
-            getContext().actorOf(RpcBroker.props(brokerSession, rpcRegistry, schemaContext).
-                withMailbox(config.getMailBoxName()), config.getRpcBrokerName());
+    private void createRpcActors() {
+        LOG.debug("Create rpc registry and broker actors");
 
-    RpcRegistry.Messages.SetLocalRouter localRouter = new RpcRegistry.Messages.SetLocalRouter(rpcBroker);
-    rpcRegistry.tell(localRouter, self());
-  }
+        rpcRegistry =
+                getContext().actorOf(Props.create(RpcRegistry.class).
+                    withMailbox(config.getMailBoxName()), config.getRpcRegistryName());
 
-  private void startListeners() {
-    LOG.debug("Registers rpc listeners");
+        rpcBroker =
+                getContext().actorOf(RpcBroker.props(rpcServices, rpcRegistry).
+                    withMailbox(config.getMailBoxName()), config.getRpcBrokerName());
 
-    rpcListener = new RpcListener(rpcRegistry);
-    routeChangeListener = new RoutedRpcListener(rpcRegistry);
-    rpcImplementation = new RemoteRpcImplementation(rpcBroker, schemaContext, config);
-
-    brokerSession.addRpcRegistrationListener(rpcListener);
-    rpcProvisionRegistry.registerRouteChangeListener(routeChangeListener);
-    rpcProvisionRegistry.setRoutedRpcDefaultDelegate(rpcImplementation);
-    announceSupportedRpcs();
-  }
-
-  /**
-   * Add all the locally registered RPCs in the clustered routing table
-   */
-  private void announceSupportedRpcs(){
-    LOG.debug("Adding all supported rpcs to routing table");
-    Set<QName> currentlySupported = brokerSession.getSupportedRpcs();
-    for (QName rpc : currentlySupported) {
-      rpcListener.onRpcImplementationAdded(rpc);
-    }
-  }
-
-
-  @Override
-  protected void handleReceive(Object message) throws Exception {
-    if(message instanceof UpdateSchemaContext) {
-      updateSchemaContext((UpdateSchemaContext) message);
+        final RpcRegistry.Messages.SetLocalRouter localRouter = new RpcRegistry.Messages.SetLocalRouter(rpcBroker);
+        rpcRegistry.tell(localRouter, self());
     }
 
-  }
+    private void startListeners() {
+        LOG.debug("Registers rpc listeners");
 
-  private void updateSchemaContext(UpdateSchemaContext message) {
-    this.schemaContext = message.getSchemaContext();
-    rpcBroker.tell(message, ActorRef.noSender());
-  }
+        rpcListener = new RpcListener(rpcRegistry);
+        routeChangeListener = new RoutedRpcListener(rpcRegistry);
+        rpcImplementation = new RemoteRpcImplementation(rpcBroker, config);
 
-  @Override
-  public SupervisorStrategy supervisorStrategy() {
-    return new OneForOneStrategy(10, Duration.create("1 minute"),
-        new Function<Throwable, SupervisorStrategy.Directive>() {
-          @Override
-          public SupervisorStrategy.Directive apply(Throwable t) {
-            return SupervisorStrategy.resume();
-          }
+        rpcServices.registerRpcListener(rpcListener);
+
+//        rpcProvisionRegistry.registerRouteChangeListener(routeChangeListener);
+//        rpcProvisionRegistry.setRoutedRpcDefaultDelegate(rpcImplementation);
+        announceSupportedRpcs();
+    }
+
+    /**
+     * Add all the locally registered RPCs in the clustered routing table
+     */
+    private void announceSupportedRpcs(){
+        LOG.debug("Adding all supported rpcs to routing table");
+        final Set<RpcDefinition> currentlySupportedRpc = schemaContext.getOperations();
+        final List<DOMRpcIdentifier> rpcs = new ArrayList<>();
+        for (final RpcDefinition rpcDef : currentlySupportedRpc) {
+            rpcs.add(DOMRpcIdentifier.create(rpcDef.getPath()));
         }
-    );
-  }
+        rpcListener.onRpcAvailable(rpcs);
+    }
+
+
+    @Override
+    protected void handleReceive(final Object message) throws Exception {
+      if(message instanceof UpdateSchemaContext) {
+        updateSchemaContext((UpdateSchemaContext) message);
+      }
+
+    }
+
+    private void updateSchemaContext(final UpdateSchemaContext message) {
+      schemaContext = message.getSchemaContext();
+      rpcBroker.tell(message, ActorRef.noSender());
+    }
+
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+      return new OneForOneStrategy(10, Duration.create("1 minute"),
+          new Function<Throwable, SupervisorStrategy.Directive>() {
+            @Override
+            public SupervisorStrategy.Directive apply(final Throwable t) {
+              return SupervisorStrategy.resume();
+            }
+          }
+      );
+    }
 }
