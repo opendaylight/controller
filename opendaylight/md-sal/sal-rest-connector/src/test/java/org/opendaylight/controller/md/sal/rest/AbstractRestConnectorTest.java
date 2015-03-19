b0e97a3acb469c20d7f0bdac3e1eecd229abdbb7
Vaclav Demcak <vdemcak@cisco.com>
2015-03-19 23:40:38 +0100

    Bug 2358 - RestConnector loader draft
    * RestConnectorImpl will be replaced RestconfProviderImpl in future commits
     - is SchemaContextListener for providing a SchemaContext changes cross project
     - a holder for static getSchemaContext method with a service unavailable exception
      (it allowes to remove a lot of unnecessary code)
    * RestconfInternalConstants - a holder for all Restconf internal constats
    for whole project in one place
    * init AbstractRestConnectorTest - designed for a basic help methods for loading
    schemaContext and mocking needed objects used in RestConnectorImpl
    
    TODOs for the future commits:
    - initialize RestContextHolder (not committed yet)
    - initialize RestBrokerFacade (not committed yet)
    - find a way for REST ResourceConfig with Servlet 3.0
    - redirect all internal constants to RestconfInternalConstants
    
    Change-Id: Iac1f359af6e04136306913bb3d46b37237cef987
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/md/sal/rest/AbstractRestConnectorTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.md.sal.rest;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.Mockito;
+import org.opendaylight.controller.md.sal.rest.common.TestRestconfUtils;
+import org.opendaylight.controller.sal.core.api.Broker.ProviderSession;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.controller.sal.restconf.impl.RestconfDocumentedException;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * sal-rest-connector
+ * org.opendaylight.controller.md.sal.rest
+ *
+ * Basic abstract help class for a Restconf test suite.
+ * Use it for all classes witch are working with {@link RestConnectorProviderImpl}
+ * in JUnit BeforeClass statement.
+ * Class provides help methods for e.g. loadingSchemaContext or mocking
+ * parts of {@link RestConnectorProviderImpl}.
+ *
+ * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
+ *
+ * Created: Mar 20, 2015
+ */
+public abstract class AbstractRestConnectorTest {
+
+    protected static RestConnectorProviderImpl restConnector = new RestConnectorProviderImpl();
+
+    /**
+     * Method overwrite actual global schema from RestConnectorImpl
+     *
+     * @param schemaContext
+     */
+    protected static void setSchemaContext(final SchemaContext schemaContext) {
+        restConnector.onGlobalContextUpdated(schemaContext);
+    }
+
+    /**
+     * Method prepare default mocked {@link ProviderSession} and initialize
+     * {@link RestConnectorProviderImpl} for using in a test suite.
+     */
+    protected static void defaultOnSessionInitMocking() {
+        final ProviderSession ps = mock(ProviderSession.class);
+        final SchemaService ss = mock(SchemaService.class);
+        when(ss.getGlobalContext()).thenReturn(null);
+        when(ps.getService(SchemaService.class)).thenReturn(ss);
+        restConnector.onSessionInitiated(ps);
+    }
+
+    /**
+     * Method is loading SchemaContext from given path from src/test/resources
+     * <code>Note:</code> existing loaded SchemaContext will be updated only.
+     *
+     * @param yangPath
+     */
+    protected static void loadSchemaContextFromFile(final String yangPath) {
+        SchemaContext sc = null;
+        try {
+            sc = RestConnectorProviderImpl.getSchemaContext();
+            // we have to set null for mocked SchemaContext
+            if (Mockito.mockingDetails(sc).isMock()) {
+                sc = null;
+            }
+        }
+        catch (final RestconfDocumentedException e) {
+            // NOOP
+        }
+        sc = TestRestconfUtils.loadSchemaContext(yangPath, sc);
+        restConnector.onGlobalContextUpdated(sc);
+    }
+}
