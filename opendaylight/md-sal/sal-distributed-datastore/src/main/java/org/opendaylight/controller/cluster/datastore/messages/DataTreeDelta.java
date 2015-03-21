2491d05e210d150e9971c8f7ea043d8a9241c7b9
Robert Varga <rovarga@cisco.com>
2015-03-21 18:03:43 +0100

    BUG-2673: add remoting for DataTreeChanges
    This patch adds the basic DataTreeDelta message to exchange change
    sbetween nodes and the logic needed to reconstruct data tree change.
    
    Change-Id: Ib8de8f0f26d076bb19550aadb1618fc62a1988c7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/DataTreeDelta.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.messages;
+
+import com.google.common.collect.Maps;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.DefaultDataTreeCandidate;
+
+/**
+ * A remotable version of {@link DataTreeChanged}. This message
+ * is used when propagating a delta across cluster nodes. Receiving
+ * node needs to perform data reconstruction.
+ */
+public final class DataTreeDelta implements Externalizable {
+    private static final long serialVersionUID = 1L;
+
+    // Java serialization APIs mean that we could make this field final
+    // at the cost of using arcane reflections in readExternal().
+    private Collection<DataTreeCandidate> changes;
+
+    /**
+     * Return the data changes. Note that these may not be semantically complete if
+     * this object was deserialized. Specifically the afterData() information will
+     * be missing if the change contained any MERGE operations. Users MUST perform
+     * data reconstruction.
+     *
+     * @return Change events
+     */
+    public Collection<DataTreeCandidate> getChanges() {
+        return changes;
+    }
+
+    private static Map<PathArgument, DataTreeCandidateNode> readChildren(final NormalizedNodeObjectInput in) throws ClassNotFoundException, IOException {
+        final int size = in.readInt();
+        if (size == 0) {
+            return Collections.emptyMap();
+        }
+
+        final Map<PathArgument, DataTreeCandidateNode> ret = Maps.newHashMapWithExpectedSize(size);
+        for (int i = 0; i < size; ++i) {
+            final DataTreeCandidateNode node = readNode(in);
+            if (node != null) {
+                ret.put(node.getIdentifier(), node);
+            }
+        }
+
+        return ret;
+    }
+
+    private static void writeChildren(final NormalizedNodeObjectOutput out, final Collection<DataTreeCandidateNode> children) throws IOException {
+        out.writeInt(children.size());
+        for (DataTreeCandidateNode child : children) {
+            writeNode(out, child);
+        }
+    }
+
+    private static DataTreeCandidateNode readNode(final NormalizedNodeObjectInput in) throws ClassNotFoundException, IOException {
+        final ModificationType type = (ModificationType) in.readObject();
+        final PathArgument id;
+
+        switch (type) {
+        case UNMODIFIED:
+            return null;
+        case DELETE:
+            id = (PathArgument) in.readObject();
+            return new DataTreeCandidateNodeImpl(id, type);
+        case SUBTREE_MODIFIED:
+            id = (PathArgument) in.readObject();
+            return new DataTreeCandidateNodeImpl(id, type, readChildren(in));
+        case WRITE:
+            id = (PathArgument) in.readObject();
+            return new DataTreeCandidateNodeImpl(id, type, in.readNormalizedNode());
+        default:
+            throw new IllegalArgumentException("Unhandled modification type " + type);
+        }
+    }
+
+    private static void writeNode(final NormalizedNodeObjectOutput out, final DataTreeCandidateNode node) throws IOException {
+        out.writeObject(node.getModificationType());
+
+        final ModificationType type = node.getModificationType();
+        if (type == ModificationType.UNMODIFIED) {
+            // No-op, but we had to write the type, as that accounts for this node
+            return;
+        }
+
+        out.writeObject(node.getIdentifier());
+
+        switch (type) {
+        case UNMODIFIED:
+            // Taken care of above
+            break;
+        case DELETE:
+            // Nothing more to do
+            break;
+        case WRITE:
+            // Just write the after-image
+            out.writeNormalizedNode(node.getDataAfter().get());
+            break;
+        case SUBTREE_MODIFIED:
+            // Write out children only
+            writeChildren(out, node.getChildNodes());
+            break;
+        default:
+            throw new IllegalArgumentException("Unsupported type " + type);
+        }
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        out.writeInt(changes.size());
+
+        final NormalizedNodeObjectOutput output = new NormalizedNodeObjectOutput(out);
+        for (DataTreeCandidate change : changes) {
+            output.writeObject(change.getRootPath());
+            writeNode(output, change.getRootNode());
+        }
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        final int size = in.readInt();
+        if (size == 0) {
+            changes = Collections.emptyList();
+            return;
+        }
+
+        final NormalizedNodeObjectInput input = new NormalizedNodeObjectInput(in);
+
+        changes = new ArrayList<>(size);
+        for (int i = 0; i < size; ++i) {
+            final YangInstanceIdentifier treeId = (YangInstanceIdentifier) in.readObject();
+            final DataTreeCandidateNode node = readNode(input);
+
+            // Null means suppressed node
+            if (node != null) {
+                changes.add(new DefaultDataTreeCandidate(treeId, node));
+            }
+        }
+    }
+}
