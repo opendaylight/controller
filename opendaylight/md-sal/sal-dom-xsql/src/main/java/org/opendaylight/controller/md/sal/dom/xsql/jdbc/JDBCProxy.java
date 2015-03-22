5c93f002bb3ce06fdd8b6e83570d9963c8d75224
Robert Varga <rovarga@cisco.com>
2015-03-22 14:47:42 +0100

    XSQL: eliminate use of System.err.println
    Use a normal logger instead.
    
    Change-Id: Ib06dfaf1a0d4c836e1c19353a57cc070fddbf95a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-dom-xsql/src/main/java/org/opendaylight/controller/md/sal/dom/xsql/jdbc/JDBCProxy.java
+++ opendaylight/md-sal/sal-dom-xsql/src/main/java/org/opendaylight/controller/md/sal/dom/xsql/jdbc/JDBCProxy.java
@@ -2,9 +2,11 @@ package org.opendaylight.controller.md.sal.dom.xsql.jdbc;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JDBCProxy implements InvocationHandler {
-
+    private static final Logger LOG = LoggerFactory.getLogger(JDBCProxy.class);
     private Object myObject = null;
     private Class<?> myObjectClass = null;
 
@@ -14,11 +16,8 @@ public class JDBCProxy implements InvocationHandler {
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args)
-            throws Throwable {
-        System.err.println("Class " + this.myObjectClass.getSimpleName()
-                + " Method " + method.getName());
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        LOG.debug("Class {} Method {}", this.myObjectClass.getSimpleName(), method.getName());
         return method.invoke(this.myObject, args);
     }
-
 }
