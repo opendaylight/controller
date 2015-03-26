f7206fe24751d4f73ebe25ef06c7ea17b85559cb
Tom Pantelis <tpanteli@brocade.com>
2015-03-25 22:22:45 -0400

    Refactor DataPersistenceProviders and RaftActor#persistence
    Refactored protected DataPersistenceProvider inner class impls to new
    files for reuse in unit tests and to reduce inner code.
    
    Also modified RaftActor so the DataPersistenceProvider is set rather
    than having derived classes provide via the persistence() abstract method.
    This makes it a little easier for derived RaftActors in that they don't
    have to maintain a field and easier for unit tests to change the
    DataPersistenceProvider impl.
    
    Added a DelegatingPersistentDataProvider that holds the actual impl for
    RaftActor. This allows the DataPersistenceProvider to be passed to
    internal helper classes without having to update them if the underlyting
    impl is changed.
    
    These changes will facilitate further refactoring of code in RaftActor
    into separate classes to simplify it.
    
    Change-Id: I520b0d83635356f195e6bff33e44ac8f49e793cf
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/PersistentDataProvider.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster;
+
+import akka.japi.Procedure;
+import akka.persistence.SnapshotSelectionCriteria;
+import akka.persistence.UntypedPersistentActor;
+import com.google.common.base.Preconditions;
+
+/**
+ * A DataPersistenceProvider implementation with persistence enabled.
+ */
+public class PersistentDataProvider implements DataPersistenceProvider {
+
+    private final UntypedPersistentActor persistentActor;
+
+    public PersistentDataProvider(UntypedPersistentActor persistentActor) {
+        this.persistentActor = Preconditions.checkNotNull(persistentActor, "persistentActor can't be null");
+    }
+
+    @Override
+    public boolean isRecoveryApplicable() {
+        return true;
+    }
+
+    @Override
+    public <T> void persist(T o, Procedure<T> procedure) {
+        persistentActor.persist(o, procedure);
+    }
+
+    @Override
+    public void saveSnapshot(Object o) {
+        persistentActor.saveSnapshot(o);
+    }
+
+    @Override
+    public void deleteSnapshots(SnapshotSelectionCriteria criteria) {
+        persistentActor.deleteSnapshots(criteria);
+    }
+
+    @Override
+    public void deleteMessages(long sequenceNumber) {
+        persistentActor.deleteMessages(sequenceNumber);
+    }
+}
