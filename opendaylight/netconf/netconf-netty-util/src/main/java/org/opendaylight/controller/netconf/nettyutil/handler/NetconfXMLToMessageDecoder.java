24195b3f9343a941d4842e9a3be6fd68fb964539
Gwenael Lambrouin <gwenael.lambrouin@b-com.com>
2015-03-27 17:41:10 +0100

    BUG-2838: Trim leading whitespaces from incoming NETCONF messages.
    Some network devices (such as Cisco routers) send RPC replies with a leading
    newline before the XML declaration. The OpenDaylight controller does not accept
    those replies: the XML parsing fails.
    
    This patch fixes the NETCONF messages before they are sent to the XML parser:
    it removes all the spurious characters at the beginning of the messages.
    
    Change-Id: Ibc6eb6dc5bad6252a3c9bed73d3db83814aff501
    Signed-off-by: Gwenael Lambrouin <gwenael.lambrouin@b-com.com>

--- opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/controller/netconf/nettyutil/handler/NetconfXMLToMessageDecoder.java
+++ opendaylight/netconf/netconf-netty-util/src/main/java/org/opendaylight/controller/netconf/nettyutil/handler/NetconfXMLToMessageDecoder.java
@@ -22,6 +22,7 @@ import org.xml.sax.SAXException;
 
 public final class NetconfXMLToMessageDecoder extends ByteToMessageDecoder {
     private static final Logger LOG = LoggerFactory.getLogger(NetconfXMLToMessageDecoder.class);
+    private static final byte XML_START = 0x3c; // '<'
 
     @Override
     public void decode(final ChannelHandlerContext ctx, final ByteBuf in, final List<Object> out) throws IOException, SAXException {
@@ -30,6 +31,37 @@ public final class NetconfXMLToMessageDecoder extends ByteToMessageDecoder {
                 LOG.trace("Received to decode: {}", ByteBufUtil.hexDump(in));
             }
 
+            /* According to the XML 1.0 specifications, when there is an XML declaration
+             * at the beginning of an XML document, it is invalid to have
+             * white spaces before that declaration (reminder: a XML declaration looks like:
+             * <?xml version="1.0" encoding="UTF-8"?>). In contrast, when there is no XML declaration,
+             * it is valid to have white spaces at the beginning of the document.
+             *
+             * When they send a NETCONF message, several NETCONF servers start with a new line (either
+             * LF or CRLF), presumably to improve readability in interactive sessions with a human being.
+             * Some NETCONF servers send an XML declaration, some others do not.
+             *
+             * If a server starts a NETCONF message with white spaces and follows with an XML
+             * declaration, XmlUtil.readXmlToDocument() will fail because this is invalid XML.
+             * But in the spirit of the "NETCONF over SSH" RFC 4742 and to improve interoperability, we want
+             * to accept those messages.
+             *
+             * To do this, the following code strips the leading bytes before the start of the XML messages.
+             */
+
+            // Try to find the start of xml netconf message by finding '<' byte and strip all bytes prior to '<'
+            final int properStart = in.bytesBefore(XML_START);
+            if (properStart == -1) {
+                throw new IOException(String.format("Unable to parse NETCONF message, unable to find character 0x%02x" +
+                        " as XML starting point in message: %s", XML_START, ByteBufUtil.hexDump(in)));
+            } else if (properStart != 0) {
+                // Reading unwanted leading bytes moves the reader index to the proper start for further processing
+                final ByteBuf strippedLeadingBytes = in.readBytes(properStart);
+                LOG.debug("XML message with unwanted leading bytes detected. " +
+                         "Discarded the {} leading byte(s): {}", properStart, ByteBufUtil.hexDump(strippedLeadingBytes));
+            }
+        }
+        if (in.isReadable()) {
             out.add(new NetconfMessage(XmlUtil.readXmlToDocument(new ByteBufInputStream(in))));
         } else {
             LOG.debug("No more content in incoming buffer.");
