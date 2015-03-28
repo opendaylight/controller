6d4d1d19ffba31e8fc37e40965dd3dd56dcee007
Tom Pantelis <tpanteli@brocade.com>
2015-03-28 11:27:06 -0400

    Add unit tests for RaftActorRecoverySupport
    Also removed redundant recovery tests from RaftActorTest and added a
    test that verifies the RaftActor forwards all recovery messages to the
    RaftActorRecoverySupport.
    
    I also refactored MockRactorActor and RaftActorTestKit to their own
    files to reduce the size of RaftActorTest.
    
    Change-Id: I75d0a1cfe0d0b168e488ce5cecda29dae4fe4321
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActor.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActor.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -152,8 +153,7 @@ public abstract class RaftActor extends AbstractUntypedPersistentActor {
     @Override
     public void handleRecover(Object message) {
         if(raftRecovery == null) {
-            raftRecovery = new RaftActorRecoverySupport(delegatingPersistenceProvider, context, currentBehavior,
-                    getRaftActorRecoveryCohort());
+            raftRecovery = newRaftActorRecoverySupport();
         }
 
         boolean recoveryComplete = raftRecovery.handleRecoveryMessage(message);
@@ -173,6 +173,11 @@ public abstract class RaftActor extends AbstractUntypedPersistentActor {
 
             raftRecovery = null;
         }
+    }
+
+    protected RaftActorRecoverySupport newRaftActorRecoverySupport() {
+        return new RaftActorRecoverySupport(delegatingPersistenceProvider, context, currentBehavior,
+                getRaftActorRecoveryCohort());
     }
 
     protected void initializeBehavior(){
