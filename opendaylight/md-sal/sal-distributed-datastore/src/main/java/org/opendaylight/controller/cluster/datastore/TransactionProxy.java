9356d833d8ecd25332b1bb1978bf4c300c7c7a1d
Robert Varga <rovarga@cisco.com>
2015-04-01 13:27:25 +0200

    Make TransactionProxyCleanupPhantomReference self-contained
    This tracker can be broken out cleanly, if we clean up interactions with
    TransactionProxy and TransactionFutureCallback. As a first step, move
    static fields inside the class and make sure we expose a nice interface
    for making proxies tracked.
    
    Change-Id: I14dbb44a2c94e758ad492222eaa9d038f5b6b858
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/TransactionProxy.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/TransactionProxy.java
@@ -104,24 +104,6 @@ public class TransactionProxy extends AbstractDOMStoreTransaction<TransactionIde
             FiniteDuration.create(1, TimeUnit.SECONDS);
 
     /**
-     * Used to enqueue the PhantomReferences for read-only TransactionProxy instances. The
-     * FinalizableReferenceQueue is safe to use statically in an OSGi environment as it uses some
-     * trickery to clean up its internal thread when the bundle is unloaded.
-     */
-    private static final FinalizableReferenceQueue phantomReferenceQueue =
-                                                                  new FinalizableReferenceQueue();
-
-    /**
-     * This stores the TransactionProxyCleanupPhantomReference instances statically, This is
-     * necessary because PhantomReferences need a hard reference so they're not garbage collected.
-     * Once finalized, the TransactionProxyCleanupPhantomReference removes itself from this map
-     * and thus becomes eligible for garbage collection.
-     */
-    private static final Map<TransactionProxyCleanupPhantomReference,
-                             TransactionProxyCleanupPhantomReference> phantomReferenceCache =
-                                                                        new ConcurrentHashMap<>();
-
-    /**
      * A PhantomReference that closes remote transactions for a TransactionProxy when it's
      * garbage collected. This is used for read-only transactions as they're not explicitly closed
      * by clients. So the only way to detect that a transaction is no longer in use and it's safe
@@ -130,15 +112,33 @@ public class TransactionProxy extends AbstractDOMStoreTransaction<TransactionIde
      * to the old generation space and thus should be cleaned up in a timely manner as the GC
      * runs on the young generation (eden, swap1...) space much more frequently.
      */
-    private static class TransactionProxyCleanupPhantomReference
+    private static final class TransactionProxyCleanupPhantomReference
                                            extends FinalizablePhantomReference<TransactionProxy> {
+        private static final Logger LOG = LoggerFactory.getLogger(TransactionProxyCleanupPhantomReference.class);
+        /**
+         * Used to enqueue the PhantomReferences for read-only TransactionProxy instances. The
+         * FinalizableReferenceQueue is safe to use statically in an OSGi environment as it uses some
+         * trickery to clean up its internal thread when the bundle is unloaded.
+         */
+        private static final FinalizableReferenceQueue phantomReferenceQueue =
+                                                                      new FinalizableReferenceQueue();
+
+        /**
+         * This stores the TransactionProxyCleanupPhantomReference instances statically, This is
+         * necessary because PhantomReferences need a hard reference so they're not garbage collected.
+         * Once finalized, the TransactionProxyCleanupPhantomReference removes itself from this map
+         * and thus becomes eligible for garbage collection.
+         */
+        private static final Map<TransactionProxyCleanupPhantomReference,
+                                 TransactionProxyCleanupPhantomReference> phantomReferenceCache =
+                                                                            new ConcurrentHashMap<>();
 
         private final List<ActorSelection> remoteTransactionActors;
         private final AtomicBoolean remoteTransactionActorsMB;
         private final ActorContext actorContext;
         private final TransactionIdentifier identifier;
 
-        protected TransactionProxyCleanupPhantomReference(TransactionProxy referent) {
+        private TransactionProxyCleanupPhantomReference(TransactionProxy referent) {
             super(referent, phantomReferenceQueue);
 
             // Note we need to cache the relevant fields from the TransactionProxy as we can't
@@ -148,6 +148,11 @@ public class TransactionProxy extends AbstractDOMStoreTransaction<TransactionIde
             remoteTransactionActorsMB = referent.remoteTransactionActorsMB;
             actorContext = referent.actorContext;
             identifier = referent.getIdentifier();
+        }
+
+        static void track(TransactionProxy referent) {
+            final TransactionProxyCleanupPhantomReference ret = new TransactionProxyCleanupPhantomReference(referent);
+            phantomReferenceCache.put(ret, ret);
         }
 
         @Override
@@ -732,9 +737,7 @@ public class TransactionProxy extends AbstractDOMStoreTransaction<TransactionIde
                     remoteTransactionActors = Lists.newArrayList();
                     remoteTransactionActorsMB = new AtomicBoolean();
 
-                    TransactionProxyCleanupPhantomReference cleanup =
-                            new TransactionProxyCleanupPhantomReference(TransactionProxy.this);
-                    phantomReferenceCache.put(cleanup, cleanup);
+                    TransactionProxyCleanupPhantomReference.track(TransactionProxy.this);
                 }
 
                 // Add the actor to the remoteTransactionActors list for access by the
