1fa467a4364d8416da7cd2cb0b33d0341a70a4fe
Tomas Cere <tcere@cisco.com>
2015-04-14 10:46:05 +0200

    WIP edit config performance with parsing strategies
    Change-Id: I99d7f049710393743714d90ac3123041e87040cd
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- opendaylight/netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/controller/netconf/mdsal/connector/ops/EditConfig.java
+++ opendaylight/netconf/mdsal-netconf-connector/src/main/java/org/opendaylight/controller/netconf/mdsal/connector/ops/EditConfig.java
@@ -9,10 +9,11 @@
 package org.opendaylight.controller.netconf.mdsal.connector.ops;
 
 import com.google.common.base.Optional;
-import com.google.common.util.concurrent.CheckedFuture;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataReadWriteTransaction;
@@ -23,20 +24,16 @@ import org.opendaylight.controller.netconf.api.NetconfDocumentedException.ErrorT
 import org.opendaylight.controller.netconf.api.xml.XmlNetconfConstants;
 import org.opendaylight.controller.netconf.mdsal.connector.CurrentSchemaContext;
 import org.opendaylight.controller.netconf.mdsal.connector.TransactionProvider;
+import org.opendaylight.controller.netconf.mdsal.connector.ops.DataTreeChangeTracker.DataTreeChange;
+import org.opendaylight.controller.netconf.mdsal.connector.ops.DataTreeChangeTracker.NetconfOperationLeafStrategy;
 import org.opendaylight.controller.netconf.util.mapping.AbstractSingletonNetconfOperation;
 import org.opendaylight.controller.netconf.util.xml.XmlElement;
 import org.opendaylight.controller.netconf.util.xml.XmlUtil;
 import org.opendaylight.yangtools.yang.data.api.ModifyAction;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.DomUtils;
 import org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.parser.DomToNormalizedNodeParserFactory;
-import org.opendaylight.yangtools.yang.data.operations.DataModificationException;
-import org.opendaylight.yangtools.yang.data.operations.DataModificationException.DataExistsException;
-import org.opendaylight.yangtools.yang.data.operations.DataModificationException.DataMissingException;
-import org.opendaylight.yangtools.yang.data.operations.DataOperations;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
@@ -55,8 +52,6 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
     private static final String CONFIG_KEY = "config";
     private static final String TARGET_KEY = "target";
     private static final String DEFAULT_OPERATION_KEY = "default-operation";
-
-
     private final CurrentSchemaContext schemaContext;
     private final TransactionProvider transactionProvider;
 
@@ -83,46 +78,90 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
         for (XmlElement element : configElement.getChildElements()) {
             final String ns = element.getNamespace();
             final DataSchemaNode schemaNode = getSchemaNodeFromNamespace(ns, element).get();
-            YangInstanceIdentifier ident = YangInstanceIdentifier.of(schemaNode.getQName());
 
-            final NormalizedNode storedNode = readStoredNode(LogicalDatastoreType.CONFIGURATION, ident);
-            try {
-                final Optional<NormalizedNode<?, ?>> newNode = modifyNode(schemaNode, element, storedNode, defaultAction);
-                final DOMDataReadWriteTransaction rwTx = transactionProvider.getOrCreateTransaction();
-                if (newNode.isPresent()) {
-                    rwTx.put(LogicalDatastoreType.CONFIGURATION, ident, newNode.get());
-                } else {
-                    rwTx.delete(LogicalDatastoreType.CONFIGURATION, ident);
-                }
-            } catch (final DataExistsException e) {
-                throw new NetconfDocumentedException(e.getMessage(), e, ErrorType.protocol, ErrorTag.data_exists, ErrorSeverity.error);
-            } catch (final DataMissingException e) {
-                throw new NetconfDocumentedException(e.getMessage(), e, ErrorType.protocol, ErrorTag.data_missing, ErrorSeverity.error);
-            } catch (final DataModificationException e) {
-                throw new NetconfDocumentedException(e.getMessage(), e, ErrorType.protocol, ErrorTag.operation_failed, ErrorSeverity.error);
-            }
+            DataTreeChangeTracker changeTracker = new DataTreeChangeTracker(defaultAction);
+
+            DataTreeChangeTracker.NetconfOperationContainerStrategy containerStrategy =
+                    new DataTreeChangeTracker.NetconfOperationContainerStrategy(changeTracker);
+            DataTreeChangeTracker.NetconfOperationLeafStrategy leafStrategy = new NetconfOperationLeafStrategy(changeTracker);
+            parseIntoNormalizedNode(schemaNode, element, containerStrategy, leafStrategy);
+
+            executeOperations(changeTracker);
         }
 
         return XmlUtil.createElement(document, XmlNetconfConstants.OK, Optional.<String>absent());
     }
 
-    private NormalizedNode readStoredNode(final LogicalDatastoreType logicalDatastoreType, final YangInstanceIdentifier path) throws NetconfDocumentedException{
-        final  DOMDataReadWriteTransaction rwTx = transactionProvider.getOrCreateTransaction();
-        final CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> readFuture = rwTx.read(logicalDatastoreType, path);
-        try {
-            if (readFuture.checkedGet().isPresent()) {
-                final NormalizedNode node = readFuture.checkedGet().get();
-                return node;
-            } else {
-                LOG.debug("Unable to read node : {} from {} datastore", path, logicalDatastoreType);
-            }
-        } catch (final ReadFailedException e) {
-            //only log this since DataOperations.modify will handle throwing an exception or writing the node.
-            LOG.debug("Unable to read stored data: {}", path, e);
-        }
+    private void executeOperations(final DataTreeChangeTracker changeTracker) throws NetconfDocumentedException {
+//        List<DataTreeChange> aa = Lists.reverse(changeTracker.getDataTreeChanges());
+        final DOMDataReadWriteTransaction rwTx = transactionProvider.getOrCreateTransaction();
+        final List<DataTreeChange> aa = changeTracker.getDataTreeChanges();
+        final ListIterator<DataTreeChange> iterator = aa.listIterator(aa.size());
 
-        //we can return null here since DataOperations.modify handles null as input
-        return null;
+        while (iterator.hasPrevious()) {
+            final DataTreeChange dtc = iterator.previous();
+            executeChange(rwTx, dtc);
+        }
+    }
+
+    private void executeChange(final DOMDataReadWriteTransaction rwtx, final DataTreeChange change) throws NetconfDocumentedException {
+        switch (change.getAction()) {
+            case NONE:
+                return;
+            case MERGE:
+                rwtx.merge(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()), change.getChangeRoot());
+                break;
+            case CREATE:
+                try {
+                    Optional<NormalizedNode<?, ?>> readResult = rwtx.read(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath())).checkedGet();
+                    if (readResult.isPresent()) {
+                        throw new NetconfDocumentedException("Data already exists, cannot execute CREATE operation", ErrorType.protocol, ErrorTag.data_exists, ErrorSeverity.error);
+                    }
+                    rwtx.put(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()), change.getChangeRoot());
+                } catch (ReadFailedException e) {
+                    LOG.warn("Read from datastore failed when trying to read data for create operation", change, e);
+                }
+                break;
+            case REPLACE:
+                rwtx.put(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()), change.getChangeRoot());
+                break;
+            case DELETE:
+                try {
+                    Optional<NormalizedNode<?, ?>> readResult = rwtx.read(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath())).checkedGet();
+                    if (readResult.isPresent()) {
+                        throw new NetconfDocumentedException("Data is missing, cannot execute DELETE operation", ErrorType.protocol, ErrorTag.data_missing, ErrorSeverity.error);
+                    }
+                    rwtx.delete(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()));
+                } catch (ReadFailedException e) {
+                    LOG.warn("Read from datastore failed when trying to read data for delete operation", change, e);
+                }
+                break;
+            case REMOVE:
+                rwtx.delete(LogicalDatastoreType.CONFIGURATION, YangInstanceIdentifier.create(change.getPath()));
+                break;
+            default:
+                LOG.warn("Unknown/not implemented operation, not executing");
+        }
+    }
+
+    private NormalizedNode parseIntoNormalizedNode(final DataSchemaNode schemaNode, final XmlElement element,
+                                                   final DataTreeChangeTracker.NetconfOperationContainerStrategy containerStrategy,
+                                                   final DataTreeChangeTracker.NetconfOperationLeafStrategy leafStrategy) {
+        if (schemaNode instanceof ContainerSchemaNode) {
+            return DomToNormalizedNodeParserFactory
+                    .getInstance(DomUtils.defaultValueCodecProvider(), schemaContext.getCurrentContext(), containerStrategy, leafStrategy)
+                    .getContainerNodeParser()
+                    .parse(Collections.singletonList(element.getDomElement()), (ContainerSchemaNode) schemaNode);
+        } else if (schemaNode instanceof ListSchemaNode) {
+            return DomToNormalizedNodeParserFactory
+                    .getInstance(DomUtils.defaultValueCodecProvider(), schemaContext.getCurrentContext(), containerStrategy, leafStrategy)
+                    .getMapNodeParser()
+                    .parse(Collections.singletonList(element.getDomElement()), (ListSchemaNode) schemaNode);
+        } else {
+            //this should never happen since edit-config on any other node type should not be possible nor makes sense
+            LOG.debug("DataNode from module is not ContainerSchemaNode nor ListSchemaNode, aborting..");
+        }
+        throw new UnsupportedOperationException("implement exception if parse fails");
     }
 
     private Optional<DataSchemaNode> getSchemaNodeFromNamespace(final String namespace, final XmlElement element) throws NetconfDocumentedException{
@@ -139,48 +178,11 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
                         ErrorTag.unknown_namespace,
                         ErrorSeverity.error);
             }
-
         } catch (URISyntaxException e) {
             LOG.debug("Unable to create URI for namespace : {}", namespace);
         }
 
         return dataSchemaNode;
-    }
-
-    private Optional<NormalizedNode<?, ?>> modifyNode(final DataSchemaNode schemaNode, final XmlElement element, final NormalizedNode storedNode, final ModifyAction defaultAction) throws DataModificationException{
-        if (schemaNode instanceof ContainerSchemaNode) {
-            final ContainerNode modifiedNode =
-                    DomToNormalizedNodeParserFactory
-                            .getInstance(DomUtils.defaultValueCodecProvider())
-                            .getContainerNodeParser()
-                            .parse(Collections.singletonList(element.getDomElement()), (ContainerSchemaNode) schemaNode);
-
-            final Optional<ContainerNode> oNode = DataOperations.modify((ContainerSchemaNode) schemaNode, (ContainerNode) storedNode, modifiedNode, defaultAction);
-            if (!oNode.isPresent()) {
-                return Optional.absent();
-            }
-
-            final NormalizedNode<?,?> node = oNode.get();
-            return Optional.<NormalizedNode<?,?>>of(node);
-        } else if (schemaNode instanceof ListSchemaNode) {
-            final MapNode modifiedNode =
-                DomToNormalizedNodeParserFactory
-                        .getInstance(DomUtils.defaultValueCodecProvider())
-                        .getMapNodeParser()
-                        .parse(Collections.singletonList(element.getDomElement()), (ListSchemaNode) schemaNode);
-
-            final Optional<MapNode> oNode = DataOperations.modify((ListSchemaNode) schemaNode, (MapNode) storedNode, modifiedNode, defaultAction);
-            if (!oNode.isPresent()) {
-                return Optional.absent();
-            }
-
-            final NormalizedNode<?, ?> node = oNode.get();
-            return Optional.<NormalizedNode<?,?>>of(node);
-        } else {
-            //this should never happen since edit-config on any other node type should not be possible nor makes sense
-            LOG.debug("DataNode from module is not ContainerSchemaNode nor ListSchemaNode, aborting..");
-            return Optional.absent();
-        }
     }
 
     private Datastore extractTargetParameter(final XmlElement operationElement) throws NetconfDocumentedException {
@@ -225,5 +227,4 @@ public class EditConfig extends AbstractSingletonNetconfOperation {
     protected String getOperationName() {
         return OPERATION_NAME;
     }
-
 }
