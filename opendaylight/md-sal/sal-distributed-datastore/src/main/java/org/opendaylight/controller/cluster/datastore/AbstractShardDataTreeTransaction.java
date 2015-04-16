5d3071cfcaec35c877a12ee80ad4fcacae800378
Robert Varga <rovarga@cisco.com>
2015-04-16 16:30:47 +0200

    WIP: use internal DataTree instance
    Change-Id: I031d0ed4d82dcc923fd377c13fefba2819923f9b
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractShardDataTreeTransaction.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import javax.annotation.concurrent.NotThreadSafe;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeSnapshot;
+
+/**
+ * Abstract base for transactions running on SharrdDataTree.
+ *
+ * @param <T> Backing transaction type.
+ */
+@NotThreadSafe
+abstract class AbstractShardDataTreeTransaction<T extends DataTreeSnapshot> {
+    private final T snapshot;
+    private final String id;
+    private boolean closed;
+
+    protected AbstractShardDataTreeTransaction(final String id, final T snapshot) {
+        this.snapshot = Preconditions.checkNotNull(snapshot);
+        this.id = Preconditions.checkNotNull(id);
+    }
+
+    final T getSnapshot() {
+        Preconditions.checkState(!closed, "Transaction is already closed");
+        return snapshot;
+    }
+
+    /**
+     * Close this transaction and mark it as closed, allowing idempotent invocations.
+     *
+     * @return True if the transaction got closed by this method invocation.
+     */
+    protected final boolean close() {
+        if (!closed) {
+            closed = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("id", id).add("closed", closed).add("snapshot", snapshot).toString();
+    }
+
+    abstract void abort();
+}
