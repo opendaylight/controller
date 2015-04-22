2e6c5447c15ff97356b2a94f30432f9c0f45b39e
Robert Varga <rovarga@cisco.com>
2015-04-22 14:25:55 +0200

    WIP: split out write operations
    Change-Id: Ibcd910191ed3ee0f97497af5a35d0708d78ac41d
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-dom-spi/src/main/java/org/opendaylight/controller/sal/core/spi/data/DOMStoreWriteTransaction.java
+++ opendaylight/md-sal/sal-dom-spi/src/main/java/org/opendaylight/controller/sal/core/spi/data/DOMStoreWriteTransaction.java
@@ -7,61 +7,7 @@
  */
 package org.opendaylight.controller.sal.core.spi.data;
 
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-
-public interface DOMStoreWriteTransaction extends DOMStoreTransaction {
-
-    /**
-     * Store a provided data at specified path. This acts as a add / replace
-     * operation, which is to say that whole subtree will be replaced by
-     * specified path.
-     *
-     * If you need add or merge of current object with specified use
-     * {@link #merge(org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType, org.opendaylight.yangtools.concepts.Path, Object)}
-     *
-     *
-     * @param path
-     * @param data
-     *            Data object to be written
-     *
-     * @throws IllegalStateException
-     *             if the client code already sealed transaction and invoked
-     *             {@link #ready()}
-     */
-    void write(YangInstanceIdentifier path, NormalizedNode<?, ?> data);
-
-    /**
-     * Store a provided data at specified path. This acts as a add / replace
-     * operation, which is to say that whole subtree will be replaced by
-     * specified path.
-     *
-     * If you need add or merge of current object with specified use
-     * {@link #merge(org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType, org.opendaylight.yangtools.concepts.Path, Object)}
-     *
-     *
-     * @param path
-     * @param data
-     *            Data object to be written
-     *
-     * @throws IllegalStateException
-     *             if the client code already sealed transaction and invoked
-     *             {@link #ready()}
-     */
-    void merge(YangInstanceIdentifier path, NormalizedNode<?, ?> data);
-
-    /**
-     *
-     * Deletes data and whole subtree located at provided path.
-     *
-     * @param path
-     *            Path to delete
-     * @throws IllegalStateException
-     *             if the client code already sealed transaction and invoked
-     *             {@link #ready()}
-     */
-    void delete(YangInstanceIdentifier path);
-
+public interface DOMStoreWriteTransaction extends DOMStoreTransaction, DOMStoreSyncMutator {
     /**
      *
      * Seals transaction, and returns three-phase commit cohort associated
@@ -70,5 +16,4 @@ public interface DOMStoreWriteTransaction extends DOMStoreTransaction {
      * @return Three Phase Commit Cohort instance for this transaction.
      */
     DOMStoreThreePhaseCommitCohort ready();
-
 }
