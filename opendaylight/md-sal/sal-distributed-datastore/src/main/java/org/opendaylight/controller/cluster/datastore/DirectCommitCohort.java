33c621bb8f20ab98023d84f0b3c8881358e3ffd3
Robert Varga <rovarga@cisco.com>
2015-04-24 13:33:24 +0200

    CDS: rework components to support non-remote operations
    Introduces ShardedTransactionChain (instead of TransactionChainProxy),
    which tracks potential local transaction chains.
    
    Updated TransactionProxy to hold a map of TransactionComponents -- each
    of which acts as proxy encapsulating how to interact with a particular
    shard. There are two basic flavors of components -- Local and Remote.
    Local transaction components are further diversified to
    read-onl/write-only/read-write kind.
    
    Selection of appropriate component is done via
    TransactionComponentFactory, of which we have two kinds -- Simple for
    disconnected transactions and ShardedTransactionChain, which handles
    waiting for previous transaction's proxies.
    
    Change-Id: If5e09335827d58a74ba704a255d359290e508e5f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DirectCommitCohort.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import akka.actor.ActorSelection;
+import akka.dispatch.OnComplete;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import org.opendaylight.controller.cluster.datastore.utils.ActorContext;
+import org.opendaylight.controller.sal.core.spi.data.DOMStoreThreePhaseCommitCohort;
+import scala.concurrent.Future;
+
+/**
+ * A {@link DOMStoreThreePhaseCommitCohort} which reports results as obtained by performing
+ * a direct commit on the backend. If resolution of actor succeeds, the transaction is assumed
+ * as having been committed.
+ */
+final class DirectCommitCohort implements DOMStoreThreePhaseCommitCohort {
+    private final SettableFuture<Boolean> canCommit;
+    private final SettableFuture<Void> others;
+
+    private DirectCommitCohort(final SettableFuture<Boolean> canCommit, final SettableFuture<Void> others) {
+        this.canCommit = Preconditions.checkNotNull(canCommit);
+        this.others = Preconditions.checkNotNull(others);
+    }
+
+    @Override
+    public ListenableFuture<Boolean> canCommit() {
+        return canCommit;
+    }
+
+    @Override
+    public ListenableFuture<Void> preCommit() {
+        return others;
+    }
+
+    @Override
+    public ListenableFuture<Void> abort() {
+        return others;
+    }
+
+    @Override
+    public ListenableFuture<Void> commit() {
+        return others;
+    }
+
+    static DOMStoreThreePhaseCommitCohort create(final ActorContext actorContext, final Future<ActorSelection> ret) {
+        final SettableFuture<Boolean> canCommit = SettableFuture.create();
+        final SettableFuture<Void> others = SettableFuture.create();
+
+        ret.onComplete(new OnComplete<ActorSelection>() {
+            @Override
+            public void onComplete(final Throwable failure, final ActorSelection success) throws Throwable {
+                if (failure != null) {
+                    others.setException(failure);
+                    canCommit.set(Boolean.FALSE);
+                } else {
+                    others.set(null);
+                    canCommit.set(Boolean.TRUE);
+                }
+            }
+        }, actorContext.getClientDispatcher());
+
+        return new DirectCommitCohort(canCommit, others);
+    }
+}
