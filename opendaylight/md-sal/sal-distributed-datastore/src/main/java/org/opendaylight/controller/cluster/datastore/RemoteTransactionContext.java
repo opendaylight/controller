758f1b85f069c0583a5918fbea8b93d2ff65a6d9
Robert Varga <rovarga@cisco.com>
2015-04-24 13:33:24 +0200

    WIP: CDS: rework components to support non-remote operations
    Introduces ShardedTransactionChain (instead of TransactionChainProxy),
    which tracks potential local transaction chains.
    
    Introduced ShardedTransaction (instead of TransactionProxy), which
    performs tracking of TransactionComponents, which act as proxies to the
    sub-transaction backend. There are two favors of those -- remote ones,
    which behave just as TransactionFutureCallback did and local ones, which
    are instantiated only after we have ascertained that the shard is local
    to us.
    
    Change-Id: If5e09335827d58a74ba704a255d359290e508e5f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/RemoteTransactionContext.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import com.google.common.base.Preconditions;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import org.opendaylight.controller.cluster.datastore.identifiers.ChainedTransactionIdentifier;
+import org.opendaylight.controller.cluster.datastore.identifiers.TransactionIdentifier;
+import org.opendaylight.controller.cluster.datastore.utils.ActorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class holding the remote context for a single {@link TransactionProxy} instance and
+ * is shared by all {@link RemoteTransactionComponent}s attached to it.
+ */
+final class RemoteTransactionContext {
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteTransactionContext.class);
+    private final TransactionProxy parent;
+    private final OperationCompleter operationCompleter;
+    private final Semaphore operationLimiter;
+
+    RemoteTransactionContext(final TransactionProxy parent) {
+        this.parent = Preconditions.checkNotNull(parent);
+        // Note : Currently mailbox-capacity comes from akka.conf and not from the config-subsystem
+        operationLimiter = new Semaphore(parent.getActorContext().getTransactionOutstandingOperationLimit());
+        operationCompleter = new OperationCompleter(operationLimiter);
+    }
+
+    ActorContext getActorContext() {
+        return parent.getActorContext();
+    }
+
+    TransactionIdentifier getIdentifier() {
+        return parent.getIdentifier();
+    }
+
+    String getTransactionChainId() {
+        if (getIdentifier() instanceof ChainedTransactionIdentifier) {
+            return ((ChainedTransactionIdentifier)getIdentifier()).getChainId();
+        } else {
+            return null;
+        }
+    }
+
+    TransactionType getTransactionType() {
+        return parent.getType();
+    }
+
+    Semaphore getOperationLimiter() {
+        return operationLimiter;
+    }
+
+    OperationCompleter getCompleter() {
+        return operationCompleter;
+    }
+
+    void throttleOperation() {
+        throttleOperation(1);
+    }
+
+    void throttleOperation(int acquirePermits) {
+        try {
+            if(!operationLimiter.tryAcquire(acquirePermits,
+                parent.getActorContext().getDatastoreContext().getOperationTimeoutInSeconds(), TimeUnit.SECONDS)){
+                LOG.warn("Failed to acquire operation permit for transaction {}", getIdentifier());
+            }
+        } catch (InterruptedException e) {
+            if(LOG.isDebugEnabled()) {
+                LOG.debug("Interrupted when trying to acquire operation permit for transaction " + getIdentifier(), e);
+            } else {
+                LOG.warn("Interrupted when trying to acquire operation permit for transaction {}", getIdentifier());
+            }
+        }
+    }
+}
