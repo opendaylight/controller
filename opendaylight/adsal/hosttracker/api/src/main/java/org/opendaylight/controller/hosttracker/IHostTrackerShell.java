e1ed67d780786997caad6c4f2020d11ef691c85d
Tony Tkacik <ttkacik@cisco.com>
2015-04-27 08:24:21 +0000

    Bug 868: Remove AD-SAL components which were deprecated in Lithium
    Removes legacy components,which were deprecated in Lithium
    and scheduled to be removed Berrylium.
    
    Code is still preserved on stable/lithium branch.
    
    Change-Id: Icef5e39cc59324db203ccb2cb5f9a3aea0e57b21
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/adsal/hosttracker/api/src/main/java/org/opendaylight/controller/hosttracker/IHostTrackerShell.java
+++
@@ -1,9 +0,0 @@
-package org.opendaylight.controller.hosttracker;
-
-import java.util.List;
-
-public interface IHostTrackerShell{
-
-    public List<String> dumpPendingArpReqList();
-    public List<String> dumpFailedArpReqList();
-}
