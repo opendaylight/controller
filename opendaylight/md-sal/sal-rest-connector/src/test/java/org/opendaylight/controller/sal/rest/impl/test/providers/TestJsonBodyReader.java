f77ca535d7e92f49907df057b67aaabaf5bb15b0
Jan Hajnar <jhajnar@cisco.com>
2015-05-04 16:57:49 +0200

    Bug 3104 - Sal Rest Connector: Data already exists for path when adding new item to list in augmentation
    * changed XmlNormalizedNodeBodyReader and JsonNormalizedNodeBodyReader
    to return YangInstanceIdentifier pointing to root of payload data.
    * changed BrokerFacade datastore functions for PUT and POST to use
    returned path from reader without modifications
    * modified rest connector tests according to new behavior and added
    tests for POST when payload is from augment/choice
    
    Change-Id: Iac6a9853ea4c4529d2f484c464bd27123f4fafa2
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonBodyReader.java
+++ opendaylight/md-sal/sal-rest-connector/src/test/java/org/opendaylight/controller/sal/rest/impl/test/providers/TestJsonBodyReader.java
@@ -8,10 +8,13 @@
 
 package org.opendaylight.controller.sal.rest.impl.test.providers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import com.google.common.base.Optional;
+import com.google.common.collect.Sets;
 import java.io.InputStream;
+import java.net.URI;
 import javax.ws.rs.core.MediaType;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,6 +28,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 /**
@@ -63,6 +67,7 @@ public class TestJsonBodyReader extends AbstractBodyReaderTest {
     @Test
     public void moduleDataTest() throws Exception {
         final DataSchemaNode dataSchemaNode = schemaContext.getDataChildByName("cont");
+        final YangInstanceIdentifier dataII = YangInstanceIdentifier.of(dataSchemaNode.getQName());
         final String uri = "instance-identifier-module:cont";
         mockBodyReader(uri, jsonBodyReader, false);
         final InputStream inputStream = TestJsonBodyReader.class
@@ -70,12 +75,15 @@ public class TestJsonBodyReader extends AbstractBodyReaderTest {
         final NormalizedNodeContext returnValue = jsonBodyReader
                 .readFrom(null, null, null, mediaType, null, inputStream);
         checkNormalizedNodeContext(returnValue);
-        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue);
+        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue, dataII);
     }
 
     @Test
     public void moduleSubContainerDataPutTest() throws Exception {
         final DataSchemaNode dataSchemaNode = schemaContext.getDataChildByName("cont");
+        QName cont1QName = QName.create(dataSchemaNode.getQName(), "cont1");
+        final YangInstanceIdentifier dataII = YangInstanceIdentifier.of(dataSchemaNode.getQName()).node(cont1QName);
+        final DataSchemaNode dataSchemaNodeOnPath = ((DataNodeContainer) dataSchemaNode).getDataChildByName(cont1QName);
         final String uri = "instance-identifier-module:cont/cont1";
         mockBodyReader(uri, jsonBodyReader, false);
         final InputStream inputStream = TestJsonBodyReader.class
@@ -83,12 +91,14 @@ public class TestJsonBodyReader extends AbstractBodyReaderTest {
         final NormalizedNodeContext returnValue = jsonBodyReader
                 .readFrom(null, null, null, mediaType, null, inputStream);
         checkNormalizedNodeContext(returnValue);
-        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue, "cont1");
+        checkExpectValueNormalizeNodeContext(dataSchemaNodeOnPath, returnValue, dataII);
     }
 
     @Test
     public void moduleSubContainerDataPostTest() throws Exception {
         final DataSchemaNode dataSchemaNode = schemaContext.getDataChildByName("cont");
+        QName cont1QName = QName.create(dataSchemaNode.getQName(), "cont1");
+        final YangInstanceIdentifier dataII = YangInstanceIdentifier.of(dataSchemaNode.getQName()).node(cont1QName);
         final String uri = "instance-identifier-module:cont";
         mockBodyReader(uri, jsonBodyReader, true);
         final InputStream inputStream = TestJsonBodyReader.class
@@ -96,7 +106,51 @@ public class TestJsonBodyReader extends AbstractBodyReaderTest {
         final NormalizedNodeContext returnValue = jsonBodyReader
                 .readFrom(null, null, null, mediaType, null, inputStream);
         checkNormalizedNodeContext(returnValue);
-        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue);
+        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue, dataII);
+    }
+
+    @Test
+    public void moduleSubContainerAugmentDataPostTest() throws Exception {
+        final DataSchemaNode dataSchemaNode = schemaContext.getDataChildByName("cont");
+        final Module augmentModule = schemaContext.findModuleByNamespace(new URI("augment:module")).iterator().next();
+        QName contAugmentQName = QName.create(augmentModule.getQNameModule(), "cont-augment");
+        YangInstanceIdentifier.AugmentationIdentifier augII = new YangInstanceIdentifier.AugmentationIdentifier(
+                Sets.newHashSet(contAugmentQName));
+        final YangInstanceIdentifier dataII = YangInstanceIdentifier.of(dataSchemaNode.getQName())
+                .node(augII).node(contAugmentQName);
+        final String uri = "instance-identifier-module:cont";
+        mockBodyReader(uri, jsonBodyReader, true);
+        final InputStream inputStream = TestXmlBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/json_augment_container.json");
+        final NormalizedNodeContext returnValue = jsonBodyReader
+                .readFrom(null, null, null, mediaType, null, inputStream);
+        checkNormalizedNodeContext(returnValue);
+        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue, dataII);
+    }
+
+    //FIXME: Uncomment this when JsonParserStream works correctly with case augmentation with choice
+    //@Test
+    public void moduleSubContainerChoiceAugmentDataPostTest() throws Exception {
+        final DataSchemaNode dataSchemaNode = schemaContext.getDataChildByName("cont");
+        final Module augmentModule = schemaContext.findModuleByNamespace(new URI("augment:module")).iterator().next();
+        QName augmentChoice1QName = QName.create(augmentModule.getQNameModule(), "augment-choice1");
+        QName augmentChoice2QName = QName.create(augmentChoice1QName, "augment-choice2");
+        final QName containerQName = QName.create(augmentChoice1QName, "case-choice-case-container1");
+        YangInstanceIdentifier.AugmentationIdentifier augChoice1II = new YangInstanceIdentifier.AugmentationIdentifier(
+                Sets.newHashSet(augmentChoice1QName));
+        YangInstanceIdentifier.AugmentationIdentifier augChoice2II = new YangInstanceIdentifier.AugmentationIdentifier(
+                Sets.newHashSet(augmentChoice2QName));
+        final YangInstanceIdentifier dataII = YangInstanceIdentifier.of(dataSchemaNode.getQName())
+                .node(augChoice1II).node(augmentChoice1QName).node(augChoice2II).node(augmentChoice2QName)
+                .node(containerQName);
+        final String uri = "instance-identifier-module:cont";
+        mockBodyReader(uri, jsonBodyReader, true);
+        final InputStream inputStream = TestXmlBodyReader.class
+                .getResourceAsStream("/instanceidentifier/json/json_augment_choice_container.json");
+        final NormalizedNodeContext returnValue = jsonBodyReader
+                .readFrom(null, null, null, mediaType, null, inputStream);
+        checkNormalizedNodeContext(returnValue);
+        checkExpectValueNormalizeNodeContext(dataSchemaNode, returnValue, dataII);
     }
 
     @Test
@@ -125,17 +179,10 @@ public class TestJsonBodyReader extends AbstractBodyReaderTest {
     }
 
     private void checkExpectValueNormalizeNodeContext(final DataSchemaNode dataSchemaNode,
-            final NormalizedNodeContext nnContext, final String localQname) {
-        YangInstanceIdentifier dataNodeIdent = YangInstanceIdentifier.of(dataSchemaNode.getQName());
-
-        if (localQname != null && dataSchemaNode instanceof DataNodeContainer) {
-            final DataSchemaNode child = ((DataNodeContainer) dataSchemaNode).getDataChildByName(localQname);
-            dataNodeIdent = YangInstanceIdentifier.builder(dataNodeIdent).node(child.getQName()).build();
-            assertTrue(nnContext.getInstanceIdentifierContext().getSchemaNode().equals(child));
-        } else {
-            assertTrue(nnContext.getInstanceIdentifierContext().getSchemaNode().equals(dataSchemaNode));
-        }
-        assertTrue(nnContext.getInstanceIdentifierContext().getInstanceIdentifier().equals(dataNodeIdent));
+                                                      final NormalizedNodeContext nnContext,
+                                                      final YangInstanceIdentifier dataNodeIdent) {
+        assertEquals(dataSchemaNode, nnContext.getInstanceIdentifierContext().getSchemaNode());
+        assertEquals(dataNodeIdent, nnContext.getInstanceIdentifierContext().getInstanceIdentifier());
         assertNotNull(NormalizedNodes.findNode(nnContext.getData(), dataNodeIdent));
     }
 }
