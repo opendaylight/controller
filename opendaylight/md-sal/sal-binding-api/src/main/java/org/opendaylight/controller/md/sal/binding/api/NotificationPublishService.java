b842770f6c60f7eafbcab0510a878c17fb17ffdc
Michal Rehak <mirehak@cisco.com>
2015-05-12 10:36:26 +0200

    BUG-3183: Extend notification publisher API
     - offer methods of notificationPublishService now return ListenableFuture
     - added exception for notification rejected outcome
    
    Change-Id: Iff17e1edd754d49e6c53f97e357c34f5eac6a8ef
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/NotificationPublishService.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/NotificationPublishService.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.controller.md.sal.binding.api;
 
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import java.util.concurrent.TimeUnit;
 import org.opendaylight.yangtools.yang.binding.Notification;
 
@@ -26,6 +28,13 @@ import org.opendaylight.yangtools.yang.binding.Notification;
  * notification has or has not been seen.
  */
 public interface NotificationPublishService extends BindingService {
+
+    /**
+     * Well-known value indicating that the binding-aware implementation is currently not
+     * able to accept a notification.
+     */
+    ListenableFuture<Object> REJECTED_BA = Futures.immediateFailedFuture(new NotificationRejectedException("Unacceptable blocking conditions encountered"));
+
     /**
      * Publishes a notification to subscribed listeners. This initiates
      * the process of sending the notification, but delivery to the
@@ -47,14 +56,14 @@ public interface NotificationPublishService extends BindingService {
      * listeners can happen asynchronously, potentially after a call to
      * this method returns.
      *
-     * This method is guaranteed not to block.
+     * This method returns listenable future.
      *
      * @param notification
      *            the notification to publish.
-     * @return true if the notification was accepted for processing, false otherwise
+     * @return future filled upon the notification was accepted for processing
      * @throws NullPointerException if the notification is null
      */
-    boolean offerNotification(Notification notification);
+    ListenableFuture<? extends Object> offerNotification(Notification notification);
 
     /**
      * Publishes a notification to subscribed listeners. This initiates
@@ -68,11 +77,12 @@ public interface NotificationPublishService extends BindingService {
      * @param timeout how long to wait before giving up, in units of unit
      * @param unit a TimeUnit determining how to interpret the
      *             timeout parameter
-     * @return true if the notification was accepted for processing, false otherwise
+     * @return future filled upon the notification was accepted for processing
      * @throws InterruptedException if interrupted while waiting
      * @throws NullPointerException if the notification or unit is null
      * @throws IllegalArgumentException if timeout is negative.
      */
-    boolean offerNotification(Notification notification, int timeout, TimeUnit unit)
-        throws InterruptedException;
+    ListenableFuture<? extends Object> offerNotification(Notification notification, int timeout, TimeUnit unit)
+            throws InterruptedException;
+
 }
