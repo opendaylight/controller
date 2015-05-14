fa15a6bb55f42fe5937acf2a9520a9c0b6352635
Tomas Cere <tcere@cisco.com>
2015-05-14 17:31:09 +0200

    Http client for testing restconf
    use --help to find out all suported args.
    
    Change-Id: Ib9cae0479d672d4432bbc0fe87dc98a32f524784
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/netconf/netconf-testtool/src/main/java/org/opendaylight/controller/netconf/test/tool/client/http/perf/AsyncExecutionStrategy.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.netconf.test.tool.client.http.perf;
+
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.HttpResponseStatus;
+import com.ning.http.client.ListenableFuture;
+import com.ning.http.client.Request;
+import com.ning.http.client.Response;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.opendaylight.controller.netconf.test.tool.client.stress.ExecutionStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsyncExecutionStrategy implements ExecutionStrategy{
+
+    private static final Logger LOG = LoggerFactory.getLogger(AsyncExecutionStrategy.class);
+
+    private final Parameters params;
+    private final ArrayList<Request> payloads;
+    private final AsyncHttpClient asyncHttpClient;
+    private final AtomicInteger requestThrottle = new AtomicInteger();
+    private final int reqLimit;
+
+    AsyncExecutionStrategy(final Parameters params, final AsyncHttpClient asyncHttpClient, final ArrayList<Request> payloads) {
+        this.params = params;
+        this.asyncHttpClient = asyncHttpClient;
+        this.payloads = payloads;
+        this.reqLimit = RestPerfClient.throttle;
+    }
+
+    @Override
+    public void invoke() {
+        final ArrayList<ListenableFuture<Response>> futures = new ArrayList<>();
+        LOG.info("Begin sending async requests");
+
+        for (final Request request : payloads) {
+            while (requestThrottle.get() == reqLimit) {
+                try {
+                    Thread.sleep(1l);
+                } catch (InterruptedException e) {
+                    LOG.error("Unable to sleep thread", e);
+                }
+            }
+            futures.add(asyncHttpClient.executeRequest(request, new AsyncCompletionHandler<Response>() {
+                @Override
+                public STATE onStatusReceived(HttpResponseStatus status) throws Exception {
+                    super.onStatusReceived(status);
+                    if (status.getStatusCode() != 200 && status.getStatusCode() != 204) {
+                        LOG.warn("Request failed, status code: {}", status.getStatusCode() + status.getStatusText());
+                        LOG.warn("request: {}", request.toString());
+                    }
+                    return STATE.CONTINUE;
+                }
+
+                @Override
+                public Response onCompleted(Response response) throws Exception {
+                    requestThrottle.decrementAndGet();
+                    return response;
+                }
+            }));
+            requestThrottle.incrementAndGet();
+        }
+        LOG.info("Requests sent, waiting for responses");
+
+        while (requestThrottle.get() > 0) {
+            try {
+                Thread.sleep(0);
+            } catch (InterruptedException e) {
+                LOG.error("Unable to sleep thread", e);
+            }
+        }
+
+        LOG.info("Responses received, ending...");
+    }
+}
