585ef7896db39651777e80bfbe022be1b3f6325b
Maros Marsalek <mmarsale@cisco.com>
2015-05-18 16:11:57 +0200

    BUG-2976 Resolve clash between current and feature cfg pusher
    Current config pusher now stores list of features present in karaf with the
    config snapshot itself. Feature pusher now looks at the list and ignores
    features listed in current config in the initial push.
    
    A WARN log message is emitted for each ignored config file + feature.
    
    Change-Id: I6e71e692e56d5219cf02cd704dd78215f4a7f5a2
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
+++ opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
@@ -7,9 +7,15 @@
  */
 package org.opendaylight.controller.configpusherfeature.internal;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.Sets;
+import java.util.Set;
+import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesListener;
 import org.apache.karaf.features.FeaturesService;
 import org.opendaylight.controller.config.persist.api.ConfigPusher;
+import org.opendaylight.controller.config.persist.storage.file.xml.FeatureListProvider;
+import org.opendaylight.controller.config.persist.storage.file.xml.XmlFileStorageAdapter;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
@@ -28,7 +34,22 @@ public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<Featur
     @Override
     public FeaturesService addingService(ServiceReference<FeaturesService> reference) {
         BundleContext bc = reference.getBundle().getBundleContext();
-        FeaturesService featureService = bc.getService(reference);
+        final FeaturesService featureService = bc.getService(reference);
+        final Optional<XmlFileStorageAdapter> currentPersister = XmlFileStorageAdapter.getInstance();
+
+        if(XmlFileStorageAdapter.getInstance().isPresent()) {
+            final Set<String> installedFeatureIds = Sets.newHashSet();
+            for (final Feature installedFeature : featureService.listInstalledFeatures()) {
+                installedFeatureIds.add(installedFeature.getId());
+            }
+
+            currentPersister.get().setFeaturesService(new FeatureListProvider() {
+                @Override
+                public Set<String> listFeatures() {
+                    return installedFeatureIds;
+                }
+            });
+        }
         configFeaturesListener = new ConfigFeaturesListener(configPusher,featureService);
         registration = bc.registerService(FeaturesListener.class.getCanonicalName(), configFeaturesListener, null);
         return featureService;
