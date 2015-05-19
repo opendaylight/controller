8d03ac62bb735fc2fa75db8b4b576060cb5e0101
Robert Varga <rovarga@cisco.com>
2015-05-19 22:04:10 +0200

    Move operation limiter down to TransactionContextWrapper
    The limiter tracks the number of operations invoked on the shard leader,
    which does not correspond to the number of operations executed on the
    frontend.
    
    The appropriate entity to decide what is throttled how is the
    TransactionContext, which unfortunately may not exist. We will solve
    this problem by making TransactionContextWrapper perform pessimistic
    limiting as long as the context does not exist. Once the context is
    materialized, the outstanding operation queue is handed off to it and
    the context becomes the entity managing the limits.
    
    This patch has the side-effect that committing a transaction requires
    number of permits equal to the number of shards it touches. It also
    ensures that readAll() is properly throttled.
    
    Change-Id: If91816d806bbb3895592e1f42b0b8e389443d0f7
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    (cherry picked from commit 9e7a9b3725ad25f9adf85f0ad796b7cf748795a4)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractTransactionContext.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractTransactionContext.java
@@ -7,31 +7,78 @@
  */
 package org.opendaylight.controller.cluster.datastore;
 
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
 import org.opendaylight.controller.cluster.datastore.identifiers.TransactionIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 abstract class AbstractTransactionContext implements TransactionContext {
-
     private static final Logger LOG = LoggerFactory.getLogger(AbstractTransactionContext.class);
-
+    private final OperationLimiter limiter;
     private long modificationCount = 0;
+    private boolean handoffComplete;
 
-    private final TransactionIdentifier identifier;
-
-    protected AbstractTransactionContext(TransactionIdentifier identifier) {
-        this.identifier = identifier;
+    protected AbstractTransactionContext(final OperationLimiter limiter) {
+        this.limiter = Preconditions.checkNotNull(limiter);
     }
 
-    protected final TransactionIdentifier getIdentifier() {
-        return identifier;
+    /**
+     * Get the transaction identifier associated with this context.
+     *
+     * @return Transaction identifier.
+     */
+    @Nonnull protected final TransactionIdentifier getIdentifier() {
+        return limiter.getIdentifier();
     }
 
-    protected void incrementModificationCount(){
+    /**
+     * Return the operation limiter associated with this context.
+     * @return Operation limiter.
+     */
+    @Nonnull protected final OperationLimiter getLimiter() {
+        return limiter;
+    }
+
+    /**
+     * Indicate whether all operations have been handed off by the {@link TransactionContextWrapper}.
+     *
+     * @return True if this context is responsible for throttling.
+     */
+    protected final boolean isOperationHandoffComplete() {
+        return handoffComplete;
+    }
+
+    /**
+     * Acquire operation from the limiter if the handoff has completed. If
+     * the handoff is still ongoing, this method does nothing.
+     */
+    protected final void acquireOperation() {
+        if (handoffComplete) {
+            limiter.acquire();
+        }
+    }
+
+    /**
+     * Acquire operation from the limiter if the handoff has NOT completed. If
+     * the handoff has completed, this method does nothing.
+     */
+    protected final void releaseOperation() {
+        if (!handoffComplete) {
+            limiter.release();
+        }
+    }
+
+    protected final void incrementModificationCount() {
         modificationCount++;
     }
 
-    protected void logModificationCount(){
-        LOG.debug("Total modifications on Tx {} = [ {} ]", identifier, modificationCount);
+    protected final void logModificationCount() {
+        LOG.debug("Total modifications on Tx {} = [ {} ]", getIdentifier(), modificationCount);
+    }
+
+    @Override
+    public final void operationHandoffComplete() {
+        handoffComplete = true;
     }
 }
