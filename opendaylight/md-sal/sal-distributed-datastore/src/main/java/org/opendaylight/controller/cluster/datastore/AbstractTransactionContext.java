50a58b3bf2adfdb78e16500910a162d8fe092348
Robert Varga <rovarga@cisco.com>
2015-05-19 22:04:10 +0200

    Move operation limiter down to TransactionContextWrapper
    The limiter tracks the number of operations invoked on the shard leader,
    which does not correspond to the number of operations executed on the
    frontend.
    
    The appropriate entity to decide what is throttled how is the
    TransactionContext, which unfortunately may not exist. We will solve
    this problem by making TransactionContextWrapper perform pessimistic
    limiting as long as the context does not exist. Once the context is
    materialized, the outstanding operation queue is handed off to it and
    the context becomes the entity managing the limits.
    
    This patch has the side-effect that committing a transaction requires
    number of permits equal to the number of shards it touches. It also
    ensures that readAll() is properly throttled.
    
    Change-Id: If91816d806bbb3895592e1f42b0b8e389443d0f7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractTransactionContext.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractTransactionContext.java
@@ -7,17 +7,66 @@
  */
 package org.opendaylight.controller.cluster.datastore;
 
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
 import org.opendaylight.controller.cluster.datastore.identifiers.TransactionIdentifier;
 
 abstract class AbstractTransactionContext implements TransactionContext {
+    private final OperationLimiter limiter;
+    private boolean handoffComplete;
 
-    private final TransactionIdentifier identifier;
-
-    protected AbstractTransactionContext(TransactionIdentifier identifier) {
-        this.identifier = identifier;
+    protected AbstractTransactionContext(final OperationLimiter limiter) {
+        this.limiter = Preconditions.checkNotNull(limiter);
     }
 
-    protected final TransactionIdentifier getIdentifier() {
-        return identifier;
+    /**
+     * Get the transaction identifier associated with this context.
+     *
+     * @return Transaction identifier.
+     */
+    @Nonnull protected final TransactionIdentifier getIdentifier() {
+        return limiter.getIdentifier();
+    }
+
+    /**
+     * Return the operation limiter associated with this context.
+     * @return Operation limiter.
+     */
+    @Nonnull protected final OperationLimiter getLimiter() {
+        return limiter;
+    }
+
+    /**
+     * Indicate whether all operations have been handed off by the {@link TransactionContextWrapper}.
+     *
+     * @return True if this context is responsible for throttling.
+     */
+    protected final boolean isOperationHandoffComplete() {
+        return handoffComplete;
+    }
+
+    /**
+     * Acquire operation from the limiter if the handoff has completed. If
+     * the handoff is still ongoing, this method does nothing.
+     */
+    protected final void acquireOperation() {
+        if (handoffComplete) {
+            limiter.acquire();
+        }
+    }
+
+    /**
+     * Acquire operation from the limiter if the handoff has NOT completed. If
+     * the handoff has completed, this method does nothing.
+     */
+    protected final void releaseOperation() {
+        if (!handoffComplete) {
+            limiter.release();
+        }
+    }
+
+    @Override
+    public final void operationHandoffComplete() {
+        handoffComplete = true;
     }
 }
