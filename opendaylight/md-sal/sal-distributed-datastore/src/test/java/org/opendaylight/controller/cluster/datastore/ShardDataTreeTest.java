b0c749ee6653a54191219070b69a1f695b0eeb78
Moiz Raja <moraja@cisco.com>
2015-05-22 17:22:02 -0700

    BUG 2970 : Create a PruningDataTreeModification
    The PruningDataTreeModification automatically tries to prune the passed
    in NormalizedNode if an IllegalArgumentException is thrown when doing
    a write or a merge on the DataTreeModification.
    
    *I know* that doing the pruning on IllegalArgumentException is flaky but
    that is the best I can do unless we can change the implementation in yangtools
    to throw a more specific exception when the data node does not map to a
    the schema context
    
    Change-Id: Iea0cea82eda8bd51c1f3f07b063404458798c348
    Signed-off-by: Moiz Raja <moraja@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/ShardDataTreeTest.java
@@ -0,0 +1,86 @@
+package org.opendaylight.controller.cluster.datastore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import com.google.common.base.Optional;
+import java.util.concurrent.ExecutionException;
+import org.junit.Test;
+import org.opendaylight.controller.md.cluster.datastore.model.CarsModel;
+import org.opendaylight.controller.md.cluster.datastore.model.PeopleModel;
+import org.opendaylight.controller.md.cluster.datastore.model.SchemaContextHelper;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeSnapshot;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class ShardDataTreeTest {
+    @Test
+    public void testWrite() throws ExecutionException, InterruptedException {
+
+        SchemaContext schemaContext = SchemaContextHelper.full();
+
+        modify(schemaContext, false, true, true);
+    }
+
+    @Test
+    public void testWriteWithMissingSchema() throws ExecutionException, InterruptedException {
+
+        SchemaContext schemaContext = SchemaContextHelper.select(SchemaContextHelper.ODL_DATASTORE_TEST_YANG, SchemaContextHelper.PEOPLE_YANG);
+
+        modify(schemaContext, false, false, true);
+    }
+
+    @Test
+    public void testMerge() throws ExecutionException, InterruptedException {
+
+        SchemaContext schemaContext = SchemaContextHelper.full();
+
+        modify(schemaContext, true, true, true);
+    }
+
+    @Test
+    public void testMergeWithMissingSchema() throws ExecutionException, InterruptedException {
+
+        SchemaContext schemaContext = SchemaContextHelper.select(SchemaContextHelper.ODL_DATASTORE_TEST_YANG, SchemaContextHelper.PEOPLE_YANG);
+
+        modify(schemaContext, true, false, true);
+    }
+
+    private void modify(SchemaContext schemaContext, boolean merge, boolean expectedCarsPresent, boolean expectedPeoplePresent) throws ExecutionException, InterruptedException {
+        ShardDataTree shardDataTree = new ShardDataTree(schemaContext);
+
+        ReadWriteShardDataTreeTransaction transaction = shardDataTree.newReadWriteTransaction("txn-1", null);
+
+        DataTreeModification snapshot = transaction.getSnapshot();
+
+        assertNotNull(snapshot);
+
+        if(merge){
+            snapshot.merge(CarsModel.BASE_PATH, CarsModel.create());
+            snapshot.merge(PeopleModel.BASE_PATH, PeopleModel.create());
+        } else {
+            snapshot.write(CarsModel.BASE_PATH, CarsModel.create());
+            snapshot.write(PeopleModel.BASE_PATH, PeopleModel.create());
+        }
+
+        ShardDataTreeCohort cohort = shardDataTree.finishTransaction(transaction);
+
+        cohort.preCommit().get();
+        cohort.commit().get();
+
+
+        ReadOnlyShardDataTreeTransaction readOnlyShardDataTreeTransaction = shardDataTree.newReadOnlyTransaction("txn-2", null);
+
+        DataTreeSnapshot snapshot1 = readOnlyShardDataTreeTransaction.getSnapshot();
+
+        Optional<NormalizedNode<?, ?>> optional = snapshot1.readNode(CarsModel.BASE_PATH);
+
+        assertEquals(expectedCarsPresent, optional.isPresent());
+
+        Optional<NormalizedNode<?, ?>> optional1 = snapshot1.readNode(PeopleModel.BASE_PATH);
+
+        assertEquals(expectedPeoplePresent, optional1.isPresent());
+
+    }
+
+}
