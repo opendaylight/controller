2cc1425b399c253608313bcf6af4ccab2db6c821
Jan Hajnar <jhajnar@cisco.com>
2015-05-28 23:28:46 +0200

    Bug 3427 - Websockets: Cannot create stream if path contains predicates or if target is a leaf
    * fixed registration of path containing nodes with predicates
    * added abillity to listen on leaves
    
    Change-Id: I2e74aa6bbc50137187a5f7ece8de6ca288a1ebbf
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>
    (cherry picked from commit 2a5c10374e32ca292466c3a43a120bfc2df0de90)

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfImpl.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/RestconfImpl.java
@@ -568,8 +568,7 @@ public class RestconfImpl implements RestconfService {
                 .withChild(ImmutableNodes.leafNode(streamNameQname, streamName)).build();
 
         if (!Notificator.existListenerFor(streamName)) {
-            final YangInstanceIdentifier normalizedPathIdentifier = controllerContext.toNormalized(pathIdentifier);
-            Notificator.createListener(normalizedPathIdentifier, streamName);
+            Notificator.createListener(pathIdentifier, streamName);
         }
 
         final DOMRpcResult defaultDOMRpcResult = new DefaultDOMRpcResult(output);
