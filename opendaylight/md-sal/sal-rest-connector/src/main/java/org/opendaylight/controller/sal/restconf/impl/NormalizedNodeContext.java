9827309368b933052fa1c213ad787a8965e60ade
Jan Hajnar <jhajnar@cisco.com>
2015-06-02 17:47:08 +0200

    BUG 2155 - depth parameter in URI
    * added interface RestconfNormalizedNodeWriter
    * added two implementations of RestconfNormalizedNodeWriter,
    RestconfDelegatingNormalizedNodeWriter just calls normalized node
    writer from yangtools and DepthAwareNormalizedNodeWriter is writer
    implementation tha checks depth
    * added CutDataToCorrectDepthTest (randomly failing, needs to be checked
    or removed for now)
    * added condition to xml and json normalized node writers to create
    depth aware normalized node writer id depth is specified in writer parameters
    
    Change-Id: I922942e24cbe505c2803644c25acd755fe4dfae7
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/NormalizedNodeContext.java
+++ opendaylight/md-sal/sal-rest-connector/src/main/java/org/opendaylight/controller/sal/restconf/impl/NormalizedNodeContext.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.controller.sal.restconf.impl;
 
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
@@ -21,7 +28,7 @@ public class NormalizedNodeContext {
         this.context = context;
         this.data = data;
         // default writer parameters
-        this.writerParameters = new WriterParameters(false, Integer.MAX_VALUE);
+        this.writerParameters = new WriterParameters.WriterParametersBuilder().build();
     }
 
     public InstanceIdentifierContext<? extends SchemaNode> getInstanceIdentifierContext() {
