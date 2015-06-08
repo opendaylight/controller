1a316e659fe8015e418c31c79ddf963db5abaea0
Tony Tkacik <ttkacik@cisco.com>
2015-06-08 16:49:36 +0200

    Bug 1014: Added config option to IMDS to enable config enforcement.
    Change-Id: I276c25523ac4954abc752cf7cf4eb58ee0d744d4
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-inmemory-datastore/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/InMemoryDOMDataStoreFactory.java
+++ opendaylight/md-sal/sal-inmemory-datastore/src/main/java/org/opendaylight/controller/md/sal/dom/store/impl/InMemoryDOMDataStoreFactory.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutorService;
 import javax.annotation.Nullable;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.yangtools.util.concurrent.SpecialExecutors;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
 
 /**
  * A factory for creating InMemoryDOMDataStore instances.
@@ -39,8 +40,9 @@ public final class InMemoryDOMDataStoreFactory {
     public static InMemoryDOMDataStore create(final String name,
             @Nullable final SchemaService schemaService,
             @Nullable final InMemoryDOMDataStoreConfigProperties properties) {
-        return create(name, schemaService, false, properties);
+        return create(name, TreeType.OPERATIONAL, schemaService, false, properties);
     }
+
 
     /**
      * Creates an InMemoryDOMDataStore instance.
@@ -55,6 +57,22 @@ public final class InMemoryDOMDataStoreFactory {
     public static InMemoryDOMDataStore create(final String name,
             @Nullable final SchemaService schemaService, final boolean debugTransactions,
             @Nullable final InMemoryDOMDataStoreConfigProperties properties) {
+        return create(name, TreeType.OPERATIONAL, schemaService, debugTransactions, properties);
+    }
+
+    /**
+     * Creates an InMemoryDOMDataStore instance.
+     *
+     * @param name the name of the data store
+     * @param schemaService the SchemaService to which to register the data store.
+     * @param debugTransactions enable transaction debugging
+     * @param properties configuration properties for the InMemoryDOMDataStore instance. If null,
+     *                   default property values are used.
+     * @return an InMemoryDOMDataStore instance
+     */
+    public static InMemoryDOMDataStore create(final String name, final TreeType treeType,
+            @Nullable final SchemaService schemaService, final boolean debugTransactions,
+            @Nullable final InMemoryDOMDataStoreConfigProperties properties) {
 
         InMemoryDOMDataStoreConfigProperties actualProperties = properties;
         if (actualProperties == null) {
@@ -65,13 +83,14 @@ public final class InMemoryDOMDataStoreFactory {
         // task execution time to get higher throughput as DataChangeListeners typically provide
         // much of the business logic for a data model. If the executor queue size limit is reached,
         // subsequent submitted notifications will block the calling thread.
-        int dclExecutorMaxQueueSize = actualProperties.getMaxDataChangeExecutorQueueSize();
-        int dclExecutorMaxPoolSize = actualProperties.getMaxDataChangeExecutorPoolSize();
+        final int dclExecutorMaxQueueSize = actualProperties.getMaxDataChangeExecutorQueueSize();
+        final int dclExecutorMaxPoolSize = actualProperties.getMaxDataChangeExecutorPoolSize();
 
-        ExecutorService dataChangeListenerExecutor = SpecialExecutors.newBlockingBoundedFastThreadPool(
+        final ExecutorService dataChangeListenerExecutor = SpecialExecutors.newBlockingBoundedFastThreadPool(
                 dclExecutorMaxPoolSize, dclExecutorMaxQueueSize, name + "-DCL" );
 
-        final InMemoryDOMDataStore dataStore = new InMemoryDOMDataStore(name, dataChangeListenerExecutor,
+
+        final InMemoryDOMDataStore dataStore = new InMemoryDOMDataStore(name, treeType, dataChangeListenerExecutor,
                 actualProperties.getMaxDataChangeListenerQueueSize(), debugTransactions);
 
         if (schemaService != null) {
