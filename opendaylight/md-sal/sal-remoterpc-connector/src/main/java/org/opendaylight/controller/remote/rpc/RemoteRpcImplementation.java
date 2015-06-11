8320675f853d6c94b80dd0555b048ad15e74f285
Tony Tkacik <ttkacik@cisco.com>
2015-06-11 17:19:46 +0200

    Removed uncessary calls to RpcBroker to find routes.
    Moved invokeRpc part to RemoteRpcImplementation
    which allowed to do ask for lookups in RemoteRpcImplementation.
    
    This changed role of RpcBroker part to only delegate
    to MD-SAL, if RPC was received via Akka.
    
    remote.rpc.RpcBroker interaction model represented
    multi-stepped pipeline which resulted in following
    message pattern
    
    RemoteRpcImplementaion ->
    RpcBroker#InvokeRpc ->
    RpcRegistry#FindRoutes ->
    RpcBroker#ExecuteRpc
    
    InvokeRpc only did lookup in FindRoutes and all
    outgoing messages needed to pass via RpcBroker.
    Unfortunatelly this also prevented lookup
    of any RPC Path during executing RPC in MD-SAL.
    
    Change-Id: I6e84bfcb74b71f7417c2d3f8c35a7f8b0406caf9
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/remote/rpc/RemoteRpcImplementation.java
@@ -3,27 +3,77 @@ package org.opendaylight.controller.remote.rpc;
 import static akka.pattern.Patterns.ask;
 
 import akka.actor.ActorRef;
+import akka.dispatch.OnComplete;
+import akka.japi.Pair;
 import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
+import java.util.List;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcException;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementation;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementationNotAvailableException;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
-import org.opendaylight.controller.remote.rpc.messages.InvokeRpc;
+import org.opendaylight.controller.remote.rpc.messages.ExecuteRpc;
+import org.opendaylight.controller.remote.rpc.registry.RpcRegistry;
+import org.opendaylight.controller.remote.rpc.registry.RpcRegistry.Messages.FindRoutersReply;
+import org.opendaylight.controller.remote.rpc.utils.LatestEntryRoutingLogic;
+import org.opendaylight.controller.sal.connector.api.RpcRouter;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.Future;
 
 public class RemoteRpcImplementation implements DOMRpcImplementation {
-    private final ActorRef rpcBroker;
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteRpcImplementation.class);
+
+    private final ActorRef rpcRegistry;
     private final RemoteRpcProviderConfig config;
 
-    public RemoteRpcImplementation(final ActorRef rpcBroker, final RemoteRpcProviderConfig config) {
-        this.rpcBroker = rpcBroker;
+    public RemoteRpcImplementation(final ActorRef rpcRegistry, final RemoteRpcProviderConfig config) {
         this.config = config;
+        this.rpcRegistry = rpcRegistry;
     }
 
     @Override
-    public CheckedFuture<DOMRpcResult, DOMRpcException> invokeRpc(final DOMRpcIdentifier rpc, final NormalizedNode<?, ?> input) {
-        final InvokeRpc rpcMsg = new InvokeRpc(rpc.getType().getLastComponent(), rpc.getContextReference(), input);
-        final scala.concurrent.Future<Object> future = ask(rpcBroker, rpcMsg, config.getAskDuration());
-        return RemoteDOMRpcFuture.from(future);
+    public CheckedFuture<DOMRpcResult, DOMRpcException> invokeRpc(final DOMRpcIdentifier rpc,
+            final NormalizedNode<?, ?> input) {
+        if (input instanceof RemoteRpcInput) {
+            LOG.warn("Rpc {} was removed during execution or there is loop present. Failing received rpc.", rpc);
+            return Futures
+                    .<DOMRpcResult, DOMRpcException>immediateFailedCheckedFuture(new DOMRpcImplementationNotAvailableException(
+                            "Rpc implementation for {} was removed during processing.", rpc));
+        }
+        final RemoteDOMRpcFuture frontEndFuture = RemoteDOMRpcFuture.create(rpc.getType().getLastComponent());
+        findRouteAsync(rpc).onComplete(new OnComplete<FindRoutersReply>() {
+
+            @Override
+            public void onComplete(final Throwable error, final FindRoutersReply routes) throws Throwable {
+                if (error != null) {
+                    frontEndFuture.failNow(error);
+                } else {
+                    final List<Pair<ActorRef, Long>> routePairs = routes.getRouterWithUpdateTime();
+                    if (routePairs == null || routePairs.isEmpty()) {
+                        frontEndFuture.failNow(new DOMRpcImplementationNotAvailableException(
+                                "No local or remote implementation available for rpc %s", rpc.getType(), error));
+                    } else {
+                        final ActorRef remoteImplRef = new LatestEntryRoutingLogic(routePairs).select();
+                        final Object executeRpcMessage = ExecuteRpc.from(rpc, input);
+                        LOG.debug("Found remote actor {} for rpc {} - sending {}", remoteImplRef, rpc.getType(), executeRpcMessage);
+                        frontEndFuture.completeWith(ask(remoteImplRef, executeRpcMessage, config.getAskDuration()));
+                    }
+                }
+            }
+        }, ExecutionContext.Implicits$.MODULE$.global());
+        return frontEndFuture;
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private Future<FindRoutersReply> findRouteAsync(final DOMRpcIdentifier rpc) {
+        // FIXME: Refactor routeId and message to use DOMRpcIdentifier directly.
+        final RpcRouter.RouteIdentifier<?, ?, ?> routeId =
+                new RouteIdentifierImpl(null, rpc.getType().getLastComponent(), rpc.getContextReference());
+        final RpcRegistry.Messages.FindRouters findMsg = new RpcRegistry.Messages.FindRouters(routeId);
+        return (Future) ask(rpcRegistry, findMsg, config.getAskDuration());
     }
 }
--- opendaylight/md-sal/sal-remoterpc-connector/src/test/java/org/opendaylight/controller/remote/rpc/RpcListenerTest.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/test/java/org/opendaylight/controller/remote/rpc/RpcListenerTest.java
@@ -8,26 +8,70 @@
 
 package org.opendaylight.controller.remote.rpc;
 
+
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.JavaTestKit;
 import com.typesafe.config.ConfigFactory;
+import java.net.URISyntaxException;
+import java.util.Collections;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
+import org.opendaylight.controller.remote.rpc.registry.RpcRegistry;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 
 public class RpcListenerTest {
 
-  static ActorSystem system;
+    private static final QName TEST_QNAME = QName.create("test", "2015-06-12", "test");
+    private static final SchemaPath RPC_TYPE = SchemaPath.create(true, TEST_QNAME);
+    private static final YangInstanceIdentifier TEST_PATH = YangInstanceIdentifier
+            .create(new YangInstanceIdentifier.NodeIdentifier(TEST_QNAME));
+    private static final DOMRpcIdentifier RPC_ID = DOMRpcIdentifier.create(RPC_TYPE, TEST_PATH);
+    static ActorSystem system;
 
 
-  @BeforeClass
-  public static void setup() throws InterruptedException {
-    system = ActorSystem.create("opendaylight-rpc", ConfigFactory.load().getConfig("odl-cluster-rpc"));
-  }
+    @BeforeClass
+    public static void setup() throws InterruptedException {
+        system = ActorSystem.create("opendaylight-rpc", ConfigFactory.load().getConfig("odl-cluster-rpc"));
+    }
 
-  @AfterClass
-  public static void teardown() {
-    JavaTestKit.shutdownActorSystem(system);
-    system = null;
-  }
+    @AfterClass
+    public static void teardown() {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
+    }
 
+    @Test
+    public void testRouteAdd() throws URISyntaxException, InterruptedException {
+        new JavaTestKit(system) {
+            {
+                // Test announcements
+                final JavaTestKit probeReg = new JavaTestKit(system);
+                final ActorRef rpcRegistry = probeReg.getRef();
+
+                final RpcListener rpcListener = new RpcListener(rpcRegistry);
+                rpcListener.onRpcAvailable(Collections.singleton(RPC_ID));
+                probeReg.expectMsgClass(RpcRegistry.Messages.AddOrUpdateRoutes.class);
+            }
+        };
+    }
+
+    @Test
+    public void testRouteRemove() throws URISyntaxException, InterruptedException {
+        new JavaTestKit(system) {
+            {
+                // Test announcements
+                final JavaTestKit probeReg = new JavaTestKit(system);
+                final ActorRef rpcRegistry = probeReg.getRef();
+
+                final RpcListener rpcListener = new RpcListener(rpcRegistry);
+                rpcListener.onRpcUnavailable(Collections.singleton(RPC_ID));
+                probeReg.expectMsgClass(RpcRegistry.Messages.RemoveRoutes.class);
+            }
+        };
+    }
 }
