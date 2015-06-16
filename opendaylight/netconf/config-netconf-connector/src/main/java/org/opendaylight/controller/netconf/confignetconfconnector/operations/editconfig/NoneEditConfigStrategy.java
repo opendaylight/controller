f776391b8945ff506e22723d9822705d49973db5
Maros Marsalek <mmarsale@cisco.com>
2015-06-16 13:36:30 +0200

    BUG-3625 Allow replace nested composite nodes in cfg-subsystem
    Netconf-endpoint for config-subsystem now supports replace operation on nested
    elements. Now it is possible to invoke PUT on nested composite attributes for
    config-subsystem modules.
    
    Change-Id: I5fde907c66c01a9c1bf58845daa868c7a141c998
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- opendaylight/netconf/config-netconf-connector/src/main/java/org/opendaylight/controller/netconf/confignetconfconnector/operations/editconfig/NoneEditConfigStrategy.java
+++ opendaylight/netconf/config-netconf-connector/src/main/java/org/opendaylight/controller/netconf/confignetconfconnector/operations/editconfig/NoneEditConfigStrategy.java
@@ -8,8 +8,10 @@
 
 package org.opendaylight.controller.netconf.confignetconfconnector.operations.editconfig;
 
+import java.util.Collections;
 import java.util.Map;
 import org.opendaylight.controller.config.util.ConfigTransactionClient;
+import org.opendaylight.controller.netconf.confignetconfconnector.exception.NetconfConfigHandlingException;
 import org.opendaylight.controller.netconf.confignetconfconnector.mapping.attributes.fromxml.AttributeConfigElement;
 import org.opendaylight.controller.netconf.confignetconfconnector.mapping.config.ServiceRegistryWrapper;
 import org.slf4j.Logger;
@@ -21,8 +23,21 @@ public class NoneEditConfigStrategy implements EditConfigStrategy {
 
     @Override
     public void executeConfiguration(String module, String instance, Map<String, AttributeConfigElement> configuration,
-                                     ConfigTransactionClient ta, ServiceRegistryWrapper services) {
-        LOG.debug("Skipping configuration element for {}:{}", module, instance);
+                                     ConfigTransactionClient ta, ServiceRegistryWrapper services) throws NetconfConfigHandlingException {
+        if(configuration != null && !configuration.isEmpty()) {
+            for (Map.Entry<String, AttributeConfigElement> attrEntry : configuration.entrySet()) {
+                if(attrEntry.getValue().getEditStrategy().isPresent()) {
+                    final Map<String, AttributeConfigElement> partialConfig =
+                            Collections.singletonMap(attrEntry.getKey(), attrEntry.getValue());
+                    attrEntry.getValue().getEditStrategy().get().getFittingStrategy()
+                            .executeConfiguration(module, instance, partialConfig, ta, services);
+                } else {
+                    LOG.debug("Skipping configuration element for {}:{}:{}", module, instance, attrEntry.getKey());
+                }
+            }
+        } else {
+            LOG.debug("Skipping configuration element for {}:{}", module, instance);
+        }
     }
 
 }
