98a12fd8256bf8feee9bb364ade1ee5f21079d61
Tom Pantelis <tpanteli@brocade.com>
2015-07-01 16:58:28 -0400

    CDS: Add pending tx queue size to ShardStats
    To aid debugging, I added the pending commit queue size of the
    ShardCommitCoordinator to the ShardStats bean.
    
    Change-Id: I2af3493eb5dd54f9f9406b0a005d66be004c12ff
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>
    (cherry picked from commit 5fc8785d919cd3cad92036b1add1a7fdd25bf715)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/jmx/mbeans/shard/ShardMBeanFactory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/jmx/mbeans/shard/ShardMBeanFactory.java
@@ -7,12 +7,6 @@
  */
 package org.opendaylight.controller.cluster.datastore.jmx.mbeans.shard;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Basheeruddin syedbahm@cisco.com
@@ -20,26 +14,10 @@ import org.slf4j.LoggerFactory;
  */
 public class ShardMBeanFactory {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ShardMBeanFactory.class);
-
-    private static final Cache<String,ShardStats> shardMBeansCache =
-                                      CacheBuilder.newBuilder().weakValues().build();
-
     public static ShardStats getShardStatsMBean(final String shardName, final String mxBeanType) {
-        final String finalMXBeanType = mxBeanType != null ? mxBeanType : "DistDataStore";
-        try {
-            return shardMBeansCache.get(shardName, new Callable<ShardStats>() {
-                @Override
-                public ShardStats call() throws Exception {
-                    ShardStats shardStatsMBeanImpl = new ShardStats(shardName, finalMXBeanType);
-                    shardStatsMBeanImpl.registerMBean();
-                    return shardStatsMBeanImpl;
-                }
-            });
-        } catch(ExecutionException e) {
-            LOG.error(String.format("Could not create MXBean for shard: %s", shardName), e);
-            // Just return an instance that isn't registered.
-            return new ShardStats(shardName, finalMXBeanType);
-        }
+        String finalMXBeanType = mxBeanType != null ? mxBeanType : "DistDataStore";
+        ShardStats shardStatsMBeanImpl = new ShardStats(shardName, finalMXBeanType);
+        shardStatsMBeanImpl.registerMBean();
+        return shardStatsMBeanImpl;
     }
 }
