24776cc268fe2583206acfde93033296753c160c
Moiz Raja <moraja@cisco.com>
2015-07-21 19:11:10 -0700

    BUG 2185 : Make the Custom Raft Policy externally configurable
    A class which implements RaftPolicy needs to be set
    to customize the behavior. I am hoping that this will be a barrier
    to people unintentionally breaking the default raft policy as
    they would need to know the internals of clustering in order to
    supply a valid class.
    
    I added two configurable raft policy classes for the two known
    use cases,
    
    - org.opendaylight.controller.cluster.datastore.policy.TestOnlyRaftPolicy
    - org.opendaylight.controller.cluster.datastore.policy.TwoNodeClusterRaftPolicy.
    
    Change-Id: Ic3cc2f27754c37e85c3be8a863764fc88ec84399
    Signed-off-by: Moiz Raja <moraja@cisco.com>
    (cherry picked from commit 1ef2f7321202ef77b5cfd0ca430e6a58cb07331e)

---
+++ opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/DefaultConfigParamsImplTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.opendaylight.controller.cluster.raft.policy.DefaultRaftPolicy;
+import org.opendaylight.controller.cluster.raft.policy.RaftPolicy;
+import org.opendaylight.controller.cluster.raft.policy.TestRaftPolicy;
+
+public class DefaultConfigParamsImplTest {
+
+    @Test
+    public void testGetRaftPolicyWithDefault(){
+        DefaultConfigParamsImpl params = new DefaultConfigParamsImpl();
+
+        assertEquals("Default instance", DefaultRaftPolicy.INSTANCE, params.getRaftPolicy());
+    }
+
+    @Test
+    public void testGetRaftPolicyInvalidClassName(){
+        DefaultConfigParamsImpl params = new DefaultConfigParamsImpl();
+        params.setCustomRaftPolicyImplementationClass("foobar");
+
+        assertEquals("Default instance", DefaultRaftPolicy.INSTANCE, params.getRaftPolicy());
+    }
+
+    @Test
+    public void testGetRaftPolicyValidClassNameButInvalidType(){
+        DefaultConfigParamsImpl params = new DefaultConfigParamsImpl();
+        params.setCustomRaftPolicyImplementationClass("java.lang.String");
+
+        assertEquals("Default instance", DefaultRaftPolicy.INSTANCE, params.getRaftPolicy());
+    }
+
+    @Test
+    public void testGetRaftPolicyValidClass(){
+        DefaultConfigParamsImpl params1 = new DefaultConfigParamsImpl();
+        params1.setCustomRaftPolicyImplementationClass("org.opendaylight.controller.cluster.raft.policy.TestRaftPolicy");
+        RaftPolicy behavior1 = params1.getRaftPolicy();
+
+        assertEquals("TestCustomBehavior", TestRaftPolicy.class, behavior1.getClass());
+        assertEquals("Same instance returned", behavior1, params1.getRaftPolicy());
+
+        DefaultConfigParamsImpl params2 = new DefaultConfigParamsImpl();
+        RaftPolicy behavior2 = params2.getRaftPolicy();
+        params1.setCustomRaftPolicyImplementationClass("org.opendaylight.controller.cluster.raft.policy.TestRaftPolicy");
+
+        assertEquals("Default instance", DefaultRaftPolicy.INSTANCE, behavior2);
+        assertEquals("Default instance", DefaultRaftPolicy.INSTANCE, params2.getRaftPolicy());
+
+    }
+
+
+}
