8a1ef425e60d7c27faa40735f3ef1bbc5133a750
Ed Warnicke <hagbard@gmail.com>
2015-07-25 14:42:37 -0700

    Changes to startup archetype to incorporate IT testing
    Note: The onSessionInitiated unit test can't every really work in a
    realistic system because of the absence of an MD-SAL to work with,
    thus its deleted so people can build real MD-SAL based Providers.
    
    Change-Id: Ie635752b6e2eec8ce1c65b737871be8c0b6bd870
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ opendaylight/archetypes/opendaylight-startup/src/main/resources/archetype-resources/it/src/test/java/__packageInPathFormat__/it/__classPrefix__IT.java
@@ -0,0 +1,74 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * ${copyright} and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package ${package}.it;
+
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
+import org.opendaylight.hello.HelloIT;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.options.MavenUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class ${classPrefix}IT extends AbstractMdsalTestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(${classPrefix}IT.class);
+    
+    @Override
+    public String getModuleName() {
+        return "${artifactId}";
+    }
+
+    @Override
+    public String getInstanceName() {
+        return "${artifactId}-default";
+    }
+
+    @Override
+    public MavenUrlReference getFeatureRepo() {
+        return maven()
+                .groupId("${groupId}")
+                .artifactId("${artifactId}-features")
+                .classifier("features")
+                .type("xml")
+                .versionAsInProject();
+    }
+
+    @Override
+    public String getFeatureName() {
+        return "odl-${artifactId}-ui";
+    }
+    
+    @Override
+    public Option getLoggingOption() {
+        Option option = editConfigurationFilePut(ORG_OPS4J_PAX_LOGGING_CFG,
+                logConfiguration(${classPrefix}IT.class),
+                LogLevel.INFO.name());
+        option = composite(option, super.getLoggingOption());
+        return option;
+    }
+    
+    @Test
+    public void test${artifactId}FeatureLoad() {
+        Assert.assertTrue(true);
+    }
+}
