d470313ed22ccd596d425194bf025a09aa2031b0
kalaiselvik <Kalaiselvi_K@Dell.com>
2015-07-28 18:31:39 +0530

    Bug 1724: Making the akka configuration file path configurable using the config subsystem
    Change-Id: I19f753f1e47e71623471997e01865aef2ffd2c84
    Signed-off-by: kalaiselvik <Kalaiselvi_K@Dell.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileConfigurationReader.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileConfigurationReader.java
@@ -11,18 +11,35 @@ package org.opendaylight.controller.cluster.datastore.config;
 import com.google.common.base.Preconditions;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class FileConfigurationReader implements ConfigurationReader{
 
     public static final String AKKA_CONF_PATH = "./configuration/initial/akka.conf";
+    private static final Logger LOG = LoggerFactory.getLogger(FileConfigurationReader.class);
+    private String configPath;
 
+    public FileConfigurationReader() {
+        this.configPath = "./configuration/initial/";
+    }
+    public FileConfigurationReader(String path) {
+        Preconditions.checkNotNull(path, "akka configuration path should not be null");
+        this.configPath = path;
+    }
     @Override
     public Config read() {
-        File defaultConfigFile = new File(AKKA_CONF_PATH);
+        File defaultConfigFile = null;
+        String fileName = configPath+"akka.conf";
+        defaultConfigFile = new File(fileName);
+        if (!defaultConfigFile.exists()) {
+            LOG.info ("akka.conf does not exist in the given path {}, searching in the default path", fileName);
+            defaultConfigFile = new File(AKKA_CONF_PATH);
+        }
+
         Preconditions.checkState(defaultConfigFile.exists(), "akka.conf is missing");
         return ConfigFactory.parseFile(defaultConfigFile);
-
     }
 }
--- opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/config/yang/config/remote_rpc_connector/RemoteRPCBrokerModule.java
+++ opendaylight/md-sal/sal-remoterpc-connector/src/main/java/org/opendaylight/controller/config/yang/config/remote_rpc_connector/RemoteRPCBrokerModule.java
@@ -1,13 +1,21 @@
 package org.opendaylight.controller.config.yang.config.remote_rpc_connector;
 
+import org.opendaylight.controller.cluster.common.actor.AkkaConfigurationReader;
 import org.opendaylight.controller.cluster.common.actor.DefaultAkkaConfigurationReader;
 import org.opendaylight.controller.remote.rpc.RemoteRpcProviderConfig;
 import org.opendaylight.controller.remote.rpc.RemoteRpcProviderFactory;
 import org.opendaylight.controller.sal.core.api.Broker;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.framework.ServiceReference;
+import java.util.Dictionary;
+import java.io.IOException;
 
 public class RemoteRPCBrokerModule extends org.opendaylight.controller.config.yang.config.remote_rpc_connector.AbstractRemoteRPCBrokerModule {
   private BundleContext bundleContext;
+  private static final String CONFIG_FILE = "org.opendaylight.controller.cluster.datastore";
+  private static final String CLUSTER_CONFIG_PATH = "config.cluster-config-path";
   public RemoteRPCBrokerModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
     super(identifier, dependencyResolver);
   }
@@ -30,10 +38,39 @@ public class RemoteRPCBrokerModule extends org.opendaylight.controller.config.ya
   public java.lang.AutoCloseable createInstance() {
     Broker broker = getDomBrokerDependency();
 
+    ServiceReference<ConfigurationAdmin> srvRef =
+                    bundleContext.getServiceReference(ConfigurationAdmin.class);
+    AkkaConfigurationReader reader = null;
+    if(srvRef != null) {
+        try {
+            ConfigurationAdmin configAdmin = bundleContext.getService(srvRef);
+            Configuration conf = configAdmin.getConfiguration(CONFIG_FILE);
+            if(conf != null) {
+                Dictionary<String, Object> configs = conf.getProperties();
+
+                if ((configs != null) && (!configs.isEmpty())) {
+                    String path = configs.get(CLUSTER_CONFIG_PATH).toString();
+                    if (path != null) {
+                        reader = new DefaultAkkaConfigurationReader(path);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // Ignoring exceptions
+        } catch(IllegalStateException e) {
+            // Ignoring exceptions
+        } finally {
+            bundleContext.ungetService(srvRef);
+        }
+    }
+    if (reader == null) {
+        reader = new DefaultAkkaConfigurationReader();
+    }
+
     RemoteRpcProviderConfig config = new RemoteRpcProviderConfig.Builder(getActorSystemName())
                               .metricCaptureEnabled(getEnableMetricCapture())
                               .mailboxCapacity(getBoundedMailboxCapacity())
-                              .withConfigReader(new DefaultAkkaConfigurationReader())
+                              .withConfigReader(reader)
                               .build();
 
     return RemoteRpcProviderFactory.createInstance(broker, bundleContext, config);
