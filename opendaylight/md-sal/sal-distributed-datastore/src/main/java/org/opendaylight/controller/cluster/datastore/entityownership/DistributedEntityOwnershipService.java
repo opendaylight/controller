0d9b804ef20b3528d1d9d1526007fc478aeb9797
Tom Pantelis <tpanteli@brocade.com>
2015-08-07 16:20:48 -0400

    Bug 4105: Add EntityOwnershipShard
    Added the EntityOwnershipShard and modified
    DistributedEntityOwnershipService to create it.
    
    Change-Id: Id173b148797e90ff5d38d7f7cde177d303943181
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/DistributedEntityOwnershipService.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/DistributedEntityOwnershipService.java
@@ -7,7 +7,12 @@
  */
 package org.opendaylight.controller.cluster.datastore.entityownership;
 
+import akka.actor.ActorRef;
+import akka.dispatch.OnComplete;
+import akka.util.Timeout;
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.controller.cluster.datastore.DistributedDataStore;
+import org.opendaylight.controller.cluster.datastore.messages.CreateShard;
 import org.opendaylight.controller.md.sal.common.api.clustering.CandidateAlreadyRegisteredException;
 import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipCandidate;
@@ -17,6 +22,7 @@ import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipL
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.concurrent.Future;
 
 /**
  * The distributed implementation of the EntityOwnershipService.
@@ -25,6 +31,8 @@ import org.slf4j.LoggerFactory;
  */
 public class DistributedEntityOwnershipService implements EntityOwnershipService, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(DistributedEntityOwnershipService.class);
+    static final String ENTITY_OWNERSHIP_SHARD_NAME = "entity-ownership";
+    private static final Timeout MESSAGE_TIMEOUT = new Timeout(1, TimeUnit.MINUTES);
 
     private final DistributedDataStore datastore;
 
@@ -33,7 +41,25 @@ public class DistributedEntityOwnershipService implements EntityOwnershipService
     }
 
     public void start() {
-        LOG.info("DistributedEntityOwnershipService starting");
+        ActorRef shardManagerActor = datastore.getActorContext().getShardManager();
+
+        CreateShard createShard = new CreateShard(ENTITY_OWNERSHIP_SHARD_NAME,
+                datastore.getActorContext().getConfiguration().getUniqueMemberNamesForAllShards(),
+                new EntityOwnershipShardPropsCreator(), null);
+
+        Future<Object> createFuture = datastore.getActorContext().executeOperationAsync(shardManagerActor,
+                createShard, MESSAGE_TIMEOUT);
+
+        createFuture.onComplete(new OnComplete<Object>() {
+            @Override
+            public void onComplete(Throwable failure, Object response) {
+                if(failure != null) {
+                    LOG.error("Failed to create {} shard", ENTITY_OWNERSHIP_SHARD_NAME);
+                } else {
+                    LOG.info("Successfully created {} shard", ENTITY_OWNERSHIP_SHARD_NAME);
+                }
+            }
+        }, datastore.getActorContext().getClientDispatcher());
     }
 
     @Override
