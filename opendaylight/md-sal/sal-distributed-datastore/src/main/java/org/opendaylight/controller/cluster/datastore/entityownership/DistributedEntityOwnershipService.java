7533613cffa81b867a4b6303053459a84faa9101
Tom Pantelis <tpanteli@brocade.com>
2015-08-07 11:04:53 -0400

    Bug 4105: Add EntityOwnershipShard
    Change-Id: Id173b148797e90ff5d38d7f7cde177d303943181
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/DistributedEntityOwnershipService.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/DistributedEntityOwnershipService.java
@@ -7,10 +7,17 @@
  */
 package org.opendaylight.controller.cluster.datastore.entityownership;
 
+import akka.actor.ActorRef;
+import akka.dispatch.Futures;
+import akka.dispatch.OnComplete;
+import akka.util.Timeout;
+import java.util.concurrent.TimeUnit;
 import org.opendaylight.controller.cluster.datastore.DistributedDataStore;
+import org.opendaylight.controller.cluster.datastore.messages.CreateShard;
 import org.opendaylight.controller.sal.core.spi.entityownership.EntityOwnershipService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.concurrent.Future;
 
 /**
  * The distributed implementation of the EntityOwnershipService.
@@ -19,18 +26,44 @@ import org.slf4j.LoggerFactory;
  */
 public class DistributedEntityOwnershipService implements EntityOwnershipService, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(DistributedEntityOwnershipService.class);
+    private static final String ENTITY_OWNERSHIP_SHARD_NAME = "entity-ownership";
 
     private final DistributedDataStore datastore;
+    private volatile Future<ActorRef> entityOwnershipShardFuture;
 
     public DistributedEntityOwnershipService(DistributedDataStore datastore) {
         this.datastore = datastore;
     }
 
     public void start() {
-        LOG.info("DistributedEntityOwnershipService starting");
+        ActorRef shardManagerActor = datastore.getActorContext().getShardManager();
+
+        CreateShard createShard = new CreateShard(ENTITY_OWNERSHIP_SHARD_NAME,
+                datastore.getActorContext().getConfiguration().getUniqueMemberNamesForAllShards(),
+                new EntityOwnershipShardPropsCreator(), null);
+
+        Future<Object> createFuture = datastore.getActorContext().executeOperationAsync(shardManagerActor, createShard,
+                new Timeout(1, TimeUnit.MINUTES));
+        createFuture.onComplete(new OnComplete<Object>() {
+            @Override
+            public void onComplete(Throwable failure, Object response) {
+                onCreateShardComplete(failure, response);
+            }
+        }, datastore.getActorContext().getClientDispatcher());
+    }
+
+    private void onCreateShardComplete(Throwable failure, Object response) {
+        if(failure != null) {
+            entityOwnershipShardFuture = Futures.failed(failure);
+            return;
+        }
+
+        LOG.info("DistributedEntityOwnershipService successfully created {} shard", ENTITY_OWNERSHIP_SHARD_NAME);
+
+        entityOwnershipShardFuture = datastore.getActorContext().findLocalShardAsync(ENTITY_OWNERSHIP_SHARD_NAME);
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
     }
 }
