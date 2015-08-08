dfa7b4f296c6b90666bf2e231e1befc97b4fabb4
Tom Pantelis <tpanteli@brocade.com>
2015-08-08 06:57:03 -0400

    Bug 4105: Implement candidate registration close
    Added an UnregisterCandidateLocal message which is sent when a
    DistributedEntityOwnershipCandidateRegistration is closed.
    
    Change-Id: I6336e1b83a7764bfb4abc2fc37e196175c008dc3
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/messages/UnregisterCandidateLocal.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.entityownership.messages;
+
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+
+/**
+ * Message sent to the local EntityOwnershipShard to unregister a candidate.
+ *
+ * @author Thomas Pantelis
+ */
+public class UnregisterCandidateLocal {
+    private final Entity entity;
+
+    public UnregisterCandidateLocal(Entity entity) {
+        this.entity = entity;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("UnregisterCandidateLocal [entity=").append(entity).append("]");
+        return builder.toString();
+    }
+}
