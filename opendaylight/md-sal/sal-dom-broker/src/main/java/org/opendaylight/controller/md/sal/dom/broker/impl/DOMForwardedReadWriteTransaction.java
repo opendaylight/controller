d16ccd43e544f6493672d4bfd1e7103f4eb70494
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-08 01:53:27 -0400

    Fix license header violations in sal-dom-broker
    Change-Id: I78a5b836b7ea5053ede25a31e8a40319d365fe60
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/broker/impl/DOMForwardedReadWriteTransaction.java
+++ opendaylight/md-sal/sal-dom-broker/src/main/java/org/opendaylight/controller/md/sal/dom/broker/impl/DOMForwardedReadWriteTransaction.java
@@ -4,7 +4,9 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- */package org.opendaylight.controller.md.sal.dom.broker.impl;
+ */
+
+package org.opendaylight.controller.md.sal.dom.broker.impl;
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
