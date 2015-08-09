b420586db5d4d99dfc6055c6c0197a6ab6d1d2d1
Tom Pantelis <tpanteli@brocade.com>
2015-08-09 00:22:02 -0400

    Bug 4105: Implement RegisterCandidate in EntityOwnershipShard
    Change-Id: Idab615399d81a8451e22bfabd30aed9a98e4b037
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/EntityOwnershipShard.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/EntityOwnershipShard.java
@@ -7,14 +7,32 @@
  */
 package org.opendaylight.controller.cluster.datastore.entityownership;
 
+import akka.actor.ActorRef;
 import akka.actor.Props;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import org.opendaylight.controller.cluster.datastore.DataStoreVersions;
 import org.opendaylight.controller.cluster.datastore.DatastoreContext;
 import org.opendaylight.controller.cluster.datastore.Shard;
 import org.opendaylight.controller.cluster.datastore.entityownership.messages.RegisterCandidateLocal;
 import org.opendaylight.controller.cluster.datastore.entityownership.messages.UnregisterCandidateLocal;
 import org.opendaylight.controller.cluster.datastore.identifiers.ShardIdentifier;
+import org.opendaylight.controller.cluster.datastore.messages.BatchedModifications;
 import org.opendaylight.controller.cluster.datastore.messages.SuccessReply;
+import org.opendaylight.controller.cluster.datastore.modification.MergeModification;
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.clustering.entity.owners.rev150804.EntityOwners;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.clustering.entity.owners.rev150804.entity.owners.EntityType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.clustering.entity.owners.rev150804.entity.owners.entity.type.entity.Candidate;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 /**
@@ -22,15 +40,26 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
  *
  * @author Thomas Pantelis
  */
-public class EntityOwnershipShard extends Shard {
+class EntityOwnershipShard extends Shard {
+    static final YangInstanceIdentifier ENTITY_OWNERS_PATH = YangInstanceIdentifier.of(EntityOwners.QNAME);
+    static final  QName ENTITY_QNAME = org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.
+            md.sal.clustering.entity.owners.rev150804.entity.owners.entity.type.Entity.QNAME;
+    static final QName CANDIDATE_NAME = QName.create(Candidate.QNAME, "name");
+    static final QName ENTITY_ID = QName.create(ENTITY_QNAME, "id");
+    static final QName ENTITY_TYPE = QName.create(EntityType.QNAME, "type");
+
+    private int transactionIDCounter = 0;
+    private final String localMemberName;
+    private final List<BatchedModifications> retryModifications = new ArrayList<>();
 
     private static DatastoreContext noPersistenceDatastoreContext(DatastoreContext datastoreContext) {
         return DatastoreContext.newBuilderFrom(datastoreContext).persistent(false).build();
     }
 
     protected EntityOwnershipShard(ShardIdentifier name, Map<String, String> peerAddresses,
-            DatastoreContext datastoreContext, SchemaContext schemaContext) {
+            DatastoreContext datastoreContext, SchemaContext schemaContext, String localMemberName) {
         super(name, peerAddresses, noPersistenceDatastoreContext(datastoreContext), schemaContext);
+        this.localMemberName = localMemberName;
     }
 
     @Override
@@ -50,8 +79,70 @@ public class EntityOwnershipShard extends Shard {
     }
 
     private void onRegisterCandidateLocal(RegisterCandidateLocal registerCandidate) {
-        // TODO - implement
+        LOG.debug("onRegisterCandidateLocal: {}", registerCandidate);
+
+        // TODO - add the listener locally.
+
+        BatchedModifications modifications = new BatchedModifications("txn-" + ++transactionIDCounter,
+                DataStoreVersions.CURRENT_VERSION, "");
+        modifications.setDoCommitOnReady(true);
+        modifications.setReady(true);
+        modifications.setTotalMessagesSent(1);
+
+        NormalizedNode<?, ?> entityOwners = createEntityOwnersWithCandidate(registerCandidate.getEntity(), localMemberName);
+        modifications.addModification(new MergeModification(ENTITY_OWNERS_PATH, entityOwners));
+
+        tryCommitModifications(modifications);
+
         getSender().tell(SuccessReply.INSTANCE, getSelf());
+    }
+
+    private NormalizedNode<?, ?> createEntityOwnersWithCandidate(Entity entity, String memberName) {
+        MapNode candidateNode = ImmutableNodes.mapNodeBuilder(Candidate.QNAME).addChild(
+                        ImmutableNodes.mapEntry(Candidate.QNAME, CANDIDATE_NAME, memberName)).build();
+
+        MapEntryNode entityNode = ImmutableNodes.mapEntryBuilder(ENTITY_QNAME, ENTITY_ID, entity.getId()).
+                addChild(candidateNode).build();
+
+        MapEntryNode entityTypeNode = ImmutableNodes.mapEntryBuilder(EntityType.QNAME, ENTITY_TYPE, entity.getType()).
+                addChild(ImmutableNodes.mapNodeBuilder(ENTITY_QNAME).addChild(entityNode).build()).build();
+
+        return ImmutableContainerNodeBuilder.create().withNodeIdentifier(new NodeIdentifier(EntityOwners.QNAME)).
+                addChild(ImmutableNodes.mapNodeBuilder(EntityType.QNAME).addChild(entityTypeNode).build()).build();
+    }
+
+    private void tryCommitModifications(BatchedModifications modifications) {
+        if (isLeader()) {
+            LOG.debug("Committing entity candidate registration");
+
+            // TODO - should use a no-op actor for sender
+            ActorRef sender = self();
+
+            // TODO - make this eventually consistent, ie apply before replicate? Then we wouldn't have to
+            // handle consensus failure here.
+            handleBatchedModificationsLocal(modifications, sender);
+        } else if (getLeader() != null) {
+            // TODO - send remote message with ask
+        } else {
+            LOG.debug("No leader - adding modifications for retry");
+
+            retryModifications.add(modifications);
+        }
+    }
+
+    @Override
+    protected void onLeaderChanged(String oldLeader, String newLeader) {
+        super.onLeaderChanged(oldLeader, newLeader);
+
+        if(!retryModifications.isEmpty()) {
+            LOG.debug("# BatchedModifications to retry {}", retryModifications.size());
+
+            List<BatchedModifications> retryModificationsCopy = new ArrayList<>(retryModifications);
+            retryModifications.clear();
+            for(BatchedModifications mods: retryModificationsCopy) {
+                tryCommitModifications(mods);
+            }
+        }
     }
 
     private void onUnregisterCandidateLocal(UnregisterCandidateLocal unregisterCandidate) {
@@ -60,21 +151,25 @@ public class EntityOwnershipShard extends Shard {
     }
 
     public static Props props(final ShardIdentifier name, final Map<String, String> peerAddresses,
-            final DatastoreContext datastoreContext, final SchemaContext schemaContext) {
-        return Props.create(new Creator(name, peerAddresses, datastoreContext, schemaContext));
+            final DatastoreContext datastoreContext, final SchemaContext schemaContext, final String localMemberName) {
+        return Props.create(new Creator(name, peerAddresses, datastoreContext, schemaContext, localMemberName));
     }
 
     private static class Creator extends AbstractShardCreator {
         private static final long serialVersionUID = 1L;
 
+        private final String localMemberName;
+
         Creator(final ShardIdentifier name, final Map<String, String> peerAddresses,
-                final DatastoreContext datastoreContext, final SchemaContext schemaContext) {
+                final DatastoreContext datastoreContext, final SchemaContext schemaContext,
+                final String localMemberName) {
             super(name, peerAddresses, datastoreContext, schemaContext);
+            this.localMemberName = localMemberName;
         }
 
         @Override
         public Shard create() throws Exception {
-            return new EntityOwnershipShard(name, peerAddresses, datastoreContext, schemaContext);
+            return new EntityOwnershipShard(name, peerAddresses, datastoreContext, schemaContext, localMemberName);
         }
     }
 }
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/entityownership/EntityOwnershipShardTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.entityownership;
+
+import static org.mockito.Mockito.mock;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.dispatch.Dispatchers;
+import akka.testkit.TestActorRef;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.Uninterruptibles;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.After;
+import org.junit.Test;
+import org.opendaylight.controller.cluster.datastore.AbstractShardTest;
+import org.opendaylight.controller.cluster.datastore.DataStoreVersions;
+import org.opendaylight.controller.cluster.datastore.DatastoreContext;
+import org.opendaylight.controller.cluster.datastore.DatastoreContext.Builder;
+import org.opendaylight.controller.cluster.datastore.ShardTestKit;
+import org.opendaylight.controller.cluster.datastore.entityownership.messages.RegisterCandidateLocal;
+import org.opendaylight.controller.cluster.datastore.identifiers.ShardIdentifier;
+import org.opendaylight.controller.cluster.datastore.messages.SuccessReply;
+import org.opendaylight.controller.cluster.raft.ReplicatedLogEntry;
+import org.opendaylight.controller.cluster.raft.TestActorFactory;
+import org.opendaylight.controller.cluster.raft.messages.AppendEntries;
+import org.opendaylight.controller.cluster.raft.messages.AppendEntriesReply;
+import org.opendaylight.controller.cluster.raft.messages.RequestVote;
+import org.opendaylight.controller.cluster.raft.messages.RequestVoteReply;
+import org.opendaylight.controller.md.cluster.datastore.model.SchemaContextHelper;
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipCandidate;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Unit tests for EntityOwnershipShard.
+ *
+ * @author Thomas Pantelis
+ */
+public class EntityOwnershipShardTest extends AbstractEntityOwnershipTest {
+    private static final String ENTITY_TYPE = "test type";
+    private static final QName ENTITY_QNAME1 = QName.create("test", "2015-08-14", "entity1");
+    private static final SchemaContext SCHEMA_CONTEXT = SchemaContextHelper.entityOwners();
+    private static final AtomicInteger NEXT_SHARD_NUM = new AtomicInteger();
+    private static final String LOCAL_MEMBER_NAME = "member-1";
+
+    private final ShardIdentifier shardID = ShardIdentifier.builder().memberName(LOCAL_MEMBER_NAME)
+            .shardName("entity-ownership").type("operational" + NEXT_SHARD_NUM.getAndIncrement()).build();
+
+    private final Builder dataStoreContextBuilder = DatastoreContext.newBuilder();
+    private final TestActorFactory actorFactory = new TestActorFactory(getSystem());
+
+    @After
+    public void tearDown() {
+        actorFactory.close();
+    }
+
+    @Test
+    public void testOnRegisterCandidateLocal() throws Exception {
+        ShardTestKit kit = new ShardTestKit(getSystem());
+
+        TestActorRef<EntityOwnershipShard> shard = actorFactory.createTestActor(newShardProps());
+
+        kit.waitUntilLeader(shard);
+
+        YangInstanceIdentifier entityId = YangInstanceIdentifier.of(ENTITY_QNAME1);
+        Entity entity = new Entity(ENTITY_TYPE, entityId);
+        EntityOwnershipCandidate candidate = mock(EntityOwnershipCandidate.class);
+
+        shard.tell(new RegisterCandidateLocal(candidate, entity), kit.getRef());
+        kit.expectMsgClass(SuccessReply.class);
+
+        verifyEntityCandidate(shard, ENTITY_TYPE, entityId, LOCAL_MEMBER_NAME);
+    }
+
+    @Test
+    public void testOnRegisterCandidateLocalWithNoInitialLeader() throws Exception {
+        ShardTestKit kit = new ShardTestKit(getSystem());
+
+        dataStoreContextBuilder.shardHeartbeatIntervalInMillis(100).shardElectionTimeoutFactor(2);
+
+        String peerId = actorFactory.generateActorId("peer");
+        TestActorRef<TestPeer> peer = actorFactory.createTestActor(Props.create(TestPeer.class, peerId).
+                withDispatcher(Dispatchers.DefaultDispatcherId()), peerId);
+
+        TestActorRef<EntityOwnershipShard> shard = actorFactory.createTestActor(newShardProps(
+                ImmutableMap.<String, String>builder().put(peerId, peer.path().toString()).build()).
+                withDispatcher(Dispatchers.DefaultDispatcherId()));
+
+        YangInstanceIdentifier entityId = YangInstanceIdentifier.of(ENTITY_QNAME1);
+        Entity entity = new Entity(ENTITY_TYPE, entityId);
+        EntityOwnershipCandidate candidate = mock(EntityOwnershipCandidate.class);
+
+        shard.tell(new RegisterCandidateLocal(candidate, entity), kit.getRef());
+        kit.expectMsgClass(SuccessReply.class);
+
+        peer.underlyingActor().grantVote = true;
+
+        verifyEntityCandidate(shard, ENTITY_TYPE, entityId, LOCAL_MEMBER_NAME);
+    }
+
+    private void verifyEntityCandidate(TestActorRef<EntityOwnershipShard> shard, String entityType,
+            YangInstanceIdentifier entityId, String candidateName) throws Exception {
+        verifyEntityCandidate(readEntityOwners(shard), entityType, entityId, candidateName);
+    }
+
+    private NormalizedNode<?, ?> readEntityOwners(TestActorRef<EntityOwnershipShard> shard) throws Exception {
+        Stopwatch sw = Stopwatch.createStarted();
+        while(sw.elapsed(TimeUnit.MILLISECONDS) <= 5000) {
+            NormalizedNode<?, ?> node = AbstractShardTest.readStore(shard, EntityOwnershipShard.ENTITY_OWNERS_PATH);
+            if(node != null) {
+                return node;
+            }
+
+            Uninterruptibles.sleepUninterruptibly(100, TimeUnit.MILLISECONDS);
+        }
+
+        return null;
+    }
+
+    private Props newShardProps() {
+        return newShardProps(Collections.<String,String>emptyMap());
+    }
+
+    private Props newShardProps(Map<String,String> peers) {
+        return EntityOwnershipShard.props(shardID, peers, dataStoreContextBuilder.build(), SCHEMA_CONTEXT,
+                LOCAL_MEMBER_NAME);
+    }
+
+    public static class TestPeer extends UntypedActor {
+        volatile boolean grantVote;
+        private final String myId;
+
+        public TestPeer(String myId) {
+            this.myId = myId;
+        }
+
+        @Override
+        public void onReceive(Object message) {
+            if(message instanceof RequestVote) {
+                if(grantVote) {
+                    System.out.println("!!!!GRANTING VOTE "+message);
+                    getSender().tell(new RequestVoteReply(((RequestVote)message).getTerm(), true), getSelf());
+                }
+            } else if(message instanceof AppendEntries) {
+                AppendEntries req = (AppendEntries) message;
+                long lastIndex = req.getLeaderCommit();
+                if (req.getEntries().size() > 0) {
+                    for(ReplicatedLogEntry entry : req.getEntries()) {
+                        lastIndex = entry.getIndex();
+                    }
+                }
+
+                getSender().tell(new AppendEntriesReply(myId, req.getTerm(), true, lastIndex, req.getTerm(),
+                        DataStoreVersions.CURRENT_VERSION), getSelf());
+            }
+        }
+    }
+}
