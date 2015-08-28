6303d53efb8b49335fc8adae3ce86e650bdfda1b
Stephen Kitt <skitt@redhat.com>
2015-08-28 18:22:36 +0200

    Bug 4129 - Switch to Karaf 4.0.1 [DO NOT MERGE, WIP]
    This needs I66e6512c06d8a94d85de5fabcf981b2f73e00f7e (Karaf 4.0.1
    support in odlparent).
    
    It breaks at :karaf-parent currently.
    
    Change-Id: Ie41cd727942ad2083ef0ca33e03f0d08003c546d
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
+++ opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
@@ -9,7 +9,9 @@ package org.opendaylight.controller.configpusherfeature.internal;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Sets;
+
 import java.util.Set;
+
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesListener;
 import org.apache.karaf.features.FeaturesService;
@@ -20,10 +22,12 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<FeaturesService, FeaturesService>, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureServiceCustomizer.class);
     private ConfigPusher configPusher = null;
-    private ConfigFeaturesListener configFeaturesListener = null;
     private ServiceRegistration<?> registration;
 
     FeatureServiceCustomizer(ConfigPusher c) {
@@ -37,10 +41,14 @@ public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<Featur
         final FeaturesService featureService = bc.getService(reference);
         final Optional<XmlFileStorageAdapter> currentPersister = XmlFileStorageAdapter.getInstance();
 
-        if(XmlFileStorageAdapter.getInstance().isPresent()) {
+        if (XmlFileStorageAdapter.getInstance().isPresent()) {
             final Set<String> installedFeatureIds = Sets.newHashSet();
-            for (final Feature installedFeature : featureService.listInstalledFeatures()) {
-                installedFeatureIds.add(installedFeature.getId());
+            try {
+                for (final Feature installedFeature : featureService.listInstalledFeatures()) {
+                    installedFeatureIds.add(installedFeature.getId());
+                }
+            } catch (Exception e) {
+                LOG.error("Error listing installed features", e);
             }
 
             currentPersister.get().setFeaturesService(new FeatureListProvider() {
@@ -50,27 +58,27 @@ public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<Featur
                 }
             });
         }
-        configFeaturesListener = new ConfigFeaturesListener(configPusher,featureService);
+        ConfigFeaturesListener configFeaturesListener = new ConfigFeaturesListener(configPusher, featureService);
         registration = bc.registerService(FeaturesListener.class.getCanonicalName(), configFeaturesListener, null);
         return featureService;
     }
 
     @Override
     public void modifiedService(ServiceReference<FeaturesService> reference,
-            FeaturesService service) {
+                                FeaturesService service) {
         // we don't care if the properties change
 
     }
 
     @Override
     public void removedService(ServiceReference<FeaturesService> reference,
-            FeaturesService service) {
+                               FeaturesService service) {
         close();
     }
 
     @Override
     public void close() {
-        if(registration != null) {
+        if (registration != null) {
             registration.unregister();
             registration = null;
         }
