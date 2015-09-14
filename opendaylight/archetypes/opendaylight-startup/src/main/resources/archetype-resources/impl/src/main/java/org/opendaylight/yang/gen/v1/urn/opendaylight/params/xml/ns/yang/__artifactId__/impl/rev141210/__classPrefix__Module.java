6dc8cb5ab4bc7260953d3b7ba7ddfed20a8f2794
Flavio Fernandes <ffernand@redhat.com>
2015-09-14 17:53:17 -0400

    Split {copyright} into {copyright-year} and {copyright}
    With this change, archetype will explicitly use year for copyright to
    ensure check style does not fail.
    
    Patch set 3: replace (c) with © symbol
    Patch set 4: add default values for copyright-year and version
    
    Ref: https://git.opendaylight.org/gerrit/#/c/25860/
    Change-Id: I5b4bdfc273d578afafd7555a96e5a1fe732677bb
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- opendaylight/archetypes/opendaylight-startup/src/main/resources/archetype-resources/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/__artifactId__/impl/rev141210/__classPrefix__Module.java
+++ opendaylight/archetypes/opendaylight-startup/src/main/resources/archetype-resources/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/__artifactId__/impl/rev141210/__classPrefix__Module.java
@@ -2,7 +2,7 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /*
- * ${copyright} and others.  All rights reserved.
+ * Copyright © ${copyrightYear} ${copyright} and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
