fd8b276987ead2aa1a309d4561ec023f0b4222ee
Gary Wu <Gary.Wu1@huawei.com>
2015-10-02 13:12:43 -0700

    Bug 4037: Allow auto-downed node to rejoin cluster
    This patch will detect when a node has been
    auto-downed/quarantined by another node. When this
    happens, the ActorSystem of the datastore will be
    restarted to allow the node to rejoin the cluster.
    
    Change-Id: I0913bf455d426b6a0fccb17eac61b74f0911fa5d
    Signed-off-by: Gary Wu <Gary.Wu1@huawei.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/actor_system_provider/impl/ActorSystemProviderImpl.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/actor_system_provider/impl/ActorSystemProviderImpl.java
@@ -8,15 +8,21 @@
 package org.opendaylight.controller.config.yang.config.actor_system_provider.impl;
 
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.Effect;
 import akka.osgi.BundleDelegatingClassLoader;
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.util.concurrent.TimeUnit;
 import org.opendaylight.controller.cluster.ActorSystemProvider;
 import org.opendaylight.controller.cluster.ActorSystemProviderListener;
 import org.opendaylight.controller.cluster.common.actor.AkkaConfigurationReader;
 import org.opendaylight.controller.cluster.common.actor.FileAkkaConfigurationReader;
+import org.opendaylight.controller.cluster.common.actor.QuarantinedMonitorActor;
+import org.opendaylight.controller.cluster.datastore.TerminationMonitor;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.util.ListenerRegistry;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,24 +32,35 @@ public class ActorSystemProviderImpl implements ActorSystemProvider, AutoCloseab
     private static final String ACTOR_SYSTEM_NAME = "opendaylight-cluster-data";
     private static final String CONFIGURATION_NAME = "odl-cluster-data";
     static final Logger LOG = LoggerFactory.getLogger(ActorSystemProviderImpl.class);
-
-    private ActorSystem actorSystem;
-    private final BundleDelegatingClassLoader classLoader;
+    private final ActorSystem actorSystem;
     private final ListenerRegistry<ActorSystemProviderListener> listeners = new ListenerRegistry<>();
 
-    public ActorSystemProviderImpl(BundleContext bundleContext) {
+    public ActorSystemProviderImpl(final BundleContext bundleContext) {
         LOG.info("Creating new ActorSystem");
 
-        classLoader = new BundleDelegatingClassLoader(bundleContext.getBundle(),
-                Thread.currentThread().getContextClassLoader());
+        final Bundle bundle = bundleContext.getBundle();
 
-        createActorSystem();
-    }
+        final BundleDelegatingClassLoader classLoader = new BundleDelegatingClassLoader(bundle, Thread.currentThread()
+                .getContextClassLoader());
 
-    private void createActorSystem() {
-        AkkaConfigurationReader configurationReader = new FileAkkaConfigurationReader();
-        actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME,
-                ConfigFactory.load(configurationReader.read()).getConfig(CONFIGURATION_NAME), classLoader);
+        final AkkaConfigurationReader configurationReader = new FileAkkaConfigurationReader();
+        final Config akkaConfig = ConfigFactory.load(configurationReader.read()).getConfig(CONFIGURATION_NAME);
+
+        actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME, akkaConfig, classLoader);
+
+        actorSystem.actorOf(Props.create(TerminationMonitor.class), TerminationMonitor.ADDRESS);
+
+        actorSystem.actorOf(QuarantinedMonitorActor.props(new Effect() {
+
+            @Override
+            public void apply() throws Exception {
+                // restart the entire karaf container
+                LOG.warn("Restarting karaf container");
+                System.setProperty("karaf.restart", "true");
+                bundleContext.getBundle(0).stop();
+            }
+        }), QuarantinedMonitorActor.ADDRESS);
+
     }
 
     @Override
