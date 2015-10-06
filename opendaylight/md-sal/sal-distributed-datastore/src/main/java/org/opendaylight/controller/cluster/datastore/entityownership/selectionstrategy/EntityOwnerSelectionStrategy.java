81ef9fd6f226dfddb760a8eab4a157457b59c329
Moiz Raja <moraja@cisco.com>
2015-10-05 19:23:42 -0700

    Introduce EntityOwnerSelectionStrategy
    Currently the EntityOwnershipService does not do any load
    balancing, in that it allows the first candidate that registers
    to become an owner. There is a need to do that so that applications
    which choose to do some *work* based on if it owns an entity can
    scale better.
    
    This patch introduces the concept of an EntityOwnerSelectionStrategy
    with the intent to provide custom strategies later to choose an owner.
    
    Since custom strategies require intimate knowledge of how the
    EntityOwnershipShard chooses a leader at this time I do not think
    a strategy can be passed to the EntityOwnershipService via API. The
    intent therefor is to choose a strategy based on configuration
    wherein a custom strategy can be chosen for each entity type. If
    the Strategy needs any custom configuration then it can have configuration
    files of it's own
    
    Change-Id: Ia53b8edb59fb1d06a426d9d9a95c07ef4ae65cd1
    Signed-off-by: Moiz Raja <moraja@cisco.com>
    (cherry picked from commit 92cdd110a42539c3d5ede9434faf55a7e47f95e6)

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/selectionstrategy/EntityOwnerSelectionStrategy.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.datastore.entityownership.selectionstrategy;
+
+import java.util.Collection;
+
+/**
+ * An EntityOwnerSelectionStrategy is to be used by the EntityOwnershipShard to select a new owner from a collection
+ * of candidates
+ */
+public interface EntityOwnerSelectionStrategy {
+    /**
+     *
+     * @return the time in millis owner selection should be delayed
+     */
+    long selectionDelayInMillis();
+
+
+    /**
+     *
+     * @param viableCandidates the available candidates from which to choose the new owner
+     * @return the new owner
+     */
+    String newOwner(Collection<String> viableCandidates);
+}
