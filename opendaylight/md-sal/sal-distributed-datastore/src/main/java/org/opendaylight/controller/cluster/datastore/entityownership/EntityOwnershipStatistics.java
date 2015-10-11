32c331b3e4fe33b1e6d2574e0f95986ac77c1786
Moiz Raja <moraja@cisco.com>
2015-10-11 11:56:25 -0700

    Maintain EntityOwnershipStatistics
    Implementing a LoadBalancing entity owner selection
    strategy depends on our ability to find the load on
    specific candidates. The EntityOwnershipStatistics collects
    this information and provides query methods to access
    ownership counts for candidates.
    
    Change-Id: I7e812b15e8fb21e3be1aed10384600b9acb8bf20
    Signed-off-by: Moiz Raja <moraja@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/entityownership/EntityOwnershipStatistics.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.datastore.entityownership;
+
+import static org.opendaylight.controller.cluster.datastore.entityownership.EntityOwnersModel.ENTITY_OWNERS_PATH;
+import static org.opendaylight.controller.cluster.datastore.entityownership.EntityOwnersModel.ENTITY_QNAME;
+import static org.opendaylight.controller.cluster.datastore.entityownership.EntityOwnersModel.entityTypeFromEntityPath;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import com.romix.scala.collection.concurrent.TrieMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.datastore.ShardDataTree;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.clustering.entity.owners.rev150804.entity.owners.EntityType;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+
+/**
+ * EntityOwnershipStatistics is a utility class that keeps track of ownership statistics for the candidates and
+ * caches it for quick count queries.
+ * <p>
+ * While the entity ownership model does maintain the information about which entity is owned by which candidate
+ * finding out how many entities of a given type are owned by a given candidate is not an efficient query.
+ */
+class EntityOwnershipStatistics implements DOMDataTreeChangeListener {
+
+    private TrieMap<String, TrieMap<String, Long>> statistics = new TrieMap<>();
+
+    EntityOwnershipStatistics(){
+    }
+
+    void init(ShardDataTree shardDataTree) {
+        shardDataTree.registerTreeChangeListener(YangInstanceIdentifier.builder(ENTITY_OWNERS_PATH).
+                node(EntityType.QNAME).node(EntityType.QNAME).node(ENTITY_QNAME).node(ENTITY_QNAME).node(EntityOwnersModel.ENTITY_OWNER_QNAME).build(), this);
+    }
+
+    @Override
+    public void onDataTreeChanged(@Nonnull Collection<DataTreeCandidate> changes) {
+        for (DataTreeCandidate change : changes) {
+            DataTreeCandidateNode changeRoot = change.getRootNode();
+            LeafNode<?> ownerLeaf = (LeafNode<?>) changeRoot.getDataAfter().get();
+            String entityType = entityTypeFromEntityPath(change.getRootPath());
+            String newOwner = extractOwner(ownerLeaf);
+            if(!Strings.isNullOrEmpty(newOwner)) {
+                updateStatistics(entityType, newOwner, 1);
+            }
+
+            String origOwner = null;
+            Optional<NormalizedNode<?, ?>> dataBefore = changeRoot.getDataBefore();
+            if (dataBefore.isPresent()) {
+                origOwner = extractOwner((LeafNode<?>) changeRoot.getDataBefore().get());
+                if(!Strings.isNullOrEmpty(origOwner)) {
+                    updateStatistics(entityType, origOwner, -1);
+                }
+            }
+        }
+
+    }
+
+    Map<String, Map<String, Long>> all() {
+        Map<String, Map<String, Long>> snapshot = new HashMap<>();
+        for (String entityType : statistics.readOnlySnapshot().keySet()) {
+            snapshot.put(entityType, byEntityType(entityType));
+        }
+        return snapshot;
+    }
+
+    Map<String, Long> byEntityType(String entityType){
+        return statistics.get(entityType).readOnlySnapshot();
+    }
+
+    private String extractOwner(LeafNode<?> ownerLeaf) {
+        Object value = ownerLeaf.getValue();
+        return value != null ? value.toString() : null;
+    }
+
+    void updateStatistics(String entityType, String candidateName, long count){
+        Map<String, Long> m = statistics.get(entityType);
+        if(m == null){
+            m = new TrieMap<>();
+            m.put(candidateName, count);
+            statistics.put(entityType, m);
+        } else {
+            Long candidateOwnedEntities = m.get(candidateName);
+            if(candidateOwnedEntities == null){
+                m.put(candidateName, count);
+            } else {
+                m.put(candidateName, candidateOwnedEntities + count);
+            }
+        }
+    }
+}
