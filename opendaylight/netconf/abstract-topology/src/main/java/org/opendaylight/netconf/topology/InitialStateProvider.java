7e67776b0e81860e38a2b18a566b61a35383101a
Vaclav Demcak <vdemcak@cisco.com>
2015-10-19 18:30:29 +0200

    Abstract topology framework apis
    Note: it is a partial mirror commit of
          https://git.opendaylight.org/gerrit/#/c/28450/3
    
    Change-Id: Ib3d8b9fab7132a25313094fe31c9a77028bb4ea0
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/InitialStateProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+
+/**
+ * Provides initial and failed state for NodeManagers
+ */
+@Beta
+public interface InitialStateProvider {
+    @Nonnull
+    Node getInitialState(@Nonnull final NodeId nodeId, @Nonnull final Node configNode);
+
+    @Nonnull
+    Node getFailedState(@Nonnull final NodeId nodeId, @Nonnull final Node configNode);
+}
+
---
+++ opendaylight/netconf/abstract-topology/src/main/java/org/opendaylight/netconf/topology/NodeManagerCallback.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netconf.topology;
+
+import akka.actor.ActorSystem;
+import akka.actor.TypedActor.Receiver;
+import com.google.common.annotations.Beta;
+
+/**
+ * Customizable layer that handles communication with your application.
+ */
+@Beta
+public interface NodeManagerCallback extends InitialStateProvider, NodeListener, Receiver {
+
+    interface NodeManagerCallbackFactory<M> {
+        NodeManagerCallback create(String nodeId, String topologyId, ActorSystem actorSystem);
+    }
+}
+
