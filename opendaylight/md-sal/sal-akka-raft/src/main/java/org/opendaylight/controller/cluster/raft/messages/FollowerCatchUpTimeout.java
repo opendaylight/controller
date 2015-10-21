c0e5dceba5956d89d09d18fa37d47d7980ff6a13
Tom Pantelis <tpanteli@brocade.com>
2015-10-21 18:47:31 -0400

    Bug 2187: Code cleanup and refactoring
    I addressed remaining comments from a prior patch.
    
    I also refactored RaftActorServerConfigurationSupport to use an FSM
    similar to the SnapshotManager with some generic classes. This will
    make it easier to implement RemoveServer and reuse code.
    
    Change-Id: Id3cdcede3f9c393c878abd3e9a9d3a5e12c5fb8a
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/FollowerCatchUpTimeout.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/FollowerCatchUpTimeout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Dell Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2015 Dell Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
