f114fcf6ffcf4036f24a75af031214fec36b23a7
Jakub Morvay <jmorvay@cisco.com>
2015-11-02 09:00:20 +0100

    Remote schema repository implementation
    Change-Id: Ie2495b1561ff95508b9a31ffa0aad79ddb59a8c5
    Signed-off-by: Jakub Morvay <jmorvay@cisco.com>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/cluster/schema/repository/impl/RemoteSchemaProviderTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.schema.repository.impl;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import akka.dispatch.ExecutionContexts;
+import akka.dispatch.Futures;
+import com.google.common.io.ByteSource;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.controller.cluster.schema.repository.RemoteSchemaRepository;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceException;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+
+public class RemoteSchemaProviderTest {
+
+    private static final SourceIdentifier ID = new SourceIdentifier("Test", "2015-10-30");
+
+    private RemoteSchemaProvider remoteSchemaProvider;
+    private RemoteSchemaRepository mockedRemoteSchemaRepository;
+
+    @Before
+    public void setUp() {
+        mockedRemoteSchemaRepository = Mockito.mock(RemoteSchemaRepository.class);
+        ExecutionContexts.fromExecutorService(MoreExecutors.newDirectExecutorService());
+        remoteSchemaProvider = new RemoteSchemaProvider(mockedRemoteSchemaRepository,
+                ExecutionContexts.fromExecutorService(MoreExecutors.newDirectExecutorService()));
+    }
+
+    @Test
+    public void getExistingYangTextSchemaSource() throws IOException, SchemaSourceException {
+        String source = "Test";
+        YangTextSchemaSource schemaSource = YangTextSchemaSource.delegateForByteSource(ID, ByteSource.wrap(source.getBytes()));
+        YangTextSchemaSourceSerializationProxy sourceProxy = new YangTextSchemaSourceSerializationProxy(schemaSource);
+        Mockito.when(mockedRemoteSchemaRepository.getYangTextSchemaSource(ID)).thenReturn(Futures.successful(sourceProxy));
+
+        YangTextSchemaSource providedSource = remoteSchemaProvider.getSource(ID).checkedGet();
+        assertEquals(providedSource.getIdentifier(), ID);
+        assertArrayEquals(providedSource.read(), schemaSource.read());
+    }
+
+    @Test(expected = SchemaSourceException.class)
+    public void getNonExistingSchemaSource() throws Exception {
+        Futures.failed(new Exception("halo"));
+
+        Mockito.when(mockedRemoteSchemaRepository.getYangTextSchemaSource(ID)).thenReturn(
+                Futures.<YangTextSchemaSourceSerializationProxy>failed(new SchemaSourceException("Source not provided")));
+
+        CheckedFuture sourceFuture = remoteSchemaProvider.getSource(ID);
+        assertTrue(sourceFuture.isDone());
+        sourceFuture.checkedGet();
+    }
+}
