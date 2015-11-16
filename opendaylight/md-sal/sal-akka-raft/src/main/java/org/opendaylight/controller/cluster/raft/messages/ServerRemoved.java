250f3f77c80284536cc32e96739f713d21844103
Moiz Raja <moraja@cisco.com>
2015-11-16 15:56:46 -0800

    BUG 2817 - Basic implementation of RemoveServer in the Raft code
    When a RemoveServer is received it may ask for the removal of
    a the current leader or one of the followers. As a first pass
    we do not support removal of the current leader. To correctly
    implement removal of the leader we would have to implement
    leader transition which I intend to build in a future patch.
    
    When a follower is removed the server configuration is changed
    immediately on the leader and the new configuration persisted
    to the journal. When other followers receive the removed
    journal entry they would also remove the server from their
    configuration, this is the same as what was done for the
    AddServer implementation.
    
    As soon as then new configuration is persisted we respond with
    success to the caller. This is the same as for AddServer.
    
    When the ServerConfiguration is complete we send a ServerRemoved
    message to the follower which has been removed.
    
    Change-Id: I2b85d82cbeef13cca830e3cc212aebbbcd95c818
    Signed-off-by: Moiz Raja <moraja@cisco.com>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/ServerRemoved.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.raft.messages;
+
+import com.google.common.base.Preconditions;
+import java.io.Serializable;
+
+/**
+ * The ServerRemoved message is sent to a server which has been removed successfully from the ServerConfiguration.
+ * The Server can then choose to self destruct or notify it's parents as needed.
+ */
+public class ServerRemoved implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final String serverId;
+
+    public ServerRemoved(String serverId) {
+        this.serverId = Preconditions.checkNotNull(serverId);
+    }
+
+    public String getServerId() {
+        return serverId;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerRemoved{" +
+                "serverId='" + serverId + '\'' +
+                '}';
+    }
+}
