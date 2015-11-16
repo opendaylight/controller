523512871c9e54551633f3fa038f39414e631615
Tom Pantelis <tpanteli@brocade.com>
2015-11-16 05:49:05 -0500

    Make ModuleConfig immutable
    The ModuleConfig class has mutator methods for
    ModuleShardConfigProviders to initially construct instances. However
    once supplied to the CondifuratonImpl they are intended to be immutable
    yet the mutator methods expose loopholes around it. Therefore I added a
    Builder to ModuleConfig and made ModuleConfig truly immutable.
    
    Change-Id: I0b8070ff3db1563427a6a70ff174053b2a66feca
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/ModuleConfig.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/ModuleConfig.java
@@ -7,11 +7,13 @@
  */
 package org.opendaylight.controller.cluster.datastore.config;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.opendaylight.controller.cluster.datastore.shardstrategy.ShardStrategy;
 
 /**
@@ -21,61 +23,98 @@ import org.opendaylight.controller.cluster.datastore.shardstrategy.ShardStrategy
  */
 public class ModuleConfig {
     private final String name;
-    private String nameSpace;
-    private ShardStrategy shardStrategy;
-    private final Map<String, ShardConfig> shardConfigs = new HashMap<>();
+    private final String nameSpace;
+    private final ShardStrategy shardStrategy;
+    private final Map<String, ShardConfig> shardConfigs;
 
-    public ModuleConfig(final String name) {
+    private ModuleConfig(String name, String nameSpace, ShardStrategy shardStrategy,
+            Map<String, ShardConfig> shardConfigs) {
         this.name = name;
+        this.nameSpace = nameSpace;
+        this.shardStrategy = shardStrategy;
+        this.shardConfigs = shardConfigs;
     }
 
-    public ModuleConfig(ModuleConfig moduleConfig) {
-        this.name = moduleConfig.getName();
-        this.nameSpace = moduleConfig.getNameSpace();
-        this.shardStrategy = moduleConfig.getShardStrategy();
-        for (ShardConfig shardConfig : moduleConfig.getShardConfigs()) {
-            shardConfigs.put(shardConfig.getName(), new ShardConfig(shardConfig.getName(),
-                ImmutableSet.copyOf(shardConfig.getReplicas())));
-        }
-    }
-
+    @Nonnull
     public String getName() {
         return name;
     }
 
+    @Nullable
     public String getNameSpace() {
         return nameSpace;
     }
 
+    @Nullable
     public ShardStrategy getShardStrategy() {
         return shardStrategy;
     }
 
+    @Nullable
     public ShardConfig getShardConfig(String name) {
         return shardConfigs.get(name);
     }
 
+    @Nonnull
     public Collection<ShardConfig> getShardConfigs() {
         return shardConfigs.values();
     }
 
+    @Nonnull
     public Collection<String> getShardNames() {
         return shardConfigs.keySet();
     }
 
-    public void addShardConfig(String name, Set<String> replicas) {
-        shardConfigs.put(name, new ShardConfig(name, replicas));
+    public static Builder builder(String name) {
+        return new Builder(name);
     }
 
-    public void setNameSpace(String nameSpace) {
-        this.nameSpace = nameSpace;
+    public static Builder builder(ModuleConfig moduleConfig) {
+        return new Builder(moduleConfig);
     }
 
-    public void setShardStrategy(ShardStrategy shardStrategy) {
-        this.shardStrategy = shardStrategy;
-    }
+    public static class Builder {
+        private String name;
+        private String nameSpace;
+        private ShardStrategy shardStrategy;
+        private final Map<String, ShardConfig> shardConfigs = new HashMap<>();
 
-    public ShardConfig removeShardConfig(String name) {
-        return shardConfigs.remove(name);
+        private Builder(String name) {
+            this.name = name;
+        }
+
+        private Builder(ModuleConfig moduleConfig) {
+            this.name = moduleConfig.getName();
+            this.nameSpace = moduleConfig.getNameSpace();
+            this.shardStrategy = moduleConfig.getShardStrategy();
+            for (ShardConfig shardConfig : moduleConfig.getShardConfigs()) {
+                shardConfigs.put(shardConfig.getName(), shardConfig);
+            }
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder nameSpace(String nameSpace) {
+            this.nameSpace = nameSpace;
+            return this;
+        }
+
+        public Builder shardStrategy(ShardStrategy shardStrategy) {
+            this.shardStrategy = shardStrategy;
+            return this;
+        }
+
+        public Builder shardConfig(String name, Collection<String> replicas) {
+            shardConfigs.put(name, new ShardConfig(name, replicas));
+            return this;
+        }
+
+        public ModuleConfig build() {
+            return new ModuleConfig(Preconditions.checkNotNull(name), nameSpace, shardStrategy,
+                    ImmutableMap.copyOf(shardConfigs));
+        }
     }
 }
