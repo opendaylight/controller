748dd0b68d2595da84962790bcf6ef8c885f59bd
Ryan Goulding <ryandgoulding@gmail.com>
2015-11-24 15:41:39 -0500

    DO NOT MERGE: Bug 4577 Allow specification of a distinct schema cache directory per netconf device
    A leaf is added to odl-sal-netconf-connector-cfg.yang to allow specification of
    a schema cache directory relative to the "cache" directory.  The leaf defaults to
    "schema", so the default directory for loaded yang files is "cache/schema".  It
    is useful to specify a distinct cache directory per netconf mount to avoid potential
    conflict.
    
    For now, the custom directory requires restart of the controller.  The custom
    directory is created but not populated, which leads me to believe that the schema
    files are downloaded prior to setting the custom schemaCacheDirectory.  After
    restarting the controller, the files download to the correct directory.  Still
    working on a solution for this, but not sure how we are going to achieve this, so
    please hold off on merging this patch for now.
    
    Change-Id: Ie0c6b653d698c91a443d1b1831306da51bffbf8a
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/connector/netconf/NetconfConnectorModuleFactory.java
+++ opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/connector/netconf/NetconfConnectorModuleFactory.java
@@ -8,7 +8,6 @@
 package org.opendaylight.controller.config.yang.md.sal.connector.netconf;
 
 import java.io.File;
-
 import org.opendaylight.controller.config.api.DependencyResolver;
 import org.opendaylight.controller.config.api.DynamicMBeanWithInstance;
 import org.opendaylight.controller.config.spi.Module;
@@ -19,6 +18,8 @@ import org.opendaylight.yangtools.yang.model.repo.util.FilesystemSchemaSourceCac
 import org.opendaylight.yangtools.yang.parser.repo.SharedSchemaRepository;
 import org.opendaylight.yangtools.yang.parser.util.TextToASTTransformer;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
 *
@@ -26,15 +27,17 @@ import org.osgi.framework.BundleContext;
 public class NetconfConnectorModuleFactory extends
         org.opendaylight.controller.config.yang.md.sal.connector.netconf.AbstractNetconfConnectorModuleFactory {
 
-    // TODO this should be injected
-    // Netconf devices have separated schema registry + factory from controller
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfConnectorModuleFactory.class);
+    private static final String CACHE_DIRECTORY = "cache";
+    private static final String SCHEMA_DIRECTORY = "schema";
+    private static final String SCHEMA_CACHE_DIRECTORY = CACHE_DIRECTORY + File.separator + SCHEMA_DIRECTORY;
     private final SharedSchemaRepository repository = new SharedSchemaRepository(NAME);
     private final SchemaContextFactory schemaContextFactory
             = repository.createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
 
     public NetconfConnectorModuleFactory() {
         // Start cache and Text to AST transformer
-        final FilesystemSchemaSourceCache<YangTextSchemaSource> cache = new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File("cache/schema"));
+        final FilesystemSchemaSourceCache<YangTextSchemaSource> cache = new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File(SCHEMA_CACHE_DIRECTORY));
         repository.registerSchemaSourceListener(cache);
         repository.registerSchemaSourceListener(TextToASTTransformer.create(repository, repository));
     }
@@ -42,22 +45,56 @@ public class NetconfConnectorModuleFactory extends
     @Override
     public Module createModule(final String instanceName, final DependencyResolver dependencyResolver,
             final DynamicMBeanWithInstance old, final BundleContext bundleContext) throws Exception {
+
         final NetconfConnectorModule module = (NetconfConnectorModule) super.createModule(instanceName, dependencyResolver,
                 old, bundleContext);
-
         module.setBundleContext(bundleContext);
-        module.setSchemaRegistry(repository);
-        module.setSchemaContextFactory(schemaContextFactory);
+        updateSchemaCacheDirectory(instanceName, module);
+        repository.registerSchemaSourceListener(TextToASTTransformer.create(repository, repository));
         return module;
     }
 
     @Override
     public Module createModule(final String instanceName, final DependencyResolver dependencyResolver, final BundleContext bundleContext) {
+
         final NetconfConnectorModule module = (NetconfConnectorModule) super.createModule(instanceName, dependencyResolver,
                 bundleContext);
         module.setBundleContext(bundleContext);
+        updateSchemaCacheDirectory(instanceName, module);
+        repository.registerSchemaSourceListener(TextToASTTransformer.create(repository, repository));
+        return module;
+    }
+
+    /**
+     * If a custom <code>schemaCacheDirectory</code> is specified, this method sets up the custom location.
+     * Otherwise, the default <code>cache/schema</code> is assumed.
+     *
+     * @param instanceName The name specified for the netconf mount point
+     * @param module The module that is actively being created.
+     */
+    private void updateSchemaCacheDirectory(final String instanceName, final NetconfConnectorModule module) {
+
         module.setSchemaRegistry(repository);
         module.setSchemaContextFactory(schemaContextFactory);
-        return module;
+
+        final String schemaCacheDirectory = module.getSchemaCacheDirectory();
+        // Checks for a custom schema cache directory
+        if(schemaCacheDirectory != null && !schemaCacheDirectory.equals(SCHEMA_DIRECTORY)) {
+            final FilesystemSchemaSourceCache<YangTextSchemaSource> deviceCache = createDeviceFilesystemCache(schemaCacheDirectory);
+            repository.registerSchemaSourceListener(deviceCache);
+            LOG.info("Netconf connector for device {} will use schema cache directory {} instead of {}",
+                    instanceName, schemaCacheDirectory, SCHEMA_CACHE_DIRECTORY);
+        }
+    }
+
+    /**
+     * Creates a <code>FilesystemSchemaSourceCache</code> for the custom schema cache directory.
+     *
+     * @param schemaCacheDirectory The custom cache directory relative to "cache"
+     * @return A <code>FilesystemSchemaSourceCache</code> for the custom schema cache directory
+     */
+    private FilesystemSchemaSourceCache<YangTextSchemaSource> createDeviceFilesystemCache(final String schemaCacheDirectory) {
+        final String relativeSchemaCacheDirectory = CACHE_DIRECTORY + File.separator + schemaCacheDirectory;
+        return new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File(relativeSchemaCacheDirectory));
     }
 }
