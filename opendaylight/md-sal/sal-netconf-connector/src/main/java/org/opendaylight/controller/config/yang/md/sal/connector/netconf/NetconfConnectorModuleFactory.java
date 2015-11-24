3bd41e39e6e03a1d63f15635807be4d08c3679be
Ryan Goulding <ryandgoulding@gmail.com>
2015-11-24 15:41:39 -0500

    Bug 4577 Allow specification of a distinct schema cache directory per netconf device
    A leaf is added to odl-sal-netconf-connector-cfg.yang to allow specification of
    a schema cache directory.  The leaf defaults to "cache/schema", which is the
    directory that is used unless a new one is specified.
    
    Change-Id: Ie0c6b653d698c91a443d1b1831306da51bffbf8a
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/connector/netconf/NetconfConnectorModuleFactory.java
+++ opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/config/yang/md/sal/connector/netconf/NetconfConnectorModuleFactory.java
@@ -19,6 +19,8 @@ import org.opendaylight.yangtools.yang.model.repo.util.FilesystemSchemaSourceCac
 import org.opendaylight.yangtools.yang.parser.repo.SharedSchemaRepository;
 import org.opendaylight.yangtools.yang.parser.util.TextToASTTransformer;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
 *
@@ -26,15 +28,17 @@ import org.osgi.framework.BundleContext;
 public class NetconfConnectorModuleFactory extends
         org.opendaylight.controller.config.yang.md.sal.connector.netconf.AbstractNetconfConnectorModuleFactory {
 
-    // TODO this should be injected
-    // Netconf devices have separated schema registry + factory from controller
+    private static final Logger LOG = LoggerFactory.getLogger(NetconfConnectorModuleFactory.class);
+    private static final String CACHE_DIRECTORY = "cache";
+    private static final String SCHEMA_DIRECTORY = "schema";
+    private static final String SCHEMA_CACHE_DIRECTORY = CACHE_DIRECTORY + File.separator + SCHEMA_DIRECTORY;
     private final SharedSchemaRepository repository = new SharedSchemaRepository(NAME);
     private final SchemaContextFactory schemaContextFactory
             = repository.createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
 
     public NetconfConnectorModuleFactory() {
         // Start cache and Text to AST transformer
-        final FilesystemSchemaSourceCache<YangTextSchemaSource> cache = new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File("cache/schema"));
+        final FilesystemSchemaSourceCache<YangTextSchemaSource> cache = new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File(SCHEMA_CACHE_DIRECTORY));
         repository.registerSchemaSourceListener(cache);
         repository.registerSchemaSourceListener(TextToASTTransformer.create(repository, repository));
     }
@@ -56,8 +60,29 @@ public class NetconfConnectorModuleFactory extends
         final NetconfConnectorModule module = (NetconfConnectorModule) super.createModule(instanceName, dependencyResolver,
                 bundleContext);
         module.setBundleContext(bundleContext);
-        module.setSchemaRegistry(repository);
-        module.setSchemaContextFactory(schemaContextFactory);
+        final String schemaCacheDirectory = module.getSchemaCacheDirectory();
+        if(schemaCacheDirectory.equals(SCHEMA_DIRECTORY)) {
+            module.setSchemaRegistry(repository);
+            module.setSchemaContextFactory(schemaContextFactory);
+        } else {
+            final SchemaContextFactory deviceSchemaContextFactory =
+                    deviceRepository.createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
+            // Specifies a custom repository stored relative to the cache directory
+            final SharedSchemaRepository deviceRepository = createDeviceRepository(instanceName, schemaCacheDirectory);
+            module.setSchemaRegistry(deviceRepository);
+            module.setSchemaContextFactory(deviceSchemaContextFactory);
+            LOG.info("connector for device {} will use schema cache directory {} instead of {}",
+                    instanceName, schemaCacheDirectory, SCHEMA_CACHE_DIRECTORY);
+        }
         return module;
+    }
+
+    private SharedSchemaRepository createDeviceRepository(final String instanceName, final String schemaCacheDirectory) {
+        final SharedSchemaRepository deviceRepository = new SharedSchemaRepository(instanceName);
+        final String relativeSchemaCacheDirectory = CACHE_DIRECTORY + File.separator + schemaCacheDirectory;
+        final FilesystemSchemaSourceCache<YangTextSchemaSource> deviceCache = new FilesystemSchemaSourceCache<>(repository, YangTextSchemaSource.class, new File(relativeSchemaCacheDirectory));
+        deviceRepository.registerSchemaSourceListener(deviceCache);
+        deviceRepository.registerSchemaSourceListener(TextToASTTransformer.create(deviceRepository, deviceRepository));
+        return deviceRepository;
     }
 }
