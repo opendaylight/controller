c1c0bb2f3b954c30c03f480cd308d43847646174
Robert Varga <robert.varga@pantheon.sk>
2015-11-27 00:28:09 +0100

    BUG-4626: Add dictionary concept
    This patch adds the ability to perform lifecycle on dictionaries, such
    that they can be reused with NormalizedNodeStreamReader/Writer
    instances.
    
    Change-Id: If03c8a09a2e9168582c49087a423c112c578095e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/AbstractNormalizedNodeStream.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.node.utils.stream;
+
+import com.google.common.base.Preconditions;
+
+abstract class AbstractNormalizedNodeStream<T extends AbstractStreamDictionary> implements DictionaryAware<T> {
+    private T dictionary;
+
+    AbstractNormalizedNodeStream(final T dictionary) {
+        this.dictionary = Preconditions.checkNotNull(dictionary);
+    }
+
+    final T dictionary() {
+        Preconditions.checkState(dictionary != null, "Dictionary has been detached");
+        return dictionary;
+    }
+
+    @Override
+    public final T detachDictionary() {
+        Preconditions.checkState(dictionary != null, "Dictionary has already been detached");
+
+        final T ret = dictionary;
+        dictionary.detach(this);
+        dictionary = null;
+        return ret;
+    }
+}
