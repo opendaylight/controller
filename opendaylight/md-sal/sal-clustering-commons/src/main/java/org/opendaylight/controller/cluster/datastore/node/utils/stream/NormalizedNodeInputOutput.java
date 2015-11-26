4a5477ebdd6890dabf38c1db1e3b1fb8d3764c26
Robert Varga <robert.varga@pantheon.sk>
2015-11-27 00:28:09 +0100

    BUG-4626: Introduce new stream version
    This patch adds the ability to perform lifecycle on dictionaries, such
    that they can be reused with NormalizedNodeData{Input,Output) instances.
    
    It comes with a new stream version, which add explicit dictionary
    invalidation and dictionaries for QNames.
    
    Change-Id: If03c8a09a2e9168582c49087a423c112c578095e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputOutput.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputOutput.java
@@ -12,6 +12,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 @Beta
 public final class NormalizedNodeInputOutput {
@@ -20,6 +21,11 @@ public final class NormalizedNodeInputOutput {
     }
 
     public static NormalizedNodeDataInput newDataInput(@Nonnull final DataInput input) throws IOException {
+        return newDictionaryDataInput(input, null);
+    }
+
+    public static DictionaryNormalizedNodeDataInput newDictionaryDataInput(@Nonnull final DataInput input,
+            @Nullable final NormalizedNodeInputDictionary dictionary) throws IOException {
         final byte marker = input.readByte();
         if (marker != TokenTypes.SIGNATURE_MARKER) {
             throw new InvalidNormalizedNodeStreamException(String.format("Invalid signature marker: %d", marker));
@@ -27,6 +33,8 @@ public final class NormalizedNodeInputOutput {
 
         final short version = input.readShort();
         switch (version) {
+            case TokenTypes.BERYLLIUM_VERSION:
+                return new DictionaryNodeInputStreamReader(input, dictionary);
             case TokenTypes.LITHIUM_VERSION:
                 return new NormalizedNodeInputStreamReader(input, true);
             default:
@@ -36,5 +44,10 @@ public final class NormalizedNodeInputOutput {
 
     public static NormalizedNodeDataOutput newDataOutput(@Nonnull final DataOutput output) throws IOException {
         return new NormalizedNodeOutputStreamWriter(output);
+    }
+
+    public static DictionaryNormalizedNodeDataOutput newDictionaryDataOutput(@Nonnull final DataOutput output,
+            @Nullable final NormalizedNodeOutputDictionary dictionary) throws IOException {
+        return new DictionaryNodeOutputStreamWriter(output, dictionary);
     }
 }
