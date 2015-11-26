3ee741416f6f67699392f8d9de9142f828d8981e
Robert Varga <rovarga@cisco.com>
2015-11-26 12:46:59 +0100

    BUG-4626: Split out stream token types
    Move token types and version into a dedicated internal class, adding
    some documentation and introducing planned token types.
    
    Also adds validation of stream version on reception, so that parsing
    fails predictably in case of sender doing the worng thing and sending us
    an unrecognized version.
    
    Change-Id: I85fd9615b897e0fe4addb7f1807416d7bfe1b903
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputStreamReader.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputStreamReader.java
@@ -93,13 +93,16 @@ public class NormalizedNodeInputStreamReader implements NormalizedNodeStreamRead
         if(readSignatureMarker) {
             readSignatureMarker = false;
 
-            byte marker = input.readByte();
-            if(marker != NormalizedNodeOutputStreamWriter.SIGNATURE_MARKER) {
+            final byte marker = input.readByte();
+            if (marker != TokenTypes.SIGNATURE_MARKER) {
                 throw new InvalidNormalizedNodeStreamException(String.format(
                         "Invalid signature marker: %d", marker));
             }
 
-            input.readShort(); // read the version - not currently used/needed.
+            final short version = input.readShort();
+            if (version != TokenTypes.LITHIUM_VERSION) {
+                throw new InvalidNormalizedNodeStreamException(String.format("Unhandled stream version %s", version));
+            }
         }
     }
 
@@ -237,9 +240,9 @@ public class NormalizedNodeInputStreamReader implements NormalizedNodeStreamRead
 
     private String readCodedString() throws IOException {
         byte valueType = input.readByte();
-        if(valueType == NormalizedNodeOutputStreamWriter.IS_CODE_VALUE) {
+        if(valueType == TokenTypes.IS_CODE_VALUE) {
             return codedStringMap.get(input.readInt());
-        } else if(valueType == NormalizedNodeOutputStreamWriter.IS_STRING_VALUE) {
+        } else if(valueType == TokenTypes.IS_STRING_VALUE) {
             String value = input.readUTF().intern();
             codedStringMap.put(Integer.valueOf(codedStringMap.size()), value);
             return value;
