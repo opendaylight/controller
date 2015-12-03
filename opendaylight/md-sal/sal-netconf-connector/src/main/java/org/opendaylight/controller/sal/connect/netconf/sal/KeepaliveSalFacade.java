58f1adfd4909bc4a56b4721c06fab3f1e1fea0a8
adetalhouet <adetalhouet@inocybe.com>
2015-12-03 11:03:49 -0500

    Bug 4712 - Fix Keepalive task to check the response of a keepalive request
    If response is null, tear down the session.
    
    Change-Id: I8ca2302f2f5f0fad09a9ef7ab4511ffb863de343
    Signed-off-by: adetalhouet <adetalhouet@inocybe.com>

--- opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/sal/KeepaliveSalFacade.java
+++ opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/sal/KeepaliveSalFacade.java
@@ -185,8 +185,14 @@ public final class KeepaliveSalFacade implements RemoteDeviceHandler<NetconfSess
 
         @Override
         public void onSuccess(final DOMRpcResult result) {
-            LOG.debug("{}: Keepalive RPC successful with response: {}", id, result.getResult());
-            scheduleKeepalive();
+            if (result != null && result.getResult() != null) {
+                LOG.debug("{}: Keepalive RPC successful with response: {}", id, result.getResult());
+                scheduleKeepalive();
+            }
+            else {
+                LOG.warn("{} Keepalive RPC returned null with response: {}. Reconnecting netconf session", id, result);
+                reconnect();
+            }
         }
 
         @Override
