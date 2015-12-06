3ef6e49db6d3e9ab0c8ed10e9de037bb1bfb83c2
Robert Varga <robert.varga@pantheon.sk>
2015-12-06 23:07:36 +0100

    BUG-4626: Introduce NormalizedNodeData{Input,Output}
    Introduce the interfaces that are actually consumed, so that the
    external interface is accurately captured.
    
    Change-Id: I8ba93c279cfb4de063afb9c1078e97080e735e44
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeDataInput.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014, 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.node.utils.stream;
+
+import com.google.common.annotations.Beta;
+import java.io.DataInput;
+import java.io.IOException;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;;
+
+/**
+ * Interface for reading {@link NormalizedNode}s, {@link YangInstanceIdentifier}s and {@link PathArgument}s.
+ */
+@Beta
+public interface NormalizedNodeDataInput extends DataInput {
+    /**
+     * Read a normalized node from the reader.
+     *
+     * @return Next node from the stream, or null if end of stream has been reached.
+     * @throws IOException if an error occurs
+     * @throws IllegalStateException if the dictionary has been detached
+     */
+    NormalizedNode<?, ?> readNormalizedNode() throws IOException;
+
+    YangInstanceIdentifier readYangInstanceIdentifier() throws IOException;
+
+    PathArgument readPathArgument() throws IOException;
+}
