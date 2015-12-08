09419e51e2e0b771b1ae013bb4c8bdf7a4f984eb
Robert Varga <rovarga@cisco.com>
2015-12-08 14:02:50 +0100

    BUG-4626: create AbstractNormalizedNodeDataOutput
    Create an internal NormalizedNodeDataOutput implementation for reuse of
    code between stream versions.
    
    Change-Id: I60e68fe150ba49d37a6821b057b9ef93a8e600e5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputOutput.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeInputOutput.java
@@ -7,18 +7,31 @@
  */
 package org.opendaylight.controller.cluster.datastore.node.utils.stream;
 
+import com.google.common.annotations.Beta;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import javax.annotation.Nonnull;
 
+@Beta
 public final class NormalizedNodeInputOutput {
     private NormalizedNodeInputOutput() {
         throw new UnsupportedOperationException();
     }
 
     public static NormalizedNodeDataInput newDataInput(@Nonnull final DataInput input) throws IOException {
-        return new NormalizedNodeInputStreamReader(input);
+        final byte marker = input.readByte();
+        if (marker != TokenTypes.SIGNATURE_MARKER) {
+            throw new InvalidNormalizedNodeStreamException(String.format("Invalid signature marker: %d", marker));
+        }
+
+        final short version = input.readShort();
+        switch (version) {
+            case TokenTypes.LITHIUM_VERSION:
+                return new NormalizedNodeInputStreamReader(input, true);
+            default:
+                throw new InvalidNormalizedNodeStreamException(String.format("Unhandled stream version %s", version));
+        }
     }
 
     public static NormalizedNodeDataOutput newDataOutput(@Nonnull final DataOutput output) throws IOException {
