f05a90087c506f1c0a9a2a5371e21685a53a92b0
Tom Pantelis <tpanteli@brocade.com>
2015-12-16 12:37:42 -0500

    Add ShutDown message to RaftActor to transfer leader
    Added a ShutDown message to gracefully stop a RaftActor. If the leader,
    it attempts leadership transfer as follows:
    
    1) Send a LeaderStateChanged message with a null leader Id to the local
    RoleChangeNotifier. This will cause the ShardManager to clear it's
    cached leader state and primaryInfoCache.
    
    2) Send a LeaderTransitioning message to each follower so each can send
    LeaderStateChanged messages to their local RoleChangeNotifiers.
    
    3) Call a protected method, leadershipTransferStarting, passing the
    RaftActorLeadershipTransferCohort. This allows derived classes to
    perform work prior to transferring leadership. The Shard will wait for
    current transactions to complete.
    
    4) The proceedWithTransfer method on the
    RaftActorLeadershipTransferCohort is called which in turn calls
    transferLeadership on the Leader.
    
    5) On transferComplete, send a PoisonPill to self.
    
    Change-Id: I27fa8a95f260b75516b7e558caea4a1a3255dda3
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActorLeadershipTransferCohort.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActorLeadershipTransferCohort.java
@@ -7,43 +7,161 @@
  */
 package org.opendaylight.controller.cluster.raft;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.Cancellable;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.opendaylight.controller.cluster.raft.base.messages.LeaderTransitioning;
 import org.opendaylight.controller.cluster.raft.behaviors.Leader;
 import org.opendaylight.controller.cluster.raft.behaviors.RaftActorBehavior;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.duration.FiniteDuration;
 
 /**
- * A helper class that participates in raft actor leadership transfer. An instance is created upon
+ * A raft actor support class that participates in leadership transfer. An instance is created upon
  * initialization of leadership transfer.
  * <p>
- * NOTE: All methods on this class must be called on the actor's thread dispatcher as they modify internal state.
+ * The transfer process is as follows:
+ * <ol>
+ * <li>Send a LeaderStateChanged message with a null leader Id to the local RoleChangeNotifier to notify
+ *     clients that we no longer have a working leader.</li>
+ * <li>Send a LeaderTransitioning message to each follower so each can send LeaderStateChanged messages to
+ *     their local RoleChangeNotifiers.</li>
+ * <li>Call {@link RaftActor#leadershipTransferStarting} passing this RaftActorLeadershipTransferCohort
+ *     instance. This allows derived classes to perform work prior to transferring leadership.</li>
+ * <li>The {@link #proceedWithTransfer} method is called which in turn calls
+ *     {@link Leader#transferLeadership}.</li>
+ * <li>The Leader calls {@link #transferComplete} on successful completion.</li>
+ * <li>Wait a short period of time for the new leader to be elected to give the derived class a chance to
+ *     possibly complete work that was suspended while we were transferring.</li>
+ * <li>On notification of the new leader from the RaftActor or on time out, notify {@link OnComplete} callbacks.</li>
+ * </ol>
+ * <p>
+ * NOTE: All methods on this class must be called on the actor's thread dispatcher as they may access/modify
+ * internal state.
  *
  * @author Thomas Pantelis
  */
-public abstract class RaftActorLeadershipTransferCohort {
-    private final RaftActor raftActor;
+public class RaftActorLeadershipTransferCohort {
+    private static final Logger LOG = LoggerFactory.getLogger(RaftActorLeadershipTransferCohort.class);
 
-    protected RaftActorLeadershipTransferCohort(RaftActor raftActor) {
+    private final RaftActor raftActor;
+    private final ActorRef replyTo;
+    private Cancellable newLeaderTimer;
+    private final List<OnComplete> onCompleteCallbacks = new ArrayList<>();
+    private long newLeaderTimeoutInMillis = 2000;
+
+    RaftActorLeadershipTransferCohort(RaftActor raftActor, ActorRef replyTo) {
         this.raftActor = raftActor;
+        this.replyTo = replyTo;
+    }
+
+    void init() {
+        RaftActorContext context = raftActor.getRaftActorContext();
+        RaftActorBehavior currentBehavior = raftActor.getCurrentBehavior();
+        if(currentBehavior.state() != RaftState.Leader || !context.hasFollowers()) {
+            LOG.debug("{}: Not the leader or no followers - skipping transfer", raftActor.persistenceId());
+            finish(true);
+            return;
+        }
+
+        Optional<ActorRef> roleChangeNotifier = raftActor.getRoleChangeNotifier();
+        if(roleChangeNotifier.isPresent()) {
+            roleChangeNotifier.get().tell(raftActor.newLeaderStateChanged(context.getId(), null,
+                    currentBehavior.getLeaderPayloadVersion()), raftActor.self());
+        }
+
+        for(String peerId: context.getPeerIds()) {
+            ActorSelection followerActor = context.getPeerActorSelection(peerId);
+            if(followerActor != null) {
+                followerActor.tell(new LeaderTransitioning(), context.getActor());
+            }
+        }
+
+        raftActor.leadershipTransferStarting(this);
     }
 
     /**
-     * This method is invoked to start leadership transfer.
+     * This method is invoked to proceed with the leadership transfer.
      */
-    public void startTransfer() {
+    public void proceedWithTransfer() {
         RaftActorBehavior behavior = raftActor.getCurrentBehavior();
+        // Sanity check...
         if(behavior instanceof Leader) {
             ((Leader)behavior).transferLeadership(this);
+        } else {
+            LOG.debug("{}: No longer the leader - skipping transfer", raftActor.persistenceId());
+            finish(true);
         }
     }
 
     /**
-     * This method is invoked to abort leadership transfer.
+     * This method is invoked to abort leadership transfer on failure.
      */
     public void abortTransfer() {
-        transferComplete();
+        LOG.debug("{}: leader transfer aborted", raftActor.persistenceId());
+        finish(false);
     }
 
     /**
-     * This method is invoked when leadership transfer is complete.
+     * This method is invoked when leadership transfer was carried out and complete.
      */
-    public abstract void transferComplete();
+    public void transferComplete() {
+        LOG.debug("{}: leader transfer complete - waiting for new leader", raftActor.persistenceId());
+
+        // We'll give it a little time for the new leader to be elected to give the derived class a
+        // chance to possibly complete work that was suspended while we were transferring. The
+        // RequestVote message from the new leader candidate should cause us to step down as leader
+        // and convert to follower due to higher term. We should then get an AppendEntries heart
+        // beat with the new leader id.
+
+        // Add a timer in case we don't get a leader change - 2 sec should be plenty of time if a new
+        // leader is elected.
+        FiniteDuration timeout = FiniteDuration.create(newLeaderTimeoutInMillis, TimeUnit.MILLISECONDS);
+        newLeaderTimer = raftActor.getContext().system().scheduler().scheduleOnce(timeout, raftActor.self(),
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        LOG.debug("{}: leader not elected in time", raftActor.persistenceId());
+                        finish(true);
+                    }
+                }, raftActor.getContext().system().dispatcher(), raftActor.self());
+    }
+
+    void onNewLeader(String newLeader) {
+        if(newLeader != null && newLeaderTimer != null) {
+            LOG.debug("{}: leader changed to {}", raftActor.persistenceId(), newLeader);
+            newLeaderTimer.cancel();
+            finish(true);
+        }
+    }
+
+    private void finish(boolean success) {
+        for(OnComplete onComplete: onCompleteCallbacks) {
+            if(success) {
+                onComplete.onSuccess(raftActor.self(), replyTo);
+            } else {
+                onComplete.onFailure(raftActor.self(), replyTo);
+            }
+        }
+    }
+
+    void addOnComplete(OnComplete onComplete) {
+        onCompleteCallbacks.add(onComplete);
+    }
+
+    @VisibleForTesting
+    void setNewLeaderTimeoutInMillis(long newLeaderTimeoutInMillis) {
+        this.newLeaderTimeoutInMillis = newLeaderTimeoutInMillis;
+    }
+
+    interface OnComplete {
+        void onSuccess(ActorRef raftActorRef, ActorRef replyTo);
+        void onFailure(ActorRef raftActorRef, ActorRef replyTo);
+    }
 }
