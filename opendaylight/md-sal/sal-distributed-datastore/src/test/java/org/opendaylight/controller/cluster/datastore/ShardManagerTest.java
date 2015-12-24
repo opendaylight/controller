1896131af43c7308599352ee35cc9f9adb6e6e30
Tom Pantelis <tpanteli@brocade.com>
2015-12-24 06:01:43 -0500

    Bug 4823: Notify findPrimary callbacks on ReachableMember event
    Modified the ShardManager to notify waiting findPrimary callbacks when a
    ReachableMember event is received and leaderAvailable is set to true.
    This prevents waiting findPrimary queries from timng out with
    NoShardLeaderException.
    
    Change-Id: Iabcc6f2a66e5271342914ebfcaad23dd1e122787
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/ShardManagerTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/ShardManagerTest.java
@@ -794,6 +794,20 @@ public class ShardManagerTest extends AbstractActorTest {
 
             MessageCollectorActor.expectFirstMatching(mockShardActor1, PeerUp.class);
 
+            // Test wait for reachable member
+
+            shardManager1.underlyingActor().onReceiveCommand(MockClusterWrapper.
+                    createUnreachableMember("member-2", "akka.tcp://cluster-test@127.0.0.1:2558"));
+            shardManager1.underlyingActor().waitForUnreachableMember();
+
+            shardManager1.tell(new FindPrimary("default", true), getRef());
+
+            shardManager1.underlyingActor().onReceiveCommand(MockClusterWrapper.
+                    createReachableMember("member-2", "akka.tcp://cluster-test@127.0.0.1:2558"));
+
+            RemotePrimaryShardFound found2 = expectMsgClass(duration("5 seconds"), RemotePrimaryShardFound.class);
+            String path2 = found2.getPrimaryPath();
+            assertTrue("Unexpected primary path " + path2, path2.contains("member-2-shard-default-config"));
         }};
 
         LOG.info("testShardAvailabilityOnChangeOfMemberReachability ending");
