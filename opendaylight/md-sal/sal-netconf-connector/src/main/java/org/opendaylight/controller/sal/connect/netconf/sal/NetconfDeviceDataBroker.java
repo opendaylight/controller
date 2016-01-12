6f139f4b87c34caa2033efca58332a61a369760a
adetalhouet <adetalhouet@inocybe.com>
2016-01-12 10:39:08 -0500

    Bug 4940 - correctly implement default-request-timeout-millis
    Change-Id: I6922839d08459e3fd5225ff08213daf646608eaf
    Signed-off-by: adetalhouet <adetalhouet@inocybe.com>

--- opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/sal/NetconfDeviceDataBroker.java
+++ opendaylight/md-sal/sal-netconf-connector/src/main/java/org/opendaylight/controller/sal/connect/netconf/sal/NetconfDeviceDataBroker.java
@@ -54,7 +54,7 @@ final class NetconfDeviceDataBroker implements DOMDataBroker {
 
     @Override
     public DOMDataReadOnlyTransaction newReadOnlyTransaction() {
-        return new ReadOnlyTx(netconfOps, id);
+        return new ReadOnlyTx(netconfOps, id, requestTimeoutMillis);
     }
 
     @Override
