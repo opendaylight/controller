e38142676e3d40663fbf63380a97f9d3ce7133b9
Tom Pantelis <tpanteli@brocade.com>
2016-01-13 16:17:28 -0500

    Implement change to voting with no leader
    Change-Id: I67b5b2d3a97745dbe9a8215f9a28f3a840f2a0db
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/ChangeServersVotingStatus.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/ChangeServersVotingStatus.java
@@ -9,9 +9,12 @@ package org.opendaylight.controller.cluster.raft.messages;
 
 import com.google.common.base.Preconditions;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Message sent to change the raft voting status for servers.
@@ -22,17 +25,30 @@ public class ChangeServersVotingStatus implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final Map<String, Boolean> serverVotingStatusMap;
+    private final Collection<String> serversVisited;
 
     public ChangeServersVotingStatus(@Nonnull Map<String, Boolean> serverVotingStatusMap) {
+        this(serverVotingStatusMap, null);
+    }
+
+    public ChangeServersVotingStatus(@Nonnull Map<String, Boolean> serverVotingStatusMap,
+            @Nullable Collection<String> serversVisited) {
         this.serverVotingStatusMap = new HashMap<>(Preconditions.checkNotNull(serverVotingStatusMap));
+        this.serversVisited = serversVisited == null ? null : new HashSet<>(serversVisited);
     }
 
     public Map<String, Boolean> getServerVotingStatusMap() {
         return serverVotingStatusMap;
     }
 
+    @Nullable
+    public Collection<String> getServersVisited() {
+        return serversVisited;
+    }
+
     @Override
     public String toString() {
-        return "ChangeServersVotingStatus [serverVotingStatusMap=" + serverVotingStatusMap + "]";
+        return "ChangeServersVotingStatus [serverVotingStatusMap=" + serverVotingStatusMap + ", "
+                + (serversVisited != null ? "serversVisited=" + serversVisited : "") + "]";
     }
 }
