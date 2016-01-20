4dd0e8af81d20ddb7dd40afdfd23b739ffedc4e1
Robert Varga <rovarga@cisco.com>
2016-01-20 15:44:55 +0100

    Introduce AbstractModuleShardConfigProvider
    For integration testing we will need to expose resource-based
    configurations. which do not rely on the file being available in any
    shape or form.
    
    Change-Id: I4a871bafe59b2b7390d8c8df8f7a46e06212809d
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
@@ -9,12 +9,7 @@ package org.opendaylight.controller.cluster.datastore.config;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigObject;
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.opendaylight.controller.cluster.datastore.shardstrategy.ShardStrategyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,7 +18,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author Thomas Pantelis
  */
-public class FileModuleShardConfigProvider implements ModuleShardConfigProvider {
+public class FileModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
     private static final Logger LOG = LoggerFactory.getLogger(FileModuleShardConfigProvider.class);
 
     private final String moduleShardsConfigPath;
@@ -35,88 +30,26 @@ public class FileModuleShardConfigProvider implements ModuleShardConfigProvider 
     }
 
     @Override
-    public Map<String, ModuleConfig.Builder> retrieveModuleConfigs(Configuration configuration) {
+    protected Config loadModuleShardsConfig() {
         File moduleShardsFile = new File("./configuration/initial/" + moduleShardsConfigPath);
-        File modulesFile = new File("./configuration/initial/" + modulesConfigPath);
-
-        Config moduleShardsConfig = null;
-        if(moduleShardsFile.exists()) {
+        if (moduleShardsFile.exists()) {
             LOG.info("module shards config file exists - reading config from it");
-            moduleShardsConfig = ConfigFactory.parseFile(moduleShardsFile);
-        } else {
-            LOG.warn("module shards configuration read from resource");
-            moduleShardsConfig = ConfigFactory.load(moduleShardsConfigPath);
+            return ConfigFactory.parseFile(moduleShardsFile);
         }
 
-        Config modulesConfig = null;
-        if(modulesFile.exists()) {
+        LOG.warn("module shards configuration read from resource");
+        return ConfigFactory.load(moduleShardsConfigPath);
+    }
+
+    @Override
+    protected Config loadModulesConfig() {
+        File modulesFile = new File("./configuration/initial/" + modulesConfigPath);
+        if (modulesFile.exists()) {
             LOG.info("modules config file exists - reading config from it");
-            modulesConfig = ConfigFactory.parseFile(modulesFile);
-        } else {
-            LOG.warn("modules configuration read from resource");
-            modulesConfig = ConfigFactory.load(modulesConfigPath);
+            return ConfigFactory.parseFile(modulesFile);
         }
 
-        Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(moduleShardsConfig);
-        readModulesConfig(modulesConfig, moduleConfigMap, configuration);
-
-        return moduleConfigMap;
-    }
-
-    private static void readModulesConfig(final Config modulesConfig, Map<String, ModuleConfig.Builder> moduleConfigMap,
-            Configuration configuration) {
-        List<? extends ConfigObject> modulesConfigObjectList = modulesConfig.getObjectList("modules");
-
-        for(ConfigObject o : modulesConfigObjectList){
-            ConfigObjectWrapper w = new ConfigObjectWrapper(o);
-
-            String moduleName = w.stringValue("name");
-            ModuleConfig.Builder builder = moduleConfigMap.get(moduleName);
-            if(builder == null) {
-                builder = ModuleConfig.builder(moduleName);
-                moduleConfigMap.put(moduleName, builder);
-            }
-
-            builder.nameSpace(w.stringValue("namespace"));
-            builder.shardStrategy(ShardStrategyFactory.newShardStrategyInstance(moduleName,
-                    w.stringValue("shard-strategy"), configuration));
-        }
-    }
-
-    private static Map<String, ModuleConfig.Builder> readModuleShardsConfig(final Config moduleShardsConfig) {
-        List<? extends ConfigObject> moduleShardsConfigObjectList =
-            moduleShardsConfig.getObjectList("module-shards");
-
-        Map<String, ModuleConfig.Builder> moduleConfigMap = new HashMap<>();
-        for(ConfigObject moduleShardConfigObject : moduleShardsConfigObjectList){
-            String moduleName = moduleShardConfigObject.get("name").unwrapped().toString();
-            ModuleConfig.Builder builder = ModuleConfig.builder(moduleName);
-
-            List<? extends ConfigObject> shardsConfigObjectList =
-                moduleShardConfigObject.toConfig().getObjectList("shards");
-
-            for(ConfigObject shard : shardsConfigObjectList){
-                String shardName = shard.get("name").unwrapped().toString();
-                List<String> replicas = shard.toConfig().getStringList("replicas");
-                builder.shardConfig(shardName, replicas);
-            }
-
-            moduleConfigMap.put(moduleName, builder);
-        }
-
-        return moduleConfigMap;
-    }
-
-    private static class ConfigObjectWrapper{
-
-        private final ConfigObject configObject;
-
-        ConfigObjectWrapper(final ConfigObject configObject){
-            this.configObject = configObject;
-        }
-
-        public String stringValue(final String name){
-            return configObject.get(name).unwrapped().toString();
-        }
+        LOG.warn("modules configuration read from resource");
+        return ConfigFactory.load(modulesConfigPath);
     }
 }
