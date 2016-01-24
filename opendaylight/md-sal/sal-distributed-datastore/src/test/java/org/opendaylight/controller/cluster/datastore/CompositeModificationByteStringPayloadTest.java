025991dc47820115c53d46a611cf8bff09d8d6dd
Tom Pantelis <tpanteli@brocade.com>
2016-01-23 23:58:46 -0500

    Remove Helium protobuff code from AppendEntries
    We no longer need the pre-lithium/legacy protobuff serialization code in
    AppendEntries so remove along with some legacy test code that was using it.
    
    Change-Id: Ie68338e9ac96b3acc3b0aa208b393fe41ff6dfd6
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/CompositeModificationByteStringPayloadTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/CompositeModificationByteStringPayloadTest.java
@@ -8,27 +8,17 @@
 
 package org.opendaylight.controller.cluster.datastore;
 
-import java.util.ArrayList;
-import java.util.List;
+import static junit.framework.Assert.assertTrue;
 import org.apache.commons.lang.SerializationUtils;
 import org.junit.Test;
 import org.opendaylight.controller.cluster.datastore.modification.MutableCompositeModification;
 import org.opendaylight.controller.cluster.datastore.modification.WriteModification;
-import org.opendaylight.controller.cluster.raft.ReplicatedLogEntry;
-import org.opendaylight.controller.cluster.raft.ReplicatedLogImplEntry;
-import org.opendaylight.controller.cluster.raft.messages.AppendEntries;
 import org.opendaylight.controller.cluster.raft.protobuff.client.messages.CompositeModificationByteStringPayload;
 import org.opendaylight.controller.md.cluster.datastore.model.TestModel;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
 
 @Deprecated
 public class CompositeModificationByteStringPayloadTest {
-
-    private static final SchemaContext SCHEMA_CONTEXT = TestModel.createTestContext();
 
     @Test
     public void testSerialization(){
@@ -50,26 +40,5 @@ public class CompositeModificationByteStringPayloadTest {
 
         assertTrue(deserialize instanceof CompositeModificationByteStringPayload);
 
-    }
-
-    @Test
-    public void testAppendEntries(){
-        List<ReplicatedLogEntry> entries = new ArrayList<>();
-
-        WriteModification writeModification = new WriteModification(TestModel.OUTER_LIST_PATH,
-                ImmutableNodes.mapNodeBuilder(TestModel.OUTER_LIST_QNAME).build());
-
-        MutableCompositeModification compositeModification = new MutableCompositeModification();
-
-        compositeModification.addModification(writeModification);
-
-        CompositeModificationByteStringPayload payload =
-                new CompositeModificationByteStringPayload(compositeModification.toSerializable());
-
-        payload.clearModificationReference();
-
-        entries.add(new ReplicatedLogImplEntry(0, 1, payload));
-
-        assertNotNull(new AppendEntries(10, "foobar", 10, 10, entries, 10, -1, (short)0).toSerializable());
     }
 }
