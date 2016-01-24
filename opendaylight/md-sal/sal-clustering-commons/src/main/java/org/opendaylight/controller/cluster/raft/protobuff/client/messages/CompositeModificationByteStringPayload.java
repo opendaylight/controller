e24a23c446ed56c9438b97c534bd9aa289294ddb
Tom Pantelis <tpanteli@brocade.com>
2016-01-24 00:44:09 -0500

    Remove deprecated Helium Payload methods
    Removed the deprecated encode/decode methods from Payload and its derived
    classes.
    
    Change-Id: I52beb2d3991efe9f3a7c2631b8682dc1f324e271
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/raft/protobuff/client/messages/CompositeModificationByteStringPayload.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/raft/protobuff/client/messages/CompositeModificationByteStringPayload.java
@@ -11,15 +11,10 @@ package org.opendaylight.controller.cluster.raft.protobuff.client.messages;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.UnknownFieldSet;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.ref.SoftReference;
-import java.util.HashMap;
-import java.util.Map;
-import org.opendaylight.controller.protobuff.messages.cluster.raft.AppendEntriesMessages;
 import org.opendaylight.controller.protobuff.messages.persistent.PersistentMessages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,39 +38,6 @@ public class CompositeModificationByteStringPayload extends Payload implements
 
     private CompositeModificationByteStringPayload(ByteString byteString){
         this.byteString = Preconditions.checkNotNull(byteString, "byteString should not be null");
-    }
-
-
-    @Override
-    public Map<GeneratedMessage.GeneratedExtension<?, ?>, PersistentMessages.CompositeModification> encode() {
-        Preconditions.checkState(byteString!=null);
-        Map<GeneratedMessage.GeneratedExtension<?, ?>, PersistentMessages.CompositeModification> map = new HashMap<>();
-        map.put(org.opendaylight.controller.protobuff.messages.shard.CompositeModificationPayload.modification,
-                getModificationInternal());
-        return map;
-    }
-
-    @Override
-    public Payload decode(
-            AppendEntriesMessages.AppendEntries.ReplicatedLogEntry.Payload payload) {
-        PersistentMessages.CompositeModification modification = payload
-                .getExtension(
-                        org.opendaylight.controller.protobuff.messages.shard.CompositeModificationPayload.modification);
-
-        // The extension was put in the unknown field.
-        // This is because extensions need to be registered
-        // see org.opendaylight.controller.mdsal.CompositeModificationPayload.registerAllExtensions
-        // also see https://developers.google.com/protocol-buffers/docs/reference/java/com/google/protobuf/ExtensionRegistry
-        // If that is not done then on the other end the extension shows up as an unknown field
-        // Need to figure out a better way to do this
-        if(payload.getUnknownFields().hasField(2)){
-            UnknownFieldSet.Field field =
-                    payload.getUnknownFields().getField(2);
-
-            return new CompositeModificationByteStringPayload(field.getLengthDelimitedList().get(0));
-        }
-
-        return new CompositeModificationByteStringPayload(modification);
     }
 
     public Object getModification(){
