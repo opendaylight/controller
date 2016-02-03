02f2d6103c0da220e96279ba5fca7c942c97d8d5
Tom Pantelis <tpanteli@brocade.com>
2016-02-03 01:43:38 -0500

    Bug 5153: Add timestamp to TransactionIdentifier
    TransactionIdentifiers are created locally but sent to the remote leader
    so it's possible, after a restart, for the remote leader to see the same id
    for 2 different txns since the local counter starts at 1. To alleviate
    this I added a timestamp to TransactionIdentifier. I could've just used
    a UUID but the counter is useful for debugging and a full UUID would
    make the string version pretty long for logging. I think an additional
    millisec timestamp is sufficient.
    
    Change-Id: Iaabd3d25eb64dd14053f96336c48de90d4364678
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/identifiers/TransactionIdentifier.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/identifiers/TransactionIdentifier.java
@@ -13,6 +13,21 @@ import com.google.common.base.Preconditions;
 public class TransactionIdentifier {
     protected static final String TX_SEPARATOR = "-txn-";
 
+    private final String memberName;
+    private final long counter;
+    private final long timestamp;
+    private String stringRepresentation;
+
+    public TransactionIdentifier(String memberName, long counter) {
+        this.memberName = Preconditions.checkNotNull(memberName, "memberName should not be null");
+        this.counter = counter;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getChainId() {
+        return "";
+    }
+
     protected String getMemberName() {
         return memberName;
     }
@@ -21,17 +36,8 @@ public class TransactionIdentifier {
         return counter;
     }
 
-    private final String memberName;
-    private final long counter;
-    private String stringRepresentation;
-
-    public TransactionIdentifier(String memberName, long counter) {
-        this.memberName = Preconditions.checkNotNull(memberName, "memberName should not be null");
-        this.counter = counter;
-    }
-
-    public String getChainId() {
-        return "";
+    protected long getTimestamp() {
+        return timestamp;
     }
 
     public static TransactionIdentifier create(String memberName, long counter) {
@@ -52,6 +58,11 @@ public class TransactionIdentifier {
         if (counter != that.counter) {
             return false;
         }
+
+        if (timestamp != that.timestamp) {
+            return false;
+        }
+
         if (!memberName.equals(that.memberName)) {
             return false;
         }
@@ -63,14 +74,16 @@ public class TransactionIdentifier {
     public int hashCode() {
         int result = memberName.hashCode();
         result = 31 * result + (int) (counter ^ (counter >>> 32));
+        result = 31 * result + (int)(timestamp ^ (timestamp >>> 32));
         return result;
     }
+
 
     @Override
     public String toString() {
         if(stringRepresentation == null) {
-            stringRepresentation = new StringBuilder(memberName.length() + TX_SEPARATOR.length() + 10).
-                append(memberName).append(TX_SEPARATOR).append(counter).toString();
+            stringRepresentation = new StringBuilder(memberName.length() + TX_SEPARATOR.length() + 21).
+                append(memberName).append(TX_SEPARATOR).append(counter).append('-').append(timestamp).toString();
         }
 
         return stringRepresentation;
