59d14b39ed072815d24c75fcf766482ae03bb4da
Robert Varga <rovarga@cisco.com>
2016-02-14 01:31:46 +0100

    Add SendInstallSnapshot javadoc
    First cut at describing mechanics. Also make the class final and
    annonate nullness.
    
    Change-Id: I94119ab818713110dfab199b0e3f9e9608cfd583
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/base/messages/SendInstallSnapshot.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/base/messages/SendInstallSnapshot.java
@@ -8,16 +8,22 @@
 
 package org.opendaylight.controller.cluster.raft.base.messages;
 
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
 import org.opendaylight.controller.cluster.raft.Snapshot;
 
-public class SendInstallSnapshot {
+/**
+ * Internal message sent from the SnapshotManager to its associated leader. The leader is expected to apply the
+ * {@link Snapshot} to its state.
+ */
+public final class SendInstallSnapshot {
     private final Snapshot snapshot;
 
-    public SendInstallSnapshot(Snapshot snapshot) {
-        this.snapshot = snapshot;
+    public SendInstallSnapshot(@Nonnull Snapshot snapshot) {
+        this.snapshot = Preconditions.checkNotNull(snapshot);
     }
 
-    public Snapshot getSnapshot() {
+    @Nonnull public Snapshot getSnapshot() {
         return snapshot;
     }
 }
