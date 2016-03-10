fd0bcefcb48a084820cb58efb823745da076d6ff
Tom Pantelis <tpanteli@brocade.com>
2016-03-10 06:48:43 -0500

    Convert toaster-it to use mdsal-it
    I recently added an mdsal dependency to naother project and it caused
    the toaster-it test to fail. There was no useful output to troubleshoot,
    just that some bundles failed to resolve.
    
    It turns out the mdsal dependency and all its dependencies would need to be
    added to the bundle configuration for the test. This will be ongoing issue
    every time a new dependency is added that might affect the toaster test.
    
    To alleviate this I converted the test from naually installing bundles to use
    the mdsal-it functionality which installs features into the
    opendaylight-karaf-empty distro.
    
    I also changed AbstractConfigTestBase to override the pax logging to
    append to stdout, otherwise most output goes to karaf.log. This is helpful to
    troubleshoot failures.
    
    Change-Id: I1dd9150c6e617e491458560fe4b360d37548f264
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/samples/toaster-it/src/test/java/org/opendaylight/controller/sample/toaster/it/ToasterTest.java
+++ opendaylight/md-sal/samples/toaster-it/src/test/java/org/opendaylight/controller/sample/toaster/it/ToasterTest.java
@@ -7,84 +7,59 @@
  */
 package org.opendaylight.controller.sample.toaster.it;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import javax.inject.Inject;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
 import org.opendaylight.controller.sample.kitchen.api.EggsType;
 import org.opendaylight.controller.sample.kitchen.api.KitchenService;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.toaster.rev091120.HashBrown;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.toaster.rev091120.WhiteBread;
-import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.options.DefaultCompositeOption;
+import org.ops4j.pax.exam.options.RawUrlReference;
+import org.ops4j.pax.exam.options.UrlReference;
 import org.ops4j.pax.exam.util.Filter;
-import org.ops4j.pax.exam.util.PathUtils;
-
-import javax.inject.Inject;
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
-
-import java.lang.management.ManagementFactory;
-
-import static org.junit.Assert.assertEquals;
-import static org.opendaylight.controller.test.sal.binding.it.TestHelper.baseModelBundles;
-import static org.opendaylight.controller.test.sal.binding.it.TestHelper.bindingAwareSalBundles;
-import static org.opendaylight.controller.test.sal.binding.it.TestHelper.configMinumumBundles;
-import static org.opendaylight.controller.test.sal.binding.it.TestHelper.junitAndMockitoBundles;
-import static org.opendaylight.controller.test.sal.binding.it.TestHelper.mdSalCoreBundles;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemPackages;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 @RunWith(PaxExam.class)
-public class ToasterTest {
-
+public class ToasterTest extends AbstractMdsalTestBase {
     @Inject
     @Filter(timeout=60*1000)
     KitchenService kitchenService;
 
-    @Configuration
-    public Option[] config() {
-        return options(systemProperty("osgi.console").value("2401"), mavenBundle("org.slf4j", "slf4j-api")
-                .versionAsInProject(), //
-                          mavenBundle("org.slf4j", "log4j-over-slf4j").versionAsInProject(), //
-
-                                systemProperty("logback.configurationFile").value(
-                        "file:" + PathUtils.getBaseDir()
-                                + "/src/test/resources/logback.xml"),
-                mavenBundle("ch.qos.logback", "logback-core").versionAsInProject(), //
-                mavenBundle("ch.qos.logback", "logback-classic").versionAsInProject(), //
-                systemProperty("osgi.bundles.defaultStartLevel").value("4"),
-                systemPackages("sun.nio.ch"),
-
-                toasterBundles(),
-                mdSalCoreBundles(),
-
-                bindingAwareSalBundles(),
-                configMinumumBundles(),
-                // BASE Models
-                baseModelBundles(),
-
-                // Set fail if unresolved bundle present
-                systemProperty("pax.exam.osgi.unresolved.fail").value("true"),
-                junitAndMockitoBundles());
+    @Override
+    public String getModuleName() {
+        return "toaster-provider-impl";
     }
 
-    private Option toasterBundles() {
-        return new DefaultCompositeOption(
-                mavenBundle("org.opendaylight.controller.samples", "sample-toaster-provider").versionAsInProject(),
-                mavenBundle("org.opendaylight.controller.samples", "sample-toaster-consumer").versionAsInProject(),
-                mavenBundle("org.opendaylight.controller.samples", "sample-toaster").versionAsInProject(),
-                mavenBundle("openexi", "nagasena").versionAsInProject(),
-                mavenBundle("openexi", "nagasena-rta").versionAsInProject()
-        );
+    @Override
+    public String getInstanceName() {
+        return "toaster-provider-impl";
+    }
+
+    @Override
+    public UrlReference getFeatureRepo() {
+        // For some reason Jenkins builds fail to find the features-mdsal artifact via the maven URL so return
+        // the local file URL. The pom file copies the features-mdsal dependency to test-classes.
+        String version = MavenUtils.asInProject().getVersion("org.opendaylight.controller", "features-mdsal");
+        String featuresURL = new File(String.format(
+                "target/test-classes/features-mdsal-%s-features.xml", version)).toURI().toString();
+        return new RawUrlReference(featuresURL);
+    }
+
+    @Override
+    public String getFeatureName() {
+        return "odl-toaster";
     }
 
     @Test
     public void testToaster() throws Exception {
-
         MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
         ObjectName providerOn = new ObjectName("org.opendaylight.controller:instanceName=toaster-provider-impl,type=RuntimeBean,moduleFactoryName=toaster-provider-impl");
 
@@ -97,11 +72,10 @@ public class ToasterTest {
         success &= kitchenService.makeBreakfast( EggsType.SCRAMBLED, HashBrown.class, 4).get().isSuccessful();
         success &= kitchenService.makeBreakfast( EggsType.POACHED, WhiteBread.class, 8 ).get().isSuccessful();
 
-        Assert.assertTrue("Not all breakfasts succeeded", success);
+        assertTrue("Not all breakfasts succeeded", success);
 
         // Verify toasts made count on provider via JMX/config-subsystem
         toastsMade = (long) platformMBeanServer.getAttribute(providerOn, "ToastsMade");
         assertEquals(2, toastsMade);
     }
-
 }
