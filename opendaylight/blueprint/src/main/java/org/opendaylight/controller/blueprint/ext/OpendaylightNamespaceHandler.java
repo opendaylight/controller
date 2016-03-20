a9e38d1a23a999dd602e2886b1b53f7d04348036
Tom Pantelis <tpanteli@brocade.com>
2016-03-20 00:06:08 -0400

    Add decorating "type" attr extension for service refs
    Change-Id: Ie61bb45da1c7539732cd31ab0f8130233c9696fc
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/OpendaylightNamespaceHandler.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/OpendaylightNamespaceHandler.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.controller.blueprint.ext;
 
+import com.google.common.base.Strings;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Set;
@@ -15,6 +16,7 @@ import org.apache.aries.blueprint.ParserContext;
 import org.apache.aries.blueprint.mutable.MutableBeanMetadata;
 import org.apache.aries.blueprint.mutable.MutableRefMetadata;
 import org.apache.aries.blueprint.mutable.MutableReferenceMetadata;
+import org.apache.aries.blueprint.mutable.MutableServiceReferenceMetadata;
 import org.apache.aries.blueprint.mutable.MutableValueMetadata;
 import org.opendaylight.controller.blueprint.BlueprintContainerRestartService;
 import org.osgi.service.blueprint.container.ComponentDefinitionException;
@@ -23,9 +25,11 @@ import org.osgi.service.blueprint.reflect.ComponentMetadata;
 import org.osgi.service.blueprint.reflect.Metadata;
 import org.osgi.service.blueprint.reflect.RefMetadata;
 import org.osgi.service.blueprint.reflect.ReferenceMetadata;
+import org.osgi.service.blueprint.reflect.ServiceReferenceMetadata;
 import org.osgi.service.blueprint.reflect.ValueMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -38,6 +42,7 @@ public class OpendaylightNamespaceHandler implements NamespaceHandler {
     private static final Logger LOG = LoggerFactory.getLogger(OpendaylightNamespaceHandler.class);
     private static final String PROCESSOR = "processor";
     private static final String RESTART_DEPENDENTS = "restart-dependents-on-update";
+    private static final String TYPE_ATTR = "type";
 
     @SuppressWarnings("rawtypes")
     @Override
@@ -69,18 +74,41 @@ public class OpendaylightNamespaceHandler implements NamespaceHandler {
 
     @Override
     public ComponentMetadata decorate(Node node, ComponentMetadata component, ParserContext context) {
-        // No decorating elements
-//        if(node instanceof Element) {
-//            if (nodeNameEquals(node, RESTART_DEPENDENTS)) {
-//                decorateRestartDependents((Element)node, component);
-//            } else {
-//                throw new ComponentDefinitionException("Unsupported standalone element: " + node.getNodeName());
-//            }
-//        } else {
-//            throw new ComponentDefinitionException("Invalid node type " + node);
-//        }
+        if(node instanceof Attr) {
+            if (nodeNameEquals(node, TYPE_ATTR)) {
+                return decorateServiceReferenceType((Attr)node, component, context);
+            }
+        }
 
-        return null;
+        throw new ComponentDefinitionException("Unsupported decorate element: " + node.getNodeName());
+    }
+
+    private ComponentMetadata decorateServiceReferenceType(Attr attr, ComponentMetadata component, ParserContext context) {
+        if (!(component instanceof ServiceReferenceMetadata)) {
+            throw new ComponentDefinitionException("Attribute " + attr.getNodeName() +
+                    " can only be used on a <reference> or <reference-list> element");
+        }
+
+        if (!(component instanceof MutableServiceReferenceMetadata)) {
+            throw new ComponentDefinitionException("Expected an instanceof MutableServiceReferenceMetadata");
+        }
+
+        MutableServiceReferenceMetadata serviceRef = (MutableServiceReferenceMetadata)component;
+        String oldFilter = serviceRef.getExtendedFilter() == null ? null :
+            serviceRef.getExtendedFilter().getStringValue();
+
+        String filter;
+        if(Strings.isNullOrEmpty(oldFilter)) {
+            filter = String.format("(type=%s)", attr.getValue());
+        } else {
+            filter = String.format("(&(%s)(type=%s))", oldFilter, attr.getValue());
+        }
+
+        LOG.debug("decorateServiceReferenceType for {} with type {}, old filter: {}, new filter: {}",
+                serviceRef.getId(), attr.getValue(), oldFilter, filter);
+
+        serviceRef.setExtendedFilter(createValue(context, filter));
+        return component;
     }
 
     private static Metadata parseProcessor(Element element, ParserContext context) {
@@ -88,6 +116,7 @@ public class OpendaylightNamespaceHandler implements NamespaceHandler {
         metadata.setProcessor(true);
         metadata.setId(context.generateId());
         metadata.setScope(BeanMetadata.SCOPE_SINGLETON);
+        metadata.setActivation(ReferenceMetadata.ACTIVATION_EAGER);
         metadata.setRuntimeClass(ComponentProcessor.class);
         metadata.setDestroyMethod("destroy");
         metadata.addProperty("bundle", createRef(context, "blueprintBundle"));
@@ -100,6 +129,8 @@ public class OpendaylightNamespaceHandler implements NamespaceHandler {
         }
 
         metadata.addProperty("restartDependentsOnUpdate", createValue(context, restartDependentsOnUpdate));
+
+        LOG.debug("parseProcessor returning {}", metadata);
 
         return metadata;
     }
