4577867f34028426fa5d39b80fd08637f440b801
Tom Pantelis <tpanteli@brocade.com>
2016-03-20 00:06:08 -0400

    Add decorating "type" attr extension for service refs
    Change-Id: Ie61bb45da1c7539732cd31ab0f8130233c9696fc
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/ComponentProcessor.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/ComponentProcessor.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.controller.blueprint.ext;
 
+import com.google.common.base.Strings;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -15,6 +16,7 @@ import org.apache.aries.blueprint.ComponentDefinitionRegistry;
 import org.apache.aries.blueprint.ComponentDefinitionRegistryProcessor;
 import org.apache.aries.blueprint.ext.AbstractPropertyPlaceholder;
 import org.apache.aries.blueprint.mutable.MutableBeanMetadata;
+import org.apache.aries.blueprint.mutable.MutableServiceReferenceMetadata;
 import org.apache.aries.util.AriesFrameworkUtil;
 import org.opendaylight.controller.blueprint.BlueprintContainerRestartService;
 import org.osgi.framework.Bundle;
@@ -59,20 +61,38 @@ public class ComponentProcessor implements ComponentDefinitionRegistryProcessor 
 
     @Override
     public void process(ComponentDefinitionRegistry registry) {
-        LOG.debug("{}: In process", bundle.getSymbolicName());
+        LOG.debug("{}: In process", logName());
 
         for(String name : registry.getComponentDefinitionNames()) {
             ComponentMetadata component = registry.getComponentDefinition(name);
             if(component instanceof MutableBeanMetadata) {
-                processMutableBeanMetadata((MutableBeanMetadata)component);
+                processBeanMetadata((MutableBeanMetadata)component);
+            } else if(component instanceof MutableServiceReferenceMetadata) {
+                processServiceReferenceMetadata((MutableServiceReferenceMetadata)component);
             }
         }
     }
 
-    private void processMutableBeanMetadata(MutableBeanMetadata bean) {
+    private void processServiceReferenceMetadata(MutableServiceReferenceMetadata serviceRef) {
+        String filter = serviceRef.getFilter();
+        String extFilter = serviceRef.getExtendedFilter() == null ? null :
+            serviceRef.getExtendedFilter().getStringValue();
+
+        LOG.debug("{}: processServiceReferenceMetadata for {}, filter: {}, ext filter: {}", logName(),
+                serviceRef.getId(), filter, extFilter);
+
+        if(Strings.isNullOrEmpty(filter) && Strings.isNullOrEmpty(extFilter)) {
+            serviceRef.setFilter("(|(type=default)(!(type=*)))");
+
+            LOG.debug("{}: processServiceReferenceMetadata for {} set filter to {}", logName(),
+                    serviceRef.getId(), serviceRef.getFilter());
+        }
+    }
+
+    private void processBeanMetadata(MutableBeanMetadata bean) {
         if(restartDependentsOnUpdate && bean.getRuntimeClass() != null &&
                 AbstractPropertyPlaceholder.class.isAssignableFrom(bean.getRuntimeClass())) {
-            LOG.debug("{}: processMutableBeanMetadata: {}, runtime {}", bundle.getSymbolicName(), bean.getId(),
+            LOG.debug("{}: processMutableBeanMetadata: {}, runtime {}", logName(), bean.getId(),
                     bean.getRuntimeClass());
 
             for(BeanProperty prop: bean.getProperties()) {
@@ -80,13 +100,13 @@ public class ComponentProcessor implements ComponentDefinitionRegistryProcessor 
                     if(prop.getValue() instanceof ValueMetadata) {
                         ValueMetadata persistentId = (ValueMetadata)prop.getValue();
 
-                        LOG.debug("{}: Found {} property, value : {}", bundle.getSymbolicName(),
+                        LOG.debug("{}: Found {} property, value : {}", logName(),
                                 CM_PERSISTENT_ID_PROPERTY, persistentId.getStringValue());
 
                         registerManagedService(persistentId.getStringValue());
                     } else {
                         LOG.debug("{}: {} property metadata {} is not instanceof ValueMetadata",
-                                bundle.getSymbolicName(), CM_PERSISTENT_ID_PROPERTY, prop.getValue());
+                                logName(), CM_PERSISTENT_ID_PROPERTY, prop.getValue());
                     }
 
                     break;
@@ -102,7 +122,7 @@ public class ComponentProcessor implements ComponentDefinitionRegistryProcessor 
             @Override
             public void updated(Dictionary<String, ?> properties) {
                 LOG.debug("{}: ManagedService updated for persistentId {}, properties: {}, initialUpdate: {}",
-                        bundle.getSymbolicName(), persistentId, properties, initialUpdate);
+                        logName(), persistentId, properties, initialUpdate);
 
                 if(initialUpdate) {
                     initialUpdate = false;
@@ -118,5 +138,9 @@ public class ComponentProcessor implements ComponentDefinitionRegistryProcessor 
         props.put(Constants.BUNDLE_VERSION, bundle.getHeaders().get(Constants.BUNDLE_VERSION));
         managedServiceRegs.add(bundle.getBundleContext().registerService(ManagedService.class.getName(),
                 managedService, props));
+    }
+
+    private String logName() {
+        return bundle.getSymbolicName();
     }
 }
