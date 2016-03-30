9eb9769d53a282a42f04d2439b7a23bee477cfca
Robert Varga <rovarga@cisco.com>
2016-03-30 18:18:33 +0200

    WIP: BUG-5626: introduce basic shard behaviors
    This introduces the basic mechanics for behaviors.
    
    Change-Id: I9e239bc402aa23861d997a79f9c2f65360ba1d95
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ShardFollowerBehavior.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.Status;
+import org.opendaylight.controller.cluster.datastore.exceptions.NoShardLeaderException;
+import org.opendaylight.controller.cluster.raft.behaviors.RaftActorBehavior;
+
+final class ShardFollowerBehavior extends ShardBehavior {
+    ShardFollowerBehavior(final Shard shard, final RaftActorBehavior delegate) {
+        super(shard, delegate);
+    }
+
+    @Override
+    ShardCandidateBehavior becomeCandidate(RaftActorBehavior raftBehavior) {
+        return new ShardCandidateBehavior(getShard(), raftBehavior);
+    }
+
+    @Override
+    ShardFollowerBehavior becomeFollower(final RaftActorBehavior raftBehavior) {
+        return new ShardFollowerBehavior(getShard(), raftBehavior);
+    }
+
+    @Override
+    ShardLeaderBehavior becomeLeader(final RaftActorBehavior raftBehavior) {
+        return new ShardLeaderBehavior(getShard(), raftBehavior);
+    }
+
+    @Override
+    void handleCreateTransaction(final ActorRef sender, final Object message) {
+        final ActorSelection leader = getShard().getLeader();
+
+        if (leader != null) {
+            leader.forward(message, getShard().getContext());
+        } else {
+            sender.tell(new Status.Failure(
+                new NoShardLeaderException("Could not create a shard transaction", persistenceId())), getSelf());
+        }
+    }
+}
