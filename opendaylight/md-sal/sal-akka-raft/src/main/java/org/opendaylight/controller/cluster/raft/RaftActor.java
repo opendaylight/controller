6be484b267326a336e8de8c7e01f252e2eb6768a
Robert Varga <rovarga@cisco.com>
2016-03-30 19:54:45 +0200

    BUG-5626: do not use needless cast
    Casting to Follower is not needed, as we can access the leader ID from
    the RaftActorBehavior interface.
    
    Change-Id: Id1b0e20571eb12b14791e7e42abe846ebe3a6b9f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActor.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/RaftActor.java
@@ -610,8 +610,8 @@ public abstract class RaftActor extends AbstractUntypedPersistentActor {
             // but transfer the previous leaderId so it doesn't immediately try to schedule an election. This
             // avoids potential disruption. Otherwise, switch to Follower normally.
             RaftActorBehavior behavior = getCurrentBehavior();
-            if(behavior instanceof Follower) {
-                String previousLeaderId = ((Follower)behavior).getLeaderId();
+            if (behavior != null && behavior.state() == RaftState.Follower) {
+                String previousLeaderId = behavior.getLeaderId();
                 short previousLeaderPayloadVersion = behavior.getLeaderPayloadVersion();
 
                 LOG.debug("{}: Re-initializing to Follower with previous leaderId {}", persistenceId(), previousLeaderId);
