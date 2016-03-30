72997f74903bd8ca9b9a09c8cfec87bbb6eb9eba
Robert Varga <rovarga@cisco.com>
2016-03-30 10:53:08 +0200

    Simplifyt isolated leader check
    Sonar warns about nested ifs, merge them.
    
    Change-Id: I77fcee90b467bc141b79aa5f8fcccb7f8b4f2fe3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/Leader.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/Leader.java
@@ -60,16 +60,15 @@ public class Leader extends AbstractLeader {
         isolatedLeaderCheck = Stopwatch.createStarted();
     }
 
-    @Override public RaftActorBehavior handleMessage(ActorRef sender, Object originalMessage) {
+    @Override
+    public RaftActorBehavior handleMessage(ActorRef sender, Object originalMessage) {
         Preconditions.checkNotNull(sender, "sender should not be null");
 
-        if (ISOLATED_LEADER_CHECK.equals(originalMessage)) {
-            if (isLeaderIsolated()) {
-                LOG.warn("{}: At least {} followers need to be active, Switching {} from Leader to IsolatedLeader",
-                        context.getId(), getMinIsolatedLeaderPeerCount(), leaderId);
+        if (ISOLATED_LEADER_CHECK.equals(originalMessage) && isLeaderIsolated()) {
+            LOG.warn("{}: At least {} followers need to be active, Switching {} from Leader to IsolatedLeader",
+                context.getId(), getMinIsolatedLeaderPeerCount(), leaderId);
 
-                return internalSwitchBehavior(RaftState.IsolatedLeader);
-            }
+            return internalSwitchBehavior(RaftState.IsolatedLeader);
         }
 
         return super.handleMessage(sender, originalMessage);
