509831d23eba19960d24ad4ada551b58c4f0e18a
Robert Varga <rovarga@cisco.com>
2016-04-18 10:07:50 +0200

    WIP: BUG-5280: add cds-access-api commands
    This defines basic concepts and messages used by the frontend to access
    the backend functions. Covered messages include the following aspects:
    
    - create and delete a local history
    - create, modify and commit transactions (without 3PC for now)
    
    Change-Id: I2345ad5afa774ebc025f58b361bee8fba4a70329
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/ModifyTransactionRequest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import akka.actor.ActorRef;
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.Optional;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+
+@Beta
+public final class ModifyTransactionRequest extends TransactionRequest<ModifyTransactionRequest> {
+    private static final long serialVersionUID = 1L;
+    private final List<TransactionModification> modifications;
+    private final PersistenceProtocol protocol;
+
+    ModifyTransactionRequest(final TransactionIdentifier target, final long sequence, final ActorRef replyTo,
+        final List<TransactionModification> modifications, final PersistenceProtocol protocol) {
+        super(target, sequence, replyTo);
+        this.modifications = ImmutableList.copyOf(modifications);
+        this.protocol = protocol;
+    }
+
+    public Optional<PersistenceProtocol> getPersistenceProtocol() {
+        return Optional.ofNullable(protocol);
+    }
+
+    public List<TransactionModification> getModifications() {
+        return modifications;
+    }
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper toStringHelper) {
+        return super.addToStringAttributes(toStringHelper).add("operations", modifications).add("protocol", protocol);
+    }
+
+    @Override
+    protected ModifyTransactionRequestProxy externalizableProxy(final ABIVersion version) {
+        return new ModifyTransactionRequestProxy(this);
+    }
+
+    @Override
+    protected ModifyTransactionRequest cloneAsVersion(final ABIVersion version) {
+        return this;
+    }
+}
---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/PersistenceProtocol.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import com.google.common.annotations.Beta;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.access.concepts.WritableObject;
+
+/**
+ * Enumeration of transaction persistence protocols. These govern which protocol is executed between the frontend
+ * and backend to drive persistence of a particular transaction.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public enum PersistenceProtocol implements WritableObject {
+    /**
+     * Abort protocol. The transaction has been aborted on the frontend and its effects should not be visible
+     * in the global history. This is a simple request/reply protocol.
+     */
+    ABORT {
+        @Override
+        byte byteValue() {
+            return 1;
+        }
+    },
+    /**
+     * Simple commit protocol. The transaction should be committed to the global history. The receiving backend
+     * it the only entity which needs to persist its effects, hence a simple request/reply protocol is sufficient.
+     */
+    SIMPLE {
+        @Override
+        byte byteValue() {
+            return 2;
+        }
+    },
+    /**
+     * Three-phase commit protocol (3PC). The transaction should be committed to the global history, but it is a part
+     * of a transaction spanning multiple entities and coordination is needed to drive persistence.
+     */
+    THREE_PHASE {
+        @Override
+        byte byteValue() {
+            return 3;
+        }
+
+    };
+
+    @Override
+    public final void writeTo(final DataOutput out) throws IOException {
+        out.writeByte(byteValue());
+    }
+
+    abstract byte byteValue();
+
+    public static PersistenceProtocol readFrom(final DataInput in) throws IOException {
+        return valueOf(in.readByte());
+    }
+
+    static int byteValue(final PersistenceProtocol finish) {
+        return finish == null ? 0 : finish.byteValue();
+    }
+
+    static PersistenceProtocol valueOf(final byte b) {
+        switch (b) {
+            case 0:
+                return null;
+            case 1:
+                return ABORT;
+            case 2:
+                return SIMPLE;
+            case 3:
+                return THREE_PHASE;
+            default:
+                throw new IllegalArgumentException("Unhandled byte value " + b);
+        }
+    }
+}
