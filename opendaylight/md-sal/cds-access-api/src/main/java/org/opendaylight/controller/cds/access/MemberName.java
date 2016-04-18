a8991141b3444c58367b04b20cb961fcf16dc7ff
Robert Varga <rovarga@cisco.com>
2016-04-18 10:07:50 +0200

    BUG-5280: add FrontendIdentifier
    Change-Id: I2345ad5afa774ebc025f58b361bee8fba4a70329
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cds/access/MemberName.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cds.access;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+/**
+ * Type-safe encapsulation of a cluster member name.
+ */
+public final class MemberName {
+    private final String name;
+
+    private MemberName(final String name) {
+        this.name = Preconditions.checkNotNull(name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        return o instanceof MemberName && name.equals(((MemberName)o).name);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(MemberName.class).add("name", name).toString();
+    }
+}
