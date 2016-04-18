c7de511690c8486897f84921b70e89576b6131b8
Robert Varga <rovarga@cisco.com>
2016-04-18 19:31:39 +0200

    BUG-5280: add FrontendActor
    This adds the skeleton of the FrontendActor.
    
    Change-Id: Iabab854524608e822202f8d00949f08919b4e3f1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/FrontendActor.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors;
+
+import akka.actor.PoisonPill;
+import akka.persistence.RecoveryCompleted;
+import akka.persistence.SaveSnapshotFailure;
+import akka.persistence.SaveSnapshotSuccess;
+import akka.persistence.UntypedPersistentActor;
+
+public final class FrontendActor extends UntypedPersistentActor {
+    private static abstract class State {
+        abstract void onReceiveCommand(Object command);
+        abstract void onReceiveRecover(Object recover);
+    }
+
+    private final class Recovering extends State {
+        private long lastGeneration = 0;
+
+        @Override
+        void onReceiveCommand(final Object command) {
+            throw new IllegalStateException("Frontend is recovering");
+        }
+
+        @Override
+        void onReceiveRecover(final Object recover) {
+            if (recover instanceof FrontendIdentifier) {
+                lastGeneration = ((FrontendIdentifier) recover).getGeneration();
+            } else if (recover instanceof RecoveryCompleted) {
+                final long myGeneration = lastGeneration + 1;
+                // FIXME: switch to SAVING
+                saveSnapshot(new FrontendGenerationSnapshot(myGeneration));
+                state = new Saving(my)
+            }
+        }
+    }
+
+    private static abstract class AbstractRecoveredState extends State {
+        @Override
+        void onReceiveRecover(final Object recover) {
+            // TODO Auto-generated method stub
+
+        }
+    }
+
+    private final class Saving extends AbstractRecoveredState {
+        @Override
+        void onReceiveCommand(Object command) {
+            if (command instanceof SaveSnapshotSuccess) {
+                // FIXME
+            } else if (command instanceof SaveSnapshotFailure) {
+                getSelf().tell(PoisonPill.getInstance(), getSelf());
+            }
+        }
+    }
+
+    private final class Operational extends AbstractRecoveredState {
+
+        @Override
+        void onReceiveCommand(Object command) {
+            // TODO Auto-generated method stub
+
+        }
+
+
+    }
+
+    private static final String PERSISTENCE_ID = "frontend actor";
+    private final State currentState = new Recovering();
+
+    @Override
+    public String persistenceId() {
+        return PERSISTENCE_ID;
+    }
+
+    @Override
+    public void onReceiveCommand(final Object command) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onReceiveRecover(final Object recover) throws Exception {
+    }
+
+    @Override
+    public void saveSnapshot(Object snapshot) {
+        // TODO Auto-generated method stub
+        super.saveSnapshot(snapshot);
+    }
+
+
+}
