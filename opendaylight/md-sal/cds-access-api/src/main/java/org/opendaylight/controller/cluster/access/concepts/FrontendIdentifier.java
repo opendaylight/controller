e75e0680f895602093ec5b604dc51289c7bafe3d
Robert Varga <rovarga@cisco.com>
2016-04-18 10:07:50 +0200

    BUG-5280: add cds-access-api
    This defines basic concepts and messages used by the frontend to access
    the backend functions. Covered messages include the following aspects:
    
    - create and delete a local history
    - create, modify and commit transactions (without 3PC for now)
    
    Change-Id: I2345ad5afa774ebc025f58b361bee8fba4a70329
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/FrontendIdentifier.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * A cluster-wide unique identifier of a frontend instance.
+ */
+@Beta
+public final class FrontendIdentifier implements Comparable<FrontendIdentifier>, Identifier {
+    private static final class Proxy implements Externalizable {
+        private static final long serialVersionUID = 1L;
+        private MemberName memberName;
+        private long generation;
+
+        public Proxy() {
+            // Needed for Externalizable
+        }
+
+        Proxy(final MemberName memberName, final long generation) {
+            this.memberName = Preconditions.checkNotNull(memberName);
+            this.generation = generation;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(memberName.getName());
+            out.writeLong(generation);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            final String str = (String) in.readObject();
+            memberName = MemberName.forString(str);
+            generation = in.readLong();
+        }
+
+        @SuppressWarnings("unused")
+        private FrontendIdentifier readResolve() {
+            return new FrontendIdentifier(memberName, generation);
+        }
+    }
+
+    private static final long serialVersionUID = 1L;
+    private final MemberName memberName;
+    private final long generation;
+
+    FrontendIdentifier(final MemberName memberName, long generation) {
+        this.memberName = Preconditions.checkNotNull(memberName);
+        this.generation = generation;
+    }
+
+    public static FrontendIdentifier create(final MemberName memberName, final long generation) {
+        return new FrontendIdentifier(memberName, generation);
+    }
+
+    public MemberName getMemberName() {
+        return memberName;
+    }
+
+    public long getGeneration() {
+        return generation;
+    }
+
+    @Override
+    public int hashCode() {
+        return memberName.hashCode() * 31 + Long.hashCode(generation);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof FrontendIdentifier)) {
+            return false;
+        }
+
+        final FrontendIdentifier other = (FrontendIdentifier) o;
+        return generation == other.generation && memberName.equals(other.memberName);
+    }
+
+    @Override
+    public int compareTo(final FrontendIdentifier o) {
+        final int cmp = memberName.compareTo(o.memberName);
+        return cmp == 0 ? Long.compareUnsigned(generation, o.generation) : cmp;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(FrontendIdentifier.class).add("memberName", memberName)
+                .add("generation", Long.toUnsignedString(generation)).toString();
+    }
+
+    @SuppressWarnings("unused")
+    private Proxy writeReplace() {
+        return new Proxy(memberName, generation);
+    }
+}
