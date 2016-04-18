bf10fafbe0e425cc7d3fe8ff936a78f1b28e6928
Robert Varga <rovarga@cisco.com>
2016-04-18 10:07:50 +0200

    WIP: BUG-5280: add cds-access-api commands
    This defines basic concepts and messages used by the frontend to access
    the backend functions. Covered messages include the following aspects:
    
    - create and delete a local history
    - create, modify and commit transactions (without 3PC for now)
    
    Change-Id: I2345ad5afa774ebc025f58b361bee8fba4a70329
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/ModifyTransactionRequestBuilder.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import akka.actor.ActorRef;
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.concurrent.NotThreadSafe;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.yangtools.concepts.Builder;
+
+@Beta
+@NotThreadSafe
+public final class ModifyTransactionRequestBuilder implements Builder<ModifyTransactionRequest> {
+    private final List<TransactionModification> modifications = new ArrayList<>(1);
+    private final TransactionIdentifier identifier;
+    private final ActorRef replyTo;
+    private PersistenceProtocol protocol = null;
+    private long sequence;
+
+    public ModifyTransactionRequestBuilder(final TransactionIdentifier identifier, final ActorRef replyTo) {
+        this.identifier = Preconditions.checkNotNull(identifier);
+        this.replyTo = Preconditions.checkNotNull(replyTo);
+    }
+
+    private void checkFinished() {
+        Preconditions.checkState(protocol != null, "Batch has already been finished");
+    }
+
+    public void setSequence(final long sequence) {
+        checkFinished();
+        Preconditions.checkState(modifications.isEmpty(), "Sequence must be set first");
+        this.sequence = sequence;
+    }
+
+    public void addModification(final TransactionModification operation) {
+        checkFinished();
+        modifications.add(Preconditions.checkNotNull(operation));
+    }
+
+    public void setAbort() {
+        checkFinished();
+        // Transaction is being aborted, no need to transmit operations
+        modifications.clear();
+        protocol = PersistenceProtocol.ABORT;
+    }
+
+    public void setCommit(final boolean coordinated) {
+        checkFinished();
+        protocol = coordinated ? PersistenceProtocol.THREE_PHASE : PersistenceProtocol.SIMPLE;
+    }
+
+    public int size() {
+        return modifications.size();
+    }
+
+    @Override
+    public ModifyTransactionRequest build() {
+        final ModifyTransactionRequest ret = new ModifyTransactionRequest(identifier, sequence, replyTo, modifications,
+            protocol);
+        modifications.clear();
+        protocol = null;
+        sequence = 0;
+        return ret;
+    }
+}
