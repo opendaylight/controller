dd6db83bcd5aaf7ff197913ab993371c86917cbc
Robert Varga <rovarga@cisco.com>
2016-05-11 23:20:45 +0200

    BUG-5280: introduce DistributedDataStoreClientActor
    This patch introduces a common ClientActor, which keeps track of frontend
    generations. Also introduce bind for DistributedDataStore, which uses this
    common infrastructure.
    
    Interface between the DistributedDataStore and the actor world is captured
    as DistributedDataStoreClient.
    
    Change-Id: I42c3281ca790fb5615a593740424ac494469e6a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/InitialClientActorContext.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+
+/**
+ *
+ * @author Robert Varga
+ */
+final class InitialClientActorContext extends AbstractClientActorContext {
+    private final AbstractClientActor actor;
+
+    InitialClientActorContext(final AbstractClientActor actor, final String persistenceId) {
+        super(actor.self(), persistenceId);
+        this.actor = Preconditions.checkNotNull(actor);
+    }
+
+    void saveSnapshot(final ClientIdentifier snapshot) {
+        actor.saveSnapshot(snapshot);
+    }
+
+    ClientActorBehavior createBehavior(final ClientActorContext context) {
+        return actor.initialBehavior(context);
+    }
+
+    void stash() {
+        actor.stash();
+    }
+
+    void unstash() {
+        actor.unstashAll();
+    }
+}
