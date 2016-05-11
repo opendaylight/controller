b0e7dd3c15624dba00bf29df5bca35695be60f7d
Robert Varga <rovarga@cisco.com>
2016-05-11 23:20:45 +0200

    BUG-5280: introduce DistributedDataStoreClientActor
    This patch introduces a common ClientActor, which keeps track of frontend
    generations. Also introduce bind for DistributedDataStore, which uses this
    common infrastructure.
    
    Interface between the DistributedDataStore and the actor world is captured
    as DistributedDataStoreClient.
    
    Change-Id: I42c3281ca790fb5615a593740424ac494469e6a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/SavingClientActorBehavior.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import akka.persistence.SaveSnapshotFailure;
+import akka.persistence.SaveSnapshotSuccess;
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Robert Varga
+ */
+final class SavingClientActorBehavior extends RecoveredClientActorBehavior<InitialClientActorContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(SavingClientActorBehavior.class);
+    private final ClientIdentifier myId;
+
+    SavingClientActorBehavior(final InitialClientActorContext context, final ClientIdentifier nextId) {
+        super(context);
+        this.myId = Preconditions.checkNotNull(nextId);
+    }
+
+    @Override
+    AbstractClientActorBehavior<?> onReceiveCommand(final Object command) {
+        if (command instanceof SaveSnapshotFailure) {
+            LOG.error("{}: failed to persist state", persistenceId(), ((SaveSnapshotFailure) command).cause());
+            return null;
+        } else if (command instanceof SaveSnapshotSuccess) {
+            context().unstash();
+            return context().createBehavior(new ClientActorContext(self(), persistenceId(), myId));
+        } else {
+            LOG.debug("{}: stashing command {}", persistenceId(), command);
+            context().stash();
+            return this;
+        }
+    }
+}
