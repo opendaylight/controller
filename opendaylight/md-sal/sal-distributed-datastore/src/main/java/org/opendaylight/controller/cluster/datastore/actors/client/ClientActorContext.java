b14f0c8e258a511d8948d8b46448f84ef19cf418
Robert Varga <rovarga@cisco.com>
2016-05-11 23:20:45 +0200

    BUG-5280: introduce DistributedDataStoreClientActor
    This patch introduces a common ClientActor, which keeps track of frontend
    generations. Also introduce bind for DistributedDataStore, which uses this
    common infrastructure.
    
    Interface between the DistributedDataStore and the actor world is captured
    as DistributedDataStoreClient.
    
    Change-Id: I42c3281ca790fb5615a593740424ac494469e6a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/ClientActorContext.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import akka.actor.ActorRef;
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendType;
+import org.opendaylight.yangtools.concepts.Mutable;
+
+/**
+ * An actor context associated with this {@link AbstractClientActor}
+ * @author user
+ *
+ * @param <T> Front type
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class ClientActorContext<T extends FrontendType> extends AbstractClientActorContext implements Mutable {
+    private final ClientIdentifier<T> identifier;
+
+    ClientActorContext(final ActorRef self, final String persistenceId, final ClientIdentifier<T> identifier) {
+        super(self, persistenceId);
+        this.identifier = Preconditions.checkNotNull(identifier);
+    }
+
+    ClientIdentifier<T> getIdentifier() {
+        return identifier;
+    }
+}
