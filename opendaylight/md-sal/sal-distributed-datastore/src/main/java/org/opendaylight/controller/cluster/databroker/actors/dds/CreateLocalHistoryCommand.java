394b2ef293af302bd4929b7ab0e45e866b03acd0
Robert Varga <rovarga@cisco.com>
2016-05-11 23:20:45 +0200

    BUG-5280: introduce DistributedDataStoreClientActor
    This patch introduces a common ClientActor, which keeps track of frontend
    generations. Also introduce bind for DistributedDataStore, which uses this
    common infrastructure.
    
    Interface between the DistributedDataStore and the actor world is captured
    as DistributedDataStoreClient.
    
    Change-Id: I42c3281ca790fb5615a593740424ac494469e6a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/CreateLocalHistoryCommand.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Command sent from the Java world to the client actor to create a new local history.
+ *
+ * @author Robert Varga
+ */
+final class CreateLocalHistoryCommand {
+    private final CompletableFuture<ClientLocalHistory> future = new CompletableFuture<>();
+
+    CompletableFuture<ClientLocalHistory> future() {
+        return future;
+    }
+}
