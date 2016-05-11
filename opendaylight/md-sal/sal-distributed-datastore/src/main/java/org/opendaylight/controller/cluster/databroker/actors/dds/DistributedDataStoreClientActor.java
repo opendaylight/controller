63c60ec067c6c3c97faa9f9f0ae1e379378c46c4
Robert Varga <rovarga@cisco.com>
2016-05-11 23:20:45 +0200

    BUG-5280: introduce DistributedDataStoreClientActor
    This patch introduces a common ClientActor, which keeps track of frontend
    generations. Also introduce bind for DistributedDataStore, which uses this
    common infrastructure.
    
    Interface between the DistributedDataStore and the actor world is captured
    as DistributedDataStoreClient.
    
    Change-Id: I42c3281ca790fb5615a593740424ac494469e6a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/DistributedDataStoreClientActor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.pattern.ExplicitAskSupport;
+import akka.util.Timeout;
+import com.google.common.base.Throwables;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendType;
+import org.opendaylight.controller.cluster.access.concepts.MemberName;
+import org.opendaylight.controller.cluster.datastore.actors.client.AbstractClientActor;
+import org.opendaylight.controller.cluster.datastore.actors.client.ClientActorBehavior;
+import org.opendaylight.controller.cluster.datastore.actors.client.ClientActorContext;
+import scala.Function1;
+import scala.concurrent.Await;
+import scala.concurrent.duration.Duration;
+import scala.runtime.AbstractFunction1;
+
+/**
+ * A {@link AbstractClientActor} which acts as the point of contact for DistributedDataStore.
+ *
+ * @author Robert Varga
+ */
+public final class DistributedDataStoreClientActor extends AbstractClientActor {
+    // Unfortunately Akka's explicit ask pattern does not work with its Java API, as it fails to invoke passed message.
+    // In order to make this work for now, we tap directly into ExplicitAskSupport and use a Scala function instead
+    // of akka.japi.Function.
+    private static final ExplicitAskSupport ASK_SUPPORT = akka.pattern.extended.package$.MODULE$;
+    private static final Function1<ActorRef, Object> GET_CLIENT_FACTORY = new AbstractFunction1<ActorRef, Object>() {
+        @Override
+        public Object apply(final ActorRef askSender) {
+            return new GetClientRequest(askSender);
+        }
+    };
+
+    private DistributedDataStoreClientActor(final FrontendIdentifier frontendId) {
+        super(frontendId);
+    }
+
+    @Override
+    protected ClientActorBehavior initialBehavior(final ClientActorContext context) {
+        return new DistributedDataStoreClientBehavior(context);
+    }
+
+    public static Props props(final @Nonnull MemberName memberName, @Nonnull final String storeName) {
+        final String name = "DistributedDataStore:storeName='" + storeName + "'";
+        final FrontendIdentifier frontendId = FrontendIdentifier.create(memberName, FrontendType.forName(name));
+        return Props.create(DistributedDataStoreClientActor.class, () -> new DistributedDataStoreClientActor(frontendId));
+    }
+
+    public static DistributedDataStoreClient getDistributedDataStoreClient(final @Nonnull ActorRef actor,
+            final long timeout, final TimeUnit unit) {
+        try {
+            return (DistributedDataStoreClient) Await.result(ASK_SUPPORT.ask(actor, GET_CLIENT_FACTORY,
+                Timeout.apply(timeout, unit)), Duration.Inf());
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/RecoveringClientActorBehavior.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import akka.persistence.RecoveryCompleted;
+import akka.persistence.SnapshotOffer;
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @param <T> Frontend type
+ *
+ * @author Robert Varga
+ */
+final class RecoveringClientActorBehavior extends AbstractClientActorBehavior<InitialClientActorContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(RecoveringClientActorBehavior.class);
+    private final FrontendIdentifier currentFrontend;
+    private ClientIdentifier lastId = null;
+
+    RecoveringClientActorBehavior(final InitialClientActorContext context, final FrontendIdentifier frontendId) {
+        super(context);
+        currentFrontend = Preconditions.checkNotNull(frontendId);
+    }
+
+    @Override
+    AbstractClientActorBehavior<?> onReceiveCommand(final Object command) {
+        throw new IllegalStateException("Frontend is recovering");
+    }
+
+    @Override
+    AbstractClientActorBehavior<?> onReceiveRecover(final Object recover) {
+        if (recover instanceof RecoveryCompleted) {
+            final ClientIdentifier nextId;
+            if (lastId != null) {
+                if (!currentFrontend.equals(lastId.getFrontendId())) {
+                    LOG.error("Mismatched frontend identifier, shutting down. Current: {} Saved: {}", currentFrontend,
+                    lastId.getFrontendId());
+                    return null;
+                }
+
+                nextId = ClientIdentifier.create(currentFrontend, lastId.getGeneration() + 1);
+            } else {
+                nextId = ClientIdentifier.create(currentFrontend, 0);
+            }
+
+            LOG.debug("{}: persisting new identifier {}", persistenceId(), nextId);
+            context().saveSnapshot(nextId);
+            return new SavingClientActorBehavior(context(), nextId);
+        } else if (recover instanceof SnapshotOffer) {
+            lastId = (ClientIdentifier) ((SnapshotOffer)recover).snapshot();
+            LOG.debug("{}: recovered identifier {}", lastId);
+        } else {
+            LOG.warn("{}: ignoring recovery message {}", recover);
+        }
+
+        return this;
+    }
+}
