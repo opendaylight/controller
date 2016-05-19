3550931a34952903fc47ae986e96b85bf2e4229f
Robert Varga <rovarga@cisco.com>
2016-05-19 17:31:22 +0200

    BUG-5280: remove support for talking to pre-Boron clients
    Both transaction chains and transaction identifiers are being changed to
    structured data. Unfortunately there is no way to reliably map string
    identifiers from versions prior to Boron in a consistent manner.
    
    Pre-Boron messages do not have a concept of a frontend identifier nor its
    generation. Implementations are assigning them in a non-deterministic
    order and rely on timestamps to disambiguate between generations.
    
    Since Shard-internal state keeping and replication logic will require
    these concepts, this patch marks a clean break in compatibility. This
    compatibility is only needed in mixed-version clusters, which have not
    been validated to work and even if the data store works, it cannot
    cope with model mismatches expected from differing versions.
    
    Change-Id: I168fa0a98be10aebd680ce3323c458aad7f15865
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/messages/CanCommitTransactionTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/messages/CanCommitTransactionTest.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 import org.apache.commons.lang.SerializationUtils;
 import org.junit.Test;
 import org.opendaylight.controller.cluster.datastore.DataStoreVersions;
-import org.opendaylight.controller.protobuff.messages.cohort3pc.ThreePhaseCommitCohortMessages;
 
 /**
  * Unit tests for CanCommitTransaction.
@@ -35,24 +34,7 @@ public class CanCommitTransactionTest {
     }
 
     @Test
-    public void testSerializationWithPreBoronVersion() {
-        CanCommitTransaction expected = new CanCommitTransaction("txId", DataStoreVersions.LITHIUM_VERSION);
-
-        Object serialized = expected.toSerializable();
-        assertEquals("Serialized type", ThreePhaseCommitCohortMessages.CanCommitTransaction.class, serialized.getClass());
-
-        CanCommitTransaction actual = CanCommitTransaction.fromSerializable(
-                SerializationUtils.clone((Serializable) serialized));
-        assertEquals("getTransactionID", expected.getTransactionID(), actual.getTransactionID());
-        assertEquals("getVersion", DataStoreVersions.LITHIUM_VERSION, actual.getVersion());
-    }
-
-    @Test
     public void testIsSerializedType() {
-        assertEquals("isSerializedType", true, CanCommitTransaction.isSerializedType(
-                ThreePhaseCommitCohortMessages.CanCommitTransaction.newBuilder().
-                setTransactionId("").build()));
-
         assertEquals("isSerializedType", true, CanCommitTransaction.isSerializedType(new CanCommitTransaction()));
         assertEquals("isSerializedType", false, CanCommitTransaction.isSerializedType(new Object()));
     }
