e8de970fc35fdf016e825afedb5e3b14c65eb606
Robert Varga <rovarga@cisco.com>
2016-05-19 18:38:28 +0200

    BUG-5280: Remove PeristentMessages
    This class is not used by any live code, remove it along with
    the previously-deprecated codepaths.
    
    Change-Id: Iaffc428a15c3cb2822d1465411e84e1d12994e2e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/modification/MergeModification.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/modification/MergeModification.java
@@ -11,9 +11,6 @@ package org.opendaylight.controller.cluster.datastore.modification;
 import java.io.IOException;
 import java.io.ObjectInput;
 import org.opendaylight.controller.cluster.datastore.DataStoreVersions;
-import org.opendaylight.controller.cluster.datastore.node.NormalizedNodeToNodeCodec;
-import org.opendaylight.controller.cluster.datastore.node.NormalizedNodeToNodeCodec.Decoded;
-import org.opendaylight.controller.protobuff.messages.persistent.PersistentMessages;
 import org.opendaylight.controller.sal.core.spi.data.DOMStoreWriteTransaction;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
@@ -50,13 +47,6 @@ public class MergeModification extends WriteModification {
     @Override
     public byte getType() {
         return MERGE;
-    }
-
-    @Deprecated
-    public static MergeModification fromSerializable(final Object serializable) {
-        PersistentMessages.Modification o = (PersistentMessages.Modification) serializable;
-        Decoded decoded = new NormalizedNodeToNodeCodec(null).decode(o.getPath(), o.getData());
-        return new MergeModification(decoded.getDecodedPath(), decoded.getDecodedNode());
     }
 
     public static MergeModification fromStream(ObjectInput in, short version)
