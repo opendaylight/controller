4558225289f2091f52b20ab8f0a29ab8f920e8ff
Robert Varga <rovarga@cisco.com>
2016-05-22 13:35:40 +0200

    BUG-5930: introduce ABIVersion
    This patch introduces the concept of ABI version in the client API.
    The concept will serve to guide frontend/backend interactions so that
    version negotiation cal ne implemented and messages are sent using
    a compatible serialization format.
    
    Change-Id: I90f9fa70c745c62350960ce151cd116e14b5208a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/ABIVersion.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import java.io.DataInput;
+import java.io.IOException;
+import javax.annotation.Nonnull;
+
+/**
+ * Enumeration of all ABI versions supported by this implementation of the client access API.
+ *
+ * @author Robert Varga
+ */
+public enum ABIVersion implements Comparable<ABIVersion> {
+    // NOTE: enumeration values need to be sorted in asceding order of their version to keep Comparable working
+
+    /**
+     * Version which is older than any other version. This version exists purely for testing purposes.
+     */
+    @VisibleForTesting
+    TEST_PAST_VERSION(0),
+
+    /**
+     * Initial ABI version, as shipped with Boron Simultaneous release.
+     */
+    BORON(1),
+
+    /**
+     * Version which is newer than any other version. This version exists purely for testing purposes.
+     */
+    @VisibleForTesting
+    TEST_FUTURE_VERSION(65535);
+
+    private final short value;
+
+    ABIVersion(final int intVersion) {
+        Preconditions.checkArgument(intVersion >= 0 && intVersion <= 65535);
+        value = (short) intVersion;
+    }
+
+    /**
+     * Return the unsigned short integer identifying this version.
+     *
+     * @return
+     */
+    public short shortValue() {
+        return value;
+    }
+
+    /**
+     * Return the {@link ABIVersion} corresponding to an unsigned short integer. This method is provided for callers
+     * which provide their own recovery strategy in case of version incompatibility.
+     *
+     * @param s Short integer as returned from {@link #shortValue()}
+     * @return {@link ABIVersion}
+     * @throws FutureVersionException if the specified integer identifies a future version
+     * @throws PastVersionException if the specified integer identifies a past version which is no longer supported
+     */
+    public static @Nonnull ABIVersion valueOf(final short s) throws FutureVersionException, PastVersionException {
+        switch (Short.toUnsignedInt(s)) {
+            case 0:
+                throw new PastVersionException(s, BORON);
+            case 1:
+                return BORON;
+            default:
+                throw new FutureVersionException(s, BORON);
+        }
+    }
+
+    /**
+     * Read an {@link ABIVersion} from a {@link DataInput}. This method is provided for callers which do not have
+     * a recovery strategy for dealing with unsupported versions.
+     *
+     * @param input Input from which to read
+     * @return An {@link ABIVersion}
+     * @throws IOException If read fails or an unsupported version is encountered.
+     */
+    public static @Nonnull ABIVersion read(final @Nonnull DataInput input) throws IOException {
+        final short s = input.readShort();
+        try {
+            return valueOf(s);
+        } catch (FutureVersionException | PastVersionException e) {
+            throw new IOException("Unsupported version", e);
+        }
+    }
+}
