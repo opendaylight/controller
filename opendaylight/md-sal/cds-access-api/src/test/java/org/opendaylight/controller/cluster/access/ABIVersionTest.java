d3895eae19cc272dc0d52a692daa6bfc935f7eab
Robert Varga <rovarga@cisco.com>
2016-05-22 13:35:40 +0200

    BUG-5930: introduce ABIVersion
    This patch introduces the concept of ABI version in the client API.
    The concept will serve to guide frontend/backend interactions so that
    version negotiation cal ne implemented and messages are sent using
    a compatible serialization format.
    
    Change-Id: I90f9fa70c745c62350960ce151cd116e14b5208a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/test/java/org/opendaylight/controller/cluster/access/ABIVersionTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.opendaylight.controller.cluster.access.ABIVersion.BORON;
+import static org.opendaylight.controller.cluster.access.ABIVersion.TEST_FUTURE_VERSION;
+import static org.opendaylight.controller.cluster.access.ABIVersion.TEST_PAST_VERSION;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import org.junit.Test;
+
+public class ABIVersionTest {
+    @Test
+    public void testInvalidVersions() {
+        assertTrue(TEST_PAST_VERSION.compareTo(TEST_FUTURE_VERSION) < 0);
+        assertTrue(TEST_PAST_VERSION.compareTo(BORON) < 0);
+        assertTrue(TEST_FUTURE_VERSION.compareTo(BORON) > 0);
+    }
+
+    @Test
+    public void testBoronVersion() throws Exception {
+        assertEquals((short)5, BORON.shortValue());
+        assertEquals(BORON, ABIVersion.valueOf(BORON.shortValue()));
+        assertEquals(BORON, ABIVersion.readFrom(ByteStreams.newDataInput(writeVersion(BORON))));
+    }
+
+    @Test(expected=PastVersionException.class)
+    public void testInvalidPastVersion() throws Exception {
+        ABIVersion.valueOf(TEST_PAST_VERSION.shortValue());
+    }
+
+    @Test(expected=FutureVersionException.class)
+    public void testInvalidFutureVersion() throws Exception {
+        ABIVersion.valueOf(TEST_FUTURE_VERSION.shortValue());
+    }
+
+    private static byte[] writeVersion(final ABIVersion version) {
+        final ByteArrayDataOutput bado = ByteStreams.newDataOutput();
+        bado.writeShort(version.shortValue());
+        return bado.toByteArray();
+    }
+
+    @Test(expected=IOException.class)
+    public void testBadRead() throws IOException {
+        ABIVersion.readFrom(ByteStreams.newDataInput(writeVersion(TEST_PAST_VERSION)));
+    }
+}
