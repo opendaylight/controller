d542a8fbb2922e2e91f4c6f9fa96571167fe11a8
Robert Varga <rovarga@cisco.com>
2016-05-22 13:35:40 +0200

    BUG-5930: introduce ABIVersion
    This patch introduces the concept of ABI version in the client API.
    The concept will serve to guide frontend/backend interactions so that
    version negotiation cal ne implemented and messages are sent using
    a compatible serialization format.
    
    Change-Id: I90f9fa70c745c62350960ce151cd116e14b5208a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/PastVersionException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * Exception thrown from {@link ABIVersion#valueOf(short)} when the specified version is too old and no longer
+ * supported by the codebase.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class PastVersionException extends AbstractVersionException {
+    private static final long serialVersionUID = 1L;
+
+    PastVersionException(final short version, final ABIVersion closest) {
+        super("Version " + Short.toUnsignedInt(version) + " is too old", version, closest);
+    }
+}
