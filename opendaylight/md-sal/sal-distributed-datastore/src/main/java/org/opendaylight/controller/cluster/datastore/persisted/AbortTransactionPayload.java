0f5a9d12f8c4ee48a4cc2a422db65527922db4e6
Robert Varga <rovarga@cisco.com>
2016-05-23 20:10:01 +0200

    BUG-5280: add CommitTransactionPayload
    This adds the base payload, which is to be used for propagating
    transaction effects instead of DataTreeCandidatePayload. Also adds
    the abort case counterpart.
    
    Change-Id: I621e0be4f26509fae9f04e230c6a7e145938d7e6
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/AbortTransactionPayload.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import java.io.DataInput;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.yangtools.concepts.Identifiable;
+
+/**
+ * Payload persisted when a transaction is aborted. It contains the transaction identifier.
+ *
+ * @author Robert Varga
+ */
+public final class AbortTransactionPayload extends AbstractIdentifiablePayload<TransactionIdentifier> {
+    private static final class Proxy extends AbstractProxy<TransactionIdentifier> {
+        private static final long serialVersionUID = 1L;
+
+        public Proxy() {
+            // For Externalizable
+        }
+
+        Proxy(final byte[] serialized) {
+            super(serialized);
+        }
+
+        @Override
+        protected TransactionIdentifier readIdentifier(final DataInput in) throws IOException {
+            return TransactionIdentifier.readFrom(in);
+        }
+
+        @Override
+        protected Identifiable<TransactionIdentifier> createObject(final TransactionIdentifier identifier,
+                final byte[] serialized) {
+            return new AbortTransactionPayload(identifier, serialized);
+        }
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    AbortTransactionPayload(final TransactionIdentifier transactionId, final byte[] serialized) {
+        super(transactionId, serialized);
+    }
+
+    public static AbortTransactionPayload create(final TransactionIdentifier transactionId) throws IOException {
+        final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        transactionId.writeTo(out);
+        return new AbortTransactionPayload(transactionId, out.toByteArray());
+    }
+
+    @Override
+    protected AbstractProxy<TransactionIdentifier> externalizableProxy(final byte[] serialized) {
+        return new Proxy(serialized);
+    }
+}
