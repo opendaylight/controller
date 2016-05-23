d415eb701b899e201ff737fac00998c320305892
Robert Varga <rovarga@cisco.com>
2016-05-23 16:44:35 +0200

    BUG-5280: move DataTreeCandidate serialization to its own class
    This is a useful utility, which can be reused across multiple objects.
    Split it out into its own class.
    
    Change-Id: Ib21abf0ae42e3f70f44b3214039c0f26464bbc3e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/DeletedDataTreeCandidateNode.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/DeletedDataTreeCandidateNode.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.cluster.datastore;
+package org.opendaylight.controller.cluster.datastore.persisted;
 
 import com.google.common.base.Optional;
 import java.util.Collection;
