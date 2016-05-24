e6a205acee1a31d7b84876ef483b8ee3eabd9652
Robert Varga <rovarga@cisco.com>
2016-05-25 00:14:29 +0200

    BUG-5930: introduce PersistenceVersions
    This patch defines the concept of StateVersion, which governs
    state flooding from leader to followers.
    
    Change-Id: I412fbe89c75f4a66bb9e780bfdf0d3153a8b5366
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/AbstractVersionException.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+
+/**
+ * Abstract base exception used for reporting version mismatches from {@link StateVersion}.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public abstract class AbstractVersionException extends Exception {
+    private static final long serialVersionUID = 1L;
+    private final StateVersion closestVersion;
+    private final int version;
+
+    AbstractVersionException(final String message, final short version, final StateVersion closestVersion) {
+        super(message);
+        this.closestVersion = Preconditions.checkNotNull(closestVersion);
+        this.version = Short.toUnsignedInt(version);
+    }
+
+    /**
+     * Return the numeric version which has caused this exception.
+     *
+     * @return Numeric version
+     */
+    public final int getVersion() {
+        return version;
+    }
+
+    /**
+     * Return the closest version supported by this codebase.
+     *
+     * @return Closest supported {@link StateVersion}
+     */
+    public final @Nonnull StateVersion getClosestVersion() {
+        return closestVersion;
+    }
+
+}
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/PastVersionException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * Exception thrown from {@link StateVersion#valueOf(short)} when the specified version is too old and no longer
+ * supported by the codebase.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class PastVersionException extends AbstractVersionException {
+    private static final long serialVersionUID = 1L;
+
+    PastVersionException(final short version, final StateVersion closest) {
+        super("Version " + Short.toUnsignedInt(version) + " is too old", version, closest);
+    }
+}
