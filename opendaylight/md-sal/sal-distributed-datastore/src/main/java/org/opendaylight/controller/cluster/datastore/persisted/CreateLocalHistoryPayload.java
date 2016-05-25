5b58327631e3cf1aae897c75f61bfadd9017f78b
Robert Varga <rovarga@cisco.com>
2016-05-25 17:44:53 +0200

    BUG-5280: add {Create,Close}LocalHistoryPayload
    This patch introduces two new payloads which deal with replicating
    local histories to followers. These are persisted whenever a transaction
    chain (e.g. local history) is created or closed cleanly on the shard
    leader. Followers can use these to track transaction chains and pick up
    processing in case of a leader failover.
    
    Change-Id: I3fe5ac153c88f23f9b871bd23cb04a8e2410af91
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/CreateLocalHistoryPayload.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import java.io.DataInput;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+
+/**
+ * Payload persisted when a local history is created. It contains a {@link LocalHistoryIdentifier}.
+ *
+ * @author Robert Varga
+ */
+public final class CreateLocalHistoryPayload extends AbstractIdentifiablePayload<LocalHistoryIdentifier> {
+    private static final class Proxy extends AbstractProxy<LocalHistoryIdentifier> {
+        private static final long serialVersionUID = 1L;
+
+        public Proxy() {
+            // For Externalizable
+        }
+
+        Proxy(final byte[] serialized) {
+            super(serialized);
+        }
+
+        @Override
+        protected LocalHistoryIdentifier readIdentifier(final DataInput in) throws IOException {
+            return LocalHistoryIdentifier.readFrom(in);
+        }
+
+        @Override
+        protected CreateLocalHistoryPayload createObject(final LocalHistoryIdentifier identifier,
+                final byte[] serialized) {
+            return new CreateLocalHistoryPayload(identifier, serialized);
+        }
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    CreateLocalHistoryPayload(final LocalHistoryIdentifier historyId, final byte[] serialized) {
+        super(historyId, serialized);
+    }
+
+    public static CreateLocalHistoryPayload create(final LocalHistoryIdentifier historyId) throws IOException {
+        final ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        historyId.writeTo(out);
+        return new CreateLocalHistoryPayload(historyId, out.toByteArray());
+    }
+
+    @Override
+    protected Proxy externalizableProxy(final byte[] serialized) {
+        return new Proxy(serialized);
+    }
+}
