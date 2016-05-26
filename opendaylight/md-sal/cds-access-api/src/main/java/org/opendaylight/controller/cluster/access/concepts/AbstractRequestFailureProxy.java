3d5b5f05cf8dce6a79042020c24d2cbd435675ae
Robert Varga <rovarga@cisco.com>
2016-05-26 13:36:35 +0200

    BUG-5280: introduce Request/Response and related concepts
    This adds a versioned message concept and Request/Response concepts
    built on top of it.
    
    Change-Id: Ic2701646bf3fc34c0d8d1dea278bbf63324a6786
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractRequestFailureProxy.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import com.google.common.annotations.Beta;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Abstract Externalizable proxy for use with {@link RequestFailure} subclasses.
+ *
+ * @author Robert Varga
+ *
+ * @param <T> Target identifier type
+ */
+@Beta
+public abstract class AbstractRequestFailureProxy<T extends Identifier & WritableObject, C extends RequestFailure<T, C>>
+        extends AbstractResponseProxy<T, C> {
+    private static final long serialVersionUID = 1L;
+    private RequestException cause;
+
+    public AbstractRequestFailureProxy() {
+        // For Externalizable
+    }
+
+    protected AbstractRequestFailureProxy(final @Nonnull C failure) {
+        super(failure);
+        this.cause = failure.getCause();
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        out.writeObject(cause);
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        cause = (RequestException) in.readObject();
+    }
+
+    @Override
+    final C createResponse(final T target, final long sequence) {
+        return createFailure(target, sequence, cause);
+    }
+
+    protected abstract C createFailure(@Nonnull T target, long sequence, @Nonnull RequestException cause);
+}
---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractRequestProxy.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import akka.actor.ActorRef;
+import com.google.common.annotations.Beta;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Abstract Externalizable proxy for use with {@link Request} subclasses.
+ *
+ * @author Robert Varga
+ *
+ * @param <T> Target identifier type
+ */
+@Beta
+public abstract class AbstractRequestProxy<T extends Identifier & WritableObject, C extends Request<T, C>>
+        extends AbstractMessageProxy<T, C> {
+    private static final long serialVersionUID = 1L;
+    private ActorRef replyTo;
+
+    public AbstractRequestProxy() {
+        // For Externalizable
+    }
+
+    protected AbstractRequestProxy(final @Nonnull C request) {
+        super(request);
+        this.replyTo = request.getReplyTo();
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        // FIXME: http://doc.akka.io/docs/akka/current/java/serialization.html#Programmatic seems to be a more efficient
+        //        and complete way of doing serialization.
+        out.writeObject(replyTo);
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        replyTo = (ActorRef) in.readObject();
+    }
+
+    @Override
+    protected final C createMessage(final T target, final long sequence) {
+        return createMessage(target, sequence, replyTo);
+    }
+
+    protected abstract C createMessage(@Nonnull T target, long sequence, ActorRef replyTo);
+}
