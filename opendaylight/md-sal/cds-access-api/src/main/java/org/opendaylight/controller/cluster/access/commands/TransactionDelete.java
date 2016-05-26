3195c1654d8f62a0be1d08fc199dc3c85162da8f
Robert Varga <rovarga@cisco.com>
2016-05-27 01:13:56 +0200

    BUG-5280: introduce base Transaction request/success
    Change-Id: I23b83c3912975497f6ab2fac73451f51e613bc2e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/TransactionDelete.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Delete a particular path.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class TransactionDelete extends TransactionModification {
+    public TransactionDelete(final YangInstanceIdentifier path) {
+        super(path);
+    }
+
+    @Override
+    byte getType() {
+        return TYPE_DELETE;
+    }
+}
