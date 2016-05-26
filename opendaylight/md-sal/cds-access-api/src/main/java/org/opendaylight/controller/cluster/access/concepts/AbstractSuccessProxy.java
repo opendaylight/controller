b069810ee5eedcc9562820b454e381c3e8e170e6
Robert Varga <rovarga@cisco.com>
2016-05-26 13:36:35 +0200

    BUG-5280: introduce Request/Response and related concepts
    This adds a versioned message concept and Request/Response concepts
    built on top of it. Serialization uses the serializable proxy pattern,
    with one proxy class per ABI version.
    
    Change-Id: Ic2701646bf3fc34c0d8d1dea278bbf63324a6786
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractSuccessProxy.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import com.google.common.annotations.Beta;
+import java.io.Externalizable;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Abstract Externalizable proxy for use with {@link RequestSuccess} subclasses.
+ *
+ * @author Robert Varga
+ *
+ * @param <T> Target identifier type
+ */
+@Beta
+public abstract class AbstractSuccessProxy<T extends Identifier & WritableObject, C extends RequestSuccess<T, C>>
+        extends AbstractResponseProxy<T, C> implements Externalizable {
+    private static final long serialVersionUID = 1L;
+
+    public AbstractSuccessProxy() {
+        // For Externalizable
+    }
+
+    protected AbstractSuccessProxy(final @Nonnull C success) {
+        super(success);
+    }
+
+    @Override
+    final C createResponse(final T target, final long sequence) {
+        return createSuccess(target, sequence);
+    }
+
+    protected abstract @Nonnull C createSuccess(@Nonnull T target, long sequence);
+}
