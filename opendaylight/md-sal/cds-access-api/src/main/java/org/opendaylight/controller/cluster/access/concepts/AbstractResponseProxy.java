5d7a55d4106fcf774dc33633b974188f84b5f151
Robert Varga <rovarga@cisco.com>
2016-05-26 13:36:35 +0200

    BUG-5280: introduce Request/Response and related concepts
    This adds a versioned message concept and Request/Response concepts
    built on top of it. Serialization uses the serializable proxy pattern,
    with one proxy class per ABI version.
    
    For ActorRefs we rely on the fact that serialization of these
    messages should only ever occur in the context of JavaSerializer,
    so we can use its #currentSystem() and perform ActorRef serialization
    without having to go through writeObject().
    
    Change-Id: Ic2701646bf3fc34c0d8d1dea278bbf63324a6786
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractResponseProxy.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Abstract Externalizable proxy class to use with {@link Response} subclasses.
+ *
+ * @author Robert Varga
+ *
+ * @param <T> Target identifier type
+ * @param <C> Message class
+ */
+abstract class AbstractResponseProxy<T extends Identifier & WritableObject, C extends Response<T, C>>
+        extends AbstractMessageProxy<T, C> {
+    private static final long serialVersionUID = 1L;
+
+    AbstractResponseProxy() {
+        // for Externalizable
+    }
+
+    AbstractResponseProxy(final @Nonnull C response) {
+        super(response);
+    }
+
+    @Override
+    final C createMessage(final T target, final long sequence) {
+        return createResponse(target, sequence);
+    }
+
+    abstract @Nonnull C createResponse(@Nonnull T target, long sequence);
+}
