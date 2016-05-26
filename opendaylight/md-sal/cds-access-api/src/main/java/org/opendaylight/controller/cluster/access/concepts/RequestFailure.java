0e16b8f47c715db8a31f016c83c3b88d3beb4e28
Robert Varga <rovarga@cisco.com>
2016-05-26 13:36:35 +0200

    BUG-5280: introduce Request/Response and related concepts
    This adds a versioned message concept and Request/Response concepts
    built on top of it. Serialization uses the serializable proxy pattern,
    with one proxy class per ABI version.
    
    Change-Id: Ic2701646bf3fc34c0d8d1dea278bbf63324a6786
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/RequestFailure.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * A failure response to a {@link Request}. Contains a {@link RequestException} detailing the cause for this failure.
+ *
+ * @author Robert Varga
+ *
+ * @param <T> Target identifier type
+ * @param <C> Message class
+ */
+@Beta
+public abstract class RequestFailure<T extends Identifier & WritableObject, C extends RequestFailure<T, C>> extends Response<T, C> {
+    private static final long serialVersionUID = 1L;
+    private final RequestException cause;
+
+    protected RequestFailure(final @Nonnull C failure, final @Nonnull ABIVersion version) {
+        super(failure, version);
+        this.cause = Preconditions.checkNotNull(failure.getCause());
+    }
+
+    protected RequestFailure(final @Nonnull T target, final long sequence, final @Nonnull RequestException cause) {
+        super(target, sequence);
+        this.cause = Preconditions.checkNotNull(cause);
+    }
+
+    /**
+     * Return the failure cause.
+     *
+     * @return Failure cause.
+     */
+    public final @Nonnull RequestException getCause() {
+        return cause;
+    }
+
+    /**
+     * Return an indication of whether this a hard failure. Hard failures must not be retried but need to be treated
+     * as authoritative response to a request.
+     */
+    public final boolean isHardFailure() {
+        return !cause.isRetriable();
+    }
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper toStringHelper) {
+        return super.addToStringAttributes(toStringHelper).add("cause", cause);
+    }
+
+    @Override
+    protected abstract AbstractRequestFailureProxy<T, C> externalizableProxy(@Nonnull ABIVersion version);
+}
