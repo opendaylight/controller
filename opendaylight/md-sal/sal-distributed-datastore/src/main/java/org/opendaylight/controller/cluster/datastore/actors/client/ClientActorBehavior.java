3000731247029029ca7656f05fa1e4a6d80cf496
Robert Varga <rovarga@cisco.com>
2016-05-27 17:24:20 +0200

    BUG-5280: define RetiredGenerationException
    This defines the error reported by the backend whenever it sees
    a request coming from an outdated client generation. The client
    behavior handles these failures by invoking an upcall to the subclass
    and initiate shutdown.
    
    Change-Id: Iea1e04e3754fdc09fbdd9c6dc0d420a05145791c
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/ClientActorBehavior.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/ClientActorBehavior.java
@@ -11,7 +11,12 @@ import com.google.common.annotations.Beta;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.RequestException;
+import org.opendaylight.controller.cluster.access.concepts.RequestFailure;
+import org.opendaylight.controller.cluster.access.concepts.RetiredGenerationException;
 import org.opendaylight.yangtools.concepts.Identifiable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A behavior, which handles messages sent to a {@link AbstractClientActor}.
@@ -23,12 +28,24 @@ import org.opendaylight.yangtools.concepts.Identifiable;
 @Beta
 public abstract class ClientActorBehavior extends RecoveredClientActorBehavior<ClientActorContext>
         implements Identifiable<ClientIdentifier> {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientActorBehavior.class);
+
     protected ClientActorBehavior(final @Nonnull ClientActorContext context) {
         super(context);
     }
 
     @Override
     final ClientActorBehavior onReceiveCommand(final Object command) {
+        if (command instanceof RequestFailure) {
+            final RequestFailure<?, ?> failure = (RequestFailure<?, ?>) command;
+            final RequestException cause = failure.getCause();
+            if (cause instanceof RetiredGenerationException) {
+                LOG.error("{}: current generation {} has been superseded", persistenceId(), getIdentifier(), cause);
+                hacf(cause);
+                return null;
+            }
+        }
+
         // TODO: any client-common logic (such as validation and common dispatch) needs to go here
         return onCommand(command);
     }
@@ -37,6 +54,16 @@ public abstract class ClientActorBehavior extends RecoveredClientActorBehavior<C
     public final @Nonnull ClientIdentifier getIdentifier() {
         return context().getIdentifier();
     }
+
+    /**
+     * Halt And Catch Fire.
+     *
+     * Halt processing on this client. Implementations need to ensure they initiate state flush procedures. No attempt
+     * to use this instance should be made after this method returns. Any such use may result in undefined behavior.
+     *
+     * @param cause Failure cause
+     */
+    protected abstract void hacf(@Nonnull Throwable cause);
 
     /**
      * Override this method to handle any command which is not handled by the base behavior.
