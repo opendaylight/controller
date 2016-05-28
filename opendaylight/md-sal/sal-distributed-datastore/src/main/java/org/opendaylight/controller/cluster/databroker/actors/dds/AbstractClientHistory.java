7aba59d7d70696aa8203a70f111f9e30e10ce113
Robert Varga <rovarga@cisco.com>
2016-05-29 01:27:24 +0200

    WIP: BUG-5280: implement message queueing
    This patch implements the basic queueing and timeout retry mechanism
    in ClientActorBehavior.
    
    Change-Id: I207a30877328dbdc08d42f76a0db55b5ae162de5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/AbstractClientHistory.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import akka.actor.ActorRef;
+import com.google.common.base.Preconditions;
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+import org.opendaylight.yangtools.concepts.Identifiable;
+
+abstract class AbstractClientHistory implements Identifiable<LocalHistoryIdentifier> {
+    private final Map<Long, LocalHistoryIdentifier> histories = new HashMap<>();
+    private final DistributedDataStoreClientBehavior client;
+    private final LocalHistoryIdentifier identifier;
+
+    protected AbstractClientHistory(final DistributedDataStoreClientBehavior client,
+            final LocalHistoryIdentifier identifier) {
+        this.client = Preconditions.checkNotNull(client);
+        this.identifier = Preconditions.checkNotNull(identifier);
+        Preconditions.checkArgument(identifier.getCookie() == 0);
+    }
+
+    final LocalHistoryIdentifier getHistoryForCookie(final Long cookie) {
+        LocalHistoryIdentifier ret = histories.get(cookie);
+        if (ret == null) {
+            ret = new LocalHistoryIdentifier(identifier.getClientId(), identifier.getHistoryId(), cookie);
+            histories.put(cookie, ret);
+        }
+
+        return ret;
+    }
+
+    @Override
+    public final LocalHistoryIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    final DistributedDataStoreClientBehavior getClient() {
+        return client;
+    }
+}
