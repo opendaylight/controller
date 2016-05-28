96d890a9bd7c367cd12a3f3deb14a0ed4fbee65a
Robert Varga <rovarga@cisco.com>
2016-05-29 01:27:24 +0200

    WIP: BUG-5280: implement message queueing
    This patch implements the basic queueing and timeout retry mechanism
    in ClientActorBehavior.
    
    This implementation is not very efficient, as each send goes through
    the actor's mailbox, but it gets the job done and is correct. It will
    be optimized in a follow-up patch, which will refactor internal
    workings so that SequencedQueue is fully thread-safe and correct with
    regard to request enqueue, timeouts and retries.
    
    Change-Id: I207a30877328dbdc08d42f76a0db55b5ae162de5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/ClientActorContext.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/ClientActorContext.java
@@ -8,12 +8,26 @@
 package org.opendaylight.controller.cluster.datastore.actors.client;
 
 import akka.actor.ActorRef;
+import akka.actor.Cancellable;
+import akka.actor.Scheduler;
 import com.google.common.annotations.Beta;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Ticker;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
 import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.Request;
+import org.opendaylight.controller.cluster.access.concepts.RequestException;
+import org.opendaylight.controller.cluster.access.concepts.RequestFailure;
+import org.opendaylight.controller.cluster.access.concepts.Response;
 import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.concepts.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.FiniteDuration;
 
 /**
  * An actor context associated with this {@link AbstractClientActor}.
@@ -25,13 +39,22 @@ import org.opendaylight.yangtools.concepts.Identifiable;
  * @author Robert Varga
  */
 @Beta
+@ThreadSafe
 public class ClientActorContext extends AbstractClientActorContext implements Identifiable<ClientIdentifier> {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientActorContext.class);
+
+    private final Map<Identifier, SequencedQueue> queues = new ConcurrentHashMap<>();
     private final ClientIdentifier identifier;
+    private final ExecutionContext executionContext;
+    private final Scheduler scheduler;
 
     // Hidden to avoid subclassing
-    ClientActorContext(final ActorRef self, final String persistenceId, final ClientIdentifier identifier) {
+    ClientActorContext(final ActorRef self, final Scheduler scheduler, final ExecutionContext executionContext,
+            final String persistenceId, final ClientIdentifier identifier) {
         super(self, persistenceId);
         this.identifier = Preconditions.checkNotNull(identifier);
+        this.scheduler = Preconditions.checkNotNull(scheduler);
+        this.executionContext = Preconditions.checkNotNull(executionContext);
     }
 
     @Override
@@ -58,4 +81,47 @@ public class ClientActorContext extends AbstractClientActorContext implements Id
     public void executeInActor(final @Nonnull InternalCommand command) {
         self().tell(Preconditions.checkNotNull(command), ActorRef.noSender());
     }
+
+    public Cancellable executeInActor(final @Nonnull InternalCommand command, final FiniteDuration delay) {
+        return scheduler.scheduleOnce(Preconditions.checkNotNull(delay), self(), Preconditions.checkNotNull(command),
+            executionContext, ActorRef.noSender());
+    }
+
+    SequencedQueue queueFor(final Request<?, ?> request) {
+        return queues.computeIfAbsent(request.getTarget(), t -> new SequencedQueue(t, ticker()));
+    }
+
+    void removeQueue(final SequencedQueue queue) {
+        queues.remove(queue.getTarget(), queue);
+    }
+
+    ClientActorBehavior completeRequest(final ClientActorBehavior current, final Response<?, ?> response) {
+        final SequencedQueue queue = queues.get(response.getTarget());
+        if (queue == null) {
+            LOG.info("{}: Ignoring unknown response {}", persistenceId(), response);
+            return current;
+        } else {
+            return queue.complete(current, response);
+        }
+    }
+
+    void poison(final RequestException cause) {
+        for (SequencedQueue q : queues.values()) {
+            q.poison(cause);
+        }
+
+        queues.clear();
+    }
+
+    // TODO: this method is currently unused
+    void retryRequest(final RequestFailure<?, ?> failure) {
+        final SequencedQueue queue = queues.get(failure.getTarget());
+        if (queue == null) {
+            LOG.info("{}: Ignoring unknown response {}", persistenceId(), failure);
+            return;
+        }
+
+        queue.retryRequest(failure);
+    }
+
 }
