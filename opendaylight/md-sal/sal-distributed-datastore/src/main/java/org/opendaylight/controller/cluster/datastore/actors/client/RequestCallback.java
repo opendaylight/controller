577f5e3d647820ba82cedec281ed65efd610c7ec
Robert Varga <rovarga@cisco.com>
2016-05-29 01:27:24 +0200

    WIP: BUG-5280: implement message queueing
    This patch implements the basic queueing and timeout retry mechanism
    in ClientActorBehavior.
    
    Change-Id: I207a30877328dbdc08d42f76a0db55b5ae162de5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/RequestCallback.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.controller.cluster.access.concepts.Response;
+import org.opendaylight.yangtools.concepts.WritableIdentifier;
+
+@FunctionalInterface
+public interface RequestCallback<T extends WritableIdentifier> {
+    /**
+     * Invoked when a particular request completes.
+     *
+     * @param response Response to the request
+     * @return Next client actor behavior
+     */
+    @Nullable ClientActorBehavior complete(@Nonnull Response<T, ?> response);
+}
