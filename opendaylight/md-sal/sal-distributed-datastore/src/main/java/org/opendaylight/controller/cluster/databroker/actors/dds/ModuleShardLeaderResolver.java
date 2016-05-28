ed6f9a6667934e11140a9ce40552674546650b48
Robert Varga <rovarga@cisco.com>
2016-05-29 01:27:24 +0200

    WIP: BUG-5280: implement message queueing
    This patch implements the basic queueing and timeout retry mechanism
    in ClientActorBehavior.
    
    Change-Id: I207a30877328dbdc08d42f76a0db55b5ae162de5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/ModuleShardLeaderResolver.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import akka.actor.ActorRef;
+import java.util.Map.Entry;
+import java.util.concurrent.CompletionStage;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+import org.opendaylight.controller.cluster.datastore.actors.client.ShardLeaderResolver;
+
+final class ModuleShardLeaderResolver extends ShardLeaderResolver {
+
+
+    @Override
+    public CompletionStage<Entry<ActorRef, ABIVersion>> getShardLeader(final Long cookie) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+}
