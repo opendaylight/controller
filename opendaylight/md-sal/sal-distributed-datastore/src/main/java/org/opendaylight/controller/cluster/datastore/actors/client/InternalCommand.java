e1a7c7715f77d5ec18f55f4a818adf77b121b2db
Robert Varga <rovarga@cisco.com>
2016-05-29 23:34:14 +0200

    BUG-5280: use a lambda for createLocalHistory()/close()
    These are internal commands, which can be efficiently implemented
    using a simple delayed execution primitive.
    
    Introduce ClientActorContext#executeInActor(), which will wrap
    a specialized subclass of Runnable and send it to the actor.
    
    This can be used to dispatch lambdas to methods, reducing the need
    for specialized messages and instanceof checks.
    
    Change-Id: Id5cd388657a274d551892a6c943b062d70c7bea7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/actors/client/InternalCommand.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.actors.client;
+
+import javax.annotation.Nullable;
+
+/**
+ * This interface is used to pass the unit of work via the actors mailbox. The command can alter behavior of the actor
+ * by returning a new behavior.
+ *
+ * @author Robert Varga
+ */
+@FunctionalInterface
+public interface InternalCommand {
+    /**
+     * Run command actions.
+     *
+     * @return Next behavior to use in the client actor
+     */
+    @Nullable ClientActorBehavior execute();
+}
