6f01a66e53c68389e8040f06dc1d691e8704e53f
Robert Varga <rovarga@cisco.com>
2016-06-01 11:12:33 +0200

    BUG-5280: introduce WritableIdentifier
    Since the Request concept requires both WritableObject and Identifier,
    it makes dealing with it in a type-safe manner quite clunky. Introduce
    a convenience interface combining the two, which makes code much more
    elegant.
    
    Change-Id: Ib571add7c5a46699a3e75150279c87b40da1ffaa
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractMessageProxy.java
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/AbstractMessageProxy.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import javax.annotation.Nonnull;
-import org.opendaylight.yangtools.concepts.Identifier;
 
 /**
  * Abstract Externalizable proxy for use with {@link Message} subclasses.
@@ -24,8 +23,7 @@ import org.opendaylight.yangtools.concepts.Identifier;
  * @param <T> Target identifier type
  * @param <C> Message class
  */
-abstract class AbstractMessageProxy<T extends Identifier & WritableObject, C extends Message<T, C>>
-        implements Externalizable {
+abstract class AbstractMessageProxy<T extends WritableIdentifier, C extends Message<T, C>> implements Externalizable {
     private static final long serialVersionUID = 1L;
     private T target;
     private long sequence;
