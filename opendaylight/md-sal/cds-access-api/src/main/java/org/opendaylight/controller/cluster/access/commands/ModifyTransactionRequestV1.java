b3cfcf7578ff90b673e36898637ccc830ae225dc
Robert Varga <rovarga@cisco.com>
2016-06-02 18:12:41 +0200

    BUG-5280: more transaction requests
    Change-Id: I0425fda026807b0f9a2fb8ac73a4ad333ee96c72
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/ModifyTransactionRequestV1.java
@@ -0,0 +1,77 @@
+package org.opendaylight.controller.cluster.access.commands;
+
+import akka.actor.ActorRef;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataInput;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataOutput;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeInputOutput;
+
+/**
+ * Externalizable proxy for use with {@link ExistsTransactionRequest}. It implements the initial (Boron) serialization
+ * format.
+ *
+ * @author Robert Varga
+ */
+final class ModifyTransactionRequestV1 extends AbstractTransactionRequestProxy<ModifyTransactionRequest> {
+    private static final long serialVersionUID = 1L;
+    private List<TransactionModification> modifications;
+    private Optional<PersistenceProtocol> protocol;
+
+    public ModifyTransactionRequestV1() {
+        // For Externalizable
+    }
+
+    ModifyTransactionRequestV1(final ModifyTransactionRequest request) {
+        super(request);
+        this.modifications = Preconditions.checkNotNull(request.getModifications());
+        this.protocol = request.getPersistenceProtocol();
+    }
+
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+
+        protocol = Optional.ofNullable(PersistenceProtocol.readFrom(in));
+
+        final int size = in.readInt();
+        if (size != 0) {
+            modifications = new ArrayList<>(size);
+            final NormalizedNodeDataInput nnin = NormalizedNodeInputOutput.newDataInput(in);
+            for (int i = 0; i < size; ++i) {
+                modifications.add(TransactionModification.readFrom(nnin));
+            }
+        } else {
+            modifications = ImmutableList.of();
+        }
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+
+        out.writeByte(PersistenceProtocol.byteValue(protocol.orElse(null)));
+        out.writeInt(modifications.size());
+        if (!modifications.isEmpty()) {
+            try (NormalizedNodeDataOutput nnout = NormalizedNodeInputOutput.newDataOutput(out)) {
+                for (TransactionModification op : modifications) {
+                    op.writeTo(nnout);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected ModifyTransactionRequest createRequest(final TransactionIdentifier target, final long sequence,
+            final ActorRef replyTo) {
+        return new ModifyTransactionRequest(target, sequence, replyTo, modifications, protocol.orElse(null));
+    }
+}
---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/TransactionDataModification.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataOutput;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+@Beta
+public abstract class TransactionDataModification extends TransactionModification {
+    private final NormalizedNode<?, ?> data;
+
+    TransactionDataModification(final YangInstanceIdentifier path, final NormalizedNode<?, ?> data) {
+        super(path);
+        this.data = Preconditions.checkNotNull(data);
+    }
+
+    public final NormalizedNode<?, ?> getData() {
+        return data;
+    }
+
+    @Override
+    final void writeTo(final NormalizedNodeDataOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeNormalizedNode(data);
+    }
+}
