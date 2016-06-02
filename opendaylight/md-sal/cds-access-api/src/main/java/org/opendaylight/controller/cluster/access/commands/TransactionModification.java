38c1c3310de1b5933c7d909be48d6fddde593e1f
Robert Varga <rovarga@cisco.com>
2016-06-02 18:12:41 +0200

    BUG-5280: more transaction requests
    Change-Id: I0425fda026807b0f9a2fb8ac73a4ad333ee96c72
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/TransactionModification.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataInput;
+import org.opendaylight.controller.cluster.datastore.node.utils.stream.NormalizedNodeDataOutput;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+@Beta
+public abstract class TransactionModification {
+    static final byte TYPE_DELETE = 1;
+    static final byte TYPE_MERGE = 2;
+    static final byte TYPE_WRITE = 3;
+
+    private final YangInstanceIdentifier path;
+
+    TransactionModification(final YangInstanceIdentifier path) {
+        this.path = Preconditions.checkNotNull(path);
+    }
+
+    public final YangInstanceIdentifier getPath() {
+        return path;
+    }
+
+    abstract byte getType();
+
+    void writeTo(final NormalizedNodeDataOutput out) throws IOException {
+        out.writeByte(getType());
+        out.writeYangInstanceIdentifier(path);
+    }
+
+    static TransactionModification readFrom(final NormalizedNodeDataInput in) throws IOException {
+        final byte type = in.readByte();
+        switch (type) {
+            case TYPE_DELETE:
+                return new TransactionDelete(in.readYangInstanceIdentifier());
+            case TYPE_MERGE:
+                return new TransactionMerge(in.readYangInstanceIdentifier(), in.readNormalizedNode());
+            case TYPE_WRITE:
+                return new TransactionWrite(in.readYangInstanceIdentifier(), in.readNormalizedNode());
+            default:
+                throw new IllegalArgumentException("Unhandled type " + type);
+        }
+    }
+}
