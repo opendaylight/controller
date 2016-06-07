e40e9a946c684b0624346a0ab9ca8c760bd110f9
Robert Varga <rovarga@cisco.com>
2016-06-07 14:59:14 +0200

    WIP: BUG-5280: implement transaction dispatch
    This implements basic transactions.
    
    Change-Id: I6b0905fb97e3269c12a5cd8f2c681e4caeb14e3e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/AbstractClientHistory.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import com.google.common.base.Preconditions;
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+import org.opendaylight.yangtools.concepts.Identifiable;
+
+/**
+ * Abstract base class for client view of a history. This class has two implementations, one for normal local histories
+ * and the other for single transactions.
+ *
+ * @author Robert Varga
+ */
+abstract class AbstractClientHistory implements Identifiable<LocalHistoryIdentifier> {
+    private final Map<Long, LocalHistoryIdentifier> histories = new HashMap<>();
+    private final DistributedDataStoreClientBehavior client;
+    private final LocalHistoryIdentifier identifier;
+
+    AbstractClientHistory(final DistributedDataStoreClientBehavior client, final LocalHistoryIdentifier identifier) {
+        this.client = Preconditions.checkNotNull(client);
+        this.identifier = Preconditions.checkNotNull(identifier);
+        Preconditions.checkArgument(identifier.getCookie() == 0);
+    }
+
+    final LocalHistoryIdentifier getHistoryForCookie(final Long cookie) {
+        LocalHistoryIdentifier ret = histories.get(cookie);
+        if (ret == null) {
+            ret = new LocalHistoryIdentifier(identifier.getClientId(), identifier.getHistoryId(), cookie);
+            histories.put(cookie, ret);
+        }
+
+        return ret;
+    }
+
+    @Override
+    public final LocalHistoryIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    final DistributedDataStoreClientBehavior getClient() {
+        return client;
+    }
+
+    /**
+     * Callback invoked from {@link ClientSingleTransaction} when a transaction has been completed.
+     *
+     * @param transaction Transaction handle
+     */
+    void transactionComplete(final ClientSingleTransaction transaction) {
+        client.transactionComplete(transaction);
+    }
+}
