dfbbe7c1fc96c68839a2efb9f9e92727dfd065c1
Robert Varga <rovarga@cisco.com>
2016-06-07 14:59:14 +0200

    BUG-5280: implement transaction dispatch
    This patch adds the DOMStore interface in DistributedDataStoreClient
    and defines the missing messages.
    
    Change-Id: I6b0905fb97e3269c12a5cd8f2c681e4caeb14e3e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/TransactionDoCommitRequestProxyV1.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import akka.actor.ActorRef;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+
+/**
+ * Externalizable proxy for use with {@link TransactionDoCommitRequest}. It implements the initial (Boron) serialization
+ * format.
+ *
+ * @author Robert Varga
+ */
+final class TransactionDoCommitRequestProxyV1 extends AbstractTransactionRequestProxy<TransactionDoCommitRequest> {
+    private static final long serialVersionUID = 1L;
+
+    public TransactionDoCommitRequestProxyV1() {
+        // For Externalizable
+    }
+
+    TransactionDoCommitRequestProxyV1(final TransactionDoCommitRequest request) {
+        super(request);
+    }
+
+    @Override
+    protected TransactionDoCommitRequest createRequest(final TransactionIdentifier target, final ActorRef replyTo) {
+        return new TransactionDoCommitRequest(target, replyTo);
+    }
+}
