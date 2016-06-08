2394b32ca1dcbb397940ca075d27bc021e0a17fd
Donald Hunter <donaldh@cisco.com>
2016-06-08 23:48:22 +0100

    Refactor AbstractConfigTestBase to AbstractKarafTestBase
    This separates the PaxExam configuration into AbstractKarafTestBase and allows
    us to deprecate AbstractConfigTestBase.
    
    Change-Id: If4f6b1cf1ccaffd338dbbd5b9b2bdedcfe9431c9
    Signed-off-by: Donald Hunter <donaldh@cisco.com>

--- opendaylight/config/config-it-base/src/main/java/org/opendaylight/controller/config/it/base/AbstractConfigTestBase.java
+++ opendaylight/config/config-it-base/src/main/java/org/opendaylight/controller/config/it/base/AbstractConfigTestBase.java
@@ -8,70 +8,33 @@
 
 package org.opendaylight.controller.config.it.base;
 
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.when;
+import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
-import com.google.common.base.Stopwatch;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.lang.management.ManagementFactory;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 import javax.management.ObjectName;
+
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.internal.AssumptionViolatedException;
-import org.junit.rules.TestRule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 import org.opendaylight.controller.config.api.ConfigRegistry;
 import org.opendaylight.controller.config.util.ConfigRegistryJMXClient;
-import org.ops4j.pax.exam.Configuration;
+import org.opendaylight.controller.karaf.it.base.AbstractKarafTestBase;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.OptionUtils;
-import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
-import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
-import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class AbstractConfigTestBase {
-    private static final String MAVEN_REPO_LOCAL = "maven.repo.local";
-    private static final String ORG_OPS4J_PAX_URL_MVN_LOCAL_REPOSITORY = "org.ops4j.pax.url.mvn.localRepository";
-    private static final String ETC_ORG_OPS4J_PAX_URL_MVN_CFG = "etc/org.ops4j.pax.url.mvn.cfg";
-    private static final String ETC_ORG_OPS4J_PAX_LOGGING_CFG = "etc/org.ops4j.pax.logging.cfg";
+import com.google.common.base.Stopwatch;
 
-    private static final String PAX_EXAM_UNPACK_DIRECTORY = "target/exam";
-    private static final String KARAF_DEBUG_PORT = "5005";
-    private static final String KARAF_DEBUG_PROP = "karaf.debug";
-    private static final String KEEP_UNPACK_DIRECTORY_PROP = "karaf.keep.unpack";
+/**
+ * Please use AbstractKarafTestBase instead.
+ * @see AbstractKarafTestBase
+ */
+@Deprecated
+public abstract class AbstractConfigTestBase extends AbstractKarafTestBase {
+
     private static final Logger LOG = LoggerFactory.getLogger(AbstractConfigTestBase.class);
-    public static final String ORG_OPS4J_PAX_LOGGING_CFG = "etc/org.ops4j.pax.logging.cfg";
-
-    /*
-     * Default values for karaf distro type, groupId, and artifactId
-     */
-    private static final String KARAF_DISTRO_TYPE = "zip";
-    private static final String KARAF_DISTRO_ARTIFACTID = "opendaylight-karaf-empty";
-    private static final String KARAF_DISTRO_GROUPID = "org.opendaylight.controller";
-
-    /*
-     * Property names to override defaults for karaf distro artifactId, groupId, version, and type
-     */
-    private static final String KARAF_DISTRO_VERSION_PROP = "karaf.distro.version";
-    private static final String KARAF_DISTRO_TYPE_PROP = "karaf.distro.type";
-    private static final String KARAF_DISTRO_ARTIFACTID_PROP = "karaf.distro.artifactId";
-    private static final String KARAF_DISTRO_GROUPID_PROP = "karaf.distro.groupId";
-
-    /**
-     * Property file used to store the Karaf distribution version
-     */
-    private static final String PROPERTIES_FILENAME = "abstractconfigtestbase.properties";
 
     /*
      * Wait up to 10s for our configured module to come up
@@ -98,19 +61,17 @@ public abstract class AbstractConfigTestBase {
         return null;
     }
 
-    public abstract MavenUrlReference getFeatureRepo();
-
-    public abstract String getFeatureName();
-
-    public Option getLoggingOption() {
+    @Override
+    protected Option getLoggingOption() {
         Option option = editConfigurationFilePut(ORG_OPS4J_PAX_LOGGING_CFG,
                         logConfiguration(AbstractConfigTestBase.class),
                         LogLevel.INFO.name());
+        option = composite(option, super.getLoggingOption());
         return option;
     }
 
     /**
-     * Override this method to provide more options to config
+     * Override this method to provide more options to config.
      *
      * @return An array of additional config options
      */
@@ -122,59 +83,15 @@ public abstract class AbstractConfigTestBase {
         return "log4j.logger." + klazz.getPackage().getName();
     }
 
-    public String getKarafDistro() {
-        String groupId = System.getProperty(KARAF_DISTRO_GROUPID_PROP,KARAF_DISTRO_GROUPID);
-        String artifactId = System.getProperty(KARAF_DISTRO_ARTIFACTID_PROP,KARAF_DISTRO_ARTIFACTID);
-        String version = System.getProperty(KARAF_DISTRO_VERSION_PROP);
-        String type = System.getProperty(KARAF_DISTRO_TYPE_PROP,KARAF_DISTRO_TYPE);
-        if (version == null) {
-            // We use a properties file to retrieve ${karaf.version}, instead of .versionAsInProject()
-            // This avoids forcing all users to depend on Karaf in their POMs
-            Properties abstractConfigTestBaseProps = new Properties();
-            try (InputStream abstractConfigTestBaseInputStream = Thread.currentThread().getContextClassLoader()
-                    .getResourceAsStream(PROPERTIES_FILENAME)) {
-                abstractConfigTestBaseProps.load(abstractConfigTestBaseInputStream);
-            } catch (IOException e) {
-                LOG.error("Unable to load {} to determine the Karaf version", PROPERTIES_FILENAME, e);
-            }
-            version = abstractConfigTestBaseProps.getProperty(KARAF_DISTRO_VERSION_PROP);
-        }
-        MavenArtifactUrlReference karafUrl = maven()
-                .groupId(groupId)
-                .artifactId(artifactId)
-                .version(version)
-                .type(type);
-        return karafUrl.getURL();
-    }
-
-    protected Option mvnLocalRepoOption() {
-        String mvnRepoLocal = System.getProperty(MAVEN_REPO_LOCAL, "");
-        LOG.info("mvnLocalRepo \"{}\"", mvnRepoLocal);
-        return editConfigurationFilePut(ETC_ORG_OPS4J_PAX_URL_MVN_CFG, ORG_OPS4J_PAX_URL_MVN_LOCAL_REPOSITORY,
-                mvnRepoLocal);
-    }
-
-    @Configuration
-    public Option[] config() {
-        Option[] options = new Option[] {
-                when(Boolean.getBoolean(KARAF_DEBUG_PROP))
-                        .useOptions(KarafDistributionOption.debugConfiguration(KARAF_DEBUG_PORT, true)),
-                karafDistributionConfiguration().frameworkUrl(getKarafDistro())
-                        .unpackDirectory(new File(PAX_EXAM_UNPACK_DIRECTORY))
-                        .useDeployFolder(false),
-                when(Boolean.getBoolean(KEEP_UNPACK_DIRECTORY_PROP)).useOptions(keepRuntimeFolder()),
-                features(getFeatureRepo(), getFeatureName()),
-                getLoggingOption(),
-                mvnLocalRepoOption(),
-                editConfigurationFilePut(ETC_ORG_OPS4J_PAX_LOGGING_CFG, "log4j.rootLogger", "INFO, stdout, osgi:*")};
-        return OptionUtils.combine(options, getAdditionalOptions());
-    }
-
+    /**
+     * Wait for module config to be pushed to the config datastore before running tests.
+     * @throws Exception on timeout
+     */
     @Before
     public void setup() throws Exception {
         String moduleName = getModuleName();
         String instanceName = getInstanceName();
-        if(moduleName == null || instanceName == null) {
+        if (moduleName == null || instanceName == null) {
             return;
         }
 
@@ -182,7 +99,7 @@ public abstract class AbstractConfigTestBase {
                 moduleName, instanceName);
         Stopwatch stopWatch = Stopwatch.createStarted();
         ObjectName objectName = null;
-        for(int i = 0;i<MODULE_TIMEOUT_MILLIS;i++) {
+        for (int i = 0; i < MODULE_TIMEOUT_MILLIS; i++) {
             try {
                 ConfigRegistry configRegistryClient = new ConfigRegistryJMXClient(ManagementFactory
                         .getPlatformMBeanServer());
@@ -191,7 +108,7 @@ public abstract class AbstractConfigTestBase {
                         moduleName,instanceName,objectName);
                 break;
             } catch (Exception e) {
-                if(i<MODULE_TIMEOUT_MILLIS) {
+                if (i < MODULE_TIMEOUT_MILLIS) {
                     Thread.sleep(1);
                     continue;
                 } else {
@@ -199,43 +116,13 @@ public abstract class AbstractConfigTestBase {
                 }
             }
         }
-        if(objectName != null) {
+        if (objectName != null) {
             LOG.info("Module: {} Instance: {} configured after {} ms",
                 moduleName,instanceName,
                 stopWatch.elapsed(TimeUnit.MILLISECONDS));
         } else {
-            throw new RuntimeException("NOT FOUND Module: " +moduleName + " Instance: " + instanceName +
-                    " configured after " + stopWatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+            throw new RuntimeException("NOT FOUND Module: " + moduleName + " Instance: " + instanceName
+                    + " configured after " + stopWatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
         }
     }
-
-    @Rule
-    public TestRule watcher = new TestWatcher() {
-
-        @Override
-        protected void starting(Description description) {
-            LOG.info("TestWatcher: Starting test: {}", description.getDisplayName());
-        }
-
-        @Override
-        protected void finished(Description description) {
-            LOG.info("TestWatcher: Finished test: {}", description.getDisplayName());
-        }
-
-        @Override
-        protected void succeeded(Description description) {
-            LOG.info("TestWatcher: Test succeeded: {}", description.getDisplayName());
-        }
-
-        @Override
-        protected void failed(Throwable ex, Description description) {
-            LOG.info("TestWatcher: Test failed: {}", description.getDisplayName(), ex);
-        }
-
-        @Override
-        protected void skipped(AssumptionViolatedException ex, Description description) {
-            LOG.info("TestWatcher: Test skipped: {} ", description.getDisplayName(), ex);
-        }
-    };
-
 }
