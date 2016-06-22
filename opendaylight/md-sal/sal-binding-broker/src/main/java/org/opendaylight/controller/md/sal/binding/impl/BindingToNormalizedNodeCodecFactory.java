cca4068ff1e76b9ec84d3eab661b59a22dc9484a
Tom Pantelis <tpanteli@brocade.com>
2016-06-22 16:01:49 -0400

    Remove global BindingToNormalizedNodeCodec instance
    The BindingToNormalizedNodeCodec was made a global static instance for
    backwards compatibility for CSS users that inject the binding-dom-mapping-service
    identity which defines the provided service as the concrete
    BindingToNormalizedNodeCodec class instead of an interface. Therefore
    the global static instance was created via blueprint and advertised via
    its interfaces and was obtained via the static reference by the
    RuntimeMappingModule for use by CSS users. The RuntimeMappingModule must
    return an instance of BindingToNormalizedNodeCodec in order to provide
    the binding-dom-mapping-service so obtaining the blueprint advertised OSGi
    service via its interfaces and casting to BindingToNormalizedNodeCodec
    failed b/c Aries creates a service proxy which loses the fact that it's a
    BindingToNormalizedNodeCodec instance.
    
    However the global BindingToNormalizedNodeCodec instance is not clean and
    is problematic for supporting blueprint container restarts. Aries supports
    concrete class proxies so I added an additional service export for the
    BindingToNormalizedNodeCodec class in the binding-broker blueprint XML.
    In addition, the blueprint XML now calls a new method, "newInstance", on the
    BindingToNormalizedNodeCodecFactory to create a new instance and calls a
    new method, "registerInstance", to register it with the SchemaService. The
    returned ListenerRegistration instance is put into a bean so it can be
    closed on destroy. The RuntimeMappingModule now obtains the
    BindingToNormalizedNodeCodec instance from the OSGi registry as the other
    blueprint-bridged CSS modules do. This eliminates the need for the global
    instance.
    
    Change-Id: I969ad5470967a81b37078393701c69d1898086cd
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingToNormalizedNodeCodecFactory.java
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingToNormalizedNodeCodecFactory.java
@@ -7,51 +7,59 @@
  */
 package org.opendaylight.controller.md.sal.binding.impl;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.opendaylight.controller.sal.binding.codegen.impl.SingletonHolder;
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.yangtools.binding.data.codec.gen.impl.StreamWriterGenerator;
 import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.sal.binding.generator.api.ClassLoadingStrategy;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
 
 /**
- * Factory class for creating and initializing the global BindingToNormalizedNodeCodec instance.
+ * Factory class for creating and initializing the BindingToNormalizedNodeCodec instances.
  *
  * @author Thomas Pantelis
  */
 public class BindingToNormalizedNodeCodecFactory {
-    private static final AtomicBoolean INSTANCE_CREATED = new AtomicBoolean();
-    private static volatile BindingToNormalizedNodeCodec instance;
-
     /**
-     * Returns the global BindingToNormalizedNodeCodec instance, creating if necessary. The returned instance
-     * is registered with tthe SchemaService as a SchemaContextListener.
+     * This method is deprecated in favor of newInstance/registerInstance.
      *
      * @param classLoadingStrategy
      * @param schemaService
-     * @return the BindingToNormalizedNodeCodec instance
+     * @return BindingToNormalizedNodeCodec instance
      */
+    @Deprecated
     public static BindingToNormalizedNodeCodec getOrCreateInstance(ClassLoadingStrategy classLoadingStrategy,
-            SchemaService schemaService) {
-        if(!INSTANCE_CREATED.compareAndSet(false, true)) {
-            return instance;
-        }
-
+                            SchemaService schemaService) {
         BindingNormalizedNodeCodecRegistry codecRegistry = new BindingNormalizedNodeCodecRegistry(
                 StreamWriterGenerator.create(SingletonHolder.JAVASSIST));
-        BindingToNormalizedNodeCodec localInstance = new BindingToNormalizedNodeCodec(
-                classLoadingStrategy, codecRegistry, true);
-
-        schemaService.registerSchemaContextListener(localInstance);
-
-        // Publish the BindingToNormalizedNodeCodec instance after we've registered it as a
-        // SchemaContextListener to avoid a race condition by publishing it too early when it isn't
-        // fully initialized.
-        instance = localInstance;
+        BindingToNormalizedNodeCodec instance = new BindingToNormalizedNodeCodec(
+                               classLoadingStrategy, codecRegistry, true);
+        schemaService.registerSchemaContextListener(instance);
         return instance;
     }
 
-    public static BindingToNormalizedNodeCodec getInstance() {
-        return instance;
+    /**
+     * Creates a new BindingToNormalizedNodeCodec instance.
+     *
+     * @param classLoadingStrategy
+     * @return the BindingToNormalizedNodeCodec instance
+     */
+    public static BindingToNormalizedNodeCodec newInstance(ClassLoadingStrategy classLoadingStrategy) {
+        BindingNormalizedNodeCodecRegistry codecRegistry = new BindingNormalizedNodeCodecRegistry(
+                StreamWriterGenerator.create(SingletonHolder.JAVASSIST));
+        return new BindingToNormalizedNodeCodec(classLoadingStrategy, codecRegistry, true);
+    }
+
+    /**
+     * Registers the given instance with the SchemaService as a SchemaContextListener.
+     *
+     * @param instance the BindingToNormalizedNodeCodec instance
+     * @param schemaService the SchemaService.
+     * @return the ListenerRegistration
+     */
+    public static ListenerRegistration<SchemaContextListener> registerInstance(BindingToNormalizedNodeCodec instance,
+            SchemaService schemaService) {
+        return schemaService.registerSchemaContextListener(instance);
     }
 }
