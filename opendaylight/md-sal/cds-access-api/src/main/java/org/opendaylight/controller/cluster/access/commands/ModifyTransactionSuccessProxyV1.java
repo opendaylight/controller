e23a6c42a5c4692b44bef20f232c14560c899aac
Robert Varga <rovarga@cisco.com>
2016-06-29 18:08:11 +0200

    WIP: BUG-5280: implement backend message handling
    This patch adds message routing on the backend so messages
    earlier in the patch series get handled correctly.
    
    Change-Id: Ie0ecfc1c8ce3c3b52b5b9c4986dd01444c2a719a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/ModifyTransactionSuccessProxyV1.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+
+/**
+ * Externalizable proxy for use with {@link ModifyTransactionSuccess}. It implements the initial (Boron) serialization
+ * format.
+ *
+ * @author Robert Varga
+ */
+final class ModifyTransactionSuccessProxyV1 extends AbstractTransactionSuccessProxy<ModifyTransactionSuccess> {
+    private static final long serialVersionUID = 1L;
+
+    public ModifyTransactionSuccessProxyV1() {
+        // For Externalizable
+    }
+
+    ModifyTransactionSuccessProxyV1(final ModifyTransactionSuccess success) {
+        super(success);
+    }
+
+    @Override
+    protected ModifyTransactionSuccess createSuccess(final TransactionIdentifier target) {
+        return new ModifyTransactionSuccess(target);
+    }
+}
--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ShardDataTree.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ShardDataTree.java
@@ -133,7 +133,7 @@ public class ShardDataTree extends ShardDataTreeTransactionParent {
         return new LegacyShardDataTreeSnapshot(dataTree.takeSnapshot().readNode(YangInstanceIdentifier.EMPTY).get());
     }
 
-    private ShardDataTreeTransactionChain ensureTransactionChain(final LocalHistoryIdentifier localHistoryIdentifier) {
+    ShardDataTreeTransactionChain ensureTransactionChain(final LocalHistoryIdentifier localHistoryIdentifier) {
         ShardDataTreeTransactionChain chain = transactionChains.get(localHistoryIdentifier);
         if (chain == null) {
             chain = new ShardDataTreeTransactionChain(localHistoryIdentifier, this);
@@ -253,7 +253,7 @@ public class ShardDataTree extends ShardDataTreeTransactionParent {
         final DataTreeModification snapshot = transaction.getSnapshot();
         snapshot.ready();
 
-        return createReadyCohort(transaction.getId(), snapshot);
+        return createReadyCohort(transaction.getIdentifier(), snapshot);
     }
 
     public Optional<NormalizedNode<?, ?>> readNode(final YangInstanceIdentifier path) {
@@ -467,10 +467,11 @@ public class ShardDataTree extends ShardDataTreeTransactionParent {
         cohortRegistry.process(sender, message);
     }
 
+    @Override
     ShardDataTreeCohort createReadyCohort(final TransactionIdentifier txId,
             final DataTreeModification modification) {
-        return new SimpleShardDataTreeCohort(this, modification, txId,
-            cohortRegistry.createCohort(schemaContext, txId, COMMIT_STEP_TIMEOUT));
+        return new SimpleShardDataTreeCohort(this, modification, txId, cohortRegistry.createCohort(schemaContext, txId,
+            COMMIT_STEP_TIMEOUT));
     }
 
     void applyStateFromLeader(final Identifier identifier, final DataTreeCandidateSupplier payload)
