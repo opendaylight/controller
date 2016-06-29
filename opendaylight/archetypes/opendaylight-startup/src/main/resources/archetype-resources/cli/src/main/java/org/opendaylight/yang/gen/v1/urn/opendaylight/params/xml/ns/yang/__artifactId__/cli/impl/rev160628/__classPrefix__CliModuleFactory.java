8c0c830337486d48c82c7e84433cc776c6016e43
Rashmi Pujar <rpujar@inocybe.com>
2016-06-29 16:08:52 -0400

    WIP: Startup archetype for cli bundle
    Change-Id: I19ef17236a25cc84a9ff4b94a990d324386d9b19
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

---
+++ opendaylight/archetypes/opendaylight-startup/src/main/resources/archetype-resources/cli/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/__artifactId__/cli/impl/rev160628/__classPrefix__CliModuleFactory.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Copyright © ${copyrightYear} ${copyright} and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.${artifactId}.cli.impl.rev160628;
+
+public class ${classPrefix}CliModuleFactory extends Abstract${classPrefix}CliModuleFactory {
+
+}
