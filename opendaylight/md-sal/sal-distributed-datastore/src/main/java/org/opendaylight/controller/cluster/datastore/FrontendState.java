48a569f1397144f022dca02cab3498ed1516f0db
Robert Varga <rovarga@cisco.com>
2016-06-29 18:08:11 +0200

    WIP: BUG-5280: implement backend message handling
    This patch adds message routing on the backend so messages
    earlier in the patch series get handled correctly.
    
    Change-Id: Ie0ecfc1c8ce3c3b52b5b9c4986dd01444c2a719a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/FrontendState.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.concurrent.NotThreadSafe;
+import org.opendaylight.controller.cluster.access.commands.OutOfOrderRequestException;
+import org.opendaylight.controller.cluster.access.commands.TransactionRequest;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.RequestException;
+import org.opendaylight.controller.cluster.access.concepts.RequestSuccess;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Frontend state as observed by the shard leader. this class is responsible for tracking generations and sequencing
+ * in the frontend/backend conversation.
+ *
+ * @author Robert Varga
+ */
+@NotThreadSafe
+final class FrontendState {
+    private static final OutOfOrderRequestException UNSEQUENCED_START = new OutOfOrderRequestException(0);
+    private static final Logger LOG = LoggerFactory.getLogger(FrontendState.class);
+
+    private final Map<TransactionIdentifier, FrontendTransaction> transactions = new HashMap<>();
+    private final ClientIdentifier clientId;
+    private final String persistenceId;
+
+    FrontendState(final String persistenceId, final ClientIdentifier clientId) {
+        this.persistenceId = Preconditions.checkNotNull(persistenceId);
+        this.clientId = Preconditions.checkNotNull(clientId);
+    }
+
+    long getGeneration() {
+        return clientId.getGeneration();
+    }
+
+    RequestSuccess<?, ?> handleTransactionRequest(final TransactionRequest<?> request, final long sequence) throws RequestException {
+        final TransactionIdentifier txId = request.getTarget();
+
+        FrontendTransaction tx = transactions.get(txId);
+        if (tx == null) {
+            if (sequence != 0) {
+                LOG.debug("{}: no transaction state present, unexpected request {}", persistenceId, request);
+                throw UNSEQUENCED_START;
+            }
+
+            final LocalHistoryIdentifier historyId = request.getTarget().getHistoryId();
+            if (historyId.getHistoryId() != 0) {
+                // FIXME: lookup local history
+
+
+            } else {
+                // FIXME: initialize to singleton local history
+            }
+
+            tx = new FrontendTransaction();
+            transactions.put(txId, tx);
+        }
+
+        return tx.handleRequest(request, sequence);
+    }
+
+    void retire() {
+        // FIXME: flush all state
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(FrontendState.class).add("clientId", clientId).toString();
+    }
+}
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/FrontendTransaction.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import org.opendaylight.controller.cluster.access.commands.CommitLocalTransactionRequest;
+import org.opendaylight.controller.cluster.access.commands.ExistsTransactionRequest;
+import org.opendaylight.controller.cluster.access.commands.ModifyTransactionRequest;
+import org.opendaylight.controller.cluster.access.commands.OutOfOrderRequestException;
+import org.opendaylight.controller.cluster.access.commands.ReadTransactionRequest;
+import org.opendaylight.controller.cluster.access.commands.TransactionAbortRequest;
+import org.opendaylight.controller.cluster.access.commands.TransactionDoCommitRequest;
+import org.opendaylight.controller.cluster.access.commands.TransactionPreCommitRequest;
+import org.opendaylight.controller.cluster.access.commands.TransactionRequest;
+import org.opendaylight.controller.cluster.access.commands.TransactionSuccess;
+import org.opendaylight.controller.cluster.access.concepts.RequestException;
+import org.opendaylight.controller.cluster.access.concepts.UnsupportedRequestException;
+
+/**
+ * Frontend transaction state as observed by the shard leader.
+ *
+ * @author Robert Varga
+ */
+@NotThreadSafe
+final class FrontendTransaction {
+    private final CohortEntry cohort;
+
+    private long expectedSequence = 0;
+
+    FrontendTransaction() {
+        // FIXME: initialized properly
+        cohort = null;
+    }
+
+    TransactionSuccess<?> handleRequest(final TransactionRequest<?> request, final long sequence) throws RequestException {
+        // Check if this is a request we can handle
+        if (expectedSequence != sequence) {
+            throw new OutOfOrderRequestException(expectedSequence);
+        }
+
+        expectedSequence++;
+
+        if (request instanceof CommitLocalTransactionRequest) {
+
+        } else if (request instanceof ModifyTransactionRequest) {
+
+        } else if (request instanceof ExistsTransactionRequest) {
+
+        } else if (request instanceof ReadTransactionRequest) {
+
+        } else if (request instanceof TransactionPreCommitRequest) {
+
+        } else if (request instanceof TransactionDoCommitRequest) {
+
+        } else if (request instanceof TransactionAbortRequest) {
+
+        } else {
+            throw new UnsupportedRequestException(request);
+        }
+
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
