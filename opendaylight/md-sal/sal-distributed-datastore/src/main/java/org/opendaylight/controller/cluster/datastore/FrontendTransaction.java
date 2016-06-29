e28cda07d7796f40aee19e310c2b90c5febaf216
Robert Varga <rovarga@cisco.com>
2016-06-29 18:08:11 +0200

    WIP: BUG-5280: implement backend message handling
    This patch adds message routing on the backend so messages
    earlier in the patch series get handled correctly.
    
    Change-Id: Ie0ecfc1c8ce3c3b52b5b9c4986dd01444c2a719a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/FrontendTransaction.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * Frontend transaction state as observed by the shard leader.
+ *
+ * @author Robert Varga
+ */
+@NotThreadSafe
+final class FrontendTransaction {
+    final long expectedSequence = 0;
+
+    long getExpectedSequence() {
+        return expectedSequence;
+    }
+
+
+}
