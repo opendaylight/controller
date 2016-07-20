20aa48bd47ee40c96734e3988753f18afb5118f8
Robert Varga <rovarga@cisco.com>
2016-07-20 16:00:19 +0200

    Pass down Operational TreeType
    Methods to create DataTree require explicit tree type,
    add it.
    
    Change-Id: I146b0102d1db33ef7e8b989ce8b755df57352bfc
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/NormalizedNodeAggregator.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/NormalizedNodeAggregator.java
@@ -15,6 +15,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
 import org.opendaylight.yangtools.yang.data.impl.schema.tree.InMemoryDataTreeFactory;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
@@ -28,7 +29,7 @@ public class NormalizedNodeAggregator {
         this.rootIdentifier = rootIdentifier;
         this.nodes = nodes;
         // FIXME: BUG-1014: pass down proper DataTree
-        this.dataTree = InMemoryDataTreeFactory.getInstance().create();
+        this.dataTree = InMemoryDataTreeFactory.getInstance().create(TreeType.OPERATIONAL);
         this.dataTree.setSchemaContext(schemaContext);
     }
 
