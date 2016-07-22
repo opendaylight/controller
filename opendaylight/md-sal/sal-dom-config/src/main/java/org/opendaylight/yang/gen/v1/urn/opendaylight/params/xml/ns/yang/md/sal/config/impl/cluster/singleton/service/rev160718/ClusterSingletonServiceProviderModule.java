7d5f85d9ab98b8961a19c2296c44aea3ea0e5300
Vaclav Demcak <vdemcak@cisco.com>
2016-07-22 12:54:16 +0200

    Add again mdsal-singleton but remove prefix DOM
    We have only one implementation of ClusterSingletonServiceProvider
    (DOM implementation) and we'd like to present it without DOM prefix.
    
    * remove DOM prefix from ConfigSubsystem yang
    * again add the odl-mdsal-singleton-dom feature
    * again add CSSProvider in 06-clustered-entity-ownership.xml file
    
    depends on: https://git.opendaylight.org/gerrit/#/c/42294/
    
    Change-Id: Ieae0d462fe9fa523b2b1b18528759e0614b0225f
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ opendaylight/md-sal/sal-dom-config/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/md/sal/config/impl/cluster/singleton/service/rev160718/ClusterSingletonServiceProviderModule.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.md.sal.config.impl.cluster.singleton.service.rev160718;
+
+import com.google.common.reflect.AbstractInvocationHandler;
+import com.google.common.reflect.Reflection;
+import java.lang.reflect.Method;
+import org.opendaylight.controller.config.api.DependencyResolver;
+import org.opendaylight.controller.config.api.ModuleIdentifier;
+import org.opendaylight.controller.config.api.osgi.WaitingServiceTracker;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @deprecated Replaced by blueprint wiring but remains for backwards compatibility until downstream users
+ *             of the provided config system service are converted to blueprint.
+ */
+@Deprecated
+public class ClusterSingletonServiceProviderModule extends AbstractClusterSingletonServiceProviderModule {
+    private BundleContext bundleContext;
+
+    public ClusterSingletonServiceProviderModule(final ModuleIdentifier identifier, final DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public ClusterSingletonServiceProviderModule(final ModuleIdentifier identifier, final DependencyResolver dependencyResolver,
+            final ClusterSingletonServiceProviderModule oldModule, final java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+
+    @Override
+    public AutoCloseable createInstance() {
+        final WaitingServiceTracker<ClusterSingletonServiceProvider> tracker = WaitingServiceTracker
+                .create(ClusterSingletonServiceProvider.class, bundleContext);
+        final ClusterSingletonServiceProvider service = tracker.waitForService(WaitingServiceTracker.FIVE_MINUTES);
+
+        // Create a proxy to override close to close the ServiceTracker. The actual DOMClusterSingletonServiceProvider
+        // instance will be closed via blueprint.
+        return Reflection.newProxy(AutoCloseableDOMClusterSingletonServiceProvider.class,
+                new AbstractInvocationHandler() {
+                    @Override
+                    protected Object handleInvocation(final Object proxy, final Method method, final Object[] args)
+                            throws Throwable {
+                        if (method.getName().equals("close")) {
+                            tracker.close();
+                            return null;
+                        } else {
+                            return method.invoke(service, args);
+                        }
+                    }
+                });
+    }
+
+    void setBundleContext(final BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    private static interface AutoCloseableDOMClusterSingletonServiceProvider
+            extends ClusterSingletonServiceProvider, AutoCloseable {
+    }
+}
