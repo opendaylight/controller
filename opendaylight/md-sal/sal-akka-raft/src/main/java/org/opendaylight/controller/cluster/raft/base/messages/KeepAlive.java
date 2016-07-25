d6fc55bf1214f010406ea590de515aca9702bd4d
Tom Pantelis <tpanteli@brocade.com>
2016-07-25 19:48:37 -0400

    Add KeepAlive actor
    Added a new KeepAliveActor that is created by the Leader behavior to
    periodically send KeepAlive messages to all followers. This is done
    to alleviate unwanted follower election timeouts that can occur if the
    leader is too busy for some reason and AppendEntries heartbeats are
    delayed. Since AppendEntries heartbeats are sent at the heartbeat
    interval (500 ms default), normally they will prevent election timeouts
    so we don't need the KeepAlive messages sent that frequently so the time
    interval in between KeepAlive messages is one-fifth the election timeout
    (or 4 times the heartbeat interval, by default 2 s).
    
    The KeepAliveActor utilizes the shared read-only PeerInfoCache to obtain
    the peer follower addresses.
    
    Change-Id: Ic58abefc6f6138a336a236a6e99c196b6a54fd50
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/base/messages/KeepAlive.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.base.messages;
+
+import com.google.common.base.Preconditions;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+import javax.annotation.Nonnull;
+
+/**
+ * Message sent by the leader's keep-alive actor to its followers to prevent election time outs.
+ *
+ * @author Thomas Pantelis
+ */
+public class KeepAlive implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final String leaderId;
+
+    public KeepAlive(@Nonnull String leaderId) {
+        this.leaderId = Preconditions.checkNotNull(leaderId);
+    }
+
+    public String getLeaderId() {
+        return leaderId;
+    }
+
+    @Override
+    public String toString() {
+        return "KeepAlive [leaderId=" + leaderId + "]";
+    }
+
+    private Object writeReplace() {
+        return new Proxy(this);
+    }
+
+    private static class Proxy implements Externalizable {
+        private KeepAlive keepAlive;
+
+        public Proxy() {
+        }
+
+        Proxy(KeepAlive keepAlive) {
+            this.keepAlive = keepAlive;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(keepAlive.getLeaderId());
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws ClassNotFoundException, IOException {
+            keepAlive = new KeepAlive((String) in.readObject());
+        }
+
+        @SuppressWarnings("unused")
+        private KeepAlive readResolve() {
+            return keepAlive;
+        }
+    }
+}
