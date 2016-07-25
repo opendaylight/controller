861b8ca73c5a9ead5d827af340dbc08cda175c71
Robert Varga <rovarga@cisco.com>
2016-07-25 20:59:37 +0200

    BUG-5280: expand ShardDataTree to cover transaction mechanics
    A chunk of ShardCommitCoordinator should actually be implemented
    by ShardDataTree. This includes transaction queueing, commit timers,
    interaction with user cohorts and persistence.
    
    This patch implements the relevant operations in an message-agnostic,
    callback-driven way.
    
    Fix: ShardDataTreeTest (missing ShardStat MBean)
    Fix: SimpleShardDataTreeCohort#canCommit (followed by Tom's suggestions)
    
    Change-Id: I353bacce8245df85c5f4d6b4cc0ce5416f2f0337
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/SimpleShardDataTreeCohort.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/SimpleShardDataTreeCohort.java
@@ -8,33 +8,64 @@
 package org.opendaylight.controller.cluster.datastore;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Verify;
+import com.google.common.primitives.UnsignedLong;
+import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
 import org.opendaylight.controller.cluster.datastore.utils.PruningDataTreeModification;
-import org.opendaylight.controller.md.sal.common.api.data.OptimisticLockFailedException;
-import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.ConflictingModificationAppliedException;
+import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateTip;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import akka.dispatch.ExecutionContexts;
+import akka.dispatch.OnComplete;
+import scala.concurrent.Future;
 
-final class SimpleShardDataTreeCohort extends ShardDataTreeCohort {
+final class SimpleShardDataTreeCohort extends ShardDataTreeCohort implements Identifiable<TransactionIdentifier> {
+    enum State {
+        READY,
+        CAN_COMMIT_PENDING,
+        CAN_COMMIT_COMPLETE,
+        PRE_COMMIT_PENDING,
+        PRE_COMMIT_COMPLETE,
+        COMMIT_PENDING,
+
+        ABORTED,
+        COMMITTED,
+        FAILED,
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(SimpleShardDataTreeCohort.class);
-    private static final ListenableFuture<Boolean> TRUE_FUTURE = Futures.immediateFuture(Boolean.TRUE);
     private static final ListenableFuture<Void> VOID_FUTURE = Futures.immediateFuture(null);
     private final DataTreeModification transaction;
     private final ShardDataTree dataTree;
     private final TransactionIdentifier transactionId;
+    private final CompositeDataTreeCohort userCohorts;
+
+    private State state = State.READY;
     private DataTreeCandidateTip candidate;
+    private FutureCallback<?> callback;
+    private Exception nextFailure;
 
     SimpleShardDataTreeCohort(final ShardDataTree dataTree, final DataTreeModification transaction,
-            final TransactionIdentifier transactionId) {
+            final TransactionIdentifier transactionId, final CompositeDataTreeCohort userCohorts) {
         this.dataTree = Preconditions.checkNotNull(dataTree);
         this.transaction = Preconditions.checkNotNull(transaction);
         this.transactionId = Preconditions.checkNotNull(transactionId);
+        this.userCohorts = Preconditions.checkNotNull(userCohorts);
+    }
+
+    @Override
+    public TransactionIdentifier getIdentifier() {
+        return transactionId;
     }
 
     @Override
@@ -43,80 +74,157 @@ final class SimpleShardDataTreeCohort extends ShardDataTreeCohort {
     }
 
     @Override
-    public ListenableFuture<Boolean> canCommit() {
-        DataTreeModification modification = getDataTreeModification();
-        try {
-            dataTree.getDataTree().validate(modification);
-            LOG.trace("Transaction {} validated", transaction);
-            return TRUE_FUTURE;
-        }
-        catch (ConflictingModificationAppliedException e) {
-            LOG.warn("Store Tx {}: Conflicting modification for path {}.", transactionId, e.getPath());
-            return Futures.immediateFailedFuture(new OptimisticLockFailedException("Optimistic lock failed.", e));
-        } catch (DataValidationFailedException e) {
-            LOG.warn("Store Tx {}: Data validation failed for path {}.", transactionId, e.getPath(), e);
-
-            // For debugging purposes, allow dumping of the modification. Coupled with the above
-            // precondition log, it should allow us to understand what went on.
-            LOG.debug("Store Tx {}: modifications: {} tree: {}", transactionId, modification, dataTree.getDataTree());
-
-            return Futures.immediateFailedFuture(new TransactionCommitFailedException("Data did not pass validation.", e));
-        } catch (Exception e) {
-            LOG.warn("Unexpected failure in validation phase", e);
-            return Futures.immediateFailedFuture(e);
-        }
-    }
-
-    @Override
-    public ListenableFuture<Void> preCommit() {
-        try {
-            candidate = dataTree.getDataTree().prepare(getDataTreeModification());
-            /*
-             * FIXME: this is the place where we should be interacting with persistence, specifically by invoking
-             *        persist on the candidate (which gives us a Future).
-             */
-            LOG.trace("Transaction {} prepared candidate {}", transaction, candidate);
-            return VOID_FUTURE;
-        } catch (Exception e) {
-            if(LOG.isTraceEnabled()) {
-                LOG.trace("Transaction {} failed to prepare", transaction, e);
-            } else {
-                LOG.error("Transaction failed to prepare", e);
-            }
-            return Futures.immediateFailedFuture(e);
-        }
-    }
-
-    @Override
     DataTreeModification getDataTreeModification() {
         DataTreeModification dataTreeModification = transaction;
-        if(transaction instanceof PruningDataTreeModification){
+        if (transaction instanceof PruningDataTreeModification){
             dataTreeModification = ((PruningDataTreeModification) transaction).getResultingModification();
         }
         return dataTreeModification;
     }
 
     @Override
-    public ListenableFuture<Void> abort() {
-        // No-op, really
-        return VOID_FUTURE;
+    public void canCommit(final FutureCallback<Void> callback) {
+        if(state == State.CAN_COMMIT_PENDING) {
+            return;
+        }
+
+        Preconditions.checkState(state == State.READY);
+        this.callback = Preconditions.checkNotNull(callback);
+        state = State.CAN_COMMIT_PENDING;
+        dataTree.startCanCommit(this);
     }
 
     @Override
-    public ListenableFuture<Void> commit() {
-        try {
-            dataTree.getDataTree().commit(candidate);
-        } catch (Exception e) {
-            if(LOG.isTraceEnabled()) {
-                LOG.trace("Transaction {} failed to commit", transaction, e);
-            } else {
-                LOG.error("Transaction failed to commit", e);
-            }
-            return Futures.immediateFailedFuture(e);
+    public void preCommit(final FutureCallback<DataTreeCandidate> callback) {
+        Preconditions.checkState(state == State.CAN_COMMIT_COMPLETE);
+        this.callback = Preconditions.checkNotNull(callback);
+        state = State.PRE_COMMIT_PENDING;
+
+        if (nextFailure == null) {
+            dataTree.startPreCommit(this);
+        } else {
+            failedPreCommit(nextFailure);
+        }
+    }
+
+    @Override
+    public ListenableFuture<Void> abort() {
+        if (state == State.READY) {
+            LOG.debug("Ready transaction {} aborted before commit started", transactionId);
+            return VOID_FUTURE;
         }
 
-        LOG.trace("Transaction {} committed, proceeding to notify", transaction);
-        dataTree.notifyListeners(candidate);
-        return VOID_FUTURE;
+        dataTree.startAbort(this);
+        state = State.ABORTED;
+
+        final Optional<Future<Iterable<Object>>> maybeAborts = userCohorts.abort();
+        if (!maybeAborts.isPresent()) {
+            return VOID_FUTURE;
+        }
+
+        final Future<Iterable<Object>> aborts = maybeAborts.get();
+        if (aborts.isCompleted()) {
+            return VOID_FUTURE;
+        }
+
+        final SettableFuture<Void> ret = SettableFuture.create();
+        aborts.onComplete(new OnComplete<Iterable<Object>>() {
+            @Override
+            public void onComplete(final Throwable arg0, final Iterable<Object> arg1) {
+                if (arg0 != null) {
+                    ret.setException(arg0);
+                } else {
+                    ret.set(null);
+                }
+            }
+        }, ExecutionContexts.global());
+
+        return ret;
+    }
+
+    @Override
+    public void commit(final FutureCallback<UnsignedLong> callback) {
+        Preconditions.checkState(state == State.PRE_COMMIT_COMPLETE);
+        this.callback = Preconditions.checkNotNull(callback);
+        state = State.COMMIT_PENDING;
+        dataTree.startCommit(this, candidate);
+    }
+
+    private <T> FutureCallback<T> switchState(final State newState) {
+        @SuppressWarnings("unchecked")
+        final FutureCallback<T> ret = (FutureCallback<T>) this.callback;
+        this.callback = null;
+        LOG.debug("Transaction {} changing state from {} to {}", transactionId, state, newState);
+        this.state = newState;
+        return ret;
+    }
+
+    void successfulCanCommit() {
+        switchState(State.CAN_COMMIT_COMPLETE).onSuccess(null);
+    }
+
+    void failedCanCommit(final Exception cause) {
+        switchState(State.FAILED).onFailure(cause);
+    }
+
+    /**
+     * Run user-defined canCommit and preCommit hooks. We want to run these before we initiate persistence so that
+     * any failure to validate is propagated before we record the transaction.
+     *
+     * @param candidate {@link DataTreeCandidate} under consideration
+     * @throws ExecutionException
+     * @throws TimeoutException
+     */
+    // FIXME: this should be asynchronous
+    void userPreCommit(final DataTreeCandidate candidate) throws ExecutionException, TimeoutException {
+        userCohorts.canCommit(candidate);
+        userCohorts.preCommit();
+    }
+
+    void successfulPreCommit(final DataTreeCandidateTip candidate) {
+        LOG.trace("Transaction {} prepared candidate {}", transaction, candidate);
+        this.candidate = Verify.verifyNotNull(candidate);
+        switchState(State.PRE_COMMIT_COMPLETE).onSuccess(candidate);
+    }
+
+    void failedPreCommit(final Exception cause) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Transaction {} failed to prepare", transaction, cause);
+        } else {
+            LOG.error("Transaction failed to prepare", cause);
+        }
+
+        userCohorts.abort();
+        switchState(State.FAILED).onFailure(cause);
+    }
+
+    void successfulCommit(final UnsignedLong journalIndex) {
+        try {
+            userCohorts.commit();
+        } catch (TimeoutException | ExecutionException e) {
+            // We are probably dead, depending on what the cohorts end up doing
+            LOG.error("User cohorts failed to commit", e);
+        }
+
+        switchState(State.COMMITTED).onSuccess(journalIndex);
+    }
+
+    void failedCommit(final Exception cause) {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Transaction {} failed to commit", transaction, cause);
+        } else {
+            LOG.error("Transaction failed to commit", cause);
+        }
+
+        userCohorts.abort();
+        switchState(State.FAILED).onFailure(cause);
+    }
+
+    State getState() {
+        return state;
+    }
+
+    void reportFailure(final Exception cause) {
+        this.nextFailure = Preconditions.checkNotNull(cause);
     }
 }
