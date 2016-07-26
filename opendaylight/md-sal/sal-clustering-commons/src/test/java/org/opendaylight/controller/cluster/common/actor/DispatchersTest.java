9d5117a33e15c6c0fbfc5ecbe5d85c49b842b37e
Tom Pantelis <tpanteli@brocade.com>
2016-07-26 01:52:22 -0400

    Move Dispatchers to sal-clustering-commons
    So it can be used in sal-akka-raft.
    
    Change-Id: I7d567c80336d3f3bd7232a8f891c8114692d7d2b
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/cluster/common/actor/DispatchersTest.java
+++ opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/cluster/common/actor/DispatchersTest.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.controller.cluster.datastore.utils;
+package org.opendaylight.controller.cluster.common.actor;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
