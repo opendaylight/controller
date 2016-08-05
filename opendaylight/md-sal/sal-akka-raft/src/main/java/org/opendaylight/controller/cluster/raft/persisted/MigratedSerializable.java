048f23855f49625cf28728e2b1bd9146ef4b2993
Robert Varga <rovarga@cisco.com>
2016-08-05 19:44:14 +0200

    Move ServerConfigurationPayload to cluster.raft.persisted
    This introduces its mirror copy and modifies the old class
    so that it readResolve()s to the new class. It also adjusts
    all users to use the new class.
    
    The new class uses Externalizable proxy pattern to allow the
    class itself be evolved without breaking compatibility. Also
    NoOpPayload is retrofitted this way, which makes all subclasses
    of Payload not have their serialization format tied to Payload
    itself.
    
    Change-Id: I26010a9e1438dbc4cb1822e1c4dbb51e2b6e538e
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    (cherry picked from commit 9d5ec5cdd146a56bc03e35b6718e9492a5c8410a)

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/persisted/MigratedSerializable.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.persisted;
+
+import java.io.Serializable;
+import javax.annotation.Nonnull;
+
+/**
+ * Transitional marker interface for use with evolution of serializable classes held in persistence. This interface
+ * should be implemented by replacement classes for as long as the old classes are supported.
+ *
+ * @author Robert Varga
+ */
+public interface MigratedSerializable extends Serializable {
+    /**
+     * Return true if this object was created from a previous serialization format.
+     *
+     * @return true if this object was created from a previous serialization format, false otherwise.
+     */
+    boolean isMigrated();
+
+    /**
+     * Return a serialization proxy. Implementations should not return this object, but rather use Externalizable
+     * proxy pattern so their serialization format is not tied to this interface.
+     *
+     * @return Serialization proxy.
+     */
+    @Nonnull Object writeReplace();
+}
---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/persisted/ServerConfigurationPayload.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.persisted;
+
+import com.google.common.collect.ImmutableList;
+import java.io.ByteArrayOutputStream;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.raft.protobuff.client.messages.Payload;
+import org.opendaylight.controller.cluster.raft.protobuff.client.messages.PersistentPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Payload data for server configuration log entries.
+ *
+ * @author Thomas Pantelis
+ */
+public final class ServerConfigurationPayload extends Payload implements PersistentPayload, MigratedSerializable {
+    private static final class Proxy implements Externalizable {
+        private static final long serialVersionUID = 1L;
+
+        private List<ServerInfo> serverConfig;
+
+        public Proxy() {
+            // For Externalizable
+        }
+
+        Proxy(final ServerConfigurationPayload payload) {
+            this.serverConfig = payload.getServerConfig();
+        }
+
+        @Override
+        public void writeExternal(final ObjectOutput out) throws IOException {
+            out.writeInt(serverConfig.size());
+            for (ServerInfo i : serverConfig) {
+                out.writeObject(i.getId());
+                out.writeBoolean(i.isVoting());
+            }
+         }
+
+        @Override
+        public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+            final int size = in.readInt();
+            serverConfig = new ArrayList<>(size);
+            for (int i = 0; i < size; ++i) {
+                final String id = (String) in.readObject();
+                final boolean voting = in.readBoolean();
+                serverConfig.add(new ServerInfo(id, voting));
+            }
+        }
+
+        private Object readResolve() {
+            return new ServerConfigurationPayload(serverConfig);
+        }
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServerConfigurationPayload.class);
+    private static final long serialVersionUID = 1L;
+
+    private final List<ServerInfo> serverConfig;
+    private final boolean migrated;
+    private int serializedSize = -1;
+
+    private ServerConfigurationPayload(final @Nonnull List<ServerInfo> serverConfig, boolean migrated) {
+        this.serverConfig = ImmutableList.copyOf(serverConfig);
+        this.migrated = migrated;
+    }
+
+    public ServerConfigurationPayload(final @Nonnull List<ServerInfo> serverConfig) {
+        this(serverConfig, false);
+    }
+
+    @Deprecated
+    public static ServerConfigurationPayload createMigrated(final @Nonnull List<ServerInfo> serverConfig) {
+        return new ServerConfigurationPayload(serverConfig, true);
+    }
+
+    @Deprecated
+    @Override
+    public boolean isMigrated() {
+        return migrated;
+    }
+
+    public @Nonnull List<ServerInfo> getServerConfig() {
+        return serverConfig;
+    }
+
+    @Override
+    public int size() {
+        if (serializedSize < 0) {
+            try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+                try (ObjectOutputStream out = new ObjectOutputStream(bos)) {
+                    out.writeObject(writeReplace());
+                }
+
+                serializedSize = bos.toByteArray().length;
+            } catch (IOException e) {
+                serializedSize = 0;
+                LOG.error("Error serializing", e);
+            }
+        }
+
+        return serializedSize;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerConfigurationPayload [serverConfig=" + serverConfig + "]";
+    }
+
+    @Override
+    public Object writeReplace() {
+        return new Proxy(this);
+    }
+}
