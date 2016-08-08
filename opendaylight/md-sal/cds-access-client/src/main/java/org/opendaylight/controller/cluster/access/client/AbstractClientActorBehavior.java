efa4a2c24edaadbe1e1b95f00c80c180d2d9d7a3
Robert Varga <rovarga@cisco.com>
2016-08-08 15:54:20 +0200

    BUG-5280: split out cds akka client substrate
    This patch splits out the baseline frontend client
    into a separate package.
    
    Change-Id: I2d8ca8b81f29a45dd8c30f3bef467fcda94d4887
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActorBehavior.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActorBehavior.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.cluster.datastore.actors.client;
+package org.opendaylight.controller.cluster.access.client;
 
 import akka.actor.ActorRef;
 import com.google.common.annotations.Beta;
