d4131438ba4091a8268607776358fdbc2404094d
Michael Vorburger <vorburger@redhat.com>
2016-08-16 21:37:20 +0200

    New test utility AssertDataObjects
    Change-Id: Ic332783e10faf75df4172c8acf32352e3af7ccfb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/XtendYangBeanGenerator.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * {@link XtendBeanGenerator} customized for YANG beans stored in MD SAL
+ * DataBroker.
+ *
+ * This is required (a) because YANG model DataObject beans (when read from a
+ * DataBroker) are funky java.lang.reflect.Proxy instances, and
+ * XtendBeanGenerator cannot find the Builder for them without a bit of help,
+ * which this class provides; and (b) to integrate it with the
+ * XtendBuilderExtensions (for ">>" instead of "->" and no build() method calls).
+ *
+ * @see XtendBeanGenerator
+ *
+ * @author Michael Vorburger
+ */
+// package-local: no need to expose this, consider it an implementation detail; public API is the AssertDataObjects
+class XtendYangBeanGenerator extends XtendBeanGenerator {
+
+    private boolean useBuilderExtensions(Object bean) {
+        return bean instanceof DataObject;
+    }
+
+    @Override
+    public String getExpression(Object bean) {
+        if (useBuilderExtensions(bean)) {
+            return new StringBuilder("import static extension ").append(XtendBuilderExtensions.class.getName())
+                    .append(".operator_doubleGreaterThan\n\n").append(super.getExpression(bean)).toString();
+        } else {
+            return super.getExpression(bean);
+        }
+    }
+
+    @Override
+    protected boolean isUsingBuilder(Object bean, Class<?> builderClass) {
+        if (useBuilderExtensions(bean)) {
+            return false;
+        } else {
+            return super.isUsingBuilder(bean, builderClass);
+        }
+    }
+
+    @Override
+    protected String getOperator(Object bean, Class<?> builderClass) {
+        if (useBuilderExtensions(bean)) {
+            return ">>";
+        } else {
+            return super.getOperator(bean, builderClass);
+        }
+    }
+
+    @Override
+    protected CharSequence getNewBeanExpression(Object bean) {
+        if (bean instanceof DataContainer) {
+            DataContainer dataContainerBean = (DataContainer) bean;
+            Class<?> builderClass = getBuilderClassByAppendingBuilderToClassName(
+                    dataContainerBean.getImplementedInterface());
+            return super.getNewBeanExpression(dataContainerBean, builderClass);
+        } else {
+            return super.getNewBeanExpression(bean);
+        }
+    }
+
+}
---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/tests/AssertDataObjectsTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans.tests;
+
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.OPERATIONAL;
+
+import com.google.common.base.Optional;
+import java.util.Map;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.controller.md.sal.binding.test.xtendbeans.AssertDataObjects;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.list.rev140701.Top;
+import org.opendaylight.yangtools.yang.binding.ChildOf;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.DataRoot;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Tests the {@link AssertDataObjects} utility.
+ *
+ * @author Michael Vorburger
+ */
+public class AssertDataObjectsTest extends AbstractDataBrokerTest {
+
+    private static final String HEADER = "import static extension org.opendaylight.controller.md.sal.binding.test.xtendbeans.XtendBuilderExtensions.operator_doubleGreaterThan\n\n";
+
+    private DataBroker dataBroker;
+
+    @Test
+    public void testAssertDataObjectsWithTop() {
+        AssertDataObjects.assertEqualByText(HEADER + "new TopBuilder\n", ExampleYangObjects.topEmpty().getValue());
+    }
+
+    @Test
+    public void testAssertDataObjectsWithTopLevelList() {
+        AssertDataObjects.assertEqualByText(HEADER + "new TopLevelListBuilder\n", ExampleYangObjects.topLevelList().getValue());
+    }
+
+    @Test
+    public void testAssertDataObjectsWithDataBroker() throws TransactionCommitFailedException, ReadFailedException {
+        dataBroker = getDataBroker();
+
+        WriteTransaction initialTx = getDataBroker().newWriteOnlyTransaction();
+        put(initialTx, OPERATIONAL, ExampleYangObjects.topEmpty());
+        put(initialTx, OPERATIONAL, ExampleYangObjects.topLevelList());
+        initialTx.submit().checkedGet();
+
+        // TODO How-to read DataRoot OpendaylightMdsalListTestData instead of Top
+        // assertEqualDataContainers(null, read(OPERATIONAL, Top.class));
+
+        AssertDataObjects.assertEqualByText("import static extension org.opendaylight.controller.md.sal.binding.test.xtendbeans.XtendBuilderExtensions.operator_doubleGreaterThan\n\n"
+                + "new TopBuilder >> [\n    topLevelList += #[\n        new TopLevelListBuilder\n    ]\n]", read(OPERATIONAL, Top.class));
+    }
+
+    <T extends DataObject> void put(WriteTransaction tx, LogicalDatastoreType store, Map.Entry<InstanceIdentifier<T>, T> obj) {
+        tx.put(OPERATIONAL, obj.getKey(), obj.getValue());
+    }
+
+    // TODO MOVE THIS CODE INTO .. where? (No DataBrokerSnapshoter, for now.)
+    <T extends ChildOf<? extends DataRoot>>
+        T read(LogicalDatastoreType store, final Class<T> container) throws ReadFailedException {
+
+        InstanceIdentifier<T> containerInstanceIdentifier = InstanceIdentifier.builder(container).build();
+        try ( ReadOnlyTransaction tx = dataBroker.newReadOnlyTransaction() ) {
+            Optional<T> optional = tx.read(store, containerInstanceIdentifier).checkedGet();
+            if (optional.isPresent()) {
+                return optional.get();
+            } else {
+                throw new ReadFailedException("Nothing in the " + store
+                        + " datastore at: " + containerInstanceIdentifier);
+            }
+        }
+    }
+
+}
