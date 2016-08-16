0f08220f93cf1ae06d711f68afaa44d4aaf0f95d
Michael Vorburger <vorburger@redhat.com>
2016-08-16 21:37:20 +0200

    New test utility AssertDataObjects
    Change-Id: Ic332783e10faf75df4172c8acf32352e3af7ccfb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/AugmentableExtension.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import org.eclipse.xtext.xbase.lib.util.ReflectExtensions;
+import org.opendaylight.yangtools.binding.data.codec.util.AugmentationReader;
+import org.opendaylight.yangtools.yang.binding.Augmentable;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+
+/**
+ * Adds an {@link #getAugmentations(Augmentable)} method to {@link Augmentable}.
+ *
+ * <p>Note that the generated *Impl classes in the *Builder do not implement
+ * {@link AugmentationReader}, only the {@link LazyDataObject} does.
+ *
+ * @see Augmentable
+ * @see AugmentationReader
+ *
+ * @author Michael Vorburger
+ */
+public class AugmentableExtension {
+
+    private static final ReflectExtensions REFLECT_EXTENSIONS = new ReflectExtensions();
+
+/*
+    public <E extends Augmentation<?>>
+    Map<Class<? extends Augmentation<E>>, Augmentation<E>>
+        getAugmentations(Augmentable<?> augmentable) {
+*/
+    public Map<Class<? extends Augmentation<?>>, Augmentation<?>> getAugmentations(Augmentable<?> augmentable) {
+        if (augmentable instanceof AugmentationReader) {
+            AugmentationReader augmentationReader = (AugmentationReader) augmentable;
+            return augmentationReader.getAugmentations(augmentable);
+        } else if (Proxy.isProxyClass(augmentable.getClass())) {
+            InvocationHandler invocationHandler = Proxy.getInvocationHandler(augmentable);
+            // class LazyDataObject implements InvocationHandler, AugmentationReader
+            AugmentationReader augmentationReader = (AugmentationReader) invocationHandler;
+            return augmentationReader.getAugmentations(augmentable);
+        } else {
+            try {
+                return REFLECT_EXTENSIONS.get(augmentable, "augmentation");
+            } catch (ClassCastException | SecurityException | NoSuchFieldException | IllegalArgumentException
+                    | IllegalAccessException e) {
+                throw new IllegalArgumentException("TODO Implement getAugmentations() for an Augmentable which "
+                        + "is neither a (Proxy of an) AugmentationReader nor has an internal field named 'augmentation': "
+                        + augmentable.getClass(), e);
+            }
+        }
+    }
+
+}
---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/tests/AssertDataObjectsTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans.tests;
+
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.OPERATIONAL;
+
+import java.util.Map;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.controller.md.sal.binding.test.tests.DataBrokerUtils;
+import org.opendaylight.controller.md.sal.binding.test.xtendbeans.AssertDataObjects;
+import org.opendaylight.controller.md.sal.binding.test.xtendbeans.AugmentableExtension;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.list.rev140701.Top;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Tests the {@link AssertDataObjects} utility.
+ *
+ * @author Michael Vorburger
+ */
+public class AssertDataObjectsTest extends AbstractDataBrokerTest {
+
+    private static final String HEADER = "import static extension org.opendaylight.controller.md.sal.binding.test.xtendbeans.XtendBuilderExtensions.operator_doubleGreaterThan\n\n";
+
+    private DataBroker dataBroker;
+    private DataBrokerUtils dataBrokerUtils;
+
+    @Test
+    public void testAssertDataObjectsWithTop() {
+        AssertDataObjects.assertEqualByText(HEADER + "new TopBuilder\n", ExampleYangObjects.topEmpty().getValue());
+    }
+
+    @Test
+    public void testAssertDataObjectsWithTopLevelList() {
+        AssertDataObjects.assertEqualByText(HEADER + "new TopLevelListBuilder\n", ExampleYangObjects.topLevelList().getValue());
+    }
+
+    @Test
+    public void testAssertDataObjectsWithDataBroker() throws Exception {
+        dataBroker = getDataBroker();
+        dataBrokerUtils = new DataBrokerUtils(dataBroker);
+
+        WriteTransaction initialTx = getDataBroker().newWriteOnlyTransaction();
+        put(initialTx, OPERATIONAL, ExampleYangObjects.topEmpty());
+        put(initialTx, OPERATIONAL, ExampleYangObjects.topLevelList());
+        initialTx.submit().checkedGet();
+
+        // TODO How-to read DataRoot OpendaylightMdsalListTestData instead of Top
+        // assertEqualDataContainers(null, read(OPERATIONAL, Top.class));
+
+        Top actualTop = dataBrokerUtils.read(OPERATIONAL, Top.class);
+
+        // TODO remove this block
+        System.out.println(actualTop);
+        System.out.println(new AugmentableExtension().getAugmentations(ExampleYangObjects.topLevelList().getValue()));
+        System.out.println(new AugmentableExtension().getAugmentations(actualTop));
+        System.out.println(new AugmentableExtension().getAugmentations(actualTop.getTopLevelList().get(0)));
+
+        String expectedTopText = "import static extension org.opendaylight.controller.md.sal.binding.test.xtendbeans.XtendBuilderExtensions.operator_doubleGreaterThan\n\n"
+                + "new TopBuilder >> [\n"
+                + "    topLevelList = #[\n"
+                + "        new TopLevelListBuilder >> [\n"
+                + "            name = \"foo\"\n"
+                + "            containerWithUses = new ContainerWithUsesBuilder >> [\n"
+                + "                leafFromGrouping = \"foo\"\n"
+                + "            ]\n"
+                + "        ]\n"
+                + "    ]\n]";
+        AssertDataObjects.assertEqualByText(expectedTopText, actualTop);
+    }
+
+    <T extends DataObject> void put(WriteTransaction tx, LogicalDatastoreType store, Map.Entry<InstanceIdentifier<T>, T> obj) {
+        tx.put(OPERATIONAL, obj.getKey(), obj.getValue());
+    }
+
+}
