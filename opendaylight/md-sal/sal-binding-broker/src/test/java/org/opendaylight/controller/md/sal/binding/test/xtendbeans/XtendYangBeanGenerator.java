b0e9cef9cb24ee385299e4d14c6a79c893bb6932
Michael Vorburger <vorburger@redhat.com>
2016-08-16 21:37:20 +0200

    New test utility AssertDataObjects
    This adapts https://github.com/vorburger/xtendbeans to OpenDaylight.
    
    That library can be used as-is for "normal" Java objects, such as Flow
    beans, as seen e.g. in https://git.opendaylight.org/gerrit/#/c/42109/.
    
    This change make it possible to use that library to compare actual
    DataObject obtained from the DataBroker with expected objects in tests,
    as illustrated e.g. in https://git.opendaylight.org/gerrit/#/c/44000/
    (Those objects have a number of peculiarities which need adaption,
    provided by the classes in this change.)
    
    Change-Id: Ic332783e10faf75df4172c8acf32352e3af7ccfb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/XtendYangBeanGenerator.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans;
+
+import ch.vorburger.xtendbeans.XtendBeanGenerator;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * {@link XtendBeanGenerator} customized for YANG beans stored in MD SAL
+ * DataBroker.
+ *
+ * This is required: (a) because YANG model DataObject beans (when read from a
+ * DataBroker) are funky java.lang.reflect.Proxy instances, and XtendBeanGenerator
+ * cannot find the Builder or the property getters for them without a bit of help,
+ * which this class provides;
+ *
+ * (b) to integrate it with the {@link XtendBuilderExtensions}
+ * (for ">>" instead of "->" and no build() method calls);
+ *
+ * (c) to integrate it with the {@link AugmentableExtension}.
+ *
+ * @see XtendBeanGenerator
+ *
+ * @author Michael Vorburger
+ */
+// package-local: no need to expose this, consider it an implementation detail; public API is the AssertDataObjects
+class XtendYangBeanGenerator extends XtendBeanGenerator {
+
+    private boolean useBuilderExtensions(Object bean) {
+        return bean instanceof DataObject;
+    }
+
+    @Override
+    public String getExpression(Object bean) {
+        final String beanText = super.getExpression(bean);
+        if (useBuilderExtensions(bean)) {
+            return new StringBuilder("import static extension ").append(XtendBuilderExtensions.class.getName())
+                    .append(".operator_doubleGreaterThan\n\n").append(beanText).toString();
+        } else {
+            return beanText;
+        }
+    }
+
+    @Override
+    protected boolean isUsingBuilder(Object bean, Class<?> builderClass) {
+        if (useBuilderExtensions(bean)) {
+            return false;
+        } else {
+            return super.isUsingBuilder(bean, builderClass);
+        }
+    }
+
+    @Override
+    protected String getOperator(Object bean, Class<?> builderClass) {
+        if (useBuilderExtensions(bean)) {
+            return ">>";
+        } else {
+            return super.getOperator(bean, builderClass);
+        }
+    }
+
+    @Override
+    protected CharSequence getNewBeanExpression(Object bean) {
+        if (bean instanceof DataContainer) {
+            DataContainer dataContainerBean = (DataContainer) bean;
+            Class<?> builderClass = getBuilderClassByAppendingBuilderToClassName(
+                    dataContainerBean.getImplementedInterface());
+            return super.getNewBeanExpression(dataContainerBean, builderClass);
+        } else {
+            return super.getNewBeanExpression(bean);
+        }
+    }
+
+    @Override
+    protected String stringify(Class<?> aClass) {
+        return aClass.getSimpleName();
+    }
+
+}
