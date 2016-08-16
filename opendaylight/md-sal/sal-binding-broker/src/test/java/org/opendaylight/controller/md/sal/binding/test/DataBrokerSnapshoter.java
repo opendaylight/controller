58efb17ab3343d655193a54482165c5de6ccaca0
Michael Vorburger <vorburger@redhat.com>
2016-08-16 21:37:20 +0200

    DataBrokerSnapshoter test utility
    Change-Id: Ic332783e10faf75df4172c8acf32352e3af7ccfb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/DataBrokerSnapshoter.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+
+/**
+ * Creates DataBrokerSnapshot instances from the current state content of a DataBroker.
+ *
+ * @author Michael Vorburger
+ */
+public class DataBrokerSnapshoter {
+
+    public DataBrokerSnapshoter(DataBroker dataBroker) {
+    }
+
+    public DataBrokerSnapshot snapshot() {
+        return null;
+    }
+
+    // public void restore(DataBrokerSnapshot snapshot)
+
+    // public void clear()
+}
