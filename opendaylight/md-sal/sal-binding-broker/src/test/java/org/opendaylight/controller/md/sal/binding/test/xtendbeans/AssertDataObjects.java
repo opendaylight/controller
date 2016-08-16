cf7089fe42b55ff9a8f948366ff17381891fca68
Michael Vorburger <vorburger@redhat.com>
2016-08-16 21:37:20 +0200

    New test utility AssertDataObjects
    This adapts https://github.com/vorburger/xtendbeans to OpenDaylight.
    
    That library can be used as-is for "normal" Java objects, such as Flow
    beans, as seen e.g. in https://git.opendaylight.org/gerrit/#/c/42109/.
    
    This change make it possible to use that library to compare actual
    DataObject obtained from the DataBroker with expected objects in tests,
    as illustrated e.g. in https://git.opendaylight.org/gerrit/#/c/44000/
    (Those objects have a number of peculiarities which need adaption,
    provided by the classes in this change.)
    
    Change-Id: Ic332783e10faf75df4172c8acf32352e3af7ccfb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/sal-binding-broker/src/test/java/org/opendaylight/controller/md/sal/binding/test/xtendbeans/AssertDataObjects.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.test.xtendbeans;
+
+import ch.vorburger.xtendbeans.AssertBeans;
+import org.junit.ComparisonFailure;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * Assert equals utility for YANG {@link DataObject}s.
+ *
+ * @see AssertBeans
+ *
+ * @author Michael Vorburger
+ */
+public final class AssertDataObjects {
+
+    private static final XtendYangBeanGenerator generator = new XtendYangBeanGenerator();
+
+    private AssertDataObjects() {
+    }
+
+    /**
+     * Assert that an actual YANG DataObject (DataContainer) is equals to an expected one.
+     *
+     * The argument types are intentionally of type Object instead of YANG DataContainer or DataObject.
+     * This is important so that this can be directly used on e.g. a List or Map etc. of DataObjects.
+     *
+     * @param expected expected bean data structure, including YANG
+     * @param actual the bean data structure to check against <code>expected</code>
+     *
+     * @see AssertBeans#assertEqualBeans(Object, Object)
+     */
+    public static void assertEqualBeans(Object expected, Object actual) throws ComparisonFailure {
+        String expectedText = generator.getExpression(expected);
+        assertEqualByText(expectedText, actual);
+    }
+
+    public static void assertEqualByText(String expectedText, Object actual) throws ComparisonFailure {
+        String actualText = generator.getExpression(actual);
+        if (!expectedText.equals(actualText)) {
+            throw new ComparisonFailure("Expected and actual beans do not match", expectedText, actualText);
+        }
+    }
+
+}
