4c4bd776977c3d39b5582018fb6ea0a40ca46644
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-08-23 19:27:01 -0400

    Add blueprint wiring for xsql
    Also:
    - fix package name definition to match org.opendaylight.controller
    instead of org.odl.controller
    - squash cli bundle within the implementation bundle
    - remove config system bundle
    
    Change-Id: I0fcfad2f13da13edfb4bea955ae9312319046bda
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ opendaylight/md-sal/sal-dom-xsql/src/main/java/org/opendaylight/xsql/XSQLModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.xsql;
+
+import org.opendaylight.controller.md.sal.dom.xsql.XSQLAdapter;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+
+/**
+ * @author Sharon Aicler(saichler@gmail.com)
+ **/
+public class XSQLModule {
+    private static final long SLEEP_TIME_BEFORE_CREATING_TRANSACTION = 10000;
+
+    public XSQLModule(DataBroker dataBroker, DOMDataBroker domDataBroker, SchemaService schemaService) {
+        XSQLAdapter xsqlAdapter = XSQLAdapter.getInstance();
+        schemaService.registerSchemaContextListener(xsqlAdapter);
+        xsqlAdapter.setDataBroker(domDataBroker);
+        final XSQLProvider p = new XSQLProvider();
+        new Runnable() {
+            @Override
+            public void run() {
+                try{Thread.sleep(SLEEP_TIME_BEFORE_CREATING_TRANSACTION);}catch(Exception err){}
+                p.buildXSQL(dataBroker);
+            }
+        };
+    }
+}
