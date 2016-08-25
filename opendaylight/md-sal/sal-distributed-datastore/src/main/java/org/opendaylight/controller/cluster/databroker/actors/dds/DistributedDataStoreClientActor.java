461b52131eca271af04838161df01050acd6f101
Robert Varga <rovarga@cisco.com>
2016-08-25 11:31:24 +0200

    BUG-5280: add ExplicitAsk utility class
    Akka's support for Explicit Ask with Java functions
    is broken and requires a workaround. This patch moves
    the previous implementation site to a reusable place
    and fixes a caller which was broken.
    
    Change-Id: Idc0fc961b1808c23e01a4ae8c4eafdc93b7974f2
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    (cherry picked from commit 2ebf9ef718ea7ddd790784a6d241e68ef8d1c564)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/DistributedDataStoreClientActor.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/DistributedDataStoreClientActor.java
@@ -9,7 +9,6 @@ package org.opendaylight.controller.cluster.databroker.actors.dds;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import akka.pattern.ExplicitAskSupport;
 import akka.util.Timeout;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
@@ -20,11 +19,11 @@ import org.opendaylight.controller.cluster.access.client.ClientActorContext;
 import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
 import org.opendaylight.controller.cluster.access.concepts.FrontendType;
 import org.opendaylight.controller.cluster.access.concepts.MemberName;
+import org.opendaylight.controller.cluster.common.actor.ExplicitAsk;
 import org.opendaylight.controller.cluster.datastore.utils.ActorContext;
 import scala.Function1;
 import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
-import scala.runtime.AbstractFunction1;
 
 /**
  * A {@link AbstractClientActor} which acts as the point of contact for DistributedDataStore.
@@ -32,16 +31,7 @@ import scala.runtime.AbstractFunction1;
  * @author Robert Varga
  */
 public final class DistributedDataStoreClientActor extends AbstractClientActor {
-    // Unfortunately Akka's explicit ask pattern does not work with its Java API, as it fails to invoke passed message.
-    // In order to make this work for now, we tap directly into ExplicitAskSupport and use a Scala function instead
-    // of akka.japi.Function.
-    private static final ExplicitAskSupport ASK_SUPPORT = akka.pattern.extended.package$.MODULE$;
-    private static final Function1<ActorRef, Object> GET_CLIENT_FACTORY = new AbstractFunction1<ActorRef, Object>() {
-        @Override
-        public Object apply(final ActorRef askSender) {
-            return new GetClientRequest(askSender);
-        }
-    };
+    private static final Function1<ActorRef, ?> GET_CLIENT_FACTORY = ExplicitAsk.toScala(t -> new GetClientRequest(t));
 
     private final ActorContext actorContext;
 
@@ -65,7 +55,7 @@ public final class DistributedDataStoreClientActor extends AbstractClientActor {
     public static DistributedDataStoreClient getDistributedDataStoreClient(final @Nonnull ActorRef actor,
             final long timeout, final TimeUnit unit) {
         try {
-            return (DistributedDataStoreClient) Await.result(ASK_SUPPORT.ask(actor, GET_CLIENT_FACTORY,
+            return (DistributedDataStoreClient) Await.result(ExplicitAsk.ask(actor, GET_CLIENT_FACTORY,
                 Timeout.apply(timeout, unit)), Duration.Inf());
         } catch (Exception e) {
             throw Throwables.propagate(e);
