6b9737bf15d1944d9315bbcee43d9395cc562acb
Robert Varga <rovarga@cisco.com>
2016-08-30 17:57:41 +0200

    BUG-5280: add AbstractClientConnection
    Introduce a connection concept. This is a replacement for
    the request queue, as it turns out we do need the concept
    of a full connection (e.g. generational logic).
    
    This comes from the need to sensibly switch behaviors
    as the locality of the backend leader changes.
    
    This patch implements two sets of strategies for dealing
    with reconnect:
    
    The first one assumes long-lived state and is used for
    proxies dealing with histories. Here we make sure to
    reinstantiate and replace them in a map, as we want
    new transactions to follow the new semantic and we do not
    want to tear histories down or follow inefficient paths.
    
    The second one assumes short-lived state and is used for
    proxies dealing with individual transactions. Transactions
    are assumed to come and go rapidly and therefore we
    do not replace the proxies in maps (as they will be
    short-lived), but rather forwards operations to successors.
    
    The first strategy has a higher access cost, but its state
    is always fully uptodate when reconnect finishes, while
    the second strategy favor access time, but operations end
    up "trailing" and will be forwarded (and hence inefficient)
    until the transaction completes.
    
    Change-Id: I7fd9e21c749f55b91229bf0b671c8dcf2e4d5982
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/SingleClientHistory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/SingleClientHistory.java
@@ -7,7 +7,7 @@
  */
 package org.opendaylight.controller.cluster.databroker.actors.dds;
 
-import java.util.Optional;
+import org.opendaylight.controller.cluster.access.client.AbstractClientConnection;
 import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
 import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
 import org.slf4j.Logger;
@@ -34,8 +34,8 @@ final class SingleClientHistory extends AbstractClientHistory {
     }
 
     @Override
-    AbstractProxyHistory createHistoryProxy(final LocalHistoryIdentifier historyId,
-            final Optional<ShardBackendInfo> backendInfo) {
-        return AbstractProxyHistory.createSingle(getClient(), backendInfo, historyId);
+    ProxyHistory createHistoryProxy(final LocalHistoryIdentifier historyId,
+            final AbstractClientConnection<ShardBackendInfo> connection) {
+        return ProxyHistory.createSingle(connection, historyId);
     }
 }
