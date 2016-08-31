7e4557e63fa9627980e62657d1373cec4bf9fac9
Tomas Cere <tcere@cisco.com>
2016-08-31 17:22:30 +0200

    BUG 2138: Introduce DistributedShardedDOMDataTree
    This is the initial patch that introduces the concept of
    prefix based shards and consumer/producer api's into the
    DistributedDatastore.
    
    Each shard has a frontend registered into the ShardedDOMDataTree
    that forwards calls into the DistributedDataStoreClient frontend
    which then handles the actual writes into the Shard.
    These ShardFrontends are then distributed into other nodes
    via ShardedDataTreeActor which also handles notification of
    other nodes of open Producers.
    
    Change-Id: Ifcbd1021fdaeac7929fc547e6e32be50da9d93fc
    Signed-off-by: Tomas Cere <tcere@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/sharding/messages/NotifyProducerCreated.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.sharding.messages;
+
+import java.io.Serializable;
+import java.util.Collection;
+import org.opendaylight.controller.cluster.sharding.ShardedDataTreeActor;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+
+/**
+ * Message sent to remote {@link ShardedDataTreeActor}'s when attempting
+ * to create a producer. The remote node should attempt to create a producer in the local sharding service and reply
+ * with success/failure based on the attempt result.
+ */
+public class NotifyProducerCreated implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final Collection<DOMDataTreeIdentifier> subtrees;
+
+    public NotifyProducerCreated(final Collection<DOMDataTreeIdentifier> subtrees) {
+        this.subtrees = subtrees;
+    }
+
+    public Collection<DOMDataTreeIdentifier> getSubtrees() {
+        return subtrees;
+    }
+}
