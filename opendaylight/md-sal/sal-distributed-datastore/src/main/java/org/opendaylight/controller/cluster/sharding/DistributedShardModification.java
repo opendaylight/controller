a3261f1d475b4623810b2ebf940b0fb84b146966
Tomas Cere <tcere@cisco.com>
2016-09-07 13:48:27 +0200

    BUG-3128: Create DistributedShardFrontend
    Use the abstract shard implementations from md-sal to create
    a frontend implementation of a cds shard that forwards requests
    to backend shards via DistributedDatastoreClient.
    
    Change-Id: I7a3485f414368728e71ab2746c84d7a0f83f1436
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/sharding/DistributedShardModification.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.sharding;
+
+import com.google.common.base.Preconditions;
+import java.util.Map;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteCursor;
+import org.opendaylight.mdsal.dom.spi.shard.ForeignShardModificationContext;
+import org.opendaylight.mdsal.dom.spi.shard.WritableNodeOperation;
+import org.opendaylight.mdsal.dom.spi.shard.WriteCursorStrategy;
+import org.opendaylight.mdsal.dom.spi.shard.WriteableModificationNode;
+import org.opendaylight.mdsal.dom.spi.shard.WriteableNodeWithSubshard;
+import org.opendaylight.mdsal.dom.spi.store.DOMStoreThreePhaseCommitCohort;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+
+public class DistributedShardModification extends WriteableNodeWithSubshard {
+
+    private final DistributedShardModificationContext context;
+    private final Map<DOMDataTreeIdentifier, ForeignShardModificationContext> childShards;
+
+    public DistributedShardModification(final DistributedShardModificationContext context,
+                                        final Map<PathArgument, WriteableModificationNode> subshards,
+                                        final Map<DOMDataTreeIdentifier, ForeignShardModificationContext> childShards) {
+        super(subshards);
+        this.context = Preconditions.checkNotNull(context);
+        this.childShards = Preconditions.checkNotNull(childShards);
+    }
+
+    @Override
+    public PathArgument getIdentifier() {
+        return context.getIdentifier().getRootIdentifier().getLastPathArgument();
+    }
+
+    @Override
+    public WriteCursorStrategy createOperation(final DOMDataTreeWriteCursor parentCursor) {
+        return new WritableNodeOperation(this, context.cursor()) {
+            @Override
+            public void exit() {
+                throw new IllegalStateException("Can not exit data tree root");
+            }
+        };
+    }
+
+    DOMStoreThreePhaseCommitCohort seal() {
+        childShards.values().forEach(ForeignShardModificationContext::ready);
+        return context.ready();
+    }
+
+    DOMDataTreeIdentifier getPrefix() {
+        return context.getIdentifier();
+    }
+
+    Map<DOMDataTreeIdentifier, ForeignShardModificationContext> getChildShards() {
+        return childShards;
+    }
+}
