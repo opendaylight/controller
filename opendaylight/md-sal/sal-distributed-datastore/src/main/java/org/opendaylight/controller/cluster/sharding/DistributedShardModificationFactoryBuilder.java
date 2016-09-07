59569a7f08fdb4958499ef52444afe41dcebae18
Tomas Cere <tcere@cisco.com>
2016-09-07 13:48:27 +0200

    BUG-3128: Create DistributedShardFrontend
    Use the abstract shard implementations from md-sal to create
    a frontend implementation of a cds shard that forwards requests
    to backend shards via DistributedDatastoreClient.
    
    Change-Id: I7a3485f414368728e71ab2746c84d7a0f83f1436
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/sharding/DistributedShardModificationFactoryBuilder.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.sharding;
+
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.databroker.actors.dds.DataStoreClient;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.store.inmemory.ShardDataModificationFactoryBuilder;
+
+public class DistributedShardModificationFactoryBuilder
+        extends ShardDataModificationFactoryBuilder<DistributedShardModificationFactory> {
+
+    private final DataStoreClient client;
+
+    public DistributedShardModificationFactoryBuilder(final DOMDataTreeIdentifier root,
+                                                      final DataStoreClient client) {
+        super(root);
+        this.client = Preconditions.checkNotNull(client);
+    }
+
+    @Override
+    public DistributedShardModificationFactory build() {
+        return new DistributedShardModificationFactory(root, buildChildren(), childShards);
+    }
+}
