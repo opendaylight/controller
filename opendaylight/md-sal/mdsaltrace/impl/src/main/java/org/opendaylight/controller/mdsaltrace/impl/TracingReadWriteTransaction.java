0f9054fc9a20494369b56c126c585b3295863189
Josh <jhershbe@redhat.com>
2016-09-13 16:59:25 +0200

    mdsaltrace utility for debugging
    TracingBroker logs 'write' operations and listener registrations to
    the md-sal. It logs the instance identifier path,
    the objects themselves, as well as the stack trace of the call
    invoking the registration or write operation.
    It works by operating as a "bump on the stack" between the
    application and actual DataBroker, intercepting write
    and registration calls and writing to the log.
    
    Change-Id: I107c1f885d3b75d0bb5a43800857630a9a226b03
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ opendaylight/md-sal/mdsaltrace/impl/src/main/java/org/opendaylight/controller/mdsaltrace/impl/TracingReadWriteTransaction.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.mdsaltrace.impl;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+class TracingReadWriteTransaction extends TracingWriteTransaction implements ReadWriteTransaction{
+
+    private final ReadWriteTransaction delegate;
+
+    public TracingReadWriteTransaction(ReadWriteTransaction delegate, TracingBroker tracingBroker) {
+        super(delegate, tracingBroker);
+        this.delegate = delegate;
+    }
+
+    @Override
+    public <T extends DataObject> CheckedFuture<Optional<T>, ReadFailedException> read(LogicalDatastoreType logicalDatastoreType, InstanceIdentifier<T> instanceIdentifier) {
+        return delegate.read(logicalDatastoreType, instanceIdentifier);
+    }
+
+}
