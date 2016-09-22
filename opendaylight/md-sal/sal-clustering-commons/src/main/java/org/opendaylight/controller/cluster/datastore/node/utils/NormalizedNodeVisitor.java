1de86a15af0760b0059098eef53266a79ab2a437
Stephen Kitt <skitt@redhat.com>
2016-09-22 17:25:04 +0200

    Mechanical code cleanup (sal-clustering-commons)
    * Remove unnecessary type specifiers (use Java 7 <>)
    * Remove unnecessary "extends Object" declarations
    * Remove unnecessary semi-colons
    * Merge identical catch blocks
    * Remove redundant modifiers:
      - enum constructors are private by default
      - interface properties are public static final by default
      - interface methods are public abstract by default
      - interfaces are abstract by default
      - inner interfaces are static by default
      - inner classes in interfaces are public static by default
    
    Change-Id: I37133b6c4737bd33926021056c244789856622c2
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/NormalizedNodeVisitor.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/NormalizedNodeVisitor.java
@@ -11,5 +11,5 @@ package org.opendaylight.controller.cluster.datastore.node.utils;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 public interface NormalizedNodeVisitor {
-    public void visitNode(int level, String parentPath, NormalizedNode<?, ?> normalizedNode);
+    void visitNode(int level, String parentPath, NormalizedNode<?, ?> normalizedNode);
 }
