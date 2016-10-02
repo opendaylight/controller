ed985fb59e8ea32e8aced44eafcec2b04ccf43b5
Michael Vorburger <vorburger@redhat.com>
2016-10-02 03:01:29 -0700

    Refactor Read/WriteTransaction up into new Readable/WriteableDataStore
    with WriteTransaction extending WriteableDataStore, and ReadTransaction
    extends ReadableDataStore (just moved code; new interfaces contain exact
    same methods that the now sub interfaces used to contain)
    
    This allows one to provide other implementations of WriteableDataStore
    which do not directly expose transaction management programmatically
    (but allow to e.g. handle it with another approach; e.g. via Provider<>
    Dependency Injection).  
    
    Without this separation, some dummy NOOP, or throw new
    UnsupportedOperationException(), kind of implementation is needed in
    read/write implementations which should not directly expose programmatic
    transaction demarcation, which is a shame and more confusing API than a
    clean separation.
    
    This separation also makes the ODL persistence API more in line with
    other persistence API in Javaland.  For example, in JPA there is a
    separate EntityManager VS EntityTransaction (even though one can obtain
    an EntityTransaction from an EntityManager; but there is really no
    need).
    
    Note that this is 100% compatible with existing code using
    WriteTransaction, as it does NOT change the existing API contract.
    
    Change-Id: Ia4818c2a76c4d16a69165e5e39b86bce5a425cc8
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ReadTransaction.java
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ReadTransaction.java
@@ -8,43 +8,13 @@
 package org.opendaylight.controller.md.sal.binding.api;
 
 import org.opendaylight.controller.md.sal.common.api.data.AsyncReadTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-import com.google.common.base.Optional;
-import com.google.common.util.concurrent.CheckedFuture;
-
 /**
- * A transaction that provides read access to a logical data store.
+ * A transaction that provides read access to a data store.
  * <p>
  * For more information on usage and examples, please see the documentation in {@link AsyncReadTransaction}.
  */
-public interface ReadTransaction extends AsyncReadTransaction<InstanceIdentifier<?>, DataObject> {
-
-    /**
-     * Reads data from the provided logical data store located at the provided path.
-     *<p>
-     * If the target is a subtree, then the whole subtree is read (and will be
-     * accessible from the returned data object).
-     *
-     * @param store
-     *            Logical data store from which read should occur.
-     * @param path
-     *            Path which uniquely identifies subtree which client want to
-     *            read
-     * @return a CheckFuture containing the result of the read. The Future blocks until the
-     *         commit operation is complete. Once complete:
-     *         <ul>
-     *         <li>If the data at the supplied path exists, the Future returns an Optional object
-     *         containing the data.</li>
-     *         <li>If the data at the supplied path does not exist, the Future returns
-     *         Optional#absent().</li>
-     *         <li>If the read of the data fails, the Future will fail with a
-     *         {@link ReadFailedException} or an exception derived from ReadFailedException.</li>
-     *         </ul>
-     */
-    <T extends DataObject> CheckedFuture<Optional<T>,ReadFailedException> read(
-            LogicalDatastoreType store, InstanceIdentifier<T> path);
+public interface ReadTransaction extends ReadableDataStore, AsyncReadTransaction<InstanceIdentifier<?>, DataObject> {
 }
