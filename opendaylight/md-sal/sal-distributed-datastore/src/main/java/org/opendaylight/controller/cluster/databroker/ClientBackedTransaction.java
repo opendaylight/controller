f8c536fd864f5c3adafc5411806b58287d258bb5
Robert Varga <rovarga@cisco.com>
2016-11-25 13:55:13 +0100

    BUG-5280: split DistributedDataStore
    Split the DistributedDataStore into two components into
    an abstract base class and concretization running with
    TransactionProxies. Add another concretization, which uses
    DataStoreClient to instantiate requests.
    
    Change-Id: I454eec76d54c2fd4e4ea1e5cd16d12398eec81f0
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/ClientBackedTransaction.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker;
+
+import com.google.common.base.FinalizablePhantomReference;
+import com.google.common.base.FinalizableReferenceQueue;
+import com.google.common.base.Preconditions;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.controller.cluster.databroker.actors.dds.ClientTransaction;
+import org.opendaylight.controller.sal.core.spi.data.AbstractDOMStoreTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An implementation of {@link DOMStoreTransaction} backed by a {@link ClientTransaction}. It guards against user-level
+ * leaks by maintaining a phantom reference on the backing transaction, which will ensure that the transaction will
+ * be aborted, if it is not otherwise closed, just before this object is garbage-collected.
+ *
+ * @author Robert Varga
+ */
+abstract class ClientBackedTransaction extends AbstractDOMStoreTransaction<TransactionIdentifier> {
+    private static final class Finalizer extends FinalizablePhantomReference<ClientBackedTransaction> {
+        private static final FinalizableReferenceQueue QUEUE = new FinalizableReferenceQueue();
+        private static final Set<Finalizer> FINALIZERS = ConcurrentHashMap.newKeySet();
+        private static final Logger LOG = LoggerFactory.getLogger(Finalizer.class);
+
+        private final ClientTransaction transaction;
+
+        private Finalizer(final ClientBackedTransaction referent, final ClientTransaction transaction) {
+            super(referent, QUEUE);
+            this.transaction = Preconditions.checkNotNull(transaction);
+        }
+
+        static @Nonnull ClientTransaction recordTransaction(@Nonnull final ClientBackedTransaction referent,
+                @Nonnull final ClientTransaction transaction) {
+            FINALIZERS.add(new Finalizer(referent, transaction));
+            return transaction;
+        }
+
+        @Override
+        public void finalizeReferent() {
+            FINALIZERS.remove(this);
+            if (transaction.abort()) {
+                LOG.warn("Aborted orphan transaction {}", transaction.getIdentifier());
+            }
+        }
+    }
+
+    private final ClientTransaction delegate;
+
+    ClientBackedTransaction(final ClientTransaction delegate) {
+        super(delegate.getIdentifier());
+        this.delegate = Finalizer.recordTransaction(this, delegate);
+    }
+
+    final ClientTransaction delegate() {
+        return delegate;
+    }
+
+    @Override
+    public final void close() {
+        delegate.abort();
+    }
+}
