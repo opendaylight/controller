b604e392408dfbf46ee0a7e1ac4cd27170a83bff
Robert Varga <rovarga@cisco.com>
2016-12-01 11:17:53 +0100

    BUG-5280: refactor AbstractClientConnection
    The structure of AbstractClientConnection and its subclasses
    makes it hard to replay messages in a coordinated fashion. Furthermore
    splitting the inflight and pending and inflight queues into separate
    classes means we would have to jump through quite a few hoops
    to correctly calculate backpressure.
    
    Refactor the base class so it includes all the operations usually
    performed, with remoteMaxMessages() acting as the limiter, which disables
    transmission in connecting/reconnecting states.
    
    Change-Id: If743e4913aade7ed65ba60375d8b7d12c563cb96
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/ReconnectingClientConnection.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/ReconnectingClientConnection.java
@@ -7,6 +7,10 @@
  */
 package org.opendaylight.controller.cluster.access.client;
 
+import akka.actor.ActorRef;
+import java.util.Map.Entry;
+import org.opendaylight.controller.cluster.access.concepts.Request;
+import org.opendaylight.controller.cluster.access.concepts.RequestEnvelope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +29,20 @@ public final class ReconnectingClientConnection<T extends BackendInfo> extends A
     }
 
     @Override
-    void sendMessages(final int count) {
-        LOG.debug("Connection {} is reconnecting, not transmitting anything", this);
-    }
-
-    @Override
     ClientActorBehavior<T> reconnectConnection(final ClientActorBehavior<T> current) {
         // Intentional no-op
         LOG.debug("Skipping reconnect of already-reconnecting connection {}", this);
         return current;
+    }
+
+    @Override
+    Entry<ActorRef, RequestEnvelope> prepareForTransmit(final Request<?, ?> req) {
+        // This is guarded by remoteMaxMessages() == 0
+        throw new UnsupportedOperationException("Attempted to transmit on a reconnecting connection");
+    }
+
+    @Override
+    int remoteMaxMessages() {
+        return 0;
     }
 }
