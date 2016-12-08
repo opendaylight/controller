a97a77e2c4633b58a8b012cb20cfbf585993a5ab
Vratko Polak <vrpolak@cisco.com>
2016-12-08 17:34:59 +0100

    WIP: ProgressTracker to be used by AbstractClientConnection
    Change-Id: Iaea65c171455b89d7117431599ebc65fe0a4f19a
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnection.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnection.java
@@ -51,7 +51,6 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
     private final Long cookie;
 
     private volatile RequestException poisoned;
-    private long lastProgress;
 
     // Do not allow subclassing outside of this package
     AbstractClientConnection(final ClientActorContext context, final Long cookie,
@@ -59,14 +58,12 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
         this.context = Preconditions.checkNotNull(context);
         this.cookie = Preconditions.checkNotNull(cookie);
         this.queue = Preconditions.checkNotNull(queue);
-        this.lastProgress = readTime();
     }
 
     // Do not allow subclassing outside of this package
     AbstractClientConnection(final AbstractClientConnection<T> oldConnection) {
         this.context = oldConnection.context;
         this.cookie = oldConnection.cookie;
-        this.lastProgress = oldConnection.lastProgress;
         this.queue = new TransmitQueue.Halted();
     }
 
@@ -86,10 +83,15 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
      * Send a request to the backend and invoke a specified callback when it finishes. This method is safe to invoke
      * from any thread.
      *
+     * <p>This method may put the caller thread to sleep in order to throttle the request rate.
+     * The callback may be called before the sleep finishes.
+     *
      * @param request Request to send
      * @param callback Callback to invoke
+     * @throws InterruptedException when the thread was interrupted during its wait period
      */
-    public final void sendRequest(final Request<?, ?> request, final Consumer<Response<?, ?>> callback) {
+    public final void sendRequest(final Request<?, ?> request, final Consumer<Response<?, ?>> callback)
+                throws InterruptedException {
         final RequestException maybePoison = poisoned;
         if (maybePoison != null) {
             throw new IllegalStateException("Connection " + this + " has been poisoned", maybePoison);
@@ -97,12 +99,7 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
 
         final ConnectionEntry entry = new ConnectionEntry(request, callback, readTime());
 
-        lock.lock();
-        try {
-            queue.enqueue(entry, entry.getEnqueuedTicks());
-        } finally {
-            lock.unlock();
-        }
+        enqueueAndWait(entry, entry.getEnqueuedTicks());
     }
 
     public abstract Optional<T> getBackendInfo();
@@ -132,13 +129,23 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
         return context.ticker().read();
     }
 
-    final void enqueueEntry(final ConnectionEntry entry, final long now) {
+    final long enqueueEntry(final ConnectionEntry entry, final long now) {
         lock.lock();
         try {
-            queue.enqueue(entry, now);
+            return queue.enqueue(entry, now);
         } finally {
             lock.unlock();
         }
+    }
+
+    final long enqueueAndWait(final ConnectionEntry entry, final long now) throws InterruptedException {
+        final long delay = enqueueEntry(entry, now);
+        if (delay > 0L) {
+            final int nanos = (int) delay;
+            Thread.sleep(nanos / 1000, nanos % 1000);
+        }
+        return delay;  // the caller is informed on how much time he was supposed to spend sleeping
+        // TODO: Expose this information also from sendRequest?
     }
 
     /**
@@ -165,16 +172,15 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
         lock.lock();
         try {
             final long now = readTime();
-            if (!queue.isEmpty()) {
-                final long ticksSinceProgress = now - lastProgress;
-                if (ticksSinceProgress >= NO_PROGRESS_TIMEOUT_NANOS) {
-                    LOG.error("Queue {} has not seen progress in {} seconds, failing all requests", this,
-                        TimeUnit.NANOSECONDS.toSeconds(ticksSinceProgress));
+            // The following line is only reliable when queue is not forwarding, but such state should not last long.
+            final long ticksSinceProgress = queue.ticksStalling(now);
+            if (ticksSinceProgress >= NO_PROGRESS_TIMEOUT_NANOS) {
+                LOG.error("Queue {} has not seen progress in {} seconds, failing all requests", this,
+                    TimeUnit.NANOSECONDS.toSeconds(ticksSinceProgress));
 
-                    poison(new NoProgressException(ticksSinceProgress));
-                    current.removeConnection(this);
-                    return current;
-                }
+                poison(new NoProgressException(ticksSinceProgress));
+                current.removeConnection(this);
+                return current;
             }
 
             // Requests are always scheduled in sequence, hence checking for timeout is relatively straightforward.
@@ -212,13 +218,13 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
             return Optional.empty();
         }
 
-        final long delay = head.getEnqueuedTicks() - now + REQUEST_TIMEOUT_NANOS;
-        if (delay <= 0) {
-            LOG.debug("Connection {} timed out", this);
+        final long beenOpen = now - head.getEnqueuedTicks();
+        if (beenOpen >= REQUEST_TIMEOUT_NANOS ) {
+            LOG.debug("Connection {} has a request not completed for {} nanos, timing out", this, beenOpen);
             return null;
         }
 
-        return Optional.of(FiniteDuration.apply(delay, TimeUnit.NANOSECONDS));
+        return Optional.of(FiniteDuration.apply(REQUEST_TIMEOUT_NANOS - beenOpen, TimeUnit.NANOSECONDS));
     }
 
     final void poison(final RequestException cause) {
@@ -240,7 +246,5 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
         } finally {
             lock.unlock();
         }
-
-        lastProgress = readTime();
     }
 }
