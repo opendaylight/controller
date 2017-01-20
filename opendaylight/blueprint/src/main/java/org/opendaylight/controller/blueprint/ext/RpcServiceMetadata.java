ef43ac28f0c22f7a64f1a2ceb374f115344c8f88
Robert Varga <rovarga@cisco.com>
2017-01-20 22:04:11 +0100

    BUG-7608: Add ActionServiceMetadata and ActionProviderBean
    This patch add the new concepts of action-provider and
    action-service.
    
    The implementation does nothing, as we are transitioning from
    a run-time logic being coupled with sal-remoterpc-connector.
    
    This allows us to migrate users, while retaining behavior indepent
    of sal-remoterpc-connector's actions. This will allow us to fix
    BUG-3128.
    
    Once it is fixed, and DOMRpcRouter can express the action-provider
    advertisement, we are going to actiovate the commented-out code
    ActionServiceMetadata.acceptableStrategy().
    
    Change-Id: I3f412d092c10b51a198721f288fdefdfc907f0b7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/RpcServiceMetadata.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/RpcServiceMetadata.java
@@ -7,26 +7,8 @@
  */
 package org.opendaylight.controller.blueprint.ext;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import org.apache.aries.blueprint.services.ExtendedBlueprintContainer;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcAvailabilityListener;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
-import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
-import org.opendaylight.controller.sal.core.api.model.SchemaService;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.RpcService;
-import org.opendaylight.yangtools.yang.binding.util.BindingReflections;
-import org.opendaylight.yangtools.yang.common.QNameModule;
-import org.opendaylight.yangtools.yang.model.api.Module;
-import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.osgi.service.blueprint.container.ComponentDefinitionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.function.Predicate;
+import org.opendaylight.controller.md.sal.dom.broker.spi.rpc.RpcRoutingStrategy;
 
 /**
  * Factory metadata corresponding to the "rpc-service" element that gets an RPC service implementation from
@@ -34,142 +16,13 @@ import org.slf4j.LoggerFactory;
  *
  * @author Thomas Pantelis
  */
-class RpcServiceMetadata extends AbstractDependentComponentFactoryMetadata {
-    private static final Logger LOG = LoggerFactory.getLogger(RpcServiceMetadata.class);
-
-    private final String interfaceName;
-    private volatile Set<SchemaPath> rpcSchemaPaths;
-    private volatile RpcProviderRegistry rpcRegistry;
-    private volatile ListenerRegistration<DOMRpcAvailabilityListener> rpcListenerReg;
-    private volatile Class<RpcService> rpcInterface;
-
+final class RpcServiceMetadata extends AbstractInvokableServiceMetadata {
     RpcServiceMetadata(final String id, final String interfaceName) {
-        super(id);
-        this.interfaceName = interfaceName;
-    }
-
-    @SuppressWarnings({ "checkstyle:IllegalCatch", "unchecked" })
-    @Override
-    public void init(final ExtendedBlueprintContainer container) {
-        super.init(container);
-
-        final Class<?> interfaceClass;
-        try {
-            interfaceClass = container().getBundleContext().getBundle().loadClass(interfaceName);
-        } catch (Exception e) {
-            throw new ComponentDefinitionException(String.format("%s: Error obtaining interface class %s",
-                    logName(), interfaceName), e);
-        }
-
-        if (!RpcService.class.isAssignableFrom(interfaceClass)) {
-            throw new ComponentDefinitionException(String.format(
-                "%s: The specified interface %s is not an RpcService", logName(), interfaceName));
-        }
-
-        rpcInterface = (Class<RpcService>)interfaceClass;
+        super(id, interfaceName);
     }
 
     @Override
-    protected void startTracking() {
-        // First get the SchemaContext. This will be used to get the RPC SchemaPaths.
-
-        retrieveService("SchemaService", SchemaService.class,
-            service -> retrievedSchemaContext(((SchemaService)service).getGlobalContext()));
-    }
-
-    private void retrievedSchemaContext(final SchemaContext schemaContext) {
-        LOG.debug("{}: retrievedSchemaContext", logName());
-
-        QNameModule moduleName = BindingReflections.getQNameModule(rpcInterface);
-        Module module = schemaContext.findModuleByNamespaceAndRevision(moduleName.getNamespace(),
-                moduleName.getRevision());
-
-        LOG.debug("{}: Got Module: {}", logName(), module);
-
-        rpcSchemaPaths = new HashSet<>();
-        for (RpcDefinition rpcDef : module.getRpcs()) {
-            rpcSchemaPaths.add(rpcDef.getPath());
-        }
-
-        LOG.debug("{}: Got SchemaPaths: {}", logName(), rpcSchemaPaths);
-
-        // First get the DOMRpcService OSGi service. This will be used to register a listener to be notified
-        // when the underlying DOM RPC service is available.
-
-        retrieveService("DOMRpcService", DOMRpcService.class,
-            service -> retrievedDOMRpcService((DOMRpcService)service));
-    }
-
-    private void retrievedDOMRpcService(final DOMRpcService domRpcService) {
-        LOG.debug("{}: retrievedDOMRpcService", logName());
-
-        setDependendencyDesc("Available DOM RPC for binding RPC: " + rpcInterface);
-        rpcListenerReg = domRpcService.registerRpcListener(new DOMRpcAvailabilityListener() {
-            @Override
-            public void onRpcAvailable(final Collection<DOMRpcIdentifier> rpcs) {
-                onRpcsAvailable(rpcs);
-            }
-
-            @Override
-            public void onRpcUnavailable(final Collection<DOMRpcIdentifier> rpcs) {
-            }
-        });
-    }
-
-    protected void onRpcsAvailable(final Collection<DOMRpcIdentifier> rpcs) {
-        for (DOMRpcIdentifier identifier : rpcs) {
-            if (rpcSchemaPaths.contains(identifier.getType())) {
-                LOG.debug("{}: onRpcsAvailable - found SchemaPath {}", logName(), identifier.getType());
-
-                retrieveService("RpcProviderRegistry", RpcProviderRegistry.class, service -> {
-                    rpcRegistry = (RpcProviderRegistry)service;
-                    setSatisfied();
-                });
-
-                break;
-            }
-        }
-    }
-
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    @Override
-    public Object create() throws ComponentDefinitionException {
-        LOG.debug("{}: In create: interfaceName: {}", logName(), interfaceName);
-
-        super.onCreate();
-
-        try {
-            RpcService rpcService = rpcRegistry.getRpcService(rpcInterface);
-
-            LOG.debug("{}: create returning service {}", logName(), rpcService);
-
-            return rpcService;
-        } catch (RuntimeException e) {
-            throw new ComponentDefinitionException("Error getting RPC service for " + interfaceName, e);
-        }
-    }
-
-    @Override
-    public void stopTracking() {
-        super.stopTracking();
-        closeRpcListenerReg();
-    }
-
-    private void closeRpcListenerReg() {
-        if (rpcListenerReg != null) {
-            rpcListenerReg.close();
-            rpcListenerReg = null;
-        }
-    }
-
-    @Override
-    public void destroy(final Object instance) {
-        super.destroy(instance);
-        closeRpcListenerReg();
-    }
-
-    @Override
-    public String toString() {
-        return "RpcServiceMetadata [id=" + getId() + ", interfaceName=" + interfaceName + "]";
+    Predicate<RpcRoutingStrategy> rpcFilter() {
+        return s -> !s.isContextBasedRouted();
     }
 }
