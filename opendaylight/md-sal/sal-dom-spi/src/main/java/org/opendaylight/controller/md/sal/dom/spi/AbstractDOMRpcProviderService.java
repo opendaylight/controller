69fe271d32cf24f8771a3a7cb94ed476cdd438cc
Robert Varga <rovarga@cisco.com>
2017-01-25 23:05:02 +0100

    Take advantage of default methods in DOMRpcProviderService
    We can make one of the methods default, as all implementations
    are exactly the same (codifying contract).
    
    Change-Id: I64f0b62fd3a0987ed1ed01ec14b2c4d3b77560ac
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- opendaylight/md-sal/sal-dom-spi/src/main/java/org/opendaylight/controller/md/sal/dom/spi/AbstractDOMRpcProviderService.java
+++ opendaylight/md-sal/sal-dom-spi/src/main/java/org/opendaylight/controller/md/sal/dom/spi/AbstractDOMRpcProviderService.java
@@ -7,18 +7,14 @@
  */
 package org.opendaylight.controller.md.sal.dom.spi;
 
-import com.google.common.collect.ImmutableSet;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementation;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementationRegistration;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
 
 /**
  * Convenience abstract base class for {@link DOMRpcProviderService} implementations.
+ *
+ * @deprecated Useless class. Implement {@link DOMRpcProviderService} directly.
  */
+@Deprecated
 public abstract class AbstractDOMRpcProviderService implements DOMRpcProviderService {
-    @Override
-    public final <T extends DOMRpcImplementation> DOMRpcImplementationRegistration<T> registerRpcImplementation(final T implementation, final DOMRpcIdentifier... types) {
-        return registerRpcImplementation(implementation, ImmutableSet.copyOf(types));
-    }
+
 }
