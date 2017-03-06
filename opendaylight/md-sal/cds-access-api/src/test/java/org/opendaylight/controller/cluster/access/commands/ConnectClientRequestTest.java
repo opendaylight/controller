7b5a8333b6dc1f7b72752bd32ff8d03a43aa7d4a
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2017-03-06 16:09:20 +0100

    Unit tests for Request derived classes
    Change-Id: I56d98cb89bf0cc474e69a797553aace1d8c2c040
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ opendaylight/md-sal/cds-access-api/src/test/java/org/opendaylight/controller/cluster/access/commands/ConnectClientRequestTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import org.junit.Test;
+
+public class ConnectClientRequestTest {
+    @Test
+    public void getMinVersion() throws Exception {
+
+    }
+
+    @Test
+    public void getMaxVersion() throws Exception {
+
+    }
+
+    @Test
+    public void toRequestFailure() throws Exception {
+
+    }
+
+    @Test
+    public void externalizableProxy() throws Exception {
+
+    }
+
+    @Test
+    public void cloneAsVersion() throws Exception {
+
+    }
+
+    @Test
+    public void addToStringAttributes() throws Exception {
+
+    }
+
+}
