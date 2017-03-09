ceb1a377acef6ede462817663be17c15863dfa9f
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2017-03-09 14:31:03 +0100

    Unit tests for RequestFailure derived classes
    Change-Id: I3b9fb7f6707b4519ea91880a369cfba0af24eb4e
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ opendaylight/md-sal/cds-access-api/src/test/java/org/opendaylight/controller/cluster/access/commands/ConnectClientFailureTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+
+public class ConnectClientFailureTest extends AbstractRequestFailureTest<ConnectClientFailure> {
+    private static final ConnectClientFailure OBJECT = new ConnectClientFailure(CLIENT_IDENTIFIER, 0, CAUSE);
+
+    @Override
+    ConnectClientFailure object() {
+        return OBJECT;
+    }
+
+    @Test
+    public void cloneAsVersion() throws Exception {
+        final ConnectClientFailure clone = OBJECT.cloneAsVersion(ABIVersion.current());
+        Assert.assertEquals(OBJECT.getTarget(), clone.getTarget());
+        Assert.assertEquals(OBJECT.getSequence(), clone.getSequence());
+        Assert.assertEquals(OBJECT.getCause(), clone.getCause());
+    }
+
+    @Test
+    public void externalizableProxy() throws Exception {
+        final ConnectClientFailureProxyV1 proxy = (ConnectClientFailureProxyV1) OBJECT.externalizableProxy(
+                ABIVersion.current());
+        Assert.assertNotNull(proxy);
+    }
+}
