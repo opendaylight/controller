b9f03b940c84a313ca94a321fa4e21260b26a384
Tomas Cere <tcere@cisco.com>
2017-03-09 14:12:45 +0100

    Bug 7803: Implement agent RPCs for data tree change listener testing
    Change-Id: Id2d53d3765fb9d518d4b052792d716d2b2b4c976
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/impl/IdIntsListener.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.clustering.it.provider.impl;
+
+import com.google.common.base.Preconditions;
+import java.util.Collection;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeListener;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
+import org.opendaylight.yangtools.yang.data.impl.schema.tree.InMemoryDataTreeFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IdIntsListener implements DOMDataTreeChangeListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IdIntsListener.class);
+
+    private NormalizedNode<?, ?> localCopy = null;
+
+    @Override
+    public void onDataTreeChanged(@Nonnull final Collection<DataTreeCandidate> changes) {
+
+        // There should only be one candidate reported
+        Preconditions.checkState(changes.size() == 1);
+
+        // do not log the change into debug, only use trace since it will lead to OOM on default heap settings
+        LOG.debug("Received data tree changed");
+
+        changes.forEach(change -> {
+            if (localCopy == null || checkEqual(change.getRootNode().getDataBefore().get())) {
+                localCopy = change.getRootNode().getDataAfter().get();
+            } else {
+                LOG.debug("Ignoring notification: {}", change);
+            }
+            LOG.trace("Received change: {}", localCopy);
+        });
+    }
+
+    public boolean checkEqual(final NormalizedNode<?, ?> expected) {
+        return localCopy.equals(expected);
+    }
+}
