51d0c3b83ed2cb0877f2eb43cd5e14ceae5c08fd
Andrej Mak <andrej.mak@pantheon.tech>
2017-03-14 09:26:15 +0100

    Add AbstractDataStoreClientBehavior unit tests
    Change-Id: I84f83c14353480c2aee54b5fc077c5c275c26f71
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/access/client/AccessClientUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.client;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+
+/**
+ * Util class to access package private members in cds-access-client for test purposes.
+ */
+public class AccessClientUtil {
+
+    public static ClientActorContext createClientActorContext(final ActorSystem system, final ActorRef actor,
+                                                              final ClientIdentifier id, final String persistenceId) {
+        return new ClientActorContext(actor, system.scheduler(), system.dispatcher(), persistenceId, id);
+    }
+
+}
