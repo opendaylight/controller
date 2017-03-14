32bcc43c207973c69371611e9596e517ccfce1b0
Andrej Mak <andrej.mak@pantheon.tech>
2017-03-14 09:26:15 +0100

    Add AbstractDataStoreClientBehavior unit tests
    -add unit tests for AbstractDataStoreClientBehavior derived classes
    -remove final modifiers from some methods to allow mocking
    
    Change-Id: I84f83c14353480c2aee54b5fc077c5c275c26f71
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActorContext.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActorContext.java
@@ -28,13 +28,24 @@ abstract class AbstractClientActorContext implements Mutable {
     }
 
     // TODO: rename this to logContext()
+
+    /**
+     * Returns persistence id. This method must not be overridden and is non final only because of testing.
+     *
+     * @return persistence id.
+     */
     @Nonnull
-    final String persistenceId() {
+    String persistenceId() {
         return persistenceId;
     }
 
+    /**
+     * This method must not be overridden and is non final only because of testing.
+     *
+     * @return self.
+     */
     @Nonnull
-    public final ActorRef self() {
+    public ActorRef self() {
         return self;
     }
 }
