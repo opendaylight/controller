63fd7a31e8c14c51fbbf00703a22f78291922de4
Andrej Mak <andrej.mak@pantheon.tech>
2017-03-15 15:24:56 +0100

    Add AbstractClientHandle derived classes tests
    Change-Id: I9eee30289814c92b7f3e7d5b2eebf49ee575b9b3
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/access/client/AccessClientUtil.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/access/client/AccessClientUtil.java
@@ -10,6 +10,7 @@ package org.opendaylight.controller.cluster.access.client;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.ResponseEnvelope;
 
 /**
  * Util class to access package private members in cds-access-client for test purposes.
@@ -19,6 +20,11 @@ public class AccessClientUtil {
     public static ClientActorContext createClientActorContext(final ActorSystem system, final ActorRef actor,
                                                               final ClientIdentifier id, final String persistenceId) {
         return new ClientActorContext(actor, system.scheduler(), system.dispatcher(), persistenceId, id);
+    }
+
+    public static void completeRequest(final AbstractClientConnection<? extends BackendInfo> connection,
+                                       final ResponseEnvelope<?> envelope) {
+        connection.receiveResponse(envelope);
     }
 
 }
