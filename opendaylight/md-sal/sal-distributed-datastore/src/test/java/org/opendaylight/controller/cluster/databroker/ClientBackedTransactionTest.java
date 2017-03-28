b2d9df59b60237fb47864964895345c1b3fe2262
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2017-03-28 17:21:21 +0200

    Unit test for ClientBackedTransaction derived classes
    Change-Id: I2967a0e224fc783ffac73a994def666e86a423a6
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/ClientBackedTransactionTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendType;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.MemberName;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+
+public abstract class ClientBackedTransactionTest<T extends ClientBackedTransaction> {
+    protected static final ClientIdentifier UNKNOWN_ID = ClientIdentifier.create(
+            FrontendIdentifier.create(MemberName.forName("local"), FrontendType.forName("unknown")), 0);
+
+    protected static FrontendIdentifier FRONTEND_IDENTIFIER = FrontendIdentifier.create(
+            MemberName.forName("member"), FrontendType.forName("frontend"));
+    protected static final ClientIdentifier CLIENT_IDENTIFIER = ClientIdentifier.create(FRONTEND_IDENTIFIER, 0);
+
+    protected static LocalHistoryIdentifier HISTORY_ID = new LocalHistoryIdentifier(CLIENT_IDENTIFIER, 0);
+    protected static final TransactionIdentifier TRANSACTION_IDENTIFIER = new TransactionIdentifier(HISTORY_ID, 0);
+
+    abstract T object() throws Exception;
+
+    @Test
+    public void delegate() throws Exception {
+        Assert.assertNotNull(object().delegate());
+    }
+
+    @Test
+    public void close() throws Exception {
+        object().close();
+    }
+}
