297146435d552acf4c1e1b3cdef9535f51787ebf
matus.kubica <matus.kubica@pantheon.tech>
2017-03-29 16:13:57 +0200

    AbstractClientHistory derived classes tests
    Change-Id: I1261eb764c730bbce6eb833644db99e4bbf0605c
    Signed-off-by: matus.kubica <matus.kubica@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/AbstractClientHistory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/AbstractClientHistory.java
@@ -231,7 +231,7 @@ public abstract class AbstractClientHistory extends LocalAbortable implements Id
     /**
      * Callback invoked from {@link ClientTransaction} when a child transaction readied for submission.
      *
-     * @param txId Transaction identifier
+     * @param tx Transaction identifier
      * @param cohort Transaction commit cohort
      */
     synchronized AbstractTransactionCommitCohort onTransactionReady(final ClientTransaction tx,
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/actors/dds/AbstractClientHistoryTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker.actors.dds;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.opendaylight.controller.cluster.databroker.actors.dds.TestUtils.CLIENT_ID;
+import static org.opendaylight.controller.cluster.databroker.actors.dds.TestUtils.TRANSACTION_ID;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.controller.cluster.access.concepts.LocalHistoryIdentifier;
+import org.opendaylight.controller.cluster.datastore.messages.PrimaryShardInfo;
+import org.opendaylight.controller.cluster.datastore.utils.ActorContext;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import scala.concurrent.Promise;
+import scala.concurrent.impl.Promise.DefaultPromise;
+
+public abstract class AbstractClientHistoryTest<T extends AbstractClientHistory> {
+
+    protected static final String SHARD_NAME = "default";
+    protected static final String PERSISTENCE_ID = "per-1";
+    protected static final LocalHistoryIdentifier HISTORY_ID = new LocalHistoryIdentifier(CLIENT_ID, 1L);
+
+    protected abstract T object();
+
+    @Test
+    public abstract void testDoCreateSnapshot() throws Exception;
+
+    @Test
+    public abstract void testDoCreateTransaction() throws Exception;
+
+    @Test
+    public abstract void testCreateHistoryProxy() throws Exception;
+
+    @Test
+    public void testCreateSnapshotProxy() throws Exception {
+        final AbstractProxyTransaction snapshotProxy = object().createSnapshotProxy(TRANSACTION_ID, 0L);
+        Assert.assertNotNull(snapshotProxy);
+        Assert.assertNotEquals(TRANSACTION_ID, snapshotProxy.getIdentifier());
+    }
+
+    @Test
+    public void testCreateTransactionProxy() throws Exception {
+        AbstractProxyTransaction transactionProxy = object().createTransactionProxy(TRANSACTION_ID, 0L);
+        Assert.assertNotNull(transactionProxy);
+        Assert.assertNotEquals(TRANSACTION_ID, transactionProxy.getIdentifier());
+    }
+
+    @Test
+    public void testState() throws Exception {
+        Assert.assertEquals(AbstractClientHistory.State.IDLE, object().state());
+    }
+
+    @Test
+    public void testUpdateState() throws Exception {
+        object().updateState(AbstractClientHistory.State.IDLE, AbstractClientHistory.State.CLOSED);
+        Assert.assertEquals(AbstractClientHistory.State.CLOSED, object().state());
+    }
+
+    @Test
+    public void testDoClose() throws Exception {
+        object().createTransactionProxy(TRANSACTION_ID, 0L);
+        object().doClose();
+        Assert.assertEquals(AbstractClientHistory.State.CLOSED, object().state());
+    }
+
+    @Test
+    public void testGetIdentifier() throws Exception {
+        Assert.assertEquals(HISTORY_ID, object().getIdentifier());
+    }
+
+    @Test
+    public void testNextTx() throws Exception {
+        Assert.assertTrue(object().nextTx() + 1 == object().nextTx());
+    }
+
+    @Test
+    public void testResolveShardForPath() throws Exception {
+        final Long shardForPath = object().resolveShardForPath(YangInstanceIdentifier.EMPTY);
+        Assert.assertEquals(0L, shardForPath.longValue());
+    }
+
+    @Test
+    public void testLocalAbort() throws Exception {
+        object().localAbort(new Throwable());
+        Assert.assertEquals(AbstractClientHistory.State.CLOSED, object().state());
+    }
+
+    @Test
+    public void testOnProxyDestroyed() throws Exception {
+        final ProxyHistory proxyHistory = Mockito.mock(ProxyHistory.class);
+        when(proxyHistory.getIdentifier()).thenReturn(HISTORY_ID);
+
+        object().onProxyDestroyed(proxyHistory);
+        verify(proxyHistory).getIdentifier();
+    }
+
+    @Test
+    public void testCreateTransaction() throws Exception {
+        final ClientTransaction transaction = object().createTransaction();
+        Assert.assertNotNull(transaction);
+    }
+
+    @Test
+    public void testTakeSnapshot() throws Exception {
+        final ClientSnapshot clientSnapshot = object().takeSnapshot();
+        Assert.assertEquals(object().getIdentifier(), clientSnapshot.getIdentifier().getHistoryId());
+    }
+
+    @Test
+    public void testOnTransactionAbort() throws Exception {
+        final ClientSnapshot clientSnapshot = object().doCreateSnapshot();
+        Assert.assertTrue(clientSnapshot.abort());
+    }
+
+    @Test
+    public void testOnTransactionReady() throws Exception {
+        // TODO
+    }
+
+    @Test
+    public void testOnTransactionComplete() throws Exception {
+        // TODO
+    }
+
+    @Test
+    public void testStartReconnect() throws Exception {
+        // TODO
+    }
+
+    @Test
+    public void testStartReconnectMissingOldProxy() throws Exception {
+        // TODO
+    }
+
+    protected static ActorContext createActorContextMock(final ActorSystem system, final ActorRef actor) {
+        final ActorContext mock = mock(ActorContext.class);
+        final Promise<PrimaryShardInfo> promise = new DefaultPromise<>();
+        final ActorSelection selection = system.actorSelection(actor.path());
+        final PrimaryShardInfo shardInfo = new PrimaryShardInfo(selection, (short) 0);
+        promise.success(shardInfo);
+        when(mock.findPrimaryShardAsync(any())).thenReturn(promise.future());
+        return mock;
+    }
+}
