60e3d24e374a43bca1daae48c02d837dd53dbf6d
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2017-03-29 10:53:23 +0200

    Unit tests for ClientBackedTransactionChain class
    Change-Id: I97953cfdc32619c31295cfed2584b7466d48aa5d
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/ClientBackedTransactionChainTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.databroker;
+
+import org.junit.Test;
+
+public class ClientBackedTransactionChainTest {
+
+    @Test
+    public void newReadOnlyTransaction() throws Exception {
+    }
+
+    @Test
+    public void newReadWriteTransaction() throws Exception {
+    }
+
+    @Test
+    public void newWriteOnlyTransaction() throws Exception {
+    }
+
+    @Test
+    public void close() throws Exception {
+    }
+
+    @Test
+    public void snapshotClosed() throws Exception {
+    }
+}
