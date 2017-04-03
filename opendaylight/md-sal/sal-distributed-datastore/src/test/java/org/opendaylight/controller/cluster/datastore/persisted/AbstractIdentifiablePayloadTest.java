de87d442b67bbe79c988548e7b92a04c48606d0e
Andrej Mak <andrej.mak@pantheon.tech>
2017-04-03 11:01:04 +0200

    Add AbstractIdentifiablePayload unit tests
    Change-Id: I884f3c35d1767ed02accabc3b9a775ef9c667716
    Signed-off-by: Andrej Mak <andrej.mak@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/persisted/AbstractIdentifiablePayloadTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.cluster.datastore.AbstractTest;
+
+abstract class AbstractIdentifiablePayloadTest<T extends AbstractIdentifiablePayload> extends AbstractTest {
+
+    abstract T object();
+
+    @Test
+    public void testSerialization() {
+        final T object = object();
+        final T cloned = SerializationUtils.clone(object);
+        Assert.assertEquals(object.getIdentifier(), cloned.getIdentifier());
+    }
+}
--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/persisted/PurgeTransactionPayloadTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/persisted/PurgeTransactionPayloadTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,10 +7,10 @@
  */
 package org.opendaylight.controller.cluster.datastore.persisted;
 
-public class AbortTransactionPayloadTest extends AbstractIdentifiablePayloadTest<AbortTransactionPayload> {
+public class PurgeTransactionPayloadTest extends AbstractIdentifiablePayloadTest<PurgeTransactionPayload> {
 
     @Override
-    AbortTransactionPayload object() {
-        return AbortTransactionPayload.create(nextTransactionId());
+    PurgeTransactionPayload object() {
+        return PurgeTransactionPayload.create(nextTransactionId());
     }
 }
