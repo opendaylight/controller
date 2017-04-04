de7611a820334a3ab0a6c638e70903e4dd4eb9b9
matus.kubica <matus.kubica@pantheon.tech>
2017-04-04 14:04:04 +0200

    RemoteRpcProviderFactory and RpcErrorsException unit tests
    Change-Id: Ife8c638d43810baede654cccac22fa8efccae1d0
    Signed-off-by: matus.kubica <matus.kubica@pantheon.tech>

---
+++ opendaylight/md-sal/sal-remoterpc-connector/src/test/java/org/opendaylight/controller/remote/rpc/RpcErrorsExceptionTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.remote.rpc;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.opendaylight.yangtools.yang.common.RpcError;
+
+public class RpcErrorsExceptionTest {
+
+    private static RpcErrorsException object;
+
+    private final String errorMessage = "Test error message.";
+    private Collection<RpcError> rpcErrors;
+
+    @Mock
+    private RpcError error1;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+
+        rpcErrors = new ArrayList<>();
+        rpcErrors.add(error1);
+        object = new RpcErrorsException(errorMessage, rpcErrors);
+    }
+
+    @Test
+    public void checkMessage() {
+        final String message = object.getMessage();
+        Assert.assertEquals(errorMessage, message);
+    }
+
+    @Test
+    public void testGetRpcErrors() throws Exception {
+        final RpcError expectedError = rpcErrors.iterator().next();
+        final RpcError actualError = object.getRpcErrors().iterator().next();
+        Assert.assertEquals(expectedError.getCause(), actualError.getCause());
+        Assert.assertEquals(expectedError.getMessage(), actualError.getMessage());
+        Assert.assertEquals(expectedError.getApplicationTag(), actualError.getApplicationTag());
+        Assert.assertEquals(expectedError.getErrorType(), actualError.getErrorType());
+    }
+}
