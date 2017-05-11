888c97bda52d493f858cf682d60f0af63b88c78a
Robert Varga <robert.varga@pantheon.tech>
2017-05-11 16:54:22 +0200

    WIP: BUG-8422: separate retry and request timeouts
    This patch corrects a thinko around request timeouts, where we
    reconnect the connection based on request timeout, not based on
    the 'try' timeout.
    
    The difference between the two is that the 'try' timeout is the
    period we allow the backend to respond to our request and when
    it does not, we reconnect the connection.
    
    Change-Id: I8c00a80e5c26c5b829056c43fe78a0567041bc5e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnection.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnection.java
@@ -40,13 +40,36 @@ import scala.concurrent.duration.FiniteDuration;
 public abstract class AbstractClientConnection<T extends BackendInfo> {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractClientConnection.class);
 
-    // Keep these constants in nanoseconds, as that prevents unnecessary conversions in the fast path
+    /*
+     * Timers involved in communication with the backend. There are three tiers which are spaced out to allow for
+     * recovery at each tier. Keep these constants in nanoseconds, as that prevents unnecessary conversions in the fast
+     * path.
+     */
+    /**
+     * Backend aliveness timer. This is reset whenever we receive a response from the backend and kept armed whenever
+     * we have an outstanding request. If when this time expires, we tear down this connection and attept to reconnect
+     * it.
+     */
+    @VisibleForTesting
+    static final long BACKEND_ALIVE_TIMEOUT_NANOS = TimeUnit.SECONDS.toNanos(30);
+
+    /**
+     * Request timeout. If the request fails to complete within this time since it was originally enqueued, we time
+     * the request out.
+     */
+    @VisibleForTesting
+    static final long REQUEST_TIMEOUT_NANOS = TimeUnit.MINUTES.toNanos(2);
+
+    /**
+     * No progress timeout. A client fails to make any forward progress in this time, it will terminate itself.
+     */
     @VisibleForTesting
     static final long NO_PROGRESS_TIMEOUT_NANOS = TimeUnit.MINUTES.toNanos(15);
-    @VisibleForTesting
-    static final long REQUEST_TIMEOUT_NANOS = TimeUnit.SECONDS.toNanos(30);
 
-    private static final FiniteDuration REQUEST_TIMEOUT_DURATION = FiniteDuration.apply(REQUEST_TIMEOUT_NANOS,
+    /**
+     * {@link #BACKEND_ALIVE_TIMEOUT_NANOS} as a {@link FiniteDuration}.
+     */
+    private static final FiniteDuration BACKEND_ALIVE_DURATION = FiniteDuration.apply(BACKEND_ALIVE_TIMEOUT_NANOS,
         TimeUnit.NANOSECONDS);
 
     private final Lock lock = new ReentrantLock();
@@ -160,7 +183,7 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
 
             if (queue.isEmpty()) {
                 // The queue is becoming non-empty, schedule a timer
-                scheduleTimer(REQUEST_TIMEOUT_DURATION);
+                scheduleTimer(BACKEND_ALIVE_DURATION);
             }
             return queue.enqueue(entry, now);
         } finally {
@@ -269,12 +292,12 @@ public abstract class AbstractClientConnection<T extends BackendInfo> {
         }
 
         final long beenOpen = now - head.getEnqueuedTicks();
-        if (beenOpen >= REQUEST_TIMEOUT_NANOS) {
+        if (beenOpen >= BACKEND_ALIVE_TIMEOUT_NANOS) {
             LOG.debug("Connection {} has a request not completed for {} nanoseconds, timing out", this, beenOpen);
             return null;
         }
 
-        return Optional.of(FiniteDuration.apply(REQUEST_TIMEOUT_NANOS - beenOpen, TimeUnit.NANOSECONDS));
+        return Optional.of(FiniteDuration.apply(BACKEND_ALIVE_TIMEOUT_NANOS - beenOpen, TimeUnit.NANOSECONDS));
     }
 
     final void poison(final RequestException cause) {
