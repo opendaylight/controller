164904d0143439d383a9aebf41eb7870ec1bb64d
Stephen Kitt <skitt@redhat.com>
2017-05-12 13:19:39 +0200

    config-persister-directory-xml-adapter: final parameters
    This automatically-generated patch flags all appropriate parameters as
    final (including caught exceptions).
    
    Change-Id: I58d51e9f65ca798f5ec0af0e21a1336edd55749b
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- opendaylight/config/config-persister-directory-xml-adapter/src/test/java/org/opendaylight/controller/config/persist/storage/directory/xml/DirectoryStorageAdapterTest.java
+++ opendaylight/config/config-persister-directory-xml-adapter/src/test/java/org/opendaylight/controller/config/persist/storage/directory/xml/DirectoryStorageAdapterTest.java
@@ -67,7 +67,7 @@ public class DirectoryStorageAdapterTest {
                 }
             });
             fail();
-        } catch (UnsupportedOperationException e) {
+        } catch (final UnsupportedOperationException e) {
 
         }
     }
