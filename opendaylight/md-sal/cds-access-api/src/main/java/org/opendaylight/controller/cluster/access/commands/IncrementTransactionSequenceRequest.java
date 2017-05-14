d40780051b670e87f2cfd9868928cc5713d7b6be
Robert Varga <robert.varga@pantheon.tech>
2017-05-14 20:36:09 +0200

    BUG-8402: fix sequencing with read/exists requests
    When replaying successful requests, we do not issue read and exists
    requests, as they have already been satisfied, but account for their
    sequence numbers.
    
    This does not work in the case where we have a remote connection,
    the first request on a transaction is a read and after it is
    satisfied subsequent requests are replayed to a different backend
    leader.
    
    Since the initial request is not replayed, but subsequent requests
    account for it and the backend has no prior knowledge of the
    transaction, it sees an initial request with sequence != 0, and
    rejects all requests with an OutOfOrderRequestException.
    
    Fix this by introducing IncrementTransactionSequenceRequest, which
    the frontend enqueues as the first request instead of the initial
    read/exist request -- introducing the transaction to backend.
    
    This patch flushes out a bug where we accept requests after an
    operation fails. Fix this up by recording the failure and preventing
    any further operations, reporting the same error.
    
    Change-Id: Ia0f048e33d417e1fdc8d15bf319d6b8b33c2b1b1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/commands/IncrementTransactionSequenceRequest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import akka.actor.ActorRef;
+import com.google.common.base.Preconditions;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+
+/**
+ * A blank transaction request. This is used to provide backfill requests in converted retransmit scenarios, such as
+ * when a initial request to a transaction (such as a {@link ReadTransactionRequest}) is satisfied by the backend
+ * before the need to replay the transaction to a different remote backend.
+ *
+ * @author Robert Varga
+ */
+public final class IncrementTransactionSequenceRequest extends TransactionRequest<IncrementTransactionSequenceRequest> {
+    private static final long serialVersionUID = 1L;
+
+    private final long increment;
+
+    public IncrementTransactionSequenceRequest(final TransactionIdentifier identifier, final long sequence,
+            final ActorRef replyTo, final long increment) {
+        super(identifier, sequence, replyTo);
+        Preconditions.checkArgument(increment >= 0);
+        this.increment = increment;
+    }
+
+    @Override
+    protected IncrementTransactionSequenceRequestProxyV1 externalizableProxy(final ABIVersion version) {
+        return new IncrementTransactionSequenceRequestProxyV1(this);
+    }
+
+    @Override
+    protected IncrementTransactionSequenceRequest cloneAsVersion(final ABIVersion targetVersion) {
+        return this;
+    }
+
+    /**
+     * Return the sequence increment beyond this request's sequence.
+     *
+     * @return Sequence increment, guaranteed to be non-negative.
+     */
+    public long getIncrement() {
+        return increment;
+    }
+}
