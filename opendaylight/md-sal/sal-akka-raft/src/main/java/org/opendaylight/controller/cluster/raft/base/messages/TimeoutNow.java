b3da5eeb3fd2189745dd8c9bb78d0d34f0fb040d
Tom Pantelis <tompantelis@gmail.com>
2017-05-24 08:13:34 -0400

    Bug 5740: Change TimeoutNow to externalizable proxy
    Change-Id: I3b2289c258ffab288901b5cbf4e5032bc143dfc7
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/base/messages/TimeoutNow.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/base/messages/TimeoutNow.java
@@ -7,6 +7,10 @@
  */
 package org.opendaylight.controller.cluster.raft.base.messages;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.io.Serializable;
 
 /**
@@ -22,7 +26,29 @@ public final class TimeoutNow implements Serializable {
         // Hidden on purpose
     }
 
-    private Object readResolve() {
-        return INSTANCE;
+    private Object writeReplace() {
+        return new Proxy();
+    }
+
+    private static class Proxy implements Externalizable {
+        private static final long serialVersionUID = 1L;
+
+        // checkstyle flags the public modifier as redundant which really doesn't make sense since it clearly isn't
+        // redundant. It is explicitly needed for Java serialization to be able to create instances via reflection.
+        @SuppressWarnings("checkstyle:RedundantModifier")
+        public Proxy() {
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        }
+
+        private Object readResolve() {
+            return INSTANCE;
+        }
     }
 }
