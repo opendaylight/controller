e291baba764985e1b571c42d0bb80f6b859ee406
Tom Pantelis <tompantelis@gmail.com>
2017-05-24 08:02:00 -0400

    Bug 5740: Change RequestVote(Reply) to externalizable proxy
    The other RaftRPC classes havwe been converted to use the
    externalizable proxy pattern so we shoild convert RequestVote(Reply)
    as well.
    
    Change-Id: I0a2054d8426f66480f37061d1a9fc51464f705da
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/RequestVoteReply.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/messages/RequestVoteReply.java
@@ -8,6 +8,11 @@
 
 package org.opendaylight.controller.cluster.raft.messages;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 public final class RequestVoteReply extends AbstractRaftRPC {
     private static final long serialVersionUID = 8427899326488775660L;
 
@@ -26,5 +31,43 @@ public final class RequestVoteReply extends AbstractRaftRPC {
     @Override
     public String toString() {
         return "RequestVoteReply [term=" + getTerm() + ", voteGranted=" + voteGranted + "]";
+    }
+
+    private Object writeReplace() {
+        return new Proxy(this);
+    }
+
+    private static class Proxy implements Externalizable {
+        private static final long serialVersionUID = 1L;
+
+        private RequestVoteReply requestVoteReply;
+
+        // checkstyle flags the public modifier as redundant which really doesn't make sense since it clearly isn't
+        // redundant. It is explicitly needed for Java serialization to be able to create instances via reflection.
+        @SuppressWarnings("checkstyle:RedundantModifier")
+        public Proxy() {
+        }
+
+        Proxy(RequestVoteReply requestVoteReply) {
+            this.requestVoteReply = requestVoteReply;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeLong(requestVoteReply.getTerm());
+            out.writeBoolean(requestVoteReply.voteGranted);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            long term = in.readLong();
+            boolean voteGranted = in.readBoolean();
+
+            requestVoteReply = new RequestVoteReply(term, voteGranted);
+        }
+
+        private Object readResolve() {
+            return requestVoteReply;
+        }
     }
 }
