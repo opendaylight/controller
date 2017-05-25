5600273a8bbee5c724f99028dab2908f61249072
Tom Pantelis <tompantelis@gmail.com>
2017-05-25 09:39:40 -0400

    Bug 5740: Add Deque-based control-aware mailbox
    Since akka persistence uses stashing, it requires a mailbox to be
    Deque-based to provide the enqueueFirst method. However, the
    control-aware mailboxes provided by akka are not Deque-based so we
    need one that is.
    
    Change-Id: I74f214c725eff16aba093aad3f2f6eed80948ee4
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/UnboundedDequeBasedControlAwareMailbox.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.common.actor;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.dispatch.ControlMessage;
+import akka.dispatch.DequeBasedMessageQueueSemantics;
+import akka.dispatch.Envelope;
+import akka.dispatch.MailboxType;
+import akka.dispatch.ProducesMessageQueue;
+import akka.dispatch.UnboundedControlAwareMailbox;
+import com.typesafe.config.Config;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Option;
+
+/**
+ * An unbounded ControlAwareMailbox that also supports DequeBasedMessageQueueSemantics so it can be used with
+ * persistent actors which use stashing.
+ *
+ * @author Thomas Pantelis
+ */
+public class UnboundedDequeBasedControlAwareMailbox implements MailboxType,
+        ProducesMessageQueue<UnboundedDequeBasedControlAwareMailbox.MessageQueue> {
+    private static final Logger LOG = LoggerFactory.getLogger(UnboundedDequeBasedControlAwareMailbox.class);
+
+    public UnboundedDequeBasedControlAwareMailbox(ActorSystem.Settings settings, Config config) {
+    }
+
+    @Override
+    public MessageQueue create(Option<ActorRef> owner, Option<ActorSystem> system) {
+        LOG.debug("Creating MessageQueue for {}", owner);
+        return new MessageQueue();
+    }
+
+    static class MessageQueue extends UnboundedControlAwareMailbox.MessageQueue
+            implements DequeBasedMessageQueueSemantics {
+        private static final long serialVersionUID = 1L;
+
+        private final Deque<Envelope> controlQueue = new ConcurrentLinkedDeque<>();
+        private final Deque<Envelope> queue = new ConcurrentLinkedDeque<>();
+
+        @Override
+        public Queue<Envelope> controlQueue() {
+            return controlQueue;
+        }
+
+        @Override
+        public Queue<Envelope> queue() {
+            return queue;
+        }
+
+        @Override
+        public void enqueueFirst(ActorRef actor, Envelope envelope) {
+            final Object message = envelope.message();
+            LOG.debug("enqueueFirst: actor {}, message type: {}", actor, message.getClass());
+            if (message instanceof ControlMessage) {
+                LOG.debug("Adding {} to the ControlMessage queue", message.getClass());
+                controlQueue.addFirst(envelope);
+            } else {
+                LOG.debug("Adding {} to the normal queue", message.getClass());
+                queue.addFirst(envelope);
+            }
+        }
+    }
+}
