bbb4002904a1d93a3b8a7c07a14e9ddd4b4b80f6
Tomas Cere <tcere@cisco.com>
2017-06-14 15:42:07 +0200

    BUG 8629: Try to allow notification processing to finish in unsubscribe of listeners.
    Change-Id: I8638c6066b86b101484d3d80cd0fed146a478778
    Signed-off-by: Tomas Cere <tcere@cisco.com>

--- opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/impl/IdIntsListener.java
+++ opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/impl/IdIntsListener.java
@@ -9,7 +9,14 @@
 package org.opendaylight.controller.clustering.it.provider.impl;
 
 import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.SettableFuture;
 import java.util.Collection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import javax.annotation.Nonnull;
 import org.opendaylight.controller.md.sal.dom.api.ClusteredDOMDataTreeChangeListener;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
@@ -20,14 +27,20 @@ import org.slf4j.LoggerFactory;
 public class IdIntsListener implements ClusteredDOMDataTreeChangeListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(IdIntsListener.class);
+    private static final long SECOND_AS_NANO = 1000000000;
 
     private NormalizedNode<?, ?> localCopy = null;
+    private AtomicLong lastNotifTimestamp = new AtomicLong(0);
+    private ScheduledExecutorService executorService;
+    private ScheduledFuture<?> scheduledFuture;
 
     @Override
     public void onDataTreeChanged(@Nonnull final Collection<DataTreeCandidate> changes) {
 
         // There should only be one candidate reported
         Preconditions.checkState(changes.size() == 1);
+
+        lastNotifTimestamp.set(System.nanoTime());
 
         // do not log the change into debug, only use trace since it will lead to OOM on default heap settings
         LOG.debug("Received data tree changed");
@@ -57,5 +70,32 @@ public class IdIntsListener implements ClusteredDOMDataTreeChangeListener {
 
     public boolean checkEqual(final NormalizedNode<?, ?> expected) {
         return localCopy.equals(expected);
+    }
+
+    public Future<Void> tryFinishProcessing() {
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        final SettableFuture<Void> settableFuture = SettableFuture.create();
+
+        scheduledFuture = executorService.scheduleAtFixedRate(new CheckFinishedTask(settableFuture), 0, 1, TimeUnit.SECONDS);
+        return settableFuture;
+    }
+
+    private class CheckFinishedTask implements Runnable {
+
+        private final SettableFuture<Void> future;
+
+        public CheckFinishedTask(final SettableFuture<Void> future) {
+            this.future = future;
+        }
+
+        @Override
+        public void run() {
+            if (System.nanoTime() - lastNotifTimestamp.get() > SECOND_AS_NANO * 4) {
+                scheduledFuture.cancel(false);
+                future.set(null);
+
+                executorService.shutdown();
+            }
+        }
     }
 }
