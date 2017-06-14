e6922e3dc95c97418f7158350448fd6e9a74eb87
Robert Varga <robert.varga@pantheon.tech>
2017-06-14 12:38:20 +0200

    BUG-8618: make sure we refresh backend info
    When we are performing a reconnection attempt we must never use
    previous backend info, but rather have to refresh it.
    
    Fix this by removing state when resolution fails.
    
    Change-Id: I65592f2101547a606a15d9c8030c7d8c58afe8a5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit a816f13940862759cefcd6e69330c6a70b512ee2)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/ModuleShardBackendResolver.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/databroker/actors/dds/ModuleShardBackendResolver.java
@@ -69,21 +69,49 @@ final class ModuleShardBackendResolver extends AbstractShardBackendResolver {
         return cookie;
     }
 
-    private ShardState resolveBackendInfo(final Long cookie) {
-        final String shardName = shards.inverse().get(cookie);
-        if (shardName == null) {
-            LOG.warn("Failing request for non-existent cookie {}", cookie);
-            return null;
-        }
-
-        LOG.debug("Resolving cookie {} to shard {}", cookie, shardName);
-
-        return resolveBackendInfo(shardName, cookie);
-    }
 
     @Override
     public CompletionStage<ShardBackendInfo> getBackendInfo(final Long cookie) {
-        return backends.computeIfAbsent(cookie, this::resolveBackendInfo).getStage();
+        /*
+         * We cannot perform a simple computeIfAbsent() here because we need to control sequencing of when the state
+         * is inserted into the map and retired from it (based on the stage result).
+         *
+         * We do not want to hook another stage one processing completes and hooking a removal on failure from a compute
+         * method runs the inherent risk of stage completing before the insertion does (i.e. we have a removal of
+         * non-existent element.
+         */
+        final ShardState existing = backends.get(cookie);
+        if (existing != null) {
+            return existing.getStage();
+        }
+
+        final String shardName = shards.inverse().get(cookie);
+        if (shardName == null) {
+            LOG.warn("Failing request for non-existent cookie {}", cookie);
+            throw new IllegalArgumentException("Cookie " + cookie + " does not have a shard assigned");
+        }
+
+        LOG.debug("Resolving cookie {} to shard {}", cookie, shardName);
+        final ShardState toInsert = resolveBackendInfo(shardName, cookie);
+
+        final ShardState raced = backends.putIfAbsent(cookie, toInsert);
+        if (raced != null) {
+            // We have had a concurrent insertion, return that
+            LOG.debug("Race during insertion of state for cookie {} shard {}", cookie, shardName);
+            return raced.getStage();
+        }
+
+        // We have succeeded in populating the map, now we need to take care of pruning the entry if it fails to
+        // complete
+        final CompletionStage<ShardBackendInfo> stage = toInsert.getStage();
+        stage.whenComplete((info, failure) -> {
+            if (failure != null) {
+                LOG.debug("Resolution of cookie {} shard {} failed, removing state", cookie, shardName, failure);
+                backends.remove(cookie, toInsert);
+            }
+        });
+
+        return stage;
     }
 
     @Override
