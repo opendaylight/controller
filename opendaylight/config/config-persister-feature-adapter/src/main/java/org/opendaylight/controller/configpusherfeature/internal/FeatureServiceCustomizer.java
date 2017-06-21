5ee8bc2ace2d08ccf845f84f1d8ac1f304e8c187
Robert Varga <robert.varga@pantheon.tech>
2017-06-21 15:40:28 +0200

    Bump to odlparent 2.0.0
    This takes odlparent 2.0.0, adjusts for guava update and feature
    movement.
    
    Change-Id: If3e1289ed7f73a79a5a47428c634bda9702e824d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
+++ opendaylight/config/config-persister-feature-adapter/src/main/java/org/opendaylight/controller/configpusherfeature/internal/FeatureServiceCustomizer.java
@@ -9,7 +9,9 @@ package org.opendaylight.controller.configpusherfeature.internal;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Sets;
+
 import java.util.Set;
+
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesListener;
 import org.apache.karaf.features.FeaturesService;
@@ -19,8 +21,11 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<FeaturesService, FeaturesService>, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureServiceCustomizer.class);
     private ConfigPusher configPusher = null;
     private ServiceRegistration<?> registration;
 
@@ -34,10 +39,14 @@ public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<Featur
         final FeaturesService featureService = bc.getService(reference);
         final Optional<XmlFileStorageAdapter> currentPersister = XmlFileStorageAdapter.getInstance();
 
-        if(XmlFileStorageAdapter.getInstance().isPresent()) {
+        if (XmlFileStorageAdapter.getInstance().isPresent()) {
             final Set<String> installedFeatureIds = Sets.newHashSet();
-            for (final Feature installedFeature : featureService.listInstalledFeatures()) {
-                installedFeatureIds.add(installedFeature.getId());
+            try {
+                for (final Feature installedFeature : featureService.listInstalledFeatures()) {
+                    installedFeatureIds.add(installedFeature.getId());
+                }
+            } catch (final Exception e) {
+                LOG.error("Error listing installed features", e);
             }
 
             currentPersister.get().setFeaturesService(() -> installedFeatureIds);
@@ -49,19 +58,19 @@ public class FeatureServiceCustomizer implements ServiceTrackerCustomizer<Featur
 
     @Override
     public void modifiedService(final ServiceReference<FeaturesService> reference,
-            final FeaturesService service) {
+                                final FeaturesService service) {
         // we don't care if the properties change
     }
 
     @Override
     public void removedService(final ServiceReference<FeaturesService> reference,
-            final FeaturesService service) {
+                               final FeaturesService service) {
         close();
     }
 
     @Override
     public void close() {
-        if(registration != null) {
+        if (registration != null) {
             registration.unregister();
             registration = null;
         }
