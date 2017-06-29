c56f51bfb945e6153429c184d29eac8e099047dd
Vratko Polak <vrpolak@cisco.com>
2017-06-29 17:20:52 +0200

    Bug 8704: Introduce reconnect constructor for progress trackers
    Also, remove unused copy constructors (including unit test).
    
    Change-Id: Ib976343ed5f50c649ea08206c897cb70dead8b86
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/AveragingProgressTrackerTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/AveragingProgressTrackerTest.java
@@ -8,15 +8,12 @@
 package org.opendaylight.controller.cluster.access.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.google.common.testing.FakeTicker;
 import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
 public class AveragingProgressTrackerTest {
     private static final long CHECKER = TimeUnit.MILLISECONDS.toNanos(500);
@@ -66,24 +63,4 @@ public class AveragingProgressTrackerTest {
         assertEquals(0, averagingProgressTracker.estimateIsolatedDelay(ticker.read()));
     }
 
-    @Test
-    public void copyObjectTest() {
-        final AveragingProgressTracker copyAverageProgressTracker = new AveragingProgressTracker(
-                averagingProgressTracker);
-
-        // copied object is the same as original
-        assertTrue(new ReflectionEquals(averagingProgressTracker).matches(copyAverageProgressTracker));
-
-        // afterwards work of copied tracker is independent
-        averagingProgressTracker.openTask(ticker.read());
-
-        final long time = ticker.read();
-        assertNotEquals("Trackers are expected to return different results for tracking",
-                averagingProgressTracker.openTask(time), copyAverageProgressTracker.openTask(time));
-        assertNotEquals("Trackers are expected to encounter different amount of tasks",
-                averagingProgressTracker.tasksEncountered(), copyAverageProgressTracker.tasksEncountered());
-
-        // and copied object is then no more the same as original
-        assertFalse(new ReflectionEquals(averagingProgressTracker).matches(copyAverageProgressTracker));
-    }
 }
