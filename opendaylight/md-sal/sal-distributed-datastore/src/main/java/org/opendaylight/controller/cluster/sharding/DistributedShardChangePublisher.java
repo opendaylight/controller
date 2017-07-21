4cfb836c50fae331df35f4a280b52be0e4e0f8c1
Robert Varga <robert.varga@pantheon.tech>
2017-07-21 13:48:42 +0200

    BUG-8733: rework DistributedShardFrontend listeners
    As it turns out, we are doing a lot of unnecessary work when we
    are doing a lot unnecessary work for leaf shards. Leaf shards
    can reuse change events directly from the backend.
    
    Change-Id: I35f6e02f068867dbed477a3143abe595967bcb51
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/sharding/DistributedShardChangePublisher.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/sharding/DistributedShardChangePublisher.java
@@ -11,10 +11,8 @@ package org.opendaylight.controller.cluster.sharding;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -48,7 +46,7 @@ import org.opendaylight.yangtools.yang.data.impl.schema.tree.SchemaValidationFai
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DistributedShardChangePublisher
+class DistributedShardChangePublisher
         extends AbstractRegistrationTree<AbstractDOMDataTreeChangeListenerRegistration<?>>
         implements DOMStoreTreeChangePublisher {
 
@@ -120,11 +118,8 @@ public class DistributedShardChangePublisher
             setupListenerContext(final YangInstanceIdentifier listenerPath, final L listener) {
         // we need to register the listener registration path based on the shards root
         // we have to strip the shard path from the listener path and then register
-        YangInstanceIdentifier strippedIdentifier = listenerPath;
-        if (!prefix.getRootIdentifier().isEmpty()) {
-            strippedIdentifier = YangInstanceIdentifier.create(stripShardPath(prefix.getRootIdentifier(),
-                        listenerPath));
-        }
+        final YangInstanceIdentifier strippedIdentifier = AbstractShardChangePublisher.stripShardPath(
+            prefix.getRootIdentifier(), listenerPath);
 
         final DOMDataTreeListenerWithSubshards subshardListener =
                 new DOMDataTreeListenerWithSubshards(strippedIdentifier, listener);
@@ -178,27 +173,6 @@ public class DistributedShardChangePublisher
         addRegistration(node, registration);
 
         return registration;
-    }
-
-    private static Iterable<PathArgument> stripShardPath(final YangInstanceIdentifier shardPath,
-                                                         final YangInstanceIdentifier listenerPath) {
-        if (shardPath.isEmpty()) {
-            return listenerPath.getPathArguments();
-        }
-
-        final List<PathArgument> listenerPathArgs = new ArrayList<>(listenerPath.getPathArguments());
-        final Iterator<PathArgument> shardIter = shardPath.getPathArguments().iterator();
-        final Iterator<PathArgument> listenerIter = listenerPathArgs.iterator();
-
-        while (shardIter.hasNext()) {
-            if (shardIter.next().equals(listenerIter.next())) {
-                listenerIter.remove();
-            } else {
-                break;
-            }
-        }
-
-        return listenerPathArgs;
     }
 
     private static class ProxyRegistration implements ListenerRegistration<DOMDataTreeChangeListener> {
@@ -310,8 +284,8 @@ public class DistributedShardChangePublisher
 
         synchronized void onDataTreeChanged(final YangInstanceIdentifier pathFromRoot,
                                             final Collection<DataTreeCandidate> changes) {
-            final YangInstanceIdentifier changeId =
-                    YangInstanceIdentifier.create(stripShardPath(dataTree.getRootPath(), pathFromRoot));
+            final YangInstanceIdentifier changeId = AbstractShardChangePublisher.stripShardPath(dataTree.getRootPath(),
+                pathFromRoot);
 
             final List<DataTreeCandidate> newCandidates = changes.stream()
                     .map(candidate -> DataTreeCandidates.newDataTreeCandidate(changeId, candidate.getRootNode()))
