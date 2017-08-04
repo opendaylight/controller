52f2b0640f4ef76c3f25ec7d7a167b537f3f656e
Robert Varga <robert.varga@pantheon.tech>
2017-08-04 19:49:19 +0200

    WIP: BUG-8722: rework ModuleInfoBundleTracker
    Change-Id: I6d8990e57249a9148f14302a22bc71dd2072d83e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/config/config-manager/src/main/java/org/opendaylight/controller/config/manager/impl/osgi/mapping/ModuleInfoBundleTracker.java
+++ opendaylight/config/config-manager/src/main/java/org/opendaylight/controller/config/manager/impl/osgi/mapping/ModuleInfoBundleTracker.java
@@ -7,14 +7,21 @@
  */
 package org.opendaylight.controller.config.manager.impl.osgi.mapping;
 
+import static org.osgi.framework.BundleEvent.RESOLVED;
+import static org.osgi.framework.BundleEvent.STARTED;
+import static org.osgi.framework.BundleEvent.STARTING;
+import static org.osgi.framework.BundleEvent.UNINSTALLED;
+import static org.osgi.framework.BundleEvent.UNRESOLVED;
+import static org.osgi.framework.BundleEvent.UPDATED;
+
+import com.google.common.base.Verify;
 import com.google.common.io.Resources;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
 import org.opendaylight.yangtools.yang.binding.YangModelBindingProvider;
 import org.opendaylight.yangtools.yang.binding.YangModuleInfo;
@@ -71,53 +78,114 @@ public final class ModuleInfoBundleTracker implements AutoCloseable,
         }
     }
 
-    @Override
-    public Collection<ObjectRegistration<YangModuleInfo>> addingBundle(final Bundle bundle, final BundleEvent event) {
-        URL resource = bundle.getEntry(YANG_MODULE_INFO_SERVICE_PATH);
+    private Collection<ObjectRegistration<YangModuleInfo>> activateBundle(final Bundle bundle, final URL resource) {
         LOG.debug("Got addingBundle({}) with YangModelBindingProvider resource {}", bundle, resource);
-        if (resource == null) {
+
+        final Collection<String> services;
+        try {
+            services = Resources.readLines(resource, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOG.error("Error while reading {} from bundle {}", resource, bundle, e);
             return Collections.emptyList();
         }
-        List<ObjectRegistration<YangModuleInfo>> registrations = new LinkedList<>();
 
-        try {
-            for (String moduleInfoName : Resources.readLines(resource, StandardCharsets.UTF_8)) {
-                LOG.trace("Retrieve ModuleInfo({}, {})", moduleInfoName, bundle);
-                YangModuleInfo moduleInfo = retrieveModuleInfo(moduleInfoName, bundle);
-                registrations.add(moduleInfoRegistry.registerModuleInfo(moduleInfo));
+        final Collection<ObjectRegistration<YangModuleInfo>> registrations = new ArrayList<>(services.size());
+        for (String moduleInfoName : services) {
+            LOG.trace("Retrieve ModuleInfo({}, {})", moduleInfoName, bundle);
+
+            final ObjectRegistration<YangModuleInfo> reg;
+            try {
+                final YangModuleInfo moduleInfo = retrieveModuleInfo(moduleInfoName, bundle);
+                reg = moduleInfoRegistry.registerModuleInfo(moduleInfo);
+            } catch (final RuntimeException e) {
+                LOG.warn("Failed to process {} for bundle {}, attempting to continue", moduleInfoName, bundle, e);
+                continue;
             }
 
-            if(!starting) {
-                moduleInfoRegistry.updateService();
-            }
-        } catch (final IOException e) {
-            LOG.error("Error while reading {} from bundle {}", resource, bundle, e);
-        } catch (final RuntimeException e) {
-            LOG.error("Failed to process {} for bundle {}", resource, bundle, e);
+            registrations.add(reg);
         }
 
-        LOG.trace("Got following registrations {}", registrations);
+        LOG.trace("Bundle {} resolved to {}", registrations);
+        if (!starting && !registrations.isEmpty()) {
+            moduleInfoRegistry.updateService();
+        }
+
         return registrations;
+    }
+
+    private void deactivateBundle(final Bundle bundle, final Collection<ObjectRegistration<YangModuleInfo>> regs) {
+        LOG.debug("Bundle {} removing registrations", bundle);
+        regs.forEach(reg -> {
+            try {
+                reg.close();
+            } catch (final Exception e) {
+                LOG.warn("Unable to unregister YangModuleInfo {}, continuing", reg.getInstance(), e);
+            }
+        });
+        regs.clear();
+    }
+
+    @Override
+    public Collection<ObjectRegistration<YangModuleInfo>> addingBundle(final Bundle bundle, final BundleEvent event) {
+        LOG.debug("Adding bundle {} event {}", bundle, event);
+
+        final URL resource = bundle.getEntry(YANG_MODULE_INFO_SERVICE_PATH);
+        if (resource == null) {
+            LOG.debug("Bundle {} is missing {}, not tracking it", bundle, YANG_MODULE_INFO_SERVICE_PATH);
+            return null;
+        }
+        if (event == null) {
+            LOG.debug("Bundle {} has no event, postponing its processing", bundle);
+            return new ArrayList<>(0);
+        }
+
+        final int type = event.getType();
+        if ((type & (RESOLVED | STARTING | STARTED)) == 0) {
+            LOG.debug("Bundle {} has event type {}, postponing its processing", bundle, type);
+            return new ArrayList<>(0);
+        }
+
+        return activateBundle(bundle, resource);
     }
 
     @Override
     public void modifiedBundle(final Bundle bundle, final BundleEvent event,
             final Collection<ObjectRegistration<YangModuleInfo>> object) {
+        if (event == null) {
+            LOG.debug("Bundle {} modification has no event", bundle);
+            return;
+        }
+
+        final int type = event.getType();
+        LOG.debug("Bundle {} got event {}", bundle, type);
+        if ((type & (RESOLVED | STARTING | STARTED)) != 0) {
+            if (!object.isEmpty()) {
+                final URL resource = Verify.verifyNotNull(bundle.getEntry(YANG_MODULE_INFO_SERVICE_PATH));
+                object.addAll(activateBundle(bundle, resource));
+            } else {
+                LOG.debug("Bundle {} already has registrations, ignoring event {}", bundle, type);
+            }
+
+            return;
+        }
+
+        if (object.isEmpty()) {
+            LOG.debug("Bundle {} has empty registrations, ignoring event {}", bundle, type);
+            return;
+        }
+
+        if ((type & (UNRESOLVED | UPDATED | UNINSTALLED)) != 0) {
+            deactivateBundle(bundle, object);
+        } else {
+            LOG.debug("Bundle {} ignoring event {}", bundle, type);
+        }
     }
 
     @Override
     public void removedBundle(final Bundle bundle, final BundleEvent event,
             final Collection<ObjectRegistration<YangModuleInfo>> regs) {
-        if (regs == null) {
-            return;
-        }
-
-        for (ObjectRegistration<YangModuleInfo> reg : regs) {
-            try {
-                reg.close();
-            } catch (final Exception e) {
-                LOG.error("Unable to unregister YangModuleInfo {}", reg.getInstance(), e);
-            }
+        if (regs != null) {
+            deactivateBundle(bundle, regs);
         }
     }
 
