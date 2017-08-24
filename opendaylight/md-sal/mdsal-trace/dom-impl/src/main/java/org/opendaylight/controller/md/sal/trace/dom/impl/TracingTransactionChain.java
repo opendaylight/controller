4a09cad994fb38a044b6561dbd998721e44465da
Michael Vorburger <vorburger@redhat.com>
2017-08-24 19:24:59 +0200

    Bug 9034: TracingBroker with TracingTransactionChain
    This is one of a serious of (small, easy to review) commits which is
    part of a solution I'm proposing in order to be able to detect OOM
    issues such as Bug 9034, based on using the mdsal-trace DataBroker.
    
    Change-Id: I098c48a1fce1da2fdd0aafdc82fd3bef5626988a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/mdsal-trace/dom-impl/src/main/java/org/opendaylight/controller/md/sal/trace/dom/impl/TracingTransactionChain.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.trace.dom.impl;
+
+import java.util.Objects;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadWriteTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
+
+class TracingTransactionChain implements DOMTransactionChain {
+
+    private final DOMTransactionChain delegate;
+    private final TracingBroker tracingBroker;
+
+    TracingTransactionChain(DOMTransactionChain delegate, TracingBroker tracingBroker) {
+        this.delegate = Objects.requireNonNull(delegate);
+        this.tracingBroker = Objects.requireNonNull(tracingBroker);
+    }
+
+    @Override
+    public DOMDataReadOnlyTransaction newReadOnlyTransaction() {
+        return delegate.newReadOnlyTransaction();
+    }
+
+    @Override
+    public DOMDataReadWriteTransaction newReadWriteTransaction() {
+        return new TracingReadWriteTransaction(delegate.newReadWriteTransaction(), tracingBroker);
+    }
+
+    @Override
+    public DOMDataWriteTransaction newWriteOnlyTransaction() {
+        return new TracingWriteTransaction(delegate.newWriteOnlyTransaction(), tracingBroker);
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+}
