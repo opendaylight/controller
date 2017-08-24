e5c3e7c6d6597967b774758495167bd75238d046
Michael Vorburger <vorburger@redhat.com>
2017-08-24 22:37:51 +0200

    Bug 9034: TracingBroker with transaction-debug-context-enabled
    This is one of a serious of commits which is part of a solution I'm
    proposing in order to be able to detect OOM issues such as Bug 9034,
    based on using the mdsal-trace DataBroker.
    
    Change-Id: If62b7f76ea03d8cabe0c5a2088983275cfe50e44
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ opendaylight/md-sal/mdsal-trace/dom-impl/src/main/java/org/opendaylight/controller/md/sal/trace/closetracker/impl/AbstractCloseTracked.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.trace.closetracker.impl;
+
+import java.time.Instant;
+import javax.annotation.Nullable;
+
+/**
+ * Convenience abstract base class for {@link CloseTracked} implementors.
+ *
+ * @author Michael Vorburger.ch
+ */
+public abstract class AbstractCloseTracked<T extends AbstractCloseTracked<T>> implements CloseTracked<T> {
+
+    private final CloseTrackedTrait<T> closeTracker;
+
+    protected AbstractCloseTracked(CloseTrackedRegistry<T> transactionChainRegistry) {
+        this.closeTracker = new CloseTrackedTrait<>(transactionChainRegistry);
+    }
+
+    protected void removeFromTrackedRegistry() {
+        closeTracker.removeFromTrackedRegistry();
+    }
+
+    @Override
+    public final Instant getObjectCreated() {
+        return closeTracker.getObjectCreated();
+    }
+
+    @Override
+    public @Nullable Throwable getAllocationContext() {
+        return closeTracker.getAllocationContext();
+    }
+}
