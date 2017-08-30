96b616e26eeecc5187eb6b3ca8bd79ad905a333f
Jakub Toth <jakub.toth@pantheon.tech>
2017-08-30 16:29:46 +0200

    Bug 8967 - Various classes in cntrl/md-sal use hardcoded dependencies on OSGI/karaf - PART 1
      * remove dependency of ActorSystemProviderImpl on OSGI
      * move Config out of ActorSystemProviderImpl
    
    Change-Id: I7e9cb184385207726839e137ea6dcafd6359c47b
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/config/yang/config/actor_system_provider/impl/factory/ActorSystemPropsFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.config.yang.config.actor_system_provider.impl.factory;
+
+import akka.actor.Props;
+import org.opendaylight.controller.cluster.common.actor.QuarantinedMonitorActor;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActorSystemPropsFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(ActorSystemPropsFactory.class);
+
+    public static Props createProps(final BundleContext bundleContext) {
+        return QuarantinedMonitorActor.props(() -> {
+            // restart the entire karaf container
+            LOG.warn("Restarting karaf container");
+            System.setProperty("karaf.restart.jvm", "true");
+            bundleContext.getBundle().stop();
+        });
+    }
+}
