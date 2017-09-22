fccaf5f808dde2f249ebb53ddfcdd8da44f88ceb
Michael Vorburger <vorburger@redhat.com>
2017-09-23 00:27:29 +0200

    ForwardingRead[Only]/WriteTransaction implementations
    this came up during https://git.opendaylight.org/gerrit/#/c/63372/
    
    a bit similar (but otherwise unrelated) to my earlier
    https://git.opendaylight.org/gerrit/#/c/63135/
    
    Change-Id: If669f94b47bf41f49e54c66c6024aeff9805f8d6
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>
    (cherry picked from commit 702a44e462672d6e4f7e5bcd94def61b70be8009)

---
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ForwardingWriteTransaction.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.api;
+
+import com.google.common.collect.ForwardingObject;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * Utility {@link WriteTransaction} implementation which forwards all interface method
+ * invocation to a delegate instance.
+ */
+@SuppressWarnings("deprecation") // due to CheckedFuture & TransactionStatus
+public class ForwardingWriteTransaction extends ForwardingObject implements WriteTransaction {
+
+    private final WriteTransaction delegate;
+
+    protected ForwardingWriteTransaction(WriteTransaction delegate) {
+        super();
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected WriteTransaction delegate() {
+        return delegate;
+    }
+
+    @Override
+    public <T extends DataObject> void put(LogicalDatastoreType store, InstanceIdentifier<T> path, T data) {
+        delegate.put(store, path, data);
+    }
+
+    @Override
+    public <T extends DataObject> void put(LogicalDatastoreType store, InstanceIdentifier<T> path, T data,
+            boolean createMissingParents) {
+        delegate.put(store, path, data, createMissingParents);
+    }
+
+    @Override
+    public <T extends DataObject> void merge(LogicalDatastoreType store, InstanceIdentifier<T> path, T data) {
+        delegate.merge(store, path, data);
+    }
+
+    @Override
+    public <T extends DataObject> void merge(LogicalDatastoreType store, InstanceIdentifier<T> path, T data,
+            boolean createMissingParents) {
+        delegate.merge(store, path, data, createMissingParents);
+    }
+
+    @Override
+    public void delete(LogicalDatastoreType store, InstanceIdentifier<?> path) {
+        delegate.delete(store, path);
+    }
+
+    @Override
+    public boolean cancel() {
+        return delegate.cancel();
+    }
+
+    @Override
+    public CheckedFuture<Void, TransactionCommitFailedException> submit() {
+        return delegate.submit();
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<TransactionStatus>> commit() {
+        return delegate.commit();
+    }
+
+    @Override
+    public Object getIdentifier() {
+        return delegate.getIdentifier();
+    }
+
+}
