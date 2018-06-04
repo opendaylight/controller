5ebbbea91ab437b8dd815bbcecd9d408c2ab0608
Tom Pantelis <tompantelis@gmail.com>
2018-06-04 17:46:56 -0400

    Remove CSS modules
    The CSS features are still installed until we can work
    out the ModuleInfoBackedContext/ClassLoaderStrategy situation
    that is currently provided by the ConfigManagerActivator.
    
    Change-Id: Id3b2cc287d17532482a2dd893c41e2dcd8d29ff0
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/BlueprintContainerRestartServiceImpl.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/BlueprintContainerRestartServiceImpl.java
@@ -7,12 +7,9 @@
  */
 package org.opendaylight.controller.blueprint;
 
-import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import java.lang.management.ManagementFactory;
-import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,34 +18,14 @@ import java.util.Deque;
 import java.util.Hashtable;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.Nullable;
-import javax.management.InstanceNotFoundException;
-import javax.management.ObjectName;
-import javax.xml.parsers.ParserConfigurationException;
 import org.apache.aries.blueprint.services.BlueprintExtenderService;
 import org.apache.aries.quiesce.participant.QuiesceParticipant;
 import org.apache.aries.util.AriesFrameworkUtil;
-import org.opendaylight.controller.config.api.ConfigRegistry;
-import org.opendaylight.controller.config.api.ConflictingVersionException;
-import org.opendaylight.controller.config.api.ModuleIdentifier;
-import org.opendaylight.controller.config.api.ValidationException;
-import org.opendaylight.controller.config.facade.xml.ConfigExecution;
-import org.opendaylight.controller.config.facade.xml.ConfigSubsystemFacade;
-import org.opendaylight.controller.config.facade.xml.ConfigSubsystemFacadeFactory;
-import org.opendaylight.controller.config.facade.xml.TestOption;
-import org.opendaylight.controller.config.facade.xml.mapping.config.Config;
-import org.opendaylight.controller.config.facade.xml.strategy.EditStrategyType;
-import org.opendaylight.controller.config.util.ConfigRegistryJMXClient;
-import org.opendaylight.controller.config.util.xml.DocumentedException;
-import org.opendaylight.controller.config.util.xml.XmlElement;
-import org.opendaylight.controller.config.util.xml.XmlMappingConstants;
-import org.opendaylight.controller.config.util.xml.XmlUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -57,8 +34,6 @@ import org.osgi.service.blueprint.container.BlueprintEvent;
 import org.osgi.service.blueprint.container.BlueprintListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  * Implementation of the BlueprintContainerRestartService.
@@ -68,9 +43,6 @@ import org.w3c.dom.Element;
 class BlueprintContainerRestartServiceImpl implements AutoCloseable, BlueprintContainerRestartService {
     private static final Logger LOG = LoggerFactory.getLogger(BlueprintContainerRestartServiceImpl.class);
     private static final int CONTAINER_CREATE_TIMEOUT_IN_MINUTES = 5;
-    private static final String CONFIG_MODULE_NAMESPACE_PROP = "config-module-namespace";
-    private static final String CONFIG_MODULE_NAME_PROP = "config-module-name";
-    private static final String CONFIG_INSTANCE_NAME_PROP = "config-instance-name";
 
     private final ExecutorService restartExecutor = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder()
             .setDaemon(true).setNameFormat("BlueprintContainerRestartService").build());
@@ -117,8 +89,7 @@ class BlueprintContainerRestartServiceImpl implements AutoCloseable, BlueprintCo
 
         // We use a LinkedHashSet to preserve insertion order as we walk the service usage hierarchy.
         Set<Bundle> containerBundlesSet = new LinkedHashSet<>();
-        List<Entry<String, ModuleIdentifier>> configModules = new ArrayList<>();
-        findDependentContainersRecursively(forBundle, containerBundlesSet, configModules);
+        findDependentContainersRecursively(forBundle, containerBundlesSet);
 
         List<Bundle> containerBundles = new ArrayList<>(containerBundlesSet);
 
@@ -164,9 +135,6 @@ class BlueprintContainerRestartServiceImpl implements AutoCloseable, BlueprintCo
         }
 
         AriesFrameworkUtil.safeUnregisterService(eventHandlerReg);
-
-        // Now restart any associated config system Modules.
-        restartConfigModules(forBundle.getBundleContext(), configModules);
 
         LOG.info("Finished restarting blueprint containers for bundle {} and its dependent bundles", forBundle);
     }
@@ -227,90 +195,14 @@ class BlueprintContainerRestartServiceImpl implements AutoCloseable, BlueprintCo
         });
     }
 
-    private void restartConfigModules(final BundleContext bundleContext, final List<Entry<String,
-            ModuleIdentifier>> configModules) {
-        if (configModules.isEmpty()) {
-            return;
-        }
-
-        ServiceReference<ConfigSubsystemFacadeFactory> configFacadeFactoryRef = bundleContext
-                .getServiceReference(ConfigSubsystemFacadeFactory.class);
-        if (configFacadeFactoryRef == null) {
-            LOG.debug("ConfigSubsystemFacadeFactory service reference not found");
-            return;
-        }
-
-        ConfigSubsystemFacadeFactory configFacadeFactory = bundleContext.getService(configFacadeFactoryRef);
-        if (configFacadeFactory == null) {
-            LOG.debug("ConfigSubsystemFacadeFactory service not found");
-            return;
-        }
-
-        try (ConfigSubsystemFacade configFacade = configFacadeFactory.createFacade(
-                "BlueprintContainerRestartService")) {
-            restartConfigModules(configModules, configFacade);
-        } catch (ParserConfigurationException | DocumentedException | ValidationException
-                | ConflictingVersionException e) {
-            LOG.error("Error restarting config modules", e);
-        } finally {
-            bundleContext.ungetService(configFacadeFactoryRef);
-        }
-
-    }
-
-    private void restartConfigModules(final List<Entry<String, ModuleIdentifier>> configModules,
-            final ConfigSubsystemFacade configFacade) throws ParserConfigurationException, DocumentedException,
-                    ValidationException, ConflictingVersionException {
-
-        Document document = XmlUtil.newDocument();
-        Element dataElement = XmlUtil.createElement(document, XmlMappingConstants.DATA_KEY, Optional.<String>absent());
-        Element modulesElement = XmlUtil.createElement(document, XmlMappingConstants.MODULES_KEY,
-                Optional.of(XmlMappingConstants.URN_OPENDAYLIGHT_PARAMS_XML_NS_YANG_CONTROLLER_CONFIG));
-        dataElement.appendChild(modulesElement);
-
-        Config configMapping = configFacade.getConfigMapping();
-
-        ConfigRegistry configRegistryClient = new ConfigRegistryJMXClient(ManagementFactory.getPlatformMBeanServer());
-        for (Entry<String, ModuleIdentifier> entry : configModules) {
-            String moduleNamespace = entry.getKey();
-            ModuleIdentifier moduleId = entry.getValue();
-            try {
-                ObjectName instanceON = configRegistryClient.lookupConfigBean(moduleId.getFactoryName(),
-                        moduleId.getInstanceName());
-
-                LOG.debug("Found config module instance ObjectName: {}", instanceON);
-
-                Element moduleElement = configMapping.moduleToXml(moduleNamespace, moduleId.getFactoryName(),
-                        moduleId.getInstanceName(), instanceON, document);
-                modulesElement.appendChild(moduleElement);
-            } catch (final InstanceNotFoundException e) {
-                LOG.warn("Error looking up config module: namespace {}, module name {}, instance {}",
-                        moduleNamespace, moduleId.getFactoryName(), moduleId.getInstanceName(), e);
-            }
-        }
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Pushing config xml: {}", XmlUtil.toString(dataElement));
-        }
-
-        ConfigExecution execution = new ConfigExecution(configMapping, XmlElement.fromDomElement(dataElement),
-                TestOption.testThenSet, EditStrategyType.recreate);
-        configFacade.executeConfigExecution(execution);
-        configFacade.commitSilentTransaction();
-    }
-
     /**
      * Recursively finds the services registered by the given bundle and the bundles using those services.
-     * User bundles that have an associated blueprint container are added to containerBundles. In addition,
-     * if a registered service has an associated config system Module, as determined via the presence of
-     * certain service properties, the ModuleIdentifier is added to the configModules list.
+     * User bundles that have an associated blueprint container are added to containerBundles.
      *
      * @param bundle the bundle to traverse
      * @param containerBundles the current set of bundles containing blueprint containers
-     * @param configModules the current set of bundles containing config modules
      */
-    private void findDependentContainersRecursively(final Bundle bundle, final Set<Bundle> containerBundles,
-            final List<Entry<String, ModuleIdentifier>> configModules) {
+    private void findDependentContainersRecursively(final Bundle bundle, final Set<Bundle> containerBundles) {
         if (!containerBundles.add(bundle)) {
             // Already seen this bundle...
             return;
@@ -319,54 +211,16 @@ class BlueprintContainerRestartServiceImpl implements AutoCloseable, BlueprintCo
         ServiceReference<?>[] references = bundle.getRegisteredServices();
         if (references != null) {
             for (ServiceReference<?> reference : references) {
-                possiblyAddConfigModuleIdentifier(reference, configModules);
-
                 Bundle[] usingBundles = reference.getUsingBundles();
                 if (usingBundles != null) {
                     for (Bundle usingBundle : usingBundles) {
                         if (blueprintExtenderService.getContainer(usingBundle) != null) {
-                            findDependentContainersRecursively(usingBundle, containerBundles, configModules);
+                            findDependentContainersRecursively(usingBundle, containerBundles);
                         }
                     }
                 }
             }
         }
-    }
-
-    private void possiblyAddConfigModuleIdentifier(final ServiceReference<?> reference,
-            final List<Entry<String, ModuleIdentifier>> configModules) {
-        Object moduleNamespace = reference.getProperty(CONFIG_MODULE_NAMESPACE_PROP);
-        if (moduleNamespace == null) {
-            return;
-        }
-
-        String moduleName = getRequiredConfigModuleProperty(CONFIG_MODULE_NAME_PROP, moduleNamespace,
-                reference);
-        String instanceName = getRequiredConfigModuleProperty(CONFIG_INSTANCE_NAME_PROP, moduleNamespace,
-                reference);
-        if (moduleName == null || instanceName == null) {
-            return;
-        }
-
-        LOG.debug("Found service with config module: namespace {}, module name {}, instance {}",
-                moduleNamespace, moduleName, instanceName);
-
-        configModules.add(new SimpleEntry<>(moduleNamespace.toString(),
-                new ModuleIdentifier(moduleName, instanceName)));
-    }
-
-    @Nullable
-    private String getRequiredConfigModuleProperty(final String propName, final Object moduleNamespace,
-            final ServiceReference<?> reference) {
-        Object value = reference.getProperty(propName);
-        if (value == null) {
-            LOG.warn(
-                "OSGi service with {} property is missing property {} therefore the config module can't be restarted",
-                CONFIG_MODULE_NAMESPACE_PROP, propName);
-            return null;
-        }
-
-        return value.toString();
     }
 
     private ServiceRegistration<?> registerEventHandler(final BundleContext bundleContext,
