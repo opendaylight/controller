5d02f58f225f9ffbd5f3173e4674a0c8c39e088f
Michael Vorburger <vorburger@redhat.com>
2018-06-21 16:49:20 +0200

    rm @Override methods with (only) super. in BindingToNormalizedNodeCodec
    Change-Id: I64f60da748260463bf83942bdcd68f718ac1e932
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingToNormalizedNodeCodec.java
+++ opendaylight/md-sal/sal-binding-broker/src/main/java/org/opendaylight/controller/md/sal/binding/impl/BindingToNormalizedNodeCodec.java
@@ -7,25 +7,16 @@
  */
 package org.opendaylight.controller.md.sal.binding.impl;
 
-import com.google.common.collect.ImmutableBiMap;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.lang.reflect.Method;
 import java.util.Iterator;
-import java.util.Map;
-import javax.annotation.Nonnull;
 import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizationException;
 import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizationOperation;
 import org.opendaylight.controller.md.sal.common.impl.util.compat.DataNormalizer;
-import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTreeNode;
 import org.opendaylight.mdsal.binding.dom.codec.impl.BindingNormalizedNodeCodecRegistry;
 import org.opendaylight.mdsal.binding.generator.api.ClassLoadingStrategy;
-import org.opendaylight.yangtools.yang.binding.DataObject;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 @SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "Migration path")
@@ -46,17 +37,6 @@ public class BindingToNormalizedNodeCodec
 
     DataNormalizer getDataNormalizer() {
         return this.legacyToNormalized;
-    }
-
-    @Override
-    public YangInstanceIdentifier toYangInstanceIdentifierBlocking(
-            final InstanceIdentifier<? extends DataObject> binding) {
-        return super.toYangInstanceIdentifierBlocking(binding);
-    }
-
-    @Override
-    public YangInstanceIdentifier toYangInstanceIdentifierCached(final InstanceIdentifier<?> binding) {
-        return super.toYangInstanceIdentifierCached(binding);
     }
 
     @Override
@@ -84,17 +64,5 @@ public class BindingToNormalizedNodeCodec
             }
         }
         return currentOp.createDefault(path.getLastPathArgument());
-    }
-
-    @Override
-    public ImmutableBiMap<Method, RpcDefinition> getRpcMethodToSchema(final Class<? extends RpcService> key) {
-        return super.getRpcMethodToSchema(key);
-    }
-
-    @Override
-    @Nonnull
-    public Map.Entry<InstanceIdentifier<?>, BindingCodecTreeNode<?>> getSubtreeCodec(
-            final YangInstanceIdentifier domIdentifier) {
-        return super.getSubtreeCodec(domIdentifier);
     }
 }
