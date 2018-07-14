b8293b9eb7eb0601d3dfa1fbe0816c0a461d91b9
Tom Pantelis <tompantelis@gmail.com>
2018-07-13 21:42:46 -0400

    Restore binding RPC shortcut
    The binding -> binding RPC serialization shortcut was broken by
    recent changes so restore it. The LazyDOMRpcResultFuture from
    the binding adapter is preserved by DOMRpcRouter via intermediate
    DOMRpcResult adapters.
    
    Change-Id: I98942471f4e8430d0043f5c09cb8a38ce2a5d7b9
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ opendaylight/md-sal/sal-dom-compat/src/main/java/org/opendaylight/controller/sal/core/compat/LegacyDOMRpcResultFutureAdapter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.core.compat;
+
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcException;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementationNotAvailableException;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
+import org.opendaylight.controller.md.sal.dom.api.DefaultDOMRpcException;
+import org.opendaylight.controller.md.sal.dom.spi.DefaultDOMRpcResult;
+import org.opendaylight.yangtools.util.concurrent.ExceptionMapper;
+
+/**
+ * Adapts a {@link org.opendaylight.mdsal.dom.api.DOMRpcResult} CheckedFuture to a {@link DOMRpcResult} CheckedFuture.
+ *
+ * @author Thomas Pantelis
+ */
+public class LegacyDOMRpcResultFutureAdapter extends AbstractDOMRpcResultFutureAdapter<DOMRpcResult, DOMRpcException,
+        org.opendaylight.mdsal.dom.api.DOMRpcResult, org.opendaylight.mdsal.dom.api.DOMRpcException> {
+
+    private static final ExceptionMapper<DOMRpcException> LEGACY_DOM_RPC_EX_MAPPER =
+            new ExceptionMapper<DOMRpcException>("rpc", DOMRpcException.class) {
+        @Override
+        protected DOMRpcException newWithCause(String message, Throwable cause) {
+            return cause instanceof DOMRpcException ? (DOMRpcException)cause
+                : cause instanceof org.opendaylight.mdsal.dom.api.DOMRpcImplementationNotAvailableException
+                    ? new DOMRpcImplementationNotAvailableException(cause.getMessage(), cause.getCause())
+                        : new DefaultDOMRpcException("RPC failed", cause);
+        }
+    };
+
+    public LegacyDOMRpcResultFutureAdapter(CheckedFuture<org.opendaylight.mdsal.dom.api.DOMRpcResult,
+            org.opendaylight.mdsal.dom.api.DOMRpcException> delegate) {
+        super(delegate, LEGACY_DOM_RPC_EX_MAPPER);
+    }
+
+    @Override
+    protected DOMRpcResult transform(org.opendaylight.mdsal.dom.api.DOMRpcResult fromResult) {
+        return new DefaultDOMRpcResult(fromResult.getResult(), fromResult.getErrors());
+    }
+}
---
+++ opendaylight/md-sal/sal-dom-compat/src/main/java/org/opendaylight/controller/sal/core/compat/MdsalDOMRpcResultFutureAdapter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.core.compat;
+
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.mdsal.dom.api.DOMRpcException;
+import org.opendaylight.mdsal.dom.api.DOMRpcResult;
+import org.opendaylight.mdsal.dom.api.DefaultDOMRpcException;
+import org.opendaylight.mdsal.dom.spi.DefaultDOMRpcResult;
+import org.opendaylight.yangtools.util.concurrent.ExceptionMapper;
+
+/**
+ * Adapts a {@link org.opendaylight.controller.md.sal.dom.api.DOMRpcResult} CheckedFuture to a
+ * {@link DOMRpcResult} CheckedFuture.
+ *
+ * @author Thomas Pantelis
+ */
+public class MdsalDOMRpcResultFutureAdapter extends AbstractDOMRpcResultFutureAdapter<
+        DOMRpcResult, DOMRpcException, org.opendaylight.controller.md.sal.dom.api.DOMRpcResult,
+            org.opendaylight.controller.md.sal.dom.api.DOMRpcException> {
+    private static final ExceptionMapper<DOMRpcException> MDSAL_DOM_RPC_EX_MAPPER =
+            new ExceptionMapper<DOMRpcException>("rpc", DOMRpcException.class) {
+        @Override
+        protected DOMRpcException newWithCause(String message, Throwable cause) {
+            return cause instanceof DOMRpcException ? (DOMRpcException) cause
+                    : new DefaultDOMRpcException("RPC failed", cause);
+        }
+    };
+
+    public MdsalDOMRpcResultFutureAdapter(CheckedFuture<org.opendaylight.controller.md.sal.dom.api.DOMRpcResult,
+            org.opendaylight.controller.md.sal.dom.api.DOMRpcException> delegate) {
+        super(delegate, MDSAL_DOM_RPC_EX_MAPPER);
+    }
+
+    @Override
+    protected DOMRpcResult transform(org.opendaylight.controller.md.sal.dom.api.DOMRpcResult fromResult) {
+        return new DefaultDOMRpcResult(fromResult.getResult(), fromResult.getErrors());
+    }
+}
