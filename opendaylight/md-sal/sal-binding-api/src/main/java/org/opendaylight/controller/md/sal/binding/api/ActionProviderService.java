4f1acb8169d38b8e2af51fadbeb7e86e8d7eec58
Robert Varga <robert.varga@pantheon.tech>
2018-07-28 12:56:34 +0200

    Add (DOM)Action(Provider) bridges
    We need to add bridge interfaces which allow NETCONF to expose
    DOMActionService through Controller's mountpoints. Also add binding
    equivalents, so users of Controller Binding have access to them.
    
    JIRA: MDSAL-283
    Change-Id: Ie6b0819345928637df29c5a15bf1634f8932de19
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-binding-api/src/main/java/org/opendaylight/controller/md/sal/binding/api/ActionProviderService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.md.sal.binding.api;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+/**
+ * Bridge to action registration.
+ *
+ * @deprecated Use {@link org.opendaylight.mdsal.binding.api.ActionProviderService} instead
+ */
+@Deprecated
+@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_INTERFACE", justification = "Migration")
+public interface ActionProviderService
+        extends BindingService, org.opendaylight.mdsal.binding.api.ActionProviderService {
+
+}
