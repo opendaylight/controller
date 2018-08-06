cc49be923975afdffc21941a86e3f5f551351856
Robert Varga <robert.varga@pantheon.tech>
2018-08-06 15:27:49 +0200

    WIP: Neon MRI changes
    This patch bumps:
    - odlparent to 4.0.0-SNAPSHOT
    - yangtools to 2.1.0-SNAPSHOT
    - mdsal to 2.7.0-SNAPSHOT
    
    Change-Id: I31b2d011015846537a99f963ded1d38e7b29d71e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/TransmittingTransmitQueueTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/TransmittingTransmitQueueTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -250,7 +250,7 @@ public class TransmittingTransmitQueueTest extends AbstractTransmitQueueTest<Tra
     @Test
     public void testSlicingFailureOnTransmit() {
         doAnswer(invocation -> {
-            invocation.getArgumentAt(0, SliceOptions.class).getOnFailureCallback().accept(new Exception("mock"));
+            invocation.<SliceOptions>getArgument(0).getOnFailureCallback().accept(new Exception("mock"));
             return Boolean.FALSE;
         }).when(mockMessageSlicer).slice(any());
 
