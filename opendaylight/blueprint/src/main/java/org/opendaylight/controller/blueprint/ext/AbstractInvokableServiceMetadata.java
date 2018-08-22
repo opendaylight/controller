8e9b2e757acc4049719e38031ece26ee677758ca
Tom Pantelis <tompantelis@gmail.com>
2018-08-22 12:45:23 -0400

    Lower warn level in AbstractInvokableServiceMetadata
    The warning "interface interface X has no accptable entries, assuming it is satisfied"
    is emmitted quite often from RpcServiceMetadata, ie for every routed RPC I believe.
    I'm unclear as to why it filters out routed RPCs here and why it's a warning but
    we should at least reduce to debug to avoid the noise.
    
    Change-Id: I954a4b7c6b2a5e0d3fdbe7bbd2cc44b47ef01fdc
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/AbstractInvokableServiceMetadata.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/ext/AbstractInvokableServiceMetadata.java
@@ -91,7 +91,7 @@ abstract class AbstractInvokableServiceMetadata extends AbstractDependentCompone
         final Collection<SchemaPath> schemaPaths = RpcUtil.decomposeRpcService(rpcInterface, schemaContext,
             rpcFilter());
         if (schemaPaths.isEmpty()) {
-            log.warn("{}: interface {} has no accptable entries, assuming it is satisfied", logName(), rpcInterface);
+            log.debug("{}: interface {} has no acceptable entries, assuming it is satisfied", logName(), rpcInterface);
             setSatisfied();
             return;
         }
