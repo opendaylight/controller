bf721aabb854f13210487dcaed84d019ffacf6f0
Robert Varga <robert.varga@pantheon.tech>
2018-08-22 13:05:08 +0200

    Cleanup TestKit use
    Do not subclass TestKit, but instantiate it as a common resource.
    Also use static imports for mocking/asserts. This makes the tests
    cleaner and less verbose.
    
    Change-Id: I18d8765c8fb5019c9f4aa94bc9474277443a3097
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/sharding/RoleChangeListenerActorTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/sharding/RoleChangeListenerActorTest.java
@@ -5,12 +5,11 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.controller.cluster.sharding;
 
 import static akka.actor.ActorRef.noSender;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
@@ -30,15 +29,12 @@ public class RoleChangeListenerActorTest extends AbstractActorTest {
 
     @Test
     public void testRegisterRoleChangeListenerOnStart() {
-        new TestKit(getSystem()) {
-            {
-                final LeaderLocationListener listener = mock(LeaderLocationListener.class);
-                final Props props = RoleChangeListenerActor.props(getRef(), listener);
+        final TestKit testKit = new TestKit(getSystem());
+        final LeaderLocationListener listener = mock(LeaderLocationListener.class);
+        final Props props = RoleChangeListenerActor.props(testKit.getRef(), listener);
 
-                getSystem().actorOf(props, "testRegisterRoleChangeListenerOnStart");
-                expectMsgClass(RegisterRoleChangeListener.class);
-            }
-        };
+        getSystem().actorOf(props, "testRegisterRoleChangeListenerOnStart");
+        testKit.expectMsgClass(RegisterRoleChangeListener.class);
     }
 
     @Test
@@ -57,6 +53,5 @@ public class RoleChangeListenerActorTest extends AbstractActorTest {
 
         subject.tell(new LeaderStateChanged("member-1", "member-2", (short) 0), noSender());
         verify(listener, timeout(5000)).onLeaderLocationChanged(eq(LeaderLocation.REMOTE));
-
     }
 }
