fdeb3caf7e5cfc223433e99aa83bfa0b00af2e49
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-09-08 20:44:58 -0400

    Bootstrap Controller documentation
    Issue: DOCS-69
    Change-Id: Iaf57d73e8d720b910c66cae41a75f10ceefba80f
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+from docs_conf.conf import *
+
---
+++ docs/index.rst
@@ -0,0 +1,5 @@
+.. _controller:
+
+########################
+Controller Documentation
+########################
