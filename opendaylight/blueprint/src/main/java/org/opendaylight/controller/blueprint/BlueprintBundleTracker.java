0d159f7a1cfa650930698cec0cf672dc35cad012
Robert Varga <robert.varga@pantheon.tech>
2018-10-24 23:01:25 +0200

    Force UntrustedXML to be initialized from activator
    Touching UntrustedXML during bundle activator should cause it to
    bind to the JRE XML provider rather than discovering other members
    via a TCCL.
    
    Change-Id: I9c6625be7873ed96d5e292a3165e7f319ac68b56
    JIRA: CONTROLLER-1867
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/BlueprintBundleTracker.java
+++ opendaylight/blueprint/src/main/java/org/opendaylight/controller/blueprint/BlueprintBundleTracker.java
@@ -22,6 +22,7 @@ import org.apache.aries.blueprint.services.BlueprintExtenderService;
 import org.apache.aries.quiesce.participant.QuiesceParticipant;
 import org.apache.aries.util.AriesFrameworkUtil;
 import org.opendaylight.controller.blueprint.ext.OpendaylightNamespaceHandler;
+import org.opendaylight.yangtools.util.xml.UntrustedXML;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -72,6 +73,9 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
     @Override
     public void start(final BundleContext context) {
         LOG.info("Starting {}", getClass().getSimpleName());
+
+        // CONTROLLER-1867: force UntrustedXML initialization, so that it uses our TCCL to initialize
+        UntrustedXML.newDocumentBuilder();
 
         restartService = new BlueprintContainerRestartServiceImpl();
 
@@ -231,7 +235,7 @@ public class BlueprintBundleTracker implements BundleActivator, BundleTrackerCus
      * @param event the event to handle
      */
     @Override
-    public void blueprintEvent(BlueprintEvent event) {
+    public void blueprintEvent(final BlueprintEvent event) {
         if (event.getType() == BlueprintEvent.CREATED) {
             LOG.info("Blueprint container for bundle {} was successfully created", event.getBundle());
             return;
