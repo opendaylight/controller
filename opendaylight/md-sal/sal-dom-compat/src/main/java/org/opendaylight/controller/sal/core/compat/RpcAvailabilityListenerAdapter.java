ee690538cbdafb3cbe58bc5f46b656a2aa20e95a
Jakub Morvay <jmorvay@frinx.io>
2018-11-06 19:17:44 +0100

    Proxy controller services in DOMMountPointServiceImpl
    When we are proxying a DOMMountPoint, we also need to take care
    of bridging legacy interfaces on top of MD-SAL interfaces.
    
    This patch refactors DOM{Notification,Rpc}Router to factor out
    DOM{Notification,Rpc}Service adapters and creates DOMActionService
    adapter. These are then used in DOMMountPointAdapter, which in
    turn is used by DOMMountPointServiceImpl.
    
    This deals with the case when an MD-SAL-registered mount point
    is accessed via Controller APIs.
    
    JIRA: CONTROLLER-1869
    Change-Id: Ide4c636afdbd13e233880ab616aec52e46ea115f
    Signed-off-by: Jakub Morvay <jmorvay@frinx.io>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit ddbc3eb6ae763f10d466bfdd1c33f0cb30665bc0)

---
+++ opendaylight/md-sal/sal-dom-compat/src/main/java/org/opendaylight/controller/sal/core/compat/RpcAvailabilityListenerAdapter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sal.core.compat;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ForwardingObject;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcAvailabilityListener;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcIdentifier;
+
+@Deprecated
+public class RpcAvailabilityListenerAdapter<T extends DOMRpcAvailabilityListener> extends ForwardingObject
+        implements org.opendaylight.mdsal.dom.api.DOMRpcAvailabilityListener {
+    private final @NonNull T delegate;
+
+    public RpcAvailabilityListenerAdapter(final T delegate) {
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public void onRpcAvailable(final Collection<org.opendaylight.mdsal.dom.api.DOMRpcIdentifier> rpcs) {
+        delegate.onRpcAvailable(convert(rpcs));
+    }
+
+    @Override
+    public void onRpcUnavailable(final Collection<org.opendaylight.mdsal.dom.api.DOMRpcIdentifier> rpcs) {
+        delegate.onRpcUnavailable(convert(rpcs));
+    }
+
+    @Override
+    protected T delegate() {
+        return delegate;
+    }
+
+    private static @NonNull Collection<DOMRpcIdentifier> convert(
+            final Collection<org.opendaylight.mdsal.dom.api.DOMRpcIdentifier> from) {
+        return from.stream().map(RpcAvailabilityListenerAdapter::convert).collect(Collectors.toList());
+    }
+
+    private static @NonNull DOMRpcIdentifier convert(final org.opendaylight.mdsal.dom.api.DOMRpcIdentifier from) {
+        return DOMRpcIdentifier.create(from.getType(), from.getContextReference());
+    }
+}
