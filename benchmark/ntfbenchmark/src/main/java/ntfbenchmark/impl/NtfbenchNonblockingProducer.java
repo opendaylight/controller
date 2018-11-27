70dfa437ef06c2a4ca19557a1ae5e9fc8a1324a3
Robert Varga <robert.varga@pantheon.tech>
2018-11-27 09:19:09 +0100

    Migrate ntfbenchmark to MDSAL APIs
    This is a trivial conversion to switch away from controller APIs.
    
    Change-Id: I2bd98524b02d975ffb677d9718642cadcd7aa43c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- benchmark/ntfbenchmark/src/main/java/ntfbenchmark/impl/NtfbenchNonblockingProducer.java
+++ benchmark/ntfbenchmark/src/main/java/ntfbenchmark/impl/NtfbenchNonblockingProducer.java
@@ -5,13 +5,12 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package ntfbenchmark.impl;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import java.util.concurrent.ExecutionException;
-import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 
 public class NtfbenchNonblockingProducer extends AbstractNtfbenchProducer {
 
