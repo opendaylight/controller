6e2b4ad7aaf8e40cec3c05ba12b90dc062930b31
Robert Varga <robert.varga@pantheon.tech>
2019-02-04 10:09:57 +0100

    Add SegmentedFileJournal
    This adds an alternative Akka persistence journal implementation
    based on Atomix's SegmentedJournal.
    
    SegmentedJournal is a linear append-only log of entries, each of
    which has a incrementing 63bit index. This logical structure is
    stored in rolling segments -- i.e. entries are appended to current
    segment until it is full, at which point a new segment is allocated
    and writeout continues there. Old segments can be freed as long as
    they are not needed.
    
    This layout makes it good match to how Akka's persistence works,
    with the one exception that the SegmentedJournal cannot explicitly
    delete entries from the head of the journal.
    
    SegmentedFileJournal allocates one SegmentedJournal for each
    persistenceId it encounters and uses a dedicated actor for it. This
    provides a major simplification in the implementation as well as
    allows for concurrent persistence of multiple PersistentActors.
    
    JIRA: CONTROLLER-1884
    Change-Id: I78140b154bab44a3e17d5ffb76b040c62add3204
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/persistence/DataJournalEntry.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.persistence;
+
+import static java.util.Objects.requireNonNull;
+
+import akka.persistence.PersistentRepr;
+import io.atomix.storage.journal.JournalSegment;
+
+/**
+ * A single entry in the data journal. We do not store {@code persistenceId} for each entry, as that is a
+ * journal-invariant, nor do we store {@code sequenceNr}, as that information is maintained by {@link JournalSegment}'s
+ * index.
+ *
+ * @author Robert Varga
+ */
+abstract class DataJournalEntry {
+    static final class ToPersistence extends DataJournalEntry {
+        private final PersistentRepr repr;
+
+        ToPersistence(final PersistentRepr repr) {
+            this.repr = requireNonNull(repr);
+        }
+
+        PersistentRepr repr() {
+            return repr;
+        }
+    }
+
+    static final class FromPersistence extends DataJournalEntry {
+        private final String manifest;
+        private final String writerUuid;
+        private final Object payload;
+
+        FromPersistence(final String manifest, final String writerUuid, final Object payload) {
+            this.manifest = manifest;
+            this.writerUuid = requireNonNull(writerUuid);
+            this.payload = requireNonNull(payload);
+        }
+
+        PersistentRepr toRepr(final String persistenceId, final long sequenceNr) {
+            return PersistentRepr.apply(payload, sequenceNr, persistenceId, manifest, false, null, writerUuid);
+        }
+    }
+}
