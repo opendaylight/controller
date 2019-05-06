882481cc64015f96dcd5e9232210cd10005bb813
Robert Varga <robert.varga@pantheon.tech>
2019-05-06 20:04:10 +0200

    Refactor odl-mdsal-distributed-datastore
    odl-mdsal-distributed-datastore forms the baseline for supporting
    akka-based cluster. This refactors it to pull its dependencies in
    so it can be properly tested.
    
    This also moves CommitStatsMXBean, so we do not depend on controller's
    broker at all.
    
    Finally we co-locate all configuration files into this feature, so
    it can boot up.
    
    JIRA: CONTROLLER-1584
    Change-Id: I9319de78d61d63e3c49825e940af5c8c70657185
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/jmx/mbeans/CommitStatsMXBeanImplTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/jmx/mbeans/CommitStatsMXBeanImplTest.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.controller.md.sal.dom.broker.impl.jmx;
+package org.opendaylight.controller.cluster.datastore.jmx.mbeans;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
