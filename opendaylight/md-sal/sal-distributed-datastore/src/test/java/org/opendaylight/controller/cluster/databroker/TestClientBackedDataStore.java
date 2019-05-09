148d69f15ddf47a67c69e3623eac01ee14ceaa74
Tomas Cere <tomas.cere@pantheon.tech>
2019-05-09 11:12:34 +0200

    Add unit test for FrontedMetadata memory leaks
    This memory leak occurs when write and read-only transactions
    intertwine, leading to sparse range set.
    We need to be able to request frontend metadata from shards,
    so we can see whether it converges to a single range.
    
    Also rework testing datastores, so we can inject custom testing
    implementations allowing us to retrieve frontend metadata.
    
    // FIXME: this flushes out a tell-based leak, which needs to be
              investigated separately.
    
    JIRA: CONTROLLER-1879
    Change-Id: Ia8f350be7831b0c8f6846ee6fa41a665d1e191f4
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/TestClientBackedDataStore.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.databroker;
+
+import akka.actor.ActorSystem;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+import org.opendaylight.controller.cluster.databroker.actors.dds.DataStoreClient;
+import org.opendaylight.controller.cluster.datastore.ClusterWrapper;
+import org.opendaylight.controller.cluster.datastore.DatastoreContextFactory;
+import org.opendaylight.controller.cluster.datastore.config.Configuration;
+import org.opendaylight.controller.cluster.datastore.persisted.DatastoreSnapshot;
+import org.opendaylight.controller.cluster.datastore.shardmanager.AbstractShardManagerCreator;
+import org.opendaylight.controller.cluster.datastore.shardmanager.TestShardManager;
+import org.opendaylight.controller.cluster.datastore.utils.ActorUtils;
+
+public class TestClientBackedDataStore extends ClientBackedDataStore {
+
+    public TestClientBackedDataStore(final ActorSystem actorSystem, final ClusterWrapper cluster,
+                                     final Configuration configuration,
+                                     final DatastoreContextFactory datastoreContextFactory,
+                                     final DatastoreSnapshot restoreFromSnapshot) {
+        super(actorSystem, cluster, configuration, datastoreContextFactory, restoreFromSnapshot);
+    }
+
+    TestClientBackedDataStore(final ActorUtils actorUtils, final ClientIdentifier identifier,
+                              final DataStoreClient clientActor) {
+        super(actorUtils, identifier, clientActor);
+    }
+
+    @Override
+    protected AbstractShardManagerCreator<?> getShardManagerCreator() {
+        return new TestShardManager.TestShardManagerCreator();
+    }
+}
