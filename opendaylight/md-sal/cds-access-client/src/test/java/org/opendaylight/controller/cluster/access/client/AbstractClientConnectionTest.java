4df69652f886377624214b8c380bc8438d6704a2
Maros Marsalek <mmarsalek@frinx.io>
2019-05-09 12:34:54 +0200

    WIP: Deadlock test
    Change-Id: I90dd66d4aa8b3ff7abbf973f34fb03bce64b5cc3
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnectionTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/AbstractClientConnectionTest.java
@@ -9,6 +9,8 @@ package org.opendaylight.controller.cluster.access.client;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -18,14 +20,20 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.TestProbe;
 import akka.testkit.javadsl.TestKit;
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
+import javax.annotation.Nonnull;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.opendaylight.controller.cluster.access.ABIVersion;
 import org.opendaylight.controller.cluster.access.commands.AbortLocalTransactionRequest;
 import org.opendaylight.controller.cluster.access.commands.TransactionAbortSuccess;
 import org.opendaylight.controller.cluster.access.commands.TransactionFailure;
@@ -42,6 +50,7 @@ import org.opendaylight.controller.cluster.access.concepts.ResponseEnvelope;
 import org.opendaylight.controller.cluster.access.concepts.RuntimeRequestException;
 import org.opendaylight.controller.cluster.access.concepts.SuccessEnvelope;
 import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.yangtools.concepts.WritableIdentifier;
 
 public abstract class AbstractClientConnectionTest<T extends AbstractClientConnection<U>, U extends BackendInfo> {
 
@@ -89,6 +98,82 @@ public abstract class AbstractClientConnectionTest<T extends AbstractClientConne
         connection.enqueueEntry(entry, 0L);
         connection.poison(new RuntimeRequestException("fail", new RuntimeException("fail")));
         verify(callback, timeout(1000)).accept(isA(TransactionFailure.class));
+    }
+
+    @Test(timeout = 60 * 1000) // TIMEOUT == DEADLOCK
+    public void testPoisonDeadlock() throws Exception {
+        AbstractReceivingClientConnection<?> forwarder = (AbstractReceivingClientConnection<?>) createConnection();
+        this.connection.setForwarder(new ReconnectForwarder(forwarder) {
+            @Override
+            protected void forwardEntry(ConnectionEntry entry, long now) {
+                forwarder.sendEntry(entry, now);
+            }
+
+            @Override
+            protected void replayEntry(ConnectionEntry entry, long now) {}
+        });
+
+        final Consumer<Response<?, ?>> callback = mock(Consumer.class);
+        doAnswer(invocationOnMock -> {
+            try {
+                this.connection.enqueueRequest(MockRequest.create(), mock(Consumer.class), 0);
+            } catch (Exception e) {
+                // shouldnt happen
+            }
+            return null;
+        }).when(callback).accept(Mockito.any(Response.class));
+
+        Thread applicationThread = new Thread(() -> {
+            for (int j = 0; j < 250; j++) {
+                try {
+                    this.connection.sendRequest(MockRequest.create(), callback);
+                } catch (Exception e) {
+                    // Already poisoned
+                }
+            }
+        });
+
+        Thread akkaThread = new Thread(() -> {
+            // Sleep for a bit to make sure sender has already started sending requests
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+
+            for (int i = 0; i < 1; i++) {
+                forwarder.poison(new RuntimeRequestException("fail", new RuntimeException("fail")));
+            }
+        });
+
+        applicationThread.start();
+        akkaThread.start();
+        akkaThread.join();
+        applicationThread.join();
+    }
+
+    static abstract class MockRequest<T extends WritableIdentifier, C extends Request<T, C>> extends Request<T, C>  {
+
+        protected MockRequest(@Nonnull T target, long sequence, @Nonnull ActorRef replyTo) {
+            super(target, sequence, replyTo);
+        }
+
+        @Override
+        protected MoreObjects.ToStringHelper addToStringAttributes(MoreObjects.ToStringHelper toStringHelper) {
+            return super.addToStringAttributes(toStringHelper);
+        }
+
+        @Nonnull
+        @Override
+        protected C cloneAsVersion(@Nonnull ABIVersion targetVersion) {
+            return (C)this;
+        }
+
+        public static MockRequest create() {
+            MockRequest mock = mock(MockRequest.class);
+            doAnswer(a -> a.getArguments()[0])
+                    .when(mock).addToStringAttributes(Mockito.any(MoreObjects.ToStringHelper.class));
+            doReturn(mock).when(mock).cloneAsVersion(Mockito.any(ABIVersion.class));
+            return mock;
+        }
     }
 
     @Test
