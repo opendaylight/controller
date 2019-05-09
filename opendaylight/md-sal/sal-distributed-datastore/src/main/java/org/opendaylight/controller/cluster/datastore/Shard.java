8fcd93d67d4be3b6bd32d2a0c03409cb98667839
Tomas Cere <tomas.cere@pantheon.tech>
2019-05-09 11:12:34 +0200

    Add unit test for FrontedMetadata memory leaks
    This memory leak occurs when write and read-only transactions
    intertwine, leading to sparse range set.
    We need to be able to request frontend metadata from shards,
    so we can see whether it converges to a single range.
    
    JIRA: CONTROLLER-1879
    Change-Id: Ia8f350be7831b0c8f6846ee6fa41a665d1e191f4
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/Shard.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/Shard.java
@@ -80,6 +80,7 @@ import org.opendaylight.controller.cluster.datastore.messages.OnDemandShardState
 import org.opendaylight.controller.cluster.datastore.messages.PeerAddressResolved;
 import org.opendaylight.controller.cluster.datastore.messages.ReadyLocalTransaction;
 import org.opendaylight.controller.cluster.datastore.messages.RegisterDataTreeChangeListener;
+import org.opendaylight.controller.cluster.datastore.messages.RequestFrontendMetadata;
 import org.opendaylight.controller.cluster.datastore.messages.ShardLeaderStateChanged;
 import org.opendaylight.controller.cluster.datastore.messages.UpdateSchemaContext;
 import org.opendaylight.controller.cluster.datastore.persisted.DatastoreSnapshot;
@@ -372,12 +373,19 @@ public class Shard extends RaftActor {
                 onMakeLeaderLocal();
             } else if (RESUME_NEXT_PENDING_TRANSACTION.equals(message)) {
                 store.resumeNextPendingTransaction();
+            } else if (message instanceof RequestFrontendMetadata) {
+                handleRequestFrontendMetadata(sender());
             } else if (message instanceof DisableTransactionTracking) {
                 onDisableTransactionTracking(((DisableTransactionTracking) message).getClientId());
             } else if (!responseMessageSlicer.handleMessage(message)) {
                 super.handleNonRaftCommand(message);
             }
         }
+    }
+
+    private void handleRequestFrontendMetadata(final ActorRef respondTo) {
+        FrontendShardDataTreeSnapshotMetadata metadataSnapshot = frontendMetadata.toSnapshot();
+        respondTo.tell(metadataSnapshot, self());
     }
 
     private void handleRequestAssemblerMessage(final Object message) {
