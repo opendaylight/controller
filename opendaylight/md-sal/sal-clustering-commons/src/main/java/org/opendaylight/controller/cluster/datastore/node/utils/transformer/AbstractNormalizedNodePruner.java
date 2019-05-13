67b8cafa059a9dcb3f4b4c6a3446ffaefc6afa43
Robert Varga <robert.varga@pantheon.tech>
2019-05-13 16:05:58 +0200

    WIP: Add DataTreeCandidateTransactionCursor
    Rather than forcing instantiation of the entire DataTreeCandidate,
    this patch introduces a streaming alternative. A serialized DTC is
    incrementally deserialized and applied to an open cursor, allowing
    us to build data exactly once rather than having to go through
    intermediate pruning stage.
    
    JIRA: CONTROLLER-1889
    Change-Id: I1cf2f8f17e39dd1d2033fe8a376de3e69dc715e2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/transformer/AbstractNormalizedNodePruner.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/transformer/AbstractNormalizedNodePruner.java
@@ -82,12 +82,23 @@ abstract class AbstractNormalizedNodePruner implements NormalizedNodeStreamWrite
     }
 
     final void initialize(final YangInstanceIdentifier nodePath) {
-        nodePathSchemaNode = tree.findChild(nodePath).orElse(null);
+        initialize(tree.findChild(nodePath).orElse(null));
+    }
+
+    final void initialize(final DataSchemaContextNode<?> root) {
+        reset(root, State.OPEN);
+    }
+
+    final void reset() {
+        reset(null, State.UNITIALIZED);
+    }
+
+    private void reset(final DataSchemaContextNode<?> root, final State newState) {
         unknown = 0;
         normalizedNode = null;
         stack.clear();
         delegate.reset();
-        state = State.OPEN;
+        state = requireNonNull(newState);
     }
 
     @Override
