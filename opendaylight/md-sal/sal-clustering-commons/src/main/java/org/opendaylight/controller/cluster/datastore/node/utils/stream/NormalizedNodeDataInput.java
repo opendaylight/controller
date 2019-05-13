73390f188c7193623724f491064a17bc6cc8ba79
Robert Varga <robert.varga@pantheon.tech>
2019-05-13 07:44:05 +0200

    Add streaming interface to NormalizedNodeDataInput
    NormalizedNodeDataInput allows reading NormalizedNodes, which is
    wired directly to immutable node builders, providing little
    flexibility.
    
    This inflexibility requires us to re-stream the data set through
    NormalizedNodePruner, effectively doubling memory requirements
    during recovery.
    
    Extend NormalizedNodeDataInput to have a NormalizedNodeStreamWriter
    to be plugged in, so that the byte stream becomes a source of
    NormalizedNodeStreamWriter events, which can be routed through
    a custom-built pipeline.
    
    readNormalizedNode() becomes a simple default wrapper, which
    pipes the stream into an ImmutableNormalizedNodeStreamWriter.
    
    JIRA: CONTROLLER-1889
    Change-Id: Ic732ba9105dd7e27d5612853b931aba66bdd83a5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeDataInput.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeDataInput.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 
 /**
@@ -22,13 +25,29 @@ import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 @Beta
 public interface NormalizedNodeDataInput extends DataInput {
     /**
+     * Interpret current stream position as a NormalizedNode, stream its events into a NormalizedNodeStreamWriter.
+     *
+     * @param writer Writer to emit events to
+     * @throws IOException if an error occurs
+     * @throws IllegalStateException if the dictionary has been detached
+     * @throws NullPointerException if {@code writer} is null
+     */
+    void streamNormalizedNode(NormalizedNodeStreamWriter writer) throws IOException;
+
+    /**
      * Read a normalized node from the reader.
      *
      * @return Next node from the stream, or null if end of stream has been reached.
      * @throws IOException if an error occurs
      * @throws IllegalStateException if the dictionary has been detached
      */
-    NormalizedNode<?, ?> readNormalizedNode() throws IOException;
+    default NormalizedNode<?, ?> readNormalizedNode() throws IOException {
+        final NormalizedNodeResult result = new NormalizedNodeResult();
+        try (NormalizedNodeStreamWriter writer = ImmutableNormalizedNodeStreamWriter.from(result)) {
+            streamNormalizedNode(writer);
+        }
+        return result.getResult();
+    }
 
     YangInstanceIdentifier readYangInstanceIdentifier() throws IOException;
 
