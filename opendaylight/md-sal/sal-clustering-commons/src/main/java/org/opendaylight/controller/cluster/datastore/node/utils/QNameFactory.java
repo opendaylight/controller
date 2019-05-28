13d5509e4691c91e812e76442aa87ba3da37ede4
Robert Varga <robert.varga@pantheon.tech>
2019-05-28 13:14:34 +0200

    Do not format QNames to string on input
    Profiling has revelead the creation of String for QNameFactory
    lookup is taking around 19% of overall cost of
    NormalizedNodeInputStreamReader.readQName().
    
    Rather than using concatenation, use a dedicated Key object, which
    holds the separated-out localname/namespace/revision strings,
    thus allocating a small object instead of a full-blown String.
    
    This improves snapshot deserialization perfomance on a 350MiB
    snapshot by about 16%, also resulting in lower memory pressure.
    
    Change-Id: If3e9e4b332b0a5acbe33aa10fca189bbc7da8c81
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/QNameFactory.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/QNameFactory.java
@@ -7,20 +7,68 @@
  */
 package org.opendaylight.controller.cluster.datastore.node.utils;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import java.util.Objects;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.common.QName;
 
 public final class QNameFactory {
+    public static final class Key implements Immutable {
+        private final @NonNull String localName;
+        private final @NonNull String namespace;
+        private final @Nullable String revision;
+
+        public Key(final String localName, final String namespace, final String revision) {
+            this.localName = requireNonNull(localName);
+            this.namespace = requireNonNull(namespace);
+            this.revision = revision;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(localName, namespace, revision);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof Key)) {
+                return false;
+            }
+            final Key other = (Key) obj;
+            return localName.equals(other.localName) && namespace.equals(other.namespace)
+                    && Objects.equals(revision, other.revision);
+        }
+
+        QName toQName() {
+            return revision != null ? QName.create(namespace, revision, localName) : QName.create(namespace, localName);
+        }
+    }
+
     private static final int MAX_QNAME_CACHE_SIZE = Integer.getInteger(
         "org.opendaylight.controller.cluster.datastore.node.utils.qname-cache.max-size", 10000);
 
-    private static final LoadingCache<String, QName> CACHE = CacheBuilder.newBuilder().maximumSize(MAX_QNAME_CACHE_SIZE)
-            .weakValues().build(new CacheLoader<String, QName>() {
+    private static final LoadingCache<String, QName> STRING_CACHE = CacheBuilder.newBuilder()
+            .maximumSize(MAX_QNAME_CACHE_SIZE).weakValues().build(new CacheLoader<String, QName>() {
                 @Override
                 public QName load(final String key) {
                     return QName.create(key);
+                }
+            });
+
+    private static final LoadingCache<Key, QName> KEY_CACHE = CacheBuilder.newBuilder()
+            .maximumSize(MAX_QNAME_CACHE_SIZE).weakValues().build(new CacheLoader<Key, QName>() {
+                @Override
+                public QName load(final Key key) {
+                    return key.toQName();
                 }
             });
 
@@ -28,7 +76,12 @@ public final class QNameFactory {
 
     }
 
+    @Deprecated
     public static QName create(final String name) {
-        return CACHE.getUnchecked(name);
+        return STRING_CACHE.getUnchecked(name);
+    }
+
+    public static QName create(final Key key) {
+        return KEY_CACHE.getUnchecked(key);
     }
 }
