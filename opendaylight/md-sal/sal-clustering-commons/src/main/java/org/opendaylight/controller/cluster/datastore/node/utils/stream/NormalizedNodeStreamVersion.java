657b0b025a92f9d0ad6647d79950071031d7b0b4
Robert Varga <robert.varga@pantheon.tech>
2019-05-29 11:27:21 +0200

    Add support for coded QNames/AugmentationIdentifiers
    Profiling is showing we are spending about 46% in readQName(),
    which further breaks down to 30% in readCodedString() and 61%
    in QNameFactory.create().
    
    Out of 1.7M invocations of readQName(), we end up loading only
    508 QNames -- which is understandable, as the number of QNames is
    bound by the SchemaContext contents. This also applies to
    AugmentationIdentifiers.
    
    This patch defines a new streaming format version, which uses
    the same linear coding approach we take for Strings, taking
    hash-based lookups out of the hot path -- bringing reconstruction
    time down from 36 to 19-24 seconds.
    
    It has also the effect of reducing the snapshot size, in our testing
    bringing a 388.8MiB snapshot down to 210.6MiB.
    
    JIRA: CONTROLLER-1898
    Change-Id: Ibbc96c286d52b3261b04dd7de02e13bc6b44e803
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/NormalizedNodeStreamVersion.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.node.utils.stream;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Enumeration of all stream versions this implementation supports on both input and output.
+ */
+@Beta
+@NonNullByDefault
+public enum NormalizedNodeStreamVersion {
+    LITHIUM,
+    SODIUM;
+}
