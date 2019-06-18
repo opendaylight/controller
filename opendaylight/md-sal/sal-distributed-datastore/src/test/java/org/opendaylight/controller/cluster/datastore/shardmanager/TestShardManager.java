fc897cfcc3856235c73edbe49af9ba4f0e75782a
Tomas Cere <tomas.cere@pantheon.tech>
2019-06-18 14:54:56 +0200

    Leader should always apply modifications as local
    After a leader -> follower -> leader transition which happens
    before gaining consensus but after sending CommitTransactionPayload off
    to persistence, the leader looses its ClientRequestTracker which
    makes it apply the modification as foreign once it resumes leadership.
    
    The consequence of applying the modification as foreign is that the
    transaction becomes stuck forever in COMMIT_PENDING state, since it will
    be never aborted or finished.
    
    Change this so that the leader will apply modifications as local even
    when it looses its ClientRequestTracker and add Identifiable to payloads
    which require it.
    
    Reproducing test included.
    
    Change-Id: I636f998cd62ec82ef02193261624e4a51275fb86
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/shardmanager/TestShardManager.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/shardmanager/TestShardManager.java
@@ -20,6 +20,15 @@ public class TestShardManager extends ShardManager {
         super(builder);
     }
 
+    @Override
+    public void handleCommand(Object message) throws Exception {
+        if (GetLocalShards.INSTANCE.equals(message)) {
+            sender().tell(new GetLocalShardsReply(localShards), null);
+        } else {
+            super.handleCommand(message);
+        }
+    }
+
     /**
      * Plug into shard actor creation to replace info with our testing one.
      * @param info shard info.
@@ -27,10 +36,12 @@ public class TestShardManager extends ShardManager {
      */
     @Override
     protected ActorRef newShardActor(ShardInformation info) {
+        Map<String, String> peerAddresses = getPeerAddresses(info.getShardName());
         ShardInformation newInfo = new ShardInformation(info.getShardName(),
-                info.getShardId(), getPeerAddresses(info.getShardName()),
+                info.getShardId(), peerAddresses,
                 info.getDatastoreContext(),
-                TestShard.builder().restoreFromSnapshot(info.getBuilder().getRestoreFromSnapshot()),
+                TestShard.builder()
+                        .restoreFromSnapshot(info.getBuilder().getRestoreFromSnapshot()),
                 peerAddressResolver);
         newInfo.setSchemaContext(info.getSchemaContext());
         newInfo.setActiveMember(info.isActiveMember());
@@ -56,6 +67,27 @@ public class TestShardManager extends ShardManager {
         public Props props() {
             verify();
             return Props.create(TestShardManager.class, this);
+        }
+    }
+
+    public static final class GetLocalShards {
+        public static final GetLocalShards INSTANCE = new GetLocalShards();
+
+        private GetLocalShards() {
+
+        }
+    }
+
+    public static class GetLocalShardsReply {
+
+        private final Map<String, ShardInformation> localShards;
+
+        public GetLocalShardsReply(Map<String, ShardInformation> localShards) {
+            this.localShards = localShards;
+        }
+
+        public Map<String, ShardInformation> getLocalShards() {
+            return localShards;
         }
     }
 }
