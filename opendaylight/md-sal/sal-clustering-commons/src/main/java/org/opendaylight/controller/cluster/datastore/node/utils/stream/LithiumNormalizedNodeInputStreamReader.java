5eb0f26924907bc1fc647f26dde0fafd86f8b6de
Robert Varga <robert.varga@pantheon.tech>
2019-08-26 22:44:30 +0200

    Deduplicate MapNode key leaf values
    When we are encountering a leaf node inside a MapEntryNode, it can
    be a leaf corresponding to a key -- in which case its value is
    already present in NodeIdentifierWithPredicates and we do want to
    de-duplicate those objects.
    
    JIRA: CONTROLLER-1908
    Change-Id: I2ed65c311f9921aa77c9f23bd1f7681d7f11355a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/LithiumNormalizedNodeInputStreamReader.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/LithiumNormalizedNodeInputStreamReader.java
@@ -155,11 +155,26 @@ class LithiumNormalizedNodeInputStreamReader extends ForwardingDataInput impleme
     }
 
     private void streamLeaf(final NormalizedNodeStreamWriter writer) throws IOException {
+        startLeaf(writer);
+        writer.scalarValue(readObject());
+        writer.endNode();
+    }
+
+    // Leaf inside a MapEntryNode, it can potentially be a key leaf, in which case we want to de-duplicate values.
+    private void streamLeaf(final NormalizedNodeStreamWriter writer, final NodeIdentifierWithPredicates entryId)
+            throws IOException {
+        final NodeIdentifier identifier = startLeaf(writer);
+        final Object value = readObject();
+        final Object entryValue = entryId.getValue(identifier.getNodeType());
+        writer.scalarValue(entryValue == null ? value : entryValue);
+        writer.endNode();
+    }
+
+    private NodeIdentifier startLeaf(final NormalizedNodeStreamWriter writer) throws IOException {
         final NodeIdentifier identifier = readNodeIdentifier();
         LOG.trace("Streaming leaf node {}", identifier);
         writer.startLeafNode(identifier);
-        writer.scalarValue(readObject());
-        writer.endNode();
+        return identifier;
     }
 
     private void streamLeafSet(final NormalizedNodeStreamWriter writer) throws IOException {
@@ -215,7 +230,17 @@ class LithiumNormalizedNodeInputStreamReader extends ForwardingDataInput impleme
         final NodeIdentifierWithPredicates entryIdentifier = readNormalizedNodeWithPredicates();
         LOG.trace("Streaming map entry node {}", entryIdentifier);
         writer.startMapEntryNode(entryIdentifier, NormalizedNodeStreamWriter.UNKNOWN_SIZE);
-        commonStreamContainer(writer);
+
+        // Same loop as commonStreamContainer(), but ...
+        for (byte nodeType = input.readByte(); nodeType != NodeTypes.END_NODE; nodeType = input.readByte()) {
+            if (nodeType == NodeTypes.LEAF_NODE) {
+                // ... leaf nodes may need de-duplication
+                streamLeaf(writer, entryIdentifier);
+            } else {
+                streamNormalizedNode(writer, nodeType);
+            }
+        }
+        writer.endNode();
     }
 
     private void streamUnkeyedList(final NormalizedNodeStreamWriter writer) throws IOException {
