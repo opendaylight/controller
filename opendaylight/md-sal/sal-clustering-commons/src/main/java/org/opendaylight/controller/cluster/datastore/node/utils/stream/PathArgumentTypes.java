22847f8198364754447604f549e4e99d20a3bc58
Robert Varga <robert.varga@pantheon.tech>
2019-08-30 11:08:37 +0200

    Rework PathArgumentTypes lookup
    This lookup relying on final methods, which is not a correct
    assumption, with NodeIdentifierWithPredicates having at least two
    properly-hidden implementations.
    
    Rework the lookup in terms of instanceof checks, which saves a table
    lookup and unboxing operation.
    
    Change-Id: Ia6b272715d19e432e7e0839b6294035e12b1d293
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/PathArgumentTypes.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/PathArgumentTypes.java
@@ -5,34 +5,35 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.controller.cluster.datastore.node.utils.stream;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
-import java.util.Map;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeWithValue;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 
 final class PathArgumentTypes {
-    public static final byte AUGMENTATION_IDENTIFIER = 1;
-    public static final byte NODE_IDENTIFIER = 2;
-    public static final byte NODE_IDENTIFIER_WITH_VALUE = 3;
-    public static final byte NODE_IDENTIFIER_WITH_PREDICATES = 4;
+    static final byte AUGMENTATION_IDENTIFIER = 1;
+    static final byte NODE_IDENTIFIER = 2;
+    static final byte NODE_IDENTIFIER_WITH_VALUE = 3;
+    static final byte NODE_IDENTIFIER_WITH_PREDICATES = 4;
 
     private PathArgumentTypes() {
         throw new UnsupportedOperationException("Utility class");
     }
 
-    private static final Map<Class<?>, Byte> CLASS_TO_ENUM_MAP = ImmutableMap.<Class<?>, Byte>builder()
-            .put(YangInstanceIdentifier.AugmentationIdentifier.class, AUGMENTATION_IDENTIFIER)
-            .put(YangInstanceIdentifier.NodeIdentifier.class, NODE_IDENTIFIER)
-            .put(YangInstanceIdentifier.NodeIdentifierWithPredicates.class, NODE_IDENTIFIER_WITH_PREDICATES)
-            .put(YangInstanceIdentifier.NodeWithValue.class, NODE_IDENTIFIER_WITH_VALUE).build();
-
-    public static byte getSerializablePathArgumentType(YangInstanceIdentifier.PathArgument pathArgument) {
-        final Byte type = CLASS_TO_ENUM_MAP.get(pathArgument.getClass());
-        Preconditions.checkArgument(type != null, "Unknown type of PathArgument = %s", pathArgument);
-        return type;
+    static byte getSerializablePathArgumentType(final PathArgument pathArgument) {
+        if (pathArgument instanceof NodeIdentifier) {
+            return NODE_IDENTIFIER;
+        } else if (pathArgument instanceof NodeIdentifierWithPredicates) {
+            return NODE_IDENTIFIER_WITH_PREDICATES;
+        } else if (pathArgument instanceof AugmentationIdentifier) {
+            return AUGMENTATION_IDENTIFIER;
+        } else if (pathArgument instanceof NodeWithValue) {
+            return NODE_IDENTIFIER_WITH_VALUE;
+        } else {
+            throw new IllegalArgumentException("Unknown type of PathArgument = " + pathArgument);
+        }
     }
-
 }
