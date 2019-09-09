2d01f8665bab351205cc944a0dc32d24e942da7d
Robert Varga <robert.varga@pantheon.tech>
2019-09-09 10:29:57 +0200

    WIP: Add Magnesium streaming format
    Changes to how uint8/16/32/64 types are mapped results in the need
    to transfer the new value types. These represent 4 brand new tokens,
    which need to be handled.
    
    One option would be to fork NeonSR2 and modify it, somehow dealing
    with value lookups (i.e. Uint8 is not valid in SR2 stream, but tokens
    are shared across Lithium/NeonSR).
    
    Another option is to create a streaming format from scratch, coming
    up with a completely new family of tokens and encoding rules. This
    is attractive, as both current formats have three deficiencies:
    
    1) they encode MapEntry key values twice, once in the leaf itself
       and once in the entry's NodeIdentifierWithPredicates
    2) they are '0-happy', i.e. they do not recognize that integer codes
       and sizes are typically much smaller than the 4-byte range and thus
       much of the stream is just small ints encoded as 4 bytes
    3) while they are simple and straighforward, they also end up wasting
       bits -- the four token families (codes, value types, node types,
       path argument types) each have at most 15 distinct values, and
       hence the bytes used to transmit them have 4 empty bits (or more)
    
    Hence we take the other option and design a streaming format with
    brand new tokens, which are structured to carry as much more
    information per byte.
    
    This is done by combining 'type' information with forward coding
    hints, for example:
    
    - a Boolean leaf value would previously require 2 bytes:
        (byte)    ValueTypes.BOOL_TYPE
        (byte)    true/falseIn the new tokens
      whereas new tokens express this in one byte:
        (byte)    LeafValue.BOOLEAN_{FALSE,TRUE}
    - a byte[5] value would require 10 bytes to encode:
        (byte)    ValueTypes.BINARY_TYPE
        (int)     length
        (5 bytes) bytes
      whereas new tokens express this in 6 bytes:
        (byte)    ValueTypes.BINARY_0 + 5 (works up to 127)
        (5 bytes) bytes
    
    Furthermore we use pretty much all of LeafValue single-byte coding
    space by also assigning certain special values, like empty strings,
    Uint64.ZERO, etc.
    
    Similar approach is taken when encoding NormalizedNode types. Here
    we recognize there are only 14 node types (currently) and hence we
    can provide up to 4 additional bits for information about how the
    node's identifier is encoded.
    
    // FIXME: rework bits tokenization
    // FIXME: decide what to do about YANG-modeled XML node
    // FIXME: finalize token type assignments and document them
    // FIXME: finish up DataInput implementation
    // FIXME: finish up DataOutput implementation
    // FIXME: clean up method ordering and document individual methods
    // FIXME: add unit tests
    // FIXME: split into four patches:
    //        -- tokens/versions
    //        -- DataInput and wiring into constructor/base decoder
    //        -- DataOuput and wiring into constructor
    //        -- switch default encoding
    
    JIRA: CONTROLLER-1919
    Change-Id: I5fcf85a76750301cd3f8bcbd505baa0f95397cb5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/MagnesiumPathArgument.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.node.utils.stream;
+
+/**
+ * Path Argument types used in Magnesium encoding. These are encoded as a single byte, three bits of which are reserved
+ * for PathArgument type itself:
+ * <pre>
+ *   7 6 5 4 3 2 1 0
+ *  +-+-+-+-+-+-+-+-+
+ *  |         | Type|
+ *  +-+-+-+-+-+-+-+-+
+ * <pre>
+ *
+ * There are for type defined:
+ * <ul>
+ *   <li>{@link #AUGMENTATION_IDENTIFIER}, which additionally holds the number of QName elements encoded:
+ *     <pre>
+ *        7 6 5 4 3 2 1 0
+ *       +-+-+-+-+-+-+-+-+
+ *       |  Count  |0 0 0|
+ *       +-+-+-+-+-+-+-+-+
+ *     </pre>
+ *     Where count is coded as an unsigned integer, with {@link #AID_COUNT_1B} and {@link #AID_COUNT_2B} and
+ *     {@link #AID_COUNT_4B} indicating extended coding with up to 4 additional bytes. This byte is followed by
+ *     {@code count} {@link MagnesiumValue} QNames.
+ *     <pre>
+ *       7 6 5 4 3 2 1 0
+ *      +-+-+-+-+-+-+-+-+
+ *      |0 0 0| Q |0 0 1|
+ *      +-+-+-+-+-+-+-+-+
+ *     </pre>
+ *     Where QName coding is achieved via {@link #QNAME_DEF}, {@link #QNAME_REF1}, {@link #QNAME_REF2} and
+ *     {@link #QNAME_REF4}.
+ *   </li>
+ *   <li>{@link #NODE_IDENTIFIER_WITH_PREDICATES}, which encodes a QName same way NodeIdentifier does:
+ *     <pre>
+ *       7 6 5 4 3 2 1 0
+ *      +-+-+-+-+-+-+-+-+
+ *      | Size| Q |0 1 0|
+ *      +-+-+-+-+-+-+-+-+
+ *      </pre>
+ *      but additionally encodes number of predicates contained using {@link #SIZE_0} through {@link #SIZE_4}. If that
+ *      number cannot be expressed, {@link #SIZE_1B}, {@value #SIZE_2B} and {@link #SIZE_4B} indicate number and format
+ *      of additional bytes that hold number of predicates.
+ *
+ *      <p>
+ *      This is then followed by the specified number of QName/Object key/value pairs based on {@link MagnesiumValue}
+ *      encoding.
+ *   <li>
+ *   <li>{@link #NODE_WITH_VALUE}, which encodes a QName same way NodeIdentifier does:
+ *     <pre>
+ *       7 6 5 4 3 2 1 0
+ *      +-+-+-+-+-+-+-+-+
+ *      |0 0 0| Q |0 1 1|
+ *      +-+-+-+-+-+-+-+-+
+ *     </pre>
+ *     but is additionally followed by a single encoded value, as per {@link MagnesiumValue}.
+ *   </li>
+ *   </li>
+ * </ul>
+ */
+final class MagnesiumPathArgument {
+    // 3 bits reserved for type...
+    static final byte AUGMENTATION_IDENTIFIER         = 0x00;
+    static final byte NODE_IDENTIFIER                 = 0x01;
+    static final byte NODE_IDENTIFIER_WITH_PREDICATES = 0x02;
+    static final byte NODE_WITH_VALUE                 = 0x03;
+
+    // ... leaving four values (or one bit) currently unused
+    // 0x04 reserved
+    // 0x05 reserved
+    // 0x06 reserved
+    // 0x07 reserved
+
+    // In case of AUGMENTATION_IDENTIFIER, top 5 bits are used to encode the number of path arguments, except last three
+    // values. This means that up to AugmentationIdentifiers with up to 28 components have this length encoded inline,
+    // otherwise we encode
+    static final byte AID_COUNT_1B                    = (byte) 0xE8; // Unsigned
+    static final byte AID_COUNT_2B                    = (byte) 0xF0; // Unsigned
+    static final byte AID_COUNT_4B                    = (byte) 0xF8; // Signed, also a mask
+
+    static final byte QNAME_DEF                       = 0x00;
+    static final byte QNAME_REF1                      = 0x08; // Unsigned
+    static final byte QNAME_REF2                      = 0x10; // Unsigned
+    static final byte QNAME_REF4                      = 0x18; // Signed, Also a mask
+
+    static final byte SIZE_0                          = 0x00;
+    static final byte SIZE_1                          = 0x20;
+    static final byte SIZE_2                          = 0x40;
+    static final byte SIZE_3                          = 0x60;
+    static final byte SIZE_4                          = (byte) 0x80;
+    static final byte SIZE_1B                         = (byte) 0xA0;
+    static final byte SIZE_2B                         = (byte) 0xC0;
+    static final byte SIZE_4B                         = (byte) 0xE0; // Also a mask
+
+    private MagnesiumPathArgument() {
+
+    }
+}
