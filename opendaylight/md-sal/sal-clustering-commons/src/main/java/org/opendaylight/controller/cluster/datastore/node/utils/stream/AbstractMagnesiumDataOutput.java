ec02d4f86c55aa502142682b6952742b0cb30567
Robert Varga <robert.varga@pantheon.tech>
2019-09-09 10:29:57 +0200

    WIP: add MagnesiumTokens
    These will support magnesium streams.
    
    Change-Id: I5fcf85a76750301cd3f8bcbd505baa0f95397cb5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/AbstractMagnesiumDataOutput.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.node.utils.stream;
+
+import java.io.DataOutput;
+
+abstract class AbstractMagnesiumDataOutput extends AbstractNormalizedNodeDataOutput {
+
+    AbstractMagnesiumDataOutput(final DataOutput output) {
+        super(output);
+    }
+
+}
