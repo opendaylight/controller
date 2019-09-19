39a2d9e66ca18ca9722fb6d2b98ec23d2f3facf9
Robert Varga <robert.varga@pantheon.tech>
2019-09-20 01:50:56 +0200

    WIP: split up transaction chunks
    This adds the infrastructure to prevent allocation of large byte[]s,
    as when those exceed 0.5-16MiB, under G1GC they end up in humongous
    object region. Not only that, but after some cut-off point, the
    copying of arrays starts to dominate performance.
    
    What we do here is ensure we always receive up to a configurable
    number of bytes, defaults to 256KiB, and keep those chunks in a list.
    
    This way we may end up with larger overhead, but that really is
    neglibeble -- even a 2GiB payload would end up using only about
    8K arrays.
    
    // FIXME: optimize output implementation:
    //        - grow chunks with power-of two until target is reached
    //        - retain previously-used small chunks for repetetive use:
    //          - if we start with 512 and we go to 1024, keep the 512
    //            and when we reach max size, store the full chunk and
    //            start fillin up the retained 512, then 1024, etc.
    
    Change-Id: I2b79a633ebf4fdf8d68d2accc644326e30b41f22
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/ChunkedOutputStream.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.math.IntMath;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+final class ChunkedOutputStream extends OutputStream {
+    static final int MAX_ARRAY_SIZE = IntMath.ceilingPowerOfTwo(Integer.getInteger(
+        "org.opendaylight.controller.cluster.datastore.persisted.max-array-size", 256 * 1024));
+    private static final int MIN_ARRAY_SIZE = 32;
+
+    private final List<byte[]> chunks = new ArrayList<>();
+    private final int initialCapacity;
+    private byte[] currentChunk;
+    private int currentOffset;
+    private int size;
+
+    ChunkedOutputStream(final int requestedInitialCapacity) {
+        this.initialCapacity = initialCapacity(requestedInitialCapacity);
+        currentChunk = new byte[initialCapacity];
+    }
+
+    @Override
+    public void write(final int b) {
+        ensureOneByte();
+        currentChunk[currentOffset] = (byte) b;
+        currentOffset++;
+        size++;
+    }
+
+    @Override
+    public void write(final byte b[], final int off, final int len) {
+        if (len < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int fromOffset = off;
+        int toCopy = len;
+
+        while (toCopy != 0) {
+            final int count = ensureMoreBytes(toCopy);
+            System.arraycopy(b, fromOffset, currentChunk, currentOffset, count);
+            currentOffset += count;
+            size += count;
+            fromOffset += count;
+            toCopy -= count;
+        }
+    }
+
+    @Override
+    public void close() {
+        if (currentOffset != 0) {
+            // We need to add current chunk, or a slice thereof
+            final byte[] lastChunk = currentOffset == currentChunk.length ? currentChunk
+                    : Arrays.copyOf(currentChunk, currentOffset);
+            chunks.add(lastChunk);
+        }
+        currentChunk = null;
+    }
+
+    List<byte[]> getChunks() {
+        checkState(currentChunk == null);
+        return chunks;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    private void ensureOneByte() {
+        if (currentChunk.length == currentOffset) {
+            allocChunk(1);
+        }
+    }
+
+    private int ensureMoreBytes(final int requested) {
+        int available = currentChunk.length - currentOffset;
+        if (available == 0) {
+            allocChunk(requested);
+            available = currentChunk.length;
+        }
+        return Math.min(requested, available);
+    }
+
+    private void allocChunk(final int allocHint) {
+        // FIXME: this is simple & stupid:
+        //        - consider requestedBytes
+        //        - we should reallocate chunks
+        // chunk is full, move it
+        chunks.add(currentChunk);
+        currentChunk = new byte[initialCapacity];
+        currentOffset = 0;
+    }
+
+    private static int initialCapacity(final int requestedSize) {
+        if (requestedSize < MIN_ARRAY_SIZE) {
+            return MIN_ARRAY_SIZE;
+        }
+        if (requestedSize > MAX_ARRAY_SIZE) {
+            return MAX_ARRAY_SIZE;
+        }
+        return IntMath.ceilingPowerOfTwo(requestedSize);
+    }
+}
