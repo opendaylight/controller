b04a1cc2d691429e5d1bd92c4f4465d36d13f2dd
Robert Varga <robert.varga@pantheon.tech>
2019-09-20 01:50:56 +0200

    Split up transaction chunks
    This adds the infrastructure to prevent allocation of large byte[]s,
    as when those exceed 0.5-16MiB, under G1GC they end up in humongous
    object region. Not only that, but after some cut-off point, the
    copying of arrays starts to dominate performance.
    
    What we do here is ensure we always receive up to a configurable
    number of bytes, defaults to 256KiB, and keep those chunks in a list.
    This way we may end up with larger overhead, but that really is
    neglibeble -- even a 2GiB payload would end up using only about
    8K arrays.
    
    While the input/output streams are similar to
    org.apache.commons.io.output.ByteArrayOutputStream, the design here
    is geared towards having the intermediate representation available
    as well as devolving to a single byte[] for memory efficiency
    reasons.
    
    JIRA: CONTROLLER-1920
    Change-Id: I2b79a633ebf4fdf8d68d2accc644326e30b41f22
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit d6875afc779570935fb6ff4df03f769cf89a7ae1)

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/persisted/ChunkedOutputStreamTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class ChunkedOutputStreamTest {
+    private static final int INITIAL_SIZE = 256;
+
+    private final ChunkedOutputStream stream = new ChunkedOutputStream(INITIAL_SIZE);
+
+    @Test
+    public void testBasicWrite() throws IOException {
+        for (int i = 0; i < INITIAL_SIZE; ++i) {
+            stream.write(i);
+        }
+
+        final byte[] chunk = assertFinishedStream(INITIAL_SIZE, 1).get(0);
+        assertEquals(INITIAL_SIZE, chunk.length);
+        for (int i = 0; i < INITIAL_SIZE; ++i) {
+            assertEquals((byte) i, chunk[i]);
+        }
+    }
+
+    @Test
+    public void testBasicLargeWrite() throws IOException {
+        final byte[] array = createArray(INITIAL_SIZE);
+        stream.write(array);
+        final byte[] chunk = assertFinishedStream(INITIAL_SIZE, 1).get(0);
+        assertArrayEquals(array, chunk);
+    }
+
+    @Test
+    public void testGrowWrite() throws IOException {
+        for (int i = 0; i < INITIAL_SIZE * 2; ++i) {
+            stream.write(i);
+        }
+
+        final byte[] chunk = assertFinishedStream(INITIAL_SIZE * 2, 1).get(0);
+        assertEquals(INITIAL_SIZE * 2, chunk.length);
+        for (int i = 0; i < INITIAL_SIZE * 2; ++i) {
+            assertEquals((byte) i, chunk[i]);
+        }
+    }
+
+    @Test
+    public void testGrowLargeWrite() throws IOException {
+        final byte[] array = createArray(INITIAL_SIZE * 2);
+        stream.write(array);
+        final byte[] chunk = assertFinishedStream(INITIAL_SIZE * 2, 1).get(0);
+        assertArrayEquals(array, chunk);
+    }
+
+    @Test
+    public void testTwoChunksWrite() throws IOException {
+        int size = ChunkedOutputStream.MAX_ARRAY_SIZE + 1;
+        for (int i = 0; i < size; ++i) {
+            stream.write(i);
+        }
+
+        int counter = 0;
+        for (byte[] chunk: assertFinishedStream(size, 2)) {
+            for (byte actual: chunk) {
+                assertEquals((byte) counter++, actual);
+            }
+        }
+    }
+
+    private List<byte[]> assertFinishedStream(final int expectedSize, final int expectedChunks) {
+        stream.close();
+        final ChunkedByteArray array = stream.toChunkedByteArray();
+        assertEquals(expectedSize, array.size());
+
+        final List<byte[]> chunks = array.getChunks();
+        assertEquals(expectedChunks, chunks.size());
+        return chunks;
+    }
+
+    private static byte[] createArray(final int size) {
+        final byte[] array = new byte[size];
+        for (int i = 0; i < size; ++i) {
+            array[i] = (byte) i;
+        }
+        return array;
+    }
+}
