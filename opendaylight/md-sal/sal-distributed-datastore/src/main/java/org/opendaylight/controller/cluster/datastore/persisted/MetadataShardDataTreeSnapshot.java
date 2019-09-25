35f5587a91d85bb2f4c7faf97a1b3b7e7d4ece0d
Robert Varga <robert.varga@pantheon.tech>
2019-09-25 16:29:34 +0200

    WIP: Switch current {ABI,DataStore,Payload}Version to Magnesium
    This mass-switches versioned resources to use Magnesium streaming
    format.
    
    JIRA: CONTROLLER-1919
    Change-Id: I4734fe3fe0064809fde899ea127e98380c8aacf4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/MetadataShardDataTreeSnapshot.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/MetadataShardDataTreeSnapshot.java
@@ -135,7 +135,7 @@ public final class MetadataShardDataTreeSnapshot extends AbstractVersionedShardD
 
     @Override
     PayloadVersion version() {
-        return PayloadVersion.SODIUM_SR1;
+        return PayloadVersion.MAGNESIUM;
     }
 
     private Object writeReplace() {
