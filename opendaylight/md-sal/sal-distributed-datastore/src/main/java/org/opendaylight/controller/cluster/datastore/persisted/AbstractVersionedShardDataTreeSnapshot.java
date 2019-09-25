b4a7c5687b9ddda0655fe856aa5a7d8f983e09c4
Robert Varga <robert.varga@pantheon.tech>
2019-09-25 12:45:18 +0200

    Define PayloadVersion.MAGNESIUM
    This adds the PayloadVersion which uses Magnesium normalized node
    stream.
    
    JIRA: CONTROLLER-1919
    Change-Id: Ie43e9932a19bd19c92f14890da13646bf56e2f6f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/AbstractVersionedShardDataTreeSnapshot.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/AbstractVersionedShardDataTreeSnapshot.java
@@ -34,6 +34,7 @@ abstract class AbstractVersionedShardDataTreeSnapshot extends ShardDataTreeSnaps
                 return new ShardSnapshotState(readSnapshot(in), true);
             case NEON_SR2:
             case SODIUM_SR1:
+            case MAGNESIUM:
                 return new ShardSnapshotState(readSnapshot(in), false);
             case TEST_FUTURE_VERSION:
             case TEST_PAST_VERSION:
@@ -78,7 +79,8 @@ abstract class AbstractVersionedShardDataTreeSnapshot extends ShardDataTreeSnaps
             case BORON:
             case NEON_SR2:
             case SODIUM_SR1:
-                // Boron, NeonSR2 and Sodium snapshots use Java Serialization, but differ in stream format
+            case MAGNESIUM:
+                // Boron, NeonSR2, Sodium and Magnesium snapshots use Java Serialization, but differ in stream format
                 out.writeObject(this);
                 return;
             case TEST_FUTURE_VERSION:
