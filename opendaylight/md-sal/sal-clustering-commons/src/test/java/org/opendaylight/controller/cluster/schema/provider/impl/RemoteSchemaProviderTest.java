849d3cfa2bac6090892a8f95f58f416d3ff79ec2
Robert Varga <robert.varga@pantheon.tech>
2019-10-10 14:05:49 +0200

    Remove CheckedFuture from RemoteSchemaProvider
    The API contract we are implementing is defined in terms of
    ListenableFuture, hence we can drop the use of CheckedFuture.
    
    Change-Id: I9d99b2e42c3995c4c11b44e595a81a93b2b31b7e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/cluster/schema/provider/impl/RemoteSchemaProviderTest.java
+++ opendaylight/md-sal/sal-clustering-commons/src/test/java/org/opendaylight/controller/cluster/schema/provider/impl/RemoteSchemaProviderTest.java
@@ -5,22 +5,26 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.controller.cluster.schema.provider.impl;
 
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
 import com.google.common.io.ByteSource;
-import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.opendaylight.controller.cluster.schema.provider.RemoteYangTextSourceProvider;
 import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
@@ -29,7 +33,6 @@ import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
 
 public class RemoteSchemaProviderTest {
-
     private static final SourceIdentifier ID = RevisionSourceIdentifier.create("Test", Revision.of("2015-10-30"));
 
     private RemoteSchemaProvider remoteSchemaProvider;
@@ -37,36 +40,37 @@ public class RemoteSchemaProviderTest {
 
     @Before
     public void setUp() {
-        mockedRemoteSchemaRepository = Mockito.mock(RemoteYangTextSourceProvider.class);
-        ExecutionContexts.fromExecutorService(MoreExecutors.newDirectExecutorService());
+        mockedRemoteSchemaRepository = mock(RemoteYangTextSourceProvider.class);
         remoteSchemaProvider = new RemoteSchemaProvider(mockedRemoteSchemaRepository,
-                ExecutionContexts.fromExecutorService(MoreExecutors.newDirectExecutorService()));
+                ExecutionContexts.fromExecutor(MoreExecutors.directExecutor()));
     }
 
     @Test
-    public void getExistingYangTextSchemaSource() throws IOException, SchemaSourceException {
+    public void getExistingYangTextSchemaSource() throws IOException, InterruptedException, ExecutionException {
         String source = "Test";
         YangTextSchemaSource schemaSource = YangTextSchemaSource.delegateForByteSource(
                 ID, ByteSource.wrap(source.getBytes()));
         YangTextSchemaSourceSerializationProxy sourceProxy = new YangTextSchemaSourceSerializationProxy(schemaSource);
-        Mockito.when(mockedRemoteSchemaRepository.getYangTextSchemaSource(ID))
-            .thenReturn(Futures.successful(sourceProxy));
+        doReturn(Futures.successful(sourceProxy)).when(mockedRemoteSchemaRepository).getYangTextSchemaSource(ID);
 
-        YangTextSchemaSource providedSource = remoteSchemaProvider.getSource(ID).checkedGet();
+        YangTextSchemaSource providedSource = remoteSchemaProvider.getSource(ID).get();
         assertEquals(providedSource.getIdentifier(), ID);
         assertArrayEquals(providedSource.read(), schemaSource.read());
     }
 
-    @Test(expected = SchemaSourceException.class)
-    public void getNonExistingSchemaSource() throws Exception {
-        Futures.failed(new Exception("halo"));
+    @Test
+    public void getNonExistingSchemaSource() throws InterruptedException {
+        doReturn(Futures.failed(new SchemaSourceException("Source not provided"))).when(mockedRemoteSchemaRepository)
+            .getYangTextSchemaSource(ID);
 
-        Mockito.when(mockedRemoteSchemaRepository.getYangTextSchemaSource(ID)).thenReturn(
-                Futures.failed(
-                        new SchemaSourceException("Source not provided")));
-
-        CheckedFuture<?, ?> sourceFuture = remoteSchemaProvider.getSource(ID);
+        ListenableFuture<YangTextSchemaSource> sourceFuture = remoteSchemaProvider.getSource(ID);
         assertTrue(sourceFuture.isDone());
-        sourceFuture.checkedGet();
+        try {
+            sourceFuture.get();
+            fail("Expected a failure to occur");
+        } catch (ExecutionException e) {
+            final Throwable cause = e.getCause();
+            assertThat(cause, instanceOf(SchemaSourceException.class));
+        }
     }
 }
