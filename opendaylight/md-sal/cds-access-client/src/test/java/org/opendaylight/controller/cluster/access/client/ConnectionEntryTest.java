03baf7dfd6bb5794a7254996a959e172955f9317
Robert Varga <robert.varga@pantheon.tech>
2019-10-16 11:26:42 +0200

    Migrate deprecated testing components
    org.scalatest.junit.JUnitRunner and org.mockito.Matchers have
    replacements, migrate to using them.
    
    Change-Id: Iff6fa0c937361bb68d4d001feee29af21a430836
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ConnectionEntryTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ConnectionEntryTest.java
@@ -7,7 +7,7 @@
  */
 package org.opendaylight.controller.cluster.access.client;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
