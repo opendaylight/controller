2cdbc95c75f22769b34f471ec386c9abcdb7fbec
Tibor Král <tibor.kral@pantheon.tech>
2019-10-22 18:24:21 +0200

    Allow programmatic module sharding configuration
    Adds the option to create Configuration without the need for
    module-shards.conf file. Current version only accepts a path
    to the module-shards.conf. The file is later loaded and
    deserialized into a Config object. This patch provides the
    option to pass the Config object directly.
    
    Change-Id: Ibeb666bd5f01d7ae972d26453046c959749a0656
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit b89c7ece8e827ce8d85b343f431d4fd974c90e32)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/HybridModuleShardConfigProvider.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/HybridModuleShardConfigProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,29 +7,25 @@
  */
 package org.opendaylight.controller.cluster.datastore.config;
 
+import static java.util.Objects.requireNonNull;
+
 import com.typesafe.config.Config;
 import java.util.Map;
 
-/**
- * Implementation of ModuleShardConfigProvider that reads the module and shard configuration from files.
- *
- * @author Thomas Pantelis
- */
-public class FileModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
-    private final String moduleShardsConfigPath;
+public class HybridModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
+    private final Config moduleShardsConfig;
     private final String modulesConfigPath;
 
-    public FileModuleShardConfigProvider(final String moduleShardsConfigPath, final String modulesConfigPath) {
-        this.moduleShardsConfigPath = moduleShardsConfigPath;
+    public HybridModuleShardConfigProvider(final Config moduleShardsConfig, final String modulesConfigPath) {
+        this.moduleShardsConfig = requireNonNull(moduleShardsConfig, "ModuleShardsConfig can't be null");
         this.modulesConfigPath = modulesConfigPath;
     }
 
     @Override
     public Map<String, ModuleConfig.Builder> retrieveModuleConfigs(final Configuration configuration) {
-        Config moduleShardsConfig = loadConfigFromPath(moduleShardsConfigPath);
         Config modulesConfig = loadConfigFromPath(modulesConfigPath);
 
-        final Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(moduleShardsConfig);
+        final Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(this.moduleShardsConfig);
         readModulesConfig(modulesConfig, moduleConfigMap, configuration);
         return moduleConfigMap;
     }
