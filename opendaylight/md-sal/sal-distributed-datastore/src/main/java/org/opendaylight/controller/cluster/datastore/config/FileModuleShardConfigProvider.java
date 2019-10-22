d29531f498dd67f35916c0839b1e83dd098a27d1
Tibor Král <tibor.kral@pantheon.tech>
2019-10-22 18:24:21 +0200

    Adds the option to create Configuration without the need for module-shards.conf file. Current version only accepts a path to the module-shards.conf. The file is later loaded and deserialized into a Config object. This patch provides the option to pass the Config object directly.
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: Ibeb666bd5f01d7ae972d26453046c959749a0656

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
@@ -8,15 +8,7 @@
 package org.opendaylight.controller.cluster.datastore.config;
 
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigObject;
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import org.opendaylight.controller.cluster.access.concepts.MemberName;
-import org.opendaylight.controller.cluster.datastore.shardstrategy.ShardStrategyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +17,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author Thomas Pantelis
  */
-public class FileModuleShardConfigProvider implements ModuleShardConfigProvider {
+public class FileModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
     private static final Logger LOG = LoggerFactory.getLogger(FileModuleShardConfigProvider.class);
 
     private final String moduleShardsConfigPath;
@@ -38,84 +30,11 @@ public class FileModuleShardConfigProvider implements ModuleShardConfigProvider 
 
     @Override
     public Map<String, ModuleConfig.Builder> retrieveModuleConfigs(final Configuration configuration) {
-        final File moduleShardsFile = new File(moduleShardsConfigPath);
-        final File modulesFile = new File(modulesConfigPath);
-
-        Config moduleShardsConfig = null;
-        if (moduleShardsFile.exists()) {
-            LOG.info("module shards config file exists - reading config from it");
-            moduleShardsConfig = ConfigFactory.parseFile(moduleShardsFile);
-        } else {
-            LOG.warn("module shards configuration read from resource");
-            moduleShardsConfig = ConfigFactory.load(moduleShardsConfigPath);
-        }
-
-        Config modulesConfig = null;
-        if (modulesFile.exists()) {
-            LOG.info("modules config file exists - reading config from it");
-            modulesConfig = ConfigFactory.parseFile(modulesFile);
-        } else {
-            LOG.warn("modules configuration read from resource");
-            modulesConfig = ConfigFactory.load(modulesConfigPath);
-        }
+        Config moduleShardsConfig = loadConfigFromPath(moduleShardsConfigPath);
+        Config modulesConfig = loadConfigFromPath(modulesConfigPath);
 
         final Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(moduleShardsConfig);
         readModulesConfig(modulesConfig, moduleConfigMap, configuration);
-
         return moduleConfigMap;
-    }
-
-    private static void readModulesConfig(final Config modulesConfig,
-            final Map<String, ModuleConfig.Builder> moduleConfigMap, final Configuration configuration) {
-        final List<? extends ConfigObject> modulesConfigObjectList = modulesConfig.getObjectList("modules");
-
-        for (final ConfigObject o : modulesConfigObjectList) {
-            final ConfigObjectWrapper wrapper = new ConfigObjectWrapper(o);
-
-            final String moduleName = wrapper.stringValue("name");
-            final ModuleConfig.Builder builder = moduleConfigMap.computeIfAbsent(moduleName, ModuleConfig::builder);
-
-            builder.nameSpace(wrapper.stringValue("namespace"));
-            builder.shardStrategy(ShardStrategyFactory.newShardStrategyInstance(moduleName,
-                    wrapper.stringValue("shard-strategy"), configuration));
-        }
-    }
-
-    private static Map<String, ModuleConfig.Builder> readModuleShardsConfig(final Config moduleShardsConfig) {
-        final List<? extends ConfigObject> moduleShardsConfigObjectList =
-            moduleShardsConfig.getObjectList("module-shards");
-
-        final Map<String, ModuleConfig.Builder> moduleConfigMap = new HashMap<>();
-        for (final ConfigObject moduleShardConfigObject : moduleShardsConfigObjectList) {
-            final String moduleName = moduleShardConfigObject.get("name").unwrapped().toString();
-            final ModuleConfig.Builder builder = ModuleConfig.builder(moduleName);
-
-            final List<? extends ConfigObject> shardsConfigObjectList =
-                moduleShardConfigObject.toConfig().getObjectList("shards");
-
-            for (final ConfigObject shard : shardsConfigObjectList) {
-                final String shardName = shard.get("name").unwrapped().toString();
-                final List<MemberName> replicas = shard.toConfig().getStringList("replicas").stream()
-                        .map(MemberName::forName).collect(Collectors.toList());
-                builder.shardConfig(shardName, replicas);
-            }
-
-            moduleConfigMap.put(moduleName, builder);
-        }
-
-        return moduleConfigMap;
-    }
-
-    private static class ConfigObjectWrapper {
-
-        private final ConfigObject configObject;
-
-        ConfigObjectWrapper(final ConfigObject configObject) {
-            this.configObject = configObject;
-        }
-
-        public String stringValue(final String name) {
-            return configObject.get(name).unwrapped().toString();
-        }
     }
 }
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/config/ConfigurationImplHybridTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.cluster.datastore.config;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.util.List;
+import org.junit.Test;
+
+public class ConfigurationImplHybridTest extends ConfigurationImplBaseTest {
+
+    @Override
+    public ConfigurationImpl createConfiguration() {
+        Config moduleShardsConf = generateModuleShards(List.of(
+                generateShard("default", "default", List.of("member-1", "member-2", "member-3")),
+                generateShard("people", "people-1", List.of("member-1")),
+                generateShard("cars", "cars-1", List.of("member-1")),
+                generateShard("test", "test-1", List.of("member-1"))
+        ));
+        return new ConfigurationImpl(new HybridModuleShardConfigProvider(moduleShardsConf, "modules.conf"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullModuleShardsConf() {
+        new HybridModuleShardConfigProvider(null, "modules.conf");
+    }
+
+    private Config generateModuleShards(List<String> shards) {
+        String moduleShardsContent = String.format("module-shards = [%n%s]", String.join(",\n", shards));
+        return ConfigFactory.parseString(moduleShardsContent);
+    }
+
+
+    private String generateShard(String name, String shardsName, List<String> replicas) {
+        return "    {"
+                + "        name = \"" + name + "\"\n"
+                + "        shards = [\n"
+                + "            {\n"
+                + "                name=\"" + shardsName + "\"\n"
+                + "                replicas = " + replicas
+                + "                \n"
+                + "            }\n"
+                + "        ]\n"
+                + "    }";
+    }
+}
