ab8b823a49f382c4738239f4970302a4b37c1d99
Tibor Král <tibor.kral@pantheon.tech>
2019-10-22 18:24:21 +0200

    Adds the option to create Configuration without the need for module-shards.conf file. Current version only accepts a path to the module-shards.conf. The file is later loaded and deserialized into a Config object. This patch provides the option to pass the Config object directly.
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: Ibeb666bd5f01d7ae972d26453046c959749a0656

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/HybridModuleShardConfigProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.config;
+
+import static java.util.Objects.requireNonNull;
+
+import com.typesafe.config.Config;
+import java.util.Map;
+
+public class HybridModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
+
+    private final Config moduleShardsConfig;
+    private final String modulesConfigPath;
+
+    public HybridModuleShardConfigProvider(final Config moduleShardsConfig, final String modulesConfigPath) {
+        this.moduleShardsConfig = requireNonNull(moduleShardsConfig, "ModuleShardsConfig can't be null");
+        this.modulesConfigPath = modulesConfigPath;
+    }
+
+    @Override
+    public Map<String, ModuleConfig.Builder> retrieveModuleConfigs(final Configuration configuration) {
+        Config modulesConfig = loadConfigFromPath(modulesConfigPath);
+
+        final Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(this.moduleShardsConfig);
+        readModulesConfig(modulesConfig, moduleConfigMap, configuration);
+        return moduleConfigMap;
+    }
+}
