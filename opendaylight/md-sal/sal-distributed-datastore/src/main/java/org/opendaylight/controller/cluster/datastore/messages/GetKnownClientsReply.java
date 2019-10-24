e32959e0bbc326f47c30ed7347f9a9af26813f89
Robert Varga <robert.varga@pantheon.tech>
2019-10-24 13:54:38 +0200

    Add the ability to report known connected clients
    In order to allow from-scratch recovery, we need the ability to
    query all existing shards to provide the identifiers of clients
    they know about. This patch adds the models to support such a query
    as well as implementing it in cluster-admin.
    
    JIRA: CONTROLLER-1626
    Change-Id: Id69aeb9021c8111dad10930620c9eaacfccd0d94
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/GetKnownClientsReply.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.messages;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableSet;
+import java.io.Serializable;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
+
+/**
+ * Reply to {@link GetKnownClients}.
+ */
+public final class GetKnownClientsReply implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final @NonNull ImmutableSet<ClientIdentifier> clients;
+
+    public GetKnownClientsReply(final ImmutableSet<ClientIdentifier> clients) {
+        this.clients = requireNonNull(clients);
+    }
+
+    public @NonNull ImmutableSet<ClientIdentifier> getClients() {
+        return clients;
+    }
+}
