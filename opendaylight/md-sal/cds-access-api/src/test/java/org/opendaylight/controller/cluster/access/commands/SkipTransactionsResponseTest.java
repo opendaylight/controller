5135b2caa3b75888e9343429529f9751a71c6f3c
Robert Varga <robert.varga@pantheon.tech>
2019-11-12 23:50:34 +0100

    Track skipped transactions
    We are allocating transaction identifiers which are shared across
    shards. As a transaction can touch only some shards, non-participating
    shards will never see the transaction ID, leading to holes in their
    UnsignedLongSets -- and those holes gradually eat up more and more
    memory.
    
    Track when we have such a hole and lazily forward a new request, which
    purges one or more such identifiers -- plugging the memory leak.
    
    The crux of the machinery is exchanging state between ProxyHistory
    and FrontendHistoryMetadataBuilder.
    
    JIRA: CONTROLLER-1991
    Change-Id: I3817fa2841e5f9c405bb20ff1a104537ad459ce3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit c7078128d6f35eebee2f98108ff929dcccfc322d)

---
+++ opendaylight/md-sal/cds-access-api/src/test/java/org/opendaylight/controller/cluster/access/commands/SkipTransactionsResponseTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.commands;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.controller.cluster.access.ABIVersion;
+
+public class SkipTransactionsResponseTest extends AbstractTransactionSuccessTest<SkipTransactionsResponse> {
+    private static final SkipTransactionsResponse OBJECT = new SkipTransactionsResponse(
+            TRANSACTION_IDENTIFIER, 0);
+
+    @Override
+    protected SkipTransactionsResponse object() {
+        return OBJECT;
+    }
+
+    @Test
+    public void cloneAsVersionTest() {
+        final SkipTransactionsResponse clone = OBJECT.cloneAsVersion(ABIVersion.BORON);
+        assertEquals(OBJECT, clone);
+    }
+
+    @Override
+    protected void doAdditionalAssertions(final Object deserialize) {
+        assertThat(deserialize, instanceOf(SkipTransactionsResponse.class));
+    }
+}
