a8d924c4bbf3135e13b141ffc1d2aad17e70252e
Robert Varga <robert.varga@pantheon.tech>
2020-02-21 06:33:21 +0100

    Remove NormalizedNodeInputOutput and related classes
    This functionality is available from yangtools, remove compatibility
    wrappers as they have been deprecated.
    
    JIRA: CONTROLLER-1925
    Change-Id: I40be1adb795a6663d647191eeab2f52afb6cbbb1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/SerializationUtils.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/datastore/node/utils/stream/SerializationUtils.java
@@ -19,6 +19,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeDataInput;
 import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeDataOutput;
+import org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeStreamVersion;
 
 /**
  * Provides various utility methods for serialization and de-serialization.
@@ -61,12 +62,6 @@ public final class SerializationUtils {
         }
     }
 
-    @Deprecated(forRemoval = true)
-    public static void writeNormalizedNode(final DataOutput out, final NormalizedNodeStreamVersion version,
-            final @Nullable NormalizedNode<?, ?> node) throws IOException {
-        writeNormalizedNode(out, version.toYangtools(), node);
-    }
-
     public static YangInstanceIdentifier readPath(final DataInput in) throws IOException {
         return NormalizedNodeDataInput.newDataInput(in).readYangInstanceIdentifier();
     }
@@ -76,18 +71,11 @@ public final class SerializationUtils {
         writePath(out, MAGNESIUM, path);
     }
 
-    public static void writePath(final DataOutput out,
-            final org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeStreamVersion version,
+    public static void writePath(final DataOutput out, final NormalizedNodeStreamVersion version,
             final @NonNull YangInstanceIdentifier path) throws IOException {
         try (NormalizedNodeDataOutput stream = version.newDataOutput(out)) {
             stream.writeYangInstanceIdentifier(path);
         }
-    }
-
-    @Deprecated(forRemoval = true)
-    public static void writePath(final DataOutput out, final NormalizedNodeStreamVersion version,
-            final @NonNull YangInstanceIdentifier path) throws IOException {
-        writePath(out, version.toYangtools(), path);
     }
 
     public static <T> void readNodeAndPath(final DataInput in, final T instance, final Applier<T> applier)
@@ -98,8 +86,7 @@ public final class SerializationUtils {
         applier.apply(instance, path, node);
     }
 
-    public static void writeNodeAndPath(final DataOutput out,
-            final org.opendaylight.yangtools.yang.data.codec.binfmt.NormalizedNodeStreamVersion version,
+    public static void writeNodeAndPath(final DataOutput out, final NormalizedNodeStreamVersion version,
             final YangInstanceIdentifier path, final NormalizedNode<?, ?> node) throws IOException {
         try (NormalizedNodeDataOutput stream = version.newDataOutput(out)) {
             stream.writeNormalizedNode(node);
