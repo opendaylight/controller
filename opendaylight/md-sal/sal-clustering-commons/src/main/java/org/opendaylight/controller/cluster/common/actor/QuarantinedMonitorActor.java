54146d795b64f015337c1398ed410049f214dd3a
Tejas Nevrekar <tejas.nevrekar@gmail.com>
2020-06-04 18:14:23 +0530

    Apply a workaround for the isolation of quarantined node
    Listen to the AssociationErrorEvent in addition to
    the ThisActorSystemQuarantinedEvent so that in cases
    like isolate followed by unisolate, the quarantine
    restart is triggered.
    
    JIRA: CONTROLLER-1941
    Change-Id: I937a9411c90fbbbaad73769efbc4e996369448a8
    Signed-off-by: Tejas Nevrekar <tejas.nevrekar@gmail.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/QuarantinedMonitorActor.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/QuarantinedMonitorActor.java
@@ -8,10 +8,15 @@
 
 package org.opendaylight.controller.cluster.common.actor;
 
+import akka.actor.Address;
 import akka.actor.Props;
 import akka.actor.UntypedAbstractActor;
 import akka.japi.Effect;
+import akka.remote.AssociationErrorEvent;
+import akka.remote.RemotingLifecycleEvent;
 import akka.remote.ThisActorSystemQuarantinedEvent;
+import java.util.HashSet;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,20 +29,23 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class QuarantinedMonitorActor extends UntypedAbstractActor {
-
-    private static final Logger LOG = LoggerFactory.getLogger(QuarantinedMonitorActor.class);
-
     public static final String ADDRESS = "quarantined-monitor";
 
+    private static final Logger LOG = LoggerFactory.getLogger(QuarantinedMonitorActor.class);
+    private static final Integer MESSAGE_THRESHOLD = 10;
+
+    private final Set<Address> addressSet = new HashSet<>();
     private final Effect callback;
+
     private boolean quarantined;
+    private int count;
 
     protected QuarantinedMonitorActor(final Effect callback) {
         this.callback = callback;
 
         LOG.debug("Created QuarantinedMonitorActor");
 
-        getContext().system().eventStream().subscribe(getSelf(), ThisActorSystemQuarantinedEvent.class);
+        getContext().system().eventStream().subscribe(getSelf(), RemotingLifecycleEvent.class);
     }
 
     @Override
@@ -62,6 +70,28 @@ public class QuarantinedMonitorActor extends UntypedAbstractActor {
 
             // execute the callback
             callback.apply();
+        } else if (message instanceof AssociationErrorEvent) {
+            final String errorMessage = message.toString();
+            LOG.trace("Got AssociationErrorEvent {}", errorMessage);
+            if (errorMessage.contains("The remote system has a UID that has been quarantined")) {
+                Address address = ((AssociationErrorEvent) message).getRemoteAddress();
+                addressSet.add(address);
+                count++;
+                LOG.trace("address:{} addressSet: {} count:{}", address, addressSet, count);
+                if (count >= MESSAGE_THRESHOLD && addressSet.size() > 1) {
+                    count = 0;
+                    addressSet.clear();
+                    final AssociationErrorEvent event = (AssociationErrorEvent) message;
+                    LOG.warn("Got quarantined via AssociationEvent by {}", event.remoteAddress());
+                    quarantined = true;
+
+                    // execute the callback
+                    callback.apply();
+                }
+            } else if (errorMessage.contains("The remote system explicitly disassociated")) {
+                count = 0;
+                addressSet.clear();
+            }
         }
     }
 
