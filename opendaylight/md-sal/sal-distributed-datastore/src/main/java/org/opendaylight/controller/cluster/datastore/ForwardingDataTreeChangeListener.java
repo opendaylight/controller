d752aa7aa98aabe8544a80abd14dbeb1712fea80
Tibor Král <tibor.kral@pantheon.tech>
2020-06-04 18:50:51 +0200

    Add sender actor to the ForwardingDataTreeChangeListener
    The ForwardingDataTreeChangeListener is created when a shard
    registers new listener. It then sends dataTreeChanges to said listener
    but doesn't specify the sender. The RootListenerActor however needs
    to know which shard produced the DataTreeChanged message.
    
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: I4971cfaefa10c3136e0ddcc350ec7e00e68e6b42

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ForwardingDataTreeChangeListener.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/ForwardingDataTreeChangeListener.java
@@ -12,6 +12,7 @@ import static java.util.Objects.requireNonNull;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import java.util.Collection;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.controller.cluster.datastore.messages.DataTreeChanged;
 import org.opendaylight.controller.cluster.datastore.messages.OnInitialData;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeChangeListener;
@@ -28,25 +29,28 @@ final class ForwardingDataTreeChangeListener implements DOMDataTreeChangeListene
     private static final Logger LOG = LoggerFactory.getLogger(ForwardingDataTreeChangeListener.class);
 
     private final ActorSelection actor;
+    private final ActorRef sendingActor;
 
-    ForwardingDataTreeChangeListener(final ActorSelection actor) {
+    ForwardingDataTreeChangeListener(final ActorSelection actor, @Nullable final ActorRef sendingActor) {
         this.actor = requireNonNull(actor, "actor should not be null");
+        this.sendingActor = sendingActor;
     }
 
     @Override
     public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
         LOG.debug("Sending DataTreeChanged to {}", actor);
-        actor.tell(new DataTreeChanged(changes), ActorRef.noSender());
+        actor.tell(new DataTreeChanged(changes), sendingActor);
     }
 
     @Override
     public void onInitialData() {
         LOG.debug("Sending OnInitialData to {}", actor);
-        actor.tell(OnInitialData.INSTANCE, ActorRef.noSender());
+        actor.tell(OnInitialData.INSTANCE, sendingActor);
     }
 
     @Override
     public String toString() {
-        return "ForwardingDataTreeChangeListener [actor=" + actor + "]";
+        return "ForwardingDataTreeChangeListener [actor=" + actor
+            + ", sending actor=" + (sendingActor != null ? sendingActor : "NO_SENDER") + "]";
     }
 }
