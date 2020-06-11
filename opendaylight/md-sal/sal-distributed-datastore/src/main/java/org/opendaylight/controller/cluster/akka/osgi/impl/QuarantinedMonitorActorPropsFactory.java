5a5c5504d40a45f3c88e7eff350545f7e360d7d9
Tibor Král <tibor.kral@pantheon.tech>
2020-06-11 10:28:55 +0200

    Add option to disable default ActorSystemQuarantinedEvent handling
    The default reaction to ThisActorSystemQuarantinedEvent is to
    restart the entire Karaf container. However some users may want
    to process the event differently.
    
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: Id65d31749dd97cb067611f7cfe4df76a6fe12204

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/akka/osgi/impl/QuarantinedMonitorActorPropsFactory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/akka/osgi/impl/QuarantinedMonitorActorPropsFactory.java
@@ -8,6 +8,7 @@
 package org.opendaylight.controller.cluster.akka.osgi.impl;
 
 import akka.actor.Props;
+import akka.japi.Effect;
 import org.opendaylight.controller.cluster.common.actor.QuarantinedMonitorActor;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -16,17 +17,30 @@ import org.slf4j.LoggerFactory;
 public final class QuarantinedMonitorActorPropsFactory {
     private static final Logger LOG = LoggerFactory.getLogger(QuarantinedMonitorActorPropsFactory.class);
 
+    private static final String DEFAULT_HANDLING_DISABLED_PROPERTY = "default_quarantine_event_handling_disabled";
+
     private QuarantinedMonitorActorPropsFactory() {
 
     }
 
     public static Props createProps(final BundleContext bundleContext) {
-        return QuarantinedMonitorActor.props(() -> {
+        Effect handling = () -> {
             // restart the entire karaf container
             LOG.warn("Restarting karaf container");
             System.setProperty("karaf.restart.jvm", "true");
             System.setProperty("karaf.restart", "true");
             bundleContext.getBundle(0).stop();
-        });
+        };
+
+        try {
+            final String property = System.getProperty(DEFAULT_HANDLING_DISABLED_PROPERTY);
+            if (property != null && property.equalsIgnoreCase("true")) {
+                handling = () -> {};
+            }
+        } catch (SecurityException secEx) {
+            LOG.warn("Access denied to system property {}. "
+                + "Therefore default handling will be used", DEFAULT_HANDLING_DISABLED_PROPERTY);
+        }
+        return QuarantinedMonitorActor.props(handling);
     }
 }
