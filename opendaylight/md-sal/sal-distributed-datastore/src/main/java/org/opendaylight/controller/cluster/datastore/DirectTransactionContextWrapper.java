bbefdc5d0788b438ac50fdad9a5412b22fc19f41
tadei.bilan <tadei.bilan@pantheon.tech>
2020-07-02 13:48:54 +0300

    Specialize TransactionContextWrapper
    Most of the time we are talking to local leader, in which case
    we do not need to queue messages and bounce them through the queue.
    
    JIRA: CONTROLLER-1952
    Change-Id: I07d85c82c2ab6e4251c70b2e6d1dafa2dc455d39
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/DirectTransactionContextWrapper.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import static java.util.Objects.requireNonNull;
+
+import akka.actor.ActorSelection;
+import java.util.Optional;
+import java.util.SortedSet;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.controller.cluster.datastore.utils.ActorUtils;
+import scala.concurrent.Future;
+
+/**
+ *  Direct implementation of TransactionContextWrapper. Operation are not queued but executed directly
+ *  on TransactionContext. Always has completed context and executes on local shard.
+ */
+final class DirectTransactionContextWrapper extends AbstractTransactionContextWrapper {
+
+    private final TransactionContext transactionContext;
+
+    DirectTransactionContextWrapper(@NonNull final TransactionIdentifier identifier,
+                                    @NonNull final ActorUtils actorUtils,
+                                    @NonNull final String shardName,
+                                    @NonNull final TransactionContext transactionContext) {
+        super(identifier, actorUtils, shardName);
+        this.transactionContext = requireNonNull(transactionContext);
+    }
+
+    @Override
+    TransactionContext getTransactionContext() {
+        return transactionContext;
+    }
+
+    @Override
+    void maybeExecuteTransactionOperation(final TransactionOperation op) {
+        op.invoke(transactionContext, null);
+    }
+
+    @Override
+    Future<ActorSelection> readyTransaction(final Optional<SortedSet<String>> participatingShardNames) {
+        return transactionContext.readyTransaction(null, participatingShardNames);
+    }
+
+}
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/DirectTransactionContextWrapperTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.controller.cluster.datastore.utils.ActorUtils;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class DirectTransactionContextWrapperTest {
+    @Mock
+    private ActorUtils actorUtils;
+
+    @Mock
+    private TransactionContext transactionContext;
+
+    @Mock
+    private TransactionOperation transactionOperation;
+
+    private DirectTransactionContextWrapper contextWrapper;
+
+    @Before
+    public void setUp() {
+        doReturn(DatastoreContext.newBuilder().build()).when(actorUtils).getDatastoreContext();
+        contextWrapper = new DirectTransactionContextWrapper(MockIdentifiers.transactionIdentifier(
+                DelayedTransactionContextWrapperTest.class, "mock"), actorUtils, "mock",
+                transactionContext);
+    }
+
+    @Test
+    public void testMaybeExecuteTransactionOperation() {
+        contextWrapper.maybeExecuteTransactionOperation(transactionOperation);
+
+        verify(transactionOperation, times(1)).invoke(transactionContext, null);
+    }
+
+}
