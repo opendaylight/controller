c452985c8fe80bf6dcb521d63452bc8a488c5b9b
tadei.bilan <tadei.bilan@pantheon.tech>
2020-07-02 13:48:54 +0300

    Specialize TransactionContextWrapper
    Most of the time we are talking to local leader, in which case
    we do not need to queue messages and bounce them through the queue.
    
    JIRA: CONTROLLER-1952
    Change-Id: I07d85c82c2ab6e4251c70b2e6d1dafa2dc455d39
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/DelayedTransactionContextWrapperTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/DelayedTransactionContextWrapperTest.java
@@ -19,20 +19,20 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.controller.cluster.datastore.utils.ActorUtils;
 
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class TransactionContextWrapperTest {
+public class DelayedTransactionContextWrapperTest {
     @Mock
     private ActorUtils actorUtils;
 
     @Mock
     private TransactionContext transactionContext;
 
-    private TransactionContextWrapper transactionContextWrapper;
+    private DelayedTransactionContextWrapper transactionContextWrapper;
 
     @Before
     public void setUp() {
         doReturn(DatastoreContext.newBuilder().build()).when(actorUtils).getDatastoreContext();
-        transactionContextWrapper = new TransactionContextWrapper(MockIdentifiers.transactionIdentifier(
-            TransactionContextWrapperTest.class, "mock"), actorUtils, "mock");
+        transactionContextWrapper = new DelayedTransactionContextWrapper(MockIdentifiers.transactionIdentifier(
+            DelayedTransactionContextWrapperTest.class, "mock"), actorUtils, "mock");
     }
 
     @Test
