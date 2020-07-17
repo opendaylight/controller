09e5c38a158ba7346717725f3b2a6922b9961d33
Robert Varga <robert.varga@pantheon.tech>
2020-07-17 19:21:41 +0200

    Migrate DatastoreContextIntrospectorFactory to OSGi DS
    We are slowly converting hand-written blueprint config to OSGi DS,
    so that we can end up ditching blueprint completely. This is the
    next small step towards that goal.
    
    Change-Id: I398d71344bccfef752128c561d6d2ff851edc5c2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractDatastoreContextIntrospectorFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.distributed.datastore.provider.rev140612.DataStorePropertiesContainer;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+abstract class AbstractDatastoreContextIntrospectorFactory implements DatastoreContextIntrospectorFactory {
+    @Override
+    public final DatastoreContextIntrospector newInstance(final LogicalDatastoreType datastoreType) {
+        return newInstance(DatastoreContext.newBuilder()
+                .logicalStoreType(datastoreType)
+                .tempFileDirectory("./data")
+                .build());
+    }
+
+    @VisibleForTesting
+    final @NonNull DatastoreContextIntrospector newInstance(final DatastoreContext context) {
+        final DataStorePropertiesContainer defaultPropsContainer = (DataStorePropertiesContainer)
+                serializer().fromNormalizedNode(YangInstanceIdentifier.of(DataStorePropertiesContainer.QNAME),
+                    ImmutableNodes.containerNode(DataStorePropertiesContainer.QNAME)).getValue();
+
+        return new DatastoreContextIntrospector(context, defaultPropsContainer);
+    }
+
+    abstract BindingNormalizedNodeSerializer serializer();
+}
---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/OSGiDatastoreContextIntrospectorFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Beta
+@Component(immediate = true, service = DatastoreContextIntrospectorFactory.class)
+public final class OSGiDatastoreContextIntrospectorFactory extends AbstractDatastoreContextIntrospectorFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(OSGiDatastoreContextIntrospectorFactory.class);
+
+    @Reference
+    volatile BindingNormalizedNodeSerializer serializer = null;
+
+    @Override
+    BindingNormalizedNodeSerializer serializer() {
+        return verifyNotNull(serializer);
+    }
+
+    @Activate
+    @SuppressWarnings("static-method")
+    void activate() {
+        LOG.info("Datastore Context Introspector activated");
+    }
+
+    @Deactivate
+    @SuppressWarnings("static-method")
+    void deactivate() {
+        LOG.info("Datastore Context Introspector deactivated");
+    }
+}
