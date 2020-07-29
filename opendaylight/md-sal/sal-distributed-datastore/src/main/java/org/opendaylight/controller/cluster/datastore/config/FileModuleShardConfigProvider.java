7cfe228a69a14b2515e48668da4be4d10816ee5a
Robert Varga <robert.varga@pantheon.tech>
2020-07-29 12:09:34 +0200

    Covert FileModuleShardConfigProvider to OSGi DS
    This is a simple injected holder, convert it to a full-blown
    component.
    
    JIRA: CONTROLLER-1882
    Change-Id: I6676055801c178c4ffb4137ec6721128014dedde
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/config/FileModuleShardConfigProvider.java
@@ -9,15 +9,27 @@ package org.opendaylight.controller.cluster.datastore.config;
 
 import com.typesafe.config.Config;
 import java.util.Map;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of ModuleShardConfigProvider that reads the module and shard configuration from files.
  *
  * @author Thomas Pantelis
  */
+@Component(immediate = true, service = ModuleShardConfigProvider.class)
 public class FileModuleShardConfigProvider extends AbstractModuleShardConfigProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(FileModuleShardConfigProvider.class);
+
     private final String moduleShardsConfigPath;
     private final String modulesConfigPath;
+
+    public FileModuleShardConfigProvider() {
+        this("./configuration/initial/module-shards.conf", "./configuration/initial/modules.conf");
+    }
 
     public FileModuleShardConfigProvider(final String moduleShardsConfigPath, final String modulesConfigPath) {
         this.moduleShardsConfigPath = moduleShardsConfigPath;
@@ -32,5 +44,15 @@ public class FileModuleShardConfigProvider extends AbstractModuleShardConfigProv
         final Map<String, ModuleConfig.Builder> moduleConfigMap = readModuleShardsConfig(moduleShardsConfig);
         readModulesConfig(modulesConfig, moduleConfigMap, configuration);
         return moduleConfigMap;
+    }
+
+    @Activate
+    void activate() {
+        LOG.info("Shard configuration provider started");
+    }
+
+    @Deactivate
+    void deactivate() {
+        LOG.info("Shard configuration provider stopped");
     }
 }
