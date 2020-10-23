20e9531d74b11975457d1fdb3530eadb0bb73833
tadei.bilan <tadei.bilan@pantheon.tech>
2020-10-23 14:54:09 +0300

    Use java.lang.ref.Cleaner instead of FinalizablePhantomReference(TransactionContextCleanup)
    Improve temporary file clean up by using a Cleaner to dispatch
    cleaning tasks. Since it gives us a Cleanable, we can dispense with
    a tracking map and removal from it -- Cleanable makes sure it is
    called exactly once.
    This is part of bigger migration.
    
    JIRA: CONTROLLER-1911
    Change-Id: I2858652357ac4db69fc10a01d13d43824cde4507
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/TransactionContextCleanup.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/TransactionContextCleanup.java
@@ -7,15 +7,17 @@
  */
 package org.opendaylight.controller.cluster.datastore;
 
-import com.google.common.base.FinalizablePhantomReference;
-import com.google.common.base.FinalizableReferenceQueue;
+import static java.util.Objects.requireNonNull;
+
+import java.lang.ref.Cleaner;
+import java.lang.ref.Cleaner.Cleanable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A PhantomReference that closes remote transactions for a TransactionContext when it's
+ * A class that closes remote transactions for a TransactionContext when it's
  * garbage collected. This is used for read-only transactions as they're not explicitly closed
  * by clients. So the only way to detect that a transaction is no longer in use and it's safe
  * to clean up is when it's garbage collected. It's inexact as to when an instance will be GC'ed
@@ -23,45 +25,38 @@ import org.slf4j.LoggerFactory;
  * to the old generation space and thus should be cleaned up in a timely manner as the GC
  * runs on the young generation (eden, swap1...) space much more frequently.
  */
-final class TransactionContextCleanup extends FinalizablePhantomReference<TransactionProxy> {
-    private static final Logger LOG = LoggerFactory.getLogger(TransactionContextCleanup.class);
-    /**
-     * Used to enqueue the PhantomReferences for read-only TransactionProxy instances. The
-     * FinalizableReferenceQueue is safe to use statically in an OSGi environment as it uses some
-     * trickery to clean up its internal thread when the bundle is unloaded.
-     */
-    private static final FinalizableReferenceQueue QUEUE = new FinalizableReferenceQueue();
+final class TransactionContextCleanup {
+    private static final class Cleanup implements Runnable {
+        private final TransactionContext cleanup;
 
-    /**
-     * This stores the TransactionProxyCleanupPhantomReference instances statically, This is
-     * necessary because PhantomReferences need a hard reference so they're not garbage collected.
-     * Once finalized, the TransactionProxyCleanupPhantomReference removes itself from this map
-     * and thus becomes eligible for garbage collection.
-     */
-    private static final Map<TransactionContext, TransactionContextCleanup> CACHE = new ConcurrentHashMap<>();
+        Cleanup(final TransactionContext cleanup) {
+            this.cleanup = requireNonNull(cleanup);
+        }
 
-    private final TransactionContext cleanup;
-
-    private TransactionContextCleanup(final TransactionProxy referent, final TransactionContext cleanup) {
-        super(referent, QUEUE);
-        this.cleanup = cleanup;
-    }
-
-    static void track(final TransactionProxy referent, final TransactionContext cleanup) {
-        final TransactionContextCleanup ret = new TransactionContextCleanup(referent, cleanup);
-        CACHE.put(cleanup, ret);
-    }
-
-    @Override
-    public void finalizeReferent() {
-        LOG.trace("Cleaning up {} Tx actors", cleanup);
-
-        if (CACHE.remove(cleanup) != null) {
+        @Override
+        public void run() {
+            LOG.trace("Cleaning up {} Tx actors", cleanup);
+            TRACKING.remove(cleanup);
             cleanup.closeTransaction();
         }
     }
 
-    static void untrack(final TransactionContext cleanup) {
-        CACHE.remove(cleanup);
+    private static final Logger LOG = LoggerFactory.getLogger(TransactionContextCleanup.class);
+    private static final Cleaner CLEANER = Cleaner.create();
+    private static final Map<TransactionContext, Cleanable> TRACKING = new ConcurrentHashMap<>();
+
+    private TransactionContextCleanup() {
+        // Hidden on purpose
+    }
+
+    static void track(final TransactionProxy referent, final TransactionContext cleanup) {
+        TRACKING.put(cleanup, CLEANER.register(referent, new Cleanup(cleanup)));
+    }
+
+    static void untrack(final TransactionContext referent) {
+        final Cleanable cleanup = TRACKING.remove(referent);
+        if (cleanup != null) {
+            cleanup.clean();
+        }
     }
 }
