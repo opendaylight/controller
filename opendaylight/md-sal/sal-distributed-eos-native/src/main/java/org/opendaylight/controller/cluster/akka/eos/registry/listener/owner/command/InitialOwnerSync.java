d34777e58ffd34e5dcdbd6c9d8c6508e492a89ec
Tomas Cere <tomas.cere@pantheon.tech>
2021-03-18 11:32:54 +0100

    Temporarily squahed native-eos
    Multi-patch job failed for the regular version, so attempt
    to sidestep the cherry-pick failure by squashing.
    
    Change-Id: I85bb83483812d985e48d11a2f2e0dfd24892f76e
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-eos-native/src/main/java/org/opendaylight/controller/cluster/akka/eos/registry/listener/owner/command/InitialOwnerSync.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.akka.eos.registry.listener.owner.command;
+
+import akka.cluster.ddata.LWWRegister;
+import akka.cluster.ddata.typed.javadsl.Replicator;
+
+public class InitialOwnerSync implements ListenerCommand {
+
+    private final Replicator.GetResponse<LWWRegister<String>> response;
+
+    public InitialOwnerSync(final Replicator.GetResponse<LWWRegister<String>> response) {
+        this.response = response;
+    }
+
+    public Replicator.GetResponse<LWWRegister<String>> getResponse() {
+        return response;
+    }
+}
