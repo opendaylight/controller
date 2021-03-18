8681f9378cc33ede952a76ff480a72c9615f83ff
Tomas Cere <tomas.cere@pantheon.tech>
2021-03-18 11:32:54 +0100

    Introduce DOMEntityOwnershipService replacement
    Introduce a new DOMEntityOwnershipService implementation
    backed by akka distributed-data and cluster-singleton.
    
    Change-Id: I30753e83d1af10658141311842bdceb315b9237f
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-eos-native/src/main/java/org/opendaylight/controller/cluster/akka/eos/bootstrap/command/RunningContext.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.akka.eos.bootstrap.command;
+
+import akka.actor.typed.ActorRef;
+import org.opendaylight.controller.cluster.akka.eos.owner.checker.command.StateCheckerCommand;
+import org.opendaylight.controller.cluster.akka.eos.owner.supervisor.command.OwnerSupervisorCommand;
+import org.opendaylight.controller.cluster.akka.eos.registry.candidate.command.CandidateRegistryCommand;
+import org.opendaylight.controller.cluster.akka.eos.registry.listener.type.command.TypeListenerRegistryCommand;
+
+public class RunningContext implements BootstrapCommand {
+
+    private final ActorRef<TypeListenerRegistryCommand> listenerRegistry;
+    private final ActorRef<CandidateRegistryCommand> candidateRegistry;
+    private final ActorRef<StateCheckerCommand> ownerStateChecker;
+    private final ActorRef<OwnerSupervisorCommand> ownerSupervisor;
+
+    public RunningContext(final ActorRef<TypeListenerRegistryCommand> listenerRegistry,
+                          final ActorRef<CandidateRegistryCommand> candidateRegistry,
+                          final ActorRef<StateCheckerCommand> ownerStateChecker,
+                          final ActorRef<OwnerSupervisorCommand> ownerSupervisor) {
+        this.listenerRegistry = listenerRegistry;
+        this.candidateRegistry = candidateRegistry;
+        this.ownerStateChecker = ownerStateChecker;
+        this.ownerSupervisor = ownerSupervisor;
+    }
+
+    public ActorRef<TypeListenerRegistryCommand> getListenerRegistry() {
+        return listenerRegistry;
+    }
+
+    public ActorRef<CandidateRegistryCommand> getCandidateRegistry() {
+        return candidateRegistry;
+    }
+
+    public ActorRef<StateCheckerCommand> getOwnerStateChecker() {
+        return ownerStateChecker;
+    }
+
+    public ActorRef<OwnerSupervisorCommand> getOwnerSupervisor() {
+        return ownerSupervisor;
+    }
+}
