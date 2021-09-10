b7b076b373a8cc69d9a71944a07e5a81ad2c715b
Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>
2021-09-10 11:09:41 +0200

    Introduce cli commands for cluster-admin
    Rework the rpcs needed by csit in cluster-admin to karaf cli commands
    
    JIRA: CONTROLLER-1996
    Change-Id: I148c7c9140701ff8ffe81c612999898115f4661d
    Signed-off-by: Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>

---
+++ opendaylight/md-sal/sal-cluster-admin-karaf-cli/src/main/java/org/opendaylight/controller/cluster/datastore/admin/command/RemoveAllShardReplicasCommand.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.admin.command;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.opendaylight.mdsal.binding.api.RpcConsumerRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.ClusterAdminService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.RemoveAllShardReplicasInputBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+@Service
+@Command(scope = "cluster-admin", name = "remove-all-shard-replicas",
+        description = "Run a remove-all-shard-replicas test")
+public class RemoveAllShardReplicasCommand extends AbstractRpcAction {
+    @Reference
+    private RpcConsumerRegistry rpcConsumerRegistry;
+    @Argument(index = 0, name = "member-name",required = true)
+    private String memberName;
+
+    @Override
+    protected ListenableFuture<? extends RpcResult<?>> invokeRpc() {
+        return rpcConsumerRegistry.getRpcService(ClusterAdminService.class)
+                .removeAllShardReplicas(new RemoveAllShardReplicasInputBuilder()
+                        .setMemberName(memberName)
+                        .build());
+    }
+}
