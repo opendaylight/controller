7560f1e6a5418b0eb1a408e630f266be9dfdd21b
Tibor Král <tibor.kral@pantheon.tech>
2022-02-07 12:56:12 -0700

    Add selectively-bounded implementation of Mailbox
    There are cases when having unbounded mailbox can
    lead to OOM errors. This implementation adds the
    option to set capacity limits for both message
    queues separately if needed. By default this
    implementation behaves identically to
    UnboundedDequeBasedControlAwareMailbox.
    
    JIRA: CONTROLLER-2030
    Change-Id: I9e04ba06880a868bf6962d3616780bd741bd5260
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/SelectivelyBoundedDequeBasedControlAwareMailbox.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.common.actor;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.dispatch.ControlMessage;
+import akka.dispatch.DequeBasedMessageQueueSemantics;
+import akka.dispatch.Envelope;
+import akka.dispatch.MailboxType;
+import akka.dispatch.ProducesMessageQueue;
+import akka.dispatch.UnboundedControlAwareMailbox;
+import com.codahale.metrics.Gauge;
+import com.typesafe.config.Config;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Option;
+
+/**
+ * By default an unbounded ControlAwareMailbox that also supports DequeBasedMessageQueueSemantics so it can be used with
+ * persistent actors which use stashing. This implementation can however put a limit on the size of queues, which
+ * can prevent OOM in case the actor can't process messages quicker than they arrive. If a queue reaches it's limit, all
+ * further messages are sent to DeadLetters (if possible)
+ */
+public final class SelectivelyBoundedDequeBasedControlAwareMailbox implements MailboxType,
+        ProducesMessageQueue<SelectivelyBoundedDequeBasedControlAwareMailbox.MessageQueue> {
+    private static final Logger LOG = LoggerFactory.getLogger(SelectivelyBoundedDequeBasedControlAwareMailbox.class);
+    private static final String NORMAL_QUEUE_SIZE = "normal-q-size";
+    private static final String CONTROL_QUEUE_SIZE = "control-q-size";
+    private static final String TOTAL_QUEUE_SIZE = "total-q-size";
+    private static final String LIMIT_NORMAL_QUEUE_CAPACITY_PROPERTY = "limit-normal-queue-capacity";
+    private static final String LIMIT_CONTROL_QUEUE_CAPACITY_PROPERTY = "limit-control-queue-capacity";
+    private final Config config;
+
+    public SelectivelyBoundedDequeBasedControlAwareMailbox(final ActorSystem.Settings settings, final Config config) {
+        this.config = config;
+    }
+
+    @Override
+    public MessageQueue create(final Option<ActorRef> owner, final Option<ActorSystem> system) {
+        LOG.debug("Creating MessageQueue for {}", owner);
+
+        final MessageQueue queue = new MessageQueue(config, system);
+
+        MeteredBoundedMailbox.registerMetric(owner, NORMAL_QUEUE_SIZE, (Gauge<Integer>) () -> queue.queue().size());
+        MeteredBoundedMailbox.registerMetric(owner, CONTROL_QUEUE_SIZE,
+            (Gauge<Integer>) () -> queue.controlQueue().size());
+        MeteredBoundedMailbox.registerMetric(owner, TOTAL_QUEUE_SIZE, (Gauge<Integer>) queue::numberOfMessages);
+
+        return queue;
+    }
+
+    static class MessageQueue extends UnboundedControlAwareMailbox.MessageQueue
+            implements DequeBasedMessageQueueSemantics {
+        private static final long serialVersionUID = 1L;
+        private static final int NO_CAPACITY_LIMIT = 0;
+
+        private final Deque<Envelope> controlQueue = new ConcurrentLinkedDeque<>();
+        private final Deque<Envelope> queue = new ConcurrentLinkedDeque<>();
+        private final ActorRef deadLetters;
+
+        private final int maxControlQueueCapacity;
+        private final int maxNormalQueueCapacity;
+
+        MessageQueue(final Config config, final Option<ActorSystem> system) {
+            if (system.nonEmpty()) {
+                deadLetters = system.get().deadLetters();
+            } else {
+                deadLetters = null;
+            }
+            if (config.hasPath(LIMIT_CONTROL_QUEUE_CAPACITY_PROPERTY)) {
+                int capacity = config.getInt(LIMIT_CONTROL_QUEUE_CAPACITY_PROPERTY);
+                this.maxControlQueueCapacity = capacity > 0 ? capacity : NO_CAPACITY_LIMIT;
+            } else {
+                this.maxControlQueueCapacity = NO_CAPACITY_LIMIT;
+            }
+            if (config.hasPath(LIMIT_NORMAL_QUEUE_CAPACITY_PROPERTY)) {
+                int capacity = config.getInt(LIMIT_NORMAL_QUEUE_CAPACITY_PROPERTY);
+                this.maxNormalQueueCapacity = capacity > 0 ? capacity : NO_CAPACITY_LIMIT;
+            } else {
+                this.maxNormalQueueCapacity = NO_CAPACITY_LIMIT;
+            }
+        }
+
+        @Override
+        public Queue<Envelope> controlQueue() {
+            return controlQueue;
+        }
+
+        @Override
+        public Queue<Envelope> queue() {
+            return queue;
+        }
+
+        @Override
+        public void enqueueFirst(final ActorRef actor, final Envelope envelope) {
+            final Object message = envelope.message();
+            LOG.trace("enqueueFirst: actor {}, message type: {}", actor, message.getClass());
+            if (message instanceof ControlMessage) {
+                if (maxControlQueueCapacity > NO_CAPACITY_LIMIT) {
+                    if (controlQueue.size() >= maxControlQueueCapacity) {
+                        LOG.warn("Control MessageQueue full(reached {}), dropping message {}", controlQueue.size(),
+                            message.getClass());
+                        toDeadLettersIfPossible(actor, envelope);
+                        return;
+                    }
+                }
+                controlQueue.addFirst(envelope);
+            } else {
+                if (maxNormalQueueCapacity > NO_CAPACITY_LIMIT) {
+                    if (queue.size() >= maxNormalQueueCapacity) {
+                        LOG.warn("Normal MessageQueue full(reached {}), dropping message {}", queue.size(),
+                            message.getClass());
+                        toDeadLettersIfPossible(actor, envelope);
+                        return;
+                    }
+                }
+                queue.addFirst(envelope);
+            }
+        }
+
+        private void toDeadLettersIfPossible(final ActorRef actor, final Envelope envelope) {
+            if (deadLetters != null) {
+                deadLetters.tell(envelope, actor);
+            }
+        }
+    }
+}
