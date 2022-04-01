7df83695979268a799be75495e690d88d514a74e
Shibu Vijayakumar <shibu.vijayakumar@infosys.com>
2022-04-01 18:14:34 +0000

    Add supervisor to EOS singleton actor
    The actor may fail, for example due to AskTimeoutException. This still
    needs to be investigated, but mitigate the failure by adding an
    OwnerSupervisor to restart it on failure.
    
    JIRA: CONTROLLER-2035
    Change-Id: I2dafaa35804357415d9aaa5400372cfd8e54b85b
    Signed-off-by: Shibu Vijayakumar <shibu.vijayakumar@infosys.com>
    (cherry picked from commit 7d48c55ad33bcb69e9e373b3e169b530771e60ab)

--- opendaylight/md-sal/eos-dom-akka/src/main/java/org/opendaylight/controller/eos/akka/bootstrap/EOSMain.java
+++ opendaylight/md-sal/eos-dom-akka/src/main/java/org/opendaylight/controller/eos/akka/bootstrap/EOSMain.java
@@ -9,6 +9,7 @@ package org.opendaylight.controller.eos.akka.bootstrap;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.SupervisorStrategy;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
@@ -47,8 +48,8 @@ public final class EOSMain extends AbstractBehavior<BootstrapCommand> {
         final ClusterSingleton clusterSingleton = ClusterSingleton.get(context.getSystem());
         // start the initial sync behavior that switches to the regular one after syncing
         ownerSupervisor = clusterSingleton.init(
-                SingletonActor.of(IdleSupervisor.create(iidCodec), "OwnerSupervisor"));
-
+                SingletonActor.of(Behaviors.supervise(IdleSupervisor.create(iidCodec))
+                        .onFailure(SupervisorStrategy.restart()), "OwnerSupervisor"));
         candidateRegistry = context.spawn(CandidateRegistryInit.create(ownerSupervisor), "CandidateRegistry");
 
         ownerStateChecker = context.spawn(OwnerStateChecker.create(role, ownerSupervisor, iidCodec),
