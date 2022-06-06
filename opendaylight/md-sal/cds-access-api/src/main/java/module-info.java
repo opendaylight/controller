b397c877eb0f41410180c4f36c9f4c759d785ea9
Robert Varga <robert.varga@pantheon.tech>
2022-06-06 19:30:14 +0200

    WIP: Promote cds-access-api to a JPMS module
    We are hitting nasties with parser-combinators, let's try to work them
    around by jumping to full JPMS.
    
    // FIXME: cannot dependencyes
    // FIXME: YANG module in a JPMS module
    
    Change-Id: I7f2b6b1f9c494dde086703250d7c8675226b010c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.controller.cluster.access {
+    exports org.opendaylight.controller.cluster.access;
+    exports org.opendaylight.controller.cluster.access.commands;
+    exports org.opendaylight.controller.cluster.access.concepts;
+    exports org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.cds.types.rev250131;
+
+    requires transitive java.desktop;
+    requires transitive org.opendaylight.controller.repackaged.pekko;
+    requires transitive org.opendaylight.yangtools.binding.spec;
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires transitive org.opendaylight.yangtools.yang.data.codec.binfmt;
+    requires com.google.common;
+    requires org.opendaylight.controller.scala3.library;
+    requires org.opendaylight.yangtools.yang.data.impl;
+    requires org.slf4j;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static com.github.spotbugs.annotations;
+}
