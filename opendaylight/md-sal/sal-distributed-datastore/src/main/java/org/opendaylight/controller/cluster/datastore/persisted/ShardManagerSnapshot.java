b1dc25848d3ba7cd6b96fdced9edddcf728c2c71
Robert Varga <robert.varga@pantheon.tech>
2022-11-26 11:59:46 +0100

    Remove old payload proxies
    We no longer need to carry compatibility serialization proxies, remove
    them.
    
    JIRA: CONTROLLER-2057
    Change-Id: Ib040c0a082c94383b0224d593ad0ebec951b4d31
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/ShardManagerSnapshot.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/ShardManagerSnapshot.java
@@ -7,16 +7,8 @@
  */
 package org.opendaylight.controller.cluster.datastore.persisted;
 
-import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.collect.ImmutableList;
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNull;
 
@@ -26,82 +18,6 @@ import org.eclipse.jdt.annotation.NonNull;
  * @author Thomas Pantelis
  */
 public final class ShardManagerSnapshot implements Serializable {
-    interface SerializedForm extends Externalizable {
-        /**
-         * Return the serial form of this object contents, corresponding to {@link ShardManagerSnapshot#shardList}.
-         *
-         * @return List of shards names.
-         */
-        List<String> shardNames();
-
-        /**
-         * Resolve this proxy to an actual {@link ShardManagerSnapshot}. Implementations can rely on the object to be
-         * set via {@link #resolveTo(ShardManagerSnapshot)}.
-         *
-         * @return A snapshot
-         */
-        Object readResolve();
-
-        /**
-         * Set this proxy to return {@code snapshot} on next {@link #readResolve()}.
-         *
-         * @param newSnapshot Snapshot to set
-         */
-        void resolveTo(@NonNull ShardManagerSnapshot newSnapshot);
-
-        @Override
-        default void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
-            final int size = in.readInt();
-            final var shardList = new ArrayList<String>(size);
-            for (int i = 0; i < size; i++) {
-                shardList.add((String) in.readObject());
-            }
-            resolveTo(new ShardManagerSnapshot(shardList));
-        }
-
-        @Override
-        default void writeExternal(final ObjectOutput out) throws IOException {
-            final var shardList = shardNames();
-            out.writeInt(shardList.size());
-            for (var shardName : shardList) {
-                out.writeObject(shardName);
-            }
-        }
-    }
-
-    @Deprecated(since = "7.0.0.", forRemoval = true)
-    private static final class Proxy implements SerializedForm {
-        private static final long serialVersionUID = 1L;
-
-        private ShardManagerSnapshot snapshot;
-
-        // checkstyle flags the public modifier as redundant which really doesn't make sense since it clearly isn't
-        // redundant. It is explicitly needed for Java serialization to be able to create instances via reflection.
-        @SuppressWarnings("checkstyle:RedundantModifier")
-        public Proxy() {
-            // For Externalizable
-        }
-
-        Proxy(final ShardManagerSnapshot snapshot) {
-            this.snapshot = snapshot;
-        }
-
-        @Override
-        public List<String> shardNames() {
-            return snapshot.getShardList();
-        }
-
-        @Override
-        public void resolveTo(final ShardManagerSnapshot newSnapshot) {
-            snapshot = requireNonNull(newSnapshot);
-        }
-
-        @Override
-        public Object readResolve() {
-            return verifyNotNull(snapshot);
-        }
-    }
-
     private static final long serialVersionUID = 1L;
 
     private final List<String> shardList;
