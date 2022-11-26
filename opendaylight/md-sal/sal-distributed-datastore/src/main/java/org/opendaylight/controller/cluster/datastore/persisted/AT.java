5569bbf02b1cc013ade1a646df51f60904cffd47
Robert Varga <robert.varga@pantheon.tech>
2022-11-26 11:19:31 +0100

    WIP: Add shorter serialization proxies
    Externalizable uses class names to identify the proxies and we are using
    rather long names. Introduce shorter versions of these, but do not emit
    them just yet -- establishing forward compatibility.
    
    Change-Id: I4da54ca57849c2bfdaaf76c22e7903abcfa58be9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/persisted/AT.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.datastore.persisted;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import org.opendaylight.controller.cluster.access.concepts.TransactionIdentifier;
+import org.opendaylight.controller.cluster.datastore.persisted.AbstractIdentifiablePayload.Proxy;
+
+/**
+ * Serialization proxy for {@link AbortTransactionPayload}.
+ */
+final class AT implements Proxy {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    private TransactionIdentifier identifier;
+    private byte[] bytes;
+
+    @SuppressWarnings("checkstyle:RedundantModifier")
+    public AT() {
+        // For Externalizable
+    }
+
+    AT(final byte[] bytes) {
+        this.bytes = requireNonNull(bytes);
+    }
+
+    @Override
+    public byte[] bytes() {
+        return bytes;
+    }
+
+    @Override
+    public void readExternal(final byte[] bytes) throws IOException {
+        this.bytes = requireNonNull(bytes);
+        identifier = verifyNotNull(TransactionIdentifier.readFrom(ByteStreams.newDataInput(bytes)));
+    }
+
+    @Override
+    public Object readResolve() {
+        return new AbortTransactionPayload(identifier, bytes);
+    }
+}
