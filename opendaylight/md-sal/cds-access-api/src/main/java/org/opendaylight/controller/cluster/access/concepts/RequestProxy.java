56a288da88c8189b0f9516ae1eb3b154339ef677
Robert Varga <robert.varga@pantheon.tech>
2022-11-27 13:59:16 +0100

    WIP: Add cds-access-api proxies
    JIRA: CONTROLLER-2051
    Change-Id: I01132665027687edc1c6d44dda8a6ab0cab6ad6a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/cds-access-api/src/main/java/org/opendaylight/controller/cluster/access/concepts/RequestProxy.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.access.concepts;
+
+import org.opendaylight.yangtools.concepts.WritableIdentifier;
+
+/**
+ * @author nite
+ *
+ */
+interface RequestProxy<T extends WritableIdentifier, C extends Request<T, C>> extends MessageProxy<T, C> {
+
+}
