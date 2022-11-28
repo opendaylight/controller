e1d69fc0de36ae9f875536af3bfbf28c0a702b09
Robert Varga <robert.varga@pantheon.tech>
2022-11-28 18:38:48 +0100

    Add serialVersionUID fields
    We are missing a few definitions and are about to modify the classes
    in backwards-compatible ways, but the changes would end up changing
    the versions. Make sure to define generated serialVersionUIDs.
    
    Change-Id: I006f51e16dd3fee66fb9c344bb99235780b08d3e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/AbortTransactionReply.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/AbortTransactionReply.java
@@ -5,12 +5,13 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.controller.cluster.datastore.messages;
 
 import org.opendaylight.controller.cluster.datastore.DataStoreVersions;
 
 public class AbortTransactionReply extends VersionedExternalizableMessage {
+    @java.io.Serial
+    private static final long serialVersionUID = 7251132353204199793L;
     private static final AbortTransactionReply INSTANCE = new AbortTransactionReply();
 
     public AbortTransactionReply() {
