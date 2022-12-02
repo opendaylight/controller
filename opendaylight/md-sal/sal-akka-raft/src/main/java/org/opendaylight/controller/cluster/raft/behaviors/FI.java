ec0e24523cf05b5daf88d8c9269e136f3213be00
Robert Varga <robert.varga@pantheon.tech>
2022-12-02 09:00:40 +0100

    Add ARGON serialization proxies
    Our serialization proxies result in a unnecessarity-big footprint.
    Define their replacements for forward compatibility with Argon raft
    version.
    
    Change-Id: I545485c6abdf16f6d81a48672b36eb83613013e1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/FI.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.behaviors;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/**
+ * Serialization proxy for {@link FollowerIdentifier}.
+ *
+ */
+final class FI implements Externalizable {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    private String value;
+
+    @SuppressWarnings("checkstyle:RedundantModifier")
+    public FI() {
+        // For Externalizable
+    }
+
+    FI(final String value) {
+        this.value = requireNonNull(value);
+    }
+
+    @Override
+    public void writeExternal(final ObjectOutput out) throws IOException {
+        out.writeObject(value);
+    }
+
+    @Override
+    public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+        value = (String) in.readObject();
+    }
+
+    @java.io.Serial
+    private Object readResolve() {
+        return new FollowerIdentifier(value);
+    }
+}
