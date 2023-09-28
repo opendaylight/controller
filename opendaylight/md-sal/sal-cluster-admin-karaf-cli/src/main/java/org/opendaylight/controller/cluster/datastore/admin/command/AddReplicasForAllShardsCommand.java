d40c2a74c1461f02200927e006efa5ab2aac8a20
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2023-09-28 18:29:11 +0200

    Do not use RpcService in sal-cluster-admin-karaf-cli
    Migrated usage of RpcService to Rpc<?,?> for sal-cluster-admin-karaf-cli.
    
    JIRA: CONTROLLER-2085
    Change-Id: I6cf42319c180df1ec89293abb6165041d3b99f88
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- opendaylight/md-sal/sal-cluster-admin-karaf-cli/src/main/java/org/opendaylight/controller/cluster/datastore/admin/command/AddReplicasForAllShardsCommand.java
+++ opendaylight/md-sal/sal-cluster-admin-karaf-cli/src/main/java/org/opendaylight/controller/cluster/datastore/admin/command/AddReplicasForAllShardsCommand.java
@@ -12,8 +12,8 @@ import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.opendaylight.mdsal.binding.api.RpcConsumerRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.AddReplicasForAllShards;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.AddReplicasForAllShardsInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.cluster.admin.rev151013.ClusterAdminService;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
 @Service
@@ -25,7 +25,7 @@ public class AddReplicasForAllShardsCommand extends AbstractRpcAction {
 
     @Override
     protected ListenableFuture<? extends RpcResult<?>> invokeRpc() {
-        return rpcConsumerRegistry.getRpcService(ClusterAdminService.class)
-                .addReplicasForAllShards(new AddReplicasForAllShardsInputBuilder().build());
+        return rpcConsumerRegistry.getRpc(AddReplicasForAllShards.class)
+            .invoke(new AddReplicasForAllShardsInputBuilder().build());
     }
 }
