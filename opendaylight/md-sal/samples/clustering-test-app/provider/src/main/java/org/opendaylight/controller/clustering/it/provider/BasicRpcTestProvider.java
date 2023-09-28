13394048bdfca829828c7d269f6d3c59c72e869d
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2023-09-28 19:18:35 +0200

    Do not use RpcService in provider
    Migrated usage of RpcService to Rpc<?,?> for provider.
    
    JIRA: CONTROLLER-2085
    Change-Id: I38d66761df26506c383402e32b5fe605d05dcda8
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/BasicRpcTestProvider.java
+++ opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/BasicRpcTestProvider.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.controller.clustering.it.provider;
 
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import javax.annotation.PreDestroy;
@@ -16,13 +17,9 @@ import org.opendaylight.mdsal.binding.api.RpcProviderService;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
 import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.controller.basic.rpc.test.rev160120.BasicGlobalInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.controller.basic.rpc.test.rev160120.BasicGlobalOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.controller.basic.rpc.test.rev160120.BasicGlobal;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.controller.basic.rpc.test.rev160120.BasicGlobalOutputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.controller.basic.rpc.test.rev160120.BasicRpcTestService;
-import org.opendaylight.yangtools.concepts.ObjectRegistration;
 import org.opendaylight.yangtools.concepts.Registration;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -33,14 +30,14 @@ import org.slf4j.LoggerFactory;
 
 @Singleton
 @Component(service = { })
-public final class BasicRpcTestProvider implements ClusterSingletonService, BasicRpcTestService {
+public final class BasicRpcTestProvider implements ClusterSingletonService {
     private static final Logger LOG = LoggerFactory.getLogger(BasicRpcTestProvider.class);
     private static final ServiceGroupIdentifier IDENTIFIER = ServiceGroupIdentifier.create("Basic-rpc-test");
 
     private final RpcProviderService rpcProviderRegistry;
     private final Registration singletonRegistration;
 
-    private ObjectRegistration<?> rpcRegistration = null;
+    private Registration rpcRegistration = null;
 
     @Inject
     @Activate
@@ -59,7 +56,11 @@ public final class BasicRpcTestProvider implements ClusterSingletonService, Basi
     @Override
     public void instantiateServiceInstance() {
         LOG.info("Basic testing rpc registered as global");
-        rpcRegistration = rpcProviderRegistry.registerRpcImplementation(BasicRpcTestService.class, this);
+        rpcRegistration = rpcProviderRegistry.registerRpcImplementations(
+            ImmutableClassToInstanceMap.of(BasicGlobal.class, input -> {
+                LOG.info("Basic test global rpc invoked");
+                return RpcResultBuilder.success(new BasicGlobalOutputBuilder().build()).buildFuture();
+            }));
     }
 
     @Override
@@ -73,12 +74,5 @@ public final class BasicRpcTestProvider implements ClusterSingletonService, Basi
     @Override
     public ServiceGroupIdentifier getIdentifier() {
         return IDENTIFIER;
-    }
-
-    @Override
-    public ListenableFuture<RpcResult<BasicGlobalOutput>> basicGlobal(final BasicGlobalInput input) {
-        LOG.info("Basic test global rpc invoked");
-
-        return Futures.immediateFuture(RpcResultBuilder.success(new BasicGlobalOutputBuilder().build()).build());
     }
 }
--- opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/BuyCarImpl.java
+++ opendaylight/md-sal/samples/clustering-test-app/provider/src/main/java/org/opendaylight/controller/clustering/it/provider/BuyCarImpl.java
@@ -12,43 +12,66 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
+import java.util.Set;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.BuyCar;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.BuyCarInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.BuyCarOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.BuyCarOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.CarBoughtBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.sal.clustering.it.car.purchase.rev140818.CarPurchaseService;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Component(service = CarPurchaseService.class)
-public final class PurchaseCarProvider implements CarPurchaseService {
-    private static final Logger LOG = LoggerFactory.getLogger(PurchaseCarProvider.class);
+@Component(service = { })
+public final class BuyCarImpl implements BuyCar, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(BuyCarImpl.class);
 
     private final NotificationPublishService notificationProvider;
+    private final RpcProviderService rpcProviderService;
+    private final Registration reg;
 
     @Inject
     @Activate
-    public PurchaseCarProvider(@Reference final NotificationPublishService notificationProvider) {
+    public BuyCarImpl(@Reference final NotificationPublishService notificationProvider,
+            @Reference final RpcProviderService rpcProviderService) {
         this.notificationProvider = requireNonNull(notificationProvider);
+        this.rpcProviderService = requireNonNull(rpcProviderService);
+        // Add global registration
+        reg = rpcProviderService.registerRpcImplementation(this);
+    }
+
+    public Registration registerTo(final Set<InstanceIdentifier<?>> paths) {
+        return rpcProviderService.registerRpcImplementation(this, paths);
     }
 
     @Override
-    public ListenableFuture<RpcResult<BuyCarOutput>> buyCar(final BuyCarInput input) {
-        LOG.info("Routed RPC buyCar : generating notification for buying car [{}]", input);
+    @PreDestroy
+    @Deactivate
+    public void close() {
+        reg.close();
+    }
 
-        return Futures.transform(notificationProvider.offerNotification(new CarBoughtBuilder()
+    @Override
+    public ListenableFuture<RpcResult<BuyCarOutput>> invoke(final BuyCarInput input) {
+        LOG.info("Routed RPC buyCar : generating notification for buying car [{}]", input);
+        final var carBought = new CarBoughtBuilder()
             .setCarId(input.getCarId())
             .setPersonId(input.getPersonId())
-            .build()),
+            .build();
+        return Futures.transform(notificationProvider.offerNotification(carBought),
             result -> RpcResultBuilder.success(new BuyCarOutputBuilder().build()).build(),
             MoreExecutors.directExecutor());
     }
