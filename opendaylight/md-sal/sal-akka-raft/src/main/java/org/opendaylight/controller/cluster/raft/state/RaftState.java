1506aca833c31a99412ba6098e208cd3ecc96461
Robert Varga <robert.varga@pantheon.tech>
2023-12-31 03:34:13 +0100

    WIP: add RaftState and related constructs
    Change-Id: Iad0a7487b24e1153933562564430822af283b4f7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/state/RaftState.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.state;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.controller.cluster.raft.policy.RaftPolicy;
+
+/**
+ * Local view of current RAFT log state and any local policy information about it.
+ */
+@NonNullByDefault
+public sealed interface RaftState {
+    /**
+     * Return attached {@link RaftPolicy}.
+     *
+     * @return attached {@link RaftPolicy}
+     */
+    RaftPolicy policy();
+
+    /**
+     * State which can be mutated.
+     */
+    sealed interface Mutable extends RaftState permits FollowerRaftState, LeaderRaftState {
+
+    }
+
+    non-sealed interface Immutable extends RaftState {
+
+    }
+}
