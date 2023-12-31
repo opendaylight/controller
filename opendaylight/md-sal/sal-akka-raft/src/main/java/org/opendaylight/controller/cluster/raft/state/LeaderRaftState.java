c73b6effadb9cb6164d646d588a55cf36fdd8524
Robert Varga <robert.varga@pantheon.tech>
2023-12-31 03:34:13 +0100

    WIP: add RaftState and related constructs
    Change-Id: Iad0a7487b24e1153933562564430822af283b4f7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/state/LeaderRaftState.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.state;
+
+import java.util.function.Consumer;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.controller.cluster.raft.ReplicatedLogEntry;
+
+/**
+ * Leader view of {@link RaftState}.
+ */
+public non-sealed interface LeaderRaftState extends RaftState.Mutable {
+    /**
+     * Appends an entry to the in-memory log and persists it as well.
+     *
+     * @param replicatedLogEntry the entry to append
+     * @param callback the callback to be notified when persistence is complete (optional).
+     * @param doAsync if true, the persistent actor can receive subsequent messages to process in between the persist
+     *        call and the execution of the associated callback. If false, subsequent messages are stashed and get
+     *        delivered after persistence is complete and the associated callback is executed. In either case the
+     *        callback is guaranteed to execute in the context of the actor associated with this log.
+     * @return true if the entry was successfully appended, false otherwise.
+     */
+    boolean appendAndPersist(@NonNull ReplicatedLogEntry replicatedLogEntry,
+            @Nullable Consumer<ReplicatedLogEntry> callback, boolean doAsync);
+}
