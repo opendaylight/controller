bb30dbd8adb4bd32b63672c7ad26f3ede9735bbc
Robert Varga <robert.varga@pantheon.tech>
2024-01-19 12:19:48 +0100

    Provide a migration path from akka persistence
    Provided migration path to transfer client identifier
    state data from akka-persistence snapshot to local
    properties file in a dedicated folder.
    
    JIRA: CONTROLLER-2116
    Change-Id: I05732d5640959c9a03c42b36e7c679782c91562a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ActorBehaviorTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ActorBehaviorTest.java
@@ -8,6 +8,8 @@
 package org.opendaylight.controller.cluster.access.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.timeout;
@@ -22,6 +24,7 @@ import akka.persistence.SnapshotMetadata;
 import akka.testkit.TestProbe;
 import akka.testkit.javadsl.TestKit;
 import com.typesafe.config.ConfigFactory;
+import java.io.File;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
@@ -39,8 +42,16 @@ import scala.concurrent.duration.FiniteDuration;
 
 @ExtendWith(MockitoExtension.class)
 class ActorBehaviorTest {
-    private static final String MEMBER_1_FRONTEND_TYPE_1 = "member-1-frontend-type-1";
+    private static FrontendIdentifier FRONTEND_ID = FrontendIdentifier.create(
+        MemberName.forName("member-1"), FrontendType.forName("type-1"));
+    private static FrontendIdentifier OTHER_FRONTEND_ID = FrontendIdentifier.create(
+        MemberName.forName("member-2"), FrontendType.forName("type-2"));
+    private static final String PERSISTENCE_ID = FRONTEND_ID.toPersistentId();
     private static final FiniteDuration TIMEOUT = FiniteDuration.create(5, TimeUnit.SECONDS);
+    private static final long GEN_0 = 0;
+    private static final long GEN_1 = 1;
+    private static final long GEN_10 = 10;
+    private static final long GEN_11 = 11;
 
     @Mock
     private InternalCommand<BackendInfo> cmd;
@@ -51,9 +62,8 @@ class ActorBehaviorTest {
 
     private ActorSystem system;
     private TestProbe probe;
-    private MockedSnapshotStore.SaveRequest saveRequest;
-    private FrontendIdentifier id;
-    private ActorRef mockedActor;
+    private ActorRef actor;
+    private File stateFile;
 
     @BeforeEach
     void beforeEach() throws Exception {
@@ -64,18 +74,23 @@ class ActorBehaviorTest {
         context.set(initialBehavior, ctx);
         final var persistenceId = AbstractClientActorContext.class.getDeclaredField("persistenceId");
         persistenceId.setAccessible(true);
-        persistenceId.set(ctx, MEMBER_1_FRONTEND_TYPE_1);
+        persistenceId.set(ctx, PERSISTENCE_ID);
 
+        // delete state file if exists
+        stateFile = ClientStateUtils.stateFile(FRONTEND_ID);
+        stateFile.delete();
+
+        // init actor system with persistence
         system = ActorSystem.apply("system1");
-        final ActorRef storeRef = system.registerExtension(Persistence.lookup()).snapshotStoreFor(null,
-            ConfigFactory.empty());
+        final var storeRef = system.registerExtension(Persistence.lookup())
+            .snapshotStoreFor(null, ConfigFactory.empty());
         probe = new TestProbe(system);
         storeRef.tell(probe.ref(), ActorRef.noSender());
-        final MemberName name = MemberName.forName("member-1");
-        id = FrontendIdentifier.create(name, FrontendType.forName("type-1"));
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
-        //handle initial actor recovery
-        saveRequest = handleRecovery(null);
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
+
+        // recovery (migration) flow with no snapshot
+        assertRecoveryFlow(null);
+        assertCurrentGeneration(GEN_0);
     }
 
     @AfterEach
@@ -84,102 +99,130 @@ class ActorBehaviorTest {
     }
 
     @Test
-    void testInitialBehavior() {
+    void initialBehavior() {
         doReturn(initialBehavior).when(cmd).execute(any());
-        mockedActor.tell(cmd, ActorRef.noSender());
+        actor.tell(cmd, ActorRef.noSender());
         verify(cmd, timeout(1000)).execute(initialBehavior);
     }
 
     @Test
-    void testCommandStashing() {
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
+    void commandStashing() {
+        system.stop(actor);
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
         doReturn(initialBehavior).when(cmd).execute(any());
-        //send messages before recovery is completed
-        mockedActor.tell(cmd, ActorRef.noSender());
-        mockedActor.tell(cmd, ActorRef.noSender());
-        mockedActor.tell(cmd, ActorRef.noSender());
-        //complete recovery
-        handleRecovery(null);
+        // send messages before recovery is completed
+        actor.tell(cmd, ActorRef.noSender());
+        actor.tell(cmd, ActorRef.noSender());
+        actor.tell(cmd, ActorRef.noSender());
+        // complete recovery
+        assertRecoveryFlow(null);
+        // verify all pending messages are executed
         verify(cmd, timeout(1000).times(3)).execute(initialBehavior);
     }
 
     @Test
-    void testRecoveryAfterRestart() {
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
-        final MockedSnapshotStore.SaveRequest newSaveRequest =
-                handleRecovery(new SelectedSnapshot(saveRequest.getMetadata(), saveRequest.getSnapshot()));
-        assertEquals(MEMBER_1_FRONTEND_TYPE_1, newSaveRequest.getMetadata().persistenceId());
+    void snapshotMigrate() {
+        system.stop(actor);
+        stateFile.delete();
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
+        // takes snapshot generation 10, migrates and uses generation 11
+        assertRecoveryFlow(snapshot(ClientIdentifier.create(FRONTEND_ID, GEN_10)));
+        assertCurrentGeneration(GEN_11);
     }
 
     @Test
-    void testRecoveryAfterRestartFrontendIdMismatch() {
-        system.stop(mockedActor);
-        //start actor again
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
-        probe.expectMsgClass(MockedSnapshotStore.LoadRequest.class);
-        //offer snapshot with incorrect client id
-        final SnapshotMetadata metadata = saveRequest.getMetadata();
-        final FrontendIdentifier anotherFrontend = FrontendIdentifier.create(MemberName.forName("another"),
-                FrontendType.forName("type-2"));
-        final ClientIdentifier incorrectClientId = ClientIdentifier.create(anotherFrontend, 0);
-        probe.watch(mockedActor);
-        probe.reply(Optional.of(new SelectedSnapshot(metadata, incorrectClientId)));
-        //actor should be stopped
-        probe.expectTerminated(mockedActor, TIMEOUT);
-    }
-
-    @Test
-    void testRecoveryAfterRestartSaveSnapshotFail() {
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
-        probe.watch(mockedActor);
-        probe.expectMsgClass(MockedSnapshotStore.LoadRequest.class);
-        probe.reply(Optional.empty());
-        probe.expectMsgClass(MockedSnapshotStore.SaveRequest.class);
-        probe.reply(new RuntimeException("save failed"));
-        probe.expectMsgClass(MockedSnapshotStore.DeleteByMetadataRequest.class);
-        probe.expectTerminated(mockedActor, TIMEOUT);
-    }
-
-    @Test
-    void testRecoveryAfterRestartDeleteSnapshotsFail() {
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(id, initialBehavior));
-        probe.watch(mockedActor);
-        probe.expectMsgClass(MockedSnapshotStore.LoadRequest.class);
-        probe.reply(Optional.empty());
-        probe.expectMsgClass(MockedSnapshotStore.SaveRequest.class);
-        probe.reply(Void.TYPE);
-        probe.expectMsgClass(MockedSnapshotStore.DeleteByCriteriaRequest.class);
-        probe.reply(new RuntimeException("delete failed"));
-        //actor shouldn't terminate
+    void recoverFromTombstone() {
+        system.stop(actor);
+        stateFile.delete();
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
+        probe.watch(actor);
+        // takes snapshot with generation 10, adopts to generation 11, no snapshot re-save
+        final var snapshot = snapshot(new PersistenceTombstone(ClientIdentifier.create(FRONTEND_ID, GEN_10)));
+        assertRecoveryFlow(snapshot);
+        // ensure the recovery flow is complete
         probe.expectNoMessage();
+        assertCurrentGeneration(GEN_11);
     }
 
-    private MockedSnapshotStore.SaveRequest handleRecovery(final SelectedSnapshot savedState) {
+    @Test
+    void frontendIdMismatch() {
+        system.stop(actor);
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
+        probe.watch(actor);
+        // snapshot with frontend id mismatch
+        final var snapshot = snapshot(ClientIdentifier.create(OTHER_FRONTEND_ID, GEN_10));
+        assertRecoveryFlow(snapshot);
+        //actor should be stopped
+        probe.expectTerminated(actor, TIMEOUT);
+        // snapshot data ignored because frontendId mismatches
+        // initial generation = 0, after restart generation = 1
+        assertCurrentGeneration(GEN_1);
+    }
+
+    @Test
+    void tombstoneFrontendIdMismatch() {
+        system.stop(actor);
+        actor = system.actorOf(TestActor.props(FRONTEND_ID, initialBehavior));
+        probe.watch(actor);
+        // snapshot with frontend id mismatch
+        final var snapshot = snapshot(new PersistenceTombstone(ClientIdentifier.create(OTHER_FRONTEND_ID, GEN_10)));
+        // snapshot data ignored because frontendId mismatches
+        assertRecoveryFlow(snapshot);
+        //actor should be stopped
+        probe.expectTerminated(actor, TIMEOUT);
+        // initial generation = 0, after restart generation = 1
+        assertCurrentGeneration(GEN_1);
+    }
+
+    private void assertRecoveryFlow(final SelectedSnapshot snapshot) {
+        // actor explicitly requests for clientId snapshot
         probe.expectMsgClass(MockedSnapshotStore.LoadRequest.class);
-        //offer snapshot
-        probe.reply(Optional.ofNullable(savedState));
-        final MockedSnapshotStore.SaveRequest nextSaveRequest =
-                probe.expectMsgClass(MockedSnapshotStore.SaveRequest.class);
-        probe.reply(Void.TYPE);
-        //check old snapshots deleted
-        probe.expectMsgClass(MockedSnapshotStore.DeleteByCriteriaRequest.class);
-        probe.reply(Void.TYPE);
-        return nextSaveRequest;
+        probe.reply(Optional.ofNullable(snapshot));
+
+        if (snapshot == null) {
+            // no snapshot
+            return;
+        }
+        // snapshot exists
+        if (snapshot.snapshot() instanceof ClientIdentifier clientId && FRONTEND_ID.equals(clientId.getFrontendId())) {
+            // current clientId expected to be updated and saved as tombstone
+            final var saveRequest = probe.expectMsgClass(MockedSnapshotStore.SaveRequest.class);
+            assertNotNull(saveRequest);
+            assertNotNull(saveRequest.getMetadata());
+            assertEquals(PERSISTENCE_ID, saveRequest.getMetadata().persistenceId());
+
+            // tombstone expected to use incremented (after migration) generation value
+            final var tombstone = assertInstanceOf(PersistenceTombstone.class, saveRequest.getSnapshot());
+            assertEquals(FRONTEND_ID, tombstone.clientId().getFrontendId());
+            assertEquals(clientId.getGeneration() + 1, tombstone.clientId().getGeneration());
+            probe.reply(Void.TYPE);
+
+            // original snapshot expected to be deleted
+            probe.expectMsgClass(MockedSnapshotStore.DeleteByCriteriaRequest.class);
+            probe.reply(Void.TYPE);
+        }
     }
 
-    private static class MockedActor extends AbstractClientActor {
+    void assertCurrentGeneration(final long expectedGeneration) {
+        final var clientId = ClientStateUtils.loadClientIdentifier(stateFile, FRONTEND_ID);
+        assertNotNull(clientId);
+        assertEquals(FRONTEND_ID, clientId.getFrontendId());
+        assertEquals(expectedGeneration, clientId.getGeneration());
+    }
+
+    private static SelectedSnapshot snapshot(final Object snapshot) {
+        return new SelectedSnapshot(SnapshotMetadata.apply(PERSISTENCE_ID, 1), snapshot);
+    }
+
+    private static class TestActor extends AbstractClientActor {
         private final ClientActorBehavior<?> initialBehavior;
         private final ClientActorConfig mockConfig = AccessClientUtil.newMockClientActorConfig();
 
         private static Props props(final FrontendIdentifier frontendId, final ClientActorBehavior<?> initialBehavior) {
-            return Props.create(MockedActor.class, () -> new MockedActor(frontendId, initialBehavior));
+            return Props.create(TestActor.class, () -> new TestActor(frontendId, initialBehavior));
         }
 
-        MockedActor(final FrontendIdentifier frontendId, final ClientActorBehavior<?> initialBehavior) {
+        TestActor(final FrontendIdentifier frontendId, final ClientActorBehavior<?> initialBehavior) {
             super(frontendId);
             this.initialBehavior = initialBehavior;
         }
