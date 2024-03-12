b22f6f02b36c7de497848802e41387fe24e11b16
Robert Varga <robert.varga@pantheon.tech>
2024-03-12 10:25:16 +0100

    Remove StorageLevel.MEMORY
    This enum is defunct and results in DISK being used. Remove it.
    
    JIRA: CONTROLLER-2101
    Change-Id: Ibc6f6916f20e5b0fc0f3c8fe6651e65f24074d53
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- atomix-storage/src/main/java/io/atomix/storage/journal/StorageLevel.java
+++ atomix-storage/src/main/java/io/atomix/storage/journal/StorageLevel.java
@@ -19,21 +19,12 @@ package io.atomix.storage.journal;
  * Storage level configuration values which control how logs are stored on disk or in memory.
  */
 public enum StorageLevel {
-
-  /**
-   * Stores data in memory only.
-   */
-  @Deprecated
-  MEMORY,
-
-  /**
-   * Stores data in a memory-mapped file.
-   */
-  MAPPED,
-
-  /**
-   * Stores data on disk.
-   */
-  DISK
-
+    /**
+     * Stores data in a memory-mapped file.
+     */
+    MAPPED,
+    /**
+     * Stores data on disk.
+     */
+    DISK
 }
