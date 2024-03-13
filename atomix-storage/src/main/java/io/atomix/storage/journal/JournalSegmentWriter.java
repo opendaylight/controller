18f733ad4cbfe4cf500f5d1cc06ca58c5004f130
Robert Varga <robert.varga@pantheon.tech>
2024-03-13 20:19:30 +0100

    WIP: Refactor segment file access
    Introduce FileAccess to act as the an intermediate holder.
    
    // FIXME: finish up lifecycle
    
    JIRA: CONTROLLER-2099
    Change-Id: Ia9eabeeb06658a905364ac2ee554c7ad94ee91b1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- atomix-storage/src/main/java/io/atomix/storage/journal/JournalSegmentWriter.java
+++ atomix-storage/src/main/java/io/atomix/storage/journal/JournalSegmentWriter.java
@@ -18,22 +18,17 @@ package io.atomix.storage.journal;
 import static java.util.Objects.requireNonNull;
 
 import io.atomix.storage.journal.index.JournalIndex;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 
 abstract sealed class JournalSegmentWriter<E> permits FileChannelJournalSegmentWriter, MappedJournalSegmentWriter {
-    final @NonNull FileChannel channel;
     final @NonNull JournalIndex index;
     final @NonNull JournalSerdes namespace;
     final int maxSegmentSize;
     final int maxEntrySize;
     final long firstIndex;
 
-    JournalSegmentWriter(final FileChannel channel, final JournalSegment<E> segment, final int maxEntrySize,
-            final JournalIndex index, final JournalSerdes namespace) {
-        this.channel = requireNonNull(channel);
+    JournalSegmentWriter(final JournalSegment<E> segment, final int maxEntrySize, final JournalIndex index,
+            final JournalSerdes namespace) {
         this.index = requireNonNull(index);
         this.namespace = requireNonNull(namespace);
         this.maxSegmentSize = segment.descriptor().maxSegmentSize();
@@ -42,7 +37,6 @@ abstract sealed class JournalSegmentWriter<E> permits FileChannelJournalSegmentW
     }
 
     JournalSegmentWriter(JournalSegmentWriter<E> previous) {
-        this.channel = previous.channel;
         this.index = previous.index;
         this.namespace = previous.namespace;
         this.maxSegmentSize = previous.maxSegmentSize;
@@ -102,16 +96,4 @@ abstract sealed class JournalSegmentWriter<E> permits FileChannelJournalSegmentW
      * Closes this writer.
      */
     abstract void close();
-
-    /**
-     * Returns the mapped buffer underlying the segment writer, or {@code null} if the writer does not have such a
-     * buffer.
-     *
-     * @return the mapped buffer underlying the segment writer, or {@code null}.
-     */
-    abstract @Nullable MappedByteBuffer buffer();
-
-    abstract @NonNull MappedJournalSegmentWriter<E> toMapped();
-
-    abstract @NonNull FileChannelJournalSegmentWriter<E> toFileChannel();
 }
