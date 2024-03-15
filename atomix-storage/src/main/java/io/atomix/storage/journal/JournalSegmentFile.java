3b32f2d51a4867184c66c1e119dfe5c4f2d18295
Robert Varga <robert.varga@pantheon.tech>
2024-03-15 13:10:18 +0100

    Normalized copyright header
    Use a closed range and our standard 'and others' moniker.
    
    JIRA: CONTROLLER-2105
    Change-Id: I868b02415666da378f395ff711d5092a334d0196
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 046b4f0ce6d29eb651c12e85bd9907732b5fff06)

--- atomix-storage/src/main/java/io/atomix/storage/journal/JournalSegmentFile.java
+++ atomix-storage/src/main/java/io/atomix/storage/journal/JournalSegmentFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-present Open Networking Foundation
+ * Copyright 2015-2022 Open Networking Foundation and others.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
