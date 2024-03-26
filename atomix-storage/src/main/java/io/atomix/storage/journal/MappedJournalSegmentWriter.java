fd9dfdc05c75e078b2d4c65d48dd48fdf10b8fe4
Robert Varga <robert.varga@pantheon.tech>
2024-03-27 00:04:35 +0100

    Centralize JournalSegmentWriter.append()
    We have almost-identical implementations of append(), differing only
    slightly in how the do buffer management.
    
    Expose a ByteBuffer-based API to writing to segment file, in terms of
    abstract methods and pull the implementation down into the superclass.
    
    JIRA: CONTROLLER-2100
    Change-Id: I8d5629019d1c5d2978b0861e30e80346fbfc3c31
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- atomix-storage/src/main/java/io/atomix/storage/journal/MappedJournalSegmentWriter.java
+++ atomix-storage/src/main/java/io/atomix/storage/journal/MappedJournalSegmentWriter.java
@@ -16,16 +16,11 @@
  */
 package io.atomix.storage.journal;
 
-import static io.atomix.storage.journal.SegmentEntry.HEADER_BYTES;
-
-import com.esotericsoftware.kryo.KryoException;
 import io.atomix.storage.journal.index.JournalIndex;
 import java.io.IOException;
-import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.zip.CRC32;
 import org.eclipse.jdt.annotation.NonNull;
 
 /**
@@ -44,121 +39,87 @@ import org.eclipse.jdt.annotation.NonNull;
  * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 final class MappedJournalSegmentWriter<E> extends JournalSegmentWriter<E> {
-  private final @NonNull MappedByteBuffer mappedBuffer;
-  private final JournalSegmentReader<E> reader;
-  private final ByteBuffer buffer;
+    private final @NonNull MappedByteBuffer mappedBuffer;
+    private final JournalSegmentReader<E> reader;
+    private final ByteBuffer buffer;
 
-  MappedJournalSegmentWriter(final FileChannel channel, final JournalSegment<E> segment, final int maxEntrySize,
-      final JournalIndex index, final JournalSerdes namespace) {
-    super(channel, segment, maxEntrySize, index, namespace);
+    MappedJournalSegmentWriter(final FileChannel channel, final JournalSegment<E> segment, final int maxEntrySize,
+        final JournalIndex index, final JournalSerdes namespace) {
+        super(channel, segment, maxEntrySize, index, namespace);
 
-    mappedBuffer = mapBuffer(channel, maxSegmentSize);
-    buffer = mappedBuffer.slice();
-    reader = new JournalSegmentReader<>(segment, new MappedFileReader(segment.file().file().toPath(), mappedBuffer),
-        maxEntrySize, namespace);
-    reset(0);
-  }
-
-  MappedJournalSegmentWriter(final JournalSegmentWriter<E> previous) {
-    super(previous);
-
-    mappedBuffer = mapBuffer(channel, maxSegmentSize);
-    buffer = mappedBuffer.slice();
-    reader = new JournalSegmentReader<>(segment, new MappedFileReader(segment.file().file().toPath(), mappedBuffer),
-        maxEntrySize, namespace);
-  }
-
-  private static @NonNull MappedByteBuffer mapBuffer(final FileChannel channel, final int maxSegmentSize) {
-    try {
-      return channel.map(FileChannel.MapMode.READ_WRITE, 0, maxSegmentSize);
-    } catch (IOException e) {
-      throw new StorageException(e);
-    }
-  }
-
-  @Override
-  @NonNull MappedByteBuffer buffer() {
-    return mappedBuffer;
-  }
-
-  @Override
-  MappedJournalSegmentWriter<E> toMapped() {
-    return this;
-  }
-
-  @Override
-  DiskJournalSegmentWriter<E> toFileChannel() {
-    close();
-    return new DiskJournalSegmentWriter<>(this);
-  }
-
-  @Override
-  JournalSegmentReader<E> reader() {
-    return reader;
-  }
-
-  @Override
-  @SuppressWarnings("unchecked")
-  <T extends E> Indexed<T> append(final T entry) {
-    // Store the entry index.
-    final long index = getNextIndex();
-
-    // Serialize the entry.
-    final int bodyPosition = currentPosition + HEADER_BYTES;
-    final int avail = maxSegmentSize - bodyPosition;
-    if (avail < 0) {
-      throw new BufferOverflowException();
+        mappedBuffer = mapBuffer(channel, maxSegmentSize);
+        buffer = mappedBuffer.slice();
+        reader = new JournalSegmentReader<>(segment, new MappedFileReader(segment.file().file().toPath(), mappedBuffer),
+            maxEntrySize, namespace);
+        reset(0);
     }
 
-    final var entryBytes = buffer.slice(bodyPosition, Math.min(avail, maxEntrySize));
-    try {
-      namespace.serialize(entry, entryBytes);
-    } catch (KryoException e) {
-      if (entryBytes.capacity() != maxEntrySize) {
-        // We have not provided enough capacity, signal to roll to next segment
-        throw new BufferOverflowException();
-      }
+    MappedJournalSegmentWriter(final JournalSegmentWriter<E> previous) {
+        super(previous);
 
-      // Just reset the buffer. There's no need to zero the bytes since we haven't written the length or checksum.
-      throw new StorageException.TooLarge("Entry size exceeds maximum allowed bytes (" + maxEntrySize + ")");
+        mappedBuffer = mapBuffer(channel, maxSegmentSize);
+        buffer = mappedBuffer.slice();
+        reader = new JournalSegmentReader<>(segment, new MappedFileReader(segment.file().file().toPath(), mappedBuffer),
+            maxEntrySize, namespace);
     }
 
-    final int length = entryBytes.position();
-
-    // Compute the checksum for the entry.
-    final var crc32 = new CRC32();
-    crc32.update(entryBytes.flip());
-
-    // Create a single byte[] in memory for the entire entry and write it as a batch to the underlying buffer.
-    buffer.putInt(currentPosition, length).putInt(currentPosition + Integer.BYTES, (int) crc32.getValue());
-
-    // Update the last entry with the correct index/term/length.
-    Indexed<E> indexedEntry = new Indexed<>(index, entry, length);
-    lastEntry = indexedEntry;
-    this.index.index(index, currentPosition);
-
-    currentPosition = currentPosition + HEADER_BYTES + length;
-    return (Indexed<T>) indexedEntry;
-  }
-
-  @Override
-  void writeEmptyHeader(final int position) {
-      // Note: we issue a single putLong() instead of two putInt()s.
-      buffer.putLong(position, 0L);
-  }
-
-  @Override
-  void flush() {
-    mappedBuffer.force();
-  }
-
-  @Override
-  void close() {
-    flush();
-    try {
-      BufferCleaner.freeBuffer(mappedBuffer);
-    } catch (IOException e) {
-      throw new StorageException(e);
+    private static @NonNull MappedByteBuffer mapBuffer(final FileChannel channel, final int maxSegmentSize) {
+        try {
+            return channel.map(FileChannel.MapMode.READ_WRITE, 0, maxSegmentSize);
+        } catch (IOException e) {
+            throw new StorageException(e);
+        }
     }
-  }
+
+    @Override
+    @NonNull MappedByteBuffer buffer() {
+        return mappedBuffer;
+    }
+
+    @Override
+    MappedJournalSegmentWriter<E> toMapped() {
+        return this;
+    }
+
+    @Override
+    DiskJournalSegmentWriter<E> toFileChannel() {
+        close();
+        return new DiskJournalSegmentWriter<>(this);
+    }
+
+    @Override
+    JournalSegmentReader<E> reader() {
+        return reader;
+    }
+
+    @Override
+    ByteBuffer startWrite(final int position, final int size) {
+        return buffer.slice(position, size);
+    }
+
+    @Override
+    void commitWrite(final int position, final ByteBuffer entry) {
+        // No-op, buffer is write-through
+    }
+
+    @Override
+    void writeEmptyHeader(final int position) {
+        // Note: we issue a single putLong() instead of two putInt()s.
+        buffer.putLong(position, 0L);
+    }
+
+    @Override
+    void flush() {
+        mappedBuffer.force();
+    }
+
+    @Override
+    void close() {
+        flush();
+        try {
+            BufferCleaner.freeBuffer(mappedBuffer);
+        } catch (IOException e) {
+            throw new StorageException(e);
+        }
+    }
 }
