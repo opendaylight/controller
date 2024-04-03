9ad8f6fb8bf68fad9ef29179c447cd82dae2a14b
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-04-03 18:17:05 +0300

    WIP separate byte-level atomic-storage access
    TBD
    
    JIRA: CONTROLLER-2115
    Change-Id: I2e4941bda3af76f0cd59e8c545131af85c668010
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ atomix-storage/src/main/java/io/atomix/storage/journal/SegmentedByteJournalWriter.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2017-2022 Open Networking Foundation and others.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.storage.journal;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+/**
+ * A {@link ByteJournalWriter} implementation.
+ */
+final class SegmentedByteJournalWriter implements ByteJournalWriter {
+  private final SegmentedByteJournal journal;
+  private JournalSegment currentSegment;
+  private JournalSegmentWriter currentWriter;
+
+  SegmentedByteJournalWriter(SegmentedByteJournal journal) {
+    this.journal = journal;
+    this.currentSegment = journal.lastSegment();
+    this.currentWriter = currentSegment.acquireWriter();
+  }
+
+  @Override
+  public long getLastIndex() {
+    return currentWriter.getLastIndex();
+  }
+
+  @Override
+  public Indexed<E> getLastEntry() {
+    final var lastBytes = currentWriter.getLastBytes();
+    if (lastBytes == null) {
+      return null;
+    }
+    final E deserialized = journal.serializer().deserialize(lastBytes);
+    return new Indexed<>(currentWriter.getLastIndex(), deserialized, lastBytes.length) ;
+  }
+
+  @Override
+  public long getNextIndex() {
+    return currentWriter.getNextIndex();
+  }
+
+  @Override
+  public void reset(long index) {
+    if (index > currentSegment.firstIndex()) {
+      currentSegment.releaseWriter();
+      currentSegment = journal.resetSegments(index);
+      currentWriter = currentSegment.acquireWriter();
+    } else {
+      truncate(index - 1);
+    }
+    journal.resetHead(index);
+  }
+
+  @Override
+  public void commit(long index) {
+    if (index > journal.getCommitIndex()) {
+      journal.setCommitIndex(index);
+      if (journal.isFlushOnCommit()) {
+        flush();
+      }
+    }
+  }
+
+  @Override
+  public <T extends E> Indexed<T> append(T entry) {
+    final var bytes = journal.serializer().serialize(entry);
+    var index = currentWriter.append(bytes);
+    if (index != null) {
+      return new Indexed<>(index, entry, bytes.length);
+    }
+
+    //  Slow path: we do not have enough capacity
+    currentWriter.flush();
+    currentSegment.releaseWriter();
+    currentSegment = journal.nextSegment();
+    currentWriter = currentSegment.acquireWriter();
+    final var newIndex = verifyNotNull(currentWriter.append(bytes));
+    return new Indexed<>(newIndex, entry, bytes.length);
+  }
+
+  @Override
+  public void truncate(long index) {
+    if (index < journal.getCommitIndex()) {
+      throw new IndexOutOfBoundsException("Cannot truncate committed index: " + index);
+    }
+
+    // Delete all segments with first indexes greater than the given index.
+    while (index < currentSegment.firstIndex() && currentSegment != journal.firstSegment()) {
+      currentSegment.releaseWriter();
+      journal.removeSegment(currentSegment);
+      currentSegment = journal.lastSegment();
+      currentWriter = currentSegment.acquireWriter();
+    }
+
+    // Truncate the current index.
+    currentWriter.truncate(index);
+
+    // Reset segment readers.
+    journal.resetTail(index + 1);
+  }
+
+  @Override
+  public void flush() {
+    currentWriter.flush();
+  }
+}
