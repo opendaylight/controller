35f3e8eb74f1825a6fe32b15903b73bdba65fd0a
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2024-04-22 17:24:04 +0300

    Separate byte-level atomic-storage access
    Byte level functionality was moved into *ByteJournal*
    artifacts and now can be accessed independently.
    SegmentedJournal is now acts as a type serialization
    layer on top of ByteJournal.
    
    // FIXME: more audit to removed/added code
    // FIXME: refactor SegmentedJournal.Builder (in a subsequent patch?)
    
    JIRA: CONTROLLER-2115
    Change-Id: I2e4941bda3af76f0cd59e8c545131af85c668010
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ atomix-storage/src/main/java/io/atomix/storage/journal/ByteBufJournal.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.storage.journal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * A journal of byte arrays. Provides the ability to write modify entries via {@link ByteBufWriter} and read them
+ * back via {@link ByteBufReader}.
+ */
+@NonNullByDefault
+public interface ByteBufJournal extends AutoCloseable {
+    /**
+     * Returns the journal writer.
+     *
+     * @return The journal writer.
+     */
+    ByteBufWriter writer();
+
+    /**
+     * Opens a new {@link ByteBufReader} reading all entries.
+     *
+     * @param index The index at which to start the reader.
+     * @return A new journal reader.
+     */
+    ByteBufReader openReader(long index);
+
+    /**
+     * Opens a new {@link ByteBufReader} reading only committed entries.
+     *
+     * @param index The index at which to start the reader.
+     * @return A new journal reader.
+     */
+    ByteBufReader openCommitsReader(long index);
+
+    @Override
+    void close();
+}
