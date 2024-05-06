372a18dcb251613ee58253350523cf0d2bbf15cd
Robert Varga <robert.varga@pantheon.tech>
2024-05-06 05:23:11 +0200

    Separate out FileAccess
    Rather than keeping mapping in MappedFileWriter, use MappedFileAccess
    to keep its lifecycle.
    
    The second part is keeping the information about last written entry,
    which we encapsulate in SegmentState.
    
    JIRA: CONTROLLER-2099
    Change-Id: I7fba8da21a021a477c965d96045c1b0d4bf8cc29
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ atomix-storage/src/main/java/io/atomix/storage/journal/SegmentState.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package io.atomix.storage.journal;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Encapsulation of a {@link JournalSegment}'s state;
+ */
+sealed interface SegmentState {
+
+    Long lastIndex();
+
+    @NonNullByDefault
+    record Active(FileAccess access, JournalSegmentWriter writer) implements SegmentState {
+        public Active {
+            requireNonNull(access);
+            requireNonNull(writer);
+        }
+
+        @Override
+        public Long lastIndex() {
+            return writer.getLastIndex();
+        }
+
+        Inactive close() {
+            final var ret = writer.toInactive();
+            access.close();
+            return ret;
+        }
+    }
+
+    record Inactive(int position, Long lastIndex) implements SegmentState {
+        // Nothing else
+    }
+}
