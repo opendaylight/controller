cb9c317303cb36c47e63c10aa7b182ea3055fbc1
Robert Varga <robert.varga@pantheon.tech>
2024-09-17 10:22:57 +0200

    Use an enhanced switch in onReceiveCommand()
    We have an if/else cascade, use a modern switch to achieve the same
    thing, improving clarity.
    
    Change-Id: I4a01c0f1b521f8013ee4e47b34ffa6e8c810abc5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/SavingClientActorBehavior.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/SavingClientActorBehavior.java
@@ -20,8 +20,6 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Transient behavior handling messages while the new generation is being persisted.
- *
- * @author Robert Varga
  */
 final class SavingClientActorBehavior extends RecoveredClientActorBehavior<InitialClientActorContext> {
     private static final Logger LOG = LoggerFactory.getLogger(SavingClientActorBehavior.class);
@@ -34,23 +32,29 @@ final class SavingClientActorBehavior extends RecoveredClientActorBehavior<Initi
 
     @Override
     AbstractClientActorBehavior<?> onReceiveCommand(final Object command) {
-        if (command instanceof SaveSnapshotFailure saveFailure) {
-            LOG.error("{}: failed to persist state", persistenceId(), saveFailure.cause());
-            return null;
-        } else if (command instanceof SaveSnapshotSuccess saved) {
-            LOG.debug("{}: got command: {}", persistenceId(), saved);
-            context().deleteSnapshots(new SnapshotSelectionCriteria(scala.Long.MaxValue(),
-                    saved.metadata().timestamp() - 1, 0L, 0L));
-            return this;
-        } else if (command instanceof DeleteSnapshotsSuccess deleteSuccess) {
-            LOG.debug("{}: got command: {}", persistenceId(), deleteSuccess);
-        } else if (command instanceof DeleteSnapshotsFailure deleteFailure) {
-            // Not treating this as a fatal error.
-            LOG.warn("{}: failed to delete prior snapshots", persistenceId(), deleteFailure.cause());
-        } else {
-            LOG.debug("{}: stashing command {}", persistenceId(), command);
-            context().stash();
-            return this;
+        switch (command) {
+            case SaveSnapshotFailure saveFailure -> {
+                LOG.error("{}: failed to persist state", persistenceId(), saveFailure.cause());
+                return null;
+            }
+            case SaveSnapshotSuccess saved -> {
+                LOG.debug("{}: got command: {}", persistenceId(), saved);
+                context().deleteSnapshots(new SnapshotSelectionCriteria(scala.Long.MaxValue(),
+                        saved.metadata().timestamp() - 1, 0L, 0L));
+                return this;
+            }
+            case DeleteSnapshotsSuccess deleteSuccess -> {
+                LOG.debug("{}: got command: {}", persistenceId(), deleteSuccess);
+            }
+            case DeleteSnapshotsFailure deleteFailure -> {
+                // Not treating this as a fatal error.
+                LOG.warn("{}: failed to delete prior snapshots", persistenceId(), deleteFailure.cause());
+            }
+            default -> {
+                LOG.debug("{}: stashing command {}", persistenceId(), command);
+                context().stash();
+                return this;
+            }
         }
 
         context().unstash();
