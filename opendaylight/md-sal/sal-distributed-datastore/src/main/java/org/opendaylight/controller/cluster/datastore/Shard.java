230ea958adeb06dbb244176d9a0e21dfdb3a0bbe
Robert Varga <robert.varga@pantheon.tech>
2024-10-28 03:16:32 +0100

    Change RaftActor.getRoleChangeNotifier()
    Rename this method to roleChangeNotifier() and return a @Nullable, which
    makes it easier to interact with.
    
    Change-Id: I707b40b95a94a6fc35ba871ece088d3a9f2ecb15
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/Shard.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/Shard.java
@@ -171,7 +171,7 @@ public class Shard extends RaftActor {
 
     private Cancellable txCommitTimeoutCheckSchedule;
 
-    private final Optional<ActorRef> roleChangeNotifier;
+    private final ActorRef roleChangeNotifier;
 
     private final MessageTracker appendEntriesReplyTracker;
 
@@ -239,7 +239,7 @@ public class Shard extends RaftActor {
         setTransactionCommitTimeout();
 
         // create a notifier actor for each cluster member
-        roleChangeNotifier = createRoleChangeNotifier(name);
+        roleChangeNotifier = getContext().actorOf(RoleChangeNotifier.getProps(name), name + "-notifier");
 
         appendEntriesReplyTracker = new MessageTracker(AppendEntriesReply.class,
                 getRaftActorContext().getConfigParams().getIsolatedCheckIntervalInMillis());
@@ -267,12 +267,6 @@ public class Shard extends RaftActor {
     private void setTransactionCommitTimeout() {
         transactionCommitTimeout = TimeUnit.MILLISECONDS.convert(
                 datastoreContext.getShardTransactionCommitTimeoutInSeconds(), TimeUnit.SECONDS) / 2;
-    }
-
-    private Optional<ActorRef> createRoleChangeNotifier(final String shardId) {
-        ActorRef shardRoleChangeNotifier = getContext().actorOf(
-            RoleChangeNotifier.getProps(shardId), shardId + "-notifier");
-        return Optional.of(shardRoleChangeNotifier);
     }
 
     @Override
@@ -349,7 +343,7 @@ public class Shard extends RaftActor {
             } else if (message instanceof DatastoreContext request) {
                 onDatastoreContext(request);
             } else if (message instanceof RegisterRoleChangeListener) {
-                roleChangeNotifier.orElseThrow().forward(message, context());
+                roleChangeNotifier.forward(message, context());
             } else if (message instanceof FollowerInitialSyncUpStatus request) {
                 shardMBean.setFollowerInitialSyncStatus(request.isInitialSyncDone());
                 context().parent().tell(message, self());
@@ -611,7 +605,7 @@ public class Shard extends RaftActor {
     }
 
     @Override
-    protected final Optional<ActorRef> getRoleChangeNotifier() {
+    protected final ActorRef roleChangeNotifier() {
         return roleChangeNotifier;
     }
 
