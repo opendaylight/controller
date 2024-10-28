b5308a505e94b1a93e7d4925c64a6943cdae768b
Robert Varga <robert.varga@pantheon.tech>
2024-10-28 19:04:18 +0100

    WIP: Factor out RAFT interface
    The idea here is to separate public access (exposed to RaftActor
    subclasses) into a set of interfaces for both sides of the conversation:
    - RaftJournal for the access afforded to leaders
    - StateBehavior for user state managers
    
    // FIXME: issue
    
    Change-Id: I7c9499bae2c8e9e0faedf0850a639e388016aac1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/state/InactiveStuff.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.state;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.controller.cluster.raft.api.RaftActorAccess;
+
+@NonNullByDefault
+public final class InactiveStuff extends Stuff {
+    InactiveStuff(final RaftActorAccess actorAccess, final short payloadVersion) {
+        super(actorAccess, payloadVersion);
+    }
+
+    InactiveStuff(final ActiveStuff prev) {
+        super(prev);
+    }
+}
