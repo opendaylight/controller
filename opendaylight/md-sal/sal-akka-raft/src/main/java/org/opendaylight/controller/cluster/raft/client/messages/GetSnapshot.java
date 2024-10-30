221c9cae2fc7483616d68d13db3957d1a2e0128f
Robert Varga <robert.varga@pantheon.tech>
2024-10-30 15:21:48 +0100

    WIP: Use replyTo in GetSnapshot
    This is a simple message, make sure we require a valid ActorRef and not
    rely on Actor.sender().
    
    Change-Id: I654ecf515d32e3fb3574ffc95dcc417a58058fb9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/client/messages/GetSnapshot.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/client/messages/GetSnapshot.java
@@ -7,8 +7,13 @@
  */
 package org.opendaylight.controller.cluster.raft.client.messages;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Optional;
+import org.apache.pekko.actor.ActorRef;
 import org.apache.pekko.util.Timeout;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * Internal client message to get a snapshot of the current state based on whether or not persistence is enabled.
@@ -16,13 +21,13 @@ import org.apache.pekko.util.Timeout;
  *
  * @author Thomas Pantelis
  */
-public final class GetSnapshot {
-    public static final GetSnapshot INSTANCE = new GetSnapshot(null);
+public record GetSnapshot(@NonNull ActorRef replyTo, @Nullable Timeout timeout) {
+    public GetSnapshot {
+        requireNonNull(replyTo);
+    }
 
-    private final Timeout timeout;
-
-    public GetSnapshot(final Timeout timeout) {
-        this.timeout = timeout;
+    public GetSnapshot(final @NonNull ActorRef replyTo) {
+        this(replyTo, null);
     }
 
     public Optional<Timeout> getTimeout() {
