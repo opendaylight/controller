2c0c34b1a747f3c652e88e2abaa9be27ef1d9a3e
Robert Varga <robert.varga@pantheon.tech>
2024-10-30 11:51:57 +0100

    WIP; Make ShardIdentifier a WritableIdentifier
    ShardIdentifier needs to be a WritableIdentifier so we can use it with
    cluster.access.concepts.Request.
    
    // FIXME: there is another ShardIdentifier in cluster-admin-impl: we
    //        need to decide on what's what
    
    Change-Id: Ic1de8d09c40e83ff602eb4c6e93a7bb889b63305
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/identifiers/ShardIdentifier.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/identifiers/ShardIdentifier.java
@@ -10,11 +10,19 @@ package org.opendaylight.controller.cluster.datastore.identifiers;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.opendaylight.controller.cluster.access.concepts.MemberName;
+import org.opendaylight.yangtools.concepts.WritableIdentifier;
 
-public class ShardIdentifier {
+public final class ShardIdentifier implements WritableIdentifier {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
     // This pattern needs to remain in sync with toString(), which produces
     // strings with corresponding to "%s-shard-%s-%s"
     private static final Pattern PATTERN = Pattern.compile("(\\S+)-shard-(\\S+)-(\\S+)");
@@ -22,14 +30,14 @@ public class ShardIdentifier {
     private final String shardName;
     private final MemberName memberName;
     private final String type;
-    private final String fullName;
+    private final String actorName;
 
     ShardIdentifier(final String shardName, final MemberName memberName, final String type) {
         this.shardName = requireNonNull(shardName, "shardName should not be null");
         this.memberName = requireNonNull(memberName, "memberName should not be null");
         this.type = requireNonNull(type, "type should not be null");
 
-        fullName = memberName.getName() + "-shard-" + shardName + "-" + type;
+        actorName = memberName.getName() + "-shard-" + shardName + "-" + type;
     }
 
     public static ShardIdentifier create(final String shardName, final MemberName memberName, final String type) {
@@ -43,31 +51,15 @@ public class ShardIdentifier {
         return new ShardIdentifier(matcher.group(2), MemberName.forName(matcher.group(1)), matcher.group(3));
     }
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-
-        final var that = (ShardIdentifier) obj;
-        return memberName.equals(that.memberName) && shardName.equals(that.shardName) && type.equals(that.type);
+    public static ShardIdentifier readFrom(final DataInput in) throws IOException {
+        final var shardName = in.readUTF();
+        final var memberName = MemberName.readFrom(in);
+        final var type = in.readUTF();
+        return new ShardIdentifier(shardName, memberName, type);
     }
 
-    @Override
-    public int hashCode() {
-        int result = shardName.hashCode();
-        result = 31 * result + memberName.hashCode();
-        result = 31 * result + type.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        // ensure the output of toString matches the pattern above
-        return fullName;
+    public static Builder builder() {
+        return new Builder();
     }
 
     public String getShardName() {
@@ -82,13 +74,46 @@ public class ShardIdentifier {
         return type;
     }
 
-    public static class Builder {
+    public String fullName() {
+        return actorName;
+    }
+
+    @Override
+    public void writeTo(final DataOutput out) throws IOException {
+        out.writeUTF(shardName);
+        memberName.writeTo(out);
+        out.writeUTF(type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shardName, memberName, type);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return this == obj || obj instanceof ShardIdentifier other
+            && memberName.equals(other.memberName) && shardName.equals(other.shardName) && type.equals(other.type);
+    }
+
+    @Override
+    public String toString() {
+        // ensure the output of toString matches the pattern above
+        return fullName();
+    }
+
+    @java.io.Serial
+    private Object writeReplace() {
+        return new SI(this);
+    }
+
+    public static final class Builder {
         private String shardName;
         private MemberName memberName;
         private String type;
 
-        public ShardIdentifier build() {
-            return new ShardIdentifier(shardName, memberName, type);
+        private Builder() {
+            // Hidden on purpose
         }
 
         public Builder shardName(final String newShardName) {
@@ -107,14 +132,17 @@ public class ShardIdentifier {
         }
 
         public Builder fromShardIdString(final String shardId) {
-            Matcher matcher = PATTERN.matcher(shardId);
-
+            final var matcher = PATTERN.matcher(shardId);
             if (matcher.matches()) {
                 memberName = MemberName.forName(matcher.group(1));
                 shardName = matcher.group(2);
                 type = matcher.group(3);
             }
             return this;
+        }
+
+        public ShardIdentifier build() {
+            return new ShardIdentifier(shardName, memberName, type);
         }
     }
 }
