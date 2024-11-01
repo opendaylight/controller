126e234e593f96ee6d834ec6e641aeed970572e7
Robert Varga <robert.varga@pantheon.tech>
2024-11-01 09:46:40 +0100

    Remove unnecessary checkReady()
    We have already asserted state and have tthe ready handle -- use it
    directly without touching state again.
    
    Change-Id: Ic227300e83bb2171a8ed287aa5e007f8edd12aed
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/FrontendReadWriteTransaction.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/FrontendReadWriteTransaction.java
@@ -372,7 +372,7 @@ final class FrontendReadWriteTransaction extends FrontendTransaction {
             case READY -> {
                 ready.stage = CommitStage.CAN_COMMIT_PENDING;
                 LOG.debug("{}: Transaction {} initiating canCommit", persistenceId(), getIdentifier());
-                checkReady().readyCohort.canCommit(new FutureCallback<>() {
+                ready.readyCohort.canCommit(new FutureCallback<>() {
                     @Override
                     public void onSuccess(final Empty result) {
                         successfulCanCommit(envelope, now);
