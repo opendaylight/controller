3253f06479a76a2a223cba9e72cec0c2a7cefb63
Robert Varga <robert.varga@pantheon.tech>
2024-11-07 18:30:54 +0100

    Modernize DataServiceIT
    Clean up builder use, use local variable type inference,
    Optional.orElseThrow() and DataObjectIdentifier.
    
    Change-Id: Iafbc742e936cc98679fc048134abaf69599d7c81
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-binding-it/src/test/java/org/opendaylight/controller/test/sal/binding/it/DataServiceIT.java
+++ opendaylight/md-sal/sal-binding-it/src/test/java/org/opendaylight/controller/test/sal/binding/it/DataServiceIT.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import org.junit.Test;
@@ -24,7 +23,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controll
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.store.rev140422.lists.unordered.container.UnorderedList;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.store.rev140422.lists.unordered.container.UnorderedListBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.store.rev140422.lists.unordered.container.UnorderedListKey;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.binding.DataObjectIdentifier;
 import org.ops4j.pax.exam.util.Filter;
 
 /**
@@ -44,44 +43,44 @@ public class DataServiceIT extends AbstractIT {
         final WriteTransaction transaction = dataBroker.newWriteOnlyTransaction();
         assertNotNull(transaction);
 
-        InstanceIdentifier<UnorderedList> node1 = createNodeRef("0");
-        Optional<UnorderedList> node = dataBroker.newReadOnlyTransaction().read(LogicalDatastoreType.OPERATIONAL, node1)
-                .get(5, TimeUnit.SECONDS);
+        final var node1 = createNodeRef("0");
+        final var node = dataBroker.newReadOnlyTransaction().read(LogicalDatastoreType.OPERATIONAL, node1)
+            .get(5, TimeUnit.SECONDS);
         assertFalse(node.isPresent());
-        UnorderedList nodeData1 = createNode("0");
 
+        final var nodeData1 = createNode("0");
         transaction.put(LogicalDatastoreType.OPERATIONAL, node1, nodeData1);
         transaction.commit().get(5, TimeUnit.SECONDS);
 
-        Optional<UnorderedList> readedData = dataBroker.newReadOnlyTransaction().read(LogicalDatastoreType.OPERATIONAL,
-                node1).get(5, TimeUnit.SECONDS);
+        final var readedData = dataBroker.newReadOnlyTransaction()
+            .read(LogicalDatastoreType.OPERATIONAL, node1)
+            .get(5, TimeUnit.SECONDS);
         assertTrue(readedData.isPresent());
-        assertEquals(nodeData1.key(), readedData.get().key());
+        assertEquals(nodeData1.key(), readedData.orElseThrow().key());
 
-        final WriteTransaction transaction2 = dataBroker.newWriteOnlyTransaction();
+        final var transaction2 = dataBroker.newWriteOnlyTransaction();
         assertNotNull(transaction2);
 
         transaction2.delete(LogicalDatastoreType.OPERATIONAL, node1);
 
         transaction2.commit().get(5, TimeUnit.SECONDS);
 
-        Optional<UnorderedList> readedData2 = dataBroker.newReadOnlyTransaction().read(LogicalDatastoreType.OPERATIONAL,
-                node1).get(5, TimeUnit.SECONDS);
-        assertFalse(readedData2.isPresent());
+        assertFalse(dataBroker.newReadOnlyTransaction().exists(LogicalDatastoreType.OPERATIONAL, node1)
+            .get(5, TimeUnit.SECONDS));
     }
 
-    private static InstanceIdentifier<UnorderedList> createNodeRef(final String string) {
-        UnorderedListKey key = new UnorderedListKey(string);
-        return InstanceIdentifier.builder(Lists.class).child(UnorderedContainer.class).child(UnorderedList.class, key)
-                .build();
+    private static DataObjectIdentifier.WithKey<UnorderedList, UnorderedListKey> createNodeRef(final String string) {
+        return DataObjectIdentifier.builder(Lists.class)
+            .child(UnorderedContainer.class)
+            .child(UnorderedList.class, new UnorderedListKey(string))
+            .build();
     }
 
     private static UnorderedList createNode(final String string) {
-        UnorderedListBuilder ret = new UnorderedListBuilder();
-        UnorderedListKey nodeKey = new UnorderedListKey(string);
-        ret.withKey(nodeKey);
-        ret.setName("name of " + string);
-        ret.setName("value of " + string);
-        return ret.build();
+        return new UnorderedListBuilder()
+            .withKey(new UnorderedListKey(string))
+            .setName("name of " + string)
+            .setValue("value of " + string)
+            .build();
     }
 }
