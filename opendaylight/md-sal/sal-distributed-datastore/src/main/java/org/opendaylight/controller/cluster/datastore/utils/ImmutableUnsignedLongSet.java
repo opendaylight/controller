3c397bb8a12e6e9cbab80e55934a542a55959198
Robert Varga <robert.varga@pantheon.tech>
2025-01-07 21:00:56 +0100

    WIP: Adjust ImmutableUnsignedLongSet.DEFAULT_MAX_ARRAY
    This patch raises the default from 4096 to 8190 and adds the reasoning
    behing the two implementations we are using.
    
    // FIXME: add issue
    
    Change-Id: Ibb7b7cc1c04222c118d9db6659e0576eb7213b2a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/ImmutableUnsignedLongSet.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/ImmutableUnsignedLongSet.java
@@ -24,7 +24,52 @@ public final class ImmutableUnsignedLongSet extends UnsignedLongSet implements I
     // TreeSet has a large per-entry overhead (40-64 bytes as of OpenJDK 21), so we prefer allocating
     // an ImmutableSortedSet, which is backed by an array of up to this many elements. For larger sizes we opt to pay
     // the TreeMap overhead so as not to create huge objects on the heap.
-    private static final int DEFAULT_MAX_ARRAY = 4096;
+    //
+    // Assuming each Entry costs 32 bytes (as of OpenJDK 21).
+    //
+    //   size() | Entry | Array size | TreeSet.Entry | array objs | tree objs | array memory | tree memory
+    //   --------------------------------------------------------------------------------------------------
+    //     2K      64Ki     8/ 16Ki      80 - 128Ki      2K+3         ~4K        72  -  80Ki    144 -192Ki
+    //     4K     128Ki    16/ 32Ki     160 - 256Ki      4K+3         ~8K       144  - 160Ki    288 -384Ki
+    //     8K     256Ki    32/ 64Ki     320 - 512Ki      8K+3        ~16K       288  - 320Ki    576 -768Ki
+    //    16K     512Ki    64/128Ki    0.63 -   1Mi     16K+3        ~32K       576  - 640Ki    1.1 -1.5Mi
+    //    32K      ~1Mi   128/256Ki    1.25 -   2Mi     32K+3        ~64K       1.1  -1.25Mi    2.25-3  Mi
+    //    64K      ~2Mi   256/512Ki    2.5  -   4Mi     64K+3       ~128K       2.25 -2.5 Mi    4.5 -6  Mi
+    //
+    // From performance perspective, ImmutableSortedSet performs a bisect search, so the amount of jumping around is
+    // roughly the same, except the access pattern might be more recognizable by the CPU prefetch logic. Overall it
+    // needs only 41.67-50% the memory.
+    //
+    // We need to take into consideration G1 GC (default in OpenJDK 21), documentation here:
+    // https://docs.oracle.com/en/java/javase/21/gctuning/garbage-first-g1-garbage-collector1.html#GUID-D74F3CC7-CC9F-45B5-B03D-510AEEAC2DAC
+    //
+    // We aim to have arrays always smaller than half a region, i.e. always strictly less than 512KiB, we need to
+    // account for Object[] sizing. In OpenJDK 21, arrays have 16 bytes of overhead, so we should rarely use more than
+    // 65532 entries, just to be on the safe side. They should work fine with heaps of up to 2GiB. At 8GiB heap we can
+    // expect objects to reliably fit into a single 4MiB region.
+    //
+    // The other part of the picture are cache sizes in modern CPUs. Here there is quite a bit of variation, but let us
+    // assume:
+    //  - 32-64KiB L1 cache per core
+    //  - 0.5-2MiB L2 cache per core, perhaps shared between cores
+    //  - 16-32MiB unified L3 cache
+    //
+    // Based on all of this, we choose to default to 8190 entries, so that we end up with 64KiB arrays. In this case the
+    // array roughly fits in the L1 and the complete dataset fits into L2.
+    //
+    // For comparison, 65532 entries result in 524_272-byte arrays (fitting into L2) and the data set easily fitting
+    // into L3.
+    //
+    // TODO: it would seem that a more lookup-performant (than array) and memory-efficient (than TreeMap) version could
+    //       be achieved by employing S+Tree as detailed in https://curiouscoding.nl/posts/static-search-tree/
+    //       and https://en.algorithmica.org/hpc/data-structures/s-tree/.
+    //       Note that our key is either 8- or 16-bytes and indexing is another 8 bytes -- so we can fit two such
+    //       objects into a single cache line. We can then decide how exactly to organize these nodes -- be it in an
+    //       array, individual nodes, or something in-between. The in-between case might be interesting for mutations,
+    //       as some operations (like extending a range) might become quite cheap.
+    //       Perhaps a B2Tree, https://link.springer.com/article/10.1007/s13222-022-00409-y, is something we can
+    //       leverage?
+    private static final int DEFAULT_MAX_ARRAY = 8190;
     private static final String PROP_MAX_ARRAY =
         "org.opendaylight.controller.cluster.datastore.utils.ImmutableUnsignedLongSet.max-array";
     private static final int MAX_ARRAY;
