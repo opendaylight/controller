9703d64f1eb8bd44a4115a42d32c4c335d6d8498
Robert Varga <robert.varga@pantheon.tech>
2025-01-28 10:26:03 +0100

    TermInfoStore.persistInfo() can report IOException
    This is a persistence operation which the RaftActor cannot recover from.
    Declare IOException as thrown, so that callers are forced to deal with
    it.
    
    JIRA: CONTROLLER-2133
    Change-Id: Ia9d1a340d3472534087a0e89876061169fbdf75a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/RaftActorBehavior.java
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/RaftActorBehavior.java
@@ -10,6 +10,8 @@ package org.opendaylight.controller.cluster.raft.behaviors;
 import static java.util.Objects.requireNonNull;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import org.apache.pekko.actor.ActorRef;
@@ -186,7 +188,12 @@ public abstract class RaftActorBehavior implements AutoCloseable {
 
         final var grantVote = canGrantVote(requestVote);
         if (grantVote) {
-            context.persistTermInfo(new TermInfo(requestVote.getTerm(), requestVote.getCandidateId()));
+            try {
+                context.persistTermInfo(new TermInfo(requestVote.getTerm(), requestVote.getCandidateId()));
+            } catch (IOException e) {
+                // FIXME: do not mask IOException
+                throw new UncheckedIOException(e);
+            }
         }
 
         final var reply = new RequestVoteReply(currentTerm(), grantVote);
