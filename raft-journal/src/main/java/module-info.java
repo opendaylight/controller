bf849c6fafb651f15201de84515ba9b084628afa
Robert Varga <robert.varga@pantheon.tech>
2025-01-28 21:08:23 +0100

    Use bnd-parent in raft-journal
    raft-journal is a simple JPMS API component at this point. Use
    bnd-parent to package it. Also fix javadocs while we are at it.
    
    Change-Id: Ib821ea720f80edcd87dbdb528366b4599dbd9d3e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft-journal/src/main/java/module-info.java
+++ raft-journal/src/main/java/module-info.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Storage primitives for a RAFT journal.
+ */
 module org.opendaylight.controller.raft.journal {
     exports org.opendaylight.controller.raft.journal;
 
@@ -20,4 +23,5 @@ module org.opendaylight.controller.raft.journal {
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
+    requires static org.osgi.annotation.bundle;
 }
