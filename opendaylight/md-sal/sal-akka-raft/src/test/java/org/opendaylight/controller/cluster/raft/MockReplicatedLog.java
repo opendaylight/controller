78fd493034d3d02bc0bd6cb4a8ec5542685f5805
Robert Varga <robert.varga@pantheon.tech>
2025-03-21 19:40:36 +0100

    Split out MockReplicatedLog
    This class deserves to be outside of the test class, simplifying its
    name.
    
    JIRA: CONTROLLER-2134
    Change-Id: I39052e6f4b6cf05dddbc025b2898239a1ff2d24d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/MockReplicatedLog.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft;
+
+import java.util.List;
+import java.util.function.Consumer;
+import org.opendaylight.controller.cluster.raft.spi.RaftEntryMeta;
+
+final class MockReplicatedLog extends AbstractReplicatedLog {
+    MockReplicatedLog() {
+        super("", -1L, -1L, List.of());
+    }
+
+    @Override
+    public boolean removeFromAndPersist(final long index) {
+        return true;
+    }
+
+    @Override
+    public <T extends ReplicatedLogEntry> boolean appendAndPersist(final T replicatedLogEntry,
+            final Consumer<T> callback, final boolean doAsync) {
+        if (callback != null) {
+            callback.accept(replicatedLogEntry);
+        }
+        return true;
+    }
+
+    @Override
+    public void captureSnapshotIfReady(final RaftEntryMeta replicatedLogEntry) {
+        // No-op
+    }
+
+    @Override
+    public boolean shouldCaptureSnapshot(final long logIndex) {
+        return false;
+    }
+}
