ffb630d3818bc58a914a0fc8d7936772c73e737e
Robert Varga <robert.varga@pantheon.tech>
2025-03-23 05:19:48 +0100

    Move cluster.notifications
    Let's rehost these into sal-akka-raft for now, as that is where they are
    used from.
    
    JIRA: CONTROLLER-2134
    Change-Id: Icb4968774d89517486c50bff95393e8c68b8265b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/PreLeaderScenarioTest.java
+++ opendaylight/md-sal/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/PreLeaderScenarioTest.java
@@ -75,9 +75,9 @@ public class PreLeaderScenarioTest extends AbstractRaftActorIntegrationTest {
 
         // Verify the expected raft state changes. It should go to PreLeader since it has an uncommitted entry.
         List<RoleChanged> roleChange = expectMatching(follower1NotifierActor, RoleChanged.class, 3);
-        assertEquals("Role change 1", RaftState.Candidate.name(), roleChange.get(0).newRole());
-        assertEquals("Role change 2", RaftState.PreLeader.name(), roleChange.get(1).newRole());
-        assertEquals("Role change 3", RaftState.Leader.name(), roleChange.get(2).newRole());
+        assertEquals("Role change 1", RaftState.Candidate, roleChange.get(0).newRole());
+        assertEquals("Role change 2", RaftState.PreLeader, roleChange.get(1).newRole());
+        assertEquals("Role change 3", RaftState.Leader, roleChange.get(2).newRole());
 
         final long previousTerm = currentTerm;
         currentTerm = follower1Context.currentTerm();
