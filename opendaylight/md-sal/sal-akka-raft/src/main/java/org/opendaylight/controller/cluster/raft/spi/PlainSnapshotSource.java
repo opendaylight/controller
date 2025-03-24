4c1f1370ffb5304ceb9a7cf1502f7bbf29f7b142
Robert Varga <robert.varga@pantheon.tech>
2025-03-24 11:07:55 +0100

    Add SnapshotSource
    We will need to deal with snapshots being available for reading in
    multiple formats. This patch add the concept of a SnapshotSource, with
    two formats: plain and LZ4.
    
    This ends up being a framework, but that is completely fine.
    
    JIRA: CONTROLLER-2134
    Change-Id: I99a8a967e6d08da681fa45082927b3039a523a49
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/spi/PlainSnapshotSource.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.spi;
+
+/**
+ * A {@link SnapshotSource} corresponding directly to the serialization format of a snapshot.
+ */
+public non-sealed interface PlainSnapshotSource extends SnapshotSource {
+    @Override
+    default PlainSnapshotSource toPlainSource() {
+        return this;
+    }
+}
