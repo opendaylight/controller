6863567308a4a13b565a119f63588412e3c04508
Robert Varga <robert.varga@pantheon.tech>
2025-03-24 22:11:50 +0100

    Split out raft-spi
    We have a nice set of classes that could be more widely used. Let's
    split them out. This has the neat benefit of making lz4-java an
    implementation detail hidden from the outside world.
    
    We also introduce odl-lz4 to package lz4-java independently of
    everything else.
    
    Change-Id: I3434bac53dba6935c022e9ab79fc40eead01e40b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/raft-spi/src/main/java/org/opendaylight/raft/spi/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Classes that are helpful for implementing RAFT.
+ */
+@org.osgi.annotation.bundle.Export
+package org.opendaylight.raft.spi;
