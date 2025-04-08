b259b35ad9fc7dcbb35c87f4a898cd17d93c7178
Robert Varga <robert.varga@pantheon.tech>
2025-04-09 00:05:16 +0200

    WIP: Add StoreEntry et al.
    We will need the tools to store entries in RaftJournal.
    
    JIRA: CONTROLLER-2137
    Change-Id: I5c14621c9f6295709959e804234e7a9223058687
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/spi/ToStoreEntryMapperTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.spi;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.opendaylight.raft.spi.CompressionType;
+
+class ToStoreEntryMapperTest {
+    @TempDir
+    private Path tempDir;
+
+    @BeforeAll
+    static void beforeAll() {
+        // Common assertion
+        assertEquals(18, ToStoreEntryMapperV1.THRESHOLD_MIN);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void rejectedThreshold(final int threshold) {
+        final var ex = assertThrows(IllegalArgumentException.class,
+            () -> new ToStoreEntryMapperV1("", tempDir, threshold, CompressionType.NONE, 0));
+        assertEquals("Invalid threshold " + threshold, ex.getMessage());
+    }
+
+    private static List<Arguments> rejectedThreshold() {
+        return List.of(arguments(Integer.MIN_VALUE), arguments(-1), arguments(0), arguments(17));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void acceptedThreshold(final int threshold) {
+        assertDoesNotThrow(() -> new ToStoreEntryMapperV1("", tempDir, threshold, CompressionType.NONE, 0));
+    }
+
+    private static List<Arguments> acceptedThreshold() {
+        return List.of(arguments(18), arguments(Integer.MAX_VALUE));
+    }
+}
