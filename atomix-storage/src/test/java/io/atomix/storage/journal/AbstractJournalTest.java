76da6fb8a79bfd8c8c2cf6fe630ed10cac1a3f7b
Robert Varga <robert.varga@pantheon.tech>
2025-05-06 11:05:32 +0200

    Move StorageLevel and custom exceptions
    The API we are providing is exposed from raft-actor, move the exceptions
    there.
    
    JIRA: CONTROLLER-2137
    Change-Id: Ia017aaa9698b3966eb4d62cda0e4fb37aecf3c7d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- atomix-storage/src/test/java/io/atomix/storage/journal/AbstractJournalTest.java
+++ atomix-storage/src/test/java/io/atomix/storage/journal/AbstractJournalTest.java
@@ -36,6 +36,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.opendaylight.controller.raft.journal.StorageLevel;
 
 /**
  * Base journal test.
