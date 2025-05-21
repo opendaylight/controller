926f0dcce350cc2c6b684d4545700f4ce29c11a8
Robert Varga <robert.varga@pantheon.tech>
2025-05-21 12:53:08 +0200

    WIP: Switch EntryStore to use RaftJournal directly
    Use a RaftJournal directly to store entries without the need to go
    through Pekko Persistance and rely on it to do the Right Thing(tm).
    
    We refactor EntryStore and ReplicateLogImpl to align with EntryJournal
    lifecycle and overall way of doing thigs.
    
    // FIXME: PekkoRecovery should take a snapshot on completion if it
    //        recovered any data
    
    // FIXME: ReplicationAndSnapshotsIntegrationTest: 1 failure
    // FIXME: ReplicationAndSnapshotsWithLaggingFollowerIntegrationTest: 1 failure
    // FIXME: ClusterAdminRpcServiceTest.testRemoveShardReplica: 1 failure
    // TODO: lazy EnabledRaftStorage instantiation
    
    JIRA: CONTROLLER-2137
    Change-Id: Iaeac853e19966a2fde17450802682967a5e7e01c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/spi/EntryStore.java
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/spi/EntryStore.java
@@ -7,32 +7,37 @@
  */
 package org.opendaylight.controller.cluster.raft.spi;
 
-import org.apache.pekko.persistence.JournalProtocol;
-import org.eclipse.jdt.annotation.NonNull;
+import static java.util.Objects.requireNonNull;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.controller.cluster.raft.RaftActor;
 import org.opendaylight.controller.cluster.raft.ReplicatedLogEntry;
 import org.opendaylight.raft.api.EntryMeta;
 
 /**
- * Interface to a access and manage {@link StateMachineCommand}-bearing entries with {@link EntryMeta}.
+ * Interface to a access and manage {@link StateMachineCommand}-bearing entries with {@link EntryMeta}. This interface
+ * is inherently asynchronous, with the assumption that each request is enqueued to a background thread, which processes
+ * requests in batches. Synchronization with the enclosing {@link RaftActor} is either asynchronous
+ * (via {@link RaftCallback}s), or defined to terminate the actor on failure.
  */
+@NonNullByDefault
 public interface EntryStore {
     /**
      * {@return the {@link RaftStorageCompleter}}
      */
-    @NonNull RaftStorageCompleter completer();
+    RaftStorageCompleter completer();
 
     /**
      * Persists an entry to the applicable journal synchronously. The contract is that the callback will be invoked
-     * before {@link RaftActor} sees any other message.
+     * before {@link RaftActor} sees any other message. Default implementation registers the callback with
+     * {@link #completer()} and defers to {@link #startPersistEntry(ReplicatedLogEntry, RaftCallback)}.
      *
      * @param entry the journal entry to persist
      * @param callback the callback when persistence is complete
      */
-    // FIXME: without callback and throwing IOException
-    @NonNullByDefault
-    void persistEntry(ReplicatedLogEntry entry, Runnable callback);
+    default void persistEntry(final ReplicatedLogEntry entry, final RaftCallback<Long> callback) {
+        startPersistEntry(requireNonNull(entry), completer().syncWithCurrentMessage(callback));
+    }
 
     /**
      * Persists an entry to the applicable journal asynchronously.
@@ -40,16 +45,24 @@ public interface EntryStore {
      * @param entry the journal entry to persist
      * @param callback the callback when persistence is complete
      */
-    // FIXME: Callback<ReplicatedLogEntry> instead of Consumer
-    @NonNullByDefault
-    void startPersistEntry(ReplicatedLogEntry entry, Runnable callback);
+    // FIXME: really just a boolean 'async' flag's difference
+    void startPersistEntry(ReplicatedLogEntry entry, RaftCallback<Long> callback);
 
     /**
-     * Delete entries starting from specified index.
+     * Deletes journal entries up to, but not including, the given {@code journalIndex}.
      *
-     * @param fromIndex the index of first entry to delete
+     * @param firstRetainedIndex the {@code journalIndex} of the first retained entry
      */
-    void deleteEntries(long fromIndex);
+    // FIXME: guarantee sync as discardTail()
+    void discardHead(long firstRetainedIndex);
+
+    /**
+     * Delete entries starting from, and including, specified index. The contract is that the callback will be invoked
+     * before {@link RaftActor} sees any other message.
+     *
+     * @param firstRemovedIndex the {@code journalIndex} of first entry to delete
+     */
+    void discardTail(long firstRemovedIndex);
 
     /**
      * Record a known value of {@code lastApplied} as a recovery optimization. If we can recover this information,
@@ -62,30 +75,9 @@ public interface EntryStore {
      * <p>If we later discover that this index lies beyond current leader's {@code commitIndex}, we will ask for
      * a complete snapshot -- which is not particularly nice, but should happen seldom enough for it not to matter much.
      *
-     * @param lastApplied lastApplied index to remember
+     * @param commitJournalIndex {@code the journalIndex} of the entry which is covered by {@code commitIndex} and has
+     *        been observed as {@code lastApplied}.
      */
-    void markLastApplied(long lastApplied);
-
-    /**
-     * Deletes journal entries up to the given sequence number.
-     *
-     * @param sequenceNumber the sequence number
-     */
-    // FIXME: throws IOException
-    void deleteMessages(long sequenceNumber);
-
-    /**
-     * Returns the last sequence number contained in the journal.
-     *
-     * @return the last sequence number
-     */
-    long lastSequenceNumber();
-
-    /**
-     * Receive and potentially handle a {@link JournalProtocol} response.
-     *
-     * @param response A {@link JournalProtocol} response
-     * @return {@code true} if the response was handled
-     */
-    boolean handleJournalResponse(JournalProtocol.@NonNull Response response);
+    // FIXME: guarantee sync as discardTail()
+    void checkpointLastApplied(long commitJournalIndex);
 }
