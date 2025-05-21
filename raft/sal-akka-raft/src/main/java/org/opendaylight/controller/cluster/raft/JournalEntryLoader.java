349666688c2381d461c8dbac2e7833931b23bdee
Robert Varga <robert.varga@pantheon.tech>
2025-05-21 12:53:08 +0200

    WIP: Switch EntryStore to use RaftJournal directly
    Use a RaftJournal directly to store entries without the need to go
    through Pekko Persistance and hope for it to do The Right Thing(tm).
    
    // FIXME: split this up
    
    // FIXME: populate on recovery
    
    // FIXME: async persist with flusher thread
    // FIXME: RaftActor needs an 'aroundReceive()' to wait for flushout
    //        of synchronous requests
    // FIXME: deal with flush of RaftJournal *and* any files we create
    // FIXME: deal with removing any files during RaftJournal delete/reset
    // FIXME: lazy EnabledRaftStorage instantiation
    // FIXME: 10 errors
    // FIXME: 21 failures
    
    JIRA: CONTROLLER-2137
    Change-Id: Ic0aa78bdff0f6cf6a0174b527fbbcaa2dfd23c62
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/JournalEntryLoader.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.COMPRESSION_LZ4;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.COMPRESSION_MASK;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.COMPRESSION_NONE;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.DISPOSITION_FILE;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.DISPOSITION_INLINE;
+import static org.opendaylight.controller.cluster.raft.PekkoRaftStorage.DISPOSITION_MASK;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.controller.cluster.raft.messages.Payload;
+import org.opendaylight.controller.cluster.raft.spi.EntryLoader;
+import org.opendaylight.raft.journal.EntryReader;
+import org.opendaylight.raft.spi.CompressionType;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.opendaylight.yangtools.concepts.WritableObjects;
+
+@NonNullByDefault
+final class JournalEntryLoader extends AbstractRegistration implements EntryLoader {
+    private final Path directory;
+    private final EntryReader reader;
+
+    JournalEntryLoader(final Path directory, final EntryReader reader) {
+        this.directory = requireNonNull(directory);
+        this.reader = requireNonNull(reader);
+    }
+
+    @Override
+    public @Nullable LoadedEntry loadNext() {
+        return reader.tryNext(this::bytesToEntry);
+    }
+
+    @Override
+    protected void removeRegistration() {
+        reader.close();
+    }
+
+    private LoadedEntry bytesToEntry(final long journalIndex, final ByteBuf buf) {
+        try (var in = new ByteBufInputStream(buf)) {
+            final var hdr = WritableObjects.readLongHeader(in);
+            final var entryIndex = WritableObjects.readFirstLong(in, hdr);
+            final var entryTerm = WritableObjects.readSecondLong(in, hdr);
+
+            final var comp = hdr & COMPRESSION_MASK;
+            final var compression = switch (comp) {
+                case COMPRESSION_NONE -> CompressionType.NONE;
+                case COMPRESSION_LZ4 -> CompressionType.LZ4;
+                default -> throw new IOException("Unrecognized compression " + comp);
+            };
+
+            final var disp = hdr & DISPOSITION_MASK;
+            final var command = switch (disp) {
+                case DISPOSITION_FILE -> readFile(journalIndex, compression);
+                case DISPOSITION_INLINE -> readCommand(in, compression);
+                default -> throw new IOException("Unrecognized disposition " + disp);
+            };
+
+            return new LoadedLogEntry(journalIndex, entryIndex, entryTerm, command);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Payload readFile(final long journalIndex, final CompressionType compression) throws IOException {
+        try (var in = Files.newInputStream(directory.resolve(PekkoRaftStorage.ENTRY_FILE_MAME.apply(journalIndex)))) {
+            return readCommand(in, compression);
+        }
+    }
+
+    private static Payload readCommand(final InputStream in, final CompressionType compression) throws IOException {
+        try (var ois = new ObjectInputStream(compression.decodeInput(in))) {
+            try {
+                return (Payload) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new IOException("Cannot read command", e);
+            }
+        }
+    }
+}
