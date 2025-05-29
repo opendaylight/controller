0d32c4cc4f86b5ab0d4a25e11440a8fd7f41c82b
Robert Varga <robert.varga@pantheon.tech>
2025-05-29 11:18:49 +0200

    Add Forwarding{Entry,Snapshot}Store
    Allow SnapshotStore and EntryStore to be manipulated separately.
    
    JIRA: CONTROLLER-2137
    Change-Id: I2ff89bde38bc20fff94a7b18128908a07d9d977a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/spi/ForwardingSnapshotStore.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.spi;
+
+import com.google.common.base.MoreObjects;
+import java.io.IOException;
+import java.time.Instant;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.controller.cluster.raft.spi.StateSnapshot.ToStorage;
+import org.opendaylight.raft.api.EntryInfo;
+import org.opendaylight.raft.spi.InstallableSnapshot;
+
+@NonNullByDefault
+public abstract class ForwardingSnapshotStore implements SnapshotStore {
+
+    protected abstract SnapshotStore delegate();
+
+    @Override
+    public @Nullable SnapshotFile lastSnapshot() throws IOException {
+        return delegate().lastSnapshot();
+    }
+
+    @Override
+    public void saveSnapshot(final RaftSnapshot raftSnapshot, final EntryInfo lastIncluded,
+            final @Nullable ToStorage<?> snapshot, final RaftCallback<Instant> callback) {
+        delegate().saveSnapshot(raftSnapshot, lastIncluded, snapshot, callback);
+    }
+
+    @Override
+    public void saveSnapshot(final RaftSnapshot raftSnapshot, final EntryInfo lastIncluded,
+            final @Nullable ToStorage<?> snapshot, final Instant timestamp) throws IOException {
+        delegate().saveSnapshot(raftSnapshot, lastIncluded, snapshot, timestamp);
+    }
+
+    @Override
+    public void streamToInstall(final EntryInfo lastIncluded, final ToStorage<?> snapshot,
+            final RaftCallback<InstallableSnapshot> callback) {
+        delegate().streamToInstall(lastIncluded, snapshot, callback);
+    }
+
+    @Override
+    public void retainSnapshots(final Instant firstRetained) {
+        delegate().retainSnapshots(firstRetained);
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("delegate", delegate()).toString();
+    }
+}
