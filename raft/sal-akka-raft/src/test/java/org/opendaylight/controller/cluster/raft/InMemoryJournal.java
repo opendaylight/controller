f26db021991c8ddaa171ea1536948c6b1ba507b9
Robert Varga <robert.varga@pantheon.tech>
2025-05-30 18:34:49 +0200

    Modernize InMemoryJournal
    Use local variable type inference, records, immutable collections and
    instanceof patterns.
    
    JIRA: CONTROLLER-2137
    Change-Id: If43c00295667e2a49f0e79f84ca388173d2094b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/InMemoryJournal.java
+++ raft/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/InMemoryJournal.java
@@ -7,10 +7,11 @@
  */
 package org.opendaylight.controller.cluster.raft;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.util.concurrent.Uninterruptibles;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,8 @@ import org.apache.pekko.persistence.AtomicWrite;
 import org.apache.pekko.persistence.PersistentImpl;
 import org.apache.pekko.persistence.PersistentRepr;
 import org.apache.pekko.persistence.journal.japi.AsyncWriteJournal;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Option;
@@ -32,42 +35,40 @@ import scala.concurrent.Future;
 import scala.jdk.javaapi.CollectionConverters;
 
 /**
- * An akka AsyncWriteJournal implementation that stores data in memory. This is intended for testing.
+ * An {@link AsyncWriteJournal} implementation that stores data in memory. This is intended for testing.
  *
  * @author Thomas Pantelis
  */
 public class InMemoryJournal extends AsyncWriteJournal {
+    private record WriteMessagesComplete<T>(@NonNull CountDownLatch latch, @Nullable Class<T> ofType) {
+        WriteMessagesComplete {
+            requireNonNull(latch);
+        }
 
-    private static class WriteMessagesComplete {
-        final CountDownLatch latch;
-        final Class<?> ofType;
-
-        WriteMessagesComplete(final int count, final Class<?> ofType) {
-            latch = new CountDownLatch(count);
-            this.ofType = ofType;
+        WriteMessagesComplete(final int count, final Class<T> ofType) {
+            this(new CountDownLatch(count), ofType);
         }
     }
 
-    static final Logger LOG = LoggerFactory.getLogger(InMemoryJournal.class);
-
-    private static final Map<String, Map<Long, Object>> JOURNALS = new ConcurrentHashMap<>();
-
-    private static final Map<String, CountDownLatch> DELETE_MESSAGES_COMPLETE_LATCHES = new ConcurrentHashMap<>();
-
-    private static final Map<String, WriteMessagesComplete> WRITE_MESSAGES_COMPLETE = new ConcurrentHashMap<>();
-
-    private static final Map<String, CountDownLatch> BLOCK_READ_MESSAGES_LATCHES = new ConcurrentHashMap<>();
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryJournal.class);
+    private static final ConcurrentHashMap<String, Map<Long, Object>> JOURNALS = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, CountDownLatch> DELETE_MESSAGES_COMPLETE_LATCHES =
+        new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, WriteMessagesComplete<?>> WRITE_MESSAGES_COMPLETE =
+        new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, CountDownLatch> BLOCK_READ_MESSAGES_LATCHES =
+        new ConcurrentHashMap<>();
 
     private static Object deserialize(final Object data) {
-        return data instanceof byte[] ? SerializationUtils.deserialize((byte[])data) : data;
+        return data instanceof byte[] bytes ? SerializationUtils.deserialize(bytes) : data;
     }
 
     public static void addEntry(final String persistenceId, final long sequenceNr, final Object data) {
-        Map<Long, Object> journal = JOURNALS.computeIfAbsent(persistenceId, k -> new LinkedHashMap<>());
+        final var journal = JOURNALS.computeIfAbsent(persistenceId, k -> new LinkedHashMap<>());
 
         synchronized (journal) {
-            journal.put(sequenceNr, data instanceof Serializable
-                    ? SerializationUtils.serialize((Serializable) data) : data);
+            journal.put(sequenceNr,
+                data instanceof Serializable serializable ? SerializationUtils.serialize(serializable) : data);
         }
     }
 
@@ -78,19 +79,18 @@ public class InMemoryJournal extends AsyncWriteJournal {
         BLOCK_READ_MESSAGES_LATCHES.clear();
     }
 
-    @SuppressWarnings("unchecked")
     public static <T> List<T> get(final String persistenceId, final Class<T> type) {
-        Map<Long, Object> journalMap = JOURNALS.get(persistenceId);
+        final var journalMap = JOURNALS.get(persistenceId);
         if (journalMap == null) {
-            return Collections.<T>emptyList();
+            return List.of();
         }
 
         synchronized (journalMap) {
-            List<T> journal = new ArrayList<>(journalMap.size());
-            for (Object entry: journalMap.values()) {
-                Object data = deserialize(entry);
+            final var journal = new ArrayList<T>(journalMap.size());
+            for (var entry : journalMap.values()) {
+                final var data = deserialize(entry);
                 if (type.isInstance(data)) {
-                    journal.add((T) data);
+                    journal.add(type.cast(data));
                 }
             }
 
@@ -99,22 +99,21 @@ public class InMemoryJournal extends AsyncWriteJournal {
     }
 
     public static Map<Long, Object> get(final String persistenceId) {
-        Map<Long, Object> journalMap = JOURNALS.get(persistenceId);
-        return journalMap != null ? journalMap : Collections.<Long, Object>emptyMap();
+        final var journalMap = JOURNALS.get(persistenceId);
+        return journalMap != null ? journalMap : Map.of();
     }
 
     public static void dumpJournal(final String persistenceId) {
-        StringBuilder builder = new StringBuilder(String.format("Journal log for %s:", persistenceId));
-        Map<Long, Object> journalMap = JOURNALS.get(persistenceId);
+        final var sb = new StringBuilder(String.format("Journal log for %s:", persistenceId));
+        final var journalMap = JOURNALS.get(persistenceId);
         if (journalMap != null) {
             synchronized (journalMap) {
-                for (Map.Entry<Long, Object> e: journalMap.entrySet()) {
-                    builder.append("\n    ").append(e.getKey()).append(" = ").append(deserialize(e.getValue()));
+                for (final var entry: journalMap.entrySet()) {
+                    sb.append("\n    ").append(entry.getKey()).append(" = ").append(deserialize(entry.getValue()));
                 }
             }
         }
-
-        LOG.info(builder.toString());
+        LOG.info(sb.toString());
     }
 
     public static void waitForDeleteMessagesComplete(final String persistenceId) {
@@ -136,12 +135,12 @@ public class InMemoryJournal extends AsyncWriteJournal {
     }
 
     public static void addWriteMessagesCompleteLatch(final String persistenceId, final int count) {
-        WRITE_MESSAGES_COMPLETE.put(persistenceId, new WriteMessagesComplete(count, null));
+        WRITE_MESSAGES_COMPLETE.put(persistenceId, new WriteMessagesComplete<>(count, null));
     }
 
     public static void addWriteMessagesCompleteLatch(final String persistenceId, final int count,
             final Class<?> ofType) {
-        WRITE_MESSAGES_COMPLETE.put(persistenceId, new WriteMessagesComplete(count, ofType));
+        WRITE_MESSAGES_COMPLETE.put(persistenceId, new WriteMessagesComplete<>(count, requireNonNull(ofType)));
     }
 
     public static void addBlockReadMessagesLatch(final String persistenceId, final CountDownLatch latch) {
@@ -151,27 +150,26 @@ public class InMemoryJournal extends AsyncWriteJournal {
     @Override
     public Future<Void> doAsyncReplayMessages(final String persistenceId, final long fromSequenceNr,
             final long toSequenceNr, final long max, final Consumer<PersistentRepr> replayCallback) {
-        LOG.trace("doAsyncReplayMessages for {}: fromSequenceNr: {}, toSequenceNr: {}", persistenceId,
-                fromSequenceNr,toSequenceNr);
+        LOG.trace("doAsyncReplayMessages for {}: fromSequenceNr: {}, toSequenceNr: {}", persistenceId, fromSequenceNr,
+            toSequenceNr);
         return Futures.future(() -> {
-            CountDownLatch blockLatch = BLOCK_READ_MESSAGES_LATCHES.remove(persistenceId);
+            final var blockLatch = BLOCK_READ_MESSAGES_LATCHES.remove(persistenceId);
             if (blockLatch != null) {
                 Uninterruptibles.awaitUninterruptibly(blockLatch);
             }
 
-            Map<Long, Object> journal = JOURNALS.get(persistenceId);
+            final var journal = JOURNALS.get(persistenceId);
             if (journal == null) {
                 return null;
             }
 
             synchronized (journal) {
                 int count = 0;
-                for (Map.Entry<Long,Object> entry : journal.entrySet()) {
-                    if (++count <= max && entry.getKey() >= fromSequenceNr && entry.getKey() <= toSequenceNr) {
-                        PersistentRepr persistentMessage =
-                                new PersistentImpl(deserialize(entry.getValue()), entry.getKey(), persistenceId,
-                                        null, false, null, null, 0, Option.empty());
-                        replayCallback.accept(persistentMessage);
+                for (var entry : journal.entrySet()) {
+                    final long key = entry.getKey();
+                    if (++count <= max && key >= fromSequenceNr && key <= toSequenceNr) {
+                        replayCallback.accept(new PersistentImpl(deserialize(entry.getValue()), key, persistenceId,
+                            null, false, null, null, 0, Option.empty()));
                     }
                 }
             }
@@ -185,19 +183,18 @@ public class InMemoryJournal extends AsyncWriteJournal {
         LOG.trace("doAsyncReadHighestSequenceNr for {}: fromSequenceNr: {}", persistenceId, fromSequenceNr);
 
         // Pekko calls this during recovery.
-        Map<Long, Object> journal = JOURNALS.get(persistenceId);
+        final var journal = JOURNALS.get(persistenceId);
         if (journal == null) {
             return Futures.successful(fromSequenceNr);
         }
 
         synchronized (journal) {
             long highest = -1;
-            for (Long seqNr : journal.keySet()) {
+            for (var seqNr : journal.keySet()) {
                 if (seqNr.longValue() >= fromSequenceNr && seqNr.longValue() > highest) {
-                    highest = seqNr.longValue();
+                    highest = seqNr;
                 }
             }
-
             return Futures.successful(highest);
         }
     }
@@ -205,14 +202,14 @@ public class InMemoryJournal extends AsyncWriteJournal {
     @Override
     public Future<Iterable<Optional<Exception>>> doAsyncWriteMessages(final Iterable<AtomicWrite> messages) {
         return Futures.future(() -> {
-            for (AtomicWrite write : messages) {
-                for (PersistentRepr repr : CollectionConverters.asJava(write.payload())) {
+            for (var write : messages) {
+                for (var repr : CollectionConverters.asJava(write.payload())) {
                     LOG.trace("doAsyncWriteMessages: id: {}: seqNr: {}, payload: {}", repr.persistenceId(),
                         repr.sequenceNr(), repr.payload());
 
                     addEntry(repr.persistenceId(), repr.sequenceNr(), repr.payload());
 
-                    WriteMessagesComplete complete = WRITE_MESSAGES_COMPLETE.get(repr.persistenceId());
+                    final var complete = WRITE_MESSAGES_COMPLETE.get(repr.persistenceId());
                     if (complete != null) {
                         if (complete.ofType == null || complete.ofType.equals(repr.payload().getClass())) {
                             complete.latch.countDown();
@@ -221,21 +218,21 @@ public class InMemoryJournal extends AsyncWriteJournal {
                 }
             }
 
-            return Collections.emptyList();
+            return List.of();
         }, context().dispatcher());
     }
 
     @Override
     public Future<Void> doAsyncDeleteMessagesTo(final String persistenceId, final long toSequenceNr) {
         LOG.trace("doAsyncDeleteMessagesTo: {}", toSequenceNr);
-        Map<Long, Object> journal = JOURNALS.get(persistenceId);
+        final var journal = JOURNALS.get(persistenceId);
         if (journal != null) {
             synchronized (journal) {
                 journal.keySet().removeIf(num -> num <= toSequenceNr);
             }
         }
 
-        CountDownLatch latch = DELETE_MESSAGES_COMPLETE_LATCHES.get(persistenceId);
+        final var latch = DELETE_MESSAGES_COMPLETE_LATCHES.get(persistenceId);
         if (latch != null) {
             latch.countDown();
         }
