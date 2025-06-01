ac6614776c30439168462ab2593e347571fa10ad
Robert Varga <robert.varga@pantheon.tech>
2025-06-01 16:12:21 +0200

    Add JournaledLogEntry
    SimpleReplicatedLogEntry has a leak of ReplicatedLogImpl's mechanics
    around asynchronous persistence.
    
    Introduce JournaledLogEntry to carry that semantics, leaving
    SimpleReplicatedLogEntry as the thing that we serialize in Pekko
    persistence.
    
    JIRA: CONTROLLER-2137
    Change-Id: Id6485040e0e9a43d6797919b9febacee94c1310f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/JournaledLogEntry.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.controller.cluster.raft.messages.Payload;
+import org.opendaylight.controller.cluster.raft.spi.LogEntry;
+
+/**
+ * A {@link ReplicatedLogEntry} implementation used by {@link ReplicatedLogImpl}.
+ */
+@NonNullByDefault
+final class JournaledLogEntry implements ReplicatedLogEntry {
+    private final long index;
+    private final long term;
+    private final Payload command;
+
+    private boolean persistencePending;
+
+    JournaledLogEntry(final long index, final long term, final Payload command) {
+        this.index = index;
+        this.term = term;
+        this.command = requireNonNull(command);
+    }
+
+    static JournaledLogEntry of(final LogEntry entry) {
+        return entry instanceof JournaledLogEntry simple && !simple.isPersistencePending() ? simple
+            : new JournaledLogEntry(entry.index(), entry.term(), entry.command().toSerialForm());
+    }
+
+    @Override
+    public Payload command() {
+        return command;
+    }
+
+    @Override
+    public long index() {
+        return index;
+    }
+
+    @Override
+    public long term() {
+        return term;
+    }
+
+    @Override
+    public int size() {
+        return command.size();
+    }
+
+    @Override
+    public int serializedSize() {
+        // Assumes WritableObjects.writeLongs() for index/term
+        return 17 + command.serializedSize();
+    }
+
+    @Override
+    public boolean isPersistencePending() {
+        return persistencePending;
+    }
+
+    /**
+     * Sets whether or not persistence is pending for this entry.
+     *
+     * @param pending the new setting.
+     */
+    void setPersistencePending(final boolean pending) {
+        persistencePending = pending;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, term, command);
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object obj) {
+        return this == obj || obj instanceof JournaledLogEntry other && index == other.index
+            && term == other.term && command.equals(other.command);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("index", index)
+            .add("term", term)
+            .add("command", command)
+            .toString();
+    }
+}
