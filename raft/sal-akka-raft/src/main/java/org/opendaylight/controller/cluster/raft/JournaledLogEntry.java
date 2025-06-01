a304e69bf619a7447ccb7a25c0233a658d1cbbae
Robert Varga <robert.varga@pantheon.tech>
2025-06-01 16:12:21 +0200

    WIP: Add JournaledLogEntry
    SimpleReplicatedLogEntry has a leak of ReplicatedLogImpl mechanics. This
    creates a new class to do that.
    
    JIRA: CONTROLLER-2137
    Change-Id: Id6485040e0e9a43d6797919b9febacee94c1310f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/JournaledLogEntry.java
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/JournaledLogEntry.java
@@ -5,50 +5,35 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.cluster.raft.persisted;
+package org.opendaylight.controller.cluster.raft;
 
 import static java.util.Objects.requireNonNull;
 
-import java.io.Serializable;
-import org.apache.commons.lang3.SerializationUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.controller.cluster.raft.ReplicatedLogEntry;
 import org.opendaylight.controller.cluster.raft.messages.Payload;
 import org.opendaylight.controller.cluster.raft.spi.LogEntry;
 
 /**
- * A {@link ReplicatedLogEntry} implementation.
- *
- * @author Thomas Pantelis
+ * A {@link ReplicatedLogEntry} implementation used by {@link ReplicatedLogImpl}.
  */
 @NonNullByDefault
-public final class SimpleReplicatedLogEntry implements ReplicatedLogEntry, Serializable {
-    @java.io.Serial
-    private static final long serialVersionUID = 1L;
-    // Estimate to how big the proxy is. Note this includes object stream overhead, so it is a bit conservative.
-    private static final int PROXY_SIZE = SerializationUtils.serialize(new LE((Void) null)).length;
-
+final class JournaledLogEntry implements ReplicatedLogEntry {
     private final long index;
     private final long term;
     private final Payload payload;
 
-    /**
-     * Constructs an instance.
-     *
-     * @param index the index
-     * @param term the term
-     * @param payload the payload
-     */
-    public SimpleReplicatedLogEntry(final long index, final long term, final Payload payload) {
+    private boolean persistencePending;
+
+    JournaledLogEntry(final long index, final long term, final Payload payload) {
         this.index = index;
         this.term = term;
         this.payload = requireNonNull(payload);
     }
 
-    public static SimpleReplicatedLogEntry of(final LogEntry entry) {
-        return entry instanceof SimpleReplicatedLogEntry simple && !simple.isPersistencePending() ? simple
-            : new SimpleReplicatedLogEntry(entry.index(), entry.term(), entry.command().toSerialForm());
+    static JournaledLogEntry of(final LogEntry entry) {
+        return entry instanceof JournaledLogEntry simple && !simple.isPersistencePending() ? simple
+            : new JournaledLogEntry(entry.index(), entry.term(), entry.command().toSerialForm());
     }
 
     @Override
@@ -73,12 +58,21 @@ public final class SimpleReplicatedLogEntry implements ReplicatedLogEntry, Seria
 
     @Override
     public int serializedSize() {
-        return PROXY_SIZE + payload.serializedSize();
+        return 17 + payload.serializedSize();
     }
 
     @Override
     public boolean isPersistencePending() {
-        return false;
+        return persistencePending;
+    }
+
+    /**
+     * Sets whether or not persistence is pending for this entry.
+     *
+     * @param pending the new setting.
+     */
+    void setPersistencePending(final boolean pending) {
+        persistencePending = pending;
     }
 
     @Override
@@ -93,17 +87,12 @@ public final class SimpleReplicatedLogEntry implements ReplicatedLogEntry, Seria
 
     @Override
     public boolean equals(final @Nullable Object obj) {
-        return this == obj || obj instanceof SimpleReplicatedLogEntry other && index == other.index
+        return this == obj || obj instanceof JournaledLogEntry other && index == other.index
             && term == other.term && payload.equals(other.payload);
     }
 
     @Override
     public String toString() {
-        return "SimpleReplicatedLogEntry [index=" + index + ", term=" + term + ", payload=" + payload + "]";
-    }
-
-    @java.io.Serial
-    private Object writeReplace() {
-        return new LE(this);
+        return "JournaledLogEntry [index=" + index + ", term=" + term + ", payload=" + payload + "]";
     }
 }
