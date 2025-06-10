e003f4c2b5cb808113f54ecb556549f6696f0305
Robert Varga <robert.varga@pantheon.tech>
2025-06-10 20:03:25 +0200

    Add EntryJournalV1
    We need to provide an alternative to Pekko persistence when it comes to
    EntryStore. This is a rather tall ask, given that the capabilities
    afforded to by RaftJournal lend themselves to a much more streamlined
    lifecycle.
    
    This patch splits out the raw interface to maintain LogEntry objects in
    a RaftJournal, along with the house keeping.
    
    JIRA: CONTROLLER-2137
    Change-Id: I4d7d3364a5422a8933420200265477051247a3e7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/test/java/org/opendaylight/controller/cluster/raft/spi/EntryJournalTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.spi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.opendaylight.controller.cluster.raft.MockCommand;
+import org.opendaylight.raft.spi.CompressionType;
+
+class EntryJournalTest {
+    @TempDir
+    private Path directory;
+
+    private EntryJournalV1 journal;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        journal = new EntryJournalV1("test", directory, CompressionType.NONE, false);
+    }
+
+    @AfterEach
+    void afterEach() {
+        journal.close();
+    }
+
+    @Test
+    void emptyReplay() throws Exception {
+        assertEquals(0, journal.applyTo());
+        try (var reader = journal.openReader()) {
+            assertEquals(1, reader.nextJournalIndex());
+            assertNull(reader.nextEntry());
+        }
+    }
+
+    @Test
+    void nonEmptyReplay() throws Exception {
+        final var entry1 = new DefaultLogEntry(3, 3, new MockCommand(""));
+        final var entry2 = new DefaultLogEntry(4, 3, new MockCommand("CAFEBABE".repeat(256 * 1024)));
+
+        assertEquals(1, journal.persistEntry(entry1));
+        assertEquals(2, journal.persistEntry(entry2));
+
+        journal.setApplyTo(2);
+        assertEquals(2, journal.applyTo());
+        journal.setReplayFrom(1);
+        journal.close();
+
+        assertTrue(Files.notExists(directory.resolve("entry-v1-0000000000000001")));
+        assertEquals(2_097_279, Files.size(directory.resolve("entry-v1-0000000000000002")));
+
+        journal = new EntryJournalV1("test", directory, CompressionType.NONE, false);
+        assertEquals(2, journal.applyTo());
+
+        try (var reader = journal.openReader()) {
+            assertEquals(1, reader.nextJournalIndex());
+            final var je1 = reader.nextEntry();
+            assertNotNull(je1);
+            assertEquals(entry1, je1.toLogEntry());
+
+            assertEquals(2, reader.nextJournalIndex());
+            final var je2 = reader.nextEntry();
+            assertNotNull(je2);
+            assertEquals(entry2, je2.toLogEntry());
+
+            assertEquals(3, reader.nextJournalIndex());
+            assertNull(reader.nextEntry());
+        }
+    }
+
+    @Test
+    void compressedEntry() throws Exception {
+        journal.close();
+        journal = new EntryJournalV1("test", directory, CompressionType.LZ4, false);
+
+        final var entry = new DefaultLogEntry(4, 3, new MockCommand("CAFEBABE".repeat(256 * 1024)));
+        assertEquals(1, journal.persistEntry(entry));
+
+        assertTrue(Files.notExists(directory.resolve("entry-v1-0000000000000001")));
+
+        try (var reader = journal.openReader()) {
+            assertEquals(1, reader.nextJournalIndex());
+            final var je = reader.nextEntry();
+            assertNotNull(je);
+            assertEquals(entry, je.toLogEntry());
+
+            assertEquals(2, reader.nextJournalIndex());
+            assertNull(reader.nextEntry());
+        }
+    }
+}
