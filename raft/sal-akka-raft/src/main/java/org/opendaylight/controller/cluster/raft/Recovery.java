853246f958e76507bdba3e6bef677b840c343141
Robert Varga <robert.varga@pantheon.tech>
2025-06-23 13:36:48 +0200

    WIP: Introduce JournalRecovery
    Once we have run Pekko recovery protocol, we need to reconcile/recover
    entries contained in EntryJournal. This patch adds JournalRecovery to
    perform that task.
    
    JIRA: CONTROLLER-2137
    Change-Id: If9f29e9bbe332d18bf88698133310284ef136649
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/Recovery.java
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/Recovery.java
@@ -28,7 +28,7 @@ import org.slf4j.LoggerFactory;
  *
  * @param <T> {@link State} type
  */
-abstract sealed class Recovery<T extends @NonNull State> permits PekkoRecovery {
+abstract sealed class Recovery<T extends @NonNull State> permits JournalRecovery, PekkoRecovery {
     private static final Logger LOG = LoggerFactory.getLogger(Recovery.class);
 
     final @NonNull RaftActor actor;
