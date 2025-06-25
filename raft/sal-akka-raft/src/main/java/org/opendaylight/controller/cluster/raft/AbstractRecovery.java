879afb73522ff509dc0ab9505c0b419a81f94e75
Robert Varga <robert.varga@pantheon.tech>
2025-06-25 14:46:03 +0200

    Split out Recovery
    PekkoRecovery deals only with the contents of SnapshotStore and Pekko
    persistence. We will also need to run a similar (but different) process
    for EntryJournal-based EntryStore.
    
    Split out the basic blocks of PekkoRecovery into an abstract superclass.
    
    JIRA: CONTROLLER-2137
    Change-Id: I7ea4ce94fa2c9dc1a052178d4d314abe824bb08c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/AbstractRecovery.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Stopwatch;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.controller.cluster.raft.persisted.MigratedSerializable;
+import org.opendaylight.controller.cluster.raft.persisted.Snapshot.State;
+import org.opendaylight.controller.cluster.raft.spi.LogEntry;
+import org.opendaylight.controller.cluster.raft.spi.SnapshotStore;
+
+/**
+ * Base class for recovery implementations.
+ *
+ * @param <T> {@link State} type
+ */
+abstract sealed class AbstractRecovery<T extends @NonNull State> permits JournalRecovery, PekkoRecovery {
+    final @NonNull RaftActor actor;
+    final @NonNull RaftActorSnapshotCohort<T> snapshotCohort;
+    final @NonNull RaftActorRecoveryCohort recoveryCohort;
+    final @NonNull PekkoReplicatedLog pekkoLog;
+    final int snapshotInterval;
+    final int batchSize;
+
+    private Stopwatch recoveryTimer;
+    // FIXME: hide this field
+    Stopwatch snapshotTimer;
+
+    @NonNullByDefault
+    AbstractRecovery(final RaftActor actor, final RaftActorSnapshotCohort<T> snapshotCohort,
+            final RaftActorRecoveryCohort recoveryCohort, final ConfigParams configParams) {
+        this.actor = requireNonNull(actor);
+        this.snapshotCohort = requireNonNull(snapshotCohort);
+        this.recoveryCohort = requireNonNull(recoveryCohort);
+        snapshotInterval = configParams.getRecoverySnapshotIntervalSeconds();
+        batchSize = configParams.getJournalRecoveryLogBatchSize();
+        pekkoLog = new PekkoReplicatedLog(actor.memberId());
+    }
+
+    @NonNullByDefault
+    final String memberId() {
+        return actor.memberId();
+    }
+
+    @NonNullByDefault
+    final SnapshotStore snapshotStore() {
+        return actor.persistence().snapshotStore();
+    }
+
+    @VisibleForTesting
+    final @NonNull PekkoReplicatedLog pekkoLog() {
+        return pekkoLog;
+    }
+
+    final void startRecoveryTimers() {
+        if (recoveryTimer == null) {
+            recoveryTimer = Stopwatch.createStarted();
+        }
+        if (snapshotTimer == null && snapshotInterval > 0) {
+            snapshotTimer = Stopwatch.createStarted();
+        }
+    }
+
+    @NonNullByDefault
+    final String stopRecoveryTimers() {
+        final String recoveryTime;
+        if (recoveryTimer != null) {
+            recoveryTime = " in " + recoveryTimer.stop();
+            recoveryTimer = null;
+        } else {
+            recoveryTime = "";
+        }
+
+        if (snapshotTimer != null) {
+            snapshotTimer.stop();
+            snapshotTimer = null;
+        }
+        return recoveryTime;
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("memberId", memberId()).toString();
+    }
+
+    @NonNullByDefault
+    static final boolean isMigratedPayload(final LogEntry logEntry) {
+        return isMigratedSerializable(logEntry.command().toSerialForm());
+    }
+
+    @NonNullByDefault
+    static final boolean isMigratedSerializable(final Object message) {
+        return message instanceof MigratedSerializable migrated && migrated.isMigrated();
+    }
+}
