a71400af3fe880b64dc4d662ea8d8edc3c80cded
Robert Varga <robert.varga@pantheon.tech>
2025-07-04 20:38:30 +0200

    Remove RaftActorTest
    We have RaftActorTestKit to hold utility methods. Move them there and
    improve their effectiveness.
    
    This sprawls int raft.client.messages, where we mark up the evolution of
    FindLeaderReply.
    
    JIRA: CONTROLLER-2137
    Change-Id: Id85fe4a1513f9ad32e09da14c021f1fe8b36c11b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/client/messages/FindLeaderReply.java
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/client/messages/FindLeaderReply.java
@@ -8,27 +8,64 @@
 package org.opendaylight.controller.cluster.raft.client.messages;
 
 import com.google.common.annotations.VisibleForTesting;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.Serializable;
 import java.util.Optional;
+import org.apache.pekko.actor.ActorPath;
 import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * Reply to {@link FindLeader} message, containing the address of the leader actor, as known to the raft actor which
- * sent the message. If the responding actor does not have knowledge of the leader, {@link #getLeaderActor()} will
- * return {@link Optional#empty()}.
+ * sent the message. If the responding actor does not have knowledge of the leader, {@link #leaderActorPath()} will
+ * return {@code null}.
  *
  * <p>This message is intended for testing purposes only.
  */
+// FIXME: 12.0.0, 11.0.x: introduce a 'record LRv1(@Nullable String leaderActo) serialization proxy
+// FIXME: 13.0.x: make this class a record and reject attempts to deserialize
+// FIXME: we do not want to be tied to ActorPath: at the end of the day we are talking to well-known entity behind
+//        an HTTP/3 connection maintained somewhere.
 @VisibleForTesting
 public final class FindLeaderReply implements Serializable {
+    @java.io.Serial
     private static final long serialVersionUID = 1L;
-    private final String leaderActor;
 
-    public FindLeaderReply(final @Nullable String leaderActor) {
-        this.leaderActor = leaderActor;
+    private final @Nullable String leaderActorPath;
+
+    /**
+     * Default constructor. Takes an {@link ActorPath} in its string form.
+     *
+     * @param leaderActorPath an {@link ActorPath#toString()}
+     */
+    public FindLeaderReply(final @Nullable String leaderActorPath) {
+        this.leaderActorPath = leaderActorPath;
     }
 
-    public Optional<String> getLeaderActor() {
-        return Optional.ofNullable(leaderActor);
+    /**
+     * Convenience constructor translating an {@link ActorPath} to its string form.
+     *
+     * @param leaderActorPath the optional {@link ActorPath}
+     */
+    public FindLeaderReply(final @Nullable ActorPath leaderActorPath) {
+        this(leaderActorPath != null ? leaderActorPath.toString() : null);
+    }
+
+    /**
+     * {@return the actor reference to the leader, or {@code null} if no leader is known}
+     */
+    public @Nullable String leaderActorPath() {
+        return leaderActorPath;
+    }
+
+    /**
+     * return the actor reference to the leader, or empty if no leader is known.
+     *
+     * @return the actor reference to the leader, or empty if no leader is known
+     * @deprecated Use {@link #leaderActorPath()} instead.
+     */
+    // Use leaderActorRef
+    @Deprecated(since = "1.0.0", forRemoval = true)
+    public @NonNull Optional<String> getLeaderActor() {
+        return Optional.ofNullable(leaderActorPath);
     }
 }
