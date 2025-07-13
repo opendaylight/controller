36b15b55eef321f45ef4bb53201a004e67a8efab
Robert Varga <robert.varga@pantheon.tech>
2025-07-13 17:19:11 +0200

    Use constructor injection
    Rather than having dynamic refreshes, let's opt for having the
    introspector torn down and restarted when the serializar changes. Also
    activate the service only when needed.
    
    JIRA: CONTROLLER-2147
    Change-Id: I44f7dc1133144ceeb8dbca72dec0e004aed2e626
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/OSGiDatastoreContextIntrospectorFactory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/OSGiDatastoreContextIntrospectorFactory.java
@@ -7,8 +7,6 @@
  */
 package org.opendaylight.controller.cluster.datastore;
 
-import static com.google.common.base.Verify.verifyNotNull;
-
 import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSerializer;
 import org.osgi.service.component.annotations.Activate;
@@ -19,21 +17,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Beta
-@Component(immediate = true, service = DatastoreContextIntrospectorFactory.class)
+@Component(service = DatastoreContextIntrospectorFactory.class)
 public final class OSGiDatastoreContextIntrospectorFactory extends AbstractDatastoreContextIntrospectorFactory {
     private static final Logger LOG = LoggerFactory.getLogger(OSGiDatastoreContextIntrospectorFactory.class);
 
-    @Reference
-    volatile BindingNormalizedNodeSerializer serializer = null;
-
-    @Override
-    BindingNormalizedNodeSerializer serializer() {
-        return verifyNotNull(serializer);
-    }
-
     @Activate
-    @SuppressWarnings("static-method")
-    void activate() {
+    public OSGiDatastoreContextIntrospectorFactory(@Reference final BindingNormalizedNodeSerializer serializer) {
+        super(serializer);
         LOG.info("Datastore Context Introspector activated");
     }
 
