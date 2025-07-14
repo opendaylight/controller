5890b382f5819d554d5c6ad032d66a150058e2fc
Robert Varga <robert.varga@pantheon.tech>
2025-07-14 22:55:52 +0200

    Add RestrictedObjectStreams
    This class encapsulates a class loading strategy delegating to an
    explicit set of ClassLoaders (plus the platform class loader).
    
    JIRA: CONTROLLER-2153
    Change-Id: I427ec697044a87f1999c5fc79d0e78c99089310e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 2da3c7637404e826889ee5e740fe5e2a5ee7f8f9)

---
+++ raft/raft-spi/src/main/java/org/opendaylight/raft/spi/RestrictedObjectInputStream.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.raft.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+import java.lang.reflect.Proxy;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * An {@link ObjectInputStream} which is bounded in its loading to a {@link RestrictedClassLoader}.
+ */
+final class RestrictedObjectInputStream extends ObjectInputStream {
+    private final RestrictedClassLoader classLoader;
+
+    @NonNullByDefault
+    RestrictedObjectInputStream(final InputStream in, final RestrictedClassLoader classLoader) throws IOException {
+        super(requireNonNull(in));
+        this.classLoader = requireNonNull(classLoader);
+    }
+
+    @Override
+    protected Class<?> resolveClass(final ObjectStreamClass objectStreamClass) throws ClassNotFoundException {
+        return classLoader.loadClass(objectStreamClass.getName());
+    }
+
+    @Override
+    protected Class<?> resolveProxyClass(final String[] interfaces) throws ClassNotFoundException {
+        final var length = interfaces.length;
+        final var classes = new Class<?>[length];
+        for (int i = 0; i < length; i++) {
+            classes[i] = classLoader.loadClass(interfaces[i]);
+        }
+
+        try {
+            return Proxy.getProxyClass(classLoader, classes);
+        } catch (IllegalArgumentException e) {
+            throw new ClassNotFoundException(null, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("classLoader", classLoader).toString();
+    }
+}
