f8ac5f816e182f5ac3fe0621565d93b0ef6a43b8
Robert Varga <robert.varga@pantheon.tech>
2025-08-21 15:58:13 +0200

    Do not use InMemorySnapshotStore in AbstractShardTest
    Introduce a utility RaftStorage.saveSnapshot() method and use that to
    initialize the datastore snapshot.
    
    JIRA: CONTROLLER-2073
    Change-Id: I537366782cb7988aa81e999d26839f5831bc96db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/JsonExportTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/datastore/JsonExportTest.java
@@ -34,10 +34,8 @@ public class JsonExportTest extends AbstractShardTest {
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @Override
     @Before
     public void setUp() throws Exception {
-        super.setUp();
         final var exportTmpFolder = temporaryFolder.newFolder("persistence-export");
         actualJournalFilePath = exportTmpFolder.getAbsolutePath() + "/journals/"
             + "member-1-shard-inventory-config" + nextShardNum + "-journal.json";
