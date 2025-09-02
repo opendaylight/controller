855e4736d4c072ab7f1814bbda6151e9de45bf60
Robert Varga <robert.varga@pantheon.tech>
2025-09-02 09:11:52 +0200

    AbstractClientActor should not be persistent
    JIRA: CONTROLLER-XYZ
    Change-Id: Icffc65f6aee83cc47ab745575d382d3e2b27916b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActor.java
+++ opendaylight/md-sal/cds-access-client/src/main/java/org/opendaylight/controller/cluster/access/client/AbstractClientActor.java
@@ -7,24 +7,49 @@
  */
 package org.opendaylight.controller.cluster.access.client;
 
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.nio.file.StandardOpenOption.SYNC;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.VisibleForTesting;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
+import org.apache.pekko.actor.AbstractActor;
 import org.apache.pekko.actor.ActorRef;
 import org.apache.pekko.actor.PoisonPill;
-import org.apache.pekko.persistence.AbstractPersistentActor;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
 import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
+import org.opendaylight.controller.cluster.access.concepts.FrontendType;
+import org.opendaylight.controller.cluster.access.concepts.MemberName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Frontend actor which takes care of persisting generations and creates an appropriate ClientIdentifier.
  */
-public abstract class AbstractClientActor extends AbstractPersistentActor {
+public abstract class AbstractClientActor extends AbstractActor {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractClientActor.class);
     private static final Path STATE_PATH = Path.of("state");
+    /*
+     * Base for the property name which overrides the initial generation when we fail to find anything from persistence.
+     * The actual property name has the frontend type name appended.
+     */
+    private static final String GENERATION_OVERRIDE_PROP_BASE =
+            "org.opendaylight.controller.cluster.access.client.initial.generation.";
+    private static final String PROP_MEMBER_NAME = "member-name";
+    private static final String PROP_CLIENT_TYPE = "client-type";
+    private static final String PROP_GENERATION = "generation";
 
+    private final @NonNull FrontendIdentifier frontendId;
     private final @NonNull String persistenceId;
+    private final @NonNull Path statePath;
 
     private AbstractClientActorBehavior<?> currentBehavior;
 
@@ -33,23 +58,50 @@ public abstract class AbstractClientActor extends AbstractPersistentActor {
     }
 
     @VisibleForTesting
-    protected AbstractClientActor(final Path statePath, final FrontendIdentifier frontendId) {
+    AbstractClientActor(final Path statePath, final FrontendIdentifier frontendId) {
+        this.statePath = requireNonNull(statePath);
+        this.frontendId = requireNonNull(frontendId);
         persistenceId = frontendId.toPersistentId();
-        currentBehavior = new RecoveringClientActorBehavior(statePath, this, persistenceId, frontendId);
     }
 
-    @Override
-    public final String persistenceId() {
+    public final @NonNull String persistenceId() {
         return persistenceId;
     }
 
     @Override
-    public void postStop() throws Exception {
-        if (currentBehavior != null) {
-            currentBehavior.close();
+    public final void preStart() throws IOException, RecoveryException {
+        final var filePath = statePath.resolve("odl.cluster.client")
+            .resolve(frontendId.getMemberName().getName())
+            .resolve(frontendId.getClientType().getName() + ".properties");
+
+        final ClientIdentifier clientId;
+        final var fromFile = loadStateFile(filePath);
+        if (fromFile != null) {
+            LOG.debug("{}: recovered identifier {} from {}", persistenceId, fromFile, filePath);
+
+            // increment generation and refuse to wraparound
+            final var nextGeneration = fromFile.getGeneration() + 1;
+            if (nextGeneration == 0) {
+                throw new RecoveryException("Generation counter exhausted for %s", frontendId);
+            }
+            clientId = ClientIdentifier.create(frontendId, nextGeneration);
+        } else {
+            clientId = ClientIdentifier.create(frontendId, initialGeneration());
         }
 
-        super.postStop();
+        LOG.debug("{}: saving new identifier {} to {}", persistenceId, clientId, filePath);
+        createStateFile(filePath, clientId);
+
+        currentBehavior = initialBehavior(new ClientActorContext(self(), persistenceId(), getContext().system(),
+            clientId, getClientActorConfig()));
+    }
+
+    @Override
+    public final void postStop() {
+        if (currentBehavior != null) {
+            currentBehavior.close();
+            currentBehavior = null;
+        }
     }
 
     private void switchBehavior(final AbstractClientActorBehavior<?> nextBehavior) {
@@ -67,13 +119,8 @@ public abstract class AbstractClientActor extends AbstractPersistentActor {
     }
 
     @Override
-    public Receive createReceive() {
+    public final Receive createReceive() {
         return receiveBuilder().matchAny(this::onReceiveCommand).build();
-    }
-
-    @Override
-    public Receive createReceiveRecover() {
-        return receiveBuilder().matchAny(this::onReceiveRecover).build();
     }
 
     private void onReceiveCommand(final Object command) {
@@ -89,11 +136,99 @@ public abstract class AbstractClientActor extends AbstractPersistentActor {
         }
     }
 
-    private void onReceiveRecover(final Object recover) {
-        switchBehavior(currentBehavior.onReceiveRecover(recover));
-    }
-
     protected abstract ClientActorBehavior<?> initialBehavior(ClientActorContext context);
 
     protected abstract ClientActorConfig getClientActorConfig();
+
+    private long initialGeneration() {
+        final String propName = GENERATION_OVERRIDE_PROP_BASE + frontendId.getClientType().getName();
+        final String propValue = System.getProperty(propName);
+        if (propValue == null) {
+            LOG.debug("{}: no initial generation override, starting from 0", persistenceId());
+            return 0;
+        }
+
+        final long ret;
+        try {
+            ret = Long.parseUnsignedLong(propValue);
+        } catch (NumberFormatException e) {
+            LOG.warn("{}: failed to parse initial generation override '{}', starting from 0", persistenceId(),
+                propValue, e);
+            return 0;
+        }
+
+        LOG.info("{}: initial generation set to {}", persistenceId(), ret);
+        return ret;
+    }
+
+    private static void createStateFile(final Path path, final ClientIdentifier clientId) throws IOException {
+        final var props = new Properties();
+        final var frontendId = clientId.getFrontendId();
+        props.setProperty(PROP_MEMBER_NAME, frontendId.getMemberName().getName());
+        props.setProperty(PROP_CLIENT_TYPE, frontendId.getClientType().getName());
+        props.setProperty(PROP_GENERATION, Long.toUnsignedString(clientId.getGeneration()));
+
+        createStateFile(path, props);
+    }
+
+    private static void createStateFile(final Path path, final Properties props) throws IOException {
+        final var parent = path.getParent();
+        Files.createDirectories(parent);
+
+        final var temp = Files.createTempFile(parent, "cds-id", null);
+
+        try {
+            try (var os = Files.newOutputStream(temp, WRITE, TRUNCATE_EXISTING, SYNC)) {
+                props.store(os, "Critical persistent state. Do not touch unless you know what you are doing!");
+            }
+
+            Files.move(temp, path, ATOMIC_MOVE, REPLACE_EXISTING);
+        } finally {
+            try {
+                Files.deleteIfExists(temp);
+            } catch (IOException e) {
+                LOG.warn("Failed to delete {}", temp, e);
+            }
+        }
+    }
+
+    private @Nullable ClientIdentifier loadStateFile(final Path filePath) throws IOException, RecoveryException {
+        if (!Files.exists(filePath)) {
+            return null;
+        }
+
+        final var props = new Properties();
+        try (var is = Files.newInputStream(filePath)) {
+            props.load(is);
+        } catch (IllegalArgumentException e) {
+            throw new RecoveryException(e, "Failed to load %s", filePath);
+        }
+
+        final var loadedFrontendId = FrontendIdentifier.create(
+            MemberName.forName(requireProp(props, PROP_MEMBER_NAME, filePath)),
+            FrontendType.forName(requireProp(props, PROP_CLIENT_TYPE, filePath)));
+        if (!frontendId.equals(loadedFrontendId)) {
+            throw new RecoveryException("Mismatched frontend identifier: current: %s saved: %s", frontendId,
+                loadedFrontendId);
+        }
+
+        final var generationStr = requireProp(props, PROP_GENERATION, filePath);
+        final long generation;
+        try {
+            generation = Long.parseUnsignedLong(generationStr);
+        } catch (NumberFormatException e) {
+            throw new RecoveryException(e, "%s contains illegal generation %s", filePath, generationStr);
+        }
+
+        return ClientIdentifier.create(loadedFrontendId, generation);
+    }
+
+    private static String requireProp(final Properties props, final String key, final Path path)
+            throws RecoveryException {
+        final var value = props.getProperty(key);
+        if (value == null) {
+            throw new RecoveryException("%s is missing property %s", path, key);
+        }
+        return value;
+    }
 }
--- opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ActorBehaviorTest.java
+++ opendaylight/md-sal/cds-access-client/src/test/java/org/opendaylight/controller/cluster/access/client/ActorBehaviorTest.java
@@ -7,29 +7,19 @@
  */
 package org.opendaylight.controller.cluster.access.client;
 
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
-import com.typesafe.config.ConfigFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import org.apache.pekko.actor.ActorRef;
 import org.apache.pekko.actor.ActorSystem;
 import org.apache.pekko.actor.Props;
-import org.apache.pekko.persistence.Persistence;
-import org.apache.pekko.persistence.SelectedSnapshot;
-import org.apache.pekko.persistence.SnapshotMetadata;
-import org.apache.pekko.testkit.TestProbe;
 import org.apache.pekko.testkit.javadsl.TestKit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,20 +29,13 @@ import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.opendaylight.controller.cluster.access.client.MockedSnapshotStore.DeleteByCriteriaRequest;
-import org.opendaylight.controller.cluster.access.client.MockedSnapshotStore.DeleteByMetadataRequest;
-import org.opendaylight.controller.cluster.access.client.MockedSnapshotStore.LoadRequest;
-import org.opendaylight.controller.cluster.access.client.MockedSnapshotStore.SaveRequest;
-import org.opendaylight.controller.cluster.access.concepts.ClientIdentifier;
 import org.opendaylight.controller.cluster.access.concepts.FrontendIdentifier;
 import org.opendaylight.controller.cluster.access.concepts.FrontendType;
 import org.opendaylight.controller.cluster.access.concepts.MemberName;
-import scala.concurrent.duration.FiniteDuration;
 
 @ExtendWith(MockitoExtension.class)
 class ActorBehaviorTest {
     private static final String MEMBER_1_FRONTEND_TYPE_1 = "member-1-frontend-type-1";
-    private static final FiniteDuration TIMEOUT = FiniteDuration.create(5, TimeUnit.SECONDS);
 
     private final FrontendIdentifier id =
         FrontendIdentifier.create(MemberName.forName("member-1"), FrontendType.forName("type-1"));
@@ -67,7 +50,6 @@ class ActorBehaviorTest {
     private AbstractClientActorContext ctx;
 
     private ActorSystem system;
-    private TestProbe probe;
     private ActorRef mockedActor;
 
     @BeforeEach
@@ -82,10 +64,6 @@ class ActorBehaviorTest {
         persistenceId.set(ctx, MEMBER_1_FRONTEND_TYPE_1);
 
         system = ActorSystem.apply("system1");
-        final var storeRef = ((Persistence) system.registerExtension(Persistence.lookup()))
-            .snapshotStoreFor(null, ConfigFactory.empty());
-        probe = new TestProbe(system);
-        storeRef.tell(probe.ref(), ActorRef.noSender());
 
         mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
     }
@@ -97,143 +75,10 @@ class ActorBehaviorTest {
 
     @Test
     void testInitialBehavior() {
-        recoverInitial();
-
         doReturn(initialBehavior).when(cmd).execute(any());
         mockedActor.tell(cmd, ActorRef.noSender());
         verify(cmd, timeout(1000)).execute(initialBehavior);
-    }
-
-    @Test
-    void testCommandStashing() {
-        recoverInitial();
-
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
-        doReturn(initialBehavior).when(cmd).execute(any());
-        //send messages before recovery is completed
-        mockedActor.tell(cmd, ActorRef.noSender());
-        mockedActor.tell(cmd, ActorRef.noSender());
-        mockedActor.tell(cmd, ActorRef.noSender());
-        //complete recovery
-        assertTombstone(1, handleRecovery(null));
-        verifyStateFile(1);
-
-        verify(cmd, timeout(1000).times(3)).execute(initialBehavior);
-    }
-
-    @Test
-    void testRecoveryAfterRestart() {
-        final var saveRequest = recoverInitial();
-
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
-
-        probe.expectMsgClass(LoadRequest.class);
-        //offer snapshot
-        probe.reply(Optional.ofNullable(new SelectedSnapshot(saveRequest.getMetadata(), saveRequest.getSnapshot())));
-
-        // state should be updated
-        await().atMost(Duration.ofSeconds(5)).untilAsserted(() -> verifyStateFile(1));
-
-        // there should be no further save
-        probe.expectNoMessage();
-    }
-
-    @Test
-    void testRecoveryAfterRestartFrontendIdMismatch() {
-        final var saveRequest = recoverInitial();
-
-        system.stop(mockedActor);
-        //start actor again
-        mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
-        probe.expectMsgClass(LoadRequest.class);
-        //offer snapshot with incorrect client id
-        final var metadata = saveRequest.getMetadata();
-        final var anotherFrontend = FrontendIdentifier.create(MemberName.forName("another"),
-                FrontendType.forName("type-2"));
-        final var incorrectClientId = ClientIdentifier.create(anotherFrontend, 0);
-        probe.watch(mockedActor);
-        probe.reply(Optional.of(new SelectedSnapshot(metadata, incorrectClientId)));
-        //actor should be stopped
-        probe.expectTerminated(mockedActor, TIMEOUT);
         verifyStateFile(0);
-    }
-
-    @Test
-    void testRecoveryAfterRestartSaveSnapshotFail() {
-        recoverInitial();
-
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
-        probe.watch(mockedActor);
-        probe.expectMsgClass(LoadRequest.class);
-        probe.reply(Optional.empty());
-
-        assertTombstone(1);
-
-        probe.reply(new RuntimeException("save failed"));
-        probe.expectMsgClass(DeleteByMetadataRequest.class);
-        probe.expectTerminated(mockedActor, TIMEOUT);
-        verifyStateFile(1);
-    }
-
-    @Test
-    void testRecoveryAfterRestartDeleteSnapshotsFail() {
-        recoverInitial();
-
-        system.stop(mockedActor);
-        mockedActor = system.actorOf(MockedActor.props(statePath, id, initialBehavior));
-        probe.watch(mockedActor);
-        probe.expectMsgClass(LoadRequest.class);
-        probe.reply(Optional.empty());
-
-        assertTombstone(1);
-
-        probe.reply(Void.TYPE);
-        probe.expectMsgClass(DeleteByCriteriaRequest.class);
-        probe.reply(new RuntimeException("delete failed"));
-        //actor shouldn't terminate
-        probe.expectNoMessage();
-        verifyStateFile(1);
-    }
-
-    @Test
-    void testMigration() {
-        final var saveRequest = handleRecovery(
-            new SelectedSnapshot(new SnapshotMetadata(MEMBER_1_FRONTEND_TYPE_1, 0, 0), ClientIdentifier.create(id, 5)));
-        assertTombstone(6, saveRequest);
-        verifyStateFile(6);
-    }
-
-    private SaveRequest recoverInitial() {
-        //handle initial actor recovery
-        final var saveRequest = handleRecovery(null);
-        assertTombstone(0, saveRequest);
-        verifyStateFile(0);
-        return saveRequest;
-    }
-
-    private SaveRequest handleRecovery(final SelectedSnapshot savedState) {
-        probe.expectMsgClass(LoadRequest.class);
-        //offer snapshot
-        probe.reply(Optional.ofNullable(savedState));
-        final var nextSaveRequest = probe.expectMsgClass(SaveRequest.class);
-        probe.reply(Void.TYPE);
-        //check old snapshots deleted
-        probe.expectMsgClass(DeleteByCriteriaRequest.class);
-        probe.reply(Void.TYPE);
-        return nextSaveRequest;
-    }
-
-    private void assertTombstone(final long expectedGeneration) {
-        assertTombstone(expectedGeneration, probe.expectMsgClass(SaveRequest.class));
-    }
-
-    private void assertTombstone(final long expectedGeneration, final SaveRequest save) {
-        final var clientId = assertInstanceOf(PersistenceTombstone.class, save.getSnapshot()).clientId();
-        assertEquals(id, clientId.getFrontendId());
-        assertEquals(expectedGeneration, clientId.getGeneration());
     }
 
     private void verifyStateFile(final long expectedGeneration) {
