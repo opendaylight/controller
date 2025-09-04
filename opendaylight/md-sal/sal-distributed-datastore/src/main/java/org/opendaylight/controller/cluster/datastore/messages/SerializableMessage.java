18d5fd71c50a6385ac5a8e98116962eeb8d8fa00
Robert Varga <robert.varga@pantheon.tech>
2025-09-04 09:57:01 +0200

    Deprecate datastore.messages.SerializableMessage
    This interface is not used anywhere, deprecate it for removal.
    
    Change-Id: I18a613297fad8a26a5b740f994c0a78a2d9341d8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit d7ef99cfc0cfd3bfa3eaeb4592d820a2d7304bcc)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/SerializableMessage.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/messages/SerializableMessage.java
@@ -5,9 +5,10 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.controller.cluster.datastore.messages;
 
+@Deprecated(since = "11.0.2", forRemoval = true)
 public interface SerializableMessage {
+
     Object toSerializable();
 }
