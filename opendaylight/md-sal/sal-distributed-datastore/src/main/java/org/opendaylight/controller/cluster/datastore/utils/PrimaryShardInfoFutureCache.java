09b62f3e694b26e50fc0d707c1e2e7d8b29ab982
Robert Varga <robert.varga@pantheon.tech>
2025-11-07 00:18:56 +0100

    FIXME: to split
    JIRA: CONTROLLER-2180
    
    Change-Id: I1ea9ca88b2338e5aada4503ab38b0605937ffca8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/PrimaryShardInfoFutureCache.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/PrimaryShardInfoFutureCache.java
@@ -9,11 +9,11 @@ package org.opendaylight.controller.cluster.datastore.utils;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import org.apache.pekko.dispatch.Futures;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.controller.cluster.datastore.messages.PrimaryShardInfo;
-import scala.concurrent.Future;
 
 /**
  * Maintains a cache of PrimaryShardInfo Future instances per shard.
@@ -21,17 +21,19 @@ import scala.concurrent.Future;
  * @author Thomas Pantelis
  */
 public class PrimaryShardInfoFutureCache {
-    private final Cache<String, Future<PrimaryShardInfo>> primaryShardInfoCache = CacheBuilder.newBuilder().build();
+    private final Cache<String, CompletionStage<PrimaryShardInfo>> primaryShardInfoCache =
+        CacheBuilder.newBuilder().build();
 
-    public @Nullable Future<PrimaryShardInfo> getIfPresent(@NonNull String shardName) {
+    public @Nullable CompletionStage<PrimaryShardInfo> getIfPresent(final @NonNull String shardName) {
         return primaryShardInfoCache.getIfPresent(shardName);
     }
 
-    public void putSuccessful(@NonNull String shardName, @NonNull PrimaryShardInfo info) {
-        primaryShardInfoCache.put(shardName, Futures.successful(info));
+    public void putSuccessful(final @NonNull String shardName, final @NonNull PrimaryShardInfo info) {
+        primaryShardInfoCache.put(shardName, CompletableFuture.completedStage(info));
     }
 
-    public void remove(@NonNull String shardName) {
+    public void remove(@NonNull
+    final String shardName) {
         primaryShardInfoCache.invalidate(shardName);
     }
 }
