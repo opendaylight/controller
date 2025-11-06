afc0c64ccb606ce5fc855dff57cba6dcd37dfb0b
Robert Varga <robert.varga@pantheon.tech>
2025-11-06 17:37:03 +0100

    Count all AskTimeoutExceptions in ActorUtils
    The ActorRef-based ask() method does not contribute to counting
    AskTimeoutExceptions. Fix that.
    
    JIRA: CONTROLLER-2183
    Change-Id: I15efdbe45975073f4edccf958eb740e442851c3b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 8b10340a10beb678c5addfd8c05f89103c264463)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/ActorUtils.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/utils/ActorUtils.java
@@ -501,7 +501,9 @@ public class ActorUtils {
     }
 
     protected Future<Object> doAsk(final ActorRef actorRef, final Object message, final Timeout timeout) {
-        return Patterns.ask(actorRef, message, timeout);
+        final var ret = Patterns.ask(actorRef, message, timeout);
+        ret.onComplete(askTimeoutCounter, askTimeoutCounter);
+        return ret;
     }
 
     protected Future<Object> doAsk(final ActorSelection actorRef, final Object message, final Timeout timeout) {
