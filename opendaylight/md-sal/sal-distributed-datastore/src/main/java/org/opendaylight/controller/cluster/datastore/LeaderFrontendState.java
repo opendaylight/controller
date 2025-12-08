01dee1bcc223ce6a54b4de04ef80498a7b193a5d
Robert Varga <robert.varga@pantheon.tech>
2025-12-08 18:46:42 +0100

    Eliminate LeaderFrontendState.Disabled
    We are not using LeaderFrontendState.Disabled anymore, so eliminate it
    and fold LeaderFrontendState.Enabled into LeaderFrontendState.
    
    Unfortunately SpotBugs does not grok the combination of @Nullable and
    @NonNullByDefault, so we have to annotate individual parameters.
    
    Change-Id: I165bd572e82b8f818db38dc6f59bedacf8783b97
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 065864cddd0463568e6b28d9163001d8f2339252)

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/LeaderFrontendState.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/LeaderFrontendState.java
@@ -10,7 +10,6 @@ package org.opendaylight.controller.cluster.datastore;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.base.MoreObjects.ToStringHelper;
 import java.util.HashMap;
 import java.util.Map;
 import org.eclipse.jdt.annotation.NonNull;
@@ -40,203 +39,24 @@ import org.slf4j.LoggerFactory;
  * Frontend state as observed by the shard leader. This class is responsible for tracking generations and sequencing
  * in the frontend/backend conversation. This class is NOT thread-safe.
  */
-abstract sealed class LeaderFrontendState implements Identifiable<ClientIdentifier> {
-    static final class Disabled extends LeaderFrontendState {
-        Disabled(final String persistenceId, final ClientIdentifier clientId, final ShardDataTree tree) {
-            super(persistenceId, clientId, tree);
-        }
-
-        @Override
-        LocalHistorySuccess handleLocalHistoryRequest(final LocalHistoryRequest<?> request,
-                final RequestEnvelope envelope, final long now) throws RequestException {
-            throw new UnsupportedRequestException(request);
-        }
-
-        @Override
-        TransactionSuccess<?> handleTransactionRequest(final TransactionRequest<?> request,
-                final RequestEnvelope envelope, final long now) throws RequestException {
-            throw new UnsupportedRequestException(request);
-        }
-    }
-
-    static final class Enabled extends LeaderFrontendState {
-        // Histories which have not been purged
-        private final Map<LocalHistoryIdentifier, LocalFrontendHistory> localHistories;
-
-        // UnsignedLongSet performs automatic merging, hence we keep minimal state tracking information
-        private final MutableUnsignedLongSet purgedHistories;
-
-        // Used for all standalone transactions
-        private final AbstractFrontendHistory standaloneHistory;
-
-        private long expectedTxSequence;
-        private Long lastSeenHistory = null;
-
-        Enabled(final String persistenceId, final ClientIdentifier clientId, final ShardDataTree tree) {
-            this(persistenceId, clientId, tree, MutableUnsignedLongSet.of(),
-                StandaloneFrontendHistory.create(persistenceId, clientId, tree), new HashMap<>());
-        }
-
-        Enabled(final String persistenceId, final ClientIdentifier clientId, final ShardDataTree tree,
-                final MutableUnsignedLongSet purgedHistories, final AbstractFrontendHistory standaloneHistory,
-                final Map<LocalHistoryIdentifier, LocalFrontendHistory> localHistories) {
-            super(persistenceId, clientId, tree);
-            this.purgedHistories = requireNonNull(purgedHistories);
-            this.standaloneHistory = requireNonNull(standaloneHistory);
-            this.localHistories = requireNonNull(localHistories);
-        }
-
-        @Override
-        LocalHistorySuccess handleLocalHistoryRequest(final LocalHistoryRequest<?> request,
-                final RequestEnvelope envelope, final long now) throws RequestException {
-            checkRequestSequence(envelope);
-
-            try {
-                return switch (request) {
-                    case CreateLocalHistoryRequest req -> handleCreateHistory(req, envelope, now);
-                    case DestroyLocalHistoryRequest req -> handleDestroyHistory(req, envelope, now);
-                    case PurgeLocalHistoryRequest req -> handlePurgeHistory(req, envelope, now);
-                    default -> {
-                        LOG.warn("{}: rejecting unsupported request {}", persistenceId(), request);
-                        throw new UnsupportedRequestException(request);
-                    }
-                };
-            } finally {
-                expectNextRequest();
-            }
-        }
-
-        @Override
-        TransactionSuccess<?> handleTransactionRequest(final TransactionRequest<?> request,
-                final RequestEnvelope envelope, final long now) throws RequestException {
-            checkRequestSequence(envelope);
-
-            try {
-                final var lhId = request.getTarget().getHistoryId();
-                final AbstractFrontendHistory history;
-
-                if (lhId.getHistoryId() != 0) {
-                    history = localHistories.get(lhId);
-                    if (history == null) {
-                        if (purgedHistories.contains(lhId.getHistoryId())) {
-                            LOG.warn("{}: rejecting request {} to purged history", persistenceId(), request);
-                            throw new DeadHistoryException(purgedHistories.toRangeSet());
-                        }
-
-                        LOG.warn("{}: rejecting unknown history request {}", persistenceId(), request);
-                        throw new UnknownHistoryException(lastSeenHistory);
-                    }
-                } else {
-                    history = standaloneHistory;
-                }
-
-                return history.handleTransactionRequest(request, envelope, now);
-            } finally {
-                expectNextRequest();
-            }
-        }
-
-        @Override
-        void reconnect() {
-            expectedTxSequence = 0;
-            super.reconnect();
-        }
-
-        @Override
-        void retire() {
-            super.retire();
-
-            // Clear out all transaction chains
-            localHistories.values().forEach(AbstractFrontendHistory::retire);
-            localHistories.clear();
-            standaloneHistory.retire();
-        }
-
-        @Override
-        ToStringHelper addToStringAttributes(final ToStringHelper helper) {
-            return super.addToStringAttributes(helper).add("purgedHistories", purgedHistories);
-        }
-
-        private LocalHistorySuccess handleCreateHistory(final CreateLocalHistoryRequest request,
-                final RequestEnvelope envelope, final long now) throws RequestException {
-            final var historyId = request.getTarget();
-            final var existing = localHistories.get(historyId);
-            if (existing != null) {
-                // History already exists: report success
-                LOG.debug("{}: history {} already exists", persistenceId(), historyId);
-                return new LocalHistorySuccess(historyId, request.getSequence());
-            }
-
-            // We have not found the history. Before we create it we need to check history ID sequencing so that we do
-            // not end up resurrecting a purged history.
-            if (purgedHistories.contains(historyId.getHistoryId())) {
-                LOG.debug("{}: rejecting purged request {}", persistenceId(), request);
-                throw new DeadHistoryException(purgedHistories.toRangeSet());
-            }
-
-            // Update last history we have seen
-            if (lastSeenHistory == null || Long.compareUnsigned(lastSeenHistory, historyId.getHistoryId()) < 0) {
-                lastSeenHistory = historyId.getHistoryId();
-            }
-
-            // We have to send the response only after persistence has completed
-            final var chain = tree().ensureChainedParent(historyId, () -> {
-                LOG.debug("{}: persisted history {}", persistenceId(), historyId);
-                envelope.sendSuccess(new LocalHistorySuccess(historyId, request.getSequence()),
-                    tree().readTime() - now);
-            });
-
-            localHistories.put(historyId, LocalFrontendHistory.create(persistenceId(), chain));
-            LOG.debug("{}: created history {}", persistenceId(), historyId);
-            return null;
-        }
-
-        private LocalHistorySuccess handleDestroyHistory(final DestroyLocalHistoryRequest request,
-                final RequestEnvelope envelope, final long now) {
-            final var id = request.getTarget();
-            final var existing = localHistories.get(id);
-            if (existing == null) {
-                // History does not exist: report success
-                LOG.debug("{}: history {} does not exist, nothing to destroy", persistenceId(), id);
-                return new LocalHistorySuccess(id, request.getSequence());
-            }
-
-            existing.destroy(request.getSequence(), envelope, now);
-            return null;
-        }
-
-        private LocalHistorySuccess handlePurgeHistory(final PurgeLocalHistoryRequest request,
-                final RequestEnvelope envelope, final long now) {
-            final var id = request.getTarget();
-            final var existing = localHistories.remove(id);
-            if (existing == null) {
-                LOG.debug("{}: history {} has already been purged", persistenceId(), id);
-                return new LocalHistorySuccess(id, request.getSequence());
-            }
-
-            LOG.debug("{}: purging history {}", persistenceId(), id);
-            purgedHistories.add(id.getHistoryId());
-            existing.purge(request.getSequence(), envelope, now);
-            return null;
-        }
-
-        private void checkRequestSequence(final RequestEnvelope envelope) throws OutOfSequenceEnvelopeException {
-            if (expectedTxSequence != envelope.getTxSequence()) {
-                throw new OutOfSequenceEnvelopeException(expectedTxSequence);
-            }
-        }
-
-        private void expectNextRequest() {
-            expectedTxSequence++;
-        }
-    }
-
+final class LeaderFrontendState implements Identifiable<ClientIdentifier> {
     private static final Logger LOG = LoggerFactory.getLogger(LeaderFrontendState.class);
 
     private final @NonNull ClientIdentifier clientId;
     private final @NonNull String persistenceId;
     private final @NonNull ShardDataTree tree;
 
+    // Histories which have not been purged
+    private final Map<LocalHistoryIdentifier, LocalFrontendHistory> localHistories;
+
+    // UnsignedLongSet performs automatic merging, hence we keep minimal state tracking information
+    private final MutableUnsignedLongSet purgedHistories;
+
+    // Used for all standalone transactions
+    private final AbstractFrontendHistory standaloneHistory;
+
+    private Long lastSeenHistory = null;
+    private long expectedTxSequence;
     private long lastConnectTicks;
     private long lastSeenTicks;
 
@@ -250,60 +70,191 @@ abstract sealed class LeaderFrontendState implements Identifiable<ClientIdentifi
     // - per-RequestException throw counters
 
     LeaderFrontendState(final String persistenceId, final ClientIdentifier clientId, final ShardDataTree tree) {
+        this(persistenceId, clientId, tree, MutableUnsignedLongSet.of(),
+            StandaloneFrontendHistory.create(persistenceId, clientId, tree), new HashMap<>());
+    }
+
+    LeaderFrontendState(final String persistenceId, final ClientIdentifier clientId, final ShardDataTree tree,
+            final MutableUnsignedLongSet purgedHistories, final AbstractFrontendHistory standaloneHistory,
+            final Map<LocalHistoryIdentifier, LocalFrontendHistory> localHistories) {
         this.persistenceId = requireNonNull(persistenceId);
         this.clientId = requireNonNull(clientId);
         this.tree = requireNonNull(tree);
+        this.purgedHistories = requireNonNull(purgedHistories);
+        this.standaloneHistory = requireNonNull(standaloneHistory);
+        this.localHistories = requireNonNull(localHistories);
         lastSeenTicks = tree.readTime();
     }
 
     @Override
-    public final ClientIdentifier getIdentifier() {
+    public ClientIdentifier getIdentifier() {
         return clientId;
     }
 
-    final String persistenceId() {
+    String persistenceId() {
         return persistenceId;
     }
 
-    final long getLastConnectTicks() {
+    long getLastConnectTicks() {
         return lastConnectTicks;
     }
 
-    final long getLastSeenTicks() {
+    long getLastSeenTicks() {
         return lastSeenTicks;
     }
 
-    final ShardDataTree tree() {
+    ShardDataTree tree() {
         return tree;
     }
 
-    final void touch() {
+    void touch() {
         lastSeenTicks = tree.readTime();
     }
 
     @NonNullByDefault
-    abstract @Nullable LocalHistorySuccess handleLocalHistoryRequest(LocalHistoryRequest<?> request,
-            RequestEnvelope envelope, long now) throws RequestException;
+    @Nullable LocalHistorySuccess handleLocalHistoryRequest(final LocalHistoryRequest<?> request,
+            final RequestEnvelope envelope, final long now) throws RequestException {
+        checkRequestSequence(envelope);
+
+        try {
+            return switch (request) {
+                case CreateLocalHistoryRequest req -> handleCreateHistory(req, envelope, now);
+                case DestroyLocalHistoryRequest req -> handleDestroyHistory(req, envelope, now);
+                case PurgeLocalHistoryRequest req -> handlePurgeHistory(req, envelope, now);
+                default -> {
+                    LOG.warn("{}: rejecting unsupported request {}", persistenceId(), request);
+                    throw new UnsupportedRequestException(request);
+                }
+            };
+        } finally {
+            expectNextRequest();
+        }
+    }
+
+    private @Nullable LocalHistorySuccess handleCreateHistory(final @NonNull CreateLocalHistoryRequest request,
+            final @NonNull RequestEnvelope envelope, final long now) throws RequestException {
+        final var historyId = request.getTarget();
+        final var existing = localHistories.get(historyId);
+        if (existing != null) {
+            // History already exists: report success
+            LOG.debug("{}: history {} already exists", persistenceId(), historyId);
+            return new LocalHistorySuccess(historyId, request.getSequence());
+        }
+
+        // We have not found the history. Before we create it we need to check history ID sequencing so that we do
+        // not end up resurrecting a purged history.
+        if (purgedHistories.contains(historyId.getHistoryId())) {
+            LOG.debug("{}: rejecting purged request {}", persistenceId(), request);
+            throw new DeadHistoryException(purgedHistories.toRangeSet());
+        }
+
+        // Update last history we have seen
+        if (lastSeenHistory == null || Long.compareUnsigned(lastSeenHistory, historyId.getHistoryId()) < 0) {
+            lastSeenHistory = historyId.getHistoryId();
+        }
+
+        // We have to send the response only after persistence has completed
+        final var chain = tree().ensureChainedParent(historyId, () -> {
+            LOG.debug("{}: persisted history {}", persistenceId(), historyId);
+            envelope.sendSuccess(new LocalHistorySuccess(historyId, request.getSequence()),
+                tree().readTime() - now);
+        });
+
+        localHistories.put(historyId, LocalFrontendHistory.create(persistenceId(), chain));
+        LOG.debug("{}: created history {}", persistenceId(), historyId);
+        return null;
+    }
+
+    private @Nullable LocalHistorySuccess handleDestroyHistory(final @NonNull DestroyLocalHistoryRequest request,
+            final @NonNull RequestEnvelope envelope, final long now) {
+        final var id = request.getTarget();
+        final var existing = localHistories.get(id);
+        if (existing == null) {
+            // History does not exist: report success
+            LOG.debug("{}: history {} does not exist, nothing to destroy", persistenceId(), id);
+            return new LocalHistorySuccess(id, request.getSequence());
+        }
+
+        existing.destroy(request.getSequence(), envelope, now);
+        return null;
+    }
+
+    private @Nullable LocalHistorySuccess handlePurgeHistory(final @NonNull PurgeLocalHistoryRequest request,
+            final @NonNull RequestEnvelope envelope, final long now) {
+        final var id = request.getTarget();
+        final var existing = localHistories.remove(id);
+        if (existing == null) {
+            LOG.debug("{}: history {} has already been purged", persistenceId(), id);
+            return new LocalHistorySuccess(id, request.getSequence());
+        }
+
+        LOG.debug("{}: purging history {}", persistenceId(), id);
+        purgedHistories.add(id.getHistoryId());
+        existing.purge(request.getSequence(), envelope, now);
+        return null;
+    }
 
     @NonNullByDefault
-    abstract @Nullable TransactionSuccess<?> handleTransactionRequest(TransactionRequest<?> request,
-            RequestEnvelope envelope, long now) throws RequestException;
+    @Nullable TransactionSuccess<?> handleTransactionRequest(final TransactionRequest<?> request,
+            final RequestEnvelope envelope, final long now) throws RequestException {
+        checkRequestSequence(envelope);
+
+        try {
+            final var lhId = request.getTarget().getHistoryId();
+            final AbstractFrontendHistory history;
+
+            if (lhId.getHistoryId() != 0) {
+                history = localHistories.get(lhId);
+                if (history == null) {
+                    if (purgedHistories.contains(lhId.getHistoryId())) {
+                        LOG.warn("{}: rejecting request {} to purged history", persistenceId(), request);
+                        throw new DeadHistoryException(purgedHistories.toRangeSet());
+                    }
+
+                    LOG.warn("{}: rejecting unknown history request {}", persistenceId(), request);
+                    throw new UnknownHistoryException(lastSeenHistory);
+                }
+            } else {
+                history = standaloneHistory;
+            }
+
+            return history.handleTransactionRequest(request, envelope, now);
+        } finally {
+            expectNextRequest();
+        }
+    }
 
     void reconnect() {
+        expectedTxSequence = 0;
         lastConnectTicks = tree.readTime();
     }
 
     void retire() {
         // Hunt down any transactions associated with this frontend
         tree.retire(clientId);
+
+        // Clear out all transaction chains
+        localHistories.values().forEach(AbstractFrontendHistory::retire);
+        localHistories.clear();
+        standaloneHistory.retire();
     }
 
     @Override
-    public final String toString() {
-        return addToStringAttributes(MoreObjects.toStringHelper(this)).toString();
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("clientId", clientId)
+            .add("nanosAgo", tree.readTime() - lastSeenTicks)
+            .add("purgedHistories", purgedHistories)
+            .toString();
     }
 
-    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
-        return helper.add("clientId", clientId).add("nanosAgo", tree.readTime() - lastSeenTicks);
+    private void checkRequestSequence(final RequestEnvelope envelope) throws OutOfSequenceEnvelopeException {
+        if (expectedTxSequence != envelope.getTxSequence()) {
+            throw new OutOfSequenceEnvelopeException(expectedTxSequence);
+        }
+    }
+
+    private void expectNextRequest() {
+        expectedTxSequence++;
     }
 }
