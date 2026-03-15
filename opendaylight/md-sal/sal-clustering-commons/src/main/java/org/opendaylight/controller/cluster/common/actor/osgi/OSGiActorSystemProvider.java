59321b01699a93b4da5e4b73f6d19a15e5a8ea2c
Robert Varga <robert.varga@pantheon.tech>
2026-03-15 17:42:59 +0100

    Split out DefailtActorSystemProvider
    We need the ability to instantiate an ActorSystem without pulling in
    sal-distributed-datastore. This splits it out into
    sal-clustering-commons.
    
    Change-Id: Ibd6d134f5f40ce3ab7944cbec22b67c804692d8a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/osgi/OSGiActorSystemProvider.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/osgi/OSGiActorSystemProvider.java
@@ -5,16 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.cluster.akka.osgi.impl;
+package org.opendaylight.controller.cluster.common.actor.osgi;
 
 import java.util.concurrent.TimeoutException;
 import org.apache.pekko.actor.ActorSystem;
 import org.apache.pekko.osgi.BundleDelegatingClassLoader;
 import org.opendaylight.controller.cluster.ActorSystemProvider;
 import org.opendaylight.controller.cluster.ActorSystemProviderListener;
-import org.opendaylight.controller.cluster.akka.impl.ActorSystemProviderImpl;
-import org.opendaylight.controller.cluster.akka.impl.AkkaConfigFactory;
 import org.opendaylight.controller.cluster.common.actor.AkkaConfigurationReader;
+import org.opendaylight.controller.cluster.datastore.util.AkkaConfigFactory;
+import org.opendaylight.controller.cluster.datastore.util.DefaultActorSystemProvider;
 import org.opendaylight.yangtools.concepts.AccessControllerCompat;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
 import org.osgi.framework.BundleContext;
@@ -31,14 +31,14 @@ import scala.concurrent.duration.Duration;
 public final class OSGiActorSystemProvider implements ActorSystemProvider {
     private static final Logger LOG = LoggerFactory.getLogger(OSGiActorSystemProvider.class);
 
-    private ActorSystemProviderImpl delegate;
+    private DefaultActorSystemProvider delegate;
 
     @Activate
     public OSGiActorSystemProvider(@Reference final AkkaConfigurationReader reader, final BundleContext bundleContext) {
         LOG.info("Actor System provider starting");
         final var akkaConfig = AkkaConfigFactory.createAkkaConfig(reader);
         final var bundle = bundleContext.getBundle();
-        delegate = new ActorSystemProviderImpl(
+        delegate = new DefaultActorSystemProvider(
             AccessControllerCompat.get(() ->
                 new BundleDelegatingClassLoader(bundle, Thread.currentThread().getContextClassLoader())),
             QuarantinedMonitorActorPropsFactory.createProps(bundleContext, akkaConfig), akkaConfig);
