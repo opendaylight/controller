7c1a76d3b8b1e7fa6bf692745bcb280663d7b6d3
Robert Varga <robert.varga@pantheon.tech>
2026-03-17 14:44:21 +0100

    Add cluster.raft.persisted.VC
    This is the new serialization proxy for VotingConfig.
    
    JIRA: CONTROLLER-2201
    Change-Id: Ie447b1d5f7a4b01ae7f02f97ca8f4e84db86400a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 3616a92867a3cec75a8aa4efa47d023b7541623b)

---
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/persisted/VC.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.raft.persisted;
+
+import java.io.Serializable;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Serialization proxy for {@link VotingConfig}.
+ */
+@NonNullByDefault
+record VC(List<ServerInfo> serverInfo) implements Serializable {
+    VC {
+        serverInfo = List.copyOf(serverInfo);
+    }
+
+    @java.io.Serial
+    private Object readResolve() {
+        return new VotingConfig(serverInfo);
+    }
+}
