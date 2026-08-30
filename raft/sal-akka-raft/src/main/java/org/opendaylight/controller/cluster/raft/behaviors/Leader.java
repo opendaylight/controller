22f91454688d8a959f0f082cf08938d929b4efbe
Robert Varga <robert.varga@pantheon.tech>
2026-08-30 12:53:36 +0200

    Update Leader javadoc
    Reformat to use proper </li> termination and use {@code} blocks for
    formulas.
    
    Change-Id: If358cff2e317b82f09c97c1ffb7c99ff36374f62
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/Leader.java
+++ raft/sal-akka-raft/src/main/java/org/opendaylight/controller/cluster/raft/behaviors/Leader.java
@@ -29,20 +29,16 @@ import org.slf4j.LoggerFactory;
  *
  * <p>Leaders:
  * <ul>
- * <li> Upon election: send initial empty AppendEntries RPCs
- * (heartbeat) to each server; repeat during idle periods to
- * prevent election timeouts (§5.2)
- * <li> If command received from client: append entry to local log,
- * respond after entry applied to state machine (§5.3)
- * <li> If last log index ≥ nextIndex for a follower: send
- * AppendEntries RPC with log entries starting at nextIndex
- * <li> If successful: update nextIndex and matchIndex for
- * follower (§5.3)
- * <li> If AppendEntries fails because of log inconsistency:
- * decrement nextIndex and retry (§5.3)
- * <li> If there exists an N such that N &gt; commitIndex, a majority
- * of matchIndex[i] ≥ N, and log[N].term == currentTerm:
- * set commitIndex = N (§5.3, §5.4).
+ *   <li>Upon election: send initial empty AppendEntries RPCs (heartbeat) to each server; repeat during idle periods to
+ *       prevent election timeouts (§5.2)</li>
+ *   <li>If command received from client: append entry to local log, respond after entry applied to state machine
+ *       (§5.3)</li>
+ *   <li>If last log {@code index ≥ nextIndex} for a follower: send * AppendEntries RPC with log entries starting at
+ *       nextIndex</li>
+ *   <li>If successful: update nextIndex and matchIndex for * follower (§5.3)</li>
+ *   <li>If AppendEntries fails because of log inconsistency: decrement nextIndex and retry (§5.3)</li>
+ *   <li>If there exists an N such that {@code N > commitIndex}, a majority * of {@code matchIndex[i] ≥ N}, and
+ *       {@code log[N].term == currentTerm}: set {@code commitIndex = N} (§5.3, §5.4)</li>
  * </ul>
  */
 // Non-final for testing
