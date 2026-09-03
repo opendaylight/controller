bca6e2b548bba03e802f6f4b917dc07c603aaf23
Robert Varga <robert.varga@pantheon.tech>
2026-09-03 10:51:44 +0200

    Inject DataTreeFactory into introspector factory
    Use constructor injection for static references and inject
    DataTreeFactory, so that it can be propagated further.
    
    JIRA: CONTROLLER-2214
    Change-Id: I4eed2afa92015c2ad142797b6970ea93d0e70a43
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractDatastoreContextIntrospectorFactory.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/main/java/org/opendaylight/controller/cluster/datastore/AbstractDatastoreContextIntrospectorFactory.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.controller.cluster.datastore;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.VisibleForTesting;
 import java.util.Map;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -18,21 +20,31 @@ import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSe
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.tree.api.DataTreeFactory;
 
 @NonNullByDefault
 abstract sealed class AbstractDatastoreContextIntrospectorFactory implements DatastoreContextIntrospectorFactory
         permits DefaultDatastoreContextIntrospectorFactory, OSGiDatastoreContextIntrospectorFactory {
+    private final RaftPolicyResolver raftPolicyResolver;
+    private final DataTreeFactory dataTreeFactory;
+
+    AbstractDatastoreContextIntrospectorFactory(final DataTreeFactory dataTreeFactory,
+            final RaftPolicyResolver raftPolicyResolver) {
+        this.dataTreeFactory = requireNonNull(dataTreeFactory);
+        this.raftPolicyResolver = requireNonNull(raftPolicyResolver);
+    }
+
     @Override
     public DatastoreContextIntrospector newInstance(final LogicalDatastoreType datastoreType,
             final @Nullable Map<String, Object> properties) {
-        final DatastoreContextIntrospector inst = newInstance(datastoreType);
+        final var inst = newInstance(datastoreType);
         inst.update(properties);
         return inst;
     }
 
     @VisibleForTesting
     final DatastoreContextIntrospector newInstance(final LogicalDatastoreType datastoreType) {
-        return newInstance(DatastoreContext.newBuilder(raftPolicyResolver())
+        return newInstance(DatastoreContext.newBuilder(raftPolicyResolver)
                 .logicalStoreType(datastoreType)
                 .tempFileDirectory("./data")
                 .build());
@@ -47,8 +59,6 @@ abstract sealed class AbstractDatastoreContextIntrospectorFactory implements Dat
                 .build())
             .getValue());
     }
-
-    abstract RaftPolicyResolver raftPolicyResolver();
 
     abstract BindingNormalizedNodeSerializer serializer();
 }
