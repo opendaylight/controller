ecf77714c70756f88af3883d574d41ce88b680b8
Robert Varga <robert.varga@pantheon.tech>
2026-09-03 09:36:42 +0200

    Move TerminationMonitor
    This actor is only used by ActorSystemInstanceImpl, which itself should
    not be living in sal-distributed-datastore. Move it to
    sal-clustering-commons and modernize it a bit.
    
    JIRA: CONTROLLER-2214
    Change-Id: Icc5e422065c438fd3887d77dea9de29f7923fca8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/TerminationMonitorActor.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.cluster.common.actor;
+
+import org.apache.pekko.actor.Terminated;
+import org.apache.pekko.actor.UntypedAbstractActor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TerminationMonitorActor extends UntypedAbstractActor {
+    private static final Logger LOG = LoggerFactory.getLogger(TerminationMonitorActor.class);
+
+    public static final String ADDRESS = "quarantined-monitor";
+
+    public TerminationMonitorActor() {
+        LOG.debug("Created TerminationMonitorActor");
+    }
+
+    @Override
+    public void onReceive(final Object message) {
+        switch (message) {
+            case Monitor monitor -> getContext().watch(monitor.getActorRef());
+            case Terminated terminated -> LOG.debug("Actor terminated : {}", terminated.actor());
+            case null, default -> {
+                // no-op
+            }
+        }
+    }
+}
