779a5dbd808fc41f88924f3e703c5c31579987ac
Robert Varga <robert.varga@pantheon.tech>
2026-09-03 12:54:59 +0200

    Split out pekko-support
    sal-clustering-commons contains a ton of things, whose common ground can
    only be characterized as "it is a utility that has to do with
    Pekko/CDS".
    
    The wiring is quite arcane and thorougly unexplained, so we start
    pulling out pure Pekko things into pekko-support.
    
    JIRA: CONTROLLER-2214
    Change-Id: I46009e5b33cdc4a022a180e663fb61e7b90ca311
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ pekko/pekko-support/src/main/java/org/opendaylight/controller/pekko/support/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Support classes for working with Pekko.
+ */
+@org.osgi.annotation.bundle.Export
+package org.opendaylight.controller.pekko.support;
