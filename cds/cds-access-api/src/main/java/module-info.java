68b6976f2c1d0e9a20a2d88761b6e5543e7011f2
Robert Varga <robert.varga@pantheon.tech>
2026-09-15 11:52:28 +0200

    Correct copyright declarations
    We have wrong declarations introduced in previous patches: correct them.
    
    Change-Id: I453804f0b82bfffd201b3b94e4b487e2cfd95564
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- cds/cds-access-api/src/main/java/module-info.java
+++ cds/cds-access-api/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
