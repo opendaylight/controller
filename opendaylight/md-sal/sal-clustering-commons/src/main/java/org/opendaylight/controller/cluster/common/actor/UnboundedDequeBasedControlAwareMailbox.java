41967cc586a38bb9df075f55738f5b64617f1ef8
Robert Varga <robert.varga@pantheon.tech>
2026-09-19 17:45:47 +0200

    Split out ControlAwareMessageDeque
    The message queue is a Serializable contract, make sure we allow it to
    be moved in the future.
    
    The old object readResolves() to the new queue, but we keep the mailbox
    at its old place.
    
    JIRA: CONTROLLER-2214
    Change-Id: I71b06f47dd6887ee37d86db3c556ada8f369292e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/UnboundedDequeBasedControlAwareMailbox.java
+++ opendaylight/md-sal/sal-clustering-commons/src/main/java/org/opendaylight/controller/cluster/common/actor/UnboundedDequeBasedControlAwareMailbox.java
@@ -14,37 +14,39 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import org.apache.pekko.actor.ActorRef;
 import org.apache.pekko.actor.ActorSystem;
-import org.apache.pekko.dispatch.ControlMessage;
 import org.apache.pekko.dispatch.DequeBasedMessageQueueSemantics;
 import org.apache.pekko.dispatch.Envelope;
 import org.apache.pekko.dispatch.MailboxType;
 import org.apache.pekko.dispatch.ProducesMessageQueue;
 import org.apache.pekko.dispatch.UnboundedControlAwareMailbox;
+import org.opendaylight.controller.pekko.support.dispatch.ControlAwareMessageDeque;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Option;
 
 /**
- * An unbounded ControlAwareMailbox that also supports DequeBasedMessageQueueSemantics so it can be used with
+ * An unbounded ControlAwareMailbox that also supports {@link DequeBasedMessageQueueSemantics} so it can be used with
  * persistent actors which use stashing.
  *
  * @author Thomas Pantelis
  */
-public class UnboundedDequeBasedControlAwareMailbox implements MailboxType,
-        ProducesMessageQueue<UnboundedDequeBasedControlAwareMailbox.MessageQueue> {
+public class UnboundedDequeBasedControlAwareMailbox
+        implements MailboxType, ProducesMessageQueue<ControlAwareMessageDeque> {
     private static final Logger LOG = LoggerFactory.getLogger(UnboundedDequeBasedControlAwareMailbox.class);
     private static final String NORMAL_QUEUE_SIZE = "normal-q-size";
     private static final String CONTROL_QUEUE_SIZE = "control-q-size";
     private static final String TOTAL_QUEUE_SIZE = "total-q-size";
 
-    public UnboundedDequeBasedControlAwareMailbox(ActorSystem.Settings settings, Config config) {
+    public UnboundedDequeBasedControlAwareMailbox(final ActorSystem.Settings settings, final Config config) {
+        // nothing here
     }
 
     @Override
-    public MessageQueue create(Option<ActorRef> owner, Option<ActorSystem> system) {
+    public ControlAwareMessageDeque create(final Option<ActorRef> owner,
+            final Option<ActorSystem> system) {
         LOG.debug("Creating MessageQueue for {}", owner);
 
-        final MessageQueue queue = new MessageQueue();
+        final var queue = new ControlAwareMessageDeque();
 
         MeteredBoundedMailbox.registerMetric(owner, NORMAL_QUEUE_SIZE, (Gauge<Integer>) () -> queue.queue().size());
         MeteredBoundedMailbox.registerMetric(owner, CONTROL_QUEUE_SIZE,
@@ -54,34 +56,40 @@ public class UnboundedDequeBasedControlAwareMailbox implements MailboxType,
         return queue;
     }
 
+    @Deprecated(since = "14.0.0", forRemoval = true)
     static class MessageQueue extends UnboundedControlAwareMailbox.MessageQueue
             implements DequeBasedMessageQueueSemantics {
+        @java.io.Serial
         private static final long serialVersionUID = 1L;
 
         private final Deque<Envelope> controlQueue = new ConcurrentLinkedDeque<>();
         private final Deque<Envelope> queue = new ConcurrentLinkedDeque<>();
 
+        private MessageQueue() {
+            // hidden on purpose
+        }
+
         @Override
+        @Deprecated(since = "14.0.0", forRemoval = true)
         public Queue<Envelope> controlQueue() {
-            return controlQueue;
+            throw new UnsupportedOperationException();
         }
 
         @Override
+        @Deprecated(since = "14.0.0", forRemoval = true)
         public Queue<Envelope> queue() {
-            return queue;
+            throw new UnsupportedOperationException();
         }
 
         @Override
-        public void enqueueFirst(ActorRef actor, Envelope envelope) {
-            final Object message = envelope.message();
-            LOG.trace("enqueueFirst: actor {}, message type: {}", actor, message.getClass());
-            if (message instanceof ControlMessage) {
-                LOG.trace("Adding {} to the ControlMessage queue", message.getClass());
-                controlQueue.addFirst(envelope);
-            } else {
-                LOG.trace("Adding {} to the normal queue", message.getClass());
-                queue.addFirst(envelope);
-            }
+        @Deprecated(since = "14.0.0", forRemoval = true)
+        public void enqueueFirst(final ActorRef actor, final Envelope envelope) {
+            throw new UnsupportedOperationException();
+        }
+
+        @java.io.Serial
+        private Object readResolve() {
+            return new ControlAwareMessageDeque(controlQueue, queue);
         }
     }
 }
