6f9c87cfd28bd09c3ec9ebc5f082cbd84cee4070
Robert Varga <robert.varga@pantheon.tech>
2026-09-25 03:03:05 +0200

    Moderniaz actors.dds tests
    Migrate to JUnit 6.
    
    Change-Id: I49d7628d414b735d060edff55767f2f869d4a6a9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/actors/dds/RemoteProxyTransactionTest.java
+++ opendaylight/md-sal/sal-distributed-datastore/src/test/java/org/opendaylight/controller/cluster/databroker/actors/dds/RemoteProxyTransactionTest.java
@@ -7,19 +7,13 @@
  */
 package org.opendaylight.controller.cluster.databroker.actors.dds;
 
-import static org.hamcrest.CoreMatchers.both;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.isA;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.opendaylight.controller.cluster.databroker.actors.dds.TestUtils.assertFutureEquals;
 
-import com.google.common.util.concurrent.FluentFuture;
-import com.google.common.util.concurrent.ListenableFuture;
-import java.util.List;
 import java.util.Optional;
-import org.apache.pekko.testkit.TestProbe;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.controller.cluster.access.commands.ExistsTransactionRequest;
 import org.opendaylight.controller.cluster.access.commands.ExistsTransactionSuccess;
 import org.opendaylight.controller.cluster.access.commands.ModifyTransactionRequest;
@@ -41,188 +35,178 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.tree.api.DataTreeSnapshot;
 
-public class RemoteProxyTransactionTest extends AbstractProxyTransactionTest<RemoteProxyTransaction> {
+class RemoteProxyTransactionTest extends AbstractProxyTransactionTest<RemoteProxyTransaction> {
     @Override
-    protected RemoteProxyTransaction createTransaction(final ProxyHistory parent, final TransactionIdentifier id,
-                                                       final DataTreeSnapshot snapshot) {
+    RemoteProxyTransaction createTransaction(final ProxyHistory parent, final TransactionIdentifier id,
+            final DataTreeSnapshot snapshot) {
         mockForRemote();
         return new RemoteProxyTransaction(parent, TRANSACTION_ID, false, false, false);
     }
 
-    @Override
     @Test
-    public void testExists() throws Exception {
-        final TransactionTester<RemoteProxyTransaction> tester = getTester();
-        final FluentFuture<Boolean> exists = transaction.exists(PATH_1);
-        final ExistsTransactionRequest req = tester.expectTransactionRequest(ExistsTransactionRequest.class);
+    @Override
+    void testExists() throws Exception {
+        final var tester = getTester();
+        final var exists = transaction.exists(PATH_1);
+        final var req = tester.expectTransactionRequest(ExistsTransactionRequest.class);
         final boolean existsResult = true;
         tester.replySuccess(new ExistsTransactionSuccess(TRANSACTION_ID, req.getSequence(), existsResult));
         assertFutureEquals(existsResult, exists);
     }
 
-    @Override
     @Test
-    public void testRead() throws Exception {
-        final TransactionTester<RemoteProxyTransaction> tester = getTester();
-        final FluentFuture<Optional<NormalizedNode>> read = transaction.read(PATH_2);
-        final ReadTransactionRequest req = tester.expectTransactionRequest(ReadTransactionRequest.class);
-        final Optional<NormalizedNode> result = Optional.of(DATA_1);
+    @Override
+    void testRead() throws Exception {
+        final var tester = getTester();
+        final var read = transaction.read(PATH_2);
+        final var req = tester.expectTransactionRequest(ReadTransactionRequest.class);
+        final var result = Optional.<NormalizedNode>of(DATA_1);
         tester.replySuccess(new ReadTransactionSuccess(TRANSACTION_ID, req.getSequence(), result));
         assertFutureEquals(result, read);
     }
 
-    @Override
     @Test
-    public void testWrite() {
-        final YangInstanceIdentifier path = PATH_1;
+    @Override
+    void testWrite() {
+        final var path = PATH_1;
         testModification(() -> transaction.write(path, DATA_1), TransactionWrite.class, path);
     }
 
-    @Override
     @Test
-    public void testMerge() {
-        final YangInstanceIdentifier path = PATH_2;
+    @Override
+    void testMerge() {
+        final var path = PATH_2;
         testModification(() -> transaction.merge(path, DATA_2), TransactionMerge.class, path);
     }
 
-    @Override
     @Test
-    public void testDelete() {
+    @Override
+    void testDelete() {
         final YangInstanceIdentifier path = PATH_3;
         testModification(() -> transaction.delete(path), TransactionDelete.class, path);
     }
 
-    @Override
     @Test
-    public void testDirectCommit() throws Exception {
+    @Override
+    void testDirectCommit() throws Exception {
         transaction.seal();
-        final ListenableFuture<Boolean> result = transaction.directCommit();
-        final TransactionTester<RemoteProxyTransaction> tester = getTester();
-        final ModifyTransactionRequest req = tester.expectTransactionRequest(ModifyTransactionRequest.class);
+        final var result = transaction.directCommit();
+        final var tester = getTester();
+        final var req = tester.expectTransactionRequest(ModifyTransactionRequest.class);
         assertEquals(Optional.of(PersistenceProtocol.SIMPLE), req.getPersistenceProtocol());
         tester.replySuccess(new TransactionCommitSuccess(TRANSACTION_ID, req.getSequence()));
         assertFutureEquals(true, result);
     }
 
-    @Override
     @Test
-    public void testCanCommit() {
-        testRequestResponse(transaction::canCommit, ModifyTransactionRequest.class,
-                TransactionCanCommitSuccess::new);
+    @Override
+    void testCanCommit() {
+        testRequestResponse(transaction::canCommit, ModifyTransactionRequest.class, TransactionCanCommitSuccess::new);
     }
 
-    @Override
     @Test
-    public void testPreCommit() {
+    @Override
+    void testPreCommit() {
         testRequestResponse(transaction::preCommit, TransactionPreCommitRequest.class,
                 TransactionPreCommitSuccess::new);
     }
 
-    @Override
     @Test
-    public void testDoCommit() {
+    @Override
+    void testDoCommit() {
         testRequestResponse(transaction::doCommit, TransactionDoCommitRequest.class, TransactionCommitSuccess::new);
     }
 
-    @Override
     @Test
-    public void testForwardToRemoteAbort() {
-        final TestProbe probe = createProbe();
-        final TransactionAbortRequest request = new TransactionAbortRequest(TRANSACTION_ID, 0L, probe.ref());
-        testForwardToRemote(request, TransactionAbortRequest.class);
-
+    @Override
+    void testForwardToRemoteAbort() {
+        testForwardToRemote(new TransactionAbortRequest(TRANSACTION_ID, 0L, createProbe().ref()),
+            TransactionAbortRequest.class);
     }
 
     @Override
-    public void testForwardToRemoteCommit() {
-        final TestProbe probe = createProbe();
-        final TransactionAbortRequest request = new TransactionAbortRequest(TRANSACTION_ID, 0L, probe.ref());
-        testForwardToRemote(request, TransactionAbortRequest.class);
+    void testForwardToRemoteCommit() {
+        testForwardToRemote(new TransactionAbortRequest(TRANSACTION_ID, 0L, createProbe().ref()),
+            TransactionAbortRequest.class);
     }
 
     @Test
-    public void testForwardToRemoteModifyCommitSimple() {
-        final TestProbe probe = createProbe();
-        final ModifyTransactionRequest request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
+    void testForwardToRemoteModifyCommitSimple() {
+        final var probe = createProbe();
+        final var request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
             .setSequence(0L)
             .setCommit(false)
             .build();
-        final ModifyTransactionRequest received = testForwardToRemote(request, ModifyTransactionRequest.class);
+        final var received = testForwardToRemote(request, ModifyTransactionRequest.class);
         assertEquals(request.getPersistenceProtocol(), received.getPersistenceProtocol());
         assertEquals(request.getModifications(), received.getModifications());
         assertEquals(request.getTarget(), received.getTarget());
     }
 
     @Test
-    public void testForwardToRemoteModifyCommit3Phase() {
-        final TestProbe probe = createProbe();
-        final ModifyTransactionRequest request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
+    void testForwardToRemoteModifyCommit3Phase() {
+        final var probe = createProbe();
+        final var request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
             .setSequence(0L)
             .setCommit(true)
             .build();
-        final ModifyTransactionRequest received = testForwardToRemote(request, ModifyTransactionRequest.class);
+        final var received = testForwardToRemote(request, ModifyTransactionRequest.class);
         assertEquals(request.getPersistenceProtocol(), received.getPersistenceProtocol());
         assertEquals(request.getModifications(), received.getModifications());
         assertEquals(request.getTarget(), received.getTarget());
     }
 
     @Test
-    public void testForwardToRemoteModifyAbort() {
-        final TestProbe probe = createProbe();
-        final ModifyTransactionRequest request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
+    void testForwardToRemoteModifyAbort() {
+        final var probe = createProbe();
+        final var request = ModifyTransactionRequest.builder(TRANSACTION_ID, probe.ref())
             .setSequence(0L)
             .setAbort()
             .build();
-        final ModifyTransactionRequest received = testForwardToRemote(request, ModifyTransactionRequest.class);
+        final var received = testForwardToRemote(request, ModifyTransactionRequest.class);
         assertEquals(request.getTarget(), received.getTarget());
         assertEquals(Optional.of(PersistenceProtocol.ABORT), received.getPersistenceProtocol());
     }
 
     @Test
-    public void testForwardToRemoteModifyRead() {
-        final TestProbe probe = createProbe();
-        final ReadTransactionRequest request =
-                new ReadTransactionRequest(TRANSACTION_ID, 0L, probe.ref(), PATH_1, false);
-        final ReadTransactionRequest received = testForwardToRemote(request, ReadTransactionRequest.class);
+    void testForwardToRemoteModifyRead() {
+        final var probe = createProbe();
+        final var request = new ReadTransactionRequest(TRANSACTION_ID, 0L, probe.ref(), PATH_1, false);
+        final var received = testForwardToRemote(request, ReadTransactionRequest.class);
         assertEquals(request.getTarget(), received.getTarget());
         assertEquals(request.getPath(), received.getPath());
     }
 
     @Test
-    public void testForwardToRemoteModifyExists() {
-        final TestProbe probe = createProbe();
-        final ExistsTransactionRequest request =
-                new ExistsTransactionRequest(TRANSACTION_ID, 0L, probe.ref(), PATH_1, false);
-        final ExistsTransactionRequest received = testForwardToRemote(request, ExistsTransactionRequest.class);
+    void testForwardToRemoteModifyExists() {
+        final var probe = createProbe();
+        final var request = new ExistsTransactionRequest(TRANSACTION_ID, 0L, probe.ref(), PATH_1, false);
+        final var received = testForwardToRemote(request, ExistsTransactionRequest.class);
         assertEquals(request.getTarget(), received.getTarget());
         assertEquals(request.getPath(), received.getPath());
     }
 
     @Test
-    public void testForwardToRemoteModifyPreCommit() {
-        final TestProbe probe = createProbe();
-        final TransactionPreCommitRequest request =
-                new TransactionPreCommitRequest(TRANSACTION_ID, 0L, probe.ref());
-        final TransactionPreCommitRequest received = testForwardToRemote(request, TransactionPreCommitRequest.class);
+    void testForwardToRemoteModifyPreCommit() {
+        final var probe = createProbe();
+        final var request = new TransactionPreCommitRequest(TRANSACTION_ID, 0L, probe.ref());
+        final var received = testForwardToRemote(request, TransactionPreCommitRequest.class);
         assertEquals(request.getTarget(), received.getTarget());
     }
 
     @Test
-    public void testForwardToRemoteModifyDoCommit() {
-        final TestProbe probe = createProbe();
-        final TransactionDoCommitRequest request =
-                new TransactionDoCommitRequest(TRANSACTION_ID, 0L, probe.ref());
-        final TransactionDoCommitRequest received = testForwardToRemote(request, TransactionDoCommitRequest.class);
+    void testForwardToRemoteModifyDoCommit() {
+        final var probe = createProbe();
+        final var request = new TransactionDoCommitRequest(TRANSACTION_ID, 0L, probe.ref());
+        final var received = testForwardToRemote(request, TransactionDoCommitRequest.class);
         assertEquals(request.getTarget(), received.getTarget());
     }
-
 
     private <T extends TransactionModification> void testModification(final Runnable modification, final Class<T> cls,
             final YangInstanceIdentifier expectedPath) {
         modification.run();
-        final ModifyTransactionRequest request = transaction.commitRequest(false);
-        final List<TransactionModification> modifications = request.getModifications();
-        assertEquals(1, modifications.size());
-        assertThat(modifications, hasItem(both(isA(cls)).and(hasPath(expectedPath))));
+        final var request = transaction.commitRequest(false);
+        final var mod = assertInstanceOf(cls, assertThat(request.getModifications()).hasSize(1).first().actual());
+        assertEquals(expectedPath, mod.getPath());
     }
 }
